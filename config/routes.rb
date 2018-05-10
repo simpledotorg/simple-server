aa7a18d8b98d3d400cb2b081e3e3a5ab2a3ea315
govind <govindkrjoshi@gmail.com>
2018-05-10 18:29:14 +0530

    Use scope, instead of resources for routing patient URLs.
    Co-authored-by: Srihari Sriraman <srihari@nilenso.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :patients, only: [] do
+      scope '/patients' do
         post 'sync', to: 'patients#sync_from_user'
       end
     end
