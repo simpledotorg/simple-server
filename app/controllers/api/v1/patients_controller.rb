0b83b716f674e224f0483764102cdd71ef844128
Tim Cheadle <tim@rationalmeans.com>
2018-05-17 18:10:48 -0400

    Style cleanup

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::PatientsController < APIController
-
   def sync_from_user
-    errors = patient_params.reduce([]) do |errors, single_patient_params|
+    errors = patients_params.reduce([]) do |errors, single_patient_params|
       patient = MergePatientService.new(single_patient_params).merge
       errors << patient.errors_hash if patient.has_errors?
       errors
@@ -13,19 +12,25 @@ class Api::V1::PatientsController < APIController
 
   def sync_to_user
     latest_record_timestamp = params[:latest_record_timestamp].to_time
-    patients_to_sync        = Patient.distinct
-                                .left_outer_joins(:address)
-                                .left_outer_joins(:phone_numbers)
-                                .where('patients.updated_on_server_at >= ?
-                                        or addresses.updated_on_server_at >= ?
-                                        or phone_numbers.updated_on_server_at >= ?',
-                                       latest_record_timestamp, latest_record_timestamp, latest_record_timestamp)
+
+    patients_to_sync = Patient.distinct
+      .left_outer_joins(:address)
+      .left_outer_joins(:phone_numbers)
+      .where(
+        'patients.updated_on_server_at >= ?
+        or addresses.updated_on_server_at >= ?
+        or phone_numbers.updated_on_server_at >= ?',
+        latest_record_timestamp,
+        latest_record_timestamp,
+        latest_record_timestamp
+      )
+
     render json: { patients: patients_to_sync.map(&:nested_hash) }, status: :ok
   end
 
   private
 
-  def patient_params
+  def patients_params
     permitted_address_params = %i[id street_address colony village district state country pin created_at updated_at]
     permitted_phone_number_params = %i[id number type active created_at updated_at]
 
