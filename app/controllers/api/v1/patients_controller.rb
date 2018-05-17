762c9c81db8e116e0a3f68a9bed7b3c081f69548
Tim Cheadle <tim@rationalmeans.com>
2018-05-17 18:06:26 -0400

    Cleaned up code order and moved to private methods

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,4 +1,44 @@
 class Api::V1::PatientsController < ApplicationController
+  def sync_from_user
+    errors = patients_params.reduce([]) do |errors, single_patient_params|
+      patient = merge_patient(single_patient_params)
+      errors << patient.errors_hash if patient.has_errors?
+      errors
+    end
+
+    response = { errors: errors.nil? ? nil : errors }
+    render json: response, status: :ok
+  end
+
+  private
+
+  def patients_params
+    permitted_address_params = %i[id street_address colony village district state country pin created_at updated_at]
+    permitted_phone_number_params = %i[id number type active created_at updated_at]
+
+    params.require(:patients).map do |single_patient_params|
+      single_patient_params.permit(
+        :id,
+        :full_name,
+        :age_when_created,
+        :gender,
+        :status,
+        :date_of_birth,
+        :created_at,
+        :updated_at,
+        phone_numbers: [permitted_phone_number_params],
+        address: permitted_address_params
+      )
+    end
+  end
+
+  def merge_patient(single_patient_params)
+    patient = Patient.new(single_patient_params.except(:address, :phone_numbers))
+    patient.address = merge_address(single_patient_params[:address]) if single_patient_params[:address].present?
+    patient.phone_numbers = merge_phone_numbers(single_patient_params)
+
+    MergeRecord.merge_by_id(patient)
+  end
 
   def merge_address(address_params)
     address = Address.new(address_params)
@@ -13,44 +53,6 @@ class Api::V1::PatientsController < ApplicationController
   def merge_phone_numbers(single_patient_params)
     single_patient_params[:phone_numbers].to_a.map do |phone_number_params|
       merge_phone_number phone_number_params
-    end
-  end
-
-  def merge_patient(single_patient_params)
-    patient = Patient.new(single_patient_params.except(:address, :phone_numbers))
-    patient.address = merge_address(single_patient_params[:address]) if single_patient_params[:address].present?
-    patient.phone_numbers = merge_phone_numbers(single_patient_params)
-    MergeRecord.merge_by_id(patient)
-  end
-
-  def sync_from_user
-    errors = patient_params.reduce([]) do |errors, single_patient_params|
-      patient = merge_patient single_patient_params
-      errors << patient.errors_hash if patient.has_errors?
-      errors
-    end
-
-    response = { errors: errors.nil? ? nil : errors }
-    render json: response, status: :ok
-  end
-
-  private
-
-  def patient_params
-    permitted_address_params      = %i[id street_address colony village district state country pin created_at updated_at]
-    permitted_phone_number_params = %i[id number type active created_at updated_at]
-    params.require(:patients).map do |single_patient_params|
-      single_patient_params.permit(
-        :id,
-        :full_name,
-        :age_when_created,
-        :gender,
-        :status,
-        :date_of_birth,
-        :created_at,
-        :updated_at,
-        phone_numbers: [permitted_phone_number_params],
-        address:       permitted_address_params)
     end
   end
 end
