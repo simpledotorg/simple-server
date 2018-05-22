981b5117cd6c224a47cb6a9bd222e8baa4633990
Srihari Sriraman <srihari@nilenso.com>
2018-05-22 18:59:43 +0530

    Add documentation for sync mechanism and caveats.

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -29,7 +29,72 @@ This API spec documents the endpoints that the devices (that are offline to vary
 ## Nesting resources
 The APIs have been designed to provide an optimal balance between accuracy and simplicity. Some of the APIs (patients) will be nested, and some other (blood pressures) will be flat.
 ## Authentication
-TODO',
+TODO
+
+## Sync
+
+### Why it needs to exist
+
+Network connectivity on phones in rural
+areas can be low, and patchy. So, the app needs to work offline,
+as much as possible. The sync mechanism exists to allow sharing
+records of patients, blook pressures, etc across devices.
+
+### Levers
+
+Physical resource constraints such as battery life, or network
+are not under our control. In order for the app to adapt
+well to the constraints, we need to have knobs on:
+
+1. Batch size: how many records to send or fetch
+2. Sync frequency: how often sending or fetchin happens
+
+Preferably, these levers are modifyable at run-time, per device.
+
+### Mechanism
+
+1. Send records from device to server
+
+The device needs to keep track of records that need to be
+synced. These can be new records, or records that have one or more
+fields updated. These records need to be formatted into the payload
+schemas as defined in the individual APIs below. The API does not
+differentiate between new and updated records; this is handled by the
+server.  These payloads then need to be sent in batches, where each
+batch is inherently independent of the previous one. However, it is
+important to _send_ all records that need syncing, before fetching
+records from the server.
+
+2. Fetch records from server to device
+
+When fetching records for the first time, the `first_time` query
+parameter should be set. The server will then send back a number of
+records as defined by the `number_of_records` query parameter. This is
+essentially the "batch_size". The first response also includes a
+`latest_record_timestamp`, that needs to be sent with the next
+request, in order to retrieve more records from the server. If there
+are no more records to be sent from the server, it returns an empty
+list.
+
+3. Merge records on device
+
+The server may send back the same record multiple times. The merging
+functionality on the device needs to be idempotent for this reason.
+
+### Caveats
+
+The computation of records to be sent is currently based on the
+`updated_on_server_at` field on the server, and the
+`latest_record_timestamp` in the query parameters. This implies, that
+if there are multiple records with the same timestamp, the same record
+will be sent multiple times. And if there are as many records with the
+same timestamp as the batch size, the same response is sent over and
+over again, effectively breaking the batching mechanism.
+
+This implies we need sequencing of records on the server. We rely on
+timestamps for this now. But we could have a separate sequence, or an
+immutable and sequenced audit log to do this.
+',
         version: 'v1',
         title: 'RedApp Server',
         'x-logo' => {
