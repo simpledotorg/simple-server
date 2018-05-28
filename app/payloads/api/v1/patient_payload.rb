892c066e669f641031597b395095092cf241f7ae
govind <govindkrjoshi@gmail.com>
2018-05-28 15:43:51 +0530

    [WIP] Rename attributes for models

--- app/payloads/api/v1/patient_payload.rb
+++ app/payloads/api/v1/patient_payload.rb
@@ -22,6 +22,11 @@ class Api::V1::PatientPayload
   validate :validate_schema
   validate :presence_of_age
 
+  def initialize(attributes = {})
+    @attributes = attributes
+    super(attributes)
+  end
+
   def presence_of_age
     unless date_of_birth.present? || (age.present? && age_updated_at.present?)
       errors.add(:age, 'Either date_of_birth or age and age_updated_at should be present')
@@ -44,5 +49,25 @@ class Api::V1::PatientPayload
     JSON::Validator.fully_validate(schema_with_definitions, to_json).each do |error_string|
       errors.add(:schema, error_string)
     end
+  end
+
+  def build_model
+    patient_address       = Address.new(rename_attributes(address)) if address.present?
+    patient_phone_numbers = phone_numbers.map { |phone_number| PatientPhoneNumber.new(rename_attributes(phone_number))} if phone_numbers.present?
+    patient               = Patient.new(@attributes.except('address', 'phone_numbers'))
+    patient.address       = patient_address
+    patient.phone_numbers = patient_phone_numbers
+    patient
+  end
+
+  private
+
+  def rename_attributes(attributes)
+    rename_keys = {
+      created_at: :device_created_at,
+      updated_at: :device_updated_at
+    }.with_indifferent_access
+
+    Hash[attributes.map { |key, value| rename_keys[key].present? ? [rename_keys[key], value] : [key, value] }]
   end
 end
