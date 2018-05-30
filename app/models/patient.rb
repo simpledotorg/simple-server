4fe5175e95145c2a2662ce414d0f2127e574a751
govind <govindkrjoshi@gmail.com>
2018-05-30 14:30:00 +0530

    Remove unnessary method errors_hash from models

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -10,14 +10,6 @@ class Patient < ApplicationRecord
   validates_associated :address, if: :address
   validates_associated :phone_numbers, if: :phone_numbers
 
-  def errors_hash
-    errors.to_hash.merge(
-      id:            id,
-      address:       address.present? ? address.errors_hash : nil,
-      phone_numbers: phone_numbers.map(&:errors_hash)
-    )
-  end
-
   def nested_hash(options = {})
     as_json(options.merge(
       except:  %i[address_id  updated_on_server_at],
