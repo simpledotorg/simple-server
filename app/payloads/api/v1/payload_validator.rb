abf91b84be6a25f6267eaae9739c0b0f6cc286a7
Srihari Sriraman <srihari@nilenso.com>
2018-06-01 18:12:31 +0530

    Send async sentry alerts for validation failures
    - Add a simple active job as per sentry documentation to send error asynchronously.

--- app/payloads/api/v1/payload_validator.rb
+++ app/payloads/api/v1/payload_validator.rb
@@ -1,5 +1,9 @@
 class Api::V1::PayloadValidator
   include ActiveModel::Model
+  include ActiveModel::Validations::Callbacks
+  include ActiveModel::Validations
+
+  after_validation :capture_error
 
   def initialize(attributes = {})
     @attributes = attributes.to_hash.with_indifferent_access
@@ -18,5 +22,22 @@ class Api::V1::PayloadValidator
     JSON::Validator.fully_validate(schema_with_definitions, to_json).each do |error_string|
       errors.add(:schema, error_string)
     end
+  end
+
+  private
+
+  def capture_error
+    return unless errors.present?
+
+    Raven.capture_message(
+      'Validation Error',
+      logger: 'logger',
+      extra: {
+        schema_errors: errors[:schema],
+        non_schema_errors: errors.to_hash.except(:schema),
+        attributes: @attributes
+      },
+      tags: { validation: 'schema' }
+    )
   end
 end
