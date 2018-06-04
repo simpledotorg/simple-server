fbdebd0b64d45862d5b034c556b25729dad3e4a5
Srihari Sriraman <srihari@nilenso.com>
2018-06-04 15:29:57 +0530

    Send async sentry alerts for validation failures
    * Send async sentry alerts for validation failures
    
    - Add a simple active job as per sentry documentation to send error asynchronously.
    - Report full payloads with errors, to work around sentry's rate limits.
    
    Co-authored-by: Govind Krishna Joshi <govind@nilenso.com>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,13 @@
+class SentryJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(event)
+    Raven.send_event(event)
+  end
+end
+
 Raven.configure do |config|
-  config.environments = %w(staging production)
+  config.environments = %w[staging production]
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.async = ->(event) { SentryJob.perform_later(event) }
 end
