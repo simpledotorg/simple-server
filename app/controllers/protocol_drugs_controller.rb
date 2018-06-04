044b0ae9d7e63ca0a9f8bc785eb45c9d9a9261ae
Srihari Sriraman <srihari@nilenso.com>
2018-06-04 16:46:22 +0530

    Remove comments from generated controllers.

--- app/controllers/protocol_drugs_controller.rb
+++ app/controllers/protocol_drugs_controller.rb
@@ -1,29 +1,21 @@
 class ProtocolDrugsController < ApplicationController
-  before_action :set_protocol_drug, only: [:show, :edit, :update, :destroy]
-  before_action :set_protocols, only: [:create, :edit, :update, :new]
+  before_action :set_protocol_drug, only: %i[show edit update destroy]
+  before_action :set_protocols, only: %i[create edit update new]
 
-  # GET /protocol_drugs
-  # GET /protocol_drugs.json
   def index
     @protocol_drugs = ProtocolDrug.all
   end
 
-  # GET /protocol_drugs/1
-  # GET /protocol_drugs/1.json
   def show
   end
 
-  # GET /protocol_drugs/new
   def new
     @protocol_drug = ProtocolDrug.new
   end
 
-  # GET /protocol_drugs/1/edit
   def edit
   end
 
-  # POST /protocol_drugs
-  # POST /protocol_drugs.json
   def create
     @protocol_drug = ProtocolDrug.new(protocol_drug_params)
 
@@ -38,8 +30,6 @@ class ProtocolDrugsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /protocol_drugs/1
-  # PATCH/PUT /protocol_drugs/1.json
   def update
     respond_to do |format|
       if @protocol_drug.update(protocol_drug_params)
@@ -52,8 +42,6 @@ class ProtocolDrugsController < ApplicationController
     end
   end
 
-  # DELETE /protocol_drugs/1
-  # DELETE /protocol_drugs/1.json
   def destroy
     @protocol_drug.destroy
     respond_to do |format|
@@ -68,12 +56,10 @@ class ProtocolDrugsController < ApplicationController
     @protocols = Protocol.all
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_protocol_drug
     @protocol_drug = ProtocolDrug.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def protocol_drug_params
     params.require(:protocol_drug).permit(:name, :dosage, :rxnorm_code, :protocol_id)
   end
--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,28 +1,20 @@
 class ProtocolsController < ApplicationController
-  before_action :set_protocol, only: [:show, :edit, :update, :destroy]
+  before_action :set_protocol, only: %i[show edit update destroy]
 
-  # GET /protocols
-  # GET /protocols.json
   def index
     @protocols = Protocol.all
   end
 
-  # GET /protocols/1
-  # GET /protocols/1.json
   def show
   end
 
-  # GET /protocols/new
   def new
     @protocol = Protocol.new
   end
 
-  # GET /protocols/1/edit
   def edit
   end
 
-  # POST /protocols
-  # POST /protocols.json
   def create
     @protocol = Protocol.new(protocol_params)
 
@@ -37,8 +29,6 @@ class ProtocolsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /protocols/1
-  # PATCH/PUT /protocols/1.json
   def update
     respond_to do |format|
       if @protocol.update(protocol_params)
@@ -51,8 +41,6 @@ class ProtocolsController < ApplicationController
     end
   end
 
-  # DELETE /protocols/1
-  # DELETE /protocols/1.json
   def destroy
     @protocol.destroy
     respond_to do |format|
@@ -62,13 +50,12 @@ class ProtocolsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_protocol
-      @protocol = Protocol.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def protocol_params
-      params.require(:protocol).permit(:name, :follow_up_days)
-    end
+  def set_protocol
+    @protocol = Protocol.find(params[:id])
+  end
+
+  def protocol_params
+    params.require(:protocol).permit(:name, :follow_up_days)
+  end
 end
