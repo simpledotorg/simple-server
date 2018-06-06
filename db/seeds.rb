a7cf5b54e82b862e7e7364cd11248db8306f430d
govind <govindkrjoshi@gmail.com>
2018-06-06 14:28:31 +0530

    Move seeding of protocol data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,40 @@ Facility.find_or_create_by(
   state: "Punjab",
   country: "India"
 )
+
+protocol_data = {
+  name:           'Punjab Hypertension Protocol',
+  follow_up_days: 30
+}
+
+protocol_drugs_data = [
+  {
+    name:   'Amlodipine',
+    dosage: '5 mg'
+  },
+  {
+    name:   'Amlodipine',
+    dosage: '10 mg'
+  },
+  {
+    name:   'Telmisartan',
+    dosage: '40 mg'
+  },
+  {
+    name:   'Telmisartan',
+    dosage: '80 mg'
+  },
+  {
+    name:   'Chlorthalidone',
+    dosage: '12.5 mg'
+  },
+  {
+    name:   'Chlorthalidone',
+    dosage: '25 mg'
+  }
+]
+
+protocol = Protocol.create(protocol_data)
+protocol_drugs_data.each do |drug_data|
+  ProtocolDrug.create(drug_data.merge(protocol_id: protocol.id))
+end
