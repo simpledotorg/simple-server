d2ce1b5366adfa81e9932cacb77146084c2ea7e4
Tim Cheadle <tim@rationalmeans.com>
2018-06-06 21:55:51 -0400

    Installed Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
