4143987f1794ea8e5117d67c4d276d74dfe4e4a1
Srihari Sriraman <srihari@nilenso.com>
2018-06-11 14:06:57 +0530

    Add test for relationship with facility.

--- spec/controllers/api/v1/prescription_drugs_controller_spec.rb
+++ spec/controllers/api/v1/prescription_drugs_controller_spec.rb
@@ -14,14 +14,18 @@ RSpec.describe Api::V1::PrescriptionDrugsController, type: :controller do
     it_behaves_like 'a working sync controller updating records'
 
     describe 'creates new prescription drugs' do
-      it 'creates new prescription drugs with associated patient' do
+      it 'creates new prescription drugs with associated patient, and facility' do
         patient = FactoryBot.create(:patient)
+        facility = FactoryBot.create(:facility)
         prescription_drugs = (1..10).map do
-          build_prescription_drug_payload(FactoryBot.build(:prescription_drug, patient: patient))
+          build_prescription_drug_payload(FactoryBot.build(:prescription_drug,
+                                                           patient: patient,
+                                                           facility: facility))
         end
         post(:sync_from_user, params: { prescription_drugs: prescription_drugs }, as: :json)
         expect(PrescriptionDrug.count).to eq 10
         expect(patient.prescription_drugs.count).to eq 10
+        expect(facility.prescription_drugs.count).to eq 10
         expect(response).to have_http_status(200)
       end
     end
