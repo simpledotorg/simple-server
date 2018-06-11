c64b9ffe7bd2b6fd54755b04bfd8abee5bf8d3dd
govind <govindkrjoshi@gmail.com>
2018-06-11 12:35:03 +0530

    Associate patients to users through blood_pressures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   belongs_to :facility
   has_many :blood_pressures
+  has_many :patients, through: :blood_pressures
 
   validates :name, presence: true
   validates :phone_number, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe 'Associations' do
     it { should belong_to(:facility) }
     it { should have_many(:blood_pressures) }
+    it { should have_many(:patients).through(:blood_pressures) }
   end
   describe 'Validations' do
     it { should validate_presence_of(:name)}
