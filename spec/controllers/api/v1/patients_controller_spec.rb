03bc72d89a4f3855bdcc9b5e4833135e4fea8757
Govind Krishna Joshi <govindkrjoshi@gmail.com>
2018-06-21 14:47:08 +0530

    Add feature toggle for authentication
    Add feature toggle for user sync api authentication

--- spec/controllers/api/v1/patients_controller_spec.rb
+++ spec/controllers/api/v1/patients_controller_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::PatientsController, type: :controller do
-  before :each do
-    request_user = FactoryBot.create(:user)
-    request.env['X_USER_ID'] = request_user.id
-    request.env['HTTP_AUTHORIZATION'] = "Bearer #{request_user.access_token}"
-  end
+  let(:request_user) { FactoryBot.create(:user) }
 
   let(:model) { Patient }
 
@@ -20,6 +16,11 @@ RSpec.describe Api::V1::PatientsController, type: :controller do
     it_behaves_like 'a working sync controller creating records'
 
     describe 'creates new patients' do
+      before :each do
+        request.env['X_USER_ID'] = request_user.id
+        request.env['HTTP_AUTHORIZATION'] = "Bearer #{request_user.access_token}"
+      end
+
       it 'creates new patients' do
         patients = (1..10).map { build_patient_payload }
         post(:sync_from_user, params: { patients: patients }, as: :json)
@@ -44,6 +45,11 @@ RSpec.describe Api::V1::PatientsController, type: :controller do
     end
 
     describe 'updates patients' do
+      before :each do
+        request.env['X_USER_ID'] = request_user.id
+        request.env['HTTP_AUTHORIZATION'] = "Bearer #{request_user.access_token}"
+      end
+
       let(:existing_patients) { FactoryBot.create_list(:patient, 10) }
       let(:updated_patients_payload) { existing_patients.map { |patient| updated_patient_payload patient } }
 
