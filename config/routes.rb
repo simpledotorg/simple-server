669281aadbcce574c42db713231d0adaadd0b1cc
govind <govindkrjoshi@gmail.com>
2018-06-27 14:55:44 +0530

    Add a route to handle ping requests

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'ping', to: 'pings#show'
       post 'login', to: 'logins#login_user'
 
       scope '/patients' do
