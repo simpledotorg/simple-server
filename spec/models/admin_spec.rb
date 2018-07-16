2a260277cce344bdced71f521005f0ae11db6d9e
Tim Cheadle <tim@rationalmeans.com>
2018-07-16 17:58:54 -0400

    Added CVHO role

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Admin, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:email)}
     it { should validate_presence_of(:password)}
+    it { should validate_presence_of(:role) }
   end
 end
