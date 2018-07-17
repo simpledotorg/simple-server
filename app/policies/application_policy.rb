b895b3e1875db440da3beb2b6871502628cb18b4
Tim Cheadle <tim@rationalmeans.com>
2018-07-16 20:08:56 -0400

    Default to allow admins to do everything

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -7,7 +7,7 @@ class ApplicationPolicy
   end
 
   def index?
-    false
+    user.admin?
   end
 
   def show?
@@ -15,7 +15,7 @@ class ApplicationPolicy
   end
 
   def create?
-    false
+    user.admin?
   end
 
   def new?
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    user.admin?
   end
 
   def edit?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    user.admin?
   end
 
   def scope
