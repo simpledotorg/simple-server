ce420ce84cce0aa70b690a3ccbc5c2b68db4aa0b
Tim Cheadle <tim@rationalmeans.com>
2018-07-26 15:28:35 -0400

    Use Rails config object

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Use SendGrid as our SMTP provider
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     user_name: ENV.fetch("SENDGRID_USERNAME"),
     password: ENV.fetch("SENDGRID_PASSWORD"),
     domain: "simple.org",
