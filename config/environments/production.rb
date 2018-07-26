3ad9312cd281d265860e56945dfbe8f1abab6d7e
Tim Cheadle <tim@rationalmeans.com>
2018-07-26 13:53:36 -0400

    Added SendGrid production config and env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Use SendGrid as our SMTP provider
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV.fetch("SENDGRID_USERNAME"),
+    password: ENV.fetch("SENDGRID_PASSWORD"),
+    domain: "simple.org",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
