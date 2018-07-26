2ce6d5989af5464ba10f6098716ba6c3a651a688
Tim Cheadle <tim@rationalmeans.com>
2018-07-26 07:47:34 -0400

    Use Bootstrap for Admin UI (#72)
    * Installed Bootstrap with a basic layout
    
    * Use bootstrap_form for easier form rendering
    
    * Use bootstrap on Devise forms
    
    * Fixed admin to use nested resources
    
    * Added site navigation
    
    * Updated all admin views to use bootstrap
    
    * Make page headings uniform
    
    * Fix failing tests due nesting resources

--- spec/controllers/admin/protocol_drugs_controller_spec.rb
+++ spec/controllers/admin/protocol_drugs_controller_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Admin::ProtocolDrugsController, type: :controller do
 
+  let(:protocol) { FactoryBot.create(:protocol) }
   let(:valid_attributes) {
-    protocol = FactoryBot.create(:protocol)
     FactoryBot.attributes_for(:protocol_drug, protocol_id: protocol.id)
   }
 
   let(:invalid_attributes) {
-    protocol = FactoryBot.create(:protocol)
     FactoryBot.attributes_for(:protocol_drug, name: nil, protocol_id: protocol.id)
   }
 
@@ -19,7 +18,7 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
   describe "GET #index" do
     it "returns a success response" do
       protocol_drug = ProtocolDrug.create! valid_attributes
-      get :index, params: {}
+      get :index, params: { protocol_id: protocol.id }
       expect(response).to be_success
     end
   end
@@ -27,14 +26,14 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       protocol_drug = ProtocolDrug.create! valid_attributes
-      get :show, params: {id: protocol_drug.to_param}
+      get :show, params: { id: protocol_drug.to_param, protocol_id: protocol.id }
       expect(response).to be_success
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
-      get :new, params: {}
+      get :new, params: { protocol_id: protocol.id }
       expect(response).to be_success
     end
   end
@@ -42,7 +41,7 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
   describe "GET #edit" do
     it "returns a success response" do
       protocol_drug = ProtocolDrug.create! valid_attributes
-      get :edit, params: {id: protocol_drug.to_param}
+      get :edit, params: { id: protocol_drug.to_param, protocol_id: protocol.id }
       expect(response).to be_success
     end
   end
@@ -51,19 +50,19 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
     context "with valid params" do
       it "creates a new ProtocolDrug" do
         expect {
-          post :create, params: {protocol_drug: valid_attributes}
+          post :create, params: { protocol_drug: valid_attributes, protocol_id: protocol.id }
         }.to change(ProtocolDrug, :count).by(1)
       end
 
       it "redirects to the list of protocols" do
-        post :create, params: {protocol_drug: valid_attributes}
-        expect(response).to redirect_to([:admin, :protocols])
+        post :create, params: { protocol_drug: valid_attributes, protocol_id: protocol.id }
+        expect(response).to redirect_to([:admin, protocol])
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {protocol_drug: invalid_attributes}
+        post :create, params: { protocol_drug: invalid_attributes, protocol_id: protocol.id }
         expect(response).to be_success
       end
     end
@@ -72,13 +71,12 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        protocol = FactoryBot.create(:protocol)
         FactoryBot.attributes_for(:protocol_drug, protocol_id: protocol.id).except(:id)
       }
 
       it "updates the requested protocol_drug" do
         protocol_drug = ProtocolDrug.create! valid_attributes
-        put :update, params: {id: protocol_drug.to_param, protocol_drug: new_attributes}
+        put :update, params: { id: protocol_drug.to_param, protocol_drug: new_attributes, protocol_id: protocol.id }
         protocol_drug.reload
         expect(protocol_drug.attributes.except('id', 'created_at', 'updated_at'))
           .to eq new_attributes.with_indifferent_access
@@ -86,15 +84,15 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
 
       it "redirects to the list of protocols" do
         protocol_drug = ProtocolDrug.create! valid_attributes
-        put :update, params: {id: protocol_drug.to_param, protocol_drug: valid_attributes}
-        expect(response).to redirect_to([:admin, :protocols])
+        put :update, params: { id: protocol_drug.to_param, protocol_drug: valid_attributes, protocol_id: protocol.id }
+        expect(response).to redirect_to([:admin, protocol])
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'edit' template)" do
         protocol_drug = ProtocolDrug.create! valid_attributes
-        put :update, params: {id: protocol_drug.to_param, protocol_drug: invalid_attributes}
+        put :update, params: { id: protocol_drug.to_param, protocol_drug: invalid_attributes, protocol_id: protocol.id }
         expect(response).to be_success
       end
     end
@@ -104,15 +102,14 @@ RSpec.describe Admin::ProtocolDrugsController, type: :controller do
     it "destroys the requested protocol_drug" do
       protocol_drug = ProtocolDrug.create! valid_attributes
       expect {
-        delete :destroy, params: {id: protocol_drug.to_param}
+        delete :destroy, params: { id: protocol_drug.to_param, protocol_id: protocol.id }
       }.to change(ProtocolDrug, :count).by(-1)
     end
 
     it "redirects to the protocol_drugs list" do
       protocol_drug = ProtocolDrug.create! valid_attributes
-      delete :destroy, params: {id: protocol_drug.to_param}
-      expect(response).to redirect_to(admin_protocols_url)
+      delete :destroy, params: { id: protocol_drug.to_param, protocol_id: protocol.id }
+      expect(response).to redirect_to([:admin, protocol])
     end
   end
-
 end
