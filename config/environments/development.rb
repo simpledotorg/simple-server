5bb5ec86ddaec7336bb8df494388e5126bf2070e
Tim Cheadle <tim@rationalmeans.com>
2018-07-27 08:57:33 -0400

    Email setup with SendGrid and Mailcatcher (#75)
    * Added SendGrid production config and env variables
    
    * Use Mailcatcher for testing email locally
    
    * Updated README
    
    * Use Rails config object
    
    * Updated Devise email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Use Mailcatcher to test mail in development
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
