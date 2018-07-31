5369473e2d69af27a57aaab48ddf6e6c270c258b
Govind Krishna Joshi <govind@nilenso.com>
2018-07-31 14:59:41 +0530

    Fix failing test

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -75,7 +75,8 @@ class Admin::UsersController < ApplicationController
       :full_name,
       :phone_number,
       :password,
-      :password_confirmation
+      :password_confirmation,
+      :status
     )
   end
 end
