c3988d755d81b38ef91c9711dac11ef6fd27ee71
Govind Krishna Joshi <govind@nilenso.com>
2018-08-01 15:21:08 +0530

    Rename status column, remove default user status

---
+++ db/migrate/20180730082447_add_unique_phone_number_constraint_and_status_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddUniquePhoneNumberConstraintAndStatusColumnToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_index :users, :phone_number, unique: true
+    add_column :users, :sync_approval_status, :string
+  end
+end
--- db/migrate/20180730082447_add_unique_phone_number_contraint_and_status_column_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUniquePhoneNumberContraintAndStatusColumnToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_index :users, :phone_number, unique: true
-    add_column :users, :status, :string, default: 'waiting_for_approval'
-  end
-end
