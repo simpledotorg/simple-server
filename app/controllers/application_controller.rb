b9a64fabbe30babf117503930a568d471ea18568
Tim Cheadle <tim@rationalmeans.com>
2018-08-01 21:48:06 -0400

    Make pundit helper available in every controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   protect_from_forgery with: :exception
+
+  # Tell pundit how to find the current user
+  def pundit_user
+    current_admin
+  end
 end
