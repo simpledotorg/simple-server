3f32ea065170852d0f9c215a02dd87707d1f16b1
Govind Krishna Joshi <govind@nilenso.com>
2018-08-10 16:54:28 +0530

    Rescue from ActiveRecord::RecordNotFound with 404 in API Controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,6 +8,10 @@ class APIController < ApplicationController
     head :bad_request
   end
 
+  rescue_from ActiveRecord::RecordNotFound do
+    head :not_found
+  end
+
   def current_user
     @current_user ||= User.find_by(id: request.headers['HTTP_X_USER_ID'])
   end
