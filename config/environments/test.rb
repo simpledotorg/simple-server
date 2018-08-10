b086a76368d47c91274b972e1fe291f805231d3b
Srihari Sriraman <srihari@nilenso.com>
2018-08-10 12:12:08 +0530

    Supervisor approves nurse
    - Setup approval emails post registration, using action mailer
      - Provide information within email for admin to make decision
      - Provide link to login and allow or deny access
    - Email is sent asynchronously
    - Use sendgrid api key to login
    - Setup owner and supervisor emails in ENV
    - Derive Users controller from AdminController, and specify
      authorizations
    - Login admin before each test
    - Test that emails get sent out, and that access is granted or denied.
    - Test for presence of phone number, since name won't match 100% in HTML
    - Add test for user policy

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_job.queue_adapter = :inline
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
