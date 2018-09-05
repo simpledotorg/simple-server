a2881f7d87ea44b687118660f5846891f47f6b33
Govind Krishna Joshi <govind@nilenso.com>
2018-09-05 11:33:58 +0530

    Remove redundant validations

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,12 +1,7 @@
 class MedicalHistory < ApplicationRecord
   include Mergeable
   belongs_to :patient, optional: true
-
-  validates :has_prior_heart_attack, inclusion: [true, false]
-  validates :has_prior_stroke, inclusion: [true, false]
-  validates :has_chronic_kidney_disease, inclusion: [true, false]
-  validates :is_on_treatment_for_hypertension, inclusion: [true, false]
-
+  
   validates :device_created_at, presence: true
   validates :device_updated_at, presence: true
 end
--- spec/models/medical_history_spec.rb
+++ spec/models/medical_history_spec.rb
@@ -7,10 +7,6 @@ describe MedicalHistory, type: :model do
 
   describe 'Validations' do
     it_behaves_like 'a record that validates device timestamps'
-    it { should validate_inclusion_of(:has_prior_heart_attack).in([true, false]) }
-    it { should validate_inclusion_of(:has_prior_stroke).in([true, false]) }
-    it { should validate_inclusion_of(:has_chronic_kidney_disease).in([true, false]) }
-    it { should validate_inclusion_of(:is_on_treatment_for_hypertension).in([true, false]) }
     it { should validate_presence_of(:device_updated_at) }
   end
 end
