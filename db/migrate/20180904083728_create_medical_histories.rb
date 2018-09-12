cd1b58022df16a3d911711e546f0978577336b4e
Govind Krishna Joshi <govindkrjoshi@gmail.com>
2018-09-12 09:39:36 +0530

    Sync patient medical history
    * Add model, schema, and controller for medical history
    * Add specs for controller, model
    * Delete all medical histories on purge
    * Add diabetes to medical history

---
+++ db/migrate/20180904083728_create_medical_histories.rb
@@ -0,0 +1,15 @@
+class CreateMedicalHistories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :medical_histories, id: :uuid do |t|
+      t.belongs_to :patient, type: :uuid, null: false
+      t.boolean :prior_heart_attack
+      t.boolean :prior_stroke
+      t.boolean :chronic_kidney_disease
+      t.boolean :receiving_treatment_for_hypertension
+      t.boolean :diabetes
+      t.datetime :device_created_at, null: false
+      t.datetime :device_updated_at, null: false
+      t.timestamps
+    end
+  end
+end
