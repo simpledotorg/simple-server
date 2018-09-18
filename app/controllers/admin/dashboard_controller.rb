c125936bf929b89a30c8b2d98f0dc9ddf34a97b5
Tim Cheadle <tim@rationalmeans.com>
2018-09-18 16:28:06 -0400

    Added a basic task list for approving sync requests

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < AdminController
   def show
     skip_authorization
 
+    @users_requesting_approval = User.where(sync_approval_status: :requested)
+
     @facilities = Facility.all
     @patients_per_facility_total = Facility.joins(:patients).group("facilities.id").count("patients.id")
     @patients_per_facility_30days = Facility.joins(:patients).where('patients.created_at > ?', 30.days.ago.beginning_of_day).group("facilities.id").count("patients.id")
---
+++ spec/features/admin/dashboards_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Dashboards", type: :feature do
+  let!(:supervisor) { create(:admin, :supervisor, email: "supervisor@example.com") }
+
+  let!(:bathinda) { create(:facility, name: "Bathinda") }
+  let!(:mansa) { create(:facility, name: "Mansa") }
+
+  let!(:new_user) { create(:user, :requested_sync_approval, facilities: [bathinda, mansa]) }
+
+  before do
+    sign_in(supervisor)
+    visit admin_dashboard_path
+  end
+
+  it "shows a basic dashboard" do
+    expect(page).to have_content("Patients Registered")
+  end
+
+  context "outstanding approval requests" do
+    it "shows a task list for approvals" do
+      expect(page).to have_content("Users waiting for approval")
+
+      within find("tr", text: new_user.full_name) do
+        expect(page).to have_content(new_user.phone_number)
+        expect(page).to have_content("Bathinda, Mansa")
+        expect(page).to have_link("Approve Sync Access")
+      end
+    end
+  end
+end
