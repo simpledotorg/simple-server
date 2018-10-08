fa3e35711d15dcd40ce8f9b4aed7557045a3c577
Srihari Sriraman <srihari@nilenso.com>
2018-10-08 10:27:49 +0530

    Fix build, view spec failing on text/DOM changes

--- spec/features/admin/login_spec.rb
+++ spec/features/admin/login_spec.rb
@@ -4,6 +4,8 @@ RSpec.feature 'Admin::Login', type: :feature do
   let(:admin) { create(:admin) }
 
   it 'Log in and see dashboard by default' do
+    FactoryBot.create(:user, sync_approval_status: :requested)
+
     visit '/'
 
     fill_in 'Email', with: admin.email
@@ -12,7 +14,7 @@ RSpec.feature 'Admin::Login', type: :feature do
     click_button 'Log in'
 
     expect(current_path).to eq(admin_dashboard_path)
-    expect(page).to have_selector('h3', text: 'Users waiting for approval')
+    expect(page).to have_selector('h4', text: '1 User waiting for Access')
     expect(page).to have_selector('h3', text: 'Patients Registered')
   end
 
