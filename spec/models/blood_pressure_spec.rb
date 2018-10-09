524eab62373dd15847ba0f5d3a185929ef48f20d
Tim Cheadle <tim@rationalmeans.com>
2018-10-09 17:27:36 -0400

    Added spec for BloodPressure#under_control?

--- spec/models/blood_pressure_spec.rb
+++ spec/models/blood_pressure_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe BloodPressure, type: :model do
     it { should belong_to(:patient)}
     it { should belong_to(:user)}
   end
+
+  describe "#under_control?" do
+    let(:bp) { build(:blood_pressure) }
+
+    it "returns true if both systolic and diastolic are under control" do
+      bp.systolic = 139
+      bp.diastolic = 89
+      expect(bp.under_control?).to eq(true)
+    end
+
+    it "returns false if systolic is high" do
+      bp.systolic = 140
+      bp.diastolic = 89
+      expect(bp.under_control?).to eq(false)
+    end
+
+    it "returns false if diastolic is high" do
+      bp.systolic = 139
+      bp.diastolic = 90
+      expect(bp.under_control?).to eq(false)
+    end
+
+    it "returns false if both systolic and diastolic are high" do
+      bp.systolic = 150
+      bp.diastolic = 100
+      expect(bp.under_control?).to eq(false)
+    end
+  end
 end
