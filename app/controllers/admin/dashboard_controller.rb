fa8915e67228500217f39a7d366fc499ff84e1bb
Tim Cheadle <tim@rationalmeans.com>
2018-10-11 21:20:40 -0400

    Updated control rate and patients regsitered table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,17 +5,15 @@ class Admin::DashboardController < AdminController
     @users_requesting_approval = User.requested_sync_approval
 
     @facilities = Facility.all.order(:name)
-    @patients_per_facility_total = patient_count_per_facility
-    @patients_per_facility_30days = patient_count_per_facility(since: 30.days.ago.beginning_of_day)
-    @control_rate_per_facility = control_rate_per_facility
 
-    @patients_per_user_total = patient_count_per_user
+    @bps_by_facility          = bps_by_facility
+    @bps_by_facility_user     = bps_by_facility_user
+    @bps_by_facility_user_day = bps_by_facility_user_day
 
-    #@bps_by_facility_nurse_per_day = bps_by_facility_nurse_per_day
+    @patients_by_facility       = patients_by_facility
+    @patients_by_facility_month = patients_by_facility_month
 
-    @bps_by_facility = bps_by_facility
-    @bps_by_facility_user = bps_by_facility_user
-    @bps_by_date_facility_user = bps_by_date_facility_user
+    @control_rate_by_facility = control_rate_by_facility
   end
 
   private
@@ -28,29 +26,23 @@ class Admin::DashboardController < AdminController
     bp_counts_by_day = BloodPressure.group(:facility_id, :user_id).count
   end
 
-  def bps_by_date_facility_user
-    bp_counts_by_day = BloodPressure.group_by_day(:device_created_at, last: 7).group(:facility_id, :user_id).count
+  def bps_by_facility_user_day
+    bp_counts_by_day = BloodPressure.group(:facility_id, :user_id).group_by_day(:device_created_at, last: 7).count
   end
 
-  def patient_count_per_facility(since: nil)
-    patients_by_facility = Facility.joins(:patients).group("facilities.id").distinct('patients.id')
-
-    if since.present?
-      patients_by_facility.where('patients.created_at > ?', since)
-    end
-
-    patients_by_facility.count("patients.id")
+  def patients_by_facility
+    Facility.joins(:patients).group("facilities.id").distinct('patients.id').count("patients.id")
   end
 
-  def patient_count_per_user
-    User.joins(:patients).group("users.id").distinct('patients.id').count("patients.id")
+  def patients_by_facility_month
+    Facility.joins(:patients).group("facilities.id").distinct('patients.id').group_by_month("patients.device_created_at", last: 3).count("patients.id")
   end
 
-  def control_rate_per_facility
+  def control_rate_by_facility
     control_rate = {}
 
-    hypertensive_patients_per_facility.each do |facility_id, patient_ids|
-      numerator = controlled_patients_per_facility(facility_id, patient_ids).size
+    hypertensive_patients_by_facility.each do |facility_id, patient_ids|
+      numerator = controlled_patients_for_facility(facility_id, patient_ids).size
       denominator = patient_ids.size
       control_rate[facility_id] = numerator.to_f / denominator unless denominator == 0
     end
@@ -58,7 +50,7 @@ class Admin::DashboardController < AdminController
     control_rate
   end
 
-  def hypertensive_patients_per_facility(since: Time.new(0), upto: Time.now)
+  def hypertensive_patients_by_facility(since: Time.new(0), upto: Time.now)
     hypertensive_patients = {}
 
     BloodPressure.hypertensive
@@ -71,7 +63,7 @@ class Admin::DashboardController < AdminController
     hypertensive_patients
   end
 
-  def controlled_patients_per_facility(facility_id, patient_ids)
+  def controlled_patients_for_facility(facility_id, patient_ids)
     BloodPressure.select('distinct on (patient_id) *')
       .where(facility_id: facility_id)
       .where(patient: patient_ids)
