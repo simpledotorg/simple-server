72744aaa1c25d1f3a11af289e2503ec19de2b624
Daniel Burka <danielburka@gmail.com>
2018-10-29 18:12:38 +0700

    Formatting of the reports

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,11 +75,78 @@ protocol_drugs_data = [
   }
 ]
 
+Facility.destroy_all
+User.destroy_all
+
 facilities.each do |facility_data|
-  Facility.find_or_create_by(facility_data)
+  facility = Facility.find_or_create_by(facility_data)
+
+  (1..3).each do |number|
+    facility.users.create(
+      full_name: "#{facility.name} User #{number}",
+      phone_number: rand(1111111111..9999999999),
+      password: "1234",
+      sync_approval_status: :allowed,
+      sync_approval_status_reason: ""
+    )
+  end
 end
 
 protocol = Protocol.find_or_create_by(protocol_data)
 protocol_drugs_data.each do |drug_data|
   ProtocolDrug.find_or_create_by(drug_data.merge(protocol_id: protocol.id))
 end
+
+
+### Utils
+
+def rand_facility
+  Facility.limit(1).order("RANDOM()").last
+end
+
+def rand_user(facility)
+  facility.users.limit(1).order("RANDOM()").last
+end
+
+def rand_systolic
+  rand(100..160)
+end
+
+def rand_diastolic
+  rand(60..110)
+end
+
+def rand_datetime
+  rand(90.days.ago..Time.now)
+end
+
+def create_bps
+  BloodPressure.destroy_all
+
+  Patient.all.each do |patient|
+    facility = rand_facility
+    patient_date = rand_datetime
+
+    patient.update!(
+      device_created_at: patient_date,
+      device_updated_at: patient_date,
+    )
+
+    rand(1..4).times do
+      user = rand_user(facility)
+      bp_date = rand_datetime
+
+      patient.blood_pressures.create!(
+        id: SecureRandom.uuid,
+        user: user,
+        facility: facility,
+        systolic: rand_systolic,
+        diastolic: rand_diastolic,
+        device_created_at: bp_date,
+        device_updated_at: bp_date
+      )
+    end
+  end
+end
+
+create_bps
