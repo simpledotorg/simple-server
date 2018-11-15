13725d5f2bbc53ba103952cdf62ef898c38c9c8a
Srihari Sriraman <srihari@nilenso.com>
2018-11-15 11:50:38 +0530

    Extract transformers to top level dir, add versioning.

--- app/payloads/api/current/user_transformer.rb
+++
@@ -1,7 +0,0 @@
-module Api::Current::UserTransformer
-  def self.to_response(user)
-    Api::Current::Transformer.to_response(user)
-      .merge(facility_ids: user.facilities.map(&:id))
-      .except('otp', 'otp_valid_until', 'access_token', 'logged_in_at')
-  end
-end
