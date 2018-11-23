8afc18ffea459811d09d8aecec38994b15ad79b9
Govind Krishna Joshi <govindkrjoshi@gmail.com>
2018-11-23 12:57:47 +0530

    Rename current API spec to reflect Current version.
    Co-Authored-By: ssrihari <srihari@nilenso.com>

--- spec/api/v1/protocols_spec.rb
+++ spec/api/v1/protocols_spec.rb
@@ -1,6 +1,6 @@
 require 'swagger_helper'
 
-describe 'Protocols API', swagger_doc: 'v1/swagger.json' do
+describe 'Protocols V1 API', swagger_doc: 'v1/swagger.json' do
   path '/protocols/sync' do
     get 'Syncs protocols and protocol drugs data from server to device.' do
       tags 'protocol'
