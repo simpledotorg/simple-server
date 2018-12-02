abbf165ef0d14a067dedd1472098f16568e6b086
Govind Krishna Joshi <govind@nilenso.com>
2018-12-02 15:15:45 +0530

    Add user count to organization index

--- app/models/facility_group.rb
+++ app/models/facility_group.rb
@@ -1,6 +1,7 @@
 class FacilityGroup < ApplicationRecord
   belongs_to :organization
   has_many :facilities
+  has_many :users, through: :facilities
 
   validates :name, presence: true
   validates :organization, presence: true
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,7 @@
 class Organization < ApplicationRecord
   has_many :facility_groups
+  has_many :facilities, through: :facility_groups
+  has_many :users, through: :facilities
 
   validates :name, presence: true
 end
