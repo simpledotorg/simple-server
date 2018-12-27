e8d4a66df8e8324b7134dfbe5befba10c2a44ffe
Govind Krishna Joshi <govind@nilenso.com>
2018-12-27 13:56:59 +0530

    Fix updating facility groups and organizations for admins

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -13,6 +13,14 @@ class AdminsController < AdminController
   end
 
   def update
+    admin_access_controls = access_controllable_ids.reject(&:empty?).map do |access_controllable_id|
+      AdminAccessControl.new(
+        admin_id: @admin.id,
+        access_controllable_type: access_controllable_type,
+        access_controllable_id: access_controllable_id)
+    end
+    @admin.admin_access_controls = admin_access_controls
+    @admin.save
     if @admin.update(admin_params)
       redirect_to @admin, notice: 'Admin was successfully updated.'
     else
@@ -26,12 +34,21 @@ class AdminsController < AdminController
   end
 
   private
-    def set_admin
-      @admin = Admin.find(params[:id])
-      authorize @admin
-    end
 
-    def admin_params
-      params.require(:admin).permit(:email, :role, facility_group_ids: [])
-    end
+  def set_admin
+    @admin = Admin.find(params[:id])
+    authorize @admin
+  end
+
+  def access_controllable_ids
+    params.require(:admin).require(:access_controllable_ids)
+  end
+
+  def access_controllable_type
+    params.require(:admin).require(:access_controllable_type)
+  end
+
+  def admin_params
+    params.require(:admin).permit(:email)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181224051858) do
+ActiveRecord::Schema.define(version: 20181227081322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20181224051858) do
   end
 
   create_table "admin_access_controls", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.bigint "admin_id", null: false
+    t.bigint "admin_id"
     t.uuid "access_controllable_id", null: false
     t.string "access_controllable_type", null: false
     t.datetime "created_at", null: false
