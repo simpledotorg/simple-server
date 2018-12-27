b553feccb29e95226b64c09d355b92be6a8451fc
Govind Krishna Joshi <govindkrjoshi@gmail.com>
2018-12-27 17:24:42 +0530

    Sync only facilities with associated facility groups

--- app/controllers/api/v1/facilities_controller.rb
+++ app/controllers/api/v1/facilities_controller.rb
@@ -4,5 +4,6 @@ class Api::V1::FacilitiesController < Api::Current::FacilitiesController
 
   def find_records_to_sync(since, limit)
     Facility.updated_on_server_since(since, limit)
+      .where.not(facility_group: nil)
   end
 end
