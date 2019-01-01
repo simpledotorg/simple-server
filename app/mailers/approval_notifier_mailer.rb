bbb4a79330c917e28fce25d2795b58ccdf351eb1
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-01 17:35:46 +0530

    Update approval mails
    * Add org name to approval mails.
    
    * Approval email lists accessible facilities
    
    * Add owner emails to bcc lists

--- app/mailers/approval_notifier_mailer.rb
+++ app/mailers/approval_notifier_mailer.rb
@@ -15,12 +15,19 @@ class ApprovalNotifierMailer < ApplicationMailer
     admin_emails('organization_owner').join(',')
   end
 
+  def owner_emails
+    Admin.where(role: 'owner').pluck(:email).join(',')
+  end
+
   def registration_approval_email
     @user = params[:user]
-    subject = I18n.t('registration_approval_email.subject', full_name: @user.full_name)
+    subject = I18n.t('registration_approval_email.subject',
+                     full_name: @user.full_name,
+                     org_name: @user.facility_group.organization.name)
     mail(subject: subject,
          to: supervisor_emails,
-         cc: organization_owner_emails)
+         cc: organization_owner_emails,
+         bcc: owner_emails)
   end
 
   def reset_password_approval_email
@@ -28,6 +35,7 @@ class ApprovalNotifierMailer < ApplicationMailer
     subject = I18n.t('reset_password_approval_email.subject', full_name: @user.full_name)
     mail(subject: subject,
          to: supervisor_emails,
-         cc: organization_owner_emails)
+         cc: organization_owner_emails,
+         bcc: owner_emails)
   end
 end
