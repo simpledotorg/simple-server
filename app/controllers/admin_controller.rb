aaafc9deb282bde6587f6700ed8d9be32f974dbd
Govind Krishna Joshi <govind@nilenso.com>
2019-01-02 15:16:19 +0530

    Ensure policy scope is used for index controllers

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,13 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   after_action :verify_authorized
   after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
