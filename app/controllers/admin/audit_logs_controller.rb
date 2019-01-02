67120d5ee2b85ed9baab9d81d89854daed66be0f
Govind Krishna Joshi <govind@nilenso.com>
2019-01-02 12:41:47 +0530

    Fix audit logs feature specs

--- app/controllers/admin/audit_logs_controller.rb
+++ app/controllers/admin/audit_logs_controller.rb
@@ -3,7 +3,7 @@ class Admin::AuditLogsController < AdminController
 
   def index
     authorize AuditLog
-    @audit_logs = []
+    @audit_logs = policy_scope(AuditLog).none
     if params[:user_name].present?
       users = policy_scope(User).where('full_name ilike ?', "%#{params[:user_name]}%")
       @audit_logs = policy_scope(AuditLog).where(user_id: users.pluck(:id)).order(created_at: :desc)
