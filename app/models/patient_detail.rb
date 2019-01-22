a0546a576806aeb6ef277c317d905a152475bc9b
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-22 23:57:19 +0530

    PatientDetail is nil if patient doesn;t have a BP/scheduled appointment

--- app/models/patient_detail.rb
+++ app/models/patient_detail.rb
@@ -1,11 +1,13 @@
 class PatientDetail < Struct.new(:patient, :latest_blood_pressure, :latest_scheduled_appointment)
   def self.for_facility(facility)
-    facility.patients.map { |patient| for_patient(patient) }
+    facility.patients.map { |patient| for_patient(patient) }.compact
   end
 
   def self.for_patient(patient)
-    PatientDetail.new(patient,
-                      patient.latest_blood_pressure,
-                      patient.latest_scheduled_appointment)
+    if patient.latest_scheduled_appointment.present? && patient.latest_blood_pressure.present?
+      PatientDetail.new(patient,
+                        patient.latest_blood_pressure,
+                        patient.latest_scheduled_appointment)
+    end
   end
 end
