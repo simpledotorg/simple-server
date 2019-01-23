a4bb0357f76acec9c2214e4e1d80ac54c0f64cdc
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-23 22:22:16 +0530

    refactor: patient_detail -> overdue_appointment

--- app/controllers/admin/overdue_appointments_controller.rb
+++ app/controllers/admin/overdue_appointments_controller.rb
@@ -2,12 +2,15 @@ class Admin::OverdueAppointmentsController < AdminController
   def index
     authorize :overdue_appointment, :index?
     facilities = policy_scope(Facility)
-    @patient_details_per_facility = {}
+    @overdue_appointments_per_facility = {}
     facilities.each do |facility|
-      @patient_details_per_facility[facility.name] = OverdueAppointment.for_facility(facility)
+      @overdue_appointments_per_facility[facility.name] = OverdueAppointment.for_facility(facility)
     end
   end
 
   def show
+  end
+
+  def edit
   end
 end
