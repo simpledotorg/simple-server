2394d470ff7592681a3a441940a4d140fc75de6f
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-01-24 03:36:48 +0530

    fix inviting counsellorn

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -36,11 +36,11 @@ class Admin < ApplicationRecord
     facility_groups.flat_map(&:facilities)
   end
 
-  def patients
-    Patient.where(registration_facility: facilities)
-  end
-
   def users
     facility_groups.flat_map(&:users)
+  end
+
+  def patients
+    Patient.where(registration_facility: facilities)
   end
 end
