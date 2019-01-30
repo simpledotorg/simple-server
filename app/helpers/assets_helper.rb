7d9829c9891fc60fbfbfce4accb303c9325ee289
Tim Cheadle <tim@rationalmeans.com>
2019-01-30 16:48:59 -0500

    A little more cleanup

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,15 +1,14 @@
 module AssetsHelper
-  def inline_file(path)
-    if assets = Rails.application.assets
-      asset = assets.find_asset(path)
-      return '' unless asset
-      asset.source
+  def inline_file(asset_name)
+    if asset = Rails.application.assets&.find_asset(asset_name)
+      asset.source.html_safe
     else
-      File.read(File.join(Rails.root, 'public', asset_path(path)))
+      asset_path = Rails.application.assets_manifest.assets[asset_name]
+      File.read(File.join(Rails.root, 'public', 'assets', asset_path)).html_safe
     end
   end
 
-  def inline_js(path)
-    content_tag(:script, inline_file(path).html_safe, type: "text/javascript")
+  def inline_js(asset_name)
+    content_tag(:script, inline_file(asset_name), type: "text/javascript")
   end
 end
