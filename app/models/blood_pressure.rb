f503632e59c77daddcee36c0e2d59f502266bd14
Tim Cheadle <tim@rationalmeans.com>
2019-02-11 16:30:25 -0500

    Added helper #to_s method for BloodPressure

--- app/models/blood_pressure.rb
+++ app/models/blood_pressure.rb
@@ -18,4 +18,8 @@ class BloodPressure < ApplicationRecord
   def recorded_days_ago
     (Date.today - device_created_at.to_date).to_i
   end
+
+  def to_s
+    [systolic, diastolic].join("/")
+  end
 end
