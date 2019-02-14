57eb3ef0df147c3ae67a1a462d17afbc96df83c1
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-02-14 17:33:46 +0530

    Update patient payload with new fields

--- app/validators/api/current/patient_payload_validator.rb
+++ app/validators/api/current/patient_payload_validator.rb
@@ -12,7 +12,9 @@ class Api::Current::PatientPayloadValidator < Api::Current::PayloadValidator
     :updated_at,
     :deleted_at,
     :address,
-    :phone_numbers
+    :phone_numbers,
+    :contacted_by_counsellor,
+    :could_not_contact_reason
   )
 
   validate :validate_schema
