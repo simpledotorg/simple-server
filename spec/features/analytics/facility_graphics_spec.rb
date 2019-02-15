c59ebcc831fdc906c1efe7efa74e10b16e2ea432
Govind Krishna Joshi <govind@nilenso.com>
2019-02-15 05:43:46 +0530

    Fix locale files

--- spec/features/analytics/facility_graphics_spec.rb
+++ spec/features/analytics/facility_graphics_spec.rb
@@ -7,6 +7,32 @@ RSpec.feature 'Facility Group Graphics', type: :feature do
   let(:facility_group) { create :facility_group, organization: organization }
   let(:facility) { create :facility, facility_group: facility_group }
 
+  let(:blood_pressures_recorded_this_month) { 10 }
+
+  def random_time(from_time, to_time)
+    Time.at(from_time.to_time.to_f + rand * (to_time.to_time - from_time.to_time).to_f)
+  end
+
+  def create_in_period(model, facility, from_date, to_date)
+    create model, facility: facility, device_created_at: random_time(from_date, to_date)
+  end
+
+  def create_list_in_period(model, count, facility, from_date, to_date)
+    created_objects = []
+    count.times { created_objects << create_in_period(model, facility, from_date, to_date) }
+    created_objects
+  end
+
+  before :each do
+    create_list_in_period(
+      :blood_pressure,
+      blood_pressures_recorded_this_month,
+      facility,
+      Date.today.at_beginning_of_month,
+      Date.today.at_end_of_month
+    )
+  end
+
   before :each do
     sign_in(owner)
     visit analytics_facility_graphics_path(facility)
@@ -23,8 +49,9 @@ RSpec.feature 'Facility Group Graphics', type: :feature do
       expect(page).to have_content(facility.name)
     end
 
-    it 'contains the number of newly enrolled patients' do
-      expect(page).to have_content(I18n.t('analytics.graphics.facility_groups.newly_enrolled'))
+    it 'contains the number of blood pressures recorded in the month' do
+      expect(page).to have_content(I18n.t('analytics.graphics.facility.blood_pressures_recorded_in_month'))
+      expect(page).to have_content(blood_pressures_recorded_this_month)
     end
   end
 end
