388a037fb0cc1259bf990ec0d81ca45853e15703
Timmy Jose <timmy@nilenso.com>
2019-02-19 20:40:12 +0530

    Change logo and banner (header) colour of simple server as per current deployment env config

---
+++ app/helpers/simple_server_env_helper.rb
@@ -0,0 +1,26 @@
+module SimpleServerEnvHelper
+  SIMPLE_SERVER_ENV = "SIMPLE_SERVER_ENV".freeze
+
+  CUSTOMIZED_ENVS = %w[qa staging sandbox production].freeze
+
+  def style_class_for_environment
+    env = ENV.fetch(SIMPLE_SERVER_ENV)
+
+    CUSTOMIZED_ENVS.include?(env) ? "navbar-#{env}" : ""
+  end
+
+  def logo_for_environment
+    env = ENV.fetch(SIMPLE_SERVER_ENV)
+
+    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}.svg" : "simple_logo.svg"
+
+    image_tag image_name, width: 30, height: 30, class: "d-inline-block mr-2 align-top", alt: alt_for_environment
+
+  end
+
+  def alt_for_environment
+    env = ENV.fetch(SIMPLE_SERVER_ENV)
+
+    CUSTOMIZED_ENVS.include?(env) ? "Simple Server #{env.capitalize} Logo" : "Simple Server Logo"
+  end
+end
