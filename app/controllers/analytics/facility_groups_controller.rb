a062f5a891bb0327bfec935f53b4ba0a911bb636
Govind Krishna Joshi <govind@nilenso.com>
2019-03-06 16:42:58 +0530

    Add analytics controllers

---
+++ app/controllers/analytics/facility_groups_controller.rb
@@ -0,0 +1,45 @@
+class Analytics::FacilityGroupsController < AnalyticsController
+  before_action :set_facility_group
+  before_action :set_organization
+  before_action :set_facilities
+
+  def show
+    @days_previous = 20
+    @months_previous = 8
+
+    @facility_group_analytics = facility_group_analytics(@from_time, @to_time)
+    @facility_analytics = facility_analytics(@from_time, @to_time)
+  end
+
+  def graphics
+    @months_previous = 4
+
+    @facility_group_analytics = facility_group_analytics(@from_time, @to_time)
+  end
+
+  private
+
+  def set_facility_group
+    facility_group_id = params[:id] || params[:facility_group_id]
+    @facility_group = FacilityGroup.friendly.find(facility_group_id)
+    authorize(@facility_group)
+  end
+
+  def set_organization
+    @organization = @facility_group.organization
+  end
+
+  def set_facilities
+    @facilities = policy_scope(@facility_group.facilities)
+  end
+
+  def facility_group_analytics(from_time, to_time)
+    @facility_group.patient_set_analytics(from_time, to_time)
+  end
+
+  def facility_analytics(from_time, to_time)
+    @facilities.map do |facility|
+      [facility, facility.patient_set_analytics(from_time, to_time)]
+    end.to_h
+  end
+end
