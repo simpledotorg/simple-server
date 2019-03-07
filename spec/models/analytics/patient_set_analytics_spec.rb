dfff4c5d369e1dbeca1c9bfbbe84d45118b732f7
Govind Krishna Joshi <govind@nilenso.com>
2019-03-07 12:28:58 +0530

    Fix some of the failing specs

--- spec/models/analytics/patient_set_analytics_spec.rb
+++ spec/models/analytics/patient_set_analytics_spec.rb
@@ -26,27 +26,6 @@ RSpec.describe Analytics::PatientSetAnalytics do
     end
   end
 
-  xdescribe '#newly_enrolled_patients_count_per_month' do
-    it 'returns the number of patients newly enrolled per month' do
-      expected_counts = {}
-      5.times do
-        n = rand(4)
-        start_of_month = (Date.today - n.months).at_beginning_of_month
-        create_in_period :patient, from_time: start_of_month, to_time: start_of_month.at_end_of_month
-
-        date_key = start_of_month.to_date
-        if expected_counts[date_key].present?
-          expected_counts[date_key] += 1
-        else
-          expected_counts[date_key] = 1
-        end
-      end
-
-      analytics = Analytics::PatientSetAnalytics.new(Patient.all, from_time, to_time)
-      expect(analytics.newly_enrolled_patients_count_per_month(4)).to include(expected_counts)
-    end
-  end
-
   describe '#returning_patients_count' do
     it 'returns the number of patients that where recorded before from_time and have BP recording in the give period' do
       _old_patients = create_list_in_period :patient, 5, past_options
@@ -74,24 +53,6 @@ RSpec.describe Analytics::PatientSetAnalytics do
 
       analytics = Analytics::PatientSetAnalytics.new(Patient.all, from_time, to_time)
       expect(analytics.non_returning_hypertensive_patients_count).to eq(3)
-    end
-  end
-
-  xdescribe '#non_returning_hypertensive_patients_count_per_month' do
-    it 'return the number of patients enrolled as hypertensives that have not had a BP recorded per month' do
-      expected_counts = {}
-      4.times do |n|
-        start_of_month = (Date.today - n.months).at_beginning_of_month
-
-        patient = create_in_period :patient, from_time: start_of_month - 9.month, to_time: start_of_month.at_end_of_month - 9.months
-        create :blood_pressure, patient: patient, device_created_at: patient.device_created_at
-
-        date_key = start_of_month.to_date
-        expected_counts[date_key] = 4
-      end
-
-      analytics = Analytics::PatientSetAnalytics.new(Patient.all, from_time, to_time)
-      expect(analytics.non_returning_hypertensive_patients_count_per_month(4)).to include(expected_counts)
     end
   end
 
@@ -125,31 +86,6 @@ RSpec.describe Analytics::PatientSetAnalytics do
                  hypertensive_patients_in_cohort: 5,
                  patients_under_control_in_period: 3)
       end
-    end
-  end
-
-  xdescribe '#blood_pressure_recored_per_week' do
-    it 'returns the number of blood pressures recorded per week for a group of patients' do
-      patients = create_list_in_period :patient, 5, from_time: 1.year.ago, to_time: to_time
-
-      expected_counts = {}
-      patients.each do |patient|
-        n = rand(12)
-        start_of_week = (Date.today - n.weeks).at_beginning_of_week
-        create_in_period :blood_pressure,
-                         from_time: start_of_week, to_time: start_of_week.at_end_of_week,
-                         patient: patient
-
-        date_key = start_of_week.prev_day.to_date
-        if expected_counts[date_key].present?
-          expected_counts[date_key] += 1
-        else
-          expected_counts[date_key] = 1
-        end
-      end
-
-      analytics = Analytics::PatientSetAnalytics.new(Patient.all, from_time, to_time)
-      expect(analytics.blood_pressures_recorded_per_week(12)).to include(expected_counts)
     end
   end
 end
