4c4cdf4c5a544d3fedad3ee5085454ae5786f30a
Tim Cheadle <tim@rationalmeans.com>
2019-03-15 23:43:41 +0530

    Began adding specs for CSV and filtering

--- spec/features/appointments_spec.rb
+++ spec/features/appointments_spec.rb
@@ -20,6 +20,7 @@ RSpec.feature 'Overdue appointments', type: :feature do
       let!(:overdue_patient_in_facility_1) do
         patient = create(:patient, full_name: 'patient_1', registration_facility: facility_1)
         create(:appointment, :overdue, facility: facility_1, patient: patient, scheduled_date: 10.days.ago)
+        create(:blood_pressure, :critical, facility: facility_1, patient: patient)
         patient
       end
 
@@ -30,6 +31,7 @@ RSpec.feature 'Overdue appointments', type: :feature do
       let!(:overdue_patient_in_facility_2) do
         patient = create(:patient, full_name: 'patient_3', registration_facility: facility_2)
         create(:appointment, :overdue, facility: facility_2, patient: patient, scheduled_date: 5.days.ago)
+        create(:blood_pressure, :high, facility: facility_2, patient: patient)
         patient
       end
 
@@ -78,6 +80,51 @@ RSpec.feature 'Overdue appointments', type: :feature do
         page.reset!
         visit appointments_path
         expect(page).not_to have_content(overdue_patient_in_facility_1.full_name)
+      end
+
+      it 'allows you to filter by facility' do
+        select facility_1.name, from: "facility_id"
+        click_button "Filter"
+
+        expect(page).to have_content(overdue_patient_in_facility_1.full_name)
+        expect(page).not_to have_content(overdue_patient_in_facility_2.full_name)
+      end
+
+      it 'allows you to download the overdue list CSV' do
+        binding.pry
+        click_link "Download Overdue List"
+
+        headers = [
+          "Patient name",
+          "Gender",
+          "Age",
+          "Days overdue",
+          "Last BP",
+          "Last BP taken at",
+          "Last BP date",
+          "Risk level",
+          "Patient address",
+          "Patient village or colony",
+          "Patient phone"
+        ]
+        expect(page).to have_content(headers.to_csv)
+
+        appointment = overdue_patient_in_facility_1.appointments.first
+
+        appointment_line = [
+          appointment.patient.full_name,
+          appointment.patient.gender.capitalize,
+          appointment.patient.current_age,
+          appointment.days_overdue,
+          appointment.patient.latest_blood_pressure.to_s,
+          appointment.patient.latest_blood_pressure.facility.name,
+          appointment.patient.latest_blood_pressure.device_created_at.to_date,
+          appointment.patient.high_risk? ? "High risk" : nil,
+          appointment.patient.address.street_address,
+          appointment.patient.address.village_or_colony,
+          appointment.patient.phone_numbers.first&.number
+        ]
+        expect(page).to have_content(appointment_line.to_csv)
       end
     end
   end
