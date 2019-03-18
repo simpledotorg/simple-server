a78684ba0426551a757b2e0988335aedb1e20f60
Govind Krishna Joshi <govind@nilenso.com>
2019-03-18 14:53:02 +0530

    Fix test for returning patients query

--- spec/queries/patients_returning_during_period_query_spec.rb
+++ spec/queries/patients_returning_during_period_query_spec.rb
@@ -1,28 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe PatientsReturningDuringPeriodQuery do
-  let(:from_time) { 1.month.ago }
-  let(:to_time) { Date.today }
-
-  let(:facilities) { create_list :facility, 2 }
+  let(:from_time) { Date.new(2019, 1, 1) }
+  let(:to_time) { Date.new(2019, 3, 31) }
+  let(:one_year_ago) { Date.new(2018, 1, 1) }
 
   describe '#call' do
-    it 'returns the number of unique patients registerted at a list of facilities' do
-      old_patients = facilities.flat_map do |facility|
-        create_list_in_period(
-          :patient, 10,
-          from_time: 1.year.ago, to_time: from_time - 1.day,
-          registration_facility: facility)
-      end
+    let!(:old_patients) do
+      Timecop.travel(one_year_ago) { create_list(:patient, 5) }
+    end
 
-      returning_patients = old_patients.sample(3)
-      returning_patients.each do |patient|
-        create_in_period(
-          :blood_pressure,
-          trait: :high, from_time: from_time, to_time: to_time,
-          patient: patient, facility: patient.registration_facility)
-      end
+    let!(:returning_patients) { old_patients.take(3) }
 
+    before :each do
+      Timecop.travel(from_time) do
+        returning_patients.each { |patient| create(:blood_pressure, patient: patient) }
+      end
+    end
+
+    it 'returns the list of the patients that have returned in the period' do
       results = PatientsReturningDuringPeriodQuery.new(patients: Patient.all, from_time: from_time, to_time: to_time).call
 
       expect(results).to match_array(returning_patients)
