01b51ebad8720996a846efa9f4f82f69129d5d38
Govind Krishna Joshi <govind@nilenso.com>
2019-03-18 14:37:01 +0530

    Remove randomness from control rate query

--- spec/queries/control_rate_query_spec.rb
+++ spec/queries/control_rate_query_spec.rb
@@ -1,43 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe PatientsReturningDuringPeriodQuery do
-  let(:from_time) { 1.month.ago }
-  let(:to_time) { Date.today }
+RSpec.describe ControlRateQuery do
+  let(:from_time) { Date.new(2019, 1, 1) }
+  let(:to_time) { Date.new(2019, 3, 31) }
 
-  let(:facilities) { create_list :facility, 2 }
-
-  let!(:hypertensive_patients_registered_9_months_ago) do
-    facilities.flat_map do |facility|
-      patients = create_list_in_period(:patient, 5, from_time: from_time - 9.months, to_time: to_time - 9.months, registration_facility: facility)
-      patients.each do |patient|
+  describe '#for_period' do
+    before :each do
+      hypertensive_patients_registered_in_cohort = Timecop.travel(from_time - ControlRateQuery::COHORT_DELTA) do
+        patients = create_list(:patient, 5)
+        patients.each { |patient| create(:blood_pressure, :high, patient: patient) }
+        patients
+      end
+      hypertensive_patients_registered_in_cohort.take(2).each do |patient|
         create_in_period(
           :blood_pressure,
-          trait: :high, from_time: from_time - 9.months, to_time: to_time - 9.months - 1.day,
-          patient: patient, facility: facility)
+          trait: :under_control, from_time: from_time, to_time: to_time,
+          patient: patient, facility: patient.registration_facility)
       end
-      patients
     end
-  end
 
-  let!(:patients_under_control_in_period) do
-    patients_under_control_in_period = hypertensive_patients_registered_9_months_ago.sample(4)
-    patients_under_control_in_period.each do |patient|
-      create_in_period(
-        :blood_pressure,
-        trait: :under_control, from_time: from_time, to_time: to_time,
-        patient: patient, facility: patient.registration_facility)
-    end
-    patients_under_control_in_period
-  end
-
-  describe '#call' do
-    it 'returns the number of unique patients registerted at a list of facilities' do
+    it 'returns the control rate for the set of patients' do
       results = ControlRateQuery.new(patients: Patient.all).for_period(from_time: from_time, to_time: to_time)
 
       expect(results)
         .to eq(control_rate: 40,
-               hypertensive_patients_in_cohort: 10,
-               patients_under_control_in_period: 4)
+               hypertensive_patients_in_cohort: 5,
+               patients_under_control_in_period: 2)
     end
   end
 end
