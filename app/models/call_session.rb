4363b7ca1bca6aab2a53f6ffb29576eab0e45d6a
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-03-20 19:32:51 +0530

    Phone number masking connect and terminate endpoints
    * This drives the ‘Connect’ flow from Exotel
    * Add terminate controller to invalidate the session: drives the final step in the call
    * Use PhoneLib to validate user numbers

--- app/models/call_session.rb
+++ app/models/call_session.rb
@@ -1,33 +1,53 @@
 class CallSession
-  KEY = 'CALL-SESSION'
   EXPIRE_CALL_SESSION_IN = 24.hours
 
-  def initialize(user_phone_number, patient_phone_number)
-    @user = User.find_by_phone_number(user_phone_number)
+  attr_reader :user, :patient_phone_number
+
+  def initialize(call_id, user_phone_number, patient_phone_number)
+    @call_id = call_id
+    @user = User.find_by_phone_number(sanitized_phone_number(user_phone_number))
     @patient_phone_number = PatientPhoneNumber.find_by_number(patient_phone_number)
   end
 
-  def save(call_id)
-    Rails.cache.write(session_key(call_id),
+  def authorized?
+    user.present? &&
+      user.sync_approval_status_allowed? &&
+      patient_phone_number.present? &&
+      user.phone_number != patient_phone_number.number
+  end
+
+  def save
+    Rails.cache.write(CallSession.session_key(@call_id),
                       session_data,
                       expires_in: EXPIRE_CALL_SESSION_IN)
   end
 
-  def authorized?
-    @user.present? &&
-      @patient_phone_number.present? &&
-      @user.phone_number != @patient_phone_number.number
+  def kill
+    Rails.cache.delete(CallSession.session_key(@call_id))
+  end
+
+  class << self
+    def fetch(call_id)
+      data = Rails.cache.fetch(session_key(call_id))
+
+      CallSession.new(call_id,
+                      data[:user_phone_number],
+                      data[:patient_phone_number]) if data.present?
+    end
+
+    def session_key(call_id)
+      [self.name, call_id].join('/')
+    end
   end
 
   private
 
-  def session_key(call_id)
-    KEY + '-' + call_id
+  def sanitized_phone_number(phone_number)
+    Phonelib.parse(phone_number, ENV['DEFAULT_COUNTRY'] || 'IN').raw_national
   end
 
   def session_data
-    { patient_phone_number: @patient_phone_number.number,
-      user_phone_number: @user.phone_number }
+    { patient_phone_number: patient_phone_number.number,
+      user_phone_number: user.phone_number }
   end
 end
-
