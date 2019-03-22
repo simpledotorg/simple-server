7c7047647edaa7c0cd6560bf9f8b453a2c212976
Akshay Gupta <kitallis@gmail.com>
2019-03-22 13:11:32 +0530

    Patient phone numbers should not be an association on call log

--- spec/models/call_log_spec.rb
+++ spec/models/call_log_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe CallLog, type: :model do
   describe 'Associations' do
     it { should belong_to(:user) }
-    it { should belong_to(:patient_phone_number) }
   end
 end
