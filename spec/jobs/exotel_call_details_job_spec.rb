2d704d5ec304a55f27d50bc0ce0ead8218508211
Akshay Gupta <kitallis@gmail.com>
2019-03-25 14:22:51 +0530

    Fixing the ExotelCallDetailsJob specs

--- spec/jobs/exotel_call_details_job_spec.rb
+++ spec/jobs/exotel_call_details_job_spec.rb
@@ -11,34 +11,39 @@ RSpec.describe ExotelCallDetailsJob, type: :job do
     allowed_call_result = OpenStruct.new({ Sid: call_id })
     allow_any_instance_of(ExotelAPIService).to receive(:call_details).with(call_id).and_return(allowed_call_result)
 
-    expect {
+    assert_performed_jobs 1 do
       described_class.perform_later(call_id, user.id, callee_phone_number)
-    }.to change { CallLog.count }.by(1)
+    end
+
+    expect(CallLog.count).to eq(1)
   end
 
   it 'should not populate a call log if exotel api is unable to return call details' do
     allow_any_instance_of(ExotelAPIService).to receive(:call_details).with(call_id).and_return(nil)
 
-    expect {
+    assert_performed_jobs 1 do
       described_class.perform_later(call_id, user.id, callee_phone_number)
-    }.to change { CallLog.count }.by(0)
+    end
+
+    expect(CallLog.count).to eq(0)
   end
 
   it 'should create a call log even the user does not exist' do
     allowed_call_result = OpenStruct.new({ Sid: call_id })
     allow_any_instance_of(ExotelAPIService).to receive(:call_details).with(call_id).and_return(allowed_call_result)
 
-    expect {
+    assert_performed_jobs 1 do
       described_class.perform_later(call_id, SecureRandom.uuid, callee_phone_number)
-    }.to change { CallLog.count }.by(1)
+    end
+
+    expect(CallLog.count).to eq(1)
   end
 
   it 'makes attempts to retry when Exotel::HTTPError is raised' do
     allow_any_instance_of(described_class).to receive(:perform).and_raise(ExotelAPIService::HTTPError.new)
-
     expect_any_instance_of(described_class).to receive(:retry_job)
 
-    perform_enqueued_jobs do
+    assert_performed_jobs 1 do
       described_class.perform_later(call_id, user.id, callee_phone_number)
     end
   end
