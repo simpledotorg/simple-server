098839e62c915f8ecdbeb63ccb36a9474c261f37
Tim Cheadle <tim@rationalmeans.com>
2019-03-26 14:39:27 -0400

    Make sure phone number first/last are ordered as expected

--- app/models/patient_phone_number.rb
+++ app/models/patient_phone_number.rb
@@ -7,4 +7,12 @@ class PatientPhoneNumber < ApplicationRecord
 
   validates :device_created_at, presence: true
   validates :device_updated_at, presence: true
+
+  def self.first
+    order(device_created_at: :asc).first
+  end
+
+  def self.last
+    order(device_created_at: :asc).last
+  end
 end
