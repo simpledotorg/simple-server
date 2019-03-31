dfb115592a434571f5904fc29e71bf477fab0a16
Govind Krishna Joshi <govind@nilenso.com>
2019-03-31 13:07:33 +0530

    Use skip_before_action to avoid authentication

--- app/controllers/api/current/help_controller.rb
+++ app/controllers/api/current/help_controller.rb
@@ -1,5 +1,9 @@
-class Api::Current::HelpController < ApplicationController
+class Api::Current::HelpController < ApiController
   layout false
+
+  skip_before_action :authenticate, only: [:show]
+  skip_before_action :validate_facility, only: [:show]
+  skip_before_action :validate_current_facility_belongs_to_users_facility_group, only: [:show]
 
   def show
   end
