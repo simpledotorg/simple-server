a1eda877f4d2791fd7967b02e7ef0dbb0f15833e
Govind Krishna Joshi <govind@nilenso.com>
2019-03-31 08:56:56 +0530

    Automatically add links for the past 4 quarters

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,4 +6,17 @@ module DashboardHelper
   def analytics_date_format(time)
     time.strftime('%Y-%m-%d')
   end
+
+  def range_for_quarter(offset)
+    date = Date.today + (3 * offset).months
+    { from_time: date.at_beginning_of_quarter,
+      to_time: date.at_end_of_quarter }
+  end
+
+  def label_for_quarter(range)
+    quarter = range[:to_time].month / 3
+    year = range[:to_time].year
+
+    "Q#{quarter} #{year}"
+  end
 end
