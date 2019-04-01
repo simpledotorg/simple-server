ebd6a95817b9c0b6d39f4ff896c11b29c514f706
Akshay Gupta <kitallis@gmail.com>
2019-04-01 17:19:23 +0530

    Validate callee_phone_number in CallLog

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -1,6 +1,8 @@
 class CallLog < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :callee_phone_number, presence: true
+
   enum result: {
     queued: 'queued',
     ringing: 'ringing',
