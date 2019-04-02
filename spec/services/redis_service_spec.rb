36899b1ac79376a045e7bc0245b29d6c6b0ca3dd
Akshay Gupta <kitallis@gmail.com>
2019-04-02 15:19:10 +0530

    Modify RedisService spec to reflect bubbling up exceptions

--- spec/services/redis_service_spec.rb
+++ spec/services/redis_service_spec.rb
@@ -9,56 +9,48 @@ RSpec.describe RedisService do
 
   context '#hmset_with_expiry' do
     it 'should store a hash in redis with an expiry' do
-      described_class.new(connection).hmset_with_expiry(key, { user: 'bob' }, expire_key_in_one_day)
+      described_class.new(connection).hmset_with_expiry(key, { user: 'margaret' }, expire_key_in_one_day)
 
-      expect(connection.hmget(key, 'user')).to eq(['bob'])
+      expect(connection.hmget(key, 'user')).to eq(['margaret'])
       expect(connection.ttl(key)).to be > 0
     end
 
     it 'should expire the key after ttl' do
-      described_class.new(connection).hmset_with_expiry(key, { user: 'bob' }, expire_key_in_one_day)
+      described_class.new(connection).hmset_with_expiry(key, { user: 'margaret' }, expire_key_in_one_day)
 
-      expect(connection.hmget(key, 'user')).to eq(['bob'])
+      expect(connection.hmget(key, 'user')).to eq(['margaret'])
 
       travel(2.days) do
         expect(connection.ttl(key)).to eq(-2) # TTL command returns -2 if the key does not exist
       end
     end
 
-    it 'should return nil if there is a connection error' do
+    it 'should throw an exception if there is a connection error' do
       expected_exception = Redis::CannotConnectError.new
       expect(connection).to receive(:hmset).and_raise(expected_exception)
 
-      result = described_class.new(connection).hmset_with_expiry(key, { user: 'bob' }, expire_key_in_one_day)
-
-      expect(result).to be_nil
-    end
-
-    it 'should log to sentry if there is a connection error' do
-      expected_exception = Redis::CannotConnectError.new
-      expect(connection).to receive(:hmset).and_raise(expected_exception)
-      expect(Raven).to receive(:capture_message).at_least(:once)
-
-      described_class.new(connection).hmset_with_expiry(key, { user: 'bob' }, expire_key_in_one_day)
+      expect {
+        described_class.new(connection).hmset_with_expiry(key, { user: 'margaret' }, expire_key_in_one_day)
+      }.to raise_error(expected_exception)
     end
   end
 
   context '#hgetall' do
     it 'should get the stored hash with keys as symbols' do
-      connection.hmset(key, 'user', 'bob')
+      connection.hmset(key, 'user', 'margaret')
 
       result = described_class.new(connection).hgetall(key)
 
-      expect(result).to eq({ user: 'bob' })
+      expect(result).to eq({ user: 'margaret' })
     end
 
-    it 'should return nil if there is a connection error' do
+    it 'should throw an exception if there is a connection error' do
       expected_exception = Redis::CannotConnectError.new
       expect(connection).to receive(:hgetall).and_raise(expected_exception)
 
-      result = described_class.new(connection).hgetall(key)
-
-      expect(result).to be_nil
+      expect {
+        described_class.new(connection).hgetall(key)
+      }.to raise_error(expected_exception)
     end
   end
 end
