9d39b8c1aefc2956d16ab3ce549c29c8f6d7650f
Timmy Jose <timmyjose@users.noreply.github.com>
2019-04-02 18:27:36 +0530

    Added Appointment Transformer for current (v2) API version for `appointment_type`

---
+++ app/tranformers/api/current/appointment_transformer.rb
@@ -0,0 +1,11 @@
+class Api::Current::AppointmentTransformer
+  class << self
+    def to_response(appointment)
+      Api::Current::Transformer.to_response(appointment).except('appointment_type')
+    end
+
+    def from_request(appointment_payload)
+      Api::Current::Transformer.from_request(appointment_payload).except('appointment_type')
+    end
+  end
+end
