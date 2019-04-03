d5b9848b5d65653b82b689ea278730c41265bec2
Akshay Gupta <kitallis@gmail.com>
2019-04-03 19:37:19 +0530

    Handle pagination on AuditLog index for when there are no records

--- app/controllers/admin/audit_logs_controller.rb
+++ app/controllers/admin/audit_logs_controller.rb
@@ -4,14 +4,14 @@ class Admin::AuditLogsController < AdminController
 
   def index
     authorize AuditLog
-    @audit_logs = policy_scope(AuditLog).none
+    audit_logs = policy_scope(AuditLog).none
 
     if params[:user_name].present?
       users = policy_scope(User).where('full_name ilike ?', "%#{params[:user_name]}%")
-      @audit_logs = paginate(policy_scope(AuditLog)
-                               .where(user_id: users.pluck(:id))
-                               .order(created_at: :desc))
+      audit_logs = policy_scope(AuditLog).where(user_id: users.pluck(:id)).order(created_at: :desc)
     end
+
+    @audit_logs = paginate(audit_logs)
   end
 
   def show
