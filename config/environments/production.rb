bc5b063a8ca0af35f505b37f4c121a8c56c77cab
Govind Krishna Joshi <govindkrjoshi@gmail.com>
2019-04-05 17:53:55 +0530

    Remove redis password from config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :redis_store,
-    { host: ENV['RAILS_CACHE_REDIS_URL'],
-      password: ENV['RAILS_CACHE_REDIS_PASSWORD'] }
+    { host: ENV['RAILS_CACHE_REDIS_URL'] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter      = :sidekiq
