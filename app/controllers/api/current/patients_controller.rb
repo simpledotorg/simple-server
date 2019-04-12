06afc69fa5ebcc221548a53ce232c3022feb9572
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-04-12 14:38:43 +0530

    Update patient sync controller to accept business identifiers

--- app/controllers/api/current/patients_controller.rb
+++ app/controllers/api/current/patients_controller.rb
@@ -49,6 +49,7 @@ class Api::Current::PatientsController < Api::Current::SyncController
   def patients_params
     permitted_address_params = %i[id street_address village_or_colony district state country pin created_at updated_at]
     permitted_phone_number_params = %i[id number phone_type active created_at updated_at]
+    permitted_business_identifier_params = %i[id identifier identifier_type metadata_version metadata created_at updated_at]
 
     params.require(:patients).map do |single_patient_params|
       single_patient_params.permit(
@@ -62,7 +63,8 @@ class Api::Current::PatientsController < Api::Current::SyncController
         :created_at,
         :updated_at,
         phone_numbers: [permitted_phone_number_params],
-        address: permitted_address_params
+        address: permitted_address_params,
+        business_identifiers: [permitted_business_identifier_params]
       )
     end
   end
