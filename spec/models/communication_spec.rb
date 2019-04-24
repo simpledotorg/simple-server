0a16887fdfd1a97cd31c82df65a02c29d6d67f5c
Akshay Gupta <kitallis@gmail.com>
2019-04-25 01:26:37 +0530

    Add specs for Communication.create_with_twilio_details!
    * And the detailable polymorphic association

--- spec/models/communication_spec.rb
+++ spec/models/communication_spec.rb
@@ -4,6 +4,7 @@ describe Communication, type: :model do
   describe 'Associations' do
     it { should belong_to(:user) }
     it { should belong_to(:appointment) }
+    it { should belong_to(:detailable) }
   end
 
   describe 'Validations' do
@@ -12,5 +13,31 @@ describe Communication, type: :model do
 
   describe 'Behavior' do
     it_behaves_like 'a record that is deletable'
+  end
+
+  describe '.create_with_twilio_details!' do
+    let(:user) { create(:user) }
+    let(:appointment) { create(:appointment) }
+
+    it 'creates a communication with a TwilioSmsDeliveryDetail' do
+      expect {
+        Communication.create_with_twilio_details!(user: user,
+                                                  appointment: appointment,
+                                                  twilio_session_id: SecureRandom.uuid,
+                                                  twilio_msg_status: 'sent')
+      }.to change { Communication.count }.by(1)
+             .and change { TwilioSmsDeliveryDetail.count }.by(1)
+    end
+
+    it 'does not create a TwilioSmsDeliveryDetail if Communication fails to save' do
+      expect {
+        Communication.create_with_twilio_details!(user: nil,
+                                                  appointment: appointment,
+                                                  twilio_session_id: SecureRandom.uuid,
+                                                  twilio_msg_status: 'sent')
+      }.to raise_error(StandardError)
+             .and change { Communication.count }.by(0)
+                    .and change { TwilioSmsDeliveryDetail.count }.by(0)
+    end
   end
 end
