6809c0e442a0f02dec7eeb89e838558e0735b0ce
Akshay Gupta <kitallis@gmail.com>
2019-04-25 18:39:43 +0530

    Spin off a SMSReminderJob from SMSReminderService in batches

--- app/jobs/sms_reminder_job.rb
+++ app/jobs/sms_reminder_job.rb
@@ -1,10 +1,34 @@
 class SMSReminderJob < ApplicationJob
+  include Rails.application.routes.url_helpers
+
   queue_as :default
   self.queue_adapter = :sidekiq
 
   DEFAULT_RETRY_TIMES = 5
   DEFAULT_RETRY_SECONDS = 10.minutes.seconds.to_i
 
-  def perform(type)
+  def perform(appointment_id, type)
+    appointment = Appointment.find(appointment_id)
+    sms_response = send_sms(appointment, type)
+    Communication.create_with_twilio_details!(user: BOT_USER,
+                                              appointment: appointment,
+                                              twilio_session_id: sms_response.sid,
+                                              twilio_msg_status: sms_response.status)
+  end
+
+  private
+
+  def send_sms(appointment, type)
+    SmsNotificationService
+      .new(appointment.patient.latest_phone_number)
+      .send_reminder_sms(type,
+                         appointment,
+                         twilio_sms_delivery_url,
+                         SmsHelper::sms_locale(appointment.patient.address))
+  end
+
+  def twilio_sms_delivery_url
+    api_current_twilio_sms_delivery_url(host: ENV.fetch('SIMPLE_SERVER_HOSTNAME'),
+                                        protocol: ENV.fetch('SIMPLE_SERVER_HOST_PROTOCOL'))
   end
 end
--- app/services/sms_reminder_service.rb
+++ app/services/sms_reminder_service.rb
@@ -1,33 +1,21 @@
 class SMSReminderService
-  include Rails.application.routes.url_helpers
-
   def three_days_after_missed_visit
-    days_overdue = 3
+    Appointment
+      .overdue
+      .select { |a| a.days_overdue > 3 }
+      .group_by(&:facility_id)
+      .slice(*ENV.fetch('SMS_REMINDER_ROLLOUT_FACILITY_IDS').split(','))
+      .flat_map { |_, appointments| appointments.sample(rollout_percentage(appointments.size)) }
+      .in_groups_of(1000) do |appointments|
 
-    Appointment.overdue.select { |a| a.days_overdue > days_overdue }.each do |appointment|
-      unless appointment.reminder_messages_around(days_overdue).present?
-        sms_response = send_sms(appointment, '3_days_after_missed_visit')
-        Communication.create_with_twilio_details!(user: BOT_USER,
-                                                  appointment: appointment,
-                                                  twilio_session_id: sms_response.sid,
-                                                  twilio_msg_status: sms_response.status)
-      end
+      eligible_appointments = appointments.select { |a| a.reminder_messages_around(3).present? }
+      SMSReminderJob.perform_later(eligible_appointments)
     end
   end
 
   private
 
-  def send_sms(appointment, type)
-    SmsNotificationService
-      .new(appointment.patient.phone_numbers.last.number)
-      .send_reminder_sms(type,
-                         appointment,
-                         twilio_sms_delivery_url,
-                         SmsHelper::sms_locale(appointment.patient.address))
-  end
-
-  def twilio_sms_delivery_url
-    api_current_twilio_sms_delivery_url(host: ENV.fetch('SIMPLE_SERVER_HOSTNAME'),
-                                        protocol: ENV.fetch('SIMPLE_SERVER_HOST_PROTOCOL'))
+  def rollout_percentage(total)
+    (ENV.fetch('SMS_REMINDER_ROLLOUT_PER_FACILITY') / 100.0) * total
   end
 end
