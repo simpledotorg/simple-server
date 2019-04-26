4efa78fd99e9996e132056713d21b3a94ad1cf3f
Akshay Gupta <kitallis@gmail.com>
2019-04-27 00:38:37 +0530

    Make BOT_USER into SMS_REMINDER_BOT_USER

--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -1,9 +1,8 @@
 ActiveSupport.on_load(:after_initialize, :yield => true) do
   unless Rails.env.test?
-    BOT_USER = User
-                 .find_or_initialize_by(id: ENV.fetch('BOT_USER_UUID'),
-                                        full_name: ENV.fetch('BOT_USER_NAME'))
-                 .save(validate: false) && \
-             User.find(ENV.fetch('BOT_USER_UUID'))
+    SMS_REMINDER_BOT_USER = User
+                              .find_or_initialize_by(id: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'),
+                                                     full_name: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
+                              .save(validate: false) && User.find(ENV.fetch('BOT_USER_UUID'))
   end
 end
--- lib/tasks/sms_reminder.rake
+++ lib/tasks/sms_reminder.rake
@@ -1,6 +1,6 @@
 namespace :sms_reminder do
   desc 'Send automatic SMS reminder to patients who missed their scheduled visit by three days'
   task three_days_after_missed_visit: :environment do
-    SMSReminderService.new(BOT_USER).three_days_after_missed_visit
+    SMSReminderService.new(SMS_REMINDER_BOT_USER).three_days_after_missed_visit
   end
 end
