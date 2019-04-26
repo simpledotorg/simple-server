faa5848796da594f400194fa7df39a39723ef7f5
Akshay Gupta <kitallis@gmail.com>
2019-04-26 16:06:43 +0530

    Initialize BOT_USER after all rails initializers have finished

--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -1,5 +1,7 @@
-BOT_USER = User
-             .find_or_initialize_by(id: ENV.fetch('BOT_USER_UUID'),
-                                    full_name: ENV.fetch('BOT_USER_NAME'))
-             .save(validate: false) && \
+Rails.configuration.after_initialize do
+  BOT_USER = User
+               .find_or_initialize_by(id: ENV.fetch('BOT_USER_UUID'),
+                                      full_name: ENV.fetch('BOT_USER_NAME'))
+               .save(validate: false) && \
              User.find(ENV.fetch('BOT_USER_UUID'))
+end
