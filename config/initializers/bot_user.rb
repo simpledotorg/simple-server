701e6584746e5700c7450dfcccab3e69b5b8c3c5
Akshay Gupta <kitallis@gmail.com>
2019-04-29 14:21:00 +0530

    Find the BOT_USER using the SMS_REMINDER_BOT_USER config

--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -3,6 +3,6 @@ ActiveSupport.on_load(:after_initialize, :yield => true) do
     SMS_REMINDER_BOT_USER = User
                               .find_or_initialize_by(id: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'),
                                                      full_name: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
-                              .save(validate: false) && User.find(ENV.fetch('BOT_USER_UUID'))
+                              .save(validate: false) && User.find(ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
   end
 end
