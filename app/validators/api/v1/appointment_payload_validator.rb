cc3c6aff8e6cf9a10687cc72ae89a1ed52dcdbf1
Timmy Jose <timmy@nilenso.com>
2019-04-30 18:15:45 +0530

    Fixed appointment_type for v1

--- app/validators/api/v1/appointment_payload_validator.rb
+++ app/validators/api/v1/appointment_payload_validator.rb
@@ -1,2 +1,20 @@
 class Api::V1::AppointmentPayloadValidator < Api::Current::AppointmentPayloadValidator
+  attr_accessor(
+    :id,
+    :patient_id,
+    :facility_id,
+    :scheduled_date,
+    :status,
+    :cancel_reason,
+    :remind_on,
+    :agreed_to_visit,
+    :created_at,
+    :updated_at
+  )
+
+  validate :validate_schema
+
+  def schema
+    Api::V1::Models.appointment
+  end
 end
