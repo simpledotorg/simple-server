5d8f4693bc03a365c3bbe7a10eb811f1af8cf834
Akshay Gupta <kitallis@gmail.com>
2019-05-02 01:22:37 +0530

    BOT_USER initializer now works consistently

--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -1,8 +1,25 @@
+module BotUser
+  def find_bot_user
+    User.where(id: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
+  end
+
+  def find_or_create_bot_user
+    bot_user = find_bot_user
+    return bot_user if bot_user.present?
+
+    User
+      .find_or_initialize_by(id: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'),
+                             full_name: ENV.fetch('SMS_REMINDER_BOT_USER_NAME'))
+      .save(validate: false)
+
+    find_bot_user
+  end
+end
+
 ActiveSupport.on_load(:after_initialize, :yield => true) do
+  include BotUser
+
   unless Rails.env.test?
-    SMS_REMINDER_BOT_USER = User
-                              .find_or_initialize_by(id: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'),
-                                                     full_name: ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
-                              .save(validate: false) && User.find(ENV.fetch('SMS_REMINDER_BOT_USER_UUID'))
+    SMS_REMINDER_BOT_USER = find_or_create_bot_user
   end
 end
