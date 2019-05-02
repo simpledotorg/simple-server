4c5f03fe3f7e4d5fb3bbfdf19b86df4de847716f
Govind Krishna Joshi <govind@nilenso.com>
2019-05-02 12:30:05 +0530

    Add specs for email authentications

--- spec/factories/phone_number_authentications.rb
+++ spec/factories/phone_number_authentications.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :phone_number_authentication do
-    
+    phone_number { Faker::PhoneNumber.phone_number }
+    password { rand(1000..9999).to_s }
+    password_confirmation { password }
+    otp { rand(100000..999999).to_s }
+    otp_valid_until { 3.minutes.from_now }
+    access_token { SecureRandom.hex(32) }
+
+    trait :with_password_digest do
+      password { nil }
+      password_digest { Faker::Crypto.sha256 }
+    end
   end
 end
--- spec/models/phone_number_authentication_spec.rb
+++ spec/models/phone_number_authentication_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumberAuthentication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_one(:master_user_authentication) }
+    it { should have_one(:master_user).through(:master_user_authentication) }
+  end
+
+  describe 'Validations' do
+    let(:subject) { create(:phone_number_authentication) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_uniqueness_of(:phone_number).ignoring_case_sensitivity }
+
+    context 'presence of password' do
+      it 'validates a PhoneNumberAuthentication with a password' do
+        phone_number_authentication = build(:phone_number_authentication, password: '1234')
+        expect(phone_number_authentication).to be_valid
+      end
+
+      it 'validates a PhoneNumberAuthentication with a password_digest' do
+        phone_number_authentication = build(:phone_number_authentication, :with_password_digest)
+        expect(phone_number_authentication).to be_valid
+      end
+
+      it 'invalidates a PhoneNumberAuthentication without a password or a password_digest' do
+        phone_number_authentication = build(:phone_number_authentication, password: nil, password_digest: nil)
+        expect(phone_number_authentication).to be_invalid
+      end
+    end
+  end
 end
