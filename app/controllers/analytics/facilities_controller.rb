f330ede6aa400e6c45847efe29351477af6ab412
Tim Cheadle <tim@rationalmeans.com>
2019-05-07 20:12:14 -0400

    Began rough draft of summary control rate charts for facility

--- app/controllers/analytics/facilities_controller.rb
+++ app/controllers/analytics/facilities_controller.rb
@@ -3,9 +3,16 @@ class Analytics::FacilitiesController < AnalyticsController
   before_action :set_facility_group
   before_action :set_organization
 
+  COHORT_MONTHS_PREVIOUS = 6
+
   def show
     @facility_analytics = @facility.patient_set_analytics(@from_time, @to_time)
     @user_analytics = user_analytics
+
+    @registered = registered
+    @visited = visited(@registered)
+    @controlled = controlled(@visited)
+    @uncontrolled = uncontrolled(@visited)
   end
 
   def graphics
@@ -35,6 +42,55 @@ class Analytics::FacilitiesController < AnalyticsController
 
   def user_analytics
     users_for_facility.map { |user| [user, Analytics::UserAnalytics.new(user, @facility)] }.to_h
+  end
+
+  def registered
+    cohort_start = @from_time - COHORT_MONTHS_PREVIOUS.months
+    cohort_end = @to_time - COHORT_MONTHS_PREVIOUS.months
+
+    Patient.select(%Q(
+      patients.*,
+      oldest_bps.device_created_at as bp_device_created_at,
+      oldest_bps.facility_id as bp_facility_id,
+      oldest_bps.systolic as bp_systolic,
+      oldest_bps.diastolic as bp_diastolic
+    )).joins(%Q(
+      INNER JOIN (
+        SELECT DISTINCT ON (patient_id) *
+        FROM blood_pressures
+        ORDER BY patient_id, device_created_at ASC
+      ) as oldest_bps
+      ON oldest_bps.patient_id = patients.id
+    )).where(
+      "oldest_bps.device_created_at" => cohort_start..cohort_end,
+      "oldest_bps.facility_id" => @facility
+    )
+  end
+
+  def visited(patients)
+    patients.select(%Q(
+      patients.*,
+      newest_bps.device_created_at as bp_device_created_at,
+      newest_bps.systolic as bp_systolic,
+      newest_bps.diastolic as bp_diastolic
+    )).joins(%Q(
+      INNER JOIN (
+        SELECT DISTINCT ON (patient_id) *
+        FROM blood_pressures
+        WHERE device_created_at >= '#{@from_time}'
+        AND device_created_at <= '#{@to_time}'
+        ORDER BY patient_id, device_created_at DESC
+      ) as newest_bps
+      ON newest_bps.patient_id = patients.id
+    ))
+  end
+
+  def controlled(patients)
+    patients.select { |p| p.bp_systolic < 140 && p.bp_diastolic < 90 }
+  end
+
+  def uncontrolled(patients)
+    patients.select { |p| p.bp_systolic >= 140 || p.bp_diastolic >= 90 }
   end
 end
 
