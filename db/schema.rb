a02388a565517cf2693d07989e1d47d36c625f96
Akshay Gupta <kitallis@gmail.com>
2019-05-10 20:12:06 +0530

    Remove User association from CallLogs in favour of caller_phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430140043) do
+ActiveRecord::Schema.define(version: 20190510143922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,14 +132,12 @@ ActiveRecord::Schema.define(version: 20190430140043) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "user_id", null: false
-    t.index ["user_id"], name: "index_call_logs_on_user_id"
+    t.string "caller_phone_number", null: false
   end
 
   create_table "communications", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "appointment_id", null: false
     t.uuid "user_id", null: false
-    t.string "communication_type"
     t.datetime "device_created_at", null: false
     t.datetime "device_updated_at", null: false
     t.datetime "created_at", null: false
@@ -147,6 +145,7 @@ ActiveRecord::Schema.define(version: 20190430140043) do
     t.datetime "deleted_at"
     t.string "detailable_type"
     t.bigint "detailable_id"
+    t.string "communication_type"
     t.index ["appointment_id"], name: "index_communications_on_appointment_id"
     t.index ["deleted_at"], name: "index_communications_on_deleted_at"
     t.index ["detailable_type", "detailable_id"], name: "index_communications_on_detailable_type_and_detailable_id"
