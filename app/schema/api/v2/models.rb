42da81e17fd2f975068d10ae737378199e6740f7
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-05-10 15:15:26 +0530

    Update v2 models to ignore recorded_at

--- app/schema/api/v2/models.rb
+++ app/schema/api/v2/models.rb
@@ -11,8 +11,50 @@ class Api::V2::Models < Api::Current::Models
 
     def appointment
       super
-        .tap { |d| d[:properties].delete(:appointment_type) }
-        .tap { |d| d[:required] -= %w(appointment_type) }
+        .tap { |d| d[:properties].delete(:appointment_type).delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(appointment_type recorded_at) }
+    end
+
+    def patient
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def address
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def phone_number
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def blood_pressure
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def prescription_drug
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def communication
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
+    end
+
+    def medical_history
+      super
+        .tap { |d| d[:properties].delete(:recorded_at) }
+        .tap { |d| d[:required] -= %w(recorded_at) }
     end
 
     def definitions
