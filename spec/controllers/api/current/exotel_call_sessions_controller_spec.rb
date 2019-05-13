95d63ebc37ee8a6174bf0edb0324a2a5bc5e8ac4
Akshay Gupta <kitallis@gmail.com>
2019-05-13 12:21:39 +0000

    Remove User authorization during a call masking flow

--- spec/controllers/api/current/exotel_call_sessions_controller_spec.rb
+++ spec/controllers/api/current/exotel_call_sessions_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Api::Current::ExotelCallSessionsController, type: :controller do
         expect(response.headers['Content-Type']).to eq('text/plain; charset=utf-8')
       end
 
-      it 'should create an Exotel session if both the user and patient are registered' do
+      it 'should create an Exotel session if the patient is registered' do
         get :create, params: { From: user.phone_number,
                                digits: patient.phone_numbers.first.number,
                                CallType: 'call-attempt',
@@ -37,15 +37,6 @@ RSpec.describe Api::Current::ExotelCallSessionsController, type: :controller do
                                CallSid: SecureRandom.uuid }
 
         expect(response.headers['Content-Type']).to eq('text/plain; charset=utf-8')
-      end
-
-      it 'should not create an Exotel session if user is not registered' do
-        get :create, params: { From: unknown_phone_number,
-                               digits: patient.phone_numbers.first.number,
-                               CallType: 'call-attempt',
-                               CallSid: SecureRandom.uuid }
-
-        expect(response).to have_http_status(403)
       end
 
       it 'should not create an Exotel session if the patient is not registered' do
@@ -156,7 +147,7 @@ RSpec.describe Api::Current::ExotelCallSessionsController, type: :controller do
       context 'call details job' do
         it 'should schedule a job to log the call details' do
           assert_enqueued_with(job: ExotelCallDetailsJob, args: [call_id,
-                                                                 user.id,
+                                                                 user.phone_number,
                                                                  patient.phone_numbers.first.number,
                                                                  'completed']) do
             get :terminate, params: { From: user.phone_number,
@@ -170,7 +161,7 @@ RSpec.describe Api::Current::ExotelCallSessionsController, type: :controller do
 
         it 'assigns call details with status as unknown if CallStatus was null' do
           assert_enqueued_with(job: ExotelCallDetailsJob, args: [call_id,
-                                                                 user.id,
+                                                                 user.phone_number,
                                                                  patient.phone_numbers.first.number,
                                                                  'unknown']) do
             get :terminate, params: { From: user.phone_number,
