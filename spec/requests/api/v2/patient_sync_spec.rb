7103951c264a17cebebd5b3a3e1457daff322224
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-05-17 18:16:20 +0530

    Add request spec to test patient's recorded_at is set correctly

--- spec/requests/api/v2/patient_sync_spec.rb
+++ spec/requests/api/v2/patient_sync_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Patients sync', type: :request do
   let(:request_user) { FactoryBot.create(:user) }
 
   let(:sync_route) { '/api/v2/patients/sync' }
+  let(:blood_pressure_sync_route) { '/api/v2/blood_pressures/sync' }
 
   let(:build_payload) { lambda { build_patient_payload_v2(FactoryBot.build(:patient, registration_facility: request_user.facility)) } }
   let(:build_invalid_payload) { lambda { build_invalid_patient_payload } }
@@ -34,5 +35,22 @@ RSpec.describe 'Patients sync', type: :request do
     get sync_route, params: { process_token: process_token }, headers: headers
 
     assert_sync_success(response, process_token)
+  end
+
+  it "sets recorded_at to the earliest bp's recorded_at in case patient is synced later" do
+    patient = FactoryBot.build(:patient)
+
+    blood_pressure_recorded_at = 1.month.ago
+    blood_pressure = FactoryBot.build(:blood_pressure,
+                                      patient: patient,
+                                      device_created_at: blood_pressure_recorded_at)
+    blood_pressure_payload = build_blood_pressure_payload_v2(blood_pressure)
+    post blood_pressure_sync_route, params: { blood_pressures: [blood_pressure_payload] }.to_json, headers: headers
+
+    patient_payload = build_patient_payload_v2(patient)
+    post sync_route, params: { patients: [patient_payload] }.to_json, headers: headers
+
+    patient_in_db = Patient.find(patient.id)
+    expect(patient_in_db.recorded_at).to eq(blood_pressure_recorded_at)
   end
 end
