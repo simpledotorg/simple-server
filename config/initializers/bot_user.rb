cbfebcaf52f19b2b28f550b78b8d70807b0e135f
vkrm <mail@vkrm.is>
2019-05-17 18:33:20 +0530

    Reorder environment and migration checks

--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -17,7 +17,7 @@ end
 
 ActiveSupport.on_load(:after_initialize, :yield => true) do
   include BotUser
-  if !defined?(is_running_migration?) && !is_running_migration? && !Rails.env.test?
+  if !Rails.env.test? && !(defined?(is_running_migration?) && is_running_migration?)
     SMS_REMINDER_BOT_USER = find_or_create_bot_user(ENV.fetch('APPOINTMENT_NOTIFICATION_BOT_USER_UUID'),
                                                     ENV.fetch('APPOINTMENT_NOTIFICATION_BOT_USER_NAME'))
   end
