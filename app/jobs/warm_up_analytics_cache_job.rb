b54a5527cb415153508443b75bb9c2e67e142338
Govind Krishna Joshi <govind@nilenso.com>
2019-05-21 18:35:34 +0530

    Use the correct time string for to_time

--- app/jobs/warm_up_analytics_cache_job.rb
+++ app/jobs/warm_up_analytics_cache_job.rb
@@ -6,7 +6,7 @@ class WarmUpAnalyticsCacheJob < ApplicationJob
     record = record_type.constantize.find(record_id)
 
     from_time = from_time_string.to_time
-    to_time = from_time_string.to_time
+    to_time = to_time_string.to_time
 
     Rails.cache.delete(record.analytics_cache_key(from_time, to_time))
     record.patient_set_analytics(from_time, to_time)
