0f3a1fbd1213af2ed711532931b56ea78f863fcf
Divyansh Prakash <divyansh.prakash@yahoo.com>
2019-05-29 17:12:29 +0530

    Merge migrations, update name to include today's date

--- db/migrate/20190516113220_add_indexes_to_recorded_at_in_patients_and_blood_pressures.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexesToRecordedAtInPatientsAndBloodPressures < ActiveRecord::Migration[5.1]
-  def change
-    add_index :patients, :recorded_at
-    add_index :blood_pressures, :recorded_at
-  end
-end
