afcadfefe8dfbb271b8d3cfa70ea0388c880db74
Akshay Gupta <kitallis@gmail.com>
2019-05-30 08:09:05 +0000

    Support Twilio Sub-accounts in SMSNotificationService (#428)
    * Remove unique sidekiq job support since they are flaky
    * Only run scheduled cron jobs for the capistrano role: cron

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "ec2-52-66-250-216.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db)
+server "ec2-52-66-250-216.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db cron)
 server "ec2-13-126-205-193.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db)
 server "ec2-13-233-151-58.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(sidekiq)
