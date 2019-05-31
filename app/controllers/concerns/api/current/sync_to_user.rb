93c627e74c3933c702334d0eac5a4c33f0483981
Akshay Gupta <kitallis@gmail.com>
2019-05-31 07:36:37 +0000

    Add non-breaking Sync API changes without frequent version bumps
    Add resync_token to process_token to determine syncing from beginning for V2 onwards

--- app/controllers/concerns/api/current/sync_to_user.rb
+++ app/controllers/concerns/api/current/sync_to_user.rb
@@ -29,7 +29,8 @@ module Api::Current::SyncToUser
     def response_process_token
       { current_facility_id: current_facility.id,
         current_facility_processed_since: processed_until(current_facility_records) || current_facility_processed_since,
-        other_facilities_processed_since: processed_until(other_facility_records) || other_facilities_processed_since }
+        other_facilities_processed_since: processed_until(other_facility_records) || other_facilities_processed_since,
+        resync_token: resync_token }
     end
 
     def encode_process_token(process_token)
@@ -37,17 +38,29 @@ module Api::Current::SyncToUser
     end
 
     def other_facilities_processed_since
+      return Time.new(0) if force_resync?
       process_token[:other_facilities_processed_since].try(:to_time) || Time.new(0)
     end
 
     def current_facility_processed_since
-      if process_token[:current_facility_processed_since].blank?
+      if force_resync?
+        Time.new(0)
+      elsif process_token[:current_facility_processed_since].blank?
         other_facilities_processed_since
       elsif process_token[:current_facility_id] != current_facility.id
-        [process_token[:current_facility_processed_since].to_time, other_facilities_processed_since].min
+        [process_token[:current_facility_processed_since].to_time,
+         other_facilities_processed_since].min
       else
         process_token[:current_facility_processed_since].to_time
       end
+    end
+
+    def force_resync?
+      process_token[:resync_token] != resync_token
+    end
+
+    def resync_token
+      request.headers['HTTP_X_RESYNC_TOKEN']
     end
   end
 end
