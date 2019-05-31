c4a7019b5da334d09e5f38c4203ae594475c8e29
Govind Krishna Joshi <govind@nilenso.com>
2019-05-31 12:05:24 +0530

    Change class for devise routes to email authentications

--- config/routes.rb
+++ config/routes.rb
@@ -141,7 +141,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :admins, controllers: { invitations: 'admins/invitations' }
+  devise_for :admins, class_name: 'EmailAuthentication', controllers: { invitations: 'admins/invitations' }
   resources :admins
 
   namespace :analytics do
