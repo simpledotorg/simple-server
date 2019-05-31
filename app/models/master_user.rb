f33503ba864e734b8daa5b0e43ac7f2947ae3ed4
Govind Krishna Joshi <govind@nilenso.com>
2019-05-31 12:04:35 +0530

    Add email_authentications association to master users

--- app/models/master_user.rb
+++ app/models/master_user.rb
@@ -12,6 +12,7 @@ class MasterUser < ApplicationRecord
   }, _prefix: true
 
   has_many :user_authentications
+  has_many :email_authentications, through: :user_authentications, source: :authenticatable, source_type: 'EmailAuthentication'
   has_many :user_permissions
 
   validates :full_name, presence: true
