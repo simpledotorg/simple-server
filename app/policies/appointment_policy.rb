c53cd4800d180338583f2096807f2048c52ce815
Timmy Jose <zoltan.jose@gmail.com>
2019-05-31 16:48:51 +0530

    Updating appointment policy to use permissions

--- app/policies/appointment_policy.rb
+++ app/policies/appointment_policy.rb
@@ -1,17 +1,30 @@
 class AppointmentPolicy < ApplicationPolicy
   def index?
-    user.owner? || user.supervisor? || user.counsellor?
+    user_has_any_permissions?(
+      :can_manage_all_organizations,
+      [:can_manage_overdue_list_for_facility, record] # record is facility because of the way we use it in the controller
+    )
+  end
+
+  def edit?
+    user_has_any_permissions?(
+      :can_manage_all_organizations,
+      [:can_manage_overdue_list_for_facility, record.facility]
+    )
   end
 
   def update?
-    index?
+    edit?
   end
 
   def download?
-    user.owner? || user.supervisor? && user.organizations.include?(Organization.find_by(name: "IHMI"))
+    user_has_any_permissions?(
+      :can_manage_all_organizations,
+      [:can_download_overdue_list_for_facility, record]
+    )
   end
 
-  class Scope
+  class Scope < Scope
     attr_reader :user, :scope
 
     def initialize(user, scope)
@@ -20,7 +33,9 @@ class AppointmentPolicy < ApplicationPolicy
     end
 
     def resolve
-      scope.where(facility: @user.facilities)
+      scope.where(
+        facility: resources_for_permission(:can_manage_overdue_list_for_facility) +
+          resources_for_permission(:can_download_overdue_list_for_facility))
     end
   end
 end
--- spec/policies/appointment_policy_spec.rb
+++ spec/policies/appointment_policy_spec.rb
@@ -3,65 +3,144 @@ require 'rails_helper'
 RSpec.describe AppointmentPolicy do
   subject { described_class }
 
-  let(:owner) { create(:admin, :owner) }
-  let(:supervisor) { create(:admin, :supervisor) }
-  let(:analyst) { create(:admin, :analyst) }
-  let(:organization_owner) { create(:admin, :organization_owner) }
-  let(:counsellor) { create(:admin, :counsellor) }
+  let(:facility1) { create(:facility) }
+  let(:facility2) { create(:facility) }
 
-  permissions :index?, :edit? do
-    it 'permits owners' do
-      expect(subject).to permit(owner, Appointment)
+  let(:user_can_manage_all_organizations) do
+    user = create(:master_user)
+    create(:user_permission, user: user, permission_slug: :can_manage_all_organizations, resource: nil)
+    user
+  end
+
+  let(:user_can_manage_overdue_list_for_facility1) do
+    user = create(:master_user)
+    create(:user_permission, user: user, permission_slug: :can_manage_overdue_list_for_facility, resource: facility1)
+    user
+  end
+
+  let(:other_user) { create(:master_user) }
+
+  permissions :index? do
+    it 'permits users who can manage overdue list for a facility' do
+      expect(subject).to permit(user_can_manage_overdue_list_for_facility1, facility1)
     end
 
-    it 'permits counsellors' do
-      expect(subject).to permit(counsellor, Appointment)
+    it 'permits users who can manage all organizations' do
+      expect(subject).to permit(user_can_manage_all_organizations, facility1)
+      expect(subject).to permit(user_can_manage_all_organizations, facility2)
     end
 
-    it 'permits supervisors' do
-      expect(subject).to permit(supervisor, Appointment)
+    it 'denies other users from managing overdue list for a facility' do
+      expect(subject).not_to permit(other_user, facility1)
+      expect(subject).not_to permit(other_user, facility2)
+    end
+  end
+
+  permissions :edit? do
+    let(:appointment1) { create(:appointment, :overdue, facility: facility1) }
+    let(:appointment2) { create(:appointment, :overdue, facility: facility2) }
+
+    it 'permits users who can manage overdue list for a facility' do
+      expect(subject).to permit(user_can_manage_overdue_list_for_facility1, appointment1)
     end
 
-    it 'denies organization_owners' do
-      expect(subject).not_to permit(organization_owner, Appointment)
+    it 'permits users who can manage all organizations' do
+      expect(subject).to permit(user_can_manage_all_organizations, appointment1)
+      expect(subject).to permit(user_can_manage_all_organizations, appointment2)
     end
 
-    it 'denies analysts' do
-      expect(subject).not_to permit(analyst, User)
+    it 'denies other users from managing overdue list for a facility' do
+      expect(subject).not_to permit(other_user, appointment1)
+    end
+
+    it 'denies uses from managing overdue list for a different facility' do
+      expect(subject).not_to permit(user_can_manage_overdue_list_for_facility1, appointment2)
     end
   end
 
   permissions :download? do
-    it 'permits owners' do
-      expect(subject).to permit(owner, Appointment)
+    let(:user_can_download_overdue_list_for_facility1) do
+      user = create(:master_user)
+      create(:user_permission, user: user, permission_slug: :can_download_overdue_list_for_facility, resource: facility1)
+      user
     end
 
-    context "supervisors" do
-      let(:ihmi) { create(:organization, name: "IHMI") }
-      let(:ihmi_group) { create(:facility_group, organization: ihmi) }
-      let(:non_ihmi_group) { create(:facility_group) }
-
-      it 'permits supervisors in IHMI' do
-        supervisor.admin_access_controls = [AdminAccessControl.new(access_controllable: ihmi_group)]
-        expect(subject).to permit(supervisor, User)
-      end
-
-      it 'denies supervisors not in IHMI' do
-        supervisor.admin_access_controls = [AdminAccessControl.new(access_controllable: non_ihmi_group)]
-        expect(subject).not_to permit(supervisor, User)
-      end
+    it 'permits users who manage all organizations' do
+      expect(subject).to permit(user_can_manage_all_organizations, facility1)
+      expect(subject).to permit(user_can_manage_all_organizations, facility2)
     end
 
-    it 'denies counsellors' do
-      expect(subject).not_to permit(counsellor, Appointment)
+    it 'permits users who can manage overdue list for a facility' do
+      expect(subject).to permit(user_can_download_overdue_list_for_facility1, facility1)
     end
 
-    it 'denies organization_owners' do
-      expect(subject).not_to permit(organization_owner, Appointment)
+    it 'denies other users from managing overdue list for a facility' do
+      expect(subject).not_to permit(other_user, facility1)
+      expect(subject).not_to permit(other_user, facility2)
     end
 
-    it 'denies analysts' do
-      expect(subject).not_to permit(analyst, User)
+    it 'denies uses from managing overdue list for a different facility' do
+      expect(subject).not_to permit(user_can_download_overdue_list_for_facility1, facility2)
+    end
+  end
+end
+
+RSpec.describe AppointmentPolicy::Scope do
+  let(:subject) { described_class }
+
+  let(:facility1) { create(:facility) }
+  let(:facility2) { create(:facility) }
+
+  let(:appointment1) { create(:appointment, :overdue, facility: facility1) }
+  let(:appointment2) { create(:appointment, :overdue, facility: facility2) }
+
+  let(:user_can_manage_all_organizations) do
+    user = create(:master_user)
+    create(:user_permission,
+           user: user,
+           permission_slug: :can_manage_all_organizations)
+    user
+  end
+
+  let(:user_can_manage_overdue_list_for_facility1) do
+    user = create(:master_user)
+    create(:user_permission, user: user, permission_slug: :can_manage_overdue_list_for_facility, resource: facility1)
+    user
+  end
+
+  let(:user_can_download_overdue_list_for_facility1) do
+    user = create(:master_user)
+    create(:user_permission, user: user, permission_slug: :can_download_overdue_list_for_facility, resource: facility2)
+    user
+  end
+
+  let(:other_user) { create(:master_user) }
+
+  context 'user who can manager all organisations' do
+    it 'resolves all appointments' do
+      resolved_records = subject.new(user_can_manage_all_organizations, Appointment.all).resolve
+      expect(resolved_records).to match_array(Appointment.all.to_a)
+    end
+  end
+
+  context 'user who can manage overdue list for a facility' do
+    it 'resolves all appointments for facilities for which the user is authorised' do
+      resolved_records = subject.new(user_can_manage_overdue_list_for_facility1, Appointment.all).resolve
+      expect(resolved_records).to match_array([appointment1])
+    end
+  end
+
+  context 'user who can download overdue list for a facility' do
+    it 'resolves all appointments for facilities for which the user is authorised' do
+      resolved_records = subject.new(user_can_download_overdue_list_for_facility1, Appointment.all).resolve
+      expect(resolved_records).to match_array([appointment2])
+    end
+  end
+
+  context 'other users' do
+    it 'resolves no appointments' do
+      resolved_records = subject.new(other_user, Appointment.all).resolve
+      expect(resolved_records).to be_empty
     end
   end
 end
