ccc9c7ea5315c8b51d84d7b0d358576cb4984dfd
Akshay Gupta <kitallis@gmail.com>
2019-06-14 18:55:50 +0530

    Refactor DistrictAnalyticsQuery

--- app/queries/district_analytics_query.rb
+++ app/queries/district_analytics_query.rb
@@ -1,24 +1,22 @@
 class DistrictAnalyticsQuery
   def initialize(district_name:)
     @district_name = district_name
-
-    @follow_up_patients = nil
-    @registered_patients = nil
-    @total_registered_patients = nil
   end
 
   def by_facility
     data = total_registered_patients
-             .deep_merge(follow_up_patients)
-             .deep_merge(registered_patients)
+             .deep_merge(follow_up_patients_by_month)
+             .deep_merge(registered_patients_by_month)
     Facility.where(id: data.keys).map { |f| [f, data[f.id]] }.to_h
   end
 
   private
 
-  FOLLOW_UP_LAST_3_MONTHS_ATTRS = %w(follow_up_penultimate_month follow_up_last_month follow_up_current_month)
-  TOTAL_REGISTERED_ATTRS = %w(total_registered_patients)
-  REGISTERED_LAST_3_MONTHS_ATTRS = %w(registered_patients_penultimate_month registered_patients_last_month registered_patients_current_month)
+  TOTAL_REGISTERED_PATIENTS_ATTRS = %w(total_registered_patients)
+  MONTHLY_REGISTERED_PATIENTS_ATTRS =
+    %w(registered_patients_penultimate_month registered_patients_last_month registered_patients_current_month)
+  MONTHLY_FOLLOW_UP_PATIENTS_ATTRS =
+    %w(follow_up_penultimate_month follow_up_last_month follow_up_current_month)
 
   def total_registered_patients
     @total_registered_patients ||=
@@ -28,13 +26,13 @@ class DistrictAnalyticsQuery
         .group('facilities.name', 'facilities.id')
         .order('facilities.name')
         .pluck("facilities.id AS facility_id",
-               "count(distinct(patients.id)) AS #{TOTAL_REGISTERED_ATTRS.first}")
+               "count(distinct(patients.id)) AS #{TOTAL_REGISTERED_PATIENTS_ATTRS.first}")
 
-    group_by_facility(@total_registered_patients, TOTAL_REGISTERED_ATTRS)
+    group_by_facility(@total_registered_patients, TOTAL_REGISTERED_PATIENTS_ATTRS)
   end
 
-  def registered_patients
-    @registered_patients ||=
+  def registered_patients_by_month
+    @registered_patients_by_month ||=
       Patient
         .joins('INNER JOIN facilities ON facilities.id = patients.registration_facility_id')
         .where('facilities.district = ?', @district_name)
@@ -45,23 +43,23 @@ class DistrictAnalyticsQuery
                          WHEN patients.device_created_at >= date_trunc('MONTH', now() - interval '2 months')::DATE
                          AND patients.device_created_at < date_trunc('MONTH', now() - interval '1 month')::DATE THEN 1
                          ELSE 0
-                     END) AS #{REGISTERED_LAST_3_MONTHS_ATTRS.first}),
+                     END) AS #{MONTHLY_REGISTERED_PATIENTS_ATTRS.first}),
                %Q(sum(CASE
                          WHEN patients.device_created_at >= date_trunc('MONTH', now() - interval '1 month')::DATE
                          AND device_created_at < date_trunc('MONTH', now())::DATE THEN 1
                          ELSE 0
-                     END) AS #{REGISTERED_LAST_3_MONTHS_ATTRS.second}),
+                     END) AS #{MONTHLY_REGISTERED_PATIENTS_ATTRS.second}),
                %Q(sum(CASE
                          WHEN patients.device_created_at >= date_trunc('MONTH', now())::DATE
                          AND patients.device_created_at  < date_trunc('MONTH', now() + interval '1 month')::DATE then 1
                          ELSE 0
-                     END) AS #{REGISTERED_LAST_3_MONTHS_ATTRS.third}))
+                     END) AS #{MONTHLY_REGISTERED_PATIENTS_ATTRS.third}))
 
-    group_by_facility(@registered_patients, REGISTERED_LAST_3_MONTHS_ATTRS)
+    group_by_facility(@registered_patients_by_month, MONTHLY_REGISTERED_PATIENTS_ATTRS)
   end
 
-  def follow_up_patients
-    @follow_up_patients ||=
+  def follow_up_patients_by_month
+    @follow_up_patients_by_month ||=
       BloodPressure
         .joins('LEFT OUTER JOIN users ON blood_pressures.user_id = users.id')
         .joins('LEFT OUTER JOIN patients ON blood_pressures.patient_id = patients.id')
@@ -75,21 +73,21 @@ class DistrictAnalyticsQuery
                          AND blood_pressures.device_created_at >= date_trunc('MONTH', now() - interval '2 months')::DATE
                          AND blood_pressures.device_created_at <  date_trunc('MONTH', now() - interval '1 month')::DATE THEN 1
                          ELSE 0
-                     END) AS #{FOLLOW_UP_LAST_3_MONTHS_ATTRS.first}),
+                     END) AS #{MONTHLY_FOLLOW_UP_PATIENTS_ATTRS.first}),
                %Q(sum(CASE
                          WHEN patients.device_created_at < date_trunc('MONTH', now() - interval '1 months')::DATE
                          AND blood_pressures.device_created_at >= date_trunc('MONTH', now() - interval '1 months')::DATE
                          AND blood_pressures.device_created_at <  date_trunc('MONTH', now())::DATE THEN 1
                          ELSE 0
-                     END) AS #{FOLLOW_UP_LAST_3_MONTHS_ATTRS.second}),
+                     END) AS #{MONTHLY_FOLLOW_UP_PATIENTS_ATTRS.second}),
                %Q(sum(CASE
                          WHEN patients.device_created_at < date_trunc('MONTH', now())::DATE
                          AND blood_pressures.device_created_at >= date_trunc('MONTH', now())::DATE
                          AND blood_pressures.device_created_at <  date_trunc('MONTH', now() - interval '1 month')::DATE THEN 1
                          ELSE 0
-                     END) AS #{FOLLOW_UP_LAST_3_MONTHS_ATTRS.third}))
+                     END) AS #{MONTHLY_FOLLOW_UP_PATIENTS_ATTRS.third}))
 
-    group_by_facility(@follow_up_patients, FOLLOW_UP_LAST_3_MONTHS_ATTRS)
+    group_by_facility(@follow_up_patients_by_month, MONTHLY_FOLLOW_UP_PATIENTS_ATTRS)
   end
 
   def group_by_facility(query_results, attrs)
