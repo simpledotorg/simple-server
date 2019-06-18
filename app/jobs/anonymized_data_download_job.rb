6068c396786b7ce0d1ef014ca72d319121ebc2d5
Timmy Jose <zoltan.jose@gmail.com>
2019-06-18 17:48:01 +0530

    Increasing retry count and retry delay

--- app/jobs/anonymized_data_download_job.rb
+++ app/jobs/anonymized_data_download_job.rb
@@ -2,10 +2,10 @@ class AnonymizedDataDownloadJob < ApplicationJob
   queue_as :anonymized_data_download
   self.queue_adapter = :sidekiq
 
-  DEFAULT_RETRY_TIMES = 2
-  DEFAULT_RETRY_SECONDS = 5.minutes.seconds.to_i
+  DEFAULT_RETRY_TIMES = 3
+  DEFAULT_RETRY_SECONDS = 10.minutes.seconds.to_i
 
   def perform(recipient_name, recipient_email, entity_map, entity_type)
-    AnonymizedDataDownloadService.new(recipient_name, recipient_email, recipient_role, entity_map, entity_type).execute
+    AnonymizedDataDownloadService.new(recipient_name, recipient_email, entity_map, entity_type).execute
   end
 end
