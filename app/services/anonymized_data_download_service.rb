c5c46a4c16ec16bb1f2279d3c3050ee7880e01ef
Timmy Jose <zoltan.jose@gmail.com>
2019-06-19 16:37:57 +0530

    Fixing slow queries, adding new phone_calls model

--- app/services/anonymized_data_download_service.rb
+++ app/services/anonymized_data_download_service.rb
@@ -61,18 +61,21 @@ class AnonymizedDataDownloadService
     csv_data[AnonymizedDataConstants::APPOINTMENTS_FILE] = appointments_csv_file
 
     overdue_appointments = Appointment.overdue.select do |overdue_app|
-      patient_ids.include?(overdue_app.id)
+      patient_ids.include?(overdue_app.patient_id)
     end
 
     overdue_appointments_csv_file = CSVGeneration::overdue_csv(overdue_appointments)
     csv_data[AnonymizedDataConstants::OVERDUES_FILE] = overdue_appointments_csv_file
 
-    communications_for_facility = appointments.flat_map(&:communications)
-    communications_csv_file = CSVGeneration::communications_csv(communications_for_facility)
-    csv_data[AnonymizedDataConstants::COMMUNICATIONS_FILE] = communications_csv_file
+    communications = appointments.flat_map(&:communications)
+    sms_reminders_file = CSVGeneration::sms_reminders(communications)
+    csv_data[AnonymizedDataConstants::SMS_REMINDERS_FILE] = sms_reminders_file
+
+    phone_calls = appointments.flat_map(&:communications)
+    phone_calls_file = CSVGeneration::phone_calls(phone_calls)
+    csv_data[AnonymizedDataConstants::PHONE_CALLS_FILE] = phone_calls_file
 
     csv_data
-
   end
 
   module CSVGeneration
@@ -108,7 +111,6 @@ class AnonymizedDataDownloadService
         csv << AnonymizedDataConstants.bp_headers.map(&:titleize)
 
         blood_pressures.each do |bp|
-          user_id = User.where(id: bp.user_id).first
           facility_name = Facility.where(id: bp.facility_id).first&.name
 
           csv << [
@@ -117,7 +119,7 @@ class AnonymizedDataDownloadService
             bp.created_at,
             bp.recorded_at,
             original_else_blank_value(facility_name),
-            hashed_else_blank_value(user_id),
+            hashed_else_blank_value(bp.user.id),
             bp.systolic,
             bp.diastolic
           ]
@@ -130,12 +132,13 @@ class AnonymizedDataDownloadService
         csv << AnonymizedDataConstants.medicines_headers.map(&:titleize)
 
         medicines.each do |med|
-          user_id = AuditLog.where(auditable_id: med.id).first
+          user_id = med.patient.registration_user_id
           facility_name = Facility.where(id: med.facility_id).first&.name
 
           csv << [
             hash_uuid(med.id),
             hash_uuid(med.patient_id),
+            med.created_at,
             original_else_blank_value(facility_name),
             hashed_else_blank_value(user_id),
             med.name,
@@ -150,7 +153,7 @@ class AnonymizedDataDownloadService
         csv << AnonymizedDataConstants.appointment_headers.map(&:titleize)
 
         appointments.each do |app|
-          user_id = AuditLog.where(auditable_id: app.id).first
+          user_id = app.patient.registration_user_id
           facility_name = Facility.where(id: app.facility_id).first&.name
 
           csv << [
@@ -170,7 +173,7 @@ class AnonymizedDataDownloadService
         csv << AnonymizedDataConstants::overdue_headers.map(&:titleize)
 
         overdue_appointments.each do |overdue_appointment|
-          user_id = AuditLog.where(auditable_id: overdue_appointment.id).first
+          user_id = overdue_appointment.patient.registration_user_id
           facility_name = Facility.where(id: overdue_appointment.facility_id).first&.name
           agreed_to_visit = overdue_appointment.agreed_to_visit
           remind_on = overdue_appointment.remind_on
@@ -191,13 +194,13 @@ class AnonymizedDataDownloadService
       end
     end
 
-    def self.communications_csv(communications)
+    def self.sms_reminders(communications)
       CSV.generate(headers: true) do |csv|
-        csv << AnonymizedDataConstants::communications_headers.map(&:titleize)
+        csv << AnonymizedDataConstants::sms_reminders_headers.map(&:titleize)
 
         communications.each do |communication|
-          patient_id = Patient.where(id: Appointment.where(id: communication.appointment_id).first).first
-          communication_result = communication.detailable_type.constantize.where(id: communication.detailable_id).first&.result
+          patient_id = communication.appointment.patient.id
+          communication_result = communication.communication_result
 
           csv << [
             hash_uuid(communication.id),
@@ -207,6 +210,23 @@ class AnonymizedDataDownloadService
             communication.created_at,
             original_else_blank_value(communication.communication_type),
             original_else_blank_value(communication_result)
+          ]
+        end
+      end
+    end
+
+    def self.phone_calls(phone_calls)
+      CSV.generate(headers: true) do |csv|
+        csv << AnonymizedDataConstants::phone_calls_headers.map(&:titleize)
+
+        phone_calls.each do |call|
+          csv << [
+            hash_uuid(call.id),
+            call.created_at,
+            call.result,
+            call.duration,
+            call.start_time,
+            call.end_time
           ]
         end
       end
@@ -237,7 +257,8 @@ class AnonymizedDataDownloadService
     MEDICINES_FILE = 'medicines.csv'
     APPOINTMENTS_FILE = 'appointments.csv'
     OVERDUES_FILE = 'overdue_appointments.csv'
-    COMMUNICATIONS_FILE = 'communications.csv'
+    SMS_REMINDERS_FILE = 'sms_reminders.csv'
+    PHONE_CALLS_FILE = 'phone_calls.csv'
 
     def self.patient_headers
       %w[id created_at registration_date facility_name user_id age gender]
@@ -259,8 +280,12 @@ class AnonymizedDataDownloadService
       %w[id patient_id created_at facility_name user_id status agreed_to_visit remind_on cancel_reason]
     end
 
-    def self.communications_headers
+    def self.sms_reminders_headers
       %w[id appointment_id patient_id user_id created_at communication_type communication_result]
+    end
+
+    def self.phone_calls_headers
+      %w[id, created_at, result, duration, start_time, end_time]
     end
   end
 end
