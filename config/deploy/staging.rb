4549415a5b9b64e069bba6748476ac5170025230
Akshay Gupta <kitallis@gmail.com>
2019-06-19 12:24:03 +0530

    Update the staging public IP for cap

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,1 @@
-server "ec2-13-233-100-22.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
+server "ec2-13-235-77-192.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
