808ef0d0956d382c9b33d773344f1cc3db057dae
Timmy Jose <zoltan.jose@gmail.com>
2019-06-19 18:02:23 +0530

    Reverting to using default queue for job

--- app/jobs/anonymized_data_download_job.rb
+++ app/jobs/anonymized_data_download_job.rb
@@ -1,9 +1,9 @@
 class AnonymizedDataDownloadJob < ApplicationJob
   queue_as :anonymized_data_download
-  self.queue_adapter = :sidekiq
+  self.queue_adapter = :default
 
   DEFAULT_RETRY_TIMES = 3
-  DEFAULT_RETRY_SECONDS = 10.minutes.seconds.to_i
+  DEFAULT_RETRY_SECONDS = 5.minutes.seconds.to_i
 
   def perform(recipient_name, recipient_email, entity_map, entity_type)
     AnonymizedDataDownloadService.new(recipient_name, recipient_email, entity_map, entity_type).execute
