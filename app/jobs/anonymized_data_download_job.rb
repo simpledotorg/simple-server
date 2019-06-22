286aef10b0fca2f2d35459e401517a34ad8e31ce
Timmy Jose <zoltan.jose@gmail.com>
2019-06-22 17:02:06 +0530

    Adding custom sentry notifications for anonymized data download job

--- app/jobs/anonymized_data_download_job.rb
+++ app/jobs/anonymized_data_download_job.rb
@@ -6,22 +6,41 @@ class AnonymizedDataDownloadJob < ApplicationJob
   DEFAULT_RETRY_SECONDS = 5.minutes.seconds.to_i
 
   def perform(recipient_name, recipient_email, model_params_map, model_type)
-    begin
-      case model_type
-      when 'district' then
+    case model_type
+    when 'district' then
+      begin
         AnonymizedDataDownloadService.new.run_for_district(recipient_name,
                                                            recipient_email,
                                                            model_params_map[:district_name],
                                                            model_params_map[:organization_id])
-      when 'facility' then
+      rescue StandardError => e
+        error_message = "Error while downloading anonymized data for District #{model_params_map[:district_name]}"
+        report_error(e.message, error_message)
+      end
+    when 'facility' then
+      begin
         AnonymizedDataDownloadService.new.run_for_facility(recipient_name,
                                                            recipient_email,
                                                            model_params_map[:facility_id])
-      else
-        raise StandardError("Unknown model type #{model_type}")
+      rescue StandardError => e
+        facility name = Facility.where(id: model_params_map[:facility_id]).first
+        error_message = "Error while downloading anonymized data for District #{facility_name}"
+        report_error(e.message, error_message)
       end
-    rescue StandardError => e
-      puts "Caught error: #{e.inspect}: #{e.backtrace}"
+    else
+      raise StandardError("Error while downloading anonymized data: unknown model type #{model_type}")
     end
+  end
+
+  private
+
+  def report_error(error_message, error_string)
+    Raven.capture_message(error_string,
+                          logger: 'logger',
+                          extra: {
+                            errors: error_message
+                          },
+                          tags: { type: 'anonymized-data-download' })
+
   end
 end
