b18ecad2d6af49a414de2b51603f07916abf9f60
Tim Cheadle <tcheadle@resolvetosavelives.org>
2019-07-01 15:11:13 -0400

    Style fixes

--- app/models/organization_district.rb
+++ app/models/organization_district.rb
@@ -25,8 +25,10 @@ class OrganizationDistrict < Struct.new(:district_name, :organization)
   def dashboard_analytics
     query = DistrictAnalyticsQuery.new(district_name)
 
-    [query.follow_up_patients_by_month,
-     query.registered_patients_by_month,
-     query.total_registered_patients].compact.inject(&:deep_merge)
+    [
+      query.follow_up_patients_by_month,
+      query.registered_patients_by_month,
+      query.total_registered_patients
+    ].compact.inject(&:deep_merge)
   end
 end
