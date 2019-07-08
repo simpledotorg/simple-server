6f9ef4daf48cd016d22f73f7739bcc7183adb776
Daniel Burka <danielburka@gmail.com>
2019-07-08 14:51:06 -0400

    Fixed date formats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,14 +21,12 @@ module ApplicationHelper
       "Today"
     elsif date == Date.yesterday
       "Yesterday"
-    elsif date <= 1.year.ago
-      "on #{date.strftime("%d/%m/%Y")}".html_safe
     else
-      "#{time_ago_in_words(date)} ago".html_safe
+      "on #{date.strftime("%d-%^b-%Y")}".html_safe
     end
   end
 
-  def handle_impossible_registration_date(date, format: "%d-%b-%Y")
+  def handle_impossible_registration_date(date, format: "%d-%^b-%Y")
     program_inception_date = ENV['PROGRAM_INCEPTION_DATE'] ? ENV['PROGRAM_INCEPTION_DATE'].to_time : DEFAULT_PROGRAM_INCEPTION_DATE
     if date < program_inception_date # Date of inception of program
       'Unclear'
@@ -41,7 +39,7 @@ module ApplicationHelper
     return if patient.appointments.count < 2
 
     last_interaction = patient.appointments.order(scheduled_date: :desc).second
-    last_interaction_date = last_interaction.created_at.strftime("%d-%b-%Y")
+    last_interaction_date = last_interaction.created_at.strftime("%d-%^b-%Y")
     interaction_result = "" + last_interaction_date
 
     if last_interaction.agreed_to_visit.present?
@@ -49,7 +47,7 @@ module ApplicationHelper
     elsif last_interaction.remind_on.present?
       interaction_result += ' - Remind to call later'
     elsif last_interaction.status_visited?
-      interaction_result += " - Visited"
+      interaction_result += " - Visited facility"
     end
 
     interaction_result
