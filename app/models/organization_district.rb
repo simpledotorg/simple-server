a947e6e962f4b1d0476352ffe68ff17b1f16519c
Tim Cheadle <tcheadle@resolvetosavelives.org>
2019-07-12 16:24:54 -0400

    Fixed merge mistake

--- app/models/organization_district.rb
+++ app/models/organization_district.rb
@@ -5,21 +5,8 @@ class OrganizationDistrict < Struct.new(:district_name, :organization)
     district_name.split(" ").select(&:present?).join("-").downcase
   end
 
-  def cohort_analytics
-    patients =
-      Patient
-        .joins(:registration_facility)
-        .where(facilities: { district: district_name })
-
-    query = CohortAnalyticsQuery.new(patients)
-    results = {}
-
-    (0..2).each do |quarters_back|
-      date = (Date.today - (quarters_back * 3).months).beginning_of_quarter
-      results[date] = query.patient_counts(year: date.year, quarter: quarter(date))
-    end
-
-    results
+  def facilities
+    organization.facilities.where(district: district_name)
   end
 
   def cohort_analytics
