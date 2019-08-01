a2cb2ccf9c7417551295650134eddbd15e1a1938
prerana <prerana.testvagrant@embibe.com>
2019-08-01 12:36:20 +0530

    Refactored Forgot password and organizations spec file

--- spec/features/admin/organizations_spec.rb
+++ spec/features/admin/organizations_spec.rb
@@ -4,8 +4,11 @@ RSpec.feature 'Organization management', type: :feature do
     let!(:owner) { create(:admin, :owner) }
     let!(:ihmi) { create(:organization, name: "IHMI") }
     let!(:path) { create(:organization, name: "PATH") }
-    login = LoginPage.new
-    homepage = HomePage.new
+
+    login = AdminPageSignIn.new
+    dashboard_navigation = DashboardPageNavigation.new
+    organization_page = AdminOrganizationsPage.new
+
 
     describe "test organization screen" do
 
@@ -13,11 +16,11 @@ RSpec.feature 'Organization management', type: :feature do
         visit root_path
         login.do_login(owner.email, owner.password)
 
-        homepage.select_main_menu_tab("Manage")
+        dashboard_navigation.select_main_menu_tab("Manage")
         expect(page).to have_content("Organizations")
         expect(page).to have_content("Protocols")
 
-        homepage.select_manage_option('Organizations')
+        dashboard_navigation.select_manage_option('Organizations')
         expect(page).to have_content("IHMI")
         expect(page).to have_content("PATH")
       end
@@ -26,11 +29,11 @@ RSpec.feature 'Organization management', type: :feature do
         visit root_path
         login.do_login(owner.email, owner.password)
 
-        homepage.select_manage_option("Organizations")
-        organization = OrganizationsPage.new
-        organization.create_new_organization("test", "testDescription")
+        dashboard_navigation.select_manage_option("Organizations")
+        organization_page.click_on_add_organization_button
 
-        organization.delete_organization("test")
+        AdminOrganizationsPageNew.new.create_new_organization("test", "testDescription")
+        organization_page.delete_organization("test")
       end
     end
   end
