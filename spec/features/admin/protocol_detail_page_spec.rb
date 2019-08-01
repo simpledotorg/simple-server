a2d98f00b8187b34d5e114d5df0125e55254c123
prerana <prerana.testvagrant@embibe.com>
2019-08-01 12:36:59 +0530

    Refactored Protocol spec file

--- spec/features/admin/protocol_detail_page_spec.rb
+++ spec/features/admin/protocol_detail_page_spec.rb
@@ -2,42 +2,44 @@ require 'rails_helper'
 
 RSpec.feature 'test protocol detail page functionality', type: :feature do
     let(:owner) { create(:admin) }
+    let!(:var_protocol) { create(:protocol, name: "PunjabTestProtocol", follow_up_days: "20") }
+    let!(:var_protocol_drug) {create(:protocol_drug, name: "test_Drug_01", dosage: "10mg", rxnorm_code: "code",protocol: var_protocol)}
 
-    login_page = LoginPage.new
-    protocol_page = ProtocolLandingPage.new
-    protocol_form = ProtocolFormPage.new
+    protocol=AdminProtocolPage.new
+    protocol_update = AdminProtocolPageEdit.new
     protocol_detail = ProtocolDetailPage.new
-    protocol_drug = ProtocolDrugPage.new
+    new_drug = ProtocolDrugsPageNew.new
 
     before(:each) do
       visit root_path
-      login_page.do_login(owner.email, owner.password)
+      sign_in(owner)
       visit admin_protocols_path
-      protocol_page.click_add_new_protocol
-      protocol_form.create_new_protocol("testProtocol", "40")
     end
 
     context "protocol detail page" do
-      it " edit prototcol" do
+      it " edit protocol" do
+        protocol.select_protocol(var_protocol.name)
+
         protocol_detail.click_edit_protocol_button
-        protocol_form.update_protocol_followup_days("10")
+        protocol_update.update_protocol_followup_days("10")
         #assertion
         protocol_detail.verify_successful_message("Protocol was successfully updated.")
         protocol_detail.verify_updated_followup_days("10")
         protocol_detail.click_message_cross_button
       end
       it 'should create new protocol drug ' do
+        protocol.select_protocol(var_protocol.name)
+
         protocol_detail.click_new_protocol_drug_button
-        protocol_drug.add_new_protocol_drug("test_drug", "10mg", "AXDSC")
+        new_drug.add_new_protocol_drug("test_drug", "10mg", "AXDSC")
         #assertion
         protocol_detail.verify_successful_message("Protocol drug was successfully created.")
         protocol_detail.verify_protocol_drug_name_list("test_drug")
       end
       it 'should edit protocol drug' do
-        protocol_detail.click_new_protocol_drug_button
-        protocol_drug.add_new_protocol_drug("test_drug", "10mg", "AXDSC")
-        protocol_detail.click_edit_protocol_drug_button("10mg test_drug")
-        protocol_drug.edit_protocol_drug_info("50mg", "AXDFC")
+        protocol.select_protocol(var_protocol.name)
+        protocol_detail.click_edit_protocol_drug_button("10mg test_Drug_01")
+        ProtocolDrugPageEdit.new.edit_protocol_drug_info("50mg", "AXDFC")
       end
     end
   end
--- spec/features/admin/protocol_landing_page_spec.rb
+++ spec/features/admin/protocol_landing_page_spec.rb
@@ -4,29 +4,32 @@ RSpec.feature 'test protocol screen functionality', type: :feature do
   let(:owner) {create(:admin)}
   let!(:var_protocol) {create(:protocol, name: "PunjabTestProtocol", follow_up_days: "20")}
 
-  protocol_page = ProtocolLandingPage.new
-  protocol_form = ProtocolFormPage.new
-  protocol_detail = ProtocolDetailPage.new
+  protocol_page = AdminProtocolPage.new
+  protocol_new = AdminProtocolPageNew.new
+  protocol_detail_page = ProtocolDetailPage.new
 
   before(:each) do
     visit root_path
-    LoginPage.new.do_login(owner.email, owner.password)
+    sign_in(owner)
     visit admin_protocols_path
   end
 
   context "protocol landing page" do
     it 'add new  protocol' do
       protocol_page.click_add_new_protocol
-      protocol_form.create_new_protocol("testProtocol", "40")
-      protocol_detail.verify_successful_message("Protocol was successfully created.")
-      protocol_detail.click_message_cross_button
+      protocol_new.create_new_protocol("testProtocol", "40")
+
+      protocol_detail_page.verify_successful_message("Protocol was successfully created.")
+      protocol_detail_page.click_message_cross_button
       #assertion
       expect(page).to have_content("testProtocol")
     end
+
     it 'edit protocol' do
       protocol_page.click_edit_protocol_link(var_protocol.name)
-      protocol_form.update_protocol_followup_days(40)
-      protocol_detail.verify_updated_followup_days("40")
+      AdminProtocolPageEdit.new.update_protocol_followup_days(40)
+      protocol_detail_page.verify_updated_followup_days("40")
+
       visit admin_protocols_path
 
       # assertion at landing page
