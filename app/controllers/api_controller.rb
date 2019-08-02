e70705fc8bac65f18394c92d4f835a2a9efced09
Timmy Jose <zoltan.jose@gmail.com>
2019-08-02 17:58:06 +0530

    Return 403 for users not allowed to sync
    Co-authored-by: Govind Krishna Joshi <govind@nilenso.com>

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,9 @@
 class APIController < ApplicationController
-  before_action :authenticate, :validate_facility, :validate_current_facility_belongs_to_users_facility_group
+  before_action :current_user_present?,
+                :validate_sync_approval_status_allowed,
+                :authenticate,
+                :validate_facility,
+                :validate_current_facility_belongs_to_users_facility_group
 
   TIME_WITHOUT_TIMEZONE_FORMAT = '%FT%T.%3NZ'.freeze
 
@@ -12,6 +16,8 @@ class APIController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound do
     head :not_found
   end
+
+  private
 
   def current_user
     @current_user ||= User.find_by(id: request.headers['HTTP_X_USER_ID'])
@@ -33,13 +39,17 @@ class APIController < ApplicationController
     return head :unauthorized unless current_user.present? && current_facility_group.facilities.where(id: current_facility.id).present?
   end
 
-  def authenticate
-    return head :unauthorized unless authenticated?
-    current_user.mark_as_logged_in if current_user.has_never_logged_in?
+  def current_user_present?
+    return head :unauthorized unless current_user.present?
   end
 
-  def authenticated?
-    current_user.present? && current_user.access_token_valid? && access_token_authorized?
+  def validate_sync_approval_status_allowed
+    head :forbidden unless current_user.sync_approval_status_allowed?
+  end
+
+  def authenticate
+    return head :unauthorized unless access_token_authorized?
+    current_user.mark_as_logged_in if current_user.has_never_logged_in?
   end
 
   def access_token_authorized?
--- spec/api/v2/analytics_spec.rb
+++ spec/api/v2/analytics_spec.rb
@@ -27,6 +27,8 @@ describe 'Analytics V2 API', swagger_doc: 'v2/swagger.json' do
 
         run_test!
       end
+
+      include_examples 'returns 403 for get requests for forbidden users'
     end
   end
 
@@ -56,6 +58,8 @@ describe 'Analytics V2 API', swagger_doc: 'v2/swagger.json' do
 
         run_test!
       end
+
+      include_examples 'returns 403 for get requests for forbidden users'
     end
   end
 end
