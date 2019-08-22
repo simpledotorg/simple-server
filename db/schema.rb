a9481086c63c20432512e8bd4b289e8ed882dacd
Govind Krishna Joshi <govind@nilenso.com>
2019-08-22 15:13:22 +0530

    Add organization id to master users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190814081621) do
+ActiveRecord::Schema.define(version: 20190819191312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -405,6 +405,17 @@ ActiveRecord::Schema.define(version: 20190814081621) do
     t.datetime "deleted_at"
     t.index ["user_id", "authenticatable_type", "authenticatable_id"], name: "user_authentications_master_users_authenticatable_uniq_index", unique: true
     t.index ["user_id"], name: "index_user_authentications_on_user_id"
+  end
+
+  create_table "user_permissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "permission_slug"
+    t.string "resource_type"
+    t.uuid "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["resource_type", "resource_id"], name: "index_user_permissions_on_resource_type_and_resource_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
