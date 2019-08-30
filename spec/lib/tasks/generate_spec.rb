16a88a8fc74e8008b2d44efe3f6e94b2e2f83ca1
Timmy Jose <zoltan.jose@gmail.com>
2019-08-30 16:53:10 +0530

    Customising name generation for Faker, refactoring code.
    Co-authored-by: Vikram <mail@vkrm.is>

--- spec/lib/tasks/generate_spec.rb
+++ spec/lib/tasks/generate_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe 'generate:seed:generate_data' do
 
   let!(:env) { ENV.fetch('SIMPLE_SERVER_ENV') }
   let! (:seed_config) { YAML.load_file('config/seed.yml').fetch(env) }
-  let!(:overdue) { seed_config.dig('patients', 'traits').include?('overdue') }
-  let!(:hypertensive) { seed_config.dig('patients', 'traits').include?('hypertensive') }
   let!(:user) { create(:user, registration_facility: create(:facility, facility_group: create(:facility_group, organization: create(:organization)))) }
   let!(:patient_factor) { seed_config.dig('patients', 'count') }
 
@@ -24,7 +22,7 @@ RSpec.describe 'generate:seed:generate_data' do
   it 'generates the correct number of appointments' do
     appointment_factor = seed_config.dig('patients', 'appointments')
 
-    expect { invoke_task('generate:seed:generate_data[1]') }.to change { Appointment.count }.by(2 * patient_factor * appointment_factor )
+    expect { invoke_task('generate:seed:generate_data[1]') }.to change { Appointment.count }.by(2 * patient_factor * appointment_factor)
   end
 
   it 'generates the correct number of medical histories' do
@@ -39,11 +37,19 @@ RSpec.describe 'generate:seed:generate_data' do
     expect { invoke_task('generate:seed:generate_data[1]') }.to change { PrescriptionDrug.count }.by(patient_factor * prescription_drugs_factor)
   end
 
+  it 'generates the correct number of call logs' do
+    expect { invoke_task('generate:seed:generate_data[1]') }.to change { CallLog.count }.by(patient_factor)
+  end
+
+  it 'generates the correct number of exotel phone number details' do
+    expect { invoke_task('generate:seed:generate_data[1]') }.to change { ExotelPhoneNumberDetail.count }.by(patient_factor)
+  end
+
   context 'overdue', if: :overdue do
     it 'has overdue appointments' do
       appointment_factor = seed_config.dig('patients', 'appointments')
 
-      expect { invoke_task('generate:seed:generate_data[1]') }.to change { Appointment.overdue.count }.by(patient_factor * appointment_factor )
+      expect { invoke_task('generate:seed:generate_data[1]') }.to change { Appointment.overdue.count }.by(patient_factor * appointment_factor)
     end
   end
 
