1be295d859407dcc482b5ad1c9af7b8ee0f4a5a4
Govind Krishna Joshi <govind@nilenso.com>
2019-09-04 10:59:28 +0530

    Allow owners to download whatsapp graphics as well

--- app/policies/organization_district_policy.rb
+++ app/policies/organization_district_policy.rb
@@ -13,6 +13,6 @@ class OrganizationDistrictPolicy < ApplicationPolicy
   end
 
   def whatsapp_graphics?
-    user.has_role?(:organization_owner, :supervisor) && user.organizations.include?(record.organization)
+    user.owner? || (user.has_role?(:organization_owner, :supervisor) && user.organizations.include?(record.organization))
   end
 end
