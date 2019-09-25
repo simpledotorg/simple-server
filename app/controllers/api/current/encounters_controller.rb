4dd027c8932ca6720134d7fe14b375e1a08ddafc
Akshay Gupta <kitallis@gmail.com>
2019-09-25 18:26:00 +0530

    Add timezone_offset to the MergeEncounterService params

--- app/controllers/api/current/encounters_controller.rb
+++ app/controllers/api/current/encounters_controller.rb
@@ -17,7 +17,8 @@ class Api::Current::EncountersController < Api::Current::SyncController
       { errors_hash: validator.errors_hash }
     else
       transformed_params = Api::Current::EncounterTransformer.from_nested_request(encounter_params)
-      { record: MergeEncounterService.new(transformed_params, current_facility).merge[:encounter] }
+      { record: MergeEncounterService.new(transformed_params.merge(timezone_offset: 3600),
+                                          current_facility).merge[:encounter] }
     end
   end
 
