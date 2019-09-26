2b28cfc04466e50ad86a4ba5db25bee146bb3beb
Tim Cheadle <tcheadle@resolvetosavelives.org>
2019-09-26 15:01:59 -0400

    Add utility method for BP passport shortcodes

--- app/models/patient_business_identifier.rb
+++ app/models/patient_business_identifier.rb
@@ -12,4 +12,13 @@ class PatientBusinessIdentifier < ApplicationRecord
 
   validates :device_created_at, presence: true
   validates :device_updated_at, presence: true
+
+  def shortcode
+    case identifier_type
+    when "simple_bp_passport"
+      identifier.split(/[^\d]/).join[0..6].insert(3, '-')
+    else
+      identifier
+    end
+  end
 end
