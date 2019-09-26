e0a75b2e8ce5e12bdf99408f9a62dadc468813ce
Tim Cheadle <tcheadle@resolvetosavelives.org>
2019-09-25 20:21:54 -0400

    Added fixes to allow for missing appointments

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -78,7 +78,7 @@ class Patient < ApplicationRecord
   end
 
   def risk_priority
-    return RISK_PRIORITIES[:NONE] if latest_scheduled_appointment.overdue_for_under_a_month?
+    return RISK_PRIORITIES[:NONE] if latest_scheduled_appointment&.overdue_for_under_a_month?
 
     if latest_blood_pressure&.critical?
       RISK_PRIORITIES[:HIGHEST]
@@ -157,10 +157,14 @@ class Patient < ApplicationRecord
     }
   end
 
+  def latest_bp_passport
+    business_identifiers.where(identifier_type: "simple_bp_passport").order(:device_created_at).last
+  end
+
   private
 
   def low_priority?
-    latest_scheduled_appointment.overdue_for_over_a_year? &&
+    latest_scheduled_appointment&.overdue_for_over_a_year? &&
       latest_blood_pressure&.under_control?
   end
 end
