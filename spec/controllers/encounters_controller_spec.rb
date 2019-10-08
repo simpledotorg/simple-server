1cb8d5872fb0a0133b2ce3f1c6a22598d239aece
Akshay Gupta <kitallis@gmail.com>
2019-10-08 19:42:12 +0530

    Remove FactoryBot namespace from Encounter spec (it’s included already)

--- spec/controllers/encounters_controller_spec.rb
+++ spec/controllers/encounters_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::Current::EncountersController, type: :controller do
-  let(:request_user) { FactoryBot.create(:user) }
-  let(:request_facility) { FactoryBot.create(:facility, facility_group: request_user.facility.facility_group) }
-  let(:request_patient) { FactoryBot.create(:patient, registration_facility: request_facility) }
+  let(:request_user) { create(:user) }
+  let(:request_facility) { create(:facility, facility_group: request_user.facility.facility_group) }
+  let(:request_patient) { create(:patient, registration_facility: request_facility) }
 
   let(:model) { Encounter }
 
@@ -14,22 +14,22 @@ RSpec.describe Api::Current::EncountersController, type: :controller do
   let(:number_of_schema_errors_in_invalid_payload) { 3 }
 
   def build_record(_options = {})
-    facility = FactoryBot.create(:facility, facility_group: request_user.facility.facility_group)
-    patient = FactoryBot.create(:patient, registration_facility: facility)
-    blood_pressure = FactoryBot.create(:blood_pressure, facility: facility, patient: patient)
-    encounter = FactoryBot.build(:encounter, patient: patient)
-    observation = FactoryBot.build(:observation, encounter: encounter, observable: blood_pressure, user: request_user)
+    facility = create(:facility, facility_group: request_user.facility.facility_group)
+    patient = create(:patient, registration_facility: facility)
+    blood_pressure = create(:blood_pressure, facility: facility, patient: patient)
+    encounter = build(:encounter, patient: patient)
+    observation = build(:observation, encounter: encounter, observable: blood_pressure, user: request_user)
     encounter.observations = [observation]
     encounter.blood_pressures = [observation.observable]
     encounter
   end
 
   def create_record(options = {})
-    facility = FactoryBot.create(:facility, facility_group: request_user.facility.facility_group)
-    patient = FactoryBot.create(:patient, registration_facility: facility)
-    blood_pressure = FactoryBot.create(:blood_pressure, facility: facility, patient: patient)
-    encounter = FactoryBot.create(:encounter, options.merge(patient: patient))
-    FactoryBot.create(:observation, encounter: encounter, observable: blood_pressure, user: request_user)
+    facility = create(:facility, facility_group: request_user.facility.facility_group)
+    patient = create(:patient, registration_facility: facility)
+    blood_pressure = create(:blood_pressure, facility: facility, patient: patient)
+    encounter = create(:encounter, options.merge(patient: patient))
+    create(:observation, encounter: encounter, observable: blood_pressure, user: request_user)
     encounter
   end
 
@@ -77,10 +77,10 @@ RSpec.describe Api::Current::EncountersController, type: :controller do
           }
         }.with_indifferent_access
 
-        encounter_with_empty_observations = build_encounters_payload(build_record).merge(empty_observations)
+        encounter_with_no_observations = build_encounters_payload(build_record).merge(empty_observations)
 
         expect {
-          post(:sync_from_user, params: { encounters: [encounter_with_empty_observations] }, as: :json)
+          post(:sync_from_user, params: { encounters: [encounter_with_no_observations] }, as: :json)
         }.to change { Encounter.count }.by(1)
                .and change { Observation.count }.by(0)
 
@@ -118,12 +118,26 @@ RSpec.describe Api::Current::EncountersController, type: :controller do
     describe 'current facility prioritisation' do
       it "syncs request facility's records first" do
         request_2_facility = create(:facility, facility_group: request_user.facility.facility_group)
+
         patient_1 = create(:patient, registration_facility: request_facility)
         patient_2 = create(:patient, registration_facility: request_2_facility)
-        create_list(:encounter, 2, patient: patient_1, facility: request_facility, updated_at: 3.minutes.ago)
-        create_list(:encounter, 2, patient: patient_1, facility: request_facility, updated_at: 5.minutes.ago)
-        create_list(:encounter, 2, patient: patient_2, facility: request_2_facility, updated_at: 7.minutes.ago)
-        create_list(:encounter, 2, patient: patient_2, facility: request_2_facility, updated_at: 10.minutes.ago)
+
+        create_list(:encounter, 2,
+                    patient: patient_1,
+                    facility: request_facility,
+                    updated_at: 3.minutes.ago)
+        create_list(:encounter, 2,
+                    patient: patient_1,
+                    facility: request_facility,
+                    updated_at: 5.minutes.ago)
+        create_list(:encounter, 2,
+                    patient: patient_2,
+                    facility: request_2_facility,
+                    updated_at: 7.minutes.ago)
+        create_list(:encounter, 2,
+                    patient: patient_2,
+                    facility: request_2_facility,
+                    updated_at: 10.minutes.ago)
 
         # GET request 1
         set_authentication_headers
@@ -154,9 +168,20 @@ RSpec.describe Api::Current::EncountersController, type: :controller do
 
       before :each do
         set_authentication_headers
-        create_list(:encounter, 2, patient: patient_in_different_facility, facility: facility_in_another_group, updated_at: 3.minutes.ago)
-        create_list(:encounter, 3, patient: patient_in_same_facility, facility: facility_in_same_group, updated_at: 5.minutes.ago)
-        create_list(:encounter, 1, patient: patient_in_same_facility, facility: request_facility, updated_at: 7.minutes.ago)
+        create_list(:encounter, 2,
+                    patient: patient_in_different_facility,
+                    facility: facility_in_another_group,
+                    updated_at: 3.minutes.ago)
+
+        create_list(:encounter, 3,
+                    patient: patient_in_same_facility,
+                    facility: facility_in_same_group,
+                    updated_at: 5.minutes.ago)
+
+        create_list(:encounter, 1,
+                    patient: patient_in_same_facility,
+                    facility: request_facility,
+                    updated_at: 7.minutes.ago)
       end
 
       it "only sends data for facilities belonging in the sync group of user's registration facility" do
