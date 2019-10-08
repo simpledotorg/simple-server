c1d37f32d43e5022329f7476f56cebefb0c95b01
Akshay Gupta <kitallis@gmail.com>
2019-10-08 18:40:27 +0530

    Make the patient reference on Encounter optional

--- db/migrate/20191003064555_create_encounters.rb
+++ db/migrate/20191003064555_create_encounters.rb
@@ -2,6 +2,7 @@ class CreateEncounters < ActiveRecord::Migration[5.1]
   def change
     create_table :encounters, id: :uuid do |t|
       t.references :facility, type: :uuid, null: false, foreign_key: true
+      t.uuid :patient_id, null: false
       t.references :patient, type: :uuid, null: false, foreign_key: true
       t.date :encountered_on, null: false
       t.integer :timezone_offset, null: false
