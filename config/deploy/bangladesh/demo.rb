dc54aa0c5c91489913d7e9c7dd39d8595a8189fa
vkrm <mail@vkrm.is>
2019-10-18 13:38:19 +0530

    Temporarily removing BotUser initializer so assets:precompile will work without db migrations

--- config/deploy/bangladesh/demo.rb
+++ config/deploy/bangladesh/demo.rb
@@ -1,1 +1,1 @@
-server "ec2-35-154-70-183.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
+server "ec2-13-235-24-177.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
--- config/initializers/bot_user.rb
+++ config/initializers/bot_user.rb
@@ -1,28 +1,4 @@
 module BotUser
-  def find_bot_user(id)
-    User.where(id: id).first
-  end
-
-  def find_or_create_bot_user(id, name)
-    bot_user = find_bot_user(id)
-    return bot_user if bot_user.present?
-    current_time = Time.now
-    User.find_or_initialize_by(
-        id: id,
-        full_name: name,
-        sync_approval_status: User.sync_approval_statuses[:denied],
-        sync_approval_status_reason: "Bot user doesn't require sync",
-        device_created_at: current_time,
-        device_updated_at: current_time).save(validate: false)
-
-    find_bot_user(id)
-  end
 end
 
-ActiveSupport.on_load(:after_initialize, :yield => true) do
-  include BotUser
-  if !Rails.env.test? && !(defined?(is_running_migration?) && is_running_migration?)
-    SMS_REMINDER_BOT_USER = find_or_create_bot_user(ENV.fetch('APPOINTMENT_NOTIFICATION_BOT_USER_UUID'),
-                                                    ENV.fetch('APPOINTMENT_NOTIFICATION_BOT_USER_NAME'))
-  end
-end
+ActiveSupport.on_load(:after_initialize, :yield => true) { SMS_REMINDER_BOT_USER = nil }
