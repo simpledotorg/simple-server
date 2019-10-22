b330a5525cb040414e3b3ed569d862e7ecf92773
vkrm <mail@vkrm.is>
2019-10-22 14:27:22 +0530

    Add short months to locales

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -1,6 +1,6 @@
 module SmsHelper
   def date_in_locale(date, locale)
-    month_in_locale = I18n.t("months.#{date.month}", locale: locale)
+    month_in_locale = I18n.t("months.full.#{date.month}", locale: locale)
     "#{date.day} #{month_in_locale}, #{date.year}"
   end
 end
