9b99f3957ccdf664735159d93463a0c92f318565
vkrmis <48791115+vkrmis@users.noreply.github.com>
2019-10-29 16:37:49 +0530

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 server "ec2-52-66-250-216.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db cron whitelist_phone_numbers)
 server "ec2-13-126-205-193.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db)
 server "ec2-13-126-110-54.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(sidekiq)
-
