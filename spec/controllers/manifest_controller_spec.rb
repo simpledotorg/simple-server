7b7cc3606848b26674299f13ba62ab796aa4fc23
Akshay Gupta <kitallis@gmail.com>
2019-10-31 17:38:40 +0530

    Fix the manifest controller spec

--- spec/controllers/manifest_controller_spec.rb
+++ spec/controllers/manifest_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ManifestsController, type: :controller do
   describe 'GET #show' do
     ['production', 'sandbox', 'demo', 'qa', 'development'].each do |env|
       it "for #{env}" do
-        expect(ENV).to receive([]).with('SIMPLE_SERVER_ENV').and_return(env)
+        expect(ENV).to receive(:[]).with('SIMPLE_SERVER_ENV').and_return(env)
         get :show
         expect(response.body).to eq(File.read("public/manifest/#{env}.json"))
       end
