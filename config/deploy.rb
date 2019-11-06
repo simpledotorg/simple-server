e33214665815932cda77f6812085ac6bcf1b6e75
Akshay Gupta <kitallis@gmail.com>
2019-11-06 12:54:32 +0530

    Configure capistrano for multi-country deployments (#606)
    * Add capistrano-multiconfig to support multiple country deployment
    * Set an explicit rbenv PATH to workaround some multi-config weirdness
    * Add instructions on how to use the cap multi-country features in the README

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,60 +27,16 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 set :whenever_path, -> { release_path }
 set :whenever_roles, [:cron, :whitelist_phone_numbers, :seed]
 set :enable_confirmation, ENV['CONFIRM'] || 'true'
+set :envs_for_confirmation_step, ["production", "staging"]
 
-ENVS_FOR_CONFIRMATION_STEP = ["production", "staging"]
-namespace :deploy do
-  desc 'Runs any rake task, example: cap deploy:rake task=db:seed'
-  task rake: [:set_rails_env] do
-    on release_roles([:db]) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, ENV['task']
-        end
-      end
-    end
-  end
-
-  desc 'Print the latest deployed revision SHA'
-  task :get_latest_deployed_sha do
-    on release_roles([:app]) do
-      invoke!("deploy:set_previous_revision")
-      puts fetch(:previous_revision)
-    end
-  end
-
-  desc 'Confirm if you really want to execute the task'
-  task :confirmation do
-    puts <<-WARN
-
-    ===============================================================================
-
-      WARNING: You're about to run tasks on #{ENVS_FOR_CONFIRMATION_STEP.join('/')} server(s)
-      Please confirm that all your intentions are kind and friendly.
-
-      READ THIS IF YOU ARE RUNNING A DEPLOY TASK:
-
-      This will deploy `#{fetch(:branch)}` to `#{fetch(:stage)}`. Ensure that:
-
-      * You are deploying the correct branch
-      * You are deploying to the correct environment
-      * Your application configs are up-to-date
-      * Any necessary database/data migrations have been run
-
-    ===============================================================================
-
-    WARN
-    ask :value, "Are you sure you want to continue? (Y)"
-
-    if fetch(:value) != 'Y'
-      puts "\nDeploy canceled!"
-      exit
-    end
-  end
-end
+set :default_env, {
+  path: '/home/deploy/.rbenv/plugins/ruby-build/bin:/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:$PATH',
+  rbenv_root: '/home/deploy/.rbenv'
+}
 
 Capistrano::DSL.stages.each do |stage|
-  if ENVS_FOR_CONFIRMATION_STEP.any? { |env| stage == env } && fetch(:enable_confirmation) == 'true'
-    after stage, 'deploy:confirmation'
-  end
+  # For each stage that requires confirmation load the `deploy:confirmation` task
+  # Use the `envs_for_confirmation_step` var to set the appropriate stages
+  next unless fetch(:enable_confirmation) == 'true'
+  after stage, 'deploy:confirmation' if fetch(:envs_for_confirmation_step).any? { |env| stage == env }
 end
