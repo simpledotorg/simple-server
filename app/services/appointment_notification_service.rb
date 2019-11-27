c42d549a89b70e83c7d874b8822fecc7cd54c9b2
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-11-27 13:58:01 +0530

    Send SMS reminders on a per organization basis
    Signed-off-by: Akshay Gupta <kitallis@gmail.com>

--- app/services/appointment_notification_service.rb
+++ app/services/appointment_notification_service.rb
@@ -1,12 +1,11 @@
-class AppointmentNotificationService
-  include TargetedReleasable
-
+class AppointmentNotificationService < Struct.new(:organization)
   FAN_OUT_BATCH_SIZE = 250
 
   def send_after_missed_visit(days_overdue: 3, schedule_at:)
     fan_out_reminders(Appointment.overdue_by(days_overdue)
-                          .joins(patient: [:phone_numbers])
-                          .where(patients: {reminder_consent: "granted"}),
+                          .includes(patient: [:phone_numbers], facility: { facility_group: :organization })
+                          .where(facility: { facility_groups: { organization: organization } })
+                          .where(patients: { reminder_consent: :granted }),
                       Communication.communication_types[:missed_visit_sms_reminder],
                       schedule_at)
   end
@@ -16,29 +15,18 @@ class AppointmentNotificationService
   attr_reader :appointments, :schedule_at
 
   def fan_out_reminders(appointments, communication_type, schedule_at)
-
-    eligible_appointments = appointments.select do |appointment|
-      eligible_for_sending_sms?(appointment,
-                                communication_type)
-    end
-
-    sampled_appointments = eligible_appointments
-                             .group_by(&:facility_id)
-                             .flat_map do |_, facility_appointments|
-      facility_appointments.sample(roll_out_for(facility_appointments.size,
-                                                'APPOINTMENT_NOTIFICATION_ROLLOUT_PERCENTAGE'))
-    end
-
-    sampled_appointments.each_slice(FAN_OUT_BATCH_SIZE) do |appointments_batch|
-      AppointmentNotification::Worker.perform_at(schedule_at,
-                                                 appointments_batch.map(&:id),
-                                                 communication_type)
+    eligible_appointments(appointments, communication_type)
+        .map(&:id)
+        .each_slice(FAN_OUT_BATCH_SIZE) do |appointments_batch|
+      AppointmentNotification::Worker.perform_at(schedule_at, appointments_batch, communication_type)
     end
   end
 
+  def eligible_appointments(appointments, communication_type)
+    appointments.select { |appointment| eligible_for_sending_sms?(appointment, communication_type) }
+  end
+
   def eligible_for_sending_sms?(appointment, communication_type)
-    facility_eligible?(appointment.facility_id, 'APPOINTMENT_NOTIFICATION_FACILITY_IDS') &&
-      (not appointment.previously_communicated_via?(communication_type)) &&
-      appointment.patient&.phone_number?
+    (not appointment.previously_communicated_via?(communication_type)) && appointment.patient&.phone_number?
   end
 end
--- spec/services/appointment_notification_service_spec.rb
+++ spec/services/appointment_notification_service_spec.rb
@@ -2,25 +2,24 @@ require 'rails_helper'
 
 RSpec.describe AppointmentNotificationService do
   context '#send_after_missed_visit' do
-    let!(:facility_1) { create(:facility) }
-    let!(:facility_2) { create(:facility) }
+    let!(:ihci) { create(:organization, name: 'IHCI') }
+    let!(:path) { create(:organization, name: 'PATH') }
+    let!(:facility_group_1) { create(:facility_group, organization: ihci) }
+    let!(:facility_group_2) { create(:facility_group, organization: path) }
+    let!(:facility_1) { create(:facility, facility_group: facility_group_1) }
+    let!(:facility_2) { create(:facility, facility_group: facility_group_2) }
     let!(:overdue_appointments_from_facility_1) { create_list(:appointment, 4, :overdue, facility: facility_1) }
     let!(:overdue_appointments_from_facility_2) { create_list(:appointment, 4, :overdue, facility: facility_2) }
-    let!(:overdue_appointments) { overdue_appointments_from_facility_1 + overdue_appointments_from_facility_2 }
-    let!(:recently_overdue_appointments) do
+    let!(:all_overdue_appointments) { overdue_appointments_from_facility_1 + overdue_appointments_from_facility_2 }
+    let!(:recently_overdue_appointments_for_facility_1) do
       create_list(:appointment,
                   2,
                   facility: facility_1,
                   scheduled_date: 1.day.ago,
                   status: :scheduled)
     end
-    let!(:patient_1) { create(:patient, reminder_consent: "granted") }
-    let!(:patient_2) { create(:patient, reminder_consent: "denied") }
-    let!(:patients) { [patient_1, patient_2] }
 
     before do
-      allow(ENV).to receive(:[]).and_call_original
-
       @sms_response_double = double('SmsNotificationServiceResponse')
       allow_any_instance_of(SmsNotificationService).to receive(:send_reminder_sms).and_return(@sms_response_double)
       allow(@sms_response_double).to receive(:sid).and_return(SecureRandom.uuid)
@@ -29,79 +28,70 @@ RSpec.describe AppointmentNotificationService do
 
     it 'should spawn sms reminder jobs' do
       expect {
-        AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+        AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       }.to change(AppointmentNotification::Worker.jobs, :size).by(1)
     end
 
-    it 'should send sms reminders to eligible overdue appointments' do
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+    it 'should send sms reminders to eligible overdue appointments for the specific organization' do
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments.count).to eq(8)
+      eligible_appointments = all_overdue_appointments.select { |a| a.communications.present? }
+      expect(eligible_appointments.count).to eq(4)
     end
 
     it 'should ignore appointments which are recently overdue (< 3 days)' do
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(days_overdue: 3, schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
-      ineligible_appointments = recently_overdue_appointments.select { |a| a.communications.present? }
+      ineligible_appointments = recently_overdue_appointments_for_facility_1.select { |a| a.communications.present? }
       expect(ineligible_appointments).to be_empty
     end
 
     it 'should skip sending reminders for appointments for which reminders are already sent' do
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments).to_not be_empty
+      appointments_with_reminders_sent = overdue_appointments_from_facility_1.select { |a| a.communications.present? }
+      expect(appointments_with_reminders_sent.count).to eq(4)
+
+      reminders_to_be_sent_for_ihci = 0
 
       expect {
-        AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
-      }.to change(AppointmentNotification::Worker.jobs, :size).by(0)
+        AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
+      }.to change(AppointmentNotification::Worker.jobs, :size).by(reminders_to_be_sent_for_ihci)
+    end
+
+    it 'sending reminders for an organization should not affect reminders for another' do
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
+      AppointmentNotification::Worker.drain
+
+      expect {
+        AppointmentNotificationService.new(path).send_after_missed_visit(schedule_at: Time.current)
+      }.to change(AppointmentNotification::Worker.jobs, :size).by(1)
     end
 
     it 'should send reminders for appointments for which previous reminders failed' do
       allow(@sms_response_double).to receive(:status).and_return('failed')
 
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments).to_not be_empty
+      appointments_with_reminders_failed = all_overdue_appointments.select { |a| a.communications.any?(&:unsuccessful?) }
+      expect(appointments_with_reminders_failed.count).to eq(4)
 
       expect {
-        AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+        AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       }.to change(AppointmentNotification::Worker.jobs, :size).by(1)
     end
 
-    it 'should only send reminders for appointments under whitelisted facilities' do
-      allow(ENV).to receive(:[]).with('APPOINTMENT_NOTIFICATION_FACILITY_IDS').and_return(facility_1.id)
-
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
-      AppointmentNotification::Worker.drain
-
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments.count).to eq(4)
-    end
-
-    it 'should only send reminder of half the appointments if the rollout percentage is 50%' do
-      allow(ENV).to receive(:[]).with('APPOINTMENT_NOTIFICATION_FACILITY_IDS').and_return(facility_1.id)
-      allow(ENV).to receive(:[]).with('APPOINTMENT_NOTIFICATION_ROLLOUT_PERCENTAGE').and_return('50')
-
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
-      AppointmentNotification::Worker.drain
-
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments.count).to eq(2)
-    end
-
     it 'should only send reminders to patients who have granted consent' do
-      overdue_appointments = patients.map do |patient|
-        create(:appointment, :overdue, patient: patient)
-      end
+      overdue_appointments =
+          [create(:patient), create(:patient, :denied)].map do |patient|
+            create(:appointment, :overdue, patient: patient, facility: facility_1)
+          end
 
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
+      AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
       eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
