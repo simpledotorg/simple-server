27cd10e995b24dfbef06a6c6fc01b9c778e6d7fb
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-12-03 14:24:52 +0530

    Remove superfluous failing test on merge
    Signed-off-by: Akshay Gupta <kitallis@gmail.com>

--- spec/services/appointment_notification_service_spec.rb
+++ spec/services/appointment_notification_service_spec.rb
@@ -18,9 +18,6 @@ RSpec.describe AppointmentNotificationService do
                   scheduled_date: 1.day.ago,
                   status: :scheduled)
     end
-    let!(:patient_1) { create(:patient, reminder_consent: "granted") }
-    let!(:patient_2) { create(:patient, reminder_consent: "denied") }
-    let!(:patients) { [patient_1, patient_2] }
 
     before do
       @sms_response_double = double('SmsNotificationServiceResponse')
@@ -95,18 +92,6 @@ RSpec.describe AppointmentNotificationService do
           end
 
       AppointmentNotificationService.new(ihci).send_after_missed_visit(schedule_at: Time.current)
-      AppointmentNotification::Worker.drain
-
-      eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
-      expect(eligible_appointments.count).to eq(1)
-    end
-
-    it 'should only send reminders to patients who have granted consent' do
-      overdue_appointments = patients.map do |patient|
-        create(:appointment, :overdue, patient: patient)
-      end
-
-      AppointmentNotificationService.new.send_after_missed_visit(schedule_at: Time.current)
       AppointmentNotification::Worker.drain
 
       eligible_appointments = overdue_appointments.select { |a| a.communications.present? }
