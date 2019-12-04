184d6cc79f1b13b8255edb36e106073613b81771
Akshay Gupta <kitallis@gmail.com>
2019-12-04 18:25:07 +0530

    Revert "Overdue list sorting and filtering updates (#682)"
    This reverts commit 6242c5121bea0b0c4ac35d92b0911dd1f9bd5edf.

--- app/exporters/appointments_exporter.rb
+++
@@ -1,6 +0,0 @@
-module AppointmentsExporter
-  def self.csv(appointments)
-    patients = appointments.sort_by { |a| [a.patient.risk_priority, a.days_overdue] }.map(&:patient)
-    PatientsExporter.csv(patients)
-  end
-end
