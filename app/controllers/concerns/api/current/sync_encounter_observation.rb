ed6e5bf1b34686bf7465ddb6b9f2f2d0fd28edb3
Govind Krishna Joshi <govind@nilenso.com>
2019-12-09 17:30:32 +0530

    Minor fixes

--- app/controllers/concerns/api/current/sync_encounter_observation.rb
+++ app/controllers/concerns/api/current/sync_encounter_observation.rb
@@ -32,17 +32,17 @@ module Api::Current::SyncEncounterObservation
       MergeEncounterService.new(encounter_merge_params, current_facility, current_user, current_timezone_offset).merge
     end
 
-    def set_patient_recorded_at(bp_params)
+    def set_patient_recorded_at(params)
       # We don't set the patient recorded if retroactive data-entry is supported by the app
       # If the app supports retroactive data-entry, we expect the app to update the patients and sync
-      return if bp_params['recorded_at'].present?
+      return if params['recorded_at'].present?
 
-      patient = Patient.find_by(id: bp_params['patient_id'])
+      patient = Patient.find_by(id: params['patient_id'])
       # If the patient is not synced yet, we simply ignore setting patient's recorded_at
       return if patient.blank?
 
       # We only try to set the patient's recorded_at when retroactive data-entry is not supported on the app
-      patient.recorded_at = patient_recorded_at(bp_params, patient)
+      patient.recorded_at = patient_recorded_at(params, patient)
       patient.save
     end
 
@@ -52,9 +52,9 @@ module Api::Current::SyncEncounterObservation
     #   2. Patient's device_created_at
     #   3. The device_created_at of the current blood pressure being synced
     #
-    def patient_recorded_at(bp_params, patient)
+    def patient_recorded_at(params, patient)
       earliest_blood_pressure = patient.blood_pressures.order(recorded_at: :asc).first
-      [bp_params['created_at'], earliest_blood_pressure&.recorded_at, patient.device_created_at].compact.min
+      [params['created_at'], earliest_blood_pressure&.recorded_at, patient.device_created_at].compact.min
     end
   end
 end
