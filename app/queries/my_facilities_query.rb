0721fb3f2a2b03ffe33600a228fbbf723b10206b
vkrm <mail@vkrm.is>
2019-12-17 19:30:53 +0530

    Fixed ambiguous group_by

--- app/queries/my_facilities_query.rb
+++ app/queries/my_facilities_query.rb
@@ -2,7 +2,7 @@ class MyFacilitiesQuery < Struct.new(:facilities)
 
   def active_facilities
     facility_ids = facilities.left_outer_joins(:blood_pressures)
-                     .group(:facility_id)
+                     .group('facilities.id')
                      .where('blood_pressures.recorded_at > ?', 1.week.ago)
                      .count(:blood_pressures)
                      .select { |_, count| count >= 10}
