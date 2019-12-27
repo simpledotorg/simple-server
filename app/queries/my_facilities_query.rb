e3657466e37e022390bae38d3d155278eea60e0a
prabhanshuguptagit <prabhanshu98@gmail.com>
2019-12-27 15:30:33 +0530

    Include facilities with zero bps in inactive facilities
    Co-authored-by: Vikram <mail@vkrm.is>

--- app/queries/my_facilities_query.rb
+++ app/queries/my_facilities_query.rb
@@ -2,11 +2,11 @@ module MyFacilitiesQuery
   class InactiveFacilitiesQuery
     def self.call(facilities = Facility.all)
       facility_ids = facilities.left_outer_joins(:blood_pressures)
-                       .group('facilities.id')
-                       .where('blood_pressures.recorded_at > ?', 1.week.ago)
-                       .count(:blood_pressures)
-                       .select { |_, count| count < 10 }
-                       .keys
+                         .where('blood_pressures.recorded_at IS NULL OR blood_pressures.recorded_at > ?', 1.week.ago)
+                         .group('facilities.id')
+                         .count(:blood_pressures)
+                         .select { |_, count| count < 10 }
+                         .keys
 
       facilities.where(id: facility_ids)
     end
