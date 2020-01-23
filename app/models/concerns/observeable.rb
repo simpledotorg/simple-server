d1fc17e0a6b7c8d7f84bce5b51d8fc65186e2b6d
Akshay Gupta <kitallis@gmail.com>
2020-01-23 13:28:06 +0530

    Avoid manually updating the updated_at for Observations

--- app/models/concerns/observeable.rb
+++ app/models/concerns/observeable.rb
@@ -4,7 +4,7 @@ module Observeable
   included do
     def find_or_update_observation!(encounter, user)
       build_observation(encounter: encounter, user: user) if observation.blank?
-      observation && observation.update!(encounter: encounter, updated_at: updated_at)
+      observation.update!(encounter: encounter)
       observation
     end
   end
