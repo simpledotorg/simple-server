051bf13b52b0cad94e0d318b591fc33c681247a5
vkrm <mail@vkrm.is>
2020-01-23 19:36:20 +0530

    Rename CohortPeriodSelection methods

--- app/controllers/concerns/cohort_period_selection.rb
+++ app/controllers/concerns/cohort_period_selection.rb
@@ -6,24 +6,24 @@ module CohortPeriodSelection
   included do
     def selected_cohort_period
       @period = params[:period] == 'month' ? :month : :quarter
-      @quarter = validate_quarter(params[:quarter])
-      @month = validate_month(params[:month])
-      @year = validate_year(params[:year])
+      @quarter = sanitize_quarter(params[:quarter])
+      @month = sanitize_month(params[:month])
+      @year = sanitize_year(params[:year])
 
       { period: @period, quarter: @quarter, month: @month, year: @year }
     end
 
     private
 
-    def validate_quarter(quarter_param)
+    def sanitize_quarter(quarter_param)
       quarter_param.to_i.between?(1, 4) ? quarter_param.to_i : quarter(Time.current)
     end
 
-    def validate_year(year)
+    def sanitize_year(year)
       year.to_i > 0 ? year.to_i : Time.current.year
     end
 
-    def validate_month(month)
+    def sanitize_month(month)
       month.to_i.between?(1, 12) ? month.to_i : Time.current.month
     end
   end
