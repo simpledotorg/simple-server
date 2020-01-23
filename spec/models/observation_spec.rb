5544a43e942d270218d0f05d8072c20e4bac4241
Akshay Gupta <kitallis@gmail.com>
2020-01-23 12:39:27 +0530

    Remove belongs_to validation on observables (in Observation)
    * This is so that we can deal with discarded observables (BP etc.) and saving the Observation does not blow up
    
    * This deals with the issue when the app syncs newly created but soft-deleted BPs fail to get synced and throw a 422.

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Observation, type: :model do
   describe 'Associations' do
     it { should belong_to(:encounter) }
     it { should belong_to(:user) }
-    it { should belong_to(:observable) }
+    it { should belong_to(:observable).optional }
   end
 end
