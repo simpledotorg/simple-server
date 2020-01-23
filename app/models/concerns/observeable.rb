bdbc0c9635b05bedcb2bb1bb5d2807e9067e7b80
Akshay Gupta <kitallis@gmail.com>
2020-01-23 15:52:49 +0530

    Return the existing record on merging discarded records

--- app/models/concerns/observeable.rb
+++ app/models/concerns/observeable.rb
@@ -1,3 +1,7 @@
+#
+# This concern is different from the association `Observable` or the Rails `Observable` pattern
+# Hence, it is named `Observeable` to avoid conflicts
+#
 module Observeable
   extend ActiveSupport::Concern
 
