11ffb469dbbb10eff97452f11e43740896906ec6
prabhanshuguptagit <prabhanshu98@gmail.com>
2020-02-03 16:49:05 +0530

    Refactor bp scopes and thresholds

--- spec/models/latest_blood_pressures_per_patient_per_quarter_spec.rb
+++ spec/models/latest_blood_pressures_per_patient_per_quarter_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LatestBloodPressuresPerPatientPerQuarter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:patient) }
+  end
 end
