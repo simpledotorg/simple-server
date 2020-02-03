5c867b389cfb681db2fa86b7d549f01c3ed06a99
prabhanshuguptagit <prabhanshu98@gmail.com>
2020-02-03 18:10:20 +0530

    Fix query spec typo

--- spec/queries/my_facilities/registrations_query_spec.rb
+++ spec/queries/my_facilities/registrations_query_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe MyFacilities::RegistrationsQuery do
         let!(:included_timestamps) { [1.month.ago, 4.months.ago] }
         let!(:excluded_timestamps) { [11.months.ago] }
         let!(:patients) do
-          (include_timestamps + exclude_timestamps).map do
+          (included_timestamps + excluded_timestamps).map do
             |recorded_at| create(:patient, recorded_at: recorded_at)
           end
         end
@@ -55,7 +55,7 @@ RSpec.describe MyFacilities::RegistrationsQuery do
         let!(:included_timestamps) { [1.month.ago, 2.months.ago] }
         let!(:excluded_timestamps) { [12.months.ago] }
         let!(:patients) do
-          (include_timestamps + exclude_timestamps).map do
+          (included_timestamps + excluded_timestamps).map do
             |recorded_at| create(:patient, recorded_at: recorded_at)
           end
         end
@@ -79,7 +79,7 @@ RSpec.describe MyFacilities::RegistrationsQuery do
         let!(:included_timestamps) { [1.day.ago, 7.days.ago] }
         let!(:excluded_timestamps) { [8.days.ago] }
         let!(:patients) do
-          patient_registration_timestamps.map { |recorded_at| create(:patient, recorded_at: recorded_at) }
+          (included_timestamps + excluded_timestamps).map { |recorded_at| create(:patient, recorded_at: recorded_at) }
         end
         let!(:blood_pressures) do
           patients.map { |patient| create(:blood_pressure, patient: patient, facility: patient.registration_facility) }
