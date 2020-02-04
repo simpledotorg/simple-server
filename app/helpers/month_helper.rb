9f8a59c22dbd2e60e99bd88e2cb9d7a1424d2f61
vkrmis <48791115+vkrmis@users.noreply.github.com>
2020-02-04 08:17:52 +0000

    My facilities: BP control view (#770)
    This PR adds three new materialized views for computing the latest blood pressures per patient. The BloodPressureControlQuery class implements query methods to calculate controlled bps, uncontrolled bps and all time control for facilities.
    
    Co-authored-by: Vikram <mail@vkrm.is>
    Co-authored-by: Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
    Co-authored-by: Daniel Burka <danielburka@gmail.com>

---
+++ app/helpers/month_helper.rb
@@ -0,0 +1,27 @@
+module MonthHelper
+  def month_start(year, month)
+    Time.new(year, month)
+  end
+
+  def local_month_start(year, month)
+    Time.zone.local(year, month)
+  end
+
+  def local_month_end(year, month)
+    local_month_start(year, month).end_of_month
+  end
+
+  def month_short_name(month_start)
+    month_start.strftime('%b')
+  end
+
+  def month_short_name_and_year(month_start)
+    month_start.strftime('%b-%Y')
+  end
+
+  def last_n_months(n)
+    (1..n).map do |i|
+      Time.current.beginning_of_month - i.months
+    end
+  end
+end
---
+++ app/models/latest_blood_pressures_per_patient.rb
@@ -0,0 +1,7 @@
+class LatestBloodPressuresPerPatient < ApplicationRecord
+  include BloodPressureable
+
+  def self.refresh
+    Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
+  end
+end
