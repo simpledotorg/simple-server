4de682f036bb74d358d32f926f623623dee461d1
Akshay Gupta <kitallis@gmail.com>
2020-02-04 13:45:04 +0530

    Updating server hosts for qa + sbx + security

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,1 +1,1 @@
-server "ec2-35-154-28-82.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
+server "ec2-13-233-19-238.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron)
