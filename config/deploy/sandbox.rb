2c816e73d58c605a6c9798d93cb5e9a3bfaf7ccb
Akshay Gupta <kitallis@gmail.com>
2020-02-10 13:27:17 +0530

    Add whenever (cron) job + cap role to populate fake data on SBX

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,1 +1,1 @@
-server "ec2-13-233-190-137.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron whitelist_phone_numbers seed)
+server "ec2-13-233-190-137.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w(web app db sidekiq cron whitelist_phone_numbers fake_data)
--- config/schedule.rb
+++ config/schedule.rb
@@ -22,8 +22,8 @@ every :week, at: local('01:00 am'), roles: [:whitelist_phone_numbers] do
   rake 'exotel_tasks:update_all_patients_phone_number_details'
 end
 
-every :month, at: local('02:00 am'), roles: [:seed] do
-  rake 'generate:seed[1]'
+every :week, at: local('02:00 am'), roles: [:fake_data] do
+  rake 'generate:fake_data'
 end
 
 every :day, at: local('03:00 am'), roles: [:cron] do
