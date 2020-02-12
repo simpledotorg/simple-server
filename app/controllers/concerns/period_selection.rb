fb37df2af05383f03fffed4bc83c0d2891bf3f5b
vkrm <mail@vkrm.is>
2020-02-12 16:59:14 +0530

    Default selected period when invalid period is passed

--- app/controllers/concerns/period_selection.rb
+++ app/controllers/concerns/period_selection.rb
@@ -8,7 +8,18 @@ module PeriodSelection
     REGISTRATIONS_PERIODS = { quarter: 'Quarterly', month: 'Monthly', day: 'Daily' }.freeze
 
     def set_selected_period
-      @selected_period = params[:period].blank? ? :quarter : params[:period].to_sym
+      @selected_period = params[:period].blank? || invalid_period? ? :quarter : params[:period].to_sym
+    end
+
+    def invalid_period?
+      valid_periods = case action_name.to_sym
+      when :missed_visits
+        MISSED_VISITS_PERIODS.keys
+      when :registrations
+        REGISTRATIONS_PERIODS.keys
+      end
+
+      !valid_periods.include?(params[:period])
     end
   end
 end
