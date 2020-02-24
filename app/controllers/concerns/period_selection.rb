8f59c30cb45f27d0e8bdfac4ddfe3ab5f510cdaf
vkrm <mail@vkrm.is>
2020-02-24 15:04:32 +0530

    Address changes requested in PR review

--- app/controllers/concerns/period_selection.rb
+++ app/controllers/concerns/period_selection.rb
@@ -5,14 +5,16 @@ module PeriodSelection
 
   included do
     PERIODS = { missed_visits: { quarter: 'Quarterly', month: 'Monthly' },
-                registrations: { quarter: 'Quarterly', month: 'Monthly', day: 'Daily' } }.freeze
+                registrations: { quarter: 'Quarterly', month: 'Monthly', day: 'Daily' } }
+              .with_indifferent_access
+              .freeze
 
     def set_selected_period
-      @selected_period = params[:period].blank? || invalid_period? ? :quarter : params[:period].to_sym
+      @selected_period = params[:period].blank? || invalid_period? ? :quarter : params[:period]
     end
 
     def invalid_period?
-      valid_periods = PERIODS[action_name.to_sym].keys
+      valid_periods = PERIODS[action_name].keys
 
       !valid_periods.include?(params[:period].to_sym)
     end
