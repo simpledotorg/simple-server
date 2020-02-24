146bd4f833fc0db1ff89304a4b1c70461ce51622
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-02-24 16:41:23 +0530

    Remove redundant compilation of print.scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( user_analytics.scss common_webview.scss help.scss print.scss )
-Rails.application.config.assets.precompile += %w( print.scss )
