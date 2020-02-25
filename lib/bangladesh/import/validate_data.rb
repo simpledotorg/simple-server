f89690e990d148c86fbe654c6984bbd0d8513179
Akshay Gupta <kitallis@gmail.com>
2020-02-25 14:00:26 +0530

    Add instructions on how to run validate_data script

--- lib/bangladesh/import/validate_data.rb
+++ lib/bangladesh/import/validate_data.rb
@@ -1,6 +1,22 @@
+#
+# The purpose of this script is to ensure that the exported data passes all schema validation checks
+#
+# This is a very low-cost way of ensuring your imported data is correct, so go through the comments down below to
+# learn how to use and run this script
+#
+# NOTE: How to run
+#
+# In a rails console,
+#
+# > require './lib/bangladesh/validate_data.rb'
+# > ValidateData.new.validate
+#
 require 'http'
 
 class ValidateData
+  #
+  # NOTE: Modify this URL as per your env
+  #
   HOST = 'http://simple.test'.freeze
 
   REQUEST_URLS = [
@@ -18,6 +34,9 @@ class ValidateData
     end
   end
 
+  #
+  # NOTE: Modify the Bearer token as per your user's access_token
+  #
   def api_get(model_name)
     response = HTTP
                  .auth('Bearer 72789892ff8f876e159e7757983ff82f60b37d3e5a84240e61470c85eecaa50f')
@@ -35,6 +54,9 @@ class ValidateData
     end
   end
 
+  #
+  # NOTE: Modify the user and facility ID headers as per your test
+  #
   def api_headers
     { 'Content-Type' => 'application/json',
       'ACCEPT' => 'application/json',
