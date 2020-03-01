14aa5d9b93af57fde0296e6a6e9544aa8c2efe9b
Daniel Burka <danielburka@gmail.com>
2020-03-01 13:32:14 -0500

    Added icons to FAQ. Removed internationalization thing we disabled. Did a spell check.

--- app/assets/javascripts/help.js
+++ app/assets/javascripts/help.js
@@ -18,11 +18,11 @@ window.onload = function() {
     for (i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var answer = this.nextElementSibling;
-        if (answer.style.display === "block") {
-          answer.style.display = "none";
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
         } else {
-          answer.style.display = "block";
+          panel.style.maxHeight = panel.scrollHeight + "px";
         }
       });
     } 
