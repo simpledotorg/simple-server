8f2688fa981ea3f28345e9390da30f03df77198d
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-03-05 13:19:58 +0530

    Revert "Email invitations (#797)"
    This reverts commit e1cf047fe2b09797a98a2b04c4466daa059f05fe.

--- app/helpers/simple_server_env_helper.rb
+++ app/helpers/simple_server_env_helper.rb
@@ -2,7 +2,7 @@ module SimpleServerEnvHelper
   CUSTOMIZED_ENVS = %w[development qa staging sandbox production].freeze
 
   def style_class_for_environment
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
+    env = ENV.fetch("SIMPLE_SERVER_ENV")
 
     styles = %w[navbar navbar-expand-md fixed-top]
     styles += bootstrap_navbar_classes_for_environment(env)
@@ -12,42 +12,35 @@ module SimpleServerEnvHelper
   end
 
   def logo_for_environment
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
+    env = ENV.fetch("SIMPLE_SERVER_ENV")
 
-    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}.svg" : 'simple_logo.svg'
+    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}.svg" : "simple_logo.svg"
 
-    image_tag image_name, width: 30, height: 30, class: 'd-inline-block mr-2 align-top', alt: alt_for_environment
-  end
+    image_tag image_name, width: 30, height: 30, class: "d-inline-block mr-2 align-top", alt: alt_for_environment
 
-  def alt_for_environment
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
-
-    CUSTOMIZED_ENVS.include?(env) ? "Simple Server #{env.capitalize} Logo" : 'Simple Server Logo'
   end
 
   def favicon_for_environment
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
+    env = ENV.fetch("SIMPLE_SERVER_ENV")
 
-    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}_favicon.png" : 'simple_logo_favicon.png'
+    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}_favicon.png" : "simple_logo_favicon.png"
 
     image_path(image_name)
   end
 
-  def mailer_logo_for_environment
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
+  def alt_for_environment
+    env = ENV.fetch("SIMPLE_SERVER_ENV")
 
-    image_name = CUSTOMIZED_ENVS.include?(env) ? "simple_logo_#{env}-256.png" : 'simple_logo-256.png'
-
-    image_tag image_name, width: 48, height: 48, style: 'width: 48px; height: 48px;'
+    CUSTOMIZED_ENVS.include?(env) ? "Simple Server #{env.capitalize} Logo" : "Simple Server Logo"
   end
 
   def get_title_for_environment
     title = I18n.t('admin.dashboard_title')
-    env = ENV.fetch('SIMPLE_SERVER_ENV')
+    env = ENV.fetch("SIMPLE_SERVER_ENV")
 
-    return title if env.downcase == 'production'
+    return title if env.downcase == "production"
 
-    prefix = CUSTOMIZED_ENVS.include?(env) ? "[#{env.humanize}] " : ''
+    prefix = CUSTOMIZED_ENVS.include?(env) ? "[#{env.humanize}] " : ""
     prefix + title
   end
 
