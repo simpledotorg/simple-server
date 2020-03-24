be3436c08cd374f494a08521e20d2e9b1b63b286
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-03-24 16:04:56 +0530

    Add v4 user swagger specs

---
+++ spec/api/v4/users_spec.rb
@@ -0,0 +1,32 @@
+require 'swagger_helper'
+
+describe 'Users v4 API', swagger_doc: 'v4/swagger.json' do
+  let(:facility) { FactoryBot.create(:facility) }
+  let!(:supervisor) { FactoryBot.create(:admin, :supervisor, facility_group: facility.facility_group) }
+  let!(:organization_owner) { FactoryBot.create(:admin, :organization_owner, organization: facility.organization) }
+
+  path '/users/find' do
+    post 'Find a existing user' do
+      tags 'User'
+      parameter name: :phone_number, in: :query, type: :string, description: 'User phone number'
+
+      let(:known_phone_number) { Faker::PhoneNumber.phone_number }
+      let!(:user) { FactoryBot.create(:user, phone_number: known_phone_number, registration_facility: facility) }
+      let(:id) { user.id }
+
+      response '200', 'user is found' do
+        schema '$ref' => '#/definitions/find_user'
+        let(:phone_number) { known_phone_number }
+        let(:id) { user.id }
+        run_test!
+      end
+
+      response '404', 'user is not found' do
+        let(:id) { SecureRandom.uuid }
+        let(:phone_number) { Faker::PhoneNumber.phone_number }
+        run_test!
+      end
+    end
+  end
+
+end
