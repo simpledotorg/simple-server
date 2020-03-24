a820571a08635d17aa81ef2a51ba5ad944e5b965
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-03-24 12:57:49 +0530

    Add sync approval fields to v3 users api

--- app/schema/api/v3/models.rb
+++ app/schema/api/v3/models.rb
@@ -250,6 +250,8 @@ class Api::V3::Models
           created_at: { '$ref' => '#/definitions/timestamp' },
           updated_at: { '$ref' => '#/definitions/timestamp' },
           full_name: { '$ref' => '#/definitions/non_empty_string' },
+          sync_approval_status: { type: [:string, 'null'] },
+          sync_approval_status_reason: { type: [:string, 'null'] },
           phone_number: { '$ref' => '#/definitions/non_empty_string' },
           password_digest: { '$ref' => '#/definitions/bcrypt_password' },
           registration_facility_id: { '$ref' => '#/definitions/uuid' },
