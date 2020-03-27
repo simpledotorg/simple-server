66522cebed759c14ee9d21f0c1891d11001bc485
Akshay Gupta <kitallis@gmail.com>
2020-03-27 11:19:58 +0530

    All dates in progress tab should be according to country tz

--- app/controllers/api/v3/analytics_controller.rb
+++ app/controllers/api/v3/analytics_controller.rb
@@ -1,14 +1,18 @@
 class Api::V3::AnalyticsController < APIController
-  before_action :set_timezone_to_IST
-  after_action :set_timezone_to_UTC
+  around_action :set_time_zone
 
   private
 
-  def set_timezone_to_IST
-    Groupdate.time_zone = "Asia/Kolkata"
-  end
+  def set_time_zone
+    time_zone = Rails.application.config.country[:time_zone] || DEFAULT_ANALYTICS_TIME_ZONE
 
-  def set_timezone_to_UTC
+    Groupdate.time_zone = time_zone
+
+    Time.use_zone(time_zone) do
+      yield
+    end
+
+    # Make sure we reset the timezone
     Groupdate.time_zone = "UTC"
   end
 end
