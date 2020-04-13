5f4336e1739518b57b6e9f13475af44dbde5e271
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-04-13 14:09:36 +0530

    Scopify overdue

--- app/models/patient_summary.rb
+++ app/models/patient_summary.rb
@@ -4,14 +4,14 @@ class PatientSummary < ActiveRecord::Base
   belongs_to :patient, foreign_key: :id
   belongs_to :next_appointment, class_name: 'Appointment', foreign_key: :next_appointment_id
 
-  def readonly?
-    true
-  end
-
-  def self.overdue
+  scope :overdue, -> do
     where(next_appointment_status: 'scheduled')
       .where('next_appointment_scheduled_date < ?', Date.current)
       .where('next_appointment_scheduled_date >= ?', 365.days.ago)
       .where('next_appointment_remind_on IS NULL OR next_appointment_remind_on <= ?', Date.current)
+  end
+
+  def readonly?
+    true
   end
 end
