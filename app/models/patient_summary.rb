63cea05618f12f9217f914cffe8768cb8a6a80ce
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-04-14 11:59:42 +0530

    Use the Appointment.overdue scope for PatientSummary

--- app/models/patient_summary.rb
+++ app/models/patient_summary.rb
@@ -4,12 +4,7 @@ class PatientSummary < ActiveRecord::Base
   belongs_to :patient, foreign_key: :id
   belongs_to :next_appointment, class_name: 'Appointment', foreign_key: :next_appointment_id
 
-  scope :overdue, -> do
-    where(next_appointment_status: 'scheduled')
-      .where('next_appointment_scheduled_date < ?', Date.current)
-      .where('next_appointment_scheduled_date >= ?', 365.days.ago)
-      .where('next_appointment_remind_on IS NULL OR next_appointment_remind_on <= ?', Date.current)
-  end
+  scope :overdue, -> { joins(:next_appointment).merge(Appointment.overdue) }
 
   def readonly?
     true
