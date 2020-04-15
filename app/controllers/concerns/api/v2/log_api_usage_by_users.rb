08ffb01d62fb569fcaeca10d299bd88302a4340a
vkrm <mail@vkrm.is>
2020-04-15 23:15:54 +0530

    Use add_custom_attributes

--- app/controllers/concerns/api/v2/log_api_usage_by_users.rb
+++ app/controllers/concerns/api/v2/log_api_usage_by_users.rb
@@ -4,7 +4,7 @@ module Api::V2::LogApiUsageByUsers
     before_action :log_api_usage
 
     def log_api_usage
-      NewRelic::Agent.add_custom_parameters(user_id: current_user&.id || 'no-user-id')
+      NewRelic::Agent.add_custom_attributes(user_id: current_user&.id || 'no-user-id')
       Loggers::ApiUsageLogger.tagged(params[:controller], params[:action], current_user&.id || "no-user-id") { Loggers::ApiUsageLogger.info(1) }
     end
   end
