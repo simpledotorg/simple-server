d186a4428037bc24d51b175719319daedc494763
vkrmis <48791115+vkrmis@users.noreply.github.com>
2020-04-16 10:58:20 +0000

    Log V2 api usage (#883)

--- app/controllers/api/v2/analytics/user_analytics_controller.rb
+++ app/controllers/api/v2/analytics/user_analytics_controller.rb
@@ -1,2 +1,3 @@
 class Api::V2::Analytics::UserAnalyticsController < Api::V3::Analytics::UserAnalyticsController
+  include Api::V2::LogApiUsageByUsers
 end
--- app/controllers/api/v2/medical_histories_controller.rb
+++ app/controllers/api/v2/medical_histories_controller.rb
@@ -1,2 +1,3 @@
 class Api::V2::MedicalHistoriesController < Api::V3::MedicalHistoriesController
+  include Api::V2::LogApiUsageByUsers
 end
