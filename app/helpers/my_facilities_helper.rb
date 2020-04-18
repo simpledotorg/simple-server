dccbba4191b8539bfc607913a3576526151eb806
Daniel Burka <danielburka@gmail.com>
2020-04-18 20:56:50 +0100

    CHanged NA to — in tables

--- app/helpers/my_facilities_helper.rb
+++ app/helpers/my_facilities_helper.rb
@@ -4,7 +4,7 @@ module MyFacilitiesHelper
   end
 
   def percentage(numerator, denominator)
-    return 'NA' if denominator.nil? || denominator.zero? || numerator.nil?
+    return '—' if denominator.nil? || denominator.zero? || numerator.nil?
     percentage_string((numerator * 100.0) / denominator)
   end
 end
