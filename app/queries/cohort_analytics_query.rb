42cd31e433b2b07957fca6581198799c51e77bad
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-04-20 17:27:46 +0530

    Support facility breakdown of cohort numbers

--- app/queries/cohort_analytics_query.rb
+++ app/queries/cohort_analytics_query.rb
@@ -6,7 +6,7 @@ class CohortAnalyticsQuery
     @include_current_period = true
   end
 
-  def patient_counts_by_period(period, prev_periods, from_time: Time.current)
+  def patient_counts_by_period(period, prev_periods, from_time: Time.current, per_facility: false)
     results = {}
 
     # index is a quick hack to allow toggling the current period in the results.
@@ -26,10 +26,11 @@ class CohortAnalyticsQuery
         report_end = report_start.end_of_quarter
       end
 
-      results[[cohort_start.to_date, report_start.to_date]] = patient_counts(cohort_start,
-                                                                             cohort_end,
-                                                                             report_start,
-                                                                             report_end)
+      if per_facility
+        results[[cohort_start.to_date, report_start.to_date]] = patient_counts_per_facility(cohort_start, cohort_end, report_start, report_end)
+      else
+        results[[cohort_start.to_date, report_start.to_date]] = patient_counts(cohort_start, cohort_end, report_start, report_end)
+      end
     end
 
     results
@@ -47,6 +48,32 @@ class CohortAnalyticsQuery
       defaulted: registered_patients.size - followed_up_patients.size,
       controlled: controlled_patients.size,
       uncontrolled: uncontrolled_patients.size
+    }
+  end
+
+  def patient_counts_per_facility(cohort_start, cohort_end, report_start, report_end)
+    registered_patients = registered(cohort_start, cohort_end)
+    followed_up_patients = followed_up(registered_patients, report_start, report_end)
+    controlled_patients = controlled(followed_up_patients)
+    uncontrolled_patients = followed_up_patients - controlled_patients
+
+    registered_counts = registered_patients.group(:registration_facility_id).size.symbolize_keys
+    followed_up_counts = followed_up_patients.group(:registration_facility_id).size.symbolize_keys
+    defaulted_counts = registered_counts.merge(followed_up_counts) do |_, registered, followed_up|
+      registered - followed_up
+    end
+
+    controlled_counts = controlled_patients.group(:registration_facility_id).size.symbolize_keys
+    uncontrolled_counts = followed_up_counts.merge(controlled_counts) do |_, followed_up, controlled|
+      followed_up - controlled
+    end
+
+    {
+      registered: { total: registered_patients.size, **registered_counts },
+      followed_up: { total: followed_up_patients.size, **followed_up_counts },
+      defaulted: { total: registered_patients.size - followed_up_patients.size, **defaulted_counts },
+      controlled: { total: controlled_patients.size, **controlled_counts },
+      uncontrolled: { total: uncontrolled_patients.size, **uncontrolled_counts }
     }
   end
 
@@ -74,6 +101,6 @@ class CohortAnalyticsQuery
   end
 
   def controlled(patients)
-    patients.select { |p| p.bp_systolic < 140 && p.bp_diastolic < 90 }
+    patients.where('newest_bps.systolic < 140 AND newest_bps.diastolic < 90')
   end
 end
