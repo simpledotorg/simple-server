cf4a78e16c897b26dbe1a5200027c60a374e3252
vkrmis <48791115+vkrmis@users.noreply.github.com>
2020-04-21 12:57:56 +0000

    Multiply monthly_estimated_opd_load by 3 for quarterly view (#892)

--- app/helpers/my_facilities_helper.rb
+++ app/helpers/my_facilities_helper.rb
@@ -7,4 +7,15 @@ module MyFacilitiesHelper
     return '—' if denominator.nil? || denominator.zero? || numerator.nil?
     percentage_string((numerator * 100.0) / denominator)
   end
+
+  def opd_load(facility, selected_period)
+    return if facility.monthly_estimated_opd_load.nil?
+
+    case selected_period
+    when :quarter
+      facility.monthly_estimated_opd_load * 3
+    when :month
+      facility.monthly_estimated_opd_load
+    end
+  end
 end
