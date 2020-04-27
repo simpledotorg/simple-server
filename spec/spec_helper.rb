1f5162765a2f285ae61b5b81b1104a0b470bc533
Rob Sanheim <rsanheim@gmail.com>
2020-04-27 15:58:38 -0500

    Turn on focused specs
    Allows running just specific specs by changing "it" to "fit" in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,9 @@ WebMock.allow_net_connect!
 RSpec.configure do |config|
   SimpleCov.start
 
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
