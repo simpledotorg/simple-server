f5d6b30e910fd33422faba803134985cd6370973
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2020-04-27 19:10:43 +0530

    Update ethiopia demo manifest (#898)
    * Update ethiopia demo manifest
    * Add ethiopia:demo cap file

---
+++ config/deploy/ethiopia/demo.rb
@@ -0,0 +1,3 @@
+server "167.71.230.18", user: "deploy", roles: %w(web app db cron)
+server "167.71.234.57", user: "deploy", roles: %w(web app db)
+server "167.71.238.74", user: "deploy", roles: %w(web sidekiq)
