b4edacc8a833a854ce30f64556fa3605d07b4a34
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-04-30 16:11:29 +0530

    Add send_reminder_whatsapp specs

--- spec/services/sms_notification_service_spec.rb
+++ spec/services/sms_notification_service_spec.rb
@@ -104,4 +104,79 @@ RSpec.describe SmsNotificationService do
       end
     end
   end
+
+  describe '#send_reminder_whatsapp' do
+    context 'follow_up_reminder' do
+      it 'should have the SMS body in the default locale' do
+        sms = SmsNotificationService.new(recipient_phone_number, sender_phone_number, twilio_client)
+
+        expected_msg_default = 'Our staff at Simple Facility are thinking of you and your heart health. Please continue your blood pressure medicines. Collect your medicine from the nearest sub centre. Contact your ANM or ASHA.'
+        expect(twilio_client).to receive_message_chain('messages.create').with(from: 'whatsapp:+15005550006',
+                                                                               to: "whatsapp:#{expected_sms_recipient_phone_number}",
+                                                                               status_callback: '',
+                                                                               body: expected_msg_default)
+
+        sms.send_reminder_whatsapp('missed_visit_sms_reminder', appointment, '')
+      end
+
+      it 'should have the SMS body in Marathi' do
+        sms = SmsNotificationService.new(recipient_phone_number, sender_phone_number, twilio_client)
+
+        expected_msg_marathi = 'Our staff at Simple Facility are thinking of you and your heart health. Please continue your blood pressure medicines. Collect your medicine from the nearest sub centre. Contact your ANM or ASHA.'
+        expect(twilio_client).to receive_message_chain('messages.create').with(from: 'whatsapp:+15005550006',
+                                                                               to: "whatsapp:#{expected_sms_recipient_phone_number}",
+                                                                               status_callback: '',
+                                                                               body: expected_msg_marathi)
+
+        sms.send_reminder_whatsapp('missed_visit_sms_reminder', appointment, '', 'mr-IN')
+      end
+
+      it 'should have the SMS body in Punjabi' do
+        sms = SmsNotificationService.new(recipient_phone_number, sender_phone_number, twilio_client)
+
+        expected_msg_punjabi = 'Our staff at Simple Facility are thinking of you and your heart health. Please continue your blood pressure medicines. Collect your medicine from the nearest sub centre. Contact your ANM or ASHA.'
+        expect(twilio_client).to receive_message_chain('messages.create').with(from: 'whatsapp:+15005550006',
+                                                                               to: "whatsapp:#{expected_sms_recipient_phone_number}",
+                                                                               status_callback: '',
+                                                                               body: expected_msg_punjabi)
+
+        sms.send_reminder_whatsapp('missed_visit_sms_reminder', appointment, '', 'pa-Guru-IN')
+      end
+
+      it 'should raise an error if the locale for the SMS body is unsupported' do
+        sms = SmsNotificationService.new(recipient_phone_number, sender_phone_number, twilio_client)
+
+        expect do
+          sms.send_reminder_whatsapp('missed_visit_sms_reminder', appointment, ' ', 'gu-IN')
+        end.to raise_error(StandardError)
+      end
+
+      context 'when country code is set in environment' do
+        before do
+          @original_country = Rails.application.config.country
+          Rails.application.config.country = { sms_country_code: '+880' }
+        end
+
+        after do
+          Rails.application.config.country = @original_country
+        end
+
+        let(:expected_sms_recipient_phone_number) { '+8808585858585' }
+
+        it 'uses the set country code' do
+          sms = SmsNotificationService.new(recipient_phone_number, sender_phone_number, twilio_client)
+          expected_msg_default = 'Our staff at Simple Facility are thinking of you and your heart health. Please continue your blood pressure medicines. Collect your medicine from the nearest sub centre. Contact your ANM or ASHA.'
+
+          expect(twilio_client).to receive_message_chain('messages.create').with(
+            from: 'whatsapp:+15005550006',
+            to: "whatsapp:#{expected_sms_recipient_phone_number}",
+            status_callback: '',
+            body: /#{expected_msg_default}/
+          )
+
+          sms.send_reminder_whatsapp('missed_visit_sms_reminder', appointment, '')
+        end
+      end
+    end
+  end
 end
