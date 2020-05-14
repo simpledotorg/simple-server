98ffd6fe162f796be4d6d2029a9e9573345e2127
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-05-14 13:45:05 +0530

    Update dosages of the three protocol drugs
    - Amlodipine: 5 mg
    - Losartan Potassium: 50 mg
    - Hydrochlorothiazide: 12.5 mg

--- lib/tasks/scripts/correct_bangladesh_medication_dosages.rb
+++ lib/tasks/scripts/correct_bangladesh_medication_dosages.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CorrectBangladeshMedicationDosages
+  PROTOCOL_DOSAGES = {
+    'Amlodipine' => '5 mg',
+    'Losartan Potassium' => '50 mg',
+    'Hydrochlorothiazide' => '12.5 mg'
+  }.freeze
+
   def self.call(*args)
     new(*args).call
   end
@@ -12,6 +18,25 @@ class CorrectBangladeshMedicationDosages
   end
 
   def call
-    puts "Thanks for playing #{dryrun}"
+    print_summary
+
+    return if dryrun
+
+    update_dosages
+  end
+
+  def print_summary
+    puts 'Zero dosage medications found:'
+    PROTOCOL_DOSAGES.keys.each { |drug| puts "#{drug}: #{eligible_drugs(drug).count} records" }
+  end
+
+  def update_dosages
+    PROTOCOL_DOSAGES.keys.each do |drug|
+      eligible_drugs(drug).update_all(dosage: PROTOCOL_DOSAGES[drug])
+    end
+  end
+
+  def eligible_drugs(name)
+    PrescriptionDrug.where(name: name, dosage: '0 mg')
   end
 end
