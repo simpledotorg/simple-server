d92e5ed6c3e27ba8bc45ff4b4c06182ba4829937
Tim Cheadle <tcheadle@resolvetosavelives.org>
2020-05-15 09:57:16 -0400

    Refactored to unify sending twilio messages

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -39,31 +39,37 @@ class NotificationService
 =end
 
   def send_sms(recipient_number, message)
-    recipient_number = parse_phone_number(recipient_number).insert(0, Rails.application.config.country[:sms_country_code])
+    sender_number    = twilio_sender_number
+    recipient_number = parse_phone_number(recipient_number)
 
-    client.messages.create(
-      from: twilio_sender_number,
-      to: recipient_number,
-      status_callback: twilio_callback_url,
-      body: body
-    )
+    send_twilio_message(sender_number, recipient_number, message)
   end
 
   def send_whatsapp(recipient_number, message)
-    recipient_number = parse_phone_number(recipient_number).insert(0, Rails.application.config.country[:sms_country_code])
+    sender_number    = "whatsapp:" + twilio_sender_number
+    recipient_number = "whatsapp:" + parse_phone_number(recipient_number)
 
-    client.messages.create(
-      from: "whatsapp:" + twilio_sender_number,
-      to: "whatsapp:" + recipient_number,
-      status_callback: twilio_callback_url,
-      body: body
-    )
+    send_twilio_message(sender_number, recipient_number, message)
+  end
+
+  def parse_phone_number(number)
+    parsed_number = Phonelib.parse(number, Rails.application.config.country[:abbreviation]).raw_national
+    default_country_code + parsed_number
   end
 
   private
 
-  def parse_phone_number(number)
-    Phonelib.parse(number, Rails.application.config.country[:abbreviation]).raw_national
+  def default_country_code
+    Rails.application.config.country[:sms_country_code]
+  end
+
+  def send_twilio_message(sender_number, recipient_number, message)
+    client.messages.create(
+      from: sender_number,
+      to: recipient_number,
+      status_callback: twilio_callback_url,
+      body: message
+    )
   end
 
   def twilio_account_sid
