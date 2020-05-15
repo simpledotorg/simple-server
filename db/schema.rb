c7efffe336c95152e5d7c13561c688991e078d4d
Akshay Gupta <kitallis@gmail.com>
2020-05-15 15:54:31 +0530

    Add index on deleted_at for Encounters

--- db/schema.rb
+++ db/schema.rb
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(version: 20200505104339) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_encounters_on_deleted_at"
     t.index ["facility_id"], name: "index_encounters_on_facility_id"
     t.index ["patient_id"], name: "index_encounters_on_patient_id"
   end
