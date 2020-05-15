79ea0e8fc4dd493c317d1303dc94731c85a7e02b
Rob Sanheim <rsanheim@gmail.com>
2020-05-15 11:20:58 -0500

    Schema updates for api lockouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505104339) do
+ActiveRecord::Schema.define(version: 2020_05_14_193029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -369,6 +369,8 @@ ActiveRecord::Schema.define(version: 20200505104339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.datetime "locked_at"
   end
 
   create_table "prescription_drugs", id: :uuid, default: nil, force: :cascade do |t|
