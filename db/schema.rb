2ec3acba2812cb6d789626a9adc1e3e483d27f40
Akshay Gupta <kitallis@gmail.com>
2020-05-15 16:30:41 +0530

    Add the schema timestamp!

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505104339) do
+ActiveRecord::Schema.define(version: 2020_05_15_101844) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
   create_table "addresses", id: :uuid, default: nil, force: :cascade do |t|
     t.string "street_address"
