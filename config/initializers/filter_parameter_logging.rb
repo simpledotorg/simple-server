4fb78b3f0fe3360833cdb19ebb1de54c1899401d
Rob Sanheim <rsanheim@gmail.com>
2020-05-21 15:42:06 -0500

    Much better specs; move constants to a module for namespacing
    I'm testing this two ways:
    
    * a spec that looks for various known sensitive attributes directly in
    the log output, and fails if those are found
    * a spec that looks at the whitelist and ensures it allows whitelisted
    attributes through, and blocks everything else
    
    This should give us more confidence that the filtering we are doing is
    working here.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,16 +1,32 @@
 # Be sure to restart your server when you modify this file.
+module ParameterFiltering
+  ATTRIBUTE_WHITELIST = %w[
+    action
+    active
+    code
+    controller
+    created_at
+    deleted_at
+    limit
+    metadata_version
+    process_token
+    recorded_at
+    status
+    updated_at
+  ].freeze
 
-# Configure sensitive parameters which will be filtered from the log file.
+  WHITELIST_REGEX = /(^|_)ids?|#{Regexp.union(ATTRIBUTE_WHITELIST)}/.freeze
+  SANITIZED_VALUE = "[FILTERED]".freeze
+end
+
 Rails.application.config.filter_parameters += [:password, :age]
-
-WHITELISTED_KEYS_MATCHER = /((^|_)ids?|action|controller|code|created_at|updated_at|recorded_at|deleted_at|limit|process_token$)/.freeze
-SANITIZED_VALUE = '[FILTERED]'.freeze
-
 Rails.application.config.filter_parameters << lambda do |key, value|
-  unless key.match(WHITELISTED_KEYS_MATCHER)
+  unless key.match(ParameterFiltering::WHITELIST_REGEX)
     case value
     when String
-      value.replace(SANITIZED_VALUE)
+      value.replace(ParameterFiltering::SANITIZED_VALUE)
+    else
+      value.to_s.replace(ParameterFiltering::SANITIZED_VALUE)
     end
   end
 end
--- spec/requests/filter_parameter_logging_spec.rb
+++ spec/requests/filter_parameter_logging_spec.rb
@@ -1,53 +1,83 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Filter parameter logging spec', type: :request do
-  let(:model) { Patient }
+RSpec.describe "Filter parameter logging spec", type: :request do
   let(:request_user) { FactoryBot.create(:user) }
-
-  let(:sync_route) { '/api/v3/patients/sync' }
-
+  let(:sync_route) { "/api/v3/patients/sync" }
   let(:build_payload) { -> { build_patient_payload(FactoryBot.build(:patient, registration_facility: request_user.facility)) } }
-  let(:build_invalid_payload) { -> { build_invalid_patient_payload } }
-  let(:update_payload) { ->(record) { updated_patient_payload record } }
 
-  def to_response(patient)
-    Api::V3::PatientTransformer.to_nested_response(patient)
+  let(:auth_headers) do
+    {"HTTP_X_USER_ID" => request_user.id,
+     "HTTP_X_FACILITY_ID" => request_user.facility.id,
+     "HTTP_AUTHORIZATION" => "Bearer #{request_user.access_token}"}
+  end
+  let(:headers) do
+    {"ACCEPT" => "application/json", "CONTENT_TYPE" => "application/json"}.merge(auth_headers)
   end
 
-  include_examples 'v3 API sync requests'
-
-  it "does not log any non-whitelisted parameters" do
-    output = StringIO.new
-    test_logger = Logger.new(output)
+  it "does not log sensitive patient data" do
+    stringio = StringIO.new
+    test_logger = Logger.new(stringio)
     allow(ActionController::Base).to receive(:logger).and_return(test_logger)
 
-    first_patients_payload = (1..3).map { build_payload.call }
-    post sync_route, params: { patients: first_patients_payload }.to_json, headers: headers
+    patient_payloads = (1..3).map { build_payload.call }
+    post sync_route, params: {patients: patient_payloads}.to_json, headers: headers
     get sync_route, params: {}, headers: headers
-    process_token = JSON(response.body)['process_token']
 
-    created_patients         = Patient.find(first_patients_payload.map { |patient| patient['id'] })
-    updated_patients_payload = created_patients.map do |patient|
-      updated_patient_payload(patient)
-        .except(%w[address phone_numbers business_identifiers].sample)
-    end
-
-    post sync_route, params: { patients: updated_patients_payload }.to_json, headers: headers
-    get sync_route, params: { process_token: process_token }, headers: headers
-
-    assert_sync_success(response, process_token)
-
-    puts output.string
-    str = output.string
-    first_patients_payload.each do |payload|
-      payload.each do |attr, value|
-        next if value.blank?
-        unless attr =~ WHITELISTED_KEYS_MATCHER
-          expect(str).to_not include(value.to_s), "expected to not have blacklisted #{attr.inspect} logged with value #{value.inspect}"
-        end
+    patients = patient_payloads.map { |payload| Patient.find(payload[:id]) }
+    output = stringio.string
+    patients.each do |patient|
+      expect(output).to_not match(/\b#{patient.full_name}\b/)
+      expect(output).to_not match(/\b#{patient.age}\b/) if patient.age
+      expect(output).to_not match(/\b#{patient.date_of_birth}\b/) if patient.date_of_birth
+      if patient.address
+        expect(output).to_not match(/\b#{patient.address.street_address}\b/)
+        expect(output).to_not match(/\b#{patient.address.village_or_colony}\b/)
       end
     end
-
   end
 
+  it "only logs whitelisted parameters and filters out everything else" do
+    stringio = StringIO.new
+    test_logger = Logger.new(stringio)
+    allow(ActionController::Base).to receive(:logger).and_return(test_logger)
+
+    time = Time.parse("January 1 2020 12:00 UTC")
+    Timecop.freeze(time) do
+      patient_payloads = (1..3).map { build_payload.call }
+      post sync_route, params: {patients: patient_payloads}.to_json, headers: headers
+      get sync_route, params: {}, headers: headers
+
+      patient_payloads.each do |payload|
+        check_payload(stringio.string, payload)
+      end
+    end
+  end
+
+  def check_payload(output, payload)
+    payload.each do |attr, value|
+      next if value.blank?
+
+      regex = nil
+      case value
+      when Hash
+        check_payload(output, value)
+        next
+      when Array
+        value.each { |v| check_payload(output, v) }
+        next
+      when ActiveSupport::TimeWithZone
+        # We have to search for a substring of the timestamp, because the log can contain a more precise
+        # timestamp than the parameters. So we strip off the Timezone info and search for that.
+        regex = /\b#{value.iso8601[0..-2]}/
+      else
+        regex = /\b#{value}\b/
+      end
+
+      if ParameterFiltering::WHITELIST_REGEX.match(attr)
+        expect(output).to match(regex), "#{output}\n\nLog output should include #{value.inspect} for attribute #{attr}"
+      else
+        expect(output).to_not match(regex), "#{output}\n\nLog output should not include #{value.inspect} for attribute #{attr}"
+      end
+    end
+  end
 end
