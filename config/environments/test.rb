c5d2d76ee6d936cbe11b22c5c6eb09110e01ea02
Akshay Gupta <kitallis@gmail.com>
2020-05-21 15:36:34 +0530

    Add a few notes around standardb + fix a lint error

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,6 @@ Rails.application.configure do
 
   config.active_support.deprecation = :raise
 
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
