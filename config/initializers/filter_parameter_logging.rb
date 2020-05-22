a99ab00f7e28d76e10716590b03846059a47c24c
Rob Sanheim <rsanheim@gmail.com>
2020-05-22 14:34:42 -0500

    refactor the lambda and explain this approach w/ a comment

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,3 +1,7 @@
+# We are using a strict approach to parametering filtering for our logs
+# to avoid exposing any patient information there. So any attributes
+# not explicitly allowed below will be filtered out from the logs.
+#
 # Be sure to restart your server when you modify this file.
 module ParameterFiltering
   ALLOWED_ATTRIBUTES = %w[
@@ -17,16 +21,20 @@ module ParameterFiltering
 
   ALLOWED_REGEX = /(^|_)ids?|#{Regexp.union(ALLOWED_ATTRIBUTES)}/.freeze
   SANITIZED_VALUE = "[FILTERED]".freeze
-end
 
-Rails.application.config.filter_parameters += [:password, :age]
-Rails.application.config.filter_parameters << lambda do |key, value|
-  unless key.match(ParameterFiltering::ALLOWED_REGEX)
-    case value
-    when String
-      value.replace(ParameterFiltering::SANITIZED_VALUE)
-    else
-      value.to_s.replace(ParameterFiltering::SANITIZED_VALUE)
+  # Returns the lamba for attributes that are okay to leave in the logs
+  def self.filter
+    lambda do |key, value|
+      unless key.match(ALLOWED_REGEX)
+        case value
+        when String
+          value.replace(SANITIZED_VALUE)
+        else
+          value.to_s.replace(SANITIZED_VALUE)
+        end
+      end
     end
   end
 end
+
+Rails.application.config.filter_parameters += [:password, :age, ParameterFiltering.filter]
