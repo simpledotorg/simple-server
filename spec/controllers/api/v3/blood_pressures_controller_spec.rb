53ff3bec4aecbe4400b6ccc071e5d2661c0a2418
Rob Sanheim <rsanheim@gmail.com>
2020-05-27 18:05:44 -0500

    focused specs while WIP

--- spec/controllers/api/v3/blood_pressures_controller_spec.rb
+++ spec/controllers/api/v3/blood_pressures_controller_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Api::V3::BloodPressuresController, type: :controller do
       end
 
       describe "updates records" do
-        it "with updated record attributes" do
+        fit "with updated record attributes" do
           post :sync_from_user, params: updated_payload, as: :json
 
           updated_records.each do |record|
@@ -67,7 +67,7 @@ RSpec.describe Api::V3::BloodPressuresController, type: :controller do
         request.env["HTTP_AUTHORIZATION"] = "Bearer #{request_user.access_token}"
       end
 
-      it "creates new blood pressures with associated patient" do
+      fit "creates new blood pressures with associated patient" do
         patient = FactoryBot.create(:patient)
         blood_pressures = (1..3).map {
           build_blood_pressure_payload(FactoryBot.build(:blood_pressure, patient: patient))
