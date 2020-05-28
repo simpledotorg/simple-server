fdb599292fa21e726d251dc8fdfd7e3b1c0d6551
Akshay Gupta <kitallis@gmail.com>
2020-05-28 17:00:47 +0530

    Fix some formatting

--- app/models/email_authentication.rb
+++ app/models/email_authentication.rb
@@ -11,7 +11,7 @@ class EmailAuthentication < ApplicationRecord
 
   delegate :full_name, :resources, :role, :organization, to: :user, allow_nil: true
 
-  validates :password, password_strength: { use_dictionary: true }, allow_nil: true
+  validates :password, password_strength: {use_dictionary: true}, allow_nil: true
 
   after_validation :strip_unnecessary_errors
 
