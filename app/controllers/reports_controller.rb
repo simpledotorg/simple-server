16967dc85b931943a3970863834308239b3599a5
Rob Sanheim <rsanheim@gmail.com>
2020-05-28 11:38:25 -0500

    Add example for quarterly cohort data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,5 +9,14 @@ class ReportsController < ApplicationController
       "Jan 2017" => 323423,
       "Feb 2017" => 23423,
     }
+    @quarterly_registrations = {
+      "Q2-2019" => {
+        "visisted_and_controlled" => 324234,
+        "visisted_and_uncontrolled" => 23423,
+        "no_bp_measure" => 234234
+      }
+
+
+    }
   end
 end
