41e78e035fddc0d7068a97ed9365541085ebb552
Akshay Gupta <kitallis@gmail.com>
2020-05-28 21:03:29 +0530

    Make all indexes concurrent

--- db/migrate/20200527182353_add_search_indices_to_users.rb
+++ db/migrate/20200527182353_add_search_indices_to_users.rb
@@ -1,8 +1,10 @@
 class AddSearchIndicesToUsers < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def up
-    execute "CREATE INDEX index_gin_users_on_full_name ON users USING GIN ((to_tsvector('simple'::regconfig, COALESCE((full_name)::text, ''::text))));"
-    execute "CREATE INDEX index_gin_phone_number_authentications_on_phone_number ON phone_number_authentications USING GIN ((to_tsvector('simple'::regconfig, COALESCE((phone_number)::text, ''::text))));"
-    execute "CREATE INDEX index_gin_email_authentications_on_email ON email_authentications USING GIN ((to_tsvector('simple'::regconfig, COALESCE((email)::text, ''::text))));"
+    execute "CREATE INDEX CONCURRENTLY index_gin_users_on_full_name ON users USING GIN ((to_tsvector('simple'::regconfig, COALESCE((full_name)::text, ''::text))));"
+    execute "CREATE INDEX CONCURRENTLY index_gin_phone_number_authentications_on_phone_number ON phone_number_authentications USING GIN ((to_tsvector('simple'::regconfig, COALESCE((phone_number)::text, ''::text))));"
+    execute "CREATE INDEX CONCURRENTLY index_gin_email_authentications_on_email ON email_authentications USING GIN ((to_tsvector('simple'::regconfig, COALESCE((email)::text, ''::text))));"
   end
 
   def down
--- db/migrate/20200528113707_add_search_index_to_facility.rb
+++ db/migrate/20200528113707_add_search_index_to_facility.rb
@@ -1,7 +1,9 @@
 class AddSearchIndexToFacility < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
   def up
-    execute "CREATE INDEX index_gin_facilities_on_name ON facilities USING GIN ((to_tsvector('simple'::regconfig, COALESCE((name)::text, ''::text))));"
-    execute "CREATE INDEX index_gin_facilities_on_slug ON facilities USING GIN ((to_tsvector('simple'::regconfig, COALESCE((slug)::text, ''::text))));"
+    execute "CREATE INDEX CONCURRENTLY index_gin_facilities_on_name ON facilities USING GIN ((to_tsvector('simple'::regconfig, COALESCE((name)::text, ''::text))));"
+    execute "CREATE INDEX CONCURRENTLY index_gin_facilities_on_slug ON facilities USING GIN ((to_tsvector('simple'::regconfig, COALESCE((slug)::text, ''::text))));"
   end
 
   def down
