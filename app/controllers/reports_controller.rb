f9d8517b7e8144db58ccef5a18138e621f9f07e1
Rob Sanheim <rsanheim@gmail.com>
2020-05-28 11:28:52 -0500

    Add example for sending down hash to view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,13 @@
 class ReportsController < ApplicationController
   layout "reports"
   def index
-    data = [
-      { value: 0, date: "Jan 2017" },
-      { value: 50, date: "Feb 2017" },
-      { value: 125, date: "Mar 2017" },
-      { value: 170, date: "Apr 2017" },
-      { value: 210, date: "May 2017" },
-      { value: 260, date: "Jun 2017" },
-      { value: 300, date: "Jul 2017" },
-      { value: 310, date: "Aug 2017" },
-      { value: 350, date: "Sep 2017" },
-      { value: 370, date: "Oct 2017" },
-      { value: 400, date: "Nov 2017" },
-      { value: 425, date: "Dec 2017" },
-    ]
+    @controlled_patients = {
+      "Jan 2017" => 323423,
+      "Feb 2017" => 23423,
+    }
+    @registrations = {
+      "Jan 2017" => 323423,
+      "Feb 2017" => 23423,
+    }
   end
 end
