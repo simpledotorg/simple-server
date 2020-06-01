c26c22fbd408450d164c76d70054f0998f8180ae
Akshay Gupta <kitallis@gmail.com>
2020-06-01 13:54:30 +0530

    Add searchability to User & Facility (#990)
    We want to be able to search for users/admins/facilities on the dashboards.
    
    This only contain search scopes on relevant models, the actual functionality from the UI will come as a separate commit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include PgSearch::Model
+
   AUTHENTICATION_TYPES = {
     email_authentication: 'EmailAuthentication',
     phone_number_authentication: 'PhoneNumberAuthentication'
@@ -35,6 +37,14 @@ class User < ApplicationRecord
   has_many :prescription_drugs
 
   has_many :user_permissions, foreign_key: :user_id, dependent: :delete_all
+
+  pg_search_scope :search_by_name, against: [:full_name], using: {tsearch: {prefix: true, any_word: true}}
+  scope :search_by_email,
+    ->(term) { joins(:email_authentications).merge(EmailAuthentication.search_by_email(term)) }
+  scope :search_by_phone,
+    ->(term) { joins(:phone_number_authentications).merge(PhoneNumberAuthentication.search_by_phone(term)) }
+  scope :search_by_name_or_email, ->(term) { search_by_name(term).union(search_by_email(term)) }
+  scope :search_by_name_or_phone, ->(term) { search_by_name(term).union(search_by_phone(term)) }
 
   validates :full_name, presence: true
   validates :role, presence: true, if: -> { email_authentication.present? }
