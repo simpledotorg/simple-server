c6f40279ad0e21fe523e9146bd9aeee9e0d8d4be
claudiovallejo <hola@claudiovallejo.mx>
2020-06-02 18:24:09 -0400

    Removing webpack, Keeping postcss and tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
-    require('autoprefixer'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
+    require('tailwindcss'),
+    require('autoprefixer')
   ]
 }
