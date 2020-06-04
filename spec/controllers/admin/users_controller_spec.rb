95640728e4152b5a9d520d1f8d364754f1917bb7
Akshay Gupta <kitallis@gmail.com>
2020-06-04 14:50:22 +0530

    Address feedback

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Admin::UsersController, type: :controller do
 
   describe 'GET #index' do
     it 'returns a success response' do
-      _user = create(:user)
+      create(:user)
       get :index, params: { facility_id: facility.id }
       expect(response).to be_successful
     end
