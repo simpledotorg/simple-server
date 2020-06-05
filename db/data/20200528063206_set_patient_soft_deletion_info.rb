70db9826db281fed8c1210f5d4a8b40a8310c6b9
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-06-05 13:41:04 +0530

    Upcoming help translations batch 2 (#1027)
    **Story card:** https://www.pivotaltracker.com/story/show/173146051
    
    This PR introduces another batch of help screen translations for a bunch of languages. Here's a summary of changes.
    
    Brand new help screen translations with the new HELP design:
    * pa
    * mr
    * kn_IN
    * am_ET
    * om_ET
    * ti_ET
    
    Escape character fixes in existing translations:
    * bn_BD
    
    **Note:** I have left the unused `upcoming/` files untouched for now, since I don't wanna mess up the Transifex integration.

--- db/data/20200528063206_set_patient_soft_deletion_info.rb
+++ db/data/20200528063206_set_patient_soft_deletion_info.rb
@@ -2,10 +2,10 @@ class SetPatientSoftDeletionInfo < ActiveRecord::Migration[5.2]
   def up
     bot_user = User.find_or_create_by!(full_name: "Data Migration Bot",
                                        sync_approval_status: "denied",
-                                       sync_approval_status_reason: "Bot user doesn't require sync") do |user|
+                                       sync_approval_status_reason: "Bot user doesn't require sync") { |user|
       user.device_created_at = Time.current
       user.device_updated_at = Time.current
-    end
+    }
 
     Patient.with_discarded.discarded.in_batches(of: 1_000) do |batch|
       batch.update_all(deleted_by_user_id: bot_user.id, deleted_reason: "unknown")
