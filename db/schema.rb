d01f299bf203af6f43c8f414293c7e3be6a1a9df
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-06-05 16:07:23 +0530

    Configure Flipper ActiveRecord adapter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_113844) do
+ActiveRecord::Schema.define(version: 2020_06_05_103543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -242,6 +242,22 @@ ActiveRecord::Schema.define(version: 2020_05_28_113844) do
     t.index ["organization_id"], name: "index_facility_groups_on_organization_id"
     t.index ["protocol_id"], name: "index_facility_groups_on_protocol_id"
     t.index ["slug"], name: "index_facility_groups_on_slug", unique: true
+  end
+
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
   end
 
   create_table "medical_histories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
