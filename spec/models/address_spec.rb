531d40d235d7af629133483fda72515d9143a8c4
Rob Sanheim <rsanheim@gmail.com>
2020-06-16 13:51:56 -0500

    standardrb --fix for models and specs (#1074)
    * standardrb --fix for models and specs
    
    * Manually resolve standardrb complaints
    
    * Remove global includes; use hash_uuid as a static method:
    
    also remove a couple useless local vars
    
    * Few more standardrb fixes:
    
    remove a global include; remove unused local vars

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Address, type: :model do
-  describe 'Validations' do
-    it_behaves_like 'a record that validates device timestamps'
+  describe "Validations" do
+    it_behaves_like "a record that validates device timestamps"
   end
 
-  describe 'Behavior' do
-    it_behaves_like 'a record that is deletable'
+  describe "Behavior" do
+    it_behaves_like "a record that is deletable"
   end
 
-  describe '.has_street_address?' do
+  describe ".has_street_address?" do
     let!(:address_without_street_address) { create(:address, :no_street_address) }
     let!(:address_with_street_address) { create(:address) }
 
-    it 'should return false for an address without a street address' do
+    it "should return false for an address without a street address" do
       expect(address_without_street_address.has_street_address?).to be_falsey
     end
 
-    it 'should return true for an address with a street address' do
+    it "should return true for an address with a street address" do
       expect(address_with_street_address.has_street_address?).to be_truthy
     end
   end
