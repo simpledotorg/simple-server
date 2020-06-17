cc1a4f6367efadb3c49fcf2052bf32978f6be1d4
Rob Sanheim <rsanheim@gmail.com>
2020-06-17 09:15:59 -0500

    Keep queries together

--- app/services/district_report_service.rb
+++ app/services/district_report_service.rb
@@ -20,22 +20,6 @@ class DistrictReportService
     data
   end
 
-  def controlled_patients(time)
-    end_range = time.end_of_month
-    mid_range = time.advance(months: -1).end_of_month
-    beg_range = time.advance(months: -2).end_of_month
-    sub_query = LatestBloodPressuresPerPatientPerMonth
-      .select("distinct on (patient_id) *")
-      .under_control
-      .order("patient_id, bp_recorded_at DESC, bp_id")
-      .where(registration_facility_id: facilities)
-      .where("(year = ? AND month = ?) OR (year = ? AND month = ?) OR (year = ? AND month = ?)",
-        beg_range.year.to_s, beg_range.month.to_s,
-        mid_range.year.to_s, mid_range.month.to_s,
-        end_range.year.to_s, end_range.month.to_s)
-    LatestBloodPressuresPerPatientPerMonth.from(sub_query, "latest_blood_pressures_per_patient_per_months")
-  end
-
   def compile_control_and_registration_data
     -11.upto(0).each do |n|
       time = selected_date.advance(months: n)
@@ -76,8 +60,26 @@ class DistrictReportService
     end
   end
 
+  def controlled_patients(time)
+    end_range = time.end_of_month
+    mid_range = time.advance(months: -1).end_of_month
+    beg_range = time.advance(months: -2).end_of_month
+    sub_query = LatestBloodPressuresPerPatientPerMonth
+      .select("distinct on (patient_id) *")
+      .under_control
+      .order("patient_id, bp_recorded_at DESC, bp_id")
+      .where(registration_facility_id: facilities)
+      .where("(year = ? AND month = ?) OR (year = ? AND month = ?) OR (year = ? AND month = ?)",
+        beg_range.year.to_s, beg_range.month.to_s,
+        mid_range.year.to_s, mid_range.month.to_s,
+        end_range.year.to_s, end_range.month.to_s)
+    LatestBloodPressuresPerPatientPerMonth.from(sub_query, "latest_blood_pressures_per_patient_per_months")
+  end
+
   def registrations
     registrations_query ||= MyFacilities::RegistrationsQuery.new(facilities: @facilities, period: :month, last_n: 12)
     registrations_query.registrations.group(:year, :month).sum(:registration_count)
   end
+
+
 end
