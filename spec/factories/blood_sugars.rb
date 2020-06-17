cd93a0f154d52a87e38c2cf7bf462370092eeb6d
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-06-17 18:24:19 +0530

    Restore the :with_hba1c trait on the BloodSugar factory

--- spec/factories/blood_sugars.rb
+++ spec/factories/blood_sugars.rb
@@ -21,6 +21,14 @@ FactoryBot.define do
       end
     end
 
+    trait :with_hba1c do
+      blood_sugar_type { BloodSugar::blood_sugar_types.keys.sample }
+      blood_sugar_value do
+        threshold = BloodSugar::THRESHOLDS[:high][blood_sugar_type]
+        rand(threshold * 0.9..threshold * 1.1).round(2)
+      end
+    end
+
     trait :random do
       blood_sugar_type { "random" }
       blood_sugar_value { 160 }
