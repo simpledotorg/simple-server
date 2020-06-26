07cf1a825583ea07b6f372dc6024e8a9903af60b
Rob Sanheim <rsanheim@gmail.com>
2020-06-26 10:34:15 -0500

    Fix escaping in spec

--- spec/jobs/send_patient_otp_sms_job_spec.rb
+++ spec/jobs/send_patient_otp_sms_job_spec.rb
@@ -4,7 +4,7 @@ require "sidekiq/testing"
 RSpec.describe SendPatientOtpSmsJob, type: :job do
   let!(:passport_authentication) { create(:passport_authentication) }
   let(:app_signature) { ENV["SIMPLE_APP_SIGNATURE"] }
-  let(:otp_message) { "<#> #{passport_authentication.otp} is your BP Passport verification code\\n#{app_signature}" }
+  let(:otp_message) { "<#> #{passport_authentication.otp} is your BP Passport verification code\n#{app_signature}" }
 
   before do
     allow_any_instance_of(NotificationService).to receive(:send_sms)
