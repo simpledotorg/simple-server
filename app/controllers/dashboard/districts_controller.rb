557d15162681b4a66b2ffe0449c1bbcc5ffe9838
Rob Sanheim <rsanheim@gmail.com>
2020-07-01 13:49:57 -0500

    Fix up controller and spec

--- app/controllers/dashboard/districts_controller.rb
+++ app/controllers/dashboard/districts_controller.rb
@@ -23,7 +23,7 @@ class Dashboard::DistrictsController < AdminController
     end
     @data = DistrictReportService.new(facilities: @district.facilities,
                                       selected_date: @selected_date,
-                                      current_user: current_user).call
+                                      current_user: current_admin).call
     @controlled_patients = @data[:controlled_patients]
     @registrations = @data[:registrations]
     @quarterly_registrations = @data[:quarterly_registrations]
