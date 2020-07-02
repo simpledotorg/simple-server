9fd1d5b6cec1a9a2be81c44e59c4306dfbddb443
Tim Cheadle <tcheadle@resolvetosavelives.org>
2020-07-02 17:24:30 -0400

    Cleaned up AppointmentNotificationService spec

--- app/jobs/appointment_notification/missed_visit_job.rb
+++ app/jobs/appointment_notification/missed_visit_job.rb
@@ -6,11 +6,14 @@ class AppointmentNotification::MissedVisitJob
   def perform
     return unless FeatureToggle.enabled?('APPOINTMENT_REMINDERS')
 
-    Organization.where(id: ENV['APPOINTMENT_NOTIFICATION_ORG_IDS'].split(',')).each do |organization|
-      appointments = Appointment.includes(facility: { facility_group: :organization })
-                       .where(facility: { facility_groups: { organization: organization } })
-
-      AppointmentNotificationService.send_after_missed_visit(appointments: appointments)
+    enabled_organizations.each do |organization|
+      AppointmentNotificationService.send_after_missed_visit(appointments: organization.appointments)
     end
+  end
+
+  private
+
+  def enabled_organizations
+    Organization.where(id: ENV['APPOINTMENT_NOTIFICATION_ORG_IDS'].split(','))
   end
 end
