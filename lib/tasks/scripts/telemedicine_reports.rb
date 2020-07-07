450dfb75cd59c6960ecf7b085e6a70d0fb105576
vkrm <mail@vkrm.is>
2020-07-07 21:36:40 +0530

    Add facility level data to the report

--- lib/tasks/scripts/telemedicine_reports.rb
+++ lib/tasks/scripts/telemedicine_reports.rb
@@ -10,13 +10,13 @@ module TelemedicineReports
 
       mixpanel.drop(1).map { |row|
         facility = User.find(row[1]).registration_facility
-        {user_id: row[1],
-         date: Date.parse(row[2]),
-         clicks: [row[3].to_i, row[4].to_i].max,
-         facility_id: facility.id,
-         district: facility.district,
-         state: facility.state,
-         type: facility.facility_type}
+        { user_id: row[1],
+          date: Date.parse(row[2]),
+          clicks: [row[3].to_i, row[4].to_i].max,
+          facility_id: facility.id,
+          district: facility.district,
+          state: facility.state,
+          type: facility.facility_type }
       }
     end
 
@@ -25,19 +25,21 @@ module TelemedicineReports
       p2_mixpanel_data = format_mixpanel_data(mixpanel_data.select { |row| (row[:date] >= p2_start) && (row[:date] <= p2_end) })
 
       facilities = Facility.where(enable_teleconsultation: true).map { |facility|
-        if facility.facility_type == "HWC" || facility.facility_type == "SC"
-          {id: facility.id,
-           state: facility.state,
-           district: facility.district,
-           type: facility.facility_type,
-           p1: facility_measures(facility, p1_start, p1_end),
-           p2: facility_measures(facility, p2_start, p2_end),
-           users: facility.users.count}
+        if (facility.facility_type == "HWC" || facility.facility_type == "SC")
+          { id: facility.id,
+            name: facility.name,
+            state: facility.state,
+            district: facility.district,
+            type: facility.facility_type,
+            p1: facility_measures(facility, p1_start, p1_end),
+            p2: facility_measures(facility, p2_start, p2_end),
+            users: facility.users.count }
         else
-          {id: facility.id,
-           state: facility.state,
-           district: facility.district,
-           type: facility.facility_type}
+          { id: facility.id,
+            name: facility.name,
+            state: facility.state,
+            district: facility.district,
+            type: facility.facility_type }
         end
       }
 
@@ -51,7 +53,9 @@ module TelemedicineReports
           "",
           "",
           "",
+          "",
           "Between #{p1_start.strftime("%d %b %Y")} and #{p1_end.strftime("%d %b %Y")}",
+          "",
           "",
           "",
           "",
@@ -68,6 +72,7 @@ module TelemedicineReports
         csv << [
           "State",
           "District",
+          "Facility",
           "Facilities with TM",
           "HWCs & SCs with TM",
           "Users at HWCs & SCs",
@@ -92,6 +97,7 @@ module TelemedicineReports
           p2_clicks = fetch_clicks(p2_mixpanel_data, state, true)
           csv << [
             state[:state],
+            "",
             "",
             state[:count],
             state[:hwc_and_sc],
@@ -118,6 +124,7 @@ module TelemedicineReports
             csv << [
               "",
               district[:district],
+              "",
               district[:count],
               district[:hwc_and_sc],
               district[:users],
@@ -136,6 +143,31 @@ module TelemedicineReports
               p2_clicks,
               percentage(p2_clicks, district[:p2][:high_bp_or_bs])
             ]
+
+            district[:facilities].each do |facility|
+              csv << [
+                "",
+                "",
+                facility[:name],
+                "",
+                "",
+                facility[:users],
+                "",
+                facility[:p1][:visits],
+                facility[:p1][:high_bp],
+                facility[:p1][:high_bs],
+                facility[:p1][:high_bp_or_bs],
+                "",
+                "",
+                "",
+                facility[:p2][:visits],
+                facility[:p2][:high_bp],
+                facility[:p2][:high_bs],
+                facility[:p2][:high_bp_or_bs],
+                "",
+                ""
+              ]
+            end
           end
         end
 
@@ -157,32 +189,33 @@ module TelemedicineReports
 
     def format_mixpanel_data(period_data)
       period_data.group_by { |row| row[:state] }.map { |state, districts|
-        {state: state,
-         clicks: sum_rows(districts, :clicks),
-         districts: districts.group_by { |row| row[:district] }.map { |district, users|
-                      {district: district,
-                       clicks: sum_rows(users, :clicks)}
-                    }}
+        { state: state,
+          clicks: sum_rows(districts, :clicks),
+          districts: districts.group_by { |row| row[:district] }.map { |district, users|
+            { district: district,
+              clicks: sum_rows(users, :clicks) }
+          } }
       }
     end
 
     def format_facility_data(facility_data)
       facility_data.group_by { |row| row[:state] }.map { |state, districts|
-        {state: state,
-         count: districts.count,
-         hwc_and_sc: hwc_and_sc_count(districts),
-         p1: aggregate_period(:p1, districts),
-         p2: aggregate_period(:p2, districts),
-         users: sum_rows(districts, :users),
-         districts: districts.group_by { |row| row[:district] }.map { |district, facilities|
-           {district: district,
-            state: state,
-            count: facilities.count,
-            hwc_and_sc: hwc_and_sc_count(facilities),
-            p1: aggregate_period(:p1, facilities),
-            p2: aggregate_period(:p2, facilities),
-            users: sum_rows(facilities, :users)}
-         }.sort_by { |district| district[:district] }}
+        { state: state,
+          count: districts.count,
+          hwc_and_sc: hwc_and_sc_count(districts),
+          p1: aggregate_period(:p1, districts),
+          p2: aggregate_period(:p2, districts),
+          users: sum_rows(districts, :users),
+          districts: districts.group_by { |row| row[:district] }.map { |district, facilities|
+            { district: district,
+              state: state,
+              count: facilities.count,
+              hwc_and_sc: hwc_and_sc_count(facilities),
+              p1: aggregate_period(:p1, facilities),
+              p2: aggregate_period(:p2, facilities),
+              users: sum_rows(facilities, :users),
+              facilities: facilities.select { |facility| %w[HWC SC].include? facility[:type] }.sort_by { |facility| facility[:name] } }
+          }.sort_by { |district| district[:district] } }
       }.sort_by { |state| state[:state] }
     end
 
@@ -203,10 +236,10 @@ module TelemedicineReports
       high_bps = high_bps(bps)
       high_sugars = high_sugars(sugars)
 
-      {high_bp: high_bps.count,
-       high_bs: high_sugars.count,
-       high_bp_or_bs: (high_bps + high_sugars).uniq { |record| record[:patient_id] }.count,
-       visits: visits.count}
+      { high_bp: high_bps.count,
+        high_bs: high_sugars.count,
+        high_bp_or_bs: (high_bps + high_sugars).uniq { |record| record[:patient_id] }.count,
+        visits: visits.count }
     end
 
     def sum_rows(rows, key)
@@ -219,10 +252,10 @@ module TelemedicineReports
 
     def aggregate_period(period, facilities)
       hwc_and_sc_data = facilities.map { |facility| facility[period] }
-      {high_bp: sum_rows(hwc_and_sc_data, :high_bp),
-       high_bs: sum_rows(hwc_and_sc_data, :high_bs),
-       high_bp_or_bs: sum_rows(hwc_and_sc_data, :high_bp_or_bs),
-       visits: sum_rows(hwc_and_sc_data, :visits)}
+      { high_bp: sum_rows(hwc_and_sc_data, :high_bp),
+        high_bs: sum_rows(hwc_and_sc_data, :high_bs),
+        high_bp_or_bs: sum_rows(hwc_and_sc_data, :high_bp_or_bs),
+        visits: sum_rows(hwc_and_sc_data, :visits) }
     end
 
     def fetch_clicks(data, record, is_state)
