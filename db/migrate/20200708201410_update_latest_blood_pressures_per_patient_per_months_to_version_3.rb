3a533cc7a61bdb805086faec1595a8990b124abe
Rob Sanheim <rsanheim@gmail.com>
2020-07-08 15:35:44 -0500

    Add patient_status and medical_history_hypertension to latest BP per month view

---
+++ db/migrate/20200708201410_update_latest_blood_pressures_per_patient_per_months_to_version_3.rb
@@ -0,0 +1,11 @@
+class UpdateLatestBloodPressuresPerPatientPerMonthsToVersion3 < ActiveRecord::Migration[5.2]
+  def change
+    drop_view :latest_blood_pressures_per_patient_per_quarters, materialized: true, revert_to_version: 2
+    drop_view :latest_blood_pressures_per_patients, materialized: true, revert_to_version: 2
+
+    update_view :latest_blood_pressures_per_patient_per_months, version: 3, revert_to_version: 2, materialized: true
+
+    create_view :latest_blood_pressures_per_patient_per_quarters, materialized: true
+    create_view :latest_blood_pressures_per_patients, materialized: true
+  end
+end
