5adea26363d5f96cff111810e1c55f39a85ce356
Akshay Gupta <kitallis@gmail.com>
2020-07-09 15:04:14 +0530

    Update assigned_facility_id only for patients who have it as nil

--- db/data/20200703072612_set_patient_assigned_facility.rb
+++ db/data/20200703072612_set_patient_assigned_facility.rb
@@ -1,12 +1,12 @@
 class SetPatientAssignedFacility < ActiveRecord::Migration[5.2]
   def up
-    Patient.in_batches(of: 10_000) do |batch|
+    Patient.where(assigned_facility_id: nil).in_batches(of: 10_000) do |batch|
       batch.update_all("assigned_facility_id = registration_facility_id")
     end
   end
 
   def down
-    Patient.in_batches(of: 10_000) do |batch|
+    Patient.where.not(assigned_facility_id: nil).in_batches(of: 10_000) do |batch|
       batch.update_all(assigned_facility_id: nil)
     end
   end
