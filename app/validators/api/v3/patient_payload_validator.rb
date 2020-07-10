df892ad066b96e49e49387cf7b137894232344b1
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-07-10 16:35:47 +0530

    Refactor guard clause

--- app/validators/api/v3/patient_payload_validator.rb
+++ app/validators/api/v3/patient_payload_validator.rb
@@ -68,7 +68,8 @@ class Api::V3::PatientPayloadValidator < Api::V3::PayloadValidator
   private
 
   def authorized_facility?(facility_id)
-    return if facility_id.blank?
+    return true if facility_id.blank?
+
     request_user.present? && request_user.facility.facility_group.facilities.where(id: facility_id).present?
   end
 
