b902b6701b943d4cf8eab63e8a0db226f544856a
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-07-10 16:46:17 +0530

    Utilize a tested association to connect org -> appointments

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,7 @@ class Organization < ApplicationRecord
 
   has_many :facility_groups, dependent: :destroy
   has_many :facilities, through: :facility_groups
+  has_many :appointments, through: :facilities
   has_many :users
   has_many :protocols, through: :facility_groups
 
@@ -14,9 +15,5 @@ class Organization < ApplicationRecord
 
   def districts
     facilities.select(:district).distinct.pluck(:district)
-  end
-
-  def appointments
-    Appointment.where(facility: facilities)
   end
 end
