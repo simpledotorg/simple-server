b664c5e8e82fa9e95b499533605904de4f02e5f4
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-07-16 14:33:12 +0530

    Thanks standard hyphen fix

--- db/migrate/20200716084425_add_indexes_to_my_facilities_views_2.rb
+++ db/migrate/20200716084425_add_indexes_to_my_facilities_views_2.rb
@@ -1,8 +1,8 @@
 class AddIndexesToMyFacilitiesViews2 < ActiveRecord::Migration[5.1]
   def change
     add_index :latest_blood_pressures_per_patient_per_quarters, :bp_id, unique: true,
-              name: "index_latest_blood_pressures_per_patient_per_quarters"
+                                                                        name: "index_latest_blood_pressures_per_patient_per_quarters"
     add_index :latest_blood_pressures_per_patients, :bp_id, unique: true,
-              name: "index_latest_blood_pressures_per_patients"
+                                                            name: "index_latest_blood_pressures_per_patients"
   end
 end
