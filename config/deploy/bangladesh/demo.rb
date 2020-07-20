ceed63a53a0671e6c27fbffe7d5ea86f6d42145e
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-07-20 17:13:02 +0530

    Update BD Demo server IP

--- config/deploy/bangladesh/demo.rb
+++ config/deploy/bangladesh/demo.rb
@@ -1,2 +1,2 @@
-server "ec2-13-127-62-208.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron seed_data]
+server "ec2-13-233-166-168.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron seed_data]
 server "ec2-3-6-91-15.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
