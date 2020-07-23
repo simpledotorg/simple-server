b488a0c26abf863d7f1871908c0c88d8a3aded00
Rob Sanheim <rsanheim@gmail.com>
2020-07-23 12:41:50 -0500

    Better formatting

--- app/models/role_scoped_regions.rb
+++ app/models/role_scoped_regions.rb
@@ -35,37 +35,36 @@ module RoleScopedRegions
       ["super_admin", "admin", "analyst"]
     end
     sql = <<-SQL
-      SELECT facilities.* FROM facilities
-      INNER JOIN facility_groups
-        ON facility_groups.id = facilities.facility_group_id
-      INNER JOIN organizations
-        ON facility_groups.organization_id = organizations.id
-      INNER JOIN roles
-        ON roles.resource_type = 'Organization'
-        AND roles.resource_id = organizations.id
-      WHERE roles.user_id = :user_id
-        AND roles.name in (:roles)
+    SELECT facilities.*
+    FROM facilities
+      INNER JOIN facility_groups ON facility_groups.id = facilities.facility_group_id
+      INNER JOIN organizations ON facility_groups.organization_id = organizations.id
+      INNER JOIN roles ON roles.resource_type = 'Organization'
+      AND roles.resource_id = organizations.id
+    WHERE
+      roles.user_id = :user_id
+      AND roles.name in (:roles)
 
-      UNION
+    UNION
 
-      SELECT facilities.* FROM facilities
-      INNER JOIN facility_groups
-        ON facility_groups.id = facilities.facility_group_id
-      INNER JOIN roles
-        ON roles.resource_type = 'FacilityGroup'
-        AND facility_groups.id = roles.resource_id
-      WHERE roles.user_id = :user_id
-        AND roles.name in (:roles)
+    SELECT facilities.*
+    FROM facilities
+      INNER JOIN facility_groups ON facility_groups.id = facilities.facility_group_id
+      INNER JOIN roles ON roles.resource_type = 'FacilityGroup'
+      AND facility_groups.id = roles.resource_id
+    WHERE
+      roles.user_id = :user_id
+      AND roles.name in (:roles)
 
-      UNION
+    UNION
 
-      SELECT facilities.* FROM facilities
-      INNER JOIN roles
-        ON roles.resource_type = 'Facility'
-        AND roles.resource_id = facilities.id
-      WHERE roles.user_id = :user_id
-        AND roles.name in (:roles)
-
+    SELECT facilities.*
+    FROM facilities
+      INNER JOIN roles ON roles.resource_type = 'Facility'
+      AND roles.resource_id = facilities.id
+    WHERE
+      roles.user_id = :user_id
+      AND roles.name in (:roles)
     SQL
     ids = Facility.find_by_sql([sql, {roles: roles, user_id: id}]).pluck(:id)
     Facility.where(id: ids)
