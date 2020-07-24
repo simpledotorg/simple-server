1b63ae7badfd8d08cb45bfebd15447367eaa129a
Rob Sanheim <rsanheim@gmail.com>
2020-07-24 17:03:50 -0500

    happy path specs

--- spec/controllers/reports/regions_controller_spec.rb
+++ spec/controllers/reports/regions_controller_spec.rb
@@ -16,6 +16,56 @@ RSpec.describe Reports::RegionsController, type: :controller do
     end
   end
 
+  context "details" do
+    render_views
+
+    before do
+      @facility_group = create(:facility_group, organization: organization)
+      @facility = create(:facility, name: "CHC Barnagar", facility_group: @facility_group)
+    end
+
+    it "is successful" do
+      jan_2020 = Time.parse("January 1 2020")
+      patient = create(:patient, registration_facility: @facility, recorded_at: jan_2020.advance(months: -1))
+      create(:blood_pressure, :under_control, recorded_at: jan_2020.advance(months: -1), patient: patient, facility: @facility)
+      create(:blood_pressure, :hypertensive, recorded_at: jan_2020, facility: @facility)
+      LatestBloodPressuresPerPatient.refresh
+      LatestBloodPressuresPerPatientPerMonth.refresh
+
+      Timecop.freeze("June 1 2020") do
+        sign_in(cvho.email_authentication)
+        get :details, params: {id: @facility.facility_group.slug, report_scope: "facility_group"}
+      end
+      expect(response).to be_successful
+      data = assigns(:data)
+    end
+  end
+
+  context "cohort" do
+    render_views
+
+    before do
+      @facility_group = create(:facility_group, organization: organization)
+      @facility = create(:facility, name: "CHC Barnagar", facility_group: @facility_group)
+    end
+
+    it "is successful" do
+      jan_2020 = Time.parse("January 1 2020")
+      patient = create(:patient, registration_facility: @facility, recorded_at: jan_2020.advance(months: -1))
+      create(:blood_pressure, :under_control, recorded_at: jan_2020.advance(months: -1), patient: patient, facility: @facility)
+      create(:blood_pressure, :hypertensive, recorded_at: jan_2020, facility: @facility)
+      LatestBloodPressuresPerPatient.refresh
+      LatestBloodPressuresPerPatientPerMonth.refresh
+
+      Timecop.freeze("June 1 2020") do
+        sign_in(cvho.email_authentication)
+        get :cohort, params: {id: @facility.facility_group.slug, report_scope: "facility_group"}
+      end
+      expect(response).to be_successful
+      data = assigns(:data)
+    end
+  end
+
   context "show" do
     render_views
 
