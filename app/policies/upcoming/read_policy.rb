10d242f5d607aba54b18a34e9aeffb18e90c5285
Akshay Gupta <kitallis@gmail.com>
2020-07-24 18:19:31 +0530

    Remove the upcoming pundit policies

--- app/policies/upcoming/read_policy.rb
+++
@@ -1,9 +0,0 @@
-class Upcoming::ReadPolicy < Struct.new(:user, :read)
-  def aggregates?
-    user.super_admin? || user.accesses.exists?
-  end
-
-  def identifiable_info?
-    user.super_admin? || user.accesses.admin.exists?
-  end
-end
