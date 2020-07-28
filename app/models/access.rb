798b3c331cab43bf36b934e41975fa177b7181d7
Akshay Gupta <kitallis@gmail.com>
2020-07-28 14:08:53 +0530

    WIP

--- app/models/access.rb
+++ app/models/access.rb
@@ -12,4 +12,38 @@ class Access < ApplicationRecord
 
   validates :mode, presence: true
   validates :resource_type, inclusion: {in: ALLOWED_RESOURCE_TYPES}, allow_nil: true
+
+  class << self
+    def organizations(action)
+      resources_for(Organization, roles_for(action))
+    end
+
+    def facility_groups(action)
+      resources_for(FacilityGroup, roles_for(action))
+        .or(FacilityGroup.where(organization: organizations(action)))
+    end
+
+    def facilities(action)
+      resources_for(Facility, roles_for(action))
+        .or(Facility.where(facility_group: facility_groups(action)))
+    end
+
+    private
+
+    def roles_for(action)
+      case action
+        when :view
+          [:manager, :viewer]
+        when :manage
+          [:manager]
+        else
+          raise ArgumentError, "Invalid action: #{action}"
+      end
+    end
+
+    def resources_for(scope, role)
+      return scope.all if super_admin?
+      scope.where(id: where(resource_type: scope.to_s, role: role).pluck(:resource_id))
+    end
+  end
 end
