d6fdccdbb7a93ec0d96fc095bb5aa6fe13226071
Akshay Gupta <kitallis@gmail.com>
2020-07-29 12:09:06 +0530

    Instead of inclusion:nil, use absence

--- app/models/access.rb
+++ app/models/access.rb
@@ -13,6 +13,6 @@ class Access < ApplicationRecord
   validates :mode, presence: true
   validates :user, uniqueness: {scope: [:resource_id, :resource_type], message: "can only have one access per resource."}
   validates :resource, presence: {unless: :super_admin?, message: "is required if not a super_admin."}
-  validates :resource, inclusion: {in: [nil], if: :super_admin?, message: "must be nil if super_admin"}
+  validates :resource, absence: {if: :super_admin?, message: "must be nil if super_admin"}
   validates :resource_type, inclusion: {in: ALLOWED_RESOURCES, unless: :super_admin?}
 end
