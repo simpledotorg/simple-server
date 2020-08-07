7c5b1955a436b138133413b7cb9c463c71b679f7
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-08-07 15:00:41 +0530

    Fix flaky spec

--- spec/exporters/patients_with_history_exporter_spec.rb
+++ spec/exporters/patients_with_history_exporter_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PatientsWithHistoryExporter do
 
   let!(:now) { Time.current }
 
-  let!(:patient) { create(:patient, status: "dead", address: create(:address, village: Faker::Address.city)) } # avoid same village and zone
+  let!(:patient) { create(:patient, status: "dead", address: create(:address, village_or_colony: Faker::Address.city)) } # avoid same village and zone
   let!(:facility) { patient.registration_facility }
   let!(:user) { patient.registration_user }
 
