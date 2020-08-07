140acab540063d1fb7dd3398fe0a491744a59267
vkrm <mail@vkrm.is>
2020-08-07 19:42:41 +0530

    standardrb fixes

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -7,16 +7,16 @@ class OrganizationsController < AdminController
     current_admin.can?(:view_reports, :facility_group)
 
     @users_requesting_approval = policy_scope([:manage, :user, User])
-                                   .requested_sync_approval
-                                   .order(updated_at: :desc)
+      .requested_sync_approval
+      .order(updated_at: :desc)
 
     @users_requesting_approval = paginate(@users_requesting_approval)
 
     @organizations = current_admin
-                       .accessible_facility_groups(:view_reports)
-                       .includes(:organization)
-                       .flat_map(&:organization)
-                       .uniq
-                       .sort_by(&:name)
+      .accessible_facility_groups(:view_reports)
+      .includes(:organization)
+      .flat_map(&:organization)
+      .uniq
+      .sort_by(&:name)
   end
 end
