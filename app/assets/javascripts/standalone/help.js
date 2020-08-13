a0859ccbeb9ff967a72c35cd4d0f2ae4a9513ea9
Akshay Gupta <kitallis@gmail.com>
2020-08-13 12:52:19 +0530

    Favor ES6 over ES5 syntax

--- app/assets/javascripts/standalone/help.js
+++ app/assets/javascripts/standalone/help.js
@@ -1,24 +1,23 @@
 // Hides and shows each page of Help
 
 function open_window(id) {
-   var element = document.getElementById(id);
+   const element = document.getElementById(id);
    element.style.display = 'block';
 }
 
 function close_window(id) {
-   var element = document.getElementById(id);
+   const element = document.getElementById(id);
    element.style.display = 'none';
 }
 
 // FAQ hides and shows answers
 window.onload = function() {
-    var acc = document.getElementsByClassName("faq");
-    var i;
+    const acc = document.getElementsByClassName("faq");
 
-    for (i = 0; i < acc.length; i++) {
+    for (let i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
         } else {
