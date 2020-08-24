315a3d7ba9be56a62ef52f156d1d37d5312a09f7
Dany Sam Salmon <danisam17@gmail.com>
2020-08-24 17:39:06 +0530

    Move polyfills to separate JS file

--- app/assets/javascripts/invite_admin.js
+++ app/assets/javascripts/invite_admin.js
@@ -94,18 +94,3 @@ function inviteAdmin() {
     updateParentCheckedState(targetCheckbox, SELECTOR)
   })
 }
-
-// Polyfill for IE
-if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest =
-    function (s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i,
-        el = this;
-      do {
-        i = matches.length;
-        while (--i >= 0 && matches.item(i) !== el) { };
-      } while ((i < 0) && (el = el.parentElement));
-      return el;
-    };
-}
