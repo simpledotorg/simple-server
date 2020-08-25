cf66ee1e86d8ff4227271aee02db0d67ecd3755f
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-08-25 21:28:24 +0530

    Standard

--- app/transformers/api/v4/user_transformer.rb
+++ app/transformers/api/v4/user_transformer.rb
@@ -5,8 +5,7 @@ class Api::V4::UserTransformer
         .merge("registration_facility_id" => user.registration_facility.id,
                "phone_number" => user.phone_number,
                "password_digest" => user.phone_number_authentication.password_digest,
-               "capabilities" => app_capabilities(user)
-        )
+               "capabilities" => app_capabilities(user))
         .except("otp", "otp_expires_at", "access_token", "logged_in_at", "role", "organization_id")
     end
 
