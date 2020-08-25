343ae5d862a55007749beb613bc9ca72d1faa3bf
Rob Sanheim <rsanheim@gmail.com>
2020-08-25 12:00:36 -0500

    Use explicit module syntax

--- app/services/reports/region_cache_warmer.rb
+++ app/services/reports/region_cache_warmer.rb
@@ -1,22 +1,24 @@
-class Reports::RegionCacheWarmer
-  def self.call
-    new.call
-  end
-
-  def initialize(period: RegionService.default_period)
-    @period = period
-  end
-
-  delegate :logger, to: Rails
-
-  def call
-    FacilityGroup.all.each do |region|
-      logger.info { "class=#{self.class.name} region=#{region.name}" }
-      Reports::RegionService.new(region: region, period: @period).call
+module Reports
+  class RegionCacheWarmer
+    def self.call
+      new.call
     end
-    Facility.all.each do |region|
-      logger.info { "class=#{self.class.name} region=#{region.name}" }
-      Reports::RegionService.new(region: region, period: @period).call
+
+    def initialize(period: RegionService.default_period)
+      @period = period
+    end
+
+    delegate :logger, to: Rails
+
+    def call
+      FacilityGroup.all.each do |region|
+        logger.info { "class=#{self.class.name} region=#{region.name}" }
+        RegionService.new(region: region, period: @period).call
+      end
+      Facility.all.each do |region|
+        logger.info { "class=#{self.class.name} region=#{region.name}" }
+        RegionService.new(region: region, period: @period).call
+      end
     end
   end
 end
