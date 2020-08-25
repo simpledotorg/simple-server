3a5c7649ffaaf37f33b6738e023588d6941ebebf
Dany Sam Salmon <danisam17@gmail.com>
2020-08-25 20:32:05 +0530

    Show only total number of facilties if user has access to all

--- app/helpers/admin_access_helper.rb
+++ app/helpers/admin_access_helper.rb
@@ -1,9 +1,13 @@
 module AdminAccessHelper
   def access_fraction(name, available, total)
-    "#{available} / #{total} #{name}"
+    if available == total
+      "#{total} #{name}"
+    else
+      "#{available} / #{total} #{name}"
+    end
   end
 
   def access_checkbox(form, name, resource)
-    form.check_box(name, {id: resource.id, class: 'access-input', label: "#{resource.name}"})
+    form.check_box(name, { id: resource.id, class: 'access-input', label: resource.name.to_s })
   end
 end
