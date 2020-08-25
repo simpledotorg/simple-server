256b08ff99dbebcc9c3ffc2efd25b224f222a3de
Rob Sanheim <rsanheim@gmail.com>
2020-08-25 12:21:25 -0500

    Ensure we maintain original force_cache state

--- app/services/reports/region_cache_warmer.rb
+++ app/services/reports/region_cache_warmer.rb
@@ -6,20 +6,24 @@ module Reports
 
     def initialize(period: RegionService.default_period)
       @period = period
+      @original_force_cache = RequestStore.store[:force_cache]
       RequestStore.store[:force_cache] = true
     end
 
+    attr_reader :original_force_cache, :period
     delegate :logger, to: Rails
 
     def call
       FacilityGroup.all.each do |region|
         logger.info { "class=#{self.class.name} region=#{region.name}" }
-        RegionService.new(region: region, period: @period).call
+        RegionService.new(region: region, period: period).call
       end
       Facility.all.each do |region|
         logger.info { "class=#{self.class.name} region=#{region.name}" }
-        RegionService.new(region: region, period: @period).call
+        RegionService.new(region: region, period: period).call
       end
+    ensure
+      RequestStore.store[:force_cache] = original_force_cache
     end
   end
 end
