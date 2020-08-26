347206af26e89d3da909383ee45a19f676c74b19
vkrm <mail@vkrm.is>
2020-08-26 16:26:27 +0530

    Refactor can?

--- app/models/user_access.rb
+++ app/models/user_access.rb
@@ -65,30 +65,9 @@ class UserAccess < Struct.new(:user)
     end
 
     if record&.new_record?
-      case model
-      when :facility
-        can_create_record?(:facility, record, accessible_facility_groups(:manage))
-      when :facility_group
-        can_create_record?(:facility_group, record, accessible_organizations(:manage))
-      when :organization
-        can_create_record?(:organization, record)
-      else
-        raise ArgumentError, "Access to #{model} is unsupported."
-      end
+      can_create?(model, record)
     elsif record.nil?
-      case model
-      when :facility
-        can_access_record?(accessible_facilities(action), record) ||
-          can_access_record?(accessible_facility_groups(action), record) ||
-          can_access_record?(accessible_organizations(action), record)
-      when :facility_group
-        can_access_record?(accessible_facility_groups(action), record) ||
-          can_access_record?(accessible_organizations(action), record)
-      when :organization
-        can_access_record?(accessible_organizations(action), record)
-      else
-        raise ArgumentError, "Access to #{model} is unsupported."
-      end
+      can_perform_action?(action, model)
     else
       case model
       when :facility
@@ -116,6 +95,19 @@ class UserAccess < Struct.new(:user)
     resources.exists?
   end
 
+  def can_create?(model, record)
+    case model
+    when :facility
+      can_create_record?(:facility, record, accessible_facility_groups(:manage))
+    when :facility_group
+      can_create_record?(:facility_group, record, accessible_organizations(:manage))
+    when :organization
+      can_create_record?(:organization, record)
+    else
+      raise ArgumentError, "Access to #{model} is unsupported."
+    end
+  end
+
   def can_create_record?(model, record, resources = nil)
     case model
     when :facility
@@ -125,7 +117,23 @@ class UserAccess < Struct.new(:user)
     when :organization
       user.power_user?
     else
-      raise ArgumentError, "Cannot create #{model}."
+      raise ArgumentError, "Can't create #{model}."
+    end
+  end
+
+  def can_perform_action?(action, model, record = nil)
+    case model
+    when :facility
+      can_access_record?(accessible_facilities(action), record) ||
+        can_access_record?(accessible_facility_groups(action), record) ||
+        can_access_record?(accessible_organizations(action), record)
+    when :facility_group
+      can_access_record?(accessible_facility_groups(action), record) ||
+        can_access_record?(accessible_organizations(action), record)
+    when :organization
+      can_access_record?(accessible_organizations(action), record)
+    else
+      raise ArgumentError, "Can't #{action} for #{model}."
     end
   end
 
