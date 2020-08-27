17b7981ed956de49e3722aa3f953965315f01531
Rob Sanheim <rsanheim@gmail.com>
2020-08-27 11:07:35 -0500

    Back off on materialized view refresh:
    Do it twice a week at 5 am India time.

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,7 +29,7 @@ end
 #   runner "Reports::RegionCacheWarmer.call"
 # end
 
-every 3.hours, roles: [:cron] do
+every [:sunday, :wednesday], at: local("05:00am"), roles: [:cron] do
   rake "refresh_materialized_db_views"
 end
 
