035b5351864c7d9dbc780d0c7fd7ef5d4158075a
Rob Sanheim <rsanheim@gmail.com>
2020-08-27 15:03:21 -0500

    Add slack notifications to RegionCacheWarmer and turn it back on (#1257)
    * Add slack notifications to RegionCacheWarmer...
    
    Also adding ability to disable via Flipper so we don't have to redeploy
    if we wanna turn it off again
    
    * Turn back on cache warmer; move seed jobs later
    
    * show current env
    
    * Allow fallback if there is no ENV var; use the existing ENV var
    
    I don't want to have to go thru deployment song and dance right now, so
    just using the existing webhook that is setup
    
    * brackets for env

--- app/services/reports/region_cache_warmer.rb
+++ app/services/reports/region_cache_warmer.rb
@@ -4,26 +4,72 @@ module Reports
       new.call
     end
 
+    def self.create_slack_notifier
+      return if ENV["SIMPLE_SERVER_DEPLOYMENT_NOTIFICATIONS_WEBHOOK"].blank?
+      Slack::Notifier.new(ENV["SIMPLE_SERVER_DEPLOYMENT_NOTIFICATIONS_WEBHOOK"], channel: "#alerts", username: "simple-server")
+    end
+
     def initialize(period: RegionService.default_period)
+      @notifier = self.class.create_slack_notifier
+      @start_time = Time.current
       @period = period
       @original_force_cache = RequestStore.store[:force_cache]
       RequestStore.store[:force_cache] = true
+      notify "#{self.class.name} Starting ..."
     end
 
     attr_reader :original_force_cache, :period
+    attr_reader :start_time
+    attr_reader :notifier
+
     delegate :logger, to: Rails
 
     def call
+      if Flipper.enabled?(:disable_region_cache_warmer)
+        notify "#{self.class.name} is disabled via Flipper! Bailing out early"
+        return
+      end
+
+      notify "Starting FacilityGroup caching"
+      time = Benchmark.realtime {
+        cache_facility_groups
+      }
+      notify "Finished FacilityGroups caching in #{time.round} seconds."
+
+      notify "Starting Facility caching."
+      time = Benchmark.realtime {
+        cache_facilities
+      }
+      end_time = Time.current
+      total_time = end_time - start_time
+      notify "Finished Facility caching in #{time.round} seconds, total cache time was #{total_time.round} seconds."
+      notify "#{self.class.name} All done!"
+    ensure
+      RequestStore.store[:force_cache] = original_force_cache
+    end
+
+    private
+
+    def notify(msg)
+      notifier.ping "[#{environment}] #{msg}"
+    end
+
+    def environment
+      ENV["SIMPLE_SERVER_ENV"]
+    end
+
+    def cache_facility_groups
       FacilityGroup.all.each do |region|
         logger.info { "class=#{self.class.name} region=#{region.name}" }
         RegionService.new(region: region, period: period).call
       end
+    end
+
+    def cache_facilities
       Facility.all.each do |region|
         logger.info { "class=#{self.class.name} region=#{region.name}" }
         RegionService.new(region: region, period: period).call
       end
-    ensure
-      RequestStore.store[:force_cache] = original_force_cache
     end
   end
 end
