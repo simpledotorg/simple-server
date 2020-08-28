ca5e33f0a9f281d6cb15fc7d2c1923920834260e
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-08-28 16:30:38 +0530

    Add index on teleconsultation_phone_number

--- db/migrate/20200821072154_add_teleconsultation_phone_number_to_users.rb
+++ db/migrate/20200821072154_add_teleconsultation_phone_number_to_users.rb
@@ -2,5 +2,6 @@ class AddTeleconsultationPhoneNumberToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :teleconsultation_phone_number, :string
     add_column :users, :teleconsultation_isd_code, :string
+    add_index :users, :teleconsultation_phone_number
   end
 end
