b8d6668f0f93a234dec54cd087781760cddd7cc5
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-08-28 16:29:58 +0530

    Standard

--- app/controllers/my_facilities_controller.rb
+++ app/controllers/my_facilities_controller.rb
@@ -26,25 +26,25 @@ class MyFacilitiesController < AdminController
     @inactive_facilities = overview_query.inactive_facilities
 
     @facility_counts_by_size = {total: @facilities.group(:facility_size).count,
-      inactive: @inactive_facilities.group(:facility_size).count}
+                                inactive: @inactive_facilities.group(:facility_size).count}
 
     @inactive_facilities_bp_counts =
       {last_week: overview_query.total_bps_in_last_n_days(n: 7),
-        last_month: overview_query.total_bps_in_last_n_days(n: 30)}
+       last_month: overview_query.total_bps_in_last_n_days(n: 30)}
   end
 
   def blood_pressure_control
     @facilities = filter_facilities([:manage, :facility])
 
     bp_query = MyFacilities::BloodPressureControlQuery.new(facilities: @facilities,
-      cohort_period: @selected_cohort_period)
+                                                           cohort_period: @selected_cohort_period)
 
     @totals = {registered: bp_query.cohort_registrations.count,
-      controlled: bp_query.cohort_controlled_bps.count,
-      uncontrolled: bp_query.cohort_uncontrolled_bps.count,
-      missed: bp_query.cohort_missed_visits_count,
-      overall_patients: bp_query.overall_patients.count,
-      overall_controlled_bps: bp_query.overall_controlled_bps.count}
+               controlled: bp_query.cohort_controlled_bps.count,
+               uncontrolled: bp_query.cohort_uncontrolled_bps.count,
+               missed: bp_query.cohort_missed_visits_count,
+               overall_patients: bp_query.overall_patients.count,
+               overall_controlled_bps: bp_query.overall_controlled_bps.count}
 
     @registered_patients_per_facility = bp_query.cohort_registrations.group(:registration_facility_id).count
     @controlled_bps_per_facility = bp_query.cohort_controlled_bps.group(:registration_facility_id).count
@@ -58,12 +58,12 @@ class MyFacilitiesController < AdminController
     @facilities = filter_facilities([:manage, :facility])
 
     registrations_query = MyFacilities::RegistrationsQuery.new(facilities: @facilities,
-      period: @selected_period,
-      last_n: PERIODS_TO_DISPLAY[@selected_period])
+                                                               period: @selected_period,
+                                                               last_n: PERIODS_TO_DISPLAY[@selected_period])
 
     @registrations = registrations_query.registrations
-                       .group(:facility_id, :year, @selected_period)
-                       .sum(:registration_count)
+      .group(:facility_id, :year, @selected_period)
+      .sum(:registration_count)
 
     @total_registrations = registrations_query.total_registrations.group(:registration_facility_id).count
     @total_registrations_by_period =
@@ -79,8 +79,8 @@ class MyFacilitiesController < AdminController
     @facilities = filter_facilities([:manage, :facility])
 
     missed_visits_query = MyFacilities::MissedVisitsQuery.new(facilities: @facilities,
-      period: @selected_period,
-      last_n: PERIODS_TO_DISPLAY[@selected_period])
+                                                              period: @selected_period,
+                                                              last_n: PERIODS_TO_DISPLAY[@selected_period])
 
     @display_periods = missed_visits_query.periods
     @missed_visits_by_facility = missed_visits_query.missed_visits_by_facility
