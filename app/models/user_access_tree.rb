f64f2bd74498efbbd3aaf69a5ab9bcd12663b671
Akshay Gupta <kitallis@gmail.com>
2020-08-30 02:58:44 +0530

    Move all access tree business into a UserAccessTree struct

---
+++ app/models/user_access_tree.rb
@@ -0,0 +1,70 @@
+class UserAccessTree < Struct.new(:user)
+  include Memery
+
+  memoize def facilities
+    visible_facilities.map do |facility|
+      info = {
+        visible: true
+      }
+
+      [facility, info]
+    end.to_h
+  end
+
+  memoize def facility_groups
+    facilities
+      .group_by { |facility, _| facility.facility_group }
+      .map do |facility_group, facilities|
+
+      info = {
+        accessible_facility_count: facilities.length,
+        visible: visible_facility_groups.include?(facility_group),
+        facilities: facilities,
+      }
+
+      [facility_group, info]
+    end.to_h
+  end
+
+  memoize def organizations
+    facility_groups
+      .group_by { |facility_group, _| facility_group.organization }
+      .map do |organization, facility_groups|
+
+      info = {
+        accessible_facility_count: facility_groups.sum { |_, info| info[:accessible_facility_count] },
+        visible: visible_organizations.include?(organization),
+        facility_groups: facility_groups,
+      }
+
+      [organization, info]
+    end.to_h
+  end
+
+  def visible?(model, record)
+    case model
+      when :facility
+        facilities.dig(record, :visible)
+      when :facility_group
+        facility_groups.dig(record, :visible)
+      when :organization
+        organizations.dig(record, :visible)
+      else
+        raise ArgumentError, "#{model} is unsupported."
+    end
+  end
+
+  private
+
+  memoize def visible_facility_groups
+    user.accessible_facility_groups(:view)
+  end
+
+  memoize def visible_facilities
+    user.accessible_facilities(:view)
+  end
+
+  memoize def visible_organizations
+    user.accessible_organizations(:view)
+  end
+end
--- app/presenters/admin_access_presenter.rb
+++ app/presenters/admin_access_presenter.rb
@@ -3,12 +3,6 @@ require "ostruct"
 class AdminAccessPresenter < SimpleDelegator
   include Memery
 
-  DEPTH_LEVEL_TO_PARTIAL = {
-    organization: "email_authentications/invitations/access_tree",
-    facility_group: "email_authentications/invitations/facility_group_access_tree",
-    facility: "email_authentications/invitations/facility_access_tree"
-  }.freeze
-
   attr_reader :admin
 
   def initialize(admin)
@@ -26,91 +20,28 @@ class AdminAccessPresenter < SimpleDelegator
       .map { |_level, info| info.values_at(:name, :id) }
   end
 
-  def access_tree_to_partial(access_tree)
-    DEPTH_LEVEL_TO_PARTIAL.fetch(access_tree.fetch(:depth_level))
+  memoize def access_tree
+    UserAccessTree.new(admin)
   end
 
-  def access_tree
+  delegate :visible?, to: :access_tree
+
+  def visible_access_tree
     if admin.access_across_organizations?(:view)
       {
-        data: organization_tree,
-        depth_level: :organization,
+        data: access_tree.organizations,
+        render_partial: "email_authentications/invitations/access_tree",
       }
     elsif admin.access_across_facility_groups?(:view)
       {
-        data: facility_group_tree,
-        depth_level: :facility_group,
+        data: access_tree.facility_groups,
+        render_partial: "email_authentications/invitations/facility_group_access_tree",
       }
     else
       {
-        data: facility_tree,
-        depth_level: :facility,
+        data: access_tree.facilities,
+        render_partial: "email_authentications/invitations/facility_access_tree",
       }
     end
-  end
-
-  def pre_selected?(model, record)
-    case model
-      when :facility
-        admin.facility_tree.dig(record, :full_access)
-      when :facility_group
-        admin.facility_group_tree.dig(record, :full_access)
-      when :organization
-        admin.organization_tree.dig(record, :full_access)
-      else
-        raise ArgumentError, "Access to #{model} is unsupported."
-    end
-  end
-
-  memoize def facility_tree
-    visible_facilities.map do |facility|
-      info = {
-        full_access: true
-      }
-
-      [facility, info]
-    end.to_h
-  end
-
-  memoize def facility_group_tree
-    facility_tree
-      .group_by { |facility, _| facility.facility_group }
-      .map do |facility_group, facilities|
-
-      info = {
-        accessible_facility_count: facilities.length,
-        full_access: visible_facility_groups.include?(facility_group),
-        facilities: facilities,
-      }
-
-      [facility_group, info]
-    end.to_h
-  end
-
-  memoize def organization_tree
-    facility_group_tree
-      .group_by { |facility_group, _| facility_group.organization }
-      .map do |organization, facility_groups|
-
-      info = {
-        accessible_facility_count: facility_groups.sum { |_, info| info[:accessible_facility_count] },
-        full_access: visible_organizations.include?(organization),
-        facility_groups: facility_groups,
-      }
-
-      [organization, info]
-    end.to_h
-  end
-
-  memoize def visible_facility_groups
-    admin.accessible_facility_groups(:view)
-  end
-
-  memoize def visible_facilities
-    admin.accessible_facilities(:view)
-  end
-
-  memoize def visible_organizations
-    admin.accessible_organizations(:view)
   end
 end
