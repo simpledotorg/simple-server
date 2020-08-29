e37e92e02bc07d3f1adb9636fe18b943f9737022
Akshay Gupta <kitallis@gmail.com>
2020-08-29 21:31:00 +0530

    Add a select-all checkbox

--- app/assets/javascripts/invite_admin.js
+++ app/assets/javascripts/invite_admin.js
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", editAdmin);
 function inviteAdmin() {
   checkboxItemListener()
   resourceRowCollapseListener()
+  selectAllListener()
 }
 
 function editAdmin() {
@@ -25,6 +26,25 @@ function editAdmin() {
 //
 // listeners
 //
+function selectAllListener() {
+  const selectAllDiv = document.getElementById("select_all_facilities")
+  const SELECTOR = "input.access-input"
+  const facilityAccessDiv = document.getElementById("facility-access")
+  const checkboxes = nodeListToArray(SELECTOR, facilityAccessDiv)
+
+  selectAllDiv.addEventListener("change", _ => {
+    if (selectAllDiv.checked) {
+      for (let checkbox of checkboxes) {
+        checkbox.checked = true
+      }
+    } else {
+      for (let checkbox of checkboxes) {
+        checkbox.checked = false
+      }
+    }
+  })
+}
+
 function checkboxItemListener() {
   const SELECTOR = "input.access-input"
   const facilityAccessDiv = document.getElementById("facility-access")
