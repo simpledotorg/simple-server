0620d8eb053c42cac8213ada463b2c6403ef4ed1
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-08-31 20:39:53 +0530

    Inherit SyncController

--- app/controllers/api/v4/teleconsultation_medical_officers_controller.rb
+++ app/controllers/api/v4/teleconsultation_medical_officers_controller.rb
@@ -1,25 +1,24 @@
-class Api::V4::TeleconsultationMedicalOfficersController < APIController
+class Api::V4::TeleconsultationMedicalOfficersController < Api::V4::SyncController
   def sync_to_user
-    medical_officers = current_facility_group.facilities.map { |facility| facility_medical_officers(facility) }
-    render json: to_response(medical_officers)
+    __sync_to_user__("teleconsultation_medical_officers")
   end
 
   private
 
-  def facility_medical_officers(facility)
+  def records_to_sync
+    current_facility_group.facilities
+  end
+
+  def transform_to_response(facility)
     {id: facility.id,
      facility_id: facility.id,
-     medical_officers: transform_to_response(facility.teleconsultation_medical_officers),
+     medical_officers: to_response(facility.teleconsultation_medical_officers),
      created_at: Time.current,
      updated_at: Time.current,
      deleted_at: Time.current}
   end
 
   def to_response(medical_officers)
-    {teleconsultation_medical_officers: medical_officers}
-  end
-
-  def transform_to_response(medical_officers)
     medical_officers.map do |medical_officer|
       Api::V4::TeleconsultationMedicalOfficerTransformer.to_response(medical_officer)
     end
