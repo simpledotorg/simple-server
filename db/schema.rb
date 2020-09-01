0824a38688be7f17c9582bed1e6d6f952e5b6946
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-01 20:04:18 +0530

    Add columns to prescription drugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_112700) do
+ActiveRecord::Schema.define(version: 2020_09_01_143158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -436,6 +436,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_112700) do
     t.boolean "is_deleted", null: false
     t.datetime "deleted_at"
     t.uuid "user_id"
+    t.string "frequency"
+    t.integer "duration_in_days"
     t.index ["deleted_at"], name: "index_prescription_drugs_on_deleted_at"
     t.index ["patient_id"], name: "index_prescription_drugs_on_patient_id"
     t.index ["user_id"], name: "index_prescription_drugs_on_user_id"
