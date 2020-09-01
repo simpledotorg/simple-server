89bbdba8953694b053e7d94edf95d87571e7fc8c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-01 20:07:03 +0530

    Add fields to API schema

--- app/schema/api/v3/models.rb
+++ app/schema/api/v3/models.rb
@@ -252,7 +252,9 @@ class Api::V3::Models
          is_protocol_drug: {type: :boolean},
          is_deleted: {type: :boolean},
          patient_id: {"$ref" => "#/definitions/uuid"},
-         facility_id: {"$ref" => "#/definitions/uuid"}
+         facility_id: {"$ref" => "#/definitions/uuid"},
+         frequency: {type: :string, enum: PrescriptionDrug.frequencies.keys},
+         duration_in_days: {type: :integer}
        },
        required: %w[id created_at updated_at name is_protocol_drug is_deleted patient_id facility_id]}
     end
