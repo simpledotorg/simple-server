63ffcb97e28b169ecdaf7f16f7892dfe822b3e7c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-02 13:12:39 +0530

    Add transformer spec

--- app/transformers/api/v4/teleconsultation_medical_officer_transformer.rb
+++ app/transformers/api/v4/teleconsultation_medical_officer_transformer.rb
@@ -1,10 +1,9 @@
 class Api::V4::TeleconsultationMedicalOfficerTransformer
   class << self
     def to_response(medical_officer)
-      medical_officer[:teleconsultation_phone_number] = medical_officer.full_teleconsultation_phone_number
-      medical_officer.slice("id",
-        "full_name",
-        "teleconsultation_phone_number")
+      medical_officer
+        .slice("id", "full_name", "teleconsultation_phone_number")
+        .merge(teleconsultation_phone_number: medical_officer.full_teleconsultation_phone_number)
     end
   end
 end
---
+++ spec/transformers/api/v4/teleconsultation_medical_officer_transformer_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Api::V4::TeleconsultationMedicalOfficerTransformer do
+  describe "#to_response" do
+    let!(:medical_officer) { create(:teleconsultation_medical_officer) }
+
+    it "includes user attributes" do
+      response = described_class.to_response(medical_officer)
+      expect(response).to include("id" => medical_officer.id,
+                                  "full_name" => medical_officer.full_name,
+                                  "teleconsultation_phone_number" => medical_officer.full_teleconsultation_phone_number)
+    end
+  end
+end
