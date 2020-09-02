bd4ae3b3e4eb80af4c6075dc550a1aba9ecaaf09
Rob Sanheim <rsanheim@gmail.com>
2020-09-02 15:49:02 -0500

    Make spec more functional and clear:
    ensure it returns exactly what we want and does not include the non HTN
    patients

--- spec/models/organization_district_spec.rb
+++ spec/models/organization_district_spec.rb
@@ -5,17 +5,28 @@ RSpec.describe OrganizationDistrict, type: :model do
     it "considers only registered hypertensive patients" do
       organization = create(:organization)
       facility_group = create(:facility_group, organization: organization)
-      facility1 = create(:facility, facility_group: facility_group, district: "Bathinda")
-      facility2 = create(:facility, facility_group: facility_group, district: "Bathinda")
+      facility = create(:facility, facility_group: facility_group, district: "Bathinda")
+      non_htn_patients_facility = create(:facility, facility_group: facility_group, district: "Bathinda")
 
       org_district = OrganizationDistrict.new("Bathinda", organization)
 
-      _non_htn_patients = create_list(:patient, 2, :without_hypertension, registration_facility: facility1)
-      htn_patients = create_list(:patient, 2, registration_facility: facility2)
+      Timecop.freeze("June 15th 2020") do
+        _non_htn_patients = create_list(:patient, 2, :without_hypertension, registration_facility: non_htn_patients_facility, recorded_at: 4.months.ago)
+        _htn_patients = create_list(:patient, 2, registration_facility: facility, recorded_at: 4.months.ago)
+        controlled_htn_patients = create_list(:patient, 2, registration_facility: facility, recorded_at: 4.months.ago)
+        controlled_htn_patients.each { |patient|
+          create(:blood_pressure, :under_control, patient: patient, facility: facility, recorded_at: 3.months.ago)
+        }
 
-      expect(CohortAnalyticsQuery).to receive(:new).with(match_array(htn_patients)).and_call_original
-
-      org_district.cohort_analytics(:month, 3)
+        result = org_district.cohort_analytics(:month, 3)
+        march_key = [Date.parse("February 1st 2020"), Date.parse("March 1st 2020")]
+        march_data = result[march_key]
+        expect(march_data["registered"].key?(non_htn_patients_facility.id)).to be false
+        expect(march_data["registered"]).to eq({facility.id => 4, "total" => 4})
+        expect(march_data["followed_up"]).to eq({facility.id => 2, "total" => 2})
+        expect(march_data["controlled"]).to eq({facility.id => 2, "total" => 2})
+        expect(march_data["uncontrolled"]).to eq({facility.id => 0, "total" => 0})
+      end
     end
   end
 end
