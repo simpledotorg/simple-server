22565b3cd87197b19e8d4aafbbb9ce4afa2d05ab
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-03 17:02:53 +0530

    Send teleconsult ids in form, js interaction

--- app/assets/javascripts/common/facility_teleconsultation_fields.js
+++ app/assets/javascripts/common/facility_teleconsultation_fields.js
@@ -33,11 +33,64 @@ function showSpinner() {
   populateDropdown(spinner);
 }
 
-function toggleTeleconsultationFields(checkbox) {
-  if (checkbox.checked) {
-    $("#teleconsultation-fields").show();
-  } else {
-    $("#teleconsultation-fields").hide();
-  }
+function toggleTeleconsultationFields() {
+  $("#teleconsultation-fields").toggle();
 }
 
+function addMedicalOfficer(user){
+  if(isAdded(user)) return false;
+
+  $(".medical-officers").append(medicalOfficerCard(user));
+  hideNoMedicalOfficers();
+}
+
+function medicalOfficersCount() {
+  return $(".medical-officer-card").length
+}
+
+function removeMedicalOfficer(userID) {
+  $(`[data-user-id="${userID}"]`).remove();
+
+  if(!medicalOfficersCount()) showNoMedicalOfficers();
+}
+
+function hideNoMedicalOfficers() {
+  $(".no-medical-officers").hide()
+}
+
+function showNoMedicalOfficers() {
+  $(".no-medical-officers").show()
+}
+
+function isAdded(user) {
+  let addedMedicalOfficerIDs = $(".medical-officer-input").map((_,el) => el.value).get();
+  return addedMedicalOfficerIDs.includes(user["userId"])
+}
+
+function medicalOfficerCard(user) {
+  let card = $("template#teleconsultation-medical-officer").html();
+  let $card = $(card)
+  let userID = user["userId"]
+
+  $card.find(".medical-officer-input").val(userID);
+  $card.find(".medical-officer-name").html(user["userFullName"]);
+  $card.find(".medical-officer-registration-facility").html(user["userRegistrationFacility"]);
+  $card.find(".medical-officer-phone-number").html(user["userTeleconsultPhoneNumber"]);
+  $card.find("[data-user-id]").attr("data-user-id", userID);
+  $card.attr("data-user-id", userID);
+
+  return $card;
+}
+
+$(document).ready(function() {
+  $("body").on("click", ".typeahead-dropdown-row", function () {
+    let user = $(this).data();
+    addMedicalOfficer(user);
+  });
+
+  $("body").on("click", ".remove-medical-officer", function () {
+    let userID = $(this).attr('data-user-id');
+    removeMedicalOfficer(userID);
+  });
+
+})
