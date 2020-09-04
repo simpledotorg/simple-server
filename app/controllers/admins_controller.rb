0d4e1a27f86e70ba6bf9ba6868d838c924fe0930
Akshay Gupta <kitallis@gmail.com>
2020-09-04 18:16:46 +0530

    Resolve comments

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -113,9 +113,7 @@ class AdminsController < AdminController
   end
 
   def 🆕set_admin
-    if Flipper.enabled?(:new_permissions_system_aug_2020, current_admin)
-      @admin = authorize1 { current_admin.accessible_admins(:manage).find(params[:id]) }
-    end
+    @admin = authorize1 { current_admin.accessible_admins(:manage).find(params[:id]) }
   end
 
   def current_admin
