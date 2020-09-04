4fed2133b76b33a25f79bc4d595c8c8cbaae2f60
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-09-04 12:56:31 +0530

    Remove security-env config files

--- config/deploy/security.rb
+++
@@ -1,3 +0,0 @@
-server "ec2-13-127-26-45.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron whitelist_phone_numbers]
-server "ec2-13-234-115-73.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db]
-server "ec2-13-234-17-30.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
