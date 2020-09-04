5c93d7a0a69e3c0bb967a0ef783e2b486940db65
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-04 15:27:56 +0530

    Fix response slicing

--- app/transformers/api/v4/teleconsultation_medical_officer_transformer.rb
+++ app/transformers/api/v4/teleconsultation_medical_officer_transformer.rb
@@ -2,12 +2,8 @@ class Api::V4::TeleconsultationMedicalOfficerTransformer
   class << self
     def to_response(medical_officer)
       medical_officer
+        .slice("id", "full_name")
         .merge(teleconsultation_phone_number: medical_officer.full_teleconsultation_phone_number)
-        .slice(response_fields)
     end
-  end
-
-  def response_fields
-    %w[id full_name teleconsultation_phone_number]
   end
 end
