0eba13ba62ac0a6fb92ec684af8004df62432af0
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-15 12:39:58 +0530

    Add constraints and initial factory

--- app/models/teleconsultation.rb
+++ app/models/teleconsultation.rb
@@ -1,2 +1,6 @@
 class Teleconsultation < ApplicationRecord
+  belongs_to :patient
+  belongs_to :facility
+  belongs_to :requester, class_name: "User", foreign_key: :requester_id
+  belongs_to :medical_officer, class_name: "User", foreign_key: :medical_officer_id
 end
---
+++ spec/factories/teleconsultations.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :teleconsultation do
+    id { SecureRandom.uuid }
+
+    association :patient, strategy: :create
+    association :facility, strategy: :create
+    association :requester, factory: :user, strategy: :create
+    association :medical_officer, factory: :user, strategy: :create
+
+    device_requested_at { Time.now }
+    requested_at { device_requested_at }
+
+    recorded_at { device_recorded_at }
+    device_recorded_at { Time.now }
+    device_updated_at { Time.now }
+
+    request_completed { "yes" }
+    teleconsultation_type { "audio" }
+    medical_officer_number { "" }
+
+    patient_took_medicines { true }
+    patient_consented { true }
+  end
+end
