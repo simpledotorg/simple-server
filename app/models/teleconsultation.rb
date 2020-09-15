d82ad1fe0ddc9f05ae43979225e1c072480abb47
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-15 13:05:12 +0530

    Add shoulda matchers

--- app/models/teleconsultation.rb
+++ app/models/teleconsultation.rb
@@ -1,6 +1,6 @@
 class Teleconsultation < ApplicationRecord
   belongs_to :patient
-  belongs_to :facility
-  belongs_to :requester, class_name: "User", foreign_key: :requester_id
   belongs_to :medical_officer, class_name: "User", foreign_key: :medical_officer_id
+  belongs_to :facility, optional: true
+  belongs_to :requester, class_name: "User", foreign_key: :requester_id, optional: true
 end
--- spec/models/teleconsultation_spec.rb
+++ spec/models/teleconsultation_spec.rb
@@ -1,4 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Teleconsultation, type: :model do
+  it { should belong_to(:patient) }
+  it { should belong_to(:medical_officer).class_name("User") }
+  it { should belong_to(:requester).class_name("User").optional }
+  it { should belong_to(:facility).optional }
 end
