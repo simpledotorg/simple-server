eaec0bc7f182fb56ee5b89839f17a32278d72a05
Akshay Gupta <kitallis@gmail.com>
2020-09-15 19:02:42 +0530

    Don’t need to add a default access level migration, just do it in code

--- db/data/20200915120848_add_default_access_level_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultAccessLevelToUsers < ActiveRecord::Migration[5.2]
-  def up
-    User.admins.where(access_level: [nil, ""]).update_all(access_level: "call_center")
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
