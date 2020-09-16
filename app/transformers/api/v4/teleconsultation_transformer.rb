234210dde1e8e0563b23f79d78dac3d784fdb4dd
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-16 17:07:17 +0530

    Add controller specs

--- app/transformers/api/v4/teleconsultation_transformer.rb
+++ app/transformers/api/v4/teleconsultation_transformer.rb
@@ -2,8 +2,8 @@ class Api::V4::TeleconsultationTransformer
   class << self
     def to_response(teleconsultation)
       Api::V4::Transformer.to_response(teleconsultation)
-        .except(teleconsultation.request.keys)
-        .except(teleconsultation.record.keys)
+        .except(*teleconsultation.request.keys)
+        .except(*teleconsultation.record.keys)
         .merge({"request" => teleconsultation.request,
                 "record" => teleconsultation.record})
     end
