72ce983332d0d6878b30a62667f6e5017f60eb66
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-09-16 19:32:39 +0530

    Commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2020_09_08_183016) do
+
+ActiveRecord::Schema.define(version: 2020_09_14_073359) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
