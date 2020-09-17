347397544b87263ac44e5b13795504eb6e49347b
vkrm <mail@vkrm.is>
2020-09-17 18:02:47 +0530

    Send user approvals emails to new permissions admins

--- app/mailers/approval_notifier_mailer.rb
+++ app/mailers/approval_notifier_mailer.rb
@@ -23,18 +23,37 @@ class ApprovalNotifierMailer < ApplicationMailer
 
   private
 
+  # permissions_users are admins as per the old permissions system
+  # accesses_users are admins as per the new permissions system
+  # we need to send emails to the superset of the two till we flip everyone over to new permissions
+
   def supervisor_emails
-    users = UserPermission.where(permission_slug: :approve_health_workers, resource: user.facility_group).map(&:user)
+    permissions_users = UserPermission.where(permission_slug: :approve_health_workers, resource: user.facility_group).map(&:user)
+    accesses_users = User.admins.where(access_level: :manager)
+                       .select { |admin| admin.accessible_facilities(:manage).include?(user.facility) }
+                       .reject { |admin| admin.accesses.map(&:resource_type).include?("Organization") }
+
+    users = (permissions_users + accesses_users).uniq.compact
     users.map(&:email).join(",")
   end
 
   def organization_owner_emails
-    users = UserPermission.where(permission_slug: :approve_health_workers, resource: user.organization).map(&:user)
+    permissions_users = UserPermission.where(permission_slug: :approve_health_workers, resource: user.organization).map(&:user)
+    accesses_users = User.admins.where(access_level: :manager)
+                       .select { |admin|
+                         admin.accessible_facilities(:manage).include?(user.facility) &&
+                           admin.accesses.map(&:resource_type).include?("Organization")
+                       }
+
+    users = (permissions_users + accesses_users).uniq.compact
     users.map(&:email).join(",")
   end
 
   def owner_emails
-    users = UserPermission.where(permission_slug: :approve_health_workers, resource: nil).map(&:user)
+    permissions_users = UserPermission.where(permission_slug: :approve_health_workers, resource: nil).map(&:user)
+    accesses_users = User.admins.where(access_level: :power_user)
+
+    users = (permissions_users + accesses_users).uniq.compact
     users.map(&:email).join(",")
   end
 end
