62606674e7f3196e1a155f64ea29845a2c82049a
vkrm <mail@vkrm.is>
2020-09-23 13:48:47 +0530

    Remove Pundit permissions usage from Organizations controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,42 +1,19 @@
 class OrganizationsController < AdminController
   include Pagination
 
-  skip_after_action :verify_authorized, if: -> { current_admin.permissions_v2_enabled? }
-  skip_after_action :verify_policy_scoped, if: -> { current_admin.permissions_v2_enabled? }
-  after_action :verify_authorization_attempted, if: -> { current_admin.permissions_v2_enabled? }
-
   def index
-    if current_admin.permissions_v2_enabled?
-      @accessible_facilities = current_admin.accessible_facilities(:view_reports)
-      authorize_v2 { @accessible_facilities.any? }
-    else
-      authorize(:dashboard, :show?)
-    end
+    @accessible_facilities = current_admin.accessible_facilities(:view_reports)
+    authorize { @accessible_facilities.any? }
 
-    users = if current_admin.permissions_v2_enabled?
-      current_admin.accessible_users(:manage)
-    else
-      policy_scope([:manage, :user, User])
-    end
-
-    @users_requesting_approval = users
-      .requested_sync_approval
-      .order(updated_at: :desc)
-
-    @users_requesting_approval = paginate(users
+    @users_requesting_approval = paginate(current_admin.accessible_users(:manage)
                                             .requested_sync_approval
                                             .order(updated_at: :desc))
 
-    @organizations =
-      if current_admin.permissions_v2_enabled?
-        @accessible_facilities
-          .includes(facility_group: :organization)
-          .flat_map(&:organization)
-          .uniq
-          .compact
-          .sort_by(&:name)
-      else
-        policy_scope([:cohort_report, Organization]).order(:name)
-      end
+    @organizations = @accessible_facilities
+      .includes(facility_group: :organization)
+      .flat_map(&:organization)
+      .uniq
+      .compact
+      .sort_by(&:name)
   end
 end
