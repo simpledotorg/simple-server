d3a6acd5e5b81ecf3c87e9b07fb1e85c466585bd
Rob Sanheim <rsanheim@gmail.com>
2020-09-23 13:09:54 -0500

    turn on more Datadog tracing / agents

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,9 @@
 if SIMPLE_SERVER_ENV == "sandbox"
   require "ddtrace"
   Datadog.configure do |c|
+    c.analytics_enabled = true
+    c.use :http
     c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
+    c.use :sidekiq
   end
 end
