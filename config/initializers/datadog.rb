c1c3bce856f6c94dbe1d2ae8ae7adc8376311294
Rob Sanheim <rsanheim@gmail.com>
2020-09-23 14:17:08 -0500

    More datadog config (#1343)
    * Add datadog tracing in sandbox
    
    * turn on more Datadog tracing / agents
    
    * Try out log_injection for trace correlation
    
    https://docs.datadoghq.com/tracing/setup/ruby/#trace-correlation

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,6 +1,9 @@
 if SIMPLE_SERVER_ENV == "sandbox"
   require "ddtrace"
   Datadog.configure do |c|
-    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
+    c.analytics_enabled = true
+    c.use :http
+    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app", log_injection: true
+    c.use :sidekiq
   end
 end
