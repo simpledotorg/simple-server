cc1257417851b1a76ed8f290792efdc4afffbfe4
Rob Sanheim <rsanheim@gmail.com>
2020-09-23 13:15:51 -0500

    Try out log_injection for trace correlation
    https://docs.datadoghq.com/tracing/setup/ruby/#trace-correlation

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,7 +3,7 @@ if SIMPLE_SERVER_ENV == "sandbox"
   Datadog.configure do |c|
     c.analytics_enabled = true
     c.use :http
-    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
+    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app", log_injection: true
     c.use :sidekiq
   end
 end
