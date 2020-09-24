d3d67b900e6599249daaf5e75bb42b0156026893
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-09-24 14:51:09 +0530

    Revert "More datadog config (#1343)"
    This reverts commit c1c3bce856f6c94dbe1d2ae8ae7adc8376311294.

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,6 @@
 if SIMPLE_SERVER_ENV == "sandbox"
   require "ddtrace"
   Datadog.configure do |c|
-    c.analytics_enabled = true
-    c.use :http
-    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app", log_injection: true
-    c.use :sidekiq
+    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
   end
 end
