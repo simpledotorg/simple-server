abc7b3cae5533e3e3e85dc92b40c4218e211dddf
Rob Sanheim <rsanheim@gmail.com>
2020-09-25 16:23:39 -0500

    Add dogstatsd; turn on datadog in sandbox and prod (#1359)

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,6 @@
-if SIMPLE_SERVER_ENV == "sandbox"
+if ["sandbox", "production"].include?(SIMPLE_SERVER_ENV)
   require "ddtrace"
+  require "datadog/statsd"
   Datadog.configure do |c|
     c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app", log_injection: true
   end
