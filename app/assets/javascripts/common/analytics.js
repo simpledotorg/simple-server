3064516d479624e59febc4f741f8765e9c5e0df2
Tim Cheadle <tcheadle@resolvetosavelives.org>
2020-09-29 20:49:59 -0400

    Sort ranked facilities ascending by rank by default

--- app/assets/javascripts/common/analytics.js
+++ app/assets/javascripts/common/analytics.js
@@ -2,4 +2,8 @@ window.addEventListener("DOMContentLoaded", function() {
   if($('#analytics-table').length) {
     new Tablesort(document.getElementById('analytics-table'), { descending: true })
   }
+
+  if($('#ranked-facilities-table').length) {
+    new Tablesort(document.getElementById('ranked-facilities-table'), { descending: false})
+  }
 });
