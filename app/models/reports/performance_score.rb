f496188909ce033c7984070c0f7e4015c931cd0d
Tim Cheadle <tcheadle@resolvetosavelives.org>
2020-09-29 20:49:17 -0400

    Added a performance scoring service based on Reports::Result

---
+++ app/models/reports/performance_score.rb
@@ -0,0 +1,44 @@
+module Reports
+  class PerformanceScore
+    CONTROL_SCORE_WEIGHT = 0.5
+    VISITS_SCORE_WEIGHT = 0.3
+    REGISTRATIONS_SCORE_WEIGHT = 0.2
+    TARGET_REGISTRATION_RATE = 0.1
+
+    def initialize(region:, result:)
+      @region = region
+      @result = result
+    end
+
+    def call
+      overall_score
+    end
+
+    def overall_score
+      # max score: 100
+      (control_score + visits_score + registrations_score)
+    end
+
+    def control_score
+      CONTROL_SCORE_WEIGHT * @result.controlled_patients_rate.values.last
+    end
+
+    def visits_score
+      visits_rate = 100 - @result.missed_visits_rate.values.last
+
+      VISITS_SCORE_WEIGHT * visits_rate
+    end
+
+    def registrations_score
+      REGISTRATIONS_SCORE_WEIGHT * registration_rate
+    end
+
+    def registration_rate
+      monthly_registrations = @result.registrations.values.last
+      monthly_opd_load = @region.monthly_estimated_opd_load
+      target_registrations = monthly_opd_load * TARGET_REGISTRATION_RATE
+
+      (monthly_registrations / target_registrations) * 100
+    end
+  end
+end
