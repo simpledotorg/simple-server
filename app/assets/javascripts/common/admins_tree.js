3043c77fe44ab22f157c3d96692e9f7761f89191
Dany Sam Salmon <danisam17@gmail.com>
2020-09-30 12:03:16 +0530

    Add methods to access items to walk up and down the tree

--- app/assets/javascripts/common/admins_tree.js
+++ app/assets/javascripts/common/admins_tree.js
@@ -1,21 +1,63 @@
-function buildAccessTree() {
-    const accessItems = Array.from(document.querySelectorAll(".access-item"))
-    const data = _.groupBy(accessItems, (item) => item.dataset.accessType)
-    return _out = _.transform(data, (result, value, key) =>
-        result[key] = _.transform(value, addAccessItemMetaData, {}))
-}
+const AccessTree = function () {
 
-function getAccessItemParentKey(accessType) {
-    if (accessType === "facility") return "facilityGroupId"
-    if (accessType === "facilityGroup") return "organizationId"
-    return "parentId"
-}
+    const self = this
 
-function addAccessItemMetaData(result, value, _key) {
-    const parentKey = getAccessItemParentKey(value.dataset.accessType)
-    return result[value.dataset.id] = {
-        element: value,
-        [parentKey]: value.dataset[parentKey],
-        name: value.dataset.name
+    getParentKey = function (accessType) {
+        if (accessType === "facility") return "facilityGroup"
+        if (accessType === "facilityGroup") return "organization"
     }
+
+    getChildKey = function (accessType) {
+        if (accessType === "organization") return "facilityGroup"
+        if (accessType === "facilityGroup") return "facility"
+    }
+
+    getParentIdKey = function (accessType) {
+        if (accessType === "facility") return "facilityGroupId"
+        if (accessType === "facilityGroup") return "organizationId"
+        return "parentId"
+    }
+
+    addAccessItemMetaData = function (result, value, _key) {
+        const accessType = value.dataset.accessType
+        const parentKey = getParentIdKey(accessType)
+        const parentId = value.dataset[parentKey]
+        return result[value.dataset.id] = {
+            element: value,
+            [parentKey]: parentId,
+            name: value.dataset.name,
+            accessType,
+            parent: getElementParent(accessType, parentId),
+            children: getElementChildren(accessType, value.dataset.id)
+        }
+    }
+
+    getElementParent = function (accessType, parentId) {
+        return function () {
+            const tree = self.accessTree
+            const parentkey = getParentKey(accessType)
+            if (!parentkey) return
+            return tree[parentkey][parentId]
+        }
+    }
+
+    getElementChildren = function (accessType, itemId) {
+        return function () {
+            const tree = self.accessTree
+            const childKey = getChildKey(accessType)
+            const parentIdKey = getParentIdKey(childKey)
+            if (!childKey) return
+            return Object.values(tree[childKey])
+                .filter(item => item[parentIdKey] === itemId)
+        }
+    }
+
+    buildAccessTree = function () {
+        const accessItems = Array.from(document.querySelectorAll(".access-item"))
+        const data = _.groupBy(accessItems, (item) => item.dataset.accessType)
+        return _out = _.transform(data, (result, value, key) =>
+            result[key] = _.transform(value, addAccessItemMetaData, {}))
+    }
+
+    this.accessTree = buildAccessTree()
 }
