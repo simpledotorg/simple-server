e9c88295252aaa5d523851086dd846a436a0069c
Rob Sanheim <rsanheim@gmail.com>
2020-09-30 19:28:07 -0500

    Datadog: more attempts to get tracing working (#1379)
    * Dont include env in DD trace info
    
    This is coming through as an empty string, so we lose the correct env
    (which is set by the agent)
    
    * Include DD trace info in lograge also
    
    * Turn on rails analytics
    
    * Make sure the datadog initializer runs first

--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -3,6 +3,8 @@ require "datadog/statsd"
 
 if ["sandbox", "production"].include?(SIMPLE_SERVER_ENV)
   Datadog.configure do |c|
-    c.use :rails, service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
+    c.use :rails,
+      analytics_enabled: true,
+      service_name: "#{SIMPLE_SERVER_ENV}-rails-app"
   end
 end
