7bd22ab3920fda3a036b84a6afc4ecd8661ec022
Rob Sanheim <rsanheim@gmail.com>
2020-10-01 11:52:36 -0500

    Add dry run mode

--- app/services/region_backfill.rb
+++ app/services/region_backfill.rb
@@ -1,10 +1,22 @@
 class RegionBackfill
-  def self.call
-    new.call
+  def self.call(*args)
+    new(*args).call
   end
 
-  def initialize
+  def initialize(dry_run: true)
+    @dry_run = dry_run
+    @write = !@dry_run
   end
+
+  def dry_run?
+    @dry_run
+  end
+
+  def write?
+    @write
+  end
+
+  delegate :logger, to: Rails
 
   def call
     root_kind = RegionKind.find_by!(name: "Root")
@@ -14,20 +26,46 @@ class RegionBackfill
     facility_kind = block_kind.children.first
 
     current_country_name = CountryConfig.current[:name]
-    instance = Region.create! name: current_country_name, path: current_country_name, kind: root_kind
+    instance = create_or_fake_object name: current_country_name, path: current_country_name, kind: root_kind
+
     Organization.all.each do |org|
-      org_region = Region.create_region_from(source: org, kind: org_kind, parent: instance)
+      org_region = create_region_from(source: org, kind: org_kind, parent: instance)
 
       org.facility_groups.each do |facility_group|
-        facility_group_region = Region.create_region_from(source: facility_group, kind: facility_group_kind, parent: org_region)
+        facility_group_region = create_region_from(source: facility_group, kind: facility_group_kind, parent: org_region)
 
         facility_group.facilities.group_by(&:zone).each do |block_name, facilities|
-          block_region = Region.create_region_from(name: block_name, kind: block_kind, parent: facility_group_region)
+          block_region = create_region_from(name: block_name, kind: block_kind, parent: facility_group_region)
           facilities.each do |facility|
-            Region.create_region_from(source: facility, kind: facility_kind, parent: block_region)
+            create_region_from(source: facility, kind: facility_kind, parent: block_region)
           end
         end
       end
     end
+  end
+
+  def create_or_fake_object(attrs)
+    if write?
+      Region.create! attrs
+    else
+      region = Region.new attrs
+      logger.info "create", region: region
+      region
+    end
+  end
+
+  def create_region_from(parent:, kind:, name: nil, source: nil)
+    raise ArgumentError, "Provide either a name or a source" if (name && source) || (name.blank? && source.blank?)
+    region_name = name || source.name
+    region = Region.new name: region_name, kind: kind
+    region.send :set_slug
+    region.source = source if source
+    region.path = "#{parent.path}.#{region.slug.tr("-", "_")}"
+    if dry_run?
+      logger.info msg: "save", region: region
+    else
+      region.save!
+    end
+    region
   end
 end
--- spec/services/region_backfill_spec.rb
+++ spec/services/region_backfill_spec.rb
@@ -10,28 +10,45 @@ RSpec.describe RegionBackfill, type: :model do
     _facility = RegionKind.create! name: "Facility", parent: block
   end
 
-  it "backfills" do
-    org = create(:organization, name: "Test Organization")
-    facility_group_1 = create(:facility_group, organization: org)
-    facility_group_2 = create(:facility_group, organization: org)
+  context "dry run mode" do
+    before do
+      org = create(:organization, name: "Test Organization")
+      facility_group_1 = create(:facility_group, organization: org)
+      _facility_group_2 = create(:facility_group, organization: org)
+      create(:facility, facility_group: facility_group_1)
+    end
 
-    facility_1 = create(:facility, name: "facility1", facility_group: facility_group_1)
-    facility_2 = create(:facility, name: "facility2", facility_group: facility_group_1)
+    it "does not create any records" do
+      expect {
+        RegionBackfill.call(dry_run: true)
+      }.to_not change { Region.count }
+    end
+  end
 
-    facility_3 = create(:facility, name: "facility3", facility_group: facility_group_2)
+  context "write mode" do
+    it "backfills" do
+      org = create(:organization, name: "Test Organization")
+      facility_group_1 = create(:facility_group, organization: org)
+      facility_group_2 = create(:facility_group, organization: org)
 
-    RegionBackfill.call
+      facility_1 = create(:facility, name: "facility1", facility_group: facility_group_1)
+      facility_2 = create(:facility, name: "facility2", facility_group: facility_group_1)
 
-    root = Region.find_by(name: "India")
-    expect(root.root?).to be true
-    org = root.children.first
-    expect(org.children.map(&:source)).to contain_exactly(facility_group_1, facility_group_2)
+      facility_3 = create(:facility, name: "facility3", facility_group: facility_group_2)
 
-    block_regions = Region.where(kind: RegionKind.find_by!(name: "Block"))
-    expect(block_regions.size).to eq(2)
-    expect(block_regions.map(&:name).uniq).to eq(["Block ABC"])
+      RegionBackfill.call(dry_run: false)
 
-    expect(org.leaves.map(&:source)).to contain_exactly(facility_1, facility_2, facility_3)
-    expect(org.leaves.map(&:kind).uniq).to contain_exactly(RegionKind.find_by!(name: "Facility"))
+      root = Region.find_by(name: "India")
+      expect(root.root?).to be true
+      org = root.children.first
+      expect(org.children.map(&:source)).to contain_exactly(facility_group_1, facility_group_2)
+
+      block_regions = Region.where(kind: RegionKind.find_by!(name: "Block"))
+      expect(block_regions.size).to eq(2)
+      expect(block_regions.map(&:name).uniq).to eq(["Block ABC"])
+
+      expect(org.leaves.map(&:source)).to contain_exactly(facility_1, facility_2, facility_3)
+      expect(org.leaves.map(&:kind).uniq).to contain_exactly(RegionKind.find_by!(name: "Facility"))
+    end
   end
 end
