2daeec1627410fce7eb3fbec05eb04828bba1a7b
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-01 12:40:41 +0530

    Fix request specs

--- spec/requests/api/v3/blood_sugar_sync_spec.rb
+++ spec/requests/api/v3/blood_sugar_sync_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.describe "BloodSugars sync", type: :request do
   let(:sync_route) { "/api/v3/blood_sugars/sync" }
   let(:request_user) { FactoryBot.create(:user) }
+  let(:patient) { create(:patient, registration_facility: request_user.facility) }
 
   let(:model) { BloodSugar }
 
-  let(:build_payload) { -> { build_blood_sugar_payload(FactoryBot.build(:blood_sugar, facility: request_user.facility)) } }
+  let(:build_payload) { -> { build_blood_sugar_payload(FactoryBot.build(:blood_sugar, patient: patient, facility: request_user.facility)) } }
   let(:build_invalid_payload) { -> { build_invalid_blood_sugar_payload } }
   let(:update_payload) { ->(blood_sugar) { updated_blood_sugar_payload blood_sugar } }
 
