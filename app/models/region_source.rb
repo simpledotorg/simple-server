5b6fbc9b75b86749fd130ac512daa73c09e5fb17
Rob Sanheim <rsanheim@gmail.com>
2020-10-01 15:03:39 -0500

    Only discard if the Region is there

--- app/models/region_source.rb
+++ app/models/region_source.rb
@@ -2,7 +2,7 @@ module RegionSource
   def self.extended(klass)
     klass.has_one :region, inverse_of: :source, foreign_key: "source_id"
     klass.after_discard do
-      region.discard
+      region.discard if region
     end
   end
 end
