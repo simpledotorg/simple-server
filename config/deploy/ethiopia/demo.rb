d6982d53b51a6734abdf8ce0b3c800129c1e1b33
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-10-06 19:12:30 +0530

    Fix cap roles

--- config/deploy/ethiopia/demo.rb
+++ config/deploy/ethiopia/demo.rb
@@ -1,2 +1,2 @@
-server "167.71.226.153", user: "deploy", roles: %w[web app db cron]
-server "157.245.99.197", user: "deploy", roles: %w[web app db]
+server "167.71.226.153", user: "deploy", roles: %w[web app db cron sidekiq]
+server "157.245.99.197", user: "deploy", roles: %w[web app]
