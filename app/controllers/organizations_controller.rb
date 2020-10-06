ecebfab9985d24ca7eaf9d2c6fb88ec1988b7cf6
Rob Sanheim <rsanheim@gmail.com>
2020-10-06 09:35:35 -0500

    Fix merge conflict

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -19,21 +19,11 @@ class OrganizationsController < AdminController
                                             .requested_sync_approval
                                             .order(updated_at: :desc))
 
-<<<<<<< HEAD
-    @organizations =
-      if current_admin.permissions_v2_enabled?
-        org_ids = @accessible_facilities.includes(facility_group: :organization).flat_map(&:organization_id)
-        Organization.where(id: org_ids).order(:name)
-      else
-        policy_scope([:cohort_report, Organization]).order(:name)
-      end
-=======
     @organizations = @accessible_facilities
       .includes(facility_group: :organization)
       .flat_map(&:organization)
       .uniq
       .compact
       .sort_by(&:name)
->>>>>>> origin/master
   end
 end
