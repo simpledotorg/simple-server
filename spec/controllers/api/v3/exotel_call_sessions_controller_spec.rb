c54bc96095259ebb3549fd3b3cce6f2309ac3f03
Rob Sanheim <rsanheim@gmail.com>
2020-10-08 13:36:25 -0500

    Add back assertion

--- spec/controllers/api/v3/exotel_call_sessions_controller_spec.rb
+++ spec/controllers/api/v3/exotel_call_sessions_controller_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe Api::V3::ExotelCallSessionsController, type: :controller do
                               digits: patient.phone_numbers.first.number,
                               CallType: "call-attempt",
                               CallSid: SecureRandom.uuid}
+        expect(response).to have_http_status(403)
       end
     end
 
