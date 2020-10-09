e1cdf8afbd71be3e3446c28cbfcef812f0dee812
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-09 13:20:02 +0530

    standardrb

--- spec/services/mark_patient_mobile_numbers_spec.rb
+++ spec/services/mark_patient_mobile_numbers_spec.rb
@@ -4,6 +4,7 @@ describe MarkPatientMobileNumbers, type: :model do
   it "marks non-mobile patient phone numbers as mobile" do
     mobile_number = create(:patient_phone_number, phone_type: "mobile")
     non_mobile_number = create(:patient_phone_number, phone_type: "landline")
+    allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with("FORCE_MARK_PATIENT_MOBILE_NUMBERS").and_return "true"
 
     MarkPatientMobileNumbers.call
@@ -14,14 +15,15 @@ describe MarkPatientMobileNumbers, type: :model do
 
   it "doesn't touch mobile numbers" do
     mobile_number = create(:patient_phone_number, phone_type: "mobile")
+    allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with("FORCE_MARK_PATIENT_MOBILE_NUMBERS").and_return "true"
 
     expect { MarkPatientMobileNumbers.call }.not_to change { mobile_number.reload.updated_at }
   end
 
   it "does nothing if config is not set" do
-    mobile_number = create(:patient_phone_number, phone_type: "mobile")
     non_mobile_number = create(:patient_phone_number, phone_type: "landline")
+    allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with("FORCE_MARK_PATIENT_MOBILE_NUMBERS").and_return "something else"
 
     expect { MarkPatientMobileNumbers.call }.not_to change { non_mobile_number.reload.phone_type }
