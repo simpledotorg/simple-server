692e26b7141cace2791afa3cbebbe00dde816ba2
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-09 13:15:45 +0530

    Add a cron task to force-mark mobile numbers

--- app/services/mark_patient_mobile_numbers.rb
+++ app/services/mark_patient_mobile_numbers.rb
@@ -6,10 +6,21 @@ class MarkPatientMobileNumbers
   def call
     return unless ENV["FORCE_MARK_PATIENT_MOBILE_NUMBERS"] == "true"
 
+    count = eligible_numbers.count
+    notify("Force-marking #{count} as mobile numbers")
+
     eligible_numbers.update_all(phone_type: "mobile")
+
+    notify("Finished force-marking #{count} as mobile numbers")
   end
+
+  private
 
   def eligible_numbers
     PatientPhoneNumber.where.not(phone_type: "mobile")
+  end
+
+  def notify(msg)
+    Rails.logger.info msg: msg, class: self.class.name
   end
 end
