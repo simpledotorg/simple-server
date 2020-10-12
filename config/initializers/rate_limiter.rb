6234c5ca04bfefe74a70fe2dc0f5afd28aecdc50
Akshay Gupta <kitallis@gmail.com>
2020-10-12 17:46:20 +0530

    Actually add the rack-attack changes

---
+++ config/initializers/rate_limiter.rb
@@ -0,0 +1,19 @@
+module RateLimit
+  def self.auth_api_options
+    # 5 requests / minute
+    limit_proc = proc { |_req| 5 }
+    period_proc = proc { |_req| 1.minute }
+
+    {limit: limit_proc, period: period_proc}
+  end
+end
+
+class Rack::Attack
+  throttle('throttle_logins', RateLimit.auth_api_options) do |req|
+    req.ip if req.post? && req.path.start_with?("/email_authentications/sign_in")
+  end
+
+  throttle('throttle_password_modifications', RateLimit.auth_api_options) do |req|
+    req.ip if req.path.start_with?("/email_authentications/password")
+  end
+end if Rails.env.production?
