34697aa59448fd97d10568db9c8a35dc086292f6
Akshay Gupta <kitallis@gmail.com>
2020-10-13 17:39:25 +0530

    Add a sentry security header endpoint

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,11 +9,9 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src(:self, :https, :data)
   policy.img_src(:self, :https, :data)
   policy.object_src(:none)
-  policy.script_src(:self, :https, :unsafe_inline)
+  policy.script_src(:self, :https)
   policy.style_src(:self, :https, :unsafe_inline)
-
-  # Specify URI for violation reports
-  # policy.report_uri "/csp-violation-report-endpoint"
+  policy.report_uri ENV["SENTRY_SECURITY_HEADER_ENDPOINT"]
 end
 
 # If you are using UJS then enable automatic nonce generation
@@ -22,4 +20,4 @@ end
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
