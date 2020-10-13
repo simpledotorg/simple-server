014b5b41f2de6fa63f652c539bc26d529e012e40
Akshay Gupta <kitallis@gmail.com>
2020-10-13 18:45:00 +0530

    Add a reporting URL to report CSP errors to sentry

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,21 +3,23 @@
 # Define an application-wide content security policy
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+if Rails.env.production?
+  Rails.application.config.content_security_policy do |policy|
+    policy.default_src(:self, :https)
+    policy.font_src(:self, :https, :data)
+    policy.img_src(:self, :https, :data)
+    policy.object_src(:none)
+    policy.script_src(:self, :https, :unsafe_inline)
+    policy.style_src(:self, :https, :unsafe_inline)
 
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src(:self, :https)
-  policy.font_src(:self, :https, :data)
-  policy.img_src(:self, :https, :data)
-  policy.object_src(:none)
-  policy.script_src(:self, :https)
-  policy.style_src(:self, :https, :unsafe_inline)
-  policy.report_uri ENV["SENTRY_SECURITY_HEADER_ENDPOINT"]
+    report_uri = Addressable::URI.parse(ENV["SENTRY_SECURITY_HEADER_ENDPOINT"])
+    report_uri.query_values = report_uri.query_values&.merge(sentry_environment: ENV["SENTRY_CURRENT_ENV"])
+    policy.report_uri(report_uri.to_s)
+  end
+
+  # If you are using UJS then enable automatic nonce generation
+  # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+
+  Rails.application.config.content_security_policy_report_only = true
 end
 
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-Rails.application.config.content_security_policy_report_only = true
