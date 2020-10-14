71f26961aa97e1d67121f7ae76daeebcf1af2728
Rob Sanheim <rsanheim@gmail.com>
2020-10-13 19:23:17 -0500

    Add current git ref as version for Datadog tracking

--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -5,8 +5,15 @@ require "datadog/statsd"
 # in dev and test so we don't want to send payloads there.
 SEND_DATA_TO_DD_AGENT = !(Rails.env.development? || Rails.env.test?)
 
+CURRENT_GIT_REF = if Rails.root.join("REVISION").exist?
+  Rails.root.join("REVISION").read
+else
+  `git rev-parse HEAD`.chomp
+end
+
 Datadog.configure do |c|
   c.tracer.enabled = SEND_DATA_TO_DD_AGENT
+  c.version = CURRENT_GIT_REF
   c.use :rails, analytics_enabled: true
 end
 
