82d45fc35565fa3d898cd103dc92c805259a3a86
vkrm <mail@vkrm.is>
2020-10-14 17:21:02 +0530

    Skip sending registration_approval_email in non-production environments

--- app/mailers/approval_notifier_mailer.rb
+++ app/mailers/approval_notifier_mailer.rb
@@ -2,14 +2,18 @@ class ApprovalNotifierMailer < ApplicationMailer
   attr_reader :user
 
   def registration_approval_email(user_id:)
-    @user = User.find(user_id)
-    subject = I18n.t("registration_approval_email.subject",
-      full_name: @user.full_name,
-      org_name: @user.facility_group.organization.name)
-    mail(subject: subject,
-         to: supervisor_emails,
-         cc: organization_owner_emails,
-         bcc: owner_emails)
+    if ENV["SIMPLE_SERVER_ENV"] == "production"
+      @user = User.find(user_id)
+      subject = I18n.t("registration_approval_email.subject",
+        full_name: @user.full_name,
+        org_name: @user.facility_group.organization.name)
+      mail(subject: subject,
+           to: supervisor_emails,
+           cc: organization_owner_emails,
+           bcc: owner_emails)
+    else
+      Rails.logger.info "Non-production environment: skipped sending registration_approval_email"
+    end
   end
 
   def reset_password_approval_email(user_id:)
