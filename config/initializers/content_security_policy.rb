5bbe5d359f7a377ad7ff6b08754c6fc00654c9cb
Akshay Gupta <kitallis@gmail.com>
2020-10-14 12:47:23 +0530

    Trying a thing out…

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,16 +9,14 @@ if Rails.env.production?
     policy.font_src(:self, :https, :data)
     policy.img_src(:self, :https, :data)
     policy.object_src(:none)
-    policy.script_src(:self, :https, :unsafe_inline)
+    policy.script_src(:self, :https)
     policy.style_src(:self, :https, :unsafe_inline)
 
-    report_uri = Addressable::URI.parse(ENV["SENTRY_SECURITY_HEADER_ENDPOINT"])
-    report_uri.query_values = report_uri.query_values&.merge(sentry_environment: ENV["SENTRY_CURRENT_ENV"])
-    policy.report_uri(report_uri.to_s)
+    # report_uri = Addressable::URI.parse(ENV["SENTRY_SECURITY_HEADER_ENDPOINT"])
+    # report_uri.query_values = report_uri.query_values&.merge(sentry_environment: ENV["SENTRY_CURRENT_ENV"])
+    # policy.report_uri(report_uri.to_s)
   end
 
-  # If you are using UJS then enable automatic nonce generation
-  # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
-
-  Rails.application.config.content_security_policy_report_only = true
+  Rails.application.config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
+  Rails.application.config.content_security_policy_report_only = false
 end
