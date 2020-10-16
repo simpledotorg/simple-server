c00f14f3ce4cfb2c5c4af5a6a5b86a07ab050d1f
Rob Sanheim <rsanheim@gmail.com>
2020-10-16 13:40:10 -0500

    Try to skip app init during assets precompile on Heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module SimpleServer
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false if SIMPLE_SERVER_ENV == "review"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
