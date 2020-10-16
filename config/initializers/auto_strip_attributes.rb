78ad782ea037d3871fe268f62a1df8872f2d2797
Akshay Gupta <kitallis@gmail.com>
2020-10-16 18:51:20 +0530

    Don’t titleize, breaks too many specs

--- config/initializers/auto_strip_attributes.rb
+++ config/initializers/auto_strip_attributes.rb
@@ -2,8 +2,4 @@ AutoStripAttributes::Config.setup do
   set_filter(upcase_first: false) do |value|
     !value.blank? && value.respond_to?(:upcase_first) ? value.upcase_first : value
   end
-
-  set_filter(titleize: false) do |value|
-    !value.blank? && value.respond_to?(:titleize) ? value.titleize : value
-  end
 end
