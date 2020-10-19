adac896e3720cd480bc85dceab1438a873592e93
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-10-19 12:53:48 +0530

    Remove prod blocks yaml file

--- app/controllers/admin/fix_block_data_controller.rb
+++ app/controllers/admin/fix_block_data_controller.rb
@@ -1,16 +1,15 @@
 class Admin::FixBlockDataController < AdminController
   CANONICAL_BLOCKS = YAML.load_file("config/data/canonical_blocks.yml")
-  PROD_BLOCKS = YAML.load_file("config/data/prod_blocks.yml")
 
   skip_before_action :verify_authenticity_token
 
   def show
     authorize { current_admin.power_user? }
 
-    canonical_blocks = CANONICAL_BLOCKS.uniq.sort.compact
-    blocks = Facility.all.pluck(:zone).uniq.sort.reject(&:empty?)
+    canonical_blocks = CANONICAL_BLOCKS.uniq.sort.compact.join("\n")
+    blocks = Facility.all.pluck(:zone).uniq.sort.reject(&:empty?).join("\n")
 
-    @diff = Diffy::Diff.new(PROD_BLOCKS.join("\n") || blocks.join("\n"), canonical_blocks.join("\n"))
+    @diff = Diffy::Diff.new(blocks, canonical_blocks)
     @facility_count = Facility.group(:zone).count
   end
 
