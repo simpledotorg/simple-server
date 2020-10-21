0c7e80207fafe81b1901d819d1985942f5857ed9
Rob Sanheim <rsanheim@gmail.com>
2020-10-20 23:43:53 -0500

    go back to debug but leave it explicit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
-  config.log_level = :info
+  config.log_level = :debug
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
