5d801b23c7d704dea4b95614e0bae441e15d9917
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-10-21 17:01:07 +0530

    Fix spec description

--- spec/transformers/api/v3/facility_transformer_spec_spec.rb
+++ spec/transformers/api/v3/facility_transformer_spec_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Api::V3::FacilityTransformer do
   describe ".to_response" do
     let(:facility) { FactoryBot.build(:facility) }
 
-    it "sets the sync_region_id to the facility's zone" do
+    it "sets the sync_region_id to the facility group id" do
       transformed_facility = Api::V3::FacilityTransformer.to_response(facility)
       expect(transformed_facility[:sync_region_id]).to eq facility.facility_group_id
     end
