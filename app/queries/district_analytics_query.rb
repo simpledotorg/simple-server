32acabe328c841a56c8beb53c2a15e96b67f44b4
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-27 00:45:17 +0530

    Remove unused methods

--- app/queries/district_analytics_query.rb
+++ app/queries/district_analytics_query.rb
@@ -71,19 +71,7 @@ class DistrictAnalyticsQuery
   def registered_patients_by_period
     result = ActivityService.new(region, last: nil, group: [:registration_facility_id]).registrations
 
-    return group_by_date_and_facility(result, :registered_patients_by_period)
-
-    @registered_patients_by_period ||=
-      Patient
-        .with_hypertension
-        .joins(:registration_facility)
-        .where(facilities: {id: facilities})
-        .group("facilities.id")
-        .group_by_period(@period, :recorded_at)
-        .count
-
-
-    group_by_facility_and_date(@registered_patients_by_period, :registered_patients_by_period)
+    group_by_date_and_facility(result, :registered_patients_by_period)
   end
 
   def patients_with_bp_by_period
@@ -100,32 +88,7 @@ class DistrictAnalyticsQuery
   def follow_up_patients_by_period
     result = ActivityService.new(region, last: nil, group: BloodPressure.arel_table[:facility_id]).follow_ups
 
-    return group_by_date_and_facility(result, :follow_up_patients_by_period)
-    #
-    # this is similar to what the group_by_period query already gives us,
-    # however, groupdate does not allow us to use these "groups" in a where clause
-    # hence, we've replicated its grouping behaviour in order to remove the patients
-    # that were registered prior to the period bucket
-    #
-    date_truncate_string =
-      "(DATE_TRUNC('#{@period}', blood_pressures.recorded_at::timestamptz AT TIME ZONE '#{Groupdate.time_zone || 'Etc/UTC'}'))"
-
-    @follow_up_patients_by_period ||=
-      BloodPressure
-        .left_outer_joins(:user)
-        .left_outer_joins(patient: [:medical_history])
-        .joins(:facility)
-        .where(facilities: { id: facilities })
-        .where(deleted_at: nil)
-        .where("medical_histories.hypertension = ?", "yes")
-        .group('facilities.id')
-        .group_by_period(@period, 'blood_pressures.recorded_at')
-        .where("patients.recorded_at < #{date_truncate_string}")
-        .order('facilities.id')
-        .distinct("recorded_at")
-        .count('patients.id')
-
-    group_by_facility_and_date(@follow_up_patients_by_period, :follow_up_patients_by_period)
+    group_by_date_and_facility(result, :follow_up_patients_by_period)
   end
 
   private
