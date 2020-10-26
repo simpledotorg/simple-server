ec9d46004c8246695a7514c022c7a8f1e30613c5
Claudio Vallejo <hola@claudiovallejo.mx>
2020-10-26 16:49:50 -0400

    Set 'Ranked facilities' with filter js

--- app/assets/javascripts/common/my_facilities_filters.js
+++ app/assets/javascripts/common/my_facilities_filters.js
@@ -1,20 +1,27 @@
 window.addEventListener("DOMContentLoaded", function() {
   const $filtersForm = document.getElementById("query-filters");
   const $filterByZoneButtons = document.getElementsByClassName("filter-by-zone");
-  const $selectedZone = document.getElementById("selected-zone");
+  const $filterBySizeButtons = document.getElementsByClassName("filter-by-size");
 
   Array.from($filterByZoneButtons).forEach($button => {
     const zone = $button.getAttribute("data-value");
+
     $button.addEventListener("click", function() {
       const $selectedZone = document.getElementById("selected-zone");
+
       $selectedZone.setAttribute("value", zone);
       $filtersForm.submit();
     });
   });
 
-  function filterBySize(size) {
-    const $selectedSize = document.getElementById("selected-size");
-    $selectedSize.setAttribute("value", size == undefined ? "" : size);
-    $filtersForm.submit();
-  }
+  Array.from($filterBySizeButtons).forEach($button => {
+    const size = $button.getAttribute("data-value");
+
+    $button.addEventListener("click", function() {
+      const $selectedSize = document.getElementById("selected-size");
+
+      $selectedSize.setAttribute("value", size);
+      $filtersForm.submit();
+    });
+  });
 });
