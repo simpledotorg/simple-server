6fcb50174b492926d7d9a507e5763ee1cc61d7ff
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-27 15:52:10 +0530

    Update activity service spec for now

--- spec/services/activity_service_spec.rb
+++ spec/services/activity_service_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe ActivityService do
 
   it "something" do
     region = create(:facility)
-    result = ActivityService.new(region).call
-    pp result
+    activity_service = ActivityService.new(region)
+    pp activity_service.registrations
+    pp activity_service.follow_ups
+    pp activity_service.controlled_visits
   end
 end
