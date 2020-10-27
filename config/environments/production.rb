fe60ad413efd6e493b6eeecbaca033ba2e5b2f19
Akshay Gupta <kitallis@gmail.com>
2020-10-27 13:50:47 +0530

    Set default_url_options to SIMPLE_SERVER_HOST

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+  config.action_controller.default_url_options = {host: ENV.fetch("SIMPLE_SERVER_HOST")}
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
