6143b27a102da90946a1028ebbc4436b66204a78
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-28 12:45:03 +0530

    Remove dev code

--- app/controllers/api/v3/analytics/user_analytics_controller.rb
+++ app/controllers/api/v3/analytics/user_analytics_controller.rb
@@ -3,15 +3,7 @@ class Api::V3::Analytics::UserAnalyticsController < Api::V3::AnalyticsController
 
   layout false
 
-  skip_before_action :current_user_present?
-  skip_before_action :validate_sync_approval_status_allowed
-  skip_before_action :authenticate
-  skip_before_action :validate_facility
-  skip_before_action :validate_current_facility_belongs_to_users_facility_group
-
   def show
-    current_facility = Facility.find_by(name: "DH Udaipur")
-    @current_facility = Facility.find_by(name: "DH Udaipur")
     @user_analytics = UserAnalyticsPresenter.new(current_facility)
     respond_to_html_or_json(@user_analytics.statistics)
   end
