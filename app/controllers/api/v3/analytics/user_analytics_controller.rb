c497b3c954a75aab9fd1c812d78b26015f4c1db8
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-10-29 16:48:51 +0530

    Undo dev changes

--- app/controllers/api/v3/analytics/user_analytics_controller.rb
+++ app/controllers/api/v3/analytics/user_analytics_controller.rb
@@ -1,16 +1,9 @@
 class Api::V3::Analytics::UserAnalyticsController < Api::V3::AnalyticsController
   include ApplicationHelper
-  skip_before_action :current_user_present?,
-    :validate_sync_approval_status_allowed,
-    :authenticate,
-    :validate_facility,
-    :validate_current_facility_belongs_to_users_facility_group
 
   layout false
 
   def show
-    current_facility = Facility.first
-    @current_facility = Facility.first
     @user_analytics = UserAnalyticsPresenter.new(current_facility)
     respond_to_html_or_json(@user_analytics.statistics)
   end
