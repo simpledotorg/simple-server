19cbf5ed625fccf4dacb771e28aa3b1ba80c8c68
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-02 20:55:07 +0530

    Keep gist index

--- db/migrate/20201102150828_add_unique_path_index_to_region.rb
+++ db/migrate/20201102150828_add_unique_path_index_to_region.rb
@@ -1,6 +1,5 @@
 class AddUniquePathIndexToRegion < ActiveRecord::Migration[5.2]
   def change
-    remove_index :regions, :path
-    add_index :regions, :path, unique: true
+    add_index :regions, :path, unique: true, using: :btree, name: :index_regions_on_unique_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -479,7 +479,8 @@ ActiveRecord::Schema.define(version: 2020_11_02_150828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "region_type", null: false
-    t.index ["path"], name: "index_regions_on_path", unique: true
+    t.index ["path"], name: "index_regions_on_path", using: :gist
+    t.index ["path"], name: "index_regions_on_unique_path", unique: true
     t.index ["region_type"], name: "index_regions_on_region_type"
     t.index ["slug"], name: "index_regions_on_slug", unique: true
     t.index ["source_type", "source_id"], name: "index_regions_on_source_type_and_source_id"
