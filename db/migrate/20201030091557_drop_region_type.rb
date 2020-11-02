cd9a3bbda05271e894f32ecd0edd877b9310962c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-02 15:10:19 +0530

    Add index on Region

--- db/migrate/20201030091557_drop_region_type.rb
+++ db/migrate/20201030091557_drop_region_type.rb
@@ -3,5 +3,7 @@ class DropRegionType < ActiveRecord::Migration[5.2]
     remove_column :regions, :region_type_id
     drop_table :region_types
     add_column :regions, :region_type, :string, null: false
+
+    add_index :regions, :region_type
   end
 end
