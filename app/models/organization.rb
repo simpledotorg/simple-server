b64527f11c857bf7d68a10e9af32c0b1d9c3109c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-04 20:20:29 +0530

    Revert "Flipper flag org callbacks"
    This reverts commit c115edd1beb7adafe2171aae3d0e4e094b4a0f54.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -16,8 +16,8 @@ class Organization < ApplicationRecord
 
   # ----------------
   # Region callbacks
-  after_create :create_region, if: -> { Flipper.enabled?(:region_level_sync) }
-  before_update :update_region, if: -> { Flipper.enabled?(:region_level_sync) }
+  after_create :create_region
+  before_update :update_region
 
   def create_region
     return if region&.persisted?
