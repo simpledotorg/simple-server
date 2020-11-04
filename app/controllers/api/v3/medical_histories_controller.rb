7ef94a95da5f4c756d3aa90f15bde8cc0fce4a60
Akshay Gupta <kitallis@gmail.com>
2020-11-04 17:34:26 +0530

    Remove redundant code

--- app/controllers/api/v3/medical_histories_controller.rb
+++ app/controllers/api/v3/medical_histories_controller.rb
@@ -13,18 +13,6 @@ class Api::V3::MedicalHistoriesController < Api::V3::SyncController
 
   private
 
-  def current_facility_records
-    region_records.where(patient: Patient.syncable_to_region(current_facility))
-      .updated_on_server_since(current_facility_processed_since, limit)
-  end
-
-  def other_facility_records
-    other_facilities_limit = limit - current_facility_records.count
-
-    region_records.where.not(patient: Patient.syncable_to_region(current_facility))
-      .updated_on_server_since(other_facilities_processed_since, other_facilities_limit)
-  end
-
   def merge_if_valid(medical_history_params)
     validator = Api::V3::MedicalHistoryPayloadValidator.new(medical_history_params)
     logger.debug "Follow Up Schedule had errors: #{validator.errors_hash}" if validator.invalid?
