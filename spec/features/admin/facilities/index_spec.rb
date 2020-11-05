12a17c58a8e14ffd5f10f0c625c9a9e5e3ef8047
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-05 13:54:45 +0530

    Fix specs

--- spec/features/admin/facilities/index_spec.rb
+++ spec/features/admin/facilities/index_spec.rb
@@ -30,46 +30,91 @@ RSpec.feature "Facility page functionality", type: :feature do
         expect(page).to have_content("Bathinda")
       end
 
-      it "create new facility group without assigning any facility" do
-        facility_page.click_add_facility_group_button
+      context "when region_level_sync is disabled" do
+        it "create new facility group without assigning any facility" do
+          facility_page.click_add_facility_group_button
 
-        expect(page).to have_content("New facility group")
-        facility_group.add_new_facility_group_without_assigningfacility("IHMI", "testfacilitygroup", "testDescription", protocol_01.name)
+          expect(page).to have_content("New facility group")
+          facility_group.add_new_facility_group_without_assigningfacility("IHMI", "testfacilitygroup", "testDescription", nil, protocol_01.name)
 
-        expect(page).to have_content("Bathinda")
-        expect(page).to have_content("Testfacilitygroup")
+          expect(page).to have_content("Bathinda")
+          expect(page).to have_content("Testfacilitygroup")
+        end
+
+        it "create new facility group with facility" do
+          facility_page.click_add_facility_group_button
+
+          expect(page).to have_content("New facility group")
+          facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", nil, unassociated_facility.name, protocol_01.name)
+
+          expect(page).to have_content("Bathinda")
+          expect(page).to have_content("Testfacilitygroup")
+          facility_page.is_edit_button_present_for_facilitygroup("Testfacilitygroup")
+        end
+
+        it "admin should be able to edit facility group info " do
+          facility_page.click_add_facility_group_button
+          facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", nil, unassociated_facility.name, protocol_01.name)
+          facility_page.click_edit_button_present_for_facilitygroup("Testfacilitygroup")
+
+          # deselecting previously selected facility
+          facility_group.select_unassociated_facility(unassociated_facility.name)
+
+          # select new unassigned facility
+          facility_group.select_unassociated_facility(unassociated_facility02.name)
+          facility_group.click_on_update_facility_group_button
+
+          expect(page).to have_content(unassociated_facility02.name)
+        end
       end
 
-      it "create new facility group with facility" do
-        facility_page.click_add_facility_group_button
+      context "when region_level_sync is enabled" do
+        before do
+          enable_flag(:region_level_sync)
+        end
 
-        expect(page).to have_content("New facility group")
-        facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", unassociated_facility.name, protocol_01.name)
+        it "create new facility group without assigning any facility" do
+          facility_page.click_add_facility_group_button
 
-        expect(page).to have_content("Bathinda")
-        expect(page).to have_content("Testfacilitygroup")
-        facility_page.is_edit_button_present_for_facilitygroup("Testfacilitygroup")
+          expect(page).to have_content("New facility group")
+          facility_group.add_new_facility_group_without_assigningfacility("IHMI", "testfacilitygroup", "testDescription", "Punjab", protocol_01.name)
+
+          expect(page).to have_content("Bathinda")
+          expect(page).to have_content("Testfacilitygroup")
+        end
+
+        it "create new facility group with facility" do
+          facility_page.click_add_facility_group_button
+
+          expect(page).to have_content("New facility group")
+          facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", "Punjab", unassociated_facility.name, protocol_01.name)
+
+          expect(page).to have_content("Bathinda")
+          expect(page).to have_content("Testfacilitygroup")
+          facility_page.is_edit_button_present_for_facilitygroup("Testfacilitygroup")
+        end
+
+        it "admin should be able to edit facility group info " do
+          facility_page.click_add_facility_group_button
+          facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", "Punjab", unassociated_facility.name, protocol_01.name)
+          facility_page.click_edit_button_present_for_facilitygroup("Testfacilitygroup")
+
+          # deselecting previously selected facility
+          facility_group.select_unassociated_facility(unassociated_facility.name)
+
+          # select new unassigned facility
+          facility_group.select_unassociated_facility(unassociated_facility02.name)
+          facility_group.click_on_update_facility_group_button
+
+          expect(page).to have_content(unassociated_facility02.name)
+        end
       end
+
 
       it "admin should be able to delete facility group without facility " do
         facility_page.click_edit_button_present_for_facilitygroup(ihmi_group_bathinda.name)
         expect(page).to have_content("Edit facility group")
         facility_group.click_on_delete_facility_group_button
-      end
-
-      it "admin should be able to edit facility group info " do
-        facility_page.click_add_facility_group_button
-        facility_group.add_new_facility_group("IHMI", "testfacilitygroup", "testDescription", unassociated_facility.name, protocol_01.name)
-        facility_page.click_edit_button_present_for_facilitygroup("Testfacilitygroup")
-
-        # deselecting previously selected facility
-        facility_group.select_unassociated_facility(unassociated_facility.name)
-
-        # select new unassigned facility
-        facility_group.select_unassociated_facility(unassociated_facility02.name)
-        facility_group.click_on_update_facility_group_button
-
-        expect(page).to have_content(unassociated_facility02.name)
       end
     end
   end
