6bd2a65963140c4c7ba246e30fa9fdf5552e174e
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-09 16:52:03 +0530

    region_level_sync -> regions_prep

--- app/controllers/admin/facility_groups_controller.rb
+++ app/controllers/admin/facility_groups_controller.rb
@@ -5,7 +5,7 @@ class Admin::FacilityGroupsController < AdminController
   before_action :set_organizations, only: [:new, :edit, :update, :create]
   before_action :set_protocols, only: [:new, :edit, :update, :create]
   before_action :set_available_states, only: [:new, :create, :edit, :update],
-                                       if: -> { Flipper.enabled?(:region_level_sync) }
+                                       if: -> { Flipper.enabled?(:regions_prep) }
 
   def show
     @facilities = @facility_group.facilities.order(:name)
