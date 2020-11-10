26bfacf89ccb678ceb241c21d4f36f3952b71b12
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-10 18:26:41 +0530

    Update spec

--- spec/tasks/scripts/import_facility_block_names_spec.rb
+++ spec/tasks/scripts/import_facility_block_names_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ImportFacilityBlockNames do
   it "imports blocks for matching facilities and skip others" do
     create(:facility, name: "Facility 1", state: "Maharashtra", district: "Wardha")
 
-    expect(Rails.logger).to receive(:info).with("Facility Facility 2 in Block A, district Singapore not found")
+    expect(Rails.logger).to receive(:info).with("Facility Facility 2 in district Singapore not found")
     expect(Rails.logger).to receive(:info).with("Updated 1 facilities, 1 facilities not found")
     ImportFacilityBlockNames.import("spec/fixtures/files/facility_blocks_list.csv")
   end
