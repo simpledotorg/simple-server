de323c9afaf40e2043eb06aa81cb08f5dd016889
Rob Sanheim <rsanheim@gmail.com>
2020-11-13 18:59:20 -0600

    fix specs

--- spec/lib/seed/fake_names_spec.rb
+++ spec/lib/seed/fake_names_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Seed::FakeNames do
   it "can return fake village names" do
     fake_names = Seed::FakeNames.instance
     possible_village_names = fake_names._csv["Villages/Cities"]
-    expect(possible_village_names).to include(fake_names.village_name)
+    expect(possible_village_names).to include(fake_names.village)
   end
 
   it "can return fake org names" do
@@ -13,10 +13,10 @@ RSpec.describe Seed::FakeNames do
     expect(org_names).to include(fake_names.org_name)
   end
 
-  it "can return consisent org name for dev" do
+  it "can return consistent org name for the seed org" do
     fake_names = Seed::FakeNames.instance
     org_names = fake_names._csv["Organizations"]
-    expect(org_names).to include(fake_names.standard_dev_org_name)
-    expect(org_names.first).to eq(fake_names.standard_dev_org_name)
+    expect(org_names).to include(fake_names.seed_org_name)
+    expect(org_names.first).to eq(fake_names.seed_org_name)
   end
 end
