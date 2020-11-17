432614903c3dd4a67efa6a2d73979ff0ae99a343
Akshay Gupta <kitallis@gmail.com>
2020-11-17 19:58:29 +0530

    Remove the block trait since it’s not required rn

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -4,10 +4,6 @@ FactoryBot.define do
     name { Faker::Company.name }
     region_type { :organization }
 
-    trait :block do
-      region_type { :block }
-    end
-
     trait :state do
       region_type { :state }
     end
