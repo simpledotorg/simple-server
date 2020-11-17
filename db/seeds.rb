dad10809425d26c42fe9b35675bc91cc52e8db4b
Rob Sanheim <rsanheim@gmail.com>
2020-11-17 10:31:03 -0600

    fix typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Rails.logger "start seed task in #{Rails.nev} and #{SimpleServer.env}"
+Rails.logger "start seed task in #{Rails.env} and #{SimpleServer.env}"
 require_relative "../lib/tasks/scripts/create_admin_user"
 require "factory_bot_rails"
 require "faker"
