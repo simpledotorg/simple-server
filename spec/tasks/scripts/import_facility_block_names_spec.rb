3ac3caa7d2c21c6fab088be54b4ddf1ad33a71cb
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-11-19 14:06:04 +0530

    Revert "Fix bad merge" - wasn't a bad merge
    This reverts commit acb8bc584da98b4850dc5f5f9ebae73a530b22ed.

---
+++ spec/tasks/scripts/import_facility_block_names_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+require "tasks/scripts/import_facility_block_names"
+
+RSpec.describe ImportFacilityBlockNames do
+  it "imports blocks for matching facilities and skip others" do
+    fg = create(:facility_group, name: "Singapore")
+    facility = create(:facility,
+      name: "Facility 1",
+      state: "Maharashtra",
+      block: "Block B",
+      district: "Wardha",
+      facility_group: fg)
+
+    ImportFacilityBlockNames.import("spec/fixtures/files/facility_blocks_list.csv")
+    expect(facility.reload.block).to eq("Block B")
+  end
+end
