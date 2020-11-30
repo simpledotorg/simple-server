29ff7e781a6c0a02b062d1be12dc14365fd3507d
Akshay Gupta <kitallis@gmail.com>
2020-11-30 15:58:46 +0530

    we don’t need to autoload parsers separately

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module SimpleServer
       g.helper false
     end
 
-    config.autoload_paths += %W[#{config.root}/lib #{config.root}/parsers]
+    config.autoload_paths += %W[#{config.root}/lib]
 
     # Locale configuration
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
