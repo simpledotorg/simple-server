c91c28c9de76b7ee30ccc5ea9d5e0f9544222111
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-12-03 16:52:17 +0530

    Pull sync region id to function

--- app/controllers/api/v3/facilities_controller.rb
+++ app/controllers/api/v3/facilities_controller.rb
@@ -35,8 +35,16 @@ class Api::V3::FacilitiesController < Api::V3::SyncController
     {
       other_facilities_processed_since: processed_until(other_facility_records) || other_facilities_processed_since,
       resync_token: resync_token,
-      sync_region_id: current_sync_region&.id || Region.root.id
+      sync_region_id: temp_sync_region_id
     }
+  end
+
+  def temp_sync_region_id
+    if current_user
+      current_sync_region&.id
+    else
+      Region.root.id
+    end
   end
 
   def records_to_sync
