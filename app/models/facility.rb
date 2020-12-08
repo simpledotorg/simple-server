5885e53a83f763fe4443f1f8b2379bf6082f3c69
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-12-08 18:00:02 +0530

    First pass at localized labels for facility sizes

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -215,4 +215,14 @@ class Facility < ApplicationRecord
       errors.add(:zone, "not present in the facility group")
     end
   end
+
+  def self.localized_facility_size(facility_size)
+    return unless facility_size
+    I18n.t("activerecord.facility.facility_size.#{facility_size}")
+  end
+
+  def localized_facility_size
+    return unless facility_size
+    I18n.t("activerecord.facility.facility_size.#{facility_size}")
+  end
 end
