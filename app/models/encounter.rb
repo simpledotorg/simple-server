3aa4ff7877e279c4a483ab3e1b7afd2dff0902db
Akshay Gupta <kitallis@gmail.com>
2020-12-16 14:27:58 +0530

    Add for_sync to encounters

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -9,6 +9,8 @@ class Encounter < ApplicationRecord
   has_many :blood_pressures, through: :observations, source: :observable, source_type: "BloodPressure"
   has_many :blood_sugars, through: :observations, source: :observable, source_type: "BloodSugar"
 
+  scope :for_sync, -> { with_discarded }
+
   def self.generate_id(facility_id, patient_id, encountered_on)
     UUIDTools::UUID
       .sha1_create(UUIDTools::UUID_DNS_NAMESPACE,
