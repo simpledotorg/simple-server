53f78feed2abb9879907532435befb6e04bbf94b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2020-12-16 18:20:13 +0530

    Log more things in datadog

--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -10,6 +10,10 @@ Datadog.configure do |c|
   c.tracer.enabled = SEND_DATA_TO_DD_AGENT
   c.version = SimpleServer.git_ref(short: true)
   c.use :rails, analytics_enabled: true
+  c.use :action_view, analytics_enabled: true
+  c.use :active_record, analytics_enabled: true
+  c.use :action_pack, analytics_enabled: true
+  c.use :active_support, analytics_enabled: true
   c.use :rack, headers: {request: ["X-USER-ID", "X-FACILITY-ID", "X-SYNC-REGION-ID"], response: ["Content-Type", "X-Request-ID"]}
 end
 
