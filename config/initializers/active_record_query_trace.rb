726d5299f2c38e85c1f46bb9a7fe0e6f1abb538c
Rob Sanheim <rsanheim@gmail.com>
2020-12-23 22:14:48 -0600

    turn off query trace by default
    too noisy when you dont want this

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,6 +1,6 @@
 if Rails.env.development?
   # See https://github.com/brunofacca/active-record-query-trace for docs
+  ActiveRecordQueryTrace.enabled = false
   ActiveRecordQueryTrace.level = :app
-  ActiveRecordQueryTrace.enabled = true
   ActiveRecordQueryTrace.lines = 15
 end
