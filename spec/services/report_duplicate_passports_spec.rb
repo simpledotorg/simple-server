bb928d3b1c36e86b5b208401e4e8cb7503840ba9
Akshay Gupta <kitallis@gmail.com>
2020-12-25 13:44:32 +0530

    Add specs for duplicate_passport_count

--- spec/services/report_duplicate_passports_spec.rb
+++ spec/services/report_duplicate_passports_spec.rb
@@ -1,6 +1,56 @@
 require "rails_helper"
 
 RSpec.describe ReportDuplicatePassports do
+  describe '.duplicate_passports_count' do
+    context "for passports with the same identifiers" do
+      let!(:identifier) { SecureRandom.uuid }
+
+      it "does not have passports with same patient at the same facility" do
+        facility_id = SecureRandom.uuid
+        patient = create(:patient, business_identifiers: [])
+        create_list(:patient_business_identifier, 2, identifier: identifier, patient: patient, metadata: {assigningFacilityUuid: facility_id})
+
+        expect(described_class.new.duplicate_passports_count).to eq(0)
+      end
+
+      it "does not have passports with duplicate patients (different by id) at the same facility" do
+        facility_id = SecureRandom.uuid
+        patients = create_list(:patient, 2, business_identifiers: [])
+        create(:patient_business_identifier, identifier: identifier, patient: patients[0], metadata: {assigningFacilityUuid: facility_id})
+        create(:patient_business_identifier, identifier: identifier, patient: patients[1], metadata: {assigningFacilityUuid: facility_id})
+
+        expect(described_class.new.duplicate_passports_count).to eq(0)
+      end
+
+      it "does not have passports with the same patient assigned to it at different facilities" do
+        patient = create(:patient, business_identifiers: [])
+        create(:patient_business_identifier, identifier: identifier, patient: patient, metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        create(:patient_business_identifier, identifier: identifier, patient: patient, metadata: {assigningFacilityUuid: SecureRandom.uuid})
+
+        expect(described_class.new.duplicate_passports_count).to eq(0)
+      end
+
+      it "only has different patients assigned to a passport at different facilities" do
+        patients = create_list(:patient, 2, business_identifiers: [])
+        create(:patient_business_identifier, identifier: identifier, patient: patients[0], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        create(:patient_business_identifier, identifier: identifier, patient: patients[1], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+
+        expected_count = described_class.new.duplicate_passports.group(:identifier).count.keys.count
+        expect(described_class.new.duplicate_passports_count).to eq(expected_count)
+      end
+    end
+
+    context "for passports without the same identifiers" do
+      it "does not count them" do
+        patients = create_list(:patient, 2, business_identifiers: [])
+        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients[0], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients[1], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+
+        expect(described_class.new.duplicate_passports_count).to eq(0)
+      end
+    end
+  end
+
   describe ".duplicate_passports" do
     context "for passports with the same identifiers" do
       let!(:identifier) { SecureRandom.uuid }
@@ -16,9 +66,8 @@ RSpec.describe ReportDuplicatePassports do
       it "does not have passports with duplicate patients (different by id) at the same facility" do
         facility_id = SecureRandom.uuid
         patients = create_list(:patient, 2, business_identifiers: [])
-        patients.map do |patient|
-          create(:patient_business_identifier, identifier: identifier, patient: patient, metadata: {assigningFacilityUuid: facility_id})
-        end
+        create(:patient_business_identifier, identifier: identifier, patient: patients[0], metadata: {assigningFacilityUuid: facility_id})
+        create(:patient_business_identifier, identifier: identifier, patient: patients[1], metadata: {assigningFacilityUuid: facility_id})
 
         expect(described_class.new.duplicate_passports).to be_empty
       end
@@ -33,21 +82,20 @@ RSpec.describe ReportDuplicatePassports do
 
       it "only has different patients assigned to a passport at different facilities" do
         patients = create_list(:patient, 2, business_identifiers: [])
-        identifiers =
-          [create(:patient_business_identifier, identifier: identifier, patient: patients.first, metadata: {assigningFacilityUuid: SecureRandom.uuid}),
-            create(:patient_business_identifier, identifier: identifier, patient: patients.second, metadata: {assigningFacilityUuid: SecureRandom.uuid})]
+        identifiers = [
+          create(:patient_business_identifier, identifier: identifier, patient: patients[0], metadata: {assigningFacilityUuid: SecureRandom.uuid}),
+          create(:patient_business_identifier, identifier: identifier, patient: patients[1], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        ]
 
         expect(described_class.new.duplicate_passports).to match_array identifiers
-        expected_count = described_class.new.duplicate_passports.group(:identifier).count.keys.count
-        expect(described_class.new.duplicate_passports_count).to eq(expected_count)
       end
     end
 
     context "for passports without the same identifiers" do
       it "does not count them" do
         patients = create_list(:patient, 2, business_identifiers: [])
-        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients.first, metadata: {assigningFacilityUuid: SecureRandom.uuid})
-        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients.second, metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients[0], metadata: {assigningFacilityUuid: SecureRandom.uuid})
+        create(:patient_business_identifier, identifier: SecureRandom.uuid, patient: patients[1], metadata: {assigningFacilityUuid: SecureRandom.uuid})
 
         expect(described_class.new.duplicate_passports).to be_empty
       end
