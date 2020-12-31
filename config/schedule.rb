e1580ddb60bc8965ebd093fc3d883dc817037c14
Akshay Gupta <kitallis@gmail.com>
2020-12-31 12:48:07 +0530

    Put the new seed script in the scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -35,6 +35,10 @@ every :day, at: local("02:00 am"), roles: [:cron] do
   runner "Reports::RegionCacheWarmer.call"
 end
 
+every :month, at: local("04:00 am"), roles: [:seed_data] do
+  rake "db:purge_and_reseed"
+end
+
 every :day, at: local("05:00 am"), roles: [:cron] do
   runner "ReportDuplicatePassports.report"
 end
