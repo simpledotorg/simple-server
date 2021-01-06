c4aa2f02f6af73c9fd4c556d498dfc9d96f8e822
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-01-06 15:32:23 +0530

    Refine specs

--- spec/mailers/patient_list_download_mailer_spec.rb
+++ spec/mailers/patient_list_download_mailer_spec.rb
@@ -3,19 +3,28 @@ require "rails_helper"
 RSpec.describe PatientListDownloadMailer, type: :mailer do
   describe "#patient_list" do
     let(:csv_content) { "This is a test csv" }
+    let!(:recipient_email) { "test@simple.org" }
     let!(:mail) {
       described_class.patient_list(
-        "test@simple.org",
+        recipient_email,
         "facility_group",
         "Bhandara",
         csv_content
       )
     }
 
-    it "sends a compresses the csv into a valid zip file and sends as attachment" do
+    it "sends an email to the recipient" do
+      expect(mail.to.first).to eq recipient_email
+    end
+
+    it "sends a zip archive in the attachment" do
       attachment = mail.attachments.first
       expect(attachment.content_type).to eq "application/zip"
       expect(attachment.filename).to eq "patient-list_facility_group_Bhandara_#{I18n.l(Date.current)}.csv.zip"
+    end
+
+    it "compresses the csv in a valid zip archive as attachment" do
+      attachment = mail.attachments.first
 
       Zip::InputStream.open(StringIO.new(attachment.body.raw_source)) do |io|
         entry = io.get_next_entry
