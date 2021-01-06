ac8c08b72aa06321c11ab14edc92ac86e7ae70b8
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-01-06 14:44:07 +0530

    Compress (zip) line list CSV

--- app/mailers/patient_list_download_mailer.rb
+++ app/mailers/patient_list_download_mailer.rb
@@ -6,11 +6,23 @@ class PatientListDownloadMailer < ApplicationMailer
     subject = I18n.t("patient_list_email.subject", model_type: @model_type, model_name: @model_name)
 
     file_name = "patient-list_#{model_type}_#{@model_name.strip}_#{I18n.l(Date.current)}.csv"
-    attachments[file_name] = {
-      mime_type: "text/csv",
-      content: patients_csv
+    zip_file_name = "#{file_name}.zip"
+    zip_file = compress_file(file_name, patients_csv)
+
+    attachments[zip_file_name] = {
+      mime_type: "application/zip",
+      content: zip_file
     }
 
     mail(to: recipient_email, subject: subject)
+  end
+
+  def compress_file(file_name, file_data)
+    output_buffer = Zip::OutputStream.write_buffer do |zip|
+      zip.put_next_entry(file_name)
+      zip.write file_data
+    end
+
+    output_buffer.string
   end
 end
