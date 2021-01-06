6df428ffd068f71a1bfa877ad4ad3904dcb3c199
Akshay Gupta <kitallis@gmail.com>
2021-01-06 14:44:18 +0530

    Use sidekiq as default adapter in production for ActiveJob

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   end
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "simple-server_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
