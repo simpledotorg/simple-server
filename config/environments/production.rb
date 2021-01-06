ef7b4bbbe8259559d83ee4a394f7957839017991
Akshay Gupta <kitallis@gmail.com>
2021-01-06 16:47:01 +0530

    Set default mailer queue to be default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "simple-server_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.deliver_later_queue_name = "default"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
