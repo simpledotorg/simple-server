f51b31fe7e056709949c5e56876a6a3664d05e69
vkrmis <48791115+vkrmis@users.noreply.github.com>
2021-01-12 09:36:57 +0000

    Add drug stock fields to protocol drugs (#1958)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_074852) do
+ActiveRecord::Schema.define(version: 2021_01_07_060947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -462,6 +462,8 @@ ActiveRecord::Schema.define(version: 2020_12_24_074852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "drug_category"
+    t.boolean "stock_tracked", default: false, null: false
     t.index ["deleted_at"], name: "index_protocol_drugs_on_deleted_at"
   end
 
