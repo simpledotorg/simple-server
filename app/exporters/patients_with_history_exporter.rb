f7dd86bc3231a993cf6e733ca37079dd42a63bdc
Akshay Gupta <kitallis@gmail.com>
2021-01-19 18:42:43 +0530

    Sort appointments in ASC order

--- app/exporters/patients_with_history_exporter.rb
+++ app/exporters/patients_with_history_exporter.rb
@@ -81,7 +81,7 @@ class PatientsWithHistoryExporter < PatientsExporter
 
     patient_appointments = patient_summary
       .appointments
-      .sort { |prev, after| after.device_created_at <=> prev.device_created_at }
+      .sort { |prev, after| prev.device_created_at <=> after.device_created_at }
 
     csv_fields = [
       registration_date(patient_summary),
