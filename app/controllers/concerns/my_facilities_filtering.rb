bf2f25f517215bea4c89fdf2bab0f0caa51644eb
Claudio Vallejo <hola@claudiovallejo.mx>
2021-01-19 10:23:36 -0500

    Cleaned my_facilities_filtering module

--- app/controllers/concerns/my_facilities_filtering.rb
+++ app/controllers/concerns/my_facilities_filtering.rb
@@ -28,12 +28,12 @@ module MyFacilitiesFiltering
       @facility_groups = FacilityGroup.where(id: @accessible_facilities.map(&:facility_group_id).uniq).order(:name)
     end
 
-    def populate_facility_sizes
-      @facility_sizes = @accessible_facilities.where(facility_group: @selected_facility_group, zone: @selected_zones).pluck(:facility_size).uniq.compact.sort
-    end
-
     def populate_zones
       @zones = @accessible_facilities.where(facility_group: @selected_facility_group).pluck(:zone).uniq.compact.sort
+    end
+
+    def populate_facility_sizes
+      @facility_sizes = @accessible_facilities.where(facility_group: @selected_facility_group, zone: @selected_zones).pluck(:facility_size).uniq.compact.sort
     end
 
     def set_selected_facility_group
@@ -52,16 +52,16 @@ module MyFacilitiesFiltering
       facilities.where(facility_group: @selected_facility_group)
     end
 
+    def facilities_by_zone(facilities)
+      facilities.where(zone: @selected_zones).or(facilities.where(zone: nil))
+    end
+
     def facilities_by_size(facilities)
       if (@facility_sizes - @selected_sizes).empty?
         facilities
       else
         facilities.where(facility_size: @selected_sizes)
       end
-    end
-
-    def facilities_by_zone(facilities)
-      facilities.where(zone: @selected_zones).or(facilities.where(zone: nil))
     end
   end
 end
