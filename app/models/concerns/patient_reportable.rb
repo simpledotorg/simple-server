b427e1ef00631433561b085936c28f2424815db6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-01-21 19:24:57 +0530

    Spike out ltfu exclusions

--- app/models/concerns/patient_reportable.rb
+++ app/models/concerns/patient_reportable.rb
@@ -1,15 +1,25 @@
 module PatientReportable
   extend ActiveSupport::Concern
+  LTFU_PERIOD = 12.months
 
   included do
     scope :with_diabetes, -> { joins(:medical_history).merge(MedicalHistory.diabetes_yes).distinct }
     scope :with_hypertension, -> { joins(:medical_history).merge(MedicalHistory.hypertension_yes).distinct }
 
     scope :excluding_dead, -> { where.not(status: :dead) }
+    scope :excluding_ltfu, -> {
+      joins(:latest_blood_pressures_per_patient)
+        .where("latest_blood_pressures_per_patients.bp_recorded_at < ?", LTFU_PERIOD.ago)
+        .where("latest_blood_pressures_per_patients.patient_recorded_at < ?", LTFU_PERIOD.ago)
+    }
 
-    scope :for_reports, ->(with_exclusions: false) do
+    scope :for_reports, ->(with_exclusions: false, excluding_ltfu: true) do
       if with_exclusions
-        with_hypertension.excluding_dead
+        if excluding_ltfu
+          with_hypertension.excluding_dead.excluding_ltfu
+        else
+          with_hypertension.excluding_dead
+        end
       else
         with_hypertension
       end
