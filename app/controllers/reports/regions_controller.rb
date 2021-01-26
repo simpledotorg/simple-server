0ef574a86970564748630d53d80b88a83df753ed
Rob Sanheim <rsanheim@gmail.com>
2021-01-26 14:11:51 -0600

    Disable patient list downloads for state and block (#2011)
    * Disable patient list downloads for state and block
    
    These require further work and refactoring
    
    * Do the delivery in process
    
    We are already in the scope of a job here, so there is no reason to
    resubmit another job, especially because the payload here can be *huge*.
    We don't want to pass the entire CSV back into sidekiq when we have
    already retrieved it a few lines above.
    
    * Use region_type for the file name
    
    * Make the @region always an actual Region
    
    at least wherever possible...the exception here is FacilityDistrict
    
    * FacilityDistrict needs to know its region_type
    
    * Filename should be district now

--- app/controllers/reports/regions_controller.rb
+++ app/controllers/reports/regions_controller.rb
@@ -130,7 +130,7 @@ class Reports::RegionsController < AdminController
   def download_filename
     time = Time.current.to_s(:number)
     region_name = @region.name.tr(" ", "-")
-    "#{@region.source.class.to_s.underscore}-#{@period.adjective.downcase}-cohort-report_#{region_name}_#{time}.csv"
+    "#{@region.region_type.to_s.underscore}-#{@period.adjective.downcase}-cohort-report_#{region_name}_#{time}.csv"
   end
 
   def set_facility_keys
@@ -172,7 +172,7 @@ class Reports::RegionsController < AdminController
         if region_reports_enabled?
           current_admin.accessible_district_regions(:view_reports).find_by!(slug: report_params[:id])
         else
-          current_admin.accessible_facility_groups(:view_reports).find_by!(slug: report_params[:id])
+          current_admin.accessible_facility_groups(:view_reports).find_by!(slug: report_params[:id]).region
         end
       when "block"
         current_admin.accessible_block_regions(:view_reports).find_by!(slug: report_params[:id])
@@ -180,7 +180,7 @@ class Reports::RegionsController < AdminController
         if region_reports_enabled?
           current_admin.accessible_facility_regions(:view_reports).find_by!(slug: report_params[:id])
         else
-          current_admin.accessible_facilities(:view_reports).find_by!(slug: report_params[:id])
+          current_admin.accessible_facilities(:view_reports).find_by!(slug: report_params[:id]).region
         end
       else
         raise ActiveRecord::RecordNotFound, "unknown report_scope #{report_scope}"
