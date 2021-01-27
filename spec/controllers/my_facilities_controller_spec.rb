c8a9d10ec85f5568cb5ef1eb05c197affd2f4faf
Claudio Vallejo <hola@claudiovallejo.mx>
2021-01-27 16:50:39 -0500

    Fixing failed checks

--- spec/controllers/my_facilities_controller_spec.rb
+++ spec/controllers/my_facilities_controller_spec.rb
@@ -35,21 +35,25 @@ RSpec.describe MyFacilitiesController, type: :controller do
   describe "GET #bp_controlled" do
     it "returns a success response" do
       facility = create(:facility, facility_group: facility_group)
-      controlled = Timecop.freeze("August 15th 2020") do
+
+      controlled = Timecop.freeze("August 15th 2020") { 
         create_list(:patient, 2, full_name: "controlled", assigned_facility: facility, registration_user: supervisor)
-      end
-      Timecop.freeze("September 20th 2020") do
+      } 
+
+      Timecop.freeze("September 20th 2020") {
         controlled.each { |patient| create(:blood_pressure, :under_control, patient: patient, facility: facility) }
-      end
+      }
 
       refresh_views
 
-      Timecop.freeze("January 15th 2021") do
+      Timecop.freeze("January 15th 2021") {
         get :bp_controlled, params: {}
-      end
+      }
 
       expect(response).to be_successful
+
       facility_data = assigns(:data_for_facility)[facility.name]
+
       expect(facility_data[:adjusted_registrations][Period.month("December 2020")]).to eq(2)
       expect(facility_data[:controlled_patients_rate][Period.month("November 2020")]).to eq(100)
     end
@@ -59,17 +63,20 @@ RSpec.describe MyFacilitiesController, type: :controller do
       facility_1 = create(:facility, facility_group: other_district)
       supervisor.accesses.create! resource: other_district
       facility_2 = create(:facility, facility_group: facility_group)
-      controlled = Timecop.freeze("August 15th 2020") do
+
+      controlled = Timecop.freeze("August 15th 2020") {
         create_list(:patient, 2, full_name: "controlled in facility_1", assigned_facility: facility_1, registration_user: supervisor)
         create_list(:patient, 2, full_name: "controlled in facility_2", assigned_facility: facility_2, registration_user: supervisor)
-      end
+      }
 
       refresh_views
 
-      Timecop.freeze("January 15th 2021") do
+      Timecop.freeze("January 15th 2021") {
         get :bp_controlled, params: { facility_group: other_district.slug }
-      end
+      }
+
       expect(response).to be_successful
+
       expect(assigns(:data_for_facility)[facility_1.name]).to_not be_nil
       expect(assigns(:data_for_facility)[facility_2.name]).to be_nil
     end
@@ -78,21 +85,24 @@ RSpec.describe MyFacilitiesController, type: :controller do
   describe "GET #bp_not_controlled" do
     it "returns a success response" do
       facility = create(:facility, facility_group: facility_group)
-      controlled = Timecop.freeze("August 15th 2020") do
+      controlled = Timecop.freeze("August 15th 2020") {
         create_list(:patient, 2, full_name: "uncontrolled", assigned_facility: facility, registration_user: supervisor)
-      end
-      Timecop.freeze("September 20th 2020") do
+      }
+
+      Timecop.freeze("September 20th 2020") {
         controlled.each { |patient| create(:blood_pressure, patient: patient, facility: facility) }
-      end
+      }
 
       refresh_views
 
-      Timecop.freeze("January 15th 2021") do
+      Timecop.freeze("January 15th 2021") {
         get :bp_controlled, params: {}
-      end
+      }
 
       expect(response).to be_successful
+
       facility_data = assigns(:data_for_facility)[facility.name]
+
       expect(facility_data[:adjusted_registrations][Period.month("December 2020")]).to eq(2)
       expect(facility_data[:uncontrolled_patients_rate][Period.month("November 2020")]).to eq(100)
     end
@@ -101,21 +111,25 @@ RSpec.describe MyFacilitiesController, type: :controller do
   describe "GET #missed_visits" do
     it "returns a success response" do
       facility = create(:facility, facility_group: facility_group)
+
       controlled = Timecop.freeze("August 15th 2020") do
         create_list(:patient, 2, full_name: "controlled", assigned_facility: facility, registration_user: supervisor)
       end
+
       Timecop.freeze("September 20th 2020") do
         controlled.each { |patient| create(:blood_pressure, :under_control, patient: patient, facility: facility) }
       end
 
       refresh_views
 
-      Timecop.freeze("January 15th 2021") do
+      Timecop.freeze("January 15th 2021") {
         get :bp_controlled, params: {}
-      end
+      }
 
       expect(response).to be_successful
+
       facility_data = assigns(:data_for_facility)[facility.name]
+
       expect(facility_data[:adjusted_registrations][Period.month("December 2020")]).to eq(2)
       expect(facility_data[:missed_visits_rate][Period.month("December 2020")]).to eq(100)
     end
