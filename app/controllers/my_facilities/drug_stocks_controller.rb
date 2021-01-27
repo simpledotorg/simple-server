0f35fc46fb3f156553ada7d9686cc1e55872aa98
Srihari Sriraman <srihari@nilenso.com>
2021-01-27 18:13:44 +0530

    First cut of drug stock reporting
    - get list of facilities in page (already have this)
    - run query to fetch patient count for given facilities (group by query we wrote, except pass in facilities)
    - run query to drug stock data for facilities given the month (hardcode first, then parameterize)
    - for each facility, compute report without patient days
    - for each facility, calculate patient days
      -  Drug stock formula(inventory(), patient_count)

--- app/controllers/my_facilities/drug_stocks_controller.rb
+++ app/controllers/my_facilities/drug_stocks_controller.rb
@@ -12,8 +12,66 @@ class MyFacilities::DrugStocksController < AdminController
 
   def index
     @facilities = filter_facilities
-      .includes(facility_group: :protocol_drugs)
-      .where(protocol_drugs: {stock_tracked: true})
+                    .includes(facility_group: :protocol_drugs)
+                    .where(protocol_drugs: { stock_tracked: true })
+
+    render and return if @facilities.empty?
+    # handle no facilities
+    for_end_of_month = Date.strptime("January 2021", "%B %Y").end_of_month
+    patient_counts = Patient.where(assigned_facility: @facilities.map(&:id)).group(:assigned_facility).count
+    latest_drug_stocks = DrugStock.latest_for_facilities(@facilities, for_end_of_month)
+    # assuming that all facilities on the page have the same protocol
+    protocol = @facilities.first.protocol
+    drug_categories = protocol.protocol_drugs.where(stock_tracked: true).group_by(&:drug_category)
+    report = patient_counts.each_with_object({}) { |(facility, patient_count), report|
+      report[facility.id] = {}
+      report[facility.id][:patient_count] = patient_count
+      next if latest_drug_stocks[facility.id].nil?
+      drug_categories.each do |drug_category|
+        report[facility.id][drug_category] = {
+          ## key this by name
+          latest_drug_stocks: latest_drug_stocks[facility.id].select { |drug_stock| drug_stock[:drug_category] },
+          patient_days: patient_days(latest_drug_stocks[facility.id], patient_count)
+        }
+      end
+    }
+  end
+
+  def patient_days(latest_drug_stocks, patient_count)
+    100
+  end
+
+  def rxnorm_codes
+    {
+      "amLODIPine 5mg": "329528",
+      "amlo 10": "329526",
+      "telmi 40": "316764",
+      "telmi 80": "316765",
+      "losartan 50": "979467",
+      "chlorthalidone 12.5": "331132",
+      "chlorthalidone 25": "315655"
+    }
+
+    ProtocolDrug.find("ea1377c3-5c9b-42eb-8087-ad99cd80ce5a").update(stock_tracked: true, rxnorm_code: "329528", drug_category: "hypertension_ccb")
+    ProtocolDrug.find("b57bb05b-992f-4376-b353-9df56c2224c9").update(stock_tracked: true, rxnorm_code: "329526", drug_category: "hypertension_ccb")
+    ProtocolDrug.find("2c2b870f-eb1d-41f7-b803-4b5ae5b64e25").update(stock_tracked: true, rxnorm_code: "316764", drug_category: "hypertension_arb")
+    ProtocolDrug.find("0588c3c3-007f-4725-9def-60d49fd379ca").update(stock_tracked: true, rxnorm_code: "316765", drug_category: "hypertension_arb")
+    ProtocolDrug.find("893d2ee3-4a8a-45fd-9213-4d25788c556d").update(stock_tracked: true, rxnorm_code: "979467", drug_category: "hypertension_arb")
+    ProtocolDrug.find("4bf6e927-7c04-478f-95f8-22602a8adb20").update(stock_tracked: true, rxnorm_code: "331132", drug_category: "hypertension_diuretic")
+    ProtocolDrug.find("7ffcc130-88af-4290-a770-6b9ebe5eddfc").update(stock_tracked: true, rxnorm_code: "315655", drug_category: "hypertension_diuretic")
+  end
+
+  def patient_days_coefficients
+    { "Punjab":
+        { load_factor: 1,
+          drug_categories:
+            { "hypertension_ccb":
+                { new_patient_coefficient: 1.4,
+                },
+              "hypertension_arb": {},
+              "hypertension_diuretic": {} }
+        }
+    }
   end
 
   def new
--- app/models/drug_stock.rb
+++ app/models/drug_stock.rb
@@ -7,9 +7,14 @@ class DrugStock < ApplicationRecord
   validates :received, numericality: true, allow_nil: true
   validates :for_end_of_month, presence: true
 
+  def self.latest_for_facilities(facilities, for_end_of_month)
+    DrugStock.select("DISTINCT ON (facility_id, protocol_drug_id) *")
+      .where(facility: facilities.pluck(:id), for_end_of_month: for_end_of_month)
+      .order(:facility_id, :protocol_drug_id, created_at: :desc)
+      .group_by(&:facility_id)
+  end
+
   def self.latest_for_facility(facility, for_end_of_month)
-    DrugStock.select("DISTINCT ON (protocol_drug_id) *")
-      .where(facility_id: facility.id, for_end_of_month: for_end_of_month)
-      .order(:protocol_drug_id, created_at: :desc)
+    self.latest_for_facilities([facility], for_end_of_month)[facility.id]
   end
 end
