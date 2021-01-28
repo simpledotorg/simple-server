896cfbb20c98b113b01cb554715129b24d6f76bd
Rob Sanheim <rsanheim@gmail.com>
2021-01-28 12:00:48 -0600

    Remove sanitize_fields - no longer necessary w/ sentry-ruby

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,6 +7,5 @@ class SentryJob < ActiveJob::Base
 end
 
 Sentry.init do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.async = ->(event) { SentryJob.perform_later(event) }
 end
