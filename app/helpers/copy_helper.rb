569c1e423606cb292a54509cf9682237d8f6898d
Claudio Vallejo <hola@claudiovallejo.mx>
2021-02-02 14:06:33 -0500

    Created a copy helper for reports numerator and denominator copy

---
+++ app/helpers/copy_helper.rb
@@ -0,0 +1,6 @@
+module CopyHelper
+  BP_CONTROLLED_NUMERATOR_COPY = "Patients with BP <140/90 at their most recent visit in the last 3 months"
+  BP_NOT_CONTROLLED_NUMERATOR_COPY = "Patients with BP ≥140/90 at their most recent visit in the last 3 months"
+  MISSED_VISITS_NUMERATOR_COPY = "Patients with no visit in >3 months"
+  DENOMINATOR_COPY = "Assigned hypertension patients registered before the last 3 months"
+end
