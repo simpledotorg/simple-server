424cd826c21d2158cd3a2e98ca418ed952238c6c
Claudio Vallejo <hola@claudiovallejo.mx>
2021-02-02 18:57:34 -0500

    Moved copy_helper constants to en.yml, Using localized strings in Dashboard instead of CopyHelper

--- app/helpers/copy_helper.rb
+++
@@ -1,7 +0,0 @@
-module CopyHelper
-  BP_CONTROLLED_NUMERATOR_COPY = "Patients with BP <140/90 at their most recent visit in the last 3 months"
-  BP_NOT_CONTROLLED_NUMERATOR_COPY = "Patients with BP ≥140/90 at their most recent visit in the last 3 months"
-  MISSED_VISITS_NUMERATOR_COPY = "Patients with no visit in >3 months"
-  VISITED_BUT_NO_BP_TAKEN_NUMERATOR_COPY = "Patients with a visit in the last 3 months but no BP taken"
-  DENOMINATOR_COPY = "Assigned hypertension patients registered before the last 3 months"
-end
