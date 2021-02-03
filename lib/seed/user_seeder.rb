1e678dc0f674bd6c0159d75369a8837cb0e6dd9a
Rob Sanheim <rsanheim@gmail.com>
2021-02-03 14:28:36 -0600

    Add more stock admins

--- lib/seed/user_seeder.rb
+++ lib/seed/user_seeder.rb
@@ -27,6 +27,12 @@ module Seed
       create_mobile_users
     end
 
+    DISTRICTS_MANAGED_BY_CVHO = 2
+    DISTRICTS_FOR_STS = 1
+    DISTRICTS_FOR_DISTRICT_OFFICIAL = 1
+    FACILITIES_FOR_MED_OFFICER = 1
+    DISTRICTS_FOR_DISTRICT_OFFICIAL = 1
+
     private
 
     def create_dashboard_admins
@@ -34,13 +40,44 @@ module Seed
         FactoryBot.create(:admin, :power_user, full_name: "Power User", email: "power_user@simple.org", password: config.admin_password)
       end
 
-      unless EmailAuthentication.exists?(email: "cvho@simple.org")
-        fg_1, fg_2 = *FacilityGroup.take(2)
-        user = FactoryBot.create(:admin, :manager, full_name: "CVHO", email: "cvho@simple.org", password: config.admin_password)
-        user.accesses.create! resource_id: fg_1.id, resource_type: "FacilityGroup"
-        user.accesses.create! resource_id: fg_2.id, resource_type: "FacilityGroup"
+      cvho = EmailAuthentication.find_by(email: "cvho@simple.org")
+      unless cvho && cvho.user.accesses.where(resource_type: "FacilityGroup").count == DISTRICTS_MANAGED_BY_CVHO
+        districts = *FacilityGroup.take(DISTRICTS_MANAGED_BY_CVHO)
+        user = cvho || FactoryBot.create(:admin, :manager, full_name: "CVHO", email: "cvho@simple.org", password: config.admin_password)
+        districts.each do |district|
+          user.accesses.create! resource: district
+        end
       end
+
+      sts = User.find_by_email(email: "sts@simple.org")
+      unless sts && sts.user.accesses.where(resource_type: "FacilityGroup").count == DISTRICTS_FOR_STS
+        districts = *FacilityGroup.order("name desc").take(DISTRICTS_FOR_STS)
+        user = sts || FactoryBot.create(:admin, :viewer_all, full_name: "STS", email: "sts@simple.org", password: config.admin_password)
+        districts.each do |district|
+          user.accesses.create! resource: district
+        end
+      end
+
+      district_official = EmailAuthentication.find_by(email: "district_official@simple.org")
+      unless district_official && district_official.user.accesses.where(resource_type: "FacilityGroup").count == DISTRICTS_FOR_DISTRICT_OFFICIAL
+        districts = *FacilityGroup.take(DISTRICTS_FOR_DISTRICT_OFFICIAL)
+        user = district_official || FactoryBot.create(:admin, :viewer_reports_only, full_name: "District Official", email: "district_official@simple.org", password: config.admin_password)
+        districts.each do |district|
+          user.accesses.create! resource: district
+        end
+      end
+
+      medical_officer = EmailAuthentication.find_by(email: "medical_officer@simple.org")
+      unless medical_officer && medical_officer.user.accesses.where(resource_type: "FacilityGroup").count == FACILITIES_FOR_MED_OFFICER
+        facilities = *Facility.take(FACILITIES_FOR_MED_OFFICER)
+        user = medical_officer || FactoryBot.create(:admin, :viewer_all, full_name: "Medical Officer", email: "medical_officer@simple.org", password: config.admin_password)
+        facilities.each do |facility|
+          user.accesses.create! resource: facility
+        end
+      end
+
     end
+
 
     def create_mobile_users
       facility_ids = Facility.pluck(:id)
--- spec/lib/seed/user_seeder_spec.rb
+++ spec/lib/seed/user_seeder_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Seed::UserSeeder do
   let(:config) { Seed::Config.new }
+  let(:expected_admins) { 5 }
 
   it "creates standard user types" do
     create_list(:facility_group, 2)
@@ -21,7 +22,7 @@ RSpec.describe Seed::UserSeeder do
     create_list(:facility, 2)
     expect {
       Seed::UserSeeder.call(config: config)
-    }.to change { User.admins.count }.by(2)
+    }.to change { User.admins.count }.by(expected_admins)
     expect {
       Seed::UserSeeder.call(config: config)
     }.to change { User.admins.count }.by(0)
@@ -30,10 +31,9 @@ RSpec.describe Seed::UserSeeder do
   it "creates Users for each facility" do
     create_list(:facility, 5, facility_size: "community")
 
-    expected_dashboard_users = 2
     expected_users_per_facility = config.max_number_of_users_per_facility
     expected_phone_sync_users = expected_users_per_facility * Facility.count
-    expected_total_users = expected_dashboard_users + expected_phone_sync_users
+    expected_total_users = expected_admins + expected_phone_sync_users
     expect {
       Seed::UserSeeder.call(config: config)
     }.to change { User.count }.by(expected_total_users)
