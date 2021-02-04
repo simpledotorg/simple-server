a34f0339bfbd7a07bcafadbe250cc7d278d7b000
Srihari Sriraman <srihari@nilenso.com>
2021-02-04 12:24:53 +0530

    Extract patient days calculation into a reports model

---
+++ app/models/reports/patient_days_calculation.rb
@@ -0,0 +1,95 @@
+module Reports
+  class PatientDaysCalculation
+    def initialize(state, protocol, drug_category, stocks_by_rxnorm_code, patient_count)
+      @protocol = protocol
+      @drug_category = drug_category
+      @stocks_by_rxnorm_code = stocks_by_rxnorm_code
+      @patient_count = patient_count
+      @coefficients = patient_days_coefficients[state]
+    end
+
+    def protocol_drugs_by_category
+      @protocol_drugs_by_category ||= @protocol.protocol_drugs.where(stock_tracked: true).group_by(&:drug_category)
+    end
+
+    def calculate
+      return nil if stocks_on_hand.nil? || stocks_on_hand.empty?
+      { stocks_on_hand: stocks_on_hand,
+        patient_count: @patient_count,
+        load_coefficient: @coefficients[:load_coefficient],
+        new_patient_coefficient: new_patient_coefficient,
+        estimated_patients: estimated_patients,
+        patient_days: patient_days }
+    rescue
+      # either drug stock is nil, or drug is not in formula
+      # Raise sentry error
+      { patient_days: "error" }
+    end
+
+    def stocks_on_hand
+      @stocks_on_hand ||= protocol_drugs_by_category[@drug_category].map do |protocol_drug|
+        rxnorm_code = protocol_drug.rxnorm_code
+        in_stock = @stocks_by_rxnorm_code[rxnorm_code]
+        next if in_stock.nil?
+        coefficient = drug_coefficient(rxnorm_code)
+
+        { protocol_drug: protocol_drug,
+          in_stock: in_stock,
+          coefficient: coefficient,
+          stock_on_hand: coefficient * in_stock }
+      end&.compact
+    end
+
+    def drug_coefficient(rxnorm_code)
+      @coefficients.dig(:drug_categories, @drug_category, rxnorm_code)
+    end
+
+    def patient_days
+      (stocks_on_hand.map { |stock| stock[:stock_on_hand] }.reduce(:+) / estimated_patients).floor
+    end
+
+    def estimated_patients
+      @estimated_patients ||= @patient_count * @coefficients[:load_coefficient] * new_patient_coefficient
+    end
+
+    def new_patient_coefficient
+      @new_patient_coefficient ||= @coefficients.dig(:drug_categories, @drug_category, :new_patient_coefficient)
+    end
+
+    def patient_days_coefficients
+      # move this to config
+      { "Karnataka":
+          { load_coefficient: 1,
+            drug_categories:
+              { "hypertension_ccb":
+                  { new_patient_coefficient: 1.4,
+                    "329528": 1,
+                    "329526": 2 },
+                "hypertension_arb":
+                  { new_patient_coefficient: 0.37,
+                    "316764": 1,
+                    "316765": 2,
+                    "979467": 1 },
+                "hypertension_diuretic":
+                  { new_patient_coefficient: 0.06,
+                    "316049": 1,
+                    "331132": 1 } } },
+        "Punjab":
+          { load_coefficient: 1,
+            drug_categories:
+              { "hypertension_ccb":
+                  { new_patient_coefficient: 1.4,
+                    "329528": 1,
+                    "329526": 2 },
+                "hypertension_arb":
+                  { new_patient_coefficient: 0.37,
+                    "316764": 1,
+                    "316765": 2,
+                    "979467": 1 },
+                "hypertension_diuretic":
+                  { new_patient_coefficient: 0.06,
+                    "316049": 1,
+                    "331132": 1 } } } }.with_indifferent_access
+    end
+  end
+end
--- app/queries/drug_stocks_query.rb
+++ app/queries/drug_stocks_query.rb
@@ -4,6 +4,7 @@ class DrugStocksQuery
     @for_end_of_month = for_end_of_month
     # assuming that all facilities on the page have the same protocol
     @protocol = @facilities.first.protocol
+    @state = @facilities.first.state
   end
 
   def protocol_drugs_by_category
@@ -43,7 +44,7 @@ class DrugStocksQuery
     drug_stocks = facility_drug_stocks.group_by { |drug_stock| drug_stock.protocol_drug.drug_category }
     protocol_drugs_by_category.each do |(drug_category, _protocol_drugs)|
       stocks = stocks_by_rxnorm_code(drug_stocks[drug_category])
-      patient_days = patient_days_calculations(drug_category, stocks, patient_count)
+      patient_days = Reports::PatientDaysCalculation.new(@state, @protocol, drug_category, stocks, patient_count).calculate
       facility_report[drug_category] = patient_days.merge(drug_stocks: drug_stocks_by_drug_id(drug_stocks[drug_category]))
     end
     facility_report
@@ -61,7 +62,7 @@ class DrugStocksQuery
     protocol_drugs_by_category.each do |(drug_category, _protocol_drugs)|
       drug_stock_by_rxnorm_code = drug_stock_totals.map { |(protocol_drug, in_stock)| [protocol_drug.rxnorm_code, in_stock] }.to_h
       drug_stock_by_id = drug_stock_totals.map { |(protocol_drug, in_stock)| [protocol_drug.id, in_stock] }.to_h
-      patient_days = patient_days_calculations(drug_category, drug_stock_by_rxnorm_code, total_patient_count)
+      patient_days = Reports::PatientDaysCalculation.new(@state, @protocol, drug_category, drug_stock_by_rxnorm_code, total_patient_count).calculate
       next if patient_days.nil?
       report_all[drug_category] = patient_days.merge(drug_stocks: drug_stock_by_id)
     end
@@ -78,75 +79,5 @@ class DrugStocksQuery
     drug_stocks.each_with_object({}) { |drug_stock, acc|
       acc[drug_stock.protocol_drug.id] = drug_stock
     }
-  end
-
-  def state
-    @facilities.first.state
-  end
-
-  def patient_days_calculations(drug_category, stocks_by_rxnorm_code, patient_count)
-    coefficients = patient_days_coefficients[state]
-    stocks_on_hand = stocks_on_hand(coefficients, drug_category, stocks_by_rxnorm_code).compact
-    return nil if stocks_on_hand.nil? || stocks_on_hand.compact.empty?
-    new_patient_coefficient = coefficients[:drug_categories][drug_category][:new_patient_coefficient]
-    estimated_patients = patient_count * coefficients[:load_factor] * new_patient_coefficient
-    {stocks_on_hand: stocks_on_hand,
-     patient_count: patient_count,
-     load_factor: coefficients[:load_factor],
-     new_patient_coefficient: new_patient_coefficient,
-     estimated_patients: estimated_patients,
-     patient_days: (stocks_on_hand.map { |stock| stock[:stock_on_hand] }.reduce(:+) / estimated_patients).floor}
-  rescue
-    # either drug stock is nil, or drug is not in formula
-    {patient_days: "error"}
-  end
-
-  def stocks_on_hand(coefficients, drug_category, stocks_by_rxnorm_code)
-    protocol_drugs_by_category[drug_category].map do |protocol_drug|
-      rxnorm_code = protocol_drug.rxnorm_code
-      coefficient = coefficients[:drug_categories][drug_category][rxnorm_code]
-      in_stock = stocks_by_rxnorm_code[rxnorm_code]
-      next if in_stock.nil?
-      {protocol_drug: protocol_drug,
-       in_stock: in_stock,
-       coefficient: coefficient,
-       stock_on_hand: coefficient * in_stock}
-    end
-  end
-
-  def patient_days_coefficients
-    # move this to config
-    {"Karnataka":
-        {load_factor: 1,
-         drug_categories:
-            {"hypertension_ccb":
-                {new_patient_coefficient: 1.4,
-                 "329528": 1,
-                 "329526": 2},
-             "hypertension_arb":
-                {new_patient_coefficient: 0.37,
-                 "316764": 1,
-                 "316765": 2,
-                 "979467": 1},
-             "hypertension_diuretic":
-                {new_patient_coefficient: 0.06,
-                 "316049": 1,
-                 "331132": 1}}},
-     "Punjab":
-        {load_factor: 1,
-         drug_categories:
-            {"hypertension_ccb":
-                {new_patient_coefficient: 1.4,
-                 "329528": 1,
-                 "329526": 2},
-             "hypertension_arb":
-                {new_patient_coefficient: 0.37,
-                 "316764": 1,
-                 "316765": 2,
-                 "979467": 1},
-             "hypertension_diuretic":
-                {new_patient_coefficient: 0.06,
-                 "316049": 1,
-                 "331132": 1}}}}.with_indifferent_access
   end
 end
