af6be2b6b4be3a8d30edf183d27dfe93ba7cf8fc
Srihari Sriraman <srihari@nilenso.com>
2021-02-05 18:27:51 +0530

    Add spec skeleton for the query

---
+++ spec/queries/drug_stocks_query_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe DrugStocksQuery do
+  # test #cache_key
+
+  # test #drug_stock_for_facility, mocking calculation, don't ignore params
+  # test all early returns
+  # structural test for data structure being returned
+
+  # test #report_for_facilities, ensure all facilities are in report even if patient_count is nil
+
+  # test #drug_stocks
+
+  # test #totals, mocking calculation, don't ignore params
+  # test totalling calculations
+  # test early exists
+end
