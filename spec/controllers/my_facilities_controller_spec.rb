43a64d2ad95a2a2300fc2731ef9e7864a870d5a0
kpethtel <kris.pethtel@gmail.com>
2021-02-08 17:35:10 -0500

    specs

--- spec/controllers/my_facilities_controller_spec.rb
+++ spec/controllers/my_facilities_controller_spec.rb
@@ -7,6 +7,8 @@ end
 RSpec.describe MyFacilitiesController, type: :controller do
   let(:facility_group) { create(:facility_group) }
   let(:supervisor) { create(:admin, :manager, :with_access, resource: facility_group) }
+  let(:facility) { create(:facility, facility_group: facility_group) }
+  let(:december) { Period.month("December 2020") }
 
   render_views
 
@@ -24,8 +26,7 @@ RSpec.describe MyFacilitiesController, type: :controller do
 
   describe "GET #index" do
     it "returns a success response" do
-      create(:facility, facility_group: facility_group)
-
+      facility
       get :index, params: {}
 
       expect(response).to be_successful
@@ -34,7 +35,6 @@ RSpec.describe MyFacilitiesController, type: :controller do
 
   describe "GET #bp_controlled" do
     it "returns a success response" do
-      facility = create(:facility, facility_group: facility_group)
       controlled = Timecop.freeze("August 15th 2020") {
         create_list(:patient, 2, full_name: "controlled", assigned_facility: facility, registration_user: supervisor)
       }
@@ -50,36 +50,46 @@ RSpec.describe MyFacilitiesController, type: :controller do
 
       facility_data = assigns(:data_for_facility)[facility.name]
 
-      expect(facility_data[:adjusted_registrations][Period.month("December 2020")]).to eq(2)
+      expect(facility_data[:adjusted_registrations][december]).to eq(2)
       expect(facility_data[:controlled_patients_rate][Period.month("November 2020")]).to eq(100)
     end
 
-    it "only returns data for the selected district" do
-      other_district = create(:facility_group, name: "other district")
-      facility_1 = create(:facility, facility_group: other_district)
-      supervisor.accesses.create! resource: other_district
-      facility_2 = create(:facility, facility_group: facility_group)
-      _controlled = Timecop.freeze("August 15th 2020") {
-        create_list(:patient, 2, full_name: "controlled in facility_1", assigned_facility: facility_1, registration_user: supervisor)
-        create_list(:patient, 2, full_name: "controlled in facility_2", assigned_facility: facility_2, registration_user: supervisor)
-      }
+    context "when admin has access to multiple facilities" do
+      let(:other_district) { create(:facility_group, name: "other district") }
+      let(:facility_2) { create(:facility, facility_group: other_district) }
 
-      Timecop.freeze("January 15th 2021") do
+      before :each do
+        supervisor.accesses.create! resource: other_district
+        Timecop.freeze("August 15th 2020") do
+          create_list(:patient, 2, full_name: "controlled in facility", assigned_facility: facility, registration_user: supervisor)
+          create_list(:patient, 2, full_name: "controlled in facility_2", assigned_facility: facility_2, registration_user: supervisor)
+        end
         refresh_views
-        get :bp_controlled, params: {facility_group: other_district.slug}
       end
 
-      expect(response).to be_successful
-      expect(assigns(:data_for_facility)[facility_1.name]).to_not be_nil
-      expect(assigns(:data_for_facility)[facility_2.name]).to be_nil
-      expect(assigns(:stats_by_size).keys).to eq(["small"])
-      expect(assigns(:display_sizes)).to eq(["small"])
+      it "only returns data for the selected district" do
+        Timecop.freeze("January 15th 2021") do
+          get :bp_controlled, params: {facility_group: other_district.slug}
+        end
+        expect(response).to be_successful
+        expect(assigns(:data_for_facility)[facility_2.name]).to_not be_nil
+        expect(assigns(:data_for_facility)[facility.name]).to be_nil
+        expect(assigns(:display_sizes)).to eq(["small"])
+      end
+
+      it "sets stats_by_size for all facilities accessible by the admin" do
+        Timecop.freeze("January 15th 2021") do
+          get :bp_controlled, params: {facility_group: other_district.slug}
+        end
+        stats = assigns(:stats_by_size)
+        patient_count = facility.assigned_patients.count + facility_2.assigned_patients.count
+        expect(stats["small"][december]["cumulative_registrations"]).to eq(patient_count)
+      end
     end
   end
 
   describe "GET #bp_not_controlled" do
     it "returns a success response" do
-      facility = create(:facility, facility_group: facility_group)
       controlled = Timecop.freeze("August 15th 2020") {
         create_list(:patient, 2, full_name: "uncontrolled", assigned_facility: facility, registration_user: supervisor)
       }
@@ -103,7 +113,6 @@ RSpec.describe MyFacilitiesController, type: :controller do
 
   describe "GET #missed_visits" do
     it "returns a success response" do
-      facility = create(:facility, facility_group: facility_group)
       controlled = Timecop.freeze("August 15th 2020") {
         create_list(:patient, 2, full_name: "controlled", assigned_facility: facility, registration_user: supervisor)
       }
