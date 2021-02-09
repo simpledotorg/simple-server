8f0bd6405ea1330e3ac841df62d353f9291d0521
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-02-09 18:05:17 +0530

    Rename TIME_TO_CONSIDER_LTFU

--- app/models/concerns/patient_reportable_matview.rb
+++ app/models/concerns/patient_reportable_matview.rb
@@ -1,7 +1,7 @@
 module PatientReportableMatview
   extend ActiveSupport::Concern
   include PatientReportable
-  TIME_TO_CONSIDER_LTFU = PatientReportable::TIME_TO_CONSIDER_LTFU
+  LTFU_TIME = PatientReportable::LTFU_TIME
 
   included do
     def self.refresh
@@ -12,13 +12,13 @@ module PatientReportableMatview
     scope :excluding_dead, -> { where.not(patient_status: :dead) }
 
     scope :ltfu_as_of, ->(date) do
-      where.not("bp_recorded_at > ? AND bp_recorded_at < ?", date.to_date - TIME_TO_CONSIDER_LTFU, date.to_date)
-        .where("patient_recorded_at < ?", date - TIME_TO_CONSIDER_LTFU)
+      where.not("bp_recorded_at > ? AND bp_recorded_at < ?", date.to_date - LTFU_TIME, date.to_date)
+        .where("patient_recorded_at < ?", date - LTFU_TIME)
     end
 
     scope :not_ltfu_as_of, ->(date) do
-      where("bp_recorded_at >= ? AND bp_recorded_at <= ?", date.to_date - TIME_TO_CONSIDER_LTFU, date.to_date)
-        .or(where("patient_recorded_at >= ?", date - TIME_TO_CONSIDER_LTFU))
+      where("bp_recorded_at >= ? AND bp_recorded_at <= ?", date.to_date - LTFU_TIME, date.to_date)
+        .or(where("patient_recorded_at >= ?", date - LTFU_TIME))
     end
   end
 end
