c1f0722a156e87e1cef7ed9749120616e021b654
Rob Sanheim <rsanheim@gmail.com>
2021-02-12 15:34:07 -0600

    Better error matches; bump up count

--- spec/rachets/lets_not_spec.rb
+++ spec/rachets/lets_not_spec.rb
@@ -1,22 +1,23 @@
 require "spec_helper"
 
 RSpec.describe "Rachet down usage of let!" do
-  expected_usages = 698
+  expected_usages = 705
 
-  it "ratchets down let!" do
+  it "prevents new usages" do
     command = %{grep -rn "\slet\!(:" spec}
     offending_lines = `#{command}`
     actual_usages = offending_lines.split("\n").count
 
     msg = <<~EOL
-      It looks like you added a new usage of 'let!'. Please change your test setup to use local variables,
-      a regular ruby method, or some other approach. For more background and ideas on alternative approaches
-      see https://thoughtbot.com/blog/lets-not and https://thoughtbot.com/blog/my-issues-with-let.
+      It looks like you added a new usage of 'let!'. We expected #{expected_usages}, but found #{actual_usages}.
+      Please change your test setup to use local variables, a regular ruby method, or some other approach.
+      For more background and ideas on alternative approaches see https://thoughtbot.com/blog/lets-not and https://thoughtbot.com/blog/my-issues-with-let.
 
-      And if you _really_, really must use let!, you can increase EXPECTED_USAGES in the #{__FILE__}.
+      And if you really must use let!, you can increase EXPECTED_USAGES in the #{__FILE__}.
     EOL
     happy_msg = <<~EOL
-      You removed a usage of let! Thank you helping us to work towards more explicit and maintainable specs.
+      You removed a usage of let! We previously had #{expected_usages}, but found #{actual_usages}.
+      Thank you helping us to work towards more explicit and maintainable specs.
       Please change EXPECTED_USAGES in #{__FILE__} to #{actual_usages} to continue to rachet this down."
     EOL
     expect(actual_usages).to_not be > expected_usages, msg
