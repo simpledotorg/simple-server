bd02f4127ed88c5eef64863d3aab2c89f56f02b6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-02-16 18:10:12 +0530

    Add specs for cache warmer job

--- app/services/reports/region_cache_warmer.rb
+++ app/services/reports/region_cache_warmer.rb
@@ -4,36 +4,24 @@ module Reports
       new.call
     end
 
-    BATCH_SIZE = 1000
-
     def initialize(period: RegionService.default_period)
       @period = period
-      @original_force_cache = RequestStore.store[:force_cache]
-      RequestStore.store[:force_cache] = true
-      notify "start"
+      notify "queueing region reports cache warming"
     end
 
-    attr_reader :original_force_cache
     attr_reader :period
-    attr_reader :notifier
 
     def call
-      duration = Benchmark.ms {
-        if Flipper.enabled?(:disable_region_cache_warmer)
-          notify "disabled via flipper - exiting"
-          return
-        end
+      if Flipper.enabled?(:disable_region_cache_warmer)
+        notify "disabled via flipper - exiting"
+        return
+      end
 
-        notify "starting region caching"
-        Statsd.instance.time("region_cache_warmer") do
-          Region.where.not(region_type: ["root", "organization"]).pluck(:id).each do |region_id|
-            RegionCacheWarmerJob.perform_async(region_id, period.attributes)
-          end
-        end
-      }
-      notify "finished", duration: duration
-    ensure
-      RequestStore.store[:force_cache] = original_force_cache
+      Region.where.not(region_type: ["root", "organization"]).pluck(:id).each do |region_id|
+        RegionCacheWarmerJob.perform_async(region_id, period.attributes)
+      end
+
+      notify "queued region reports cache warming"
     end
 
     private
--- spec/jobs/region_cache_warmer_job_spec.rb
+++ spec/jobs/region_cache_warmer_job_spec.rb
@@ -3,9 +3,17 @@ require "rails_helper"
 RSpec.describe RegionCacheWarmerJob, type: :job do
   include ActiveJob::TestHelper
 
-  let!(:facility) { create(:facility).region }
+  let!(:facility) { create(:facility) }
   let!(:region) { facility.region }
-  let!(:period) { Period.month(Time.current) }
+  let!(:period) { Period.month(Time.current.beginning_of_month) }
+
+  let!(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+  let!(:cache) { Rails.cache }
+
+  before do
+    allow(Rails).to receive(:cache).and_return(memory_store)
+    Rails.cache.clear
+  end
 
   it "queues the job on the low queue" do
     expect {
@@ -20,5 +28,18 @@ RSpec.describe RegionCacheWarmerJob, type: :job do
 
     described_class.perform_async(region.id, period.attributes)
     described_class.drain
+  end
+
+  it "refreshes the cache" do
+    described_class.perform_async(region.id, period.attributes)
+    described_class.drain
+    initial_registrations = Reports::RegionService.new(region: region, period: period).call[:cumulative_registrations]
+
+    create(:patient, assigned_facility: facility, recorded_at: 1.month.ago)
+    described_class.perform_async(region.id, period.attributes)
+    described_class.drain
+    final_registrations = Reports::RegionService.new(region: region, period: period).call[:cumulative_registrations]
+
+    expect(final_registrations).not_to eq(initial_registrations)
   end
 end
