1e6bf9607b8fa4c04847611599ca1724b7f7f0b7
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-02-16 20:16:42 +0530

    Dont use let

--- spec/jobs/region_cache_warmer_job_spec.rb
+++ spec/jobs/region_cache_warmer_job_spec.rb
@@ -3,42 +3,46 @@ require "rails_helper"
 RSpec.describe RegionCacheWarmerJob, type: :job do
   include ActiveJob::TestHelper
 
-  let!(:facility) { create(:facility) }
-  let!(:region) { facility.region }
-  let!(:period) { Period.month(Time.current.beginning_of_month) }
-
-  let!(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
-  let!(:cache) { Rails.cache }
-
   before do
+    memory_store = ActiveSupport::Cache.lookup_store(:memory_store)
+
     allow(Rails).to receive(:cache).and_return(memory_store)
     Rails.cache.clear
   end
 
   it "queues the job on the default queue" do
+    facility = create(:facility)
+    period = Period.month(Time.current.beginning_of_month)
+
     expect {
-      RegionCacheWarmerJob.perform_async(region.id, period.attributes)
+      RegionCacheWarmerJob.perform_async(facility.region.id, period.attributes)
     }.to change(Sidekiq::Queues["default"], :size).by(1)
     RegionCacheWarmerJob.clear
   end
 
   it "calls RegionService for the region and period" do
-    expect(Reports::RegionService).to receive(:call).with(region: region, period: period)
-    expect(Reports::RegionService).to receive(:call).with(region: region, period: period, with_exclusions: true)
+    facility = create(:facility)
+    period = Period.month(Time.current.beginning_of_month)
 
-    described_class.perform_async(region.id, period.attributes)
+    expect(Reports::RegionService).to receive(:call).with(region: facility.region, period: period)
+    expect(Reports::RegionService).to receive(:call).with(region: facility.region, period: period, with_exclusions: true)
+
+    described_class.perform_async(facility.region.id, period.attributes)
     described_class.drain
   end
 
   it "refreshes the cache" do
-    described_class.perform_async(region.id, period.attributes)
+    facility = create(:facility)
+    period = Period.month(Time.current.beginning_of_month)
+
+    described_class.perform_async(facility.region.id, period.attributes)
     described_class.drain
-    initial_registrations = Reports::RegionService.new(region: region, period: period).call[:cumulative_registrations]
+    initial_registrations = Reports::RegionService.new(region: facility.region, period: period).call[:cumulative_registrations]
 
     create(:patient, assigned_facility: facility, recorded_at: 1.month.ago)
-    described_class.perform_async(region.id, period.attributes)
+    described_class.perform_async(facility.region.id, period.attributes)
     described_class.drain
-    final_registrations = Reports::RegionService.new(region: region, period: period).call[:cumulative_registrations]
+    final_registrations = Reports::RegionService.new(region: facility.region, period: period).call[:cumulative_registrations]
 
     expect(final_registrations).not_to eq(initial_registrations)
   end
