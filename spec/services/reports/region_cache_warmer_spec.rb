8488a6bb6333af75f2610a34622e165fc95b09ce
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-02-16 19:15:35 +0530

    Cleanup region warmer spec

--- spec/services/reports/region_cache_warmer_spec.rb
+++ spec/services/reports/region_cache_warmer_spec.rb
@@ -1,24 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Reports::RegionCacheWarmer, type: :model do
-  let(:organization) { create(:organization, name: "org-1") }
-  let(:user) { create(:admin, :manager, :with_access, resource: organization) }
-  let(:facility_group_1) { FactoryBot.create(:facility_group, name: "facility_group_1", organization: organization) }
-
-  let(:jan_2019) { Time.parse("January 1st, 2019") }
-  let(:jan_2020) { Time.parse("January 1st, 2020") }
-  let(:june_1) { Time.parse("June 1st, 2020") }
-  let(:july_1_2019) { Time.parse("July 1st, 2019") }
-  let(:july_2020) { Time.parse("July 1st, 2020") }
-
-  def refresh_views
-    ActiveRecord::Base.transaction do
-      LatestBloodPressuresPerPatientPerMonth.refresh
-      LatestBloodPressuresPerPatientPerQuarter.refresh
-      PatientRegistrationsPerDayPerFacility.refresh
-    end
-  end
-
   it "skips caching if disabled via Flipper" do
     Flipper.enable(:disable_region_cache_warmer)
     expect(Reports::RegionService).to receive(:new).never
@@ -26,7 +8,15 @@ RSpec.describe Reports::RegionCacheWarmer, type: :model do
   end
 
   it "completes successfully" do
-    _facilities = FactoryBot.create_list(:facility, 2, facility_group: facility_group_1)
+    create_list(:patient, 2)
     Reports::RegionCacheWarmer.call
+  end
+
+  it "queues a job on the default queue for every region" do
+    create(:patient)
+    reporting_regions = Region.where.not(region_type: ["root", "organization"])
+    expect {
+      Reports::RegionCacheWarmer.call
+    }.to change(Sidekiq::Queues["default"], :size).by(reporting_regions.count)
   end
 end
