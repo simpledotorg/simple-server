1c1841e4a8c8fb6ae9a5f27d90f582c041d6eeba
Rob Sanheim <rsanheim@gmail.com>
2021-02-17 14:00:50 -0600

    lnting

--- spec/lib/extensions/fetch_multi_fix_spec.rb
+++ spec/lib/extensions/fetch_multi_fix_spec.rb
@@ -10,22 +10,22 @@ RSpec.describe "Rails cache fetch multi fix" do
   end
 
   it "does not set new values without force option" do
-    Rails.cache.fetch_multi("key1", "key2") { |key| "value set for #{key}"}
+    Rails.cache.fetch_multi("key1", "key2") { |key| "value set for #{key}" }
     expect(Rails.cache.read("key1")).to eq("value set for key1")
     expect(Rails.cache.read("key2")).to eq("value set for key2")
 
-    Rails.cache.fetch_multi("key1", "key2") { |key| "forced value set for #{key}"}
+    Rails.cache.fetch_multi("key1", "key2") { |key| "forced value set for #{key}" }
 
     expect(Rails.cache.read("key1")).to eq("value set for key1")
     expect(Rails.cache.read("key2")).to eq("value set for key2")
   end
 
   it "sets all values if force option is provided" do
-    Rails.cache.fetch_multi("key1", "key2") { |key| "value set for #{key}"}
+    Rails.cache.fetch_multi("key1", "key2") { |key| "value set for #{key}" }
     expect(Rails.cache.read("key1")).to eq("value set for key1")
     expect(Rails.cache.read("key2")).to eq("value set for key2")
 
-    Rails.cache.fetch_multi("key1", "key2", force: true) { |key| "forced value set for #{key}"}
+    Rails.cache.fetch_multi("key1", "key2", force: true) { |key| "forced value set for #{key}" }
 
     expect(Rails.cache.read("key1")).to eq("forced value set for key1")
     expect(Rails.cache.read("key2")).to eq("forced value set for key2")
