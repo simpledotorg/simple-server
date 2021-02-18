e60cf51da9dc6d9b0b1881ee4231e989667f61cf
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-02-18 14:29:01 +0530

    Lets not

--- spec/services/patient_breakdown_service_spec.rb
+++ spec/services/patient_breakdown_service_spec.rb
@@ -1,28 +1,25 @@
 require "rails_helper"
 
 RSpec.describe PatientBreakdownService do
-  let!(:facility) { create(:facility) }
-  let!(:ltfu_patient) { create(:patient, :hypertension, recorded_at: 2.years.ago, assigned_facility: facility) }
-  let!(:not_ltfu_patient) { create(:patient, :hypertension, assigned_facility: facility) }
-  let!(:ltfu_transferred_patient) { create(:patient, :hypertension, recorded_at: 2.years.ago, status: :migrated, assigned_facility: facility) }
-  let!(:not_ltfu_transferred_patient) { create(:patient, :hypertension, status: :migrated, assigned_facility: facility) }
-  let!(:dead_patient) { create(:patient, :hypertension, status: :dead, assigned_facility: facility) }
-  let!(:not_hypertensive_patient) { create(:patient, :without_hypertension, assigned_facility: facility) }
-  let!(:blood_pressures) {
-    [create(:blood_pressure, recorded_at: 1.day.ago, patient: not_ltfu_patient),
-      create(:blood_pressure, recorded_at: 1.day.ago, patient: not_ltfu_transferred_patient)]
-  }
-
-  let!(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
-  let!(:cache) { Rails.cache }
-
   before do
+    memory_store = ActiveSupport::Cache.lookup_store(:memory_store)
+
     allow(Rails).to receive(:cache).and_return(memory_store)
     Rails.cache.clear
   end
 
   describe ".call" do
     it "gets the patient breakdowns by status and ltfu-ness and caches it" do
+      facility = create(:facility)
+      ltfu_patient = create(:patient, :hypertension, recorded_at: 2.years.ago, assigned_facility: facility)
+      not_ltfu_patient = create(:patient, :hypertension, assigned_facility: facility)
+      ltfu_transferred_patient = create(:patient, :hypertension, recorded_at: 2.years.ago, status: :migrated, assigned_facility: facility)
+      not_ltfu_transferred_patient = create(:patient, :hypertension, status: :migrated, assigned_facility: facility)
+      dead_patient = create(:patient, :hypertension, status: :dead, assigned_facility: facility)
+      not_hypertensive_patient = create(:patient, :without_hypertension, assigned_facility: facility)
+      create(:blood_pressure, recorded_at: 1.day.ago, patient: not_ltfu_patient)
+      create(:blood_pressure, recorded_at: 1.day.ago, patient: not_ltfu_transferred_patient)
+
       expected_result = {
         dead_patients: 1,
         ltfu_patients: 2,
