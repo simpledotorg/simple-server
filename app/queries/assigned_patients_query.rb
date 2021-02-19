f0e8ffeb739e7f584c322b93641690337e259687
Rob Sanheim <rsanheim@gmail.com>
2021-02-19 13:03:30 -0600

    Make consistent w/ registrations query

--- app/queries/assigned_patients_query.rb
+++ app/queries/assigned_patients_query.rb
@@ -1,10 +1,10 @@
 class AssignedPatientsQuery
   # Returns ALL assigned counts for a region
-  def count(region, type, with_exclusions: false)
-    formatter = lambda { |v| type == :quarter ? Period.quarter(v) : Period.month(v) }
+  def count(region, period_type, with_exclusions: false)
+    formatter = lambda { |v| period_type == :quarter ? Period.quarter(v) : Period.month(v) }
 
     query = region.assigned_patients
     query = query.for_reports(with_exclusions: with_exclusions) if with_exclusions
-    query.group_by_period(type.to_s, :recorded_at, {format: formatter}).count
+    query.group_by_period(period_type, :recorded_at, {format: formatter}).count
   end
 end
