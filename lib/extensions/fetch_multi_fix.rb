6f75c8f980c3d0c0b5a6bf191ca6734529926458
Rob Sanheim <rsanheim@gmail.com>
2021-02-23 13:32:32 -0600

    Add note back to bug in Rails

--- lib/extensions/fetch_multi_fix.rb
+++ lib/extensions/fetch_multi_fix.rb
@@ -1,3 +1,5 @@
+# Created to work around a bug in Rails: https://github.com/rails/rails/issues/41535
+# Once the upstream bug is fixed, this workaround can be removed.
 module FetchMultiFix
   def fetch_multi(*names)
     options = names.extract_options!
