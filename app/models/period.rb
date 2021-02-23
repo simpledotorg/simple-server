daf3389ebb330b0350f3930e1573c7a1540d201c
Claudio Vallejo <hola@claudiovallejo.mx>
2021-02-22 19:51:55 -0500

    Updated blood_pressure_control_range, Set right denominator date in regions 'Overview'

--- app/models/period.rb
+++ app/models/period.rb
@@ -53,7 +53,7 @@ class Period
   # Returns a range of dates that correspond to the 'control range' for this period.
   # For example, for a month period of July 1st 2020, this will return the range of April 30th..July 31st.
   def blood_pressure_control_range
-    three_months_ago = end_date.advance(months: -3)
+    three_months_ago = end_date.advance(months: -3).end_of_month
     (three_months_ago..end_date)
   end
 
