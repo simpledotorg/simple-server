f65b2bd875406bda4a257e4726cccb71003b8345
Claudio Vallejo <hola@claudiovallejo.mx>
2021-03-01 14:57:12 -0600

    Reverted ltfu_since_date changes in 'result.rb'

--- app/models/reports/result.rb
+++ app/models/reports/result.rb
@@ -180,7 +180,7 @@ module Reports
         hsh[period] = {
           name: period.to_s,
           start_date: period.start_date.to_s(:day_mon_year),
-          ltfu_since_date: period.end_date.advance(months: -12).to_s(:day_mon_year),
+          ltfu_since_date: period.start_date.advance(months: -12).to_s(:day_mon_year),
           bp_control_start_date: period.bp_control_range_start_date,
           bp_control_end_date: period.bp_control_range_end_date,
           bp_control_registration_date: period.bp_control_range_registration_date
