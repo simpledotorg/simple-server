9dada72157f916ad940573ae2592332799ef1dd5
vkrm <mail@vkrm.is>
2021-03-02 19:26:39 +0530

    update let-bang ratchet

--- spec/rachets/lets_not_spec.rb
+++ spec/rachets/lets_not_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe "Rachet down usage of let!" do
-  expected_usages = 679
+  expected_usages = 677
 
   it "prevents new usages" do
     command = %{grep -rn "\slet\!(:" spec}
