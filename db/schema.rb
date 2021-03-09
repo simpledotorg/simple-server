63acbb07739b7fbb637c5e4092a94ecac49134fd
Srihari Sriraman <srihari@nilenso.com>
2021-03-09 17:14:51 +0530

    Add index on identifier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_120024) do
+ActiveRecord::Schema.define(version: 2021_03_09_112622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -374,6 +374,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_120024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_patient_business_identifiers_on_deleted_at"
+    t.index ["identifier"], name: "index_patient_business_identifiers_identifier"
     t.index ["patient_id"], name: "index_patient_business_identifiers_on_patient_id"
   end
 
