77d5771d5b643ceee5559497fbbb32afb8132763
Srihari Sriraman <srihari@nilenso.com>
2021-03-12 12:59:27 +0530

    UX polish: add duplicate count, navigation, and format phone numbers

--- app/controllers/admin/deduplicate_patients_controller.rb
+++ app/controllers/admin/deduplicate_patients_controller.rb
@@ -1,10 +1,13 @@
 class Admin::DeduplicatePatientsController < AdminController
   skip_before_action :verify_authenticity_token
+  DUPLICATE_LIMIT = 250
 
   def show
     authorize { current_admin.power_user? }
 
-    @patients = Patient.where(id: PatientDeduplication::Strategies.identifier_match(limit: 250).sample)
+    duplicate_patient_ids = PatientDeduplication::Strategies.identifier_match(limit: DUPLICATE_LIMIT)
+    @duplicate_count = duplicate_patient_ids.count
+    @patients = Patient.where(id: duplicate_patient_ids.sample)
   end
 
   def merge
