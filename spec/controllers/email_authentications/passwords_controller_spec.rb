185e705358541a421208bc40fcd328c8bb3db007
kpethtel <kris.pethtel@gmail.com>
2021-03-12 16:58:46 -0500

    lint

--- spec/controllers/email_authentications/passwords_controller_spec.rb
+++ spec/controllers/email_authentications/passwords_controller_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe EmailAuthentications::PasswordsController, type: :controller do
-
   before :each do
     @request.env["devise.mapping"] = Devise.mappings[:email_authentication]
   end
@@ -10,7 +9,7 @@ RSpec.describe EmailAuthentications::PasswordsController, type: :controller do
     it "renders the edit page when the reset token is valid" do
       auth = build(:email_authentication)
       token = auth.send_reset_password_instructions
-      put :edit, params: { reset_password_token: token }
+      put :edit, params: {reset_password_token: token}
       expect(response).to render_template "edit"
     end
 
@@ -18,9 +17,8 @@ RSpec.describe EmailAuthentications::PasswordsController, type: :controller do
       auth = build(:email_authentication)
       token = auth.send_reset_password_instructions
       auth.update_attribute(:reset_password_sent_at, 7.hours.ago)
-      put :edit, params: { reset_password_token: token }
+      put :edit, params: {reset_password_token: token}
       expect(response).to render_template "expired_reset_token"
     end
   end
-
 end
