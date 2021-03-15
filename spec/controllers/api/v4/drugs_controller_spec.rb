371638d6c9506b9a3a834f4eb9d38ab0343fe3e7
vkrm <mail@vkrm.is>
2021-03-15 18:35:50 +0530

    standardrb fixes

--- spec/controllers/api/v4/drugs_controller_spec.rb
+++ spec/controllers/api/v4/drugs_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Api::V4::DrugsController, type: :controller do
     end
 
     it "Returns records from the beginning of time, even when process_token is set" do
-      get :sync_to_user, params: { process_token: make_process_token(other_facilities_processed_since: 10.minutes.ago) }
+      get :sync_to_user, params: {process_token: make_process_token(other_facilities_processed_since: 10.minutes.ago)}
       response_body = JSON(response.body)
       expect(response_body[response_key].count).to eq model.count
       expect(response_body[response_key].map { |record| record["id"] }.to_set)
@@ -23,11 +23,10 @@ RSpec.describe Api::V4::DrugsController, type: :controller do
 
     describe "batching" do
       it "ignores limit param, returns all records" do
-        get :sync_to_user, params: { limit: 2 }
+        get :sync_to_user, params: {limit: 2}
         response_body = JSON(response.body)
         expect(response_body[response_key].count).to eq model.count
       end
     end
   end
 end
-
