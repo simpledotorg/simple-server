14efc77d572796b6e9519e4c2886d4aeaf46785d
kpethtel <kris.pethtel@gmail.com>
2021-03-15 15:01:02 -0400

    PR feedback

--- spec/controllers/email_authentications/passwords_controller_spec.rb
+++ spec/controllers/email_authentications/passwords_controller_spec.rb
@@ -13,10 +13,11 @@ RSpec.describe EmailAuthentications::PasswordsController, type: :controller do
       expect(response).to render_template "edit"
     end
 
-    it "responds successfully when the reset token is expired" do
+    it "renders the expired reset token page when the reset token is expired" do
       auth = build(:email_authentication)
       token = auth.send_reset_password_instructions
-      auth.update_attribute(:reset_password_sent_at, 7.hours.ago)
+      expired_sent_time = Devise.reset_password_within + 1.hour
+      auth.update(reset_password_sent_at: expired_sent_time)
       put :edit, params: {reset_password_token: token}
       expect(response).to render_template "expired_reset_token"
     end
