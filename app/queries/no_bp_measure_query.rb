34e78f4500f5f2595179fca1d550aabc68d9cb05
Rob Sanheim <rsanheim@gmail.com>
2021-03-17 13:33:43 -0700

    Better logging

--- app/queries/no_bp_measure_query.rb
+++ app/queries/no_bp_measure_query.rb
@@ -1,8 +1,10 @@
 class NoBPMeasureQuery
+  delegate :logger, to: Rails
   delegate :sanitize_sql, to: ActiveRecord::Base
 
   def call(region, period, with_exclusions: false)
-    Rails.logger.info "#{self.class} called with #{region.slug} #{period} with_exclusions=#{with_exclusions}"
+    logger.info { "#{self.class} called for region=#{region.slug} period=#{period} with_exclusions=#{with_exclusions}" }
+
     facility_ids = region.facilities.map(&:id)
     return 0 if facility_ids.blank?
     start_date = period.blood_pressure_control_range.begin
