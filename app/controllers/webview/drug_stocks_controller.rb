6c2529a7887660305ed79a1d08fabdeb5bd52d29
Rob Sanheim <rsanheim@gmail.com>
2021-03-22 12:29:11 -0500

    Dont show drug stock form for community facilities (#2170)
    * Dont show drug stock form for community facilities
    
    * Use unless; use the enum question method
    
    * prevent drug stocks for any community facilities

--- app/controllers/webview/drug_stocks_controller.rb
+++ app/controllers/webview/drug_stocks_controller.rb
@@ -57,6 +57,10 @@ class Webview::DrugStocksController < ApplicationController
 
   def find_current_facility
     @current_facility = Facility.find(safe_params[:facility_id])
+    if @current_facility.community?
+      logger.error "Cannot create DrugStocks for community facility #{@current_facility.slug}"
+      render json: {status: "invalid", errors: "community facility"}, status: 422
+    end
   end
 
   def safe_params
