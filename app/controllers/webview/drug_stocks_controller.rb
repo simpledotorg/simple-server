00296fe41986bb5f3f09fd307b3af17d0bfc68ac
Rob Sanheim <rsanheim@gmail.com>
2021-03-23 15:09:48 -0500

    Show drug stock report on success page (mobile app) (#2150)
    * Begin spiking out drug stock report
    
    * Wiring up with the actual report
    
    * Show correct category; remove commented code
    
    * Setup cache busting
    
    * Generalize the patient css class for webview and dashboard
    
    * Add the collection cache_key to drug stock reports cache_key
    
    using the collection's cache_key method will ensure that the cache gets
    bumped whne new drug stocks are entered...so we don't need to bypass the
    cache manually anymore.  activerecord has the smarts builtin to handle
    this for us
    
    * Protocol should be https in dev too
    
    This is primarily used for pointing the android app to your local
    server, in which case you need to use HTTPS or Android won't even
    connect.  Given our normal setup is via ngrok for the https tunnel, we
    may as well default this to https here.
    
    * linting and formatting
    
    * Remove bust_cache params from specs
    
    * remove dead code
    
    * Rename to patient_days_css_class
    
    as this isn't strictly background related anymore
    
    * update label

--- app/controllers/webview/drug_stocks_controller.rb
+++ app/controllers/webview/drug_stocks_controller.rb
@@ -1,10 +1,12 @@
 # This controller is meant to be used from webviews from the Android app _only_,
 # hence we handle authentication ourselves from params passed from the client.
 class Webview::DrugStocksController < ApplicationController
+  include BustCache
   skip_before_action :verify_authenticity_token
   before_action :authenticate
   before_action :find_current_facility
   before_action :set_for_end_of_month
+  before_action :set_bust_cache
   layout false
 
   def new
@@ -26,6 +28,9 @@ class Webview::DrugStocksController < ApplicationController
   def index
     @protocol_drugs = current_facility.protocol.protocol_drugs.where(stock_tracked: true).sort_by(&:sort_key)
     @drug_stocks = DrugStock.latest_for_facilities_grouped_by_protocol_drug(current_facility, @for_end_of_month)
+    @query = DrugStocksQuery.new(facilities: [current_facility], for_end_of_month: @for_end_of_month)
+    @drugs_by_category = @query.protocol_drugs_by_category
+    @report = @query.drug_stocks_report
   end
 
   private
@@ -73,9 +78,13 @@ class Webview::DrugStocksController < ApplicationController
 
   def set_for_end_of_month
     @for_end_of_month ||= if params[:for_end_of_month]
-      Date.parse(params[:for_end_of_month])
+      Date.parse(params[:for_end_of_month]).end_of_month
     else
       Date.current.end_of_month
     end
+  end
+
+  def set_bust_cache
+    RequestStore.store[:bust_cache] = true if params[:bust_cache].present?
   end
 end
