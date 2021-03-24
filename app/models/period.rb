0393a8bde4773e905e719b6bda3c896d750d9b27
Rob Sanheim <rsanheim@gmail.com>
2021-03-24 16:40:05 -0500

    Get names more consistent
    we shouldnt call things 'begin_date' or 'end_date' if they dont return dates

--- app/models/period.rb
+++ app/models/period.rb
@@ -59,8 +59,8 @@ class Period
   # Returns a range of dates that correspond to the 'control range' for this period.
   # For example, for a month period of July 1st 2020, this will return the range of April 30th..July 31st.
   def blood_pressure_control_range
-    three_months_ago = end_date.advance(months: -3).end_of_month
-    (three_months_ago..end_date)
+    three_months_ago = end_time.advance(months: -3).end_of_month
+    (three_months_ago..end_time)
   end
 
   alias_method :bp_control_range, :blood_pressure_control_range
@@ -94,14 +94,14 @@ class Period
   end
   alias_method :begin, :start_date
 
-  def end_date
+  def end
     if quarter?
       value.end
     else
       value.end_of_month.end_of_day
     end
   end
-  alias_method :end, :end_date
+  alias_method :end_time, :end
 
   def succ
     if quarter?
--- app/queries/no_bp_measure_query.rb
+++ app/queries/no_bp_measure_query.rb
@@ -4,21 +4,21 @@ class NoBPMeasureQuery
   def call(region, period, with_exclusions: false)
     facility_ids = region.facilities.map(&:id)
     return 0 if facility_ids.blank?
-    start_date = period.blood_pressure_control_range.begin
-    end_date = period.blood_pressure_control_range.end
+    start_time = period.blood_pressure_control_range.begin
+    end_time = period.blood_pressure_control_range.end
     registration_date = period.blood_pressure_control_range.begin
 
     Patient
-      .for_reports(with_exclusions: with_exclusions, exclude_ltfu_as_of: period.end_date)
+      .for_reports(with_exclusions: with_exclusions, exclude_ltfu_as_of: period.end_time)
       .joins(sanitize_sql(["LEFT OUTER JOIN appointments ON appointments.patient_id = patients.id
           AND appointments.device_created_at > ?
-          AND appointments.device_created_at <= ?", start_date, end_date]))
+          AND appointments.device_created_at <= ?", start_time, end_time]))
       .joins(sanitize_sql(["LEFT OUTER JOIN prescription_drugs ON prescription_drugs.patient_id = patients.id
           AND prescription_drugs.device_created_at > ?
-          AND prescription_drugs.device_created_at <= ?", start_date, end_date]))
+          AND prescription_drugs.device_created_at <= ?", start_time, end_time]))
       .joins(sanitize_sql(["LEFT OUTER JOIN blood_sugars ON blood_sugars.patient_id = patients.id
           AND blood_sugars.recorded_at > ?
-          AND blood_sugars.recorded_at <= ?", start_date, end_date]))
+          AND blood_sugars.recorded_at <= ?", start_time, end_time]))
       .where(assigned_facility_id: facility_ids)
       .where("patients.recorded_at <= ?", registration_date)
       .where("appointments.id IS NOT NULL
@@ -29,7 +29,7 @@ class NoBPMeasureQuery
                   FROM blood_pressures bps
                   WHERE patients.id = bps.patient_id
                   AND bps.recorded_at > ?
-                  AND bps.recorded_at <= ?)", start_date, end_date)
+                  AND bps.recorded_at <= ?)", start_time, end_time)
       .distinct("patients.id")
       .count
   end
