813fbf21e6bbdbe6d2c517366b220645405ca8df
Srihari Sriraman <srihari@nilenso.com>
2021-03-25 12:06:45 +0530

    format

--- spec/models/deduplication_log_spec.rb
+++ spec/models/deduplication_log_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe DeduplicationLog, type: :model do
-  context 'associations' do
+  context "associations" do
     it { is_expected.to belong_to(:user).optional }
     it { is_expected.to belong_to(:deduped_record) }
     it { is_expected.to belong_to(:deleted_record) }
