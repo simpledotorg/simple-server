d729fd36582d89b498bf72f6292ed22740644f5f
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-03-25 17:02:59 +0530

    Fix missed visits percent calculation

--- app/models/reports/result.rb
+++ app/models/reports/result.rb
@@ -175,18 +175,24 @@ module Reports
     # If we determined the percentage directly, we would have cases where the percentages do not add up to 100
     # due to rounding and losing precision.
     def calculate_missed_visits_percentages(range, with_ltfu: false)
-      self.missed_visits_rate = range.each_with_object(Hash.new(0)) do |period, hsh|
-        remaining_percentages = if with_ltfu
-          controlled_patients_with_ltfu_rate_for(period) +
+      if with_ltfu
+        self.missed_visits_with_ltfu_rate = range.each_with_object(Hash.new(0)) do |period, hsh|
+          remaining_percentages =
+            controlled_patients_with_ltfu_rate_for(period) +
             uncontrolled_patients_with_ltfu_rate_for(period) +
             visited_without_bp_taken_with_ltfu_rate_for(period)
-        else
-          controlled_patients_rate_for(period) +
+
+          hsh[period] = 100 - remaining_percentages
+        end
+      else
+        self.missed_visits_rate = range.each_with_object(Hash.new(0)) do |period, hsh|
+          remaining_percentages =
+            controlled_patients_rate_for(period) +
             uncontrolled_patients_rate_for(period) +
             visited_without_bp_taken_rate_for(period)
-        end
 
-        hsh[period] = 100 - remaining_percentages
+          hsh[period] = 100 - remaining_percentages
+        end
       end
     end
 
