1e42cde9d4cee1644ac87a13f3bc4b16fc907f96
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-03-29 13:16:33 +0530

    Sort patients by recorded_at

--- app/controllers/admin/deduplicate_patients_controller.rb
+++ app/controllers/admin/deduplicate_patients_controller.rb
@@ -7,7 +7,7 @@ class Admin::DeduplicatePatientsController < AdminController
 
     duplicate_patient_ids = PatientDeduplication::Strategies.identifier_excluding_full_name_match(limit: DUPLICATE_LIMIT)
     @duplicate_count = duplicate_patient_ids.count
-    @patients = Patient.where(id: duplicate_patient_ids.sample)
+    @patients = Patient.where(id: duplicate_patient_ids.sample).order(recorded_at: :asc)
   end
 
   def merge
