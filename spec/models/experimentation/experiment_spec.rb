269c11e7d5fc39fdaf0208888039bd02039faa5c
Rob Sanheim <rsanheim@gmail.com>
2021-04-02 14:24:19 -0500

    wip spec

--- spec/models/experimentation/experiment_spec.rb
+++ spec/models/experimentation/experiment_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe Experimentation::Experiment, type: :model do
     it { experiment.should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:state) }
     it { should validate_presence_of(:experiment_type) }
+
+    it "there can only be one live experiment at a time" do
+      experiment_1 = create(:experiment, state: :live, experiment_type: "current_patient_reminder")
+      experiment_2 = create(:experiment, state: :live, experiment_type: "stale_patient_reminder")
+
+      experiment_3 = build(:experiment, state: :live, experiment_type: "current_patient_reminder")
+      expect(experiment_3).to be_invalid
+
+      experiment_4 = build(:experiment, state: :live, experiment_type: "stale_patient_reminder")
+      expect(experiment_4).to be_invalid
+    end
   end
 end
