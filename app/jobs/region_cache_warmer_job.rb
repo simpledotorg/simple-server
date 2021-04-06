4c0097467a89c1797c6f0aec6138e5db86a3274b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-04-06 16:04:03 +0530

    Dont run anything in the job

--- app/jobs/region_cache_warmer_job.rb
+++ app/jobs/region_cache_warmer_job.rb
@@ -6,17 +6,6 @@ class RegionCacheWarmerJob
   # sidekiq_throttle(concurrency: {limit: ENV["REGION_CACHE_WARMER_CONCURRENCY"] || DEFAULT_CONCURRENCY_LIMIT})
 
   def perform(region_id, period_attributes)
-    region = Region.find(region_id)
-    period = Period.new(period_attributes)
-    RequestStore.store[:bust_cache] = true
-
-    notify "starting region caching for region #{region_id}"
-    # Reports::RegionService.call(region: region, period: period, with_exclusions: true)
-    # Statsd.instance.increment("region_cache_warmer.with_exclusions.#{region.region_type}.cache")
-
-    PatientBreakdownService.call(region: region, period: period)
-    Statsd.instance.increment("patient_breakdown_service.#{region.region_type}.cache")
-    notify "finished region caching for region #{region_id}"
   end
 
   private
