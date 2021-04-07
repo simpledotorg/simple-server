576d39e47d4aa37f720648bcc74e7d97fc4c2e05
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-04-07 16:30:18 +0530

    Leave a note around NoBPMeasureQuery

--- app/queries/no_bp_measure_query.rb
+++ app/queries/no_bp_measure_query.rb
@@ -3,8 +3,10 @@ class NoBPMeasureQuery
   delegate :sanitize_sql, to: ActiveRecord::Base
 
   def call(region, period)
-    # TODO: This should take in a with_ltfu option, since LTFU patients are eligible to be counted.
-    logger.info { "#{self.class} called for region=#{region.slug} period=#{period} with_exclusions=#{with_exclusions}" }
+    # This query always excludes LTFU patients currently, which is incorrect.
+    # TODO: Accept a with_ltfu parameter which controls this behaviour.
+    # See: https://app.clubhouse.io/simpledotorg/story/3112
+    logger.info { "#{self.class} called for region=#{region.slug} period=#{period}" }
 
     facility_ids = region.facilities.map(&:id)
     return 0 if facility_ids.blank?
@@ -13,7 +15,7 @@ class NoBPMeasureQuery
     registration_date = period.blood_pressure_control_range.begin
 
     Patient
-      .for_reports
+      .for_reports(exclude_ltfu_as_of: period.end_time)
       .joins(sanitize_sql(["LEFT OUTER JOIN appointments ON appointments.patient_id = patients.id
           AND appointments.device_created_at >= ?
           AND appointments.device_created_at <= ?", start_time, end_time]))
