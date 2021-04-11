3c7eed5e50bddb24e93b378d6432e51c8875050e
kpethtel <kris.pethtel@gmail.com>
2021-04-11 13:38:38 -0400

    add scheduled status to appointment reminders

--- app/jobs/appointment_reminders/send_daily_reminders_job.rb
+++ app/jobs/appointment_reminders/send_daily_reminders_job.rb
@@ -7,9 +7,10 @@ class AppointmentReminders::SendDailyRemindersJob < ApplicationJob
     # result in messages being sent the same day, but we will need to be certain of that
     # either by carefully scheduling the cron or by moving away from next_messaging_time
     next_messaging_time = Communication.next_messaging_time
-    reminders = AppointmentReminder.where(remind_on: Date.current).pluck(:id)
+    reminders = AppointmentReminder.where(remind_on: Date.current, status: "pending")
     reminders.each do |reminder|
-      AppointmentReminders::SendReminderJob.perform_at(next_messaging_time, reminder)
+      AppointmentReminders::SendReminderJob.perform_at(next_messaging_time, reminder.id)
+      reminder.update(status: "scheduled")
     end
   end
 end
--- spec/jobs/appointment_reminders/send_daily_reminders_job_spec.rb
+++ spec/jobs/appointment_reminders/send_daily_reminders_job_spec.rb
@@ -3,18 +3,46 @@ require "rails_helper"
 RSpec.describe AppointmentReminders::SendDailyRemindersJob, type: :job do
   describe "#perform" do
     it "schedules send_reminder_job for all appointment reminders with a remind_on of today" do
-      reminder1 = create(:appointment_reminder, remind_on: 1.day.ago)
-      reminder2 = create(:appointment_reminder, remind_on: Date.current)
-      reminder3 = create(:appointment_reminder, remind_on: 1.day.from_now)
+      past = create(:appointment_reminder, remind_on: 1.day.ago, status: "pending")
+      current = create(:appointment_reminder, remind_on: Date.current, status: "pending")
+      upcoming = create(:appointment_reminder, remind_on: 1.day.from_now, status: "pending")
 
       schedule_time = 5.minutes.from_now
       allow(Communication).to receive(:next_messaging_time).and_return(schedule_time)
 
-      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, reminder1.id)
-      expect(AppointmentReminders::SendReminderJob).to receive(:perform_at).with(schedule_time, reminder2.id)
-      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, reminder3.id)
+      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, past.id)
+      expect(AppointmentReminders::SendReminderJob).to receive(:perform_at).with(schedule_time, current.id)
+      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, upcoming.id)
 
       AppointmentReminders::SendDailyRemindersJob.perform_now
+    end
+
+    it "only schedules pending appointment reminders" do
+      pending = create(:appointment_reminder, remind_on: Date.current, status: "pending")
+      scheduled = create(:appointment_reminder, remind_on: Date.current, status: "scheduled")
+      sent = create(:appointment_reminder, remind_on: Date.current, status: "sent")
+      cancelled = create(:appointment_reminder, remind_on: Date.current, status: "cancelled")
+
+      schedule_time = 5.minutes.from_now
+      allow(Communication).to receive(:next_messaging_time).and_return(schedule_time)
+
+      expect(AppointmentReminders::SendReminderJob).to receive(:perform_at).with(schedule_time, pending.id)
+      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, scheduled.id)
+      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, sent.id)
+      expect(AppointmentReminders::SendReminderJob).not_to receive(:perform_at).with(schedule_time, cancelled.id)
+
+      AppointmentReminders::SendDailyRemindersJob.perform_now
+    end
+
+    it "updates status to scheduled" do
+      pending = create(:appointment_reminder, remind_on: Date.current, status: "pending")
+
+      schedule_time = 5.minutes.from_now
+      allow(Communication).to receive(:next_messaging_time).and_return(schedule_time)
+
+      expect{
+        AppointmentReminders::SendDailyRemindersJob.perform_now
+      }.to change{ pending.reload.status }.from("pending").to("scheduled")
     end
   end
 end
