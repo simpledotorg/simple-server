cc27335e13fa772386b58cf4d1eaa625b9397438
Rob Sanheim <rsanheim@gmail.com>
2021-04-15 15:00:27 -0500

    Remove unnecessary Org feature flagging

--- app/jobs/appointment_notification/missed_visit_job.rb
+++ app/jobs/appointment_notification/missed_visit_job.rb
@@ -4,14 +4,8 @@ class AppointmentNotification::MissedVisitJob < ApplicationJob
   def perform
     return unless FeatureToggle.enabled?("APPOINTMENT_REMINDERS")
 
-    enabled_organizations.each do |organization|
+    Organization.all.each do |organization|
       AppointmentNotificationService.send_after_missed_visit(appointments: organization.appointments)
     end
-  end
-
-  private
-
-  def enabled_organizations
-    Organization.where(id: ENV["APPOINTMENT_NOTIFICATION_ORG_IDS"].split(","))
   end
 end
