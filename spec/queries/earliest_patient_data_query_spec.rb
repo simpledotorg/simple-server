344f6a14a355483e5b042276ec6136884618b66d
Rob Sanheim <rsanheim@gmail.com>
2021-04-16 14:00:16 -0500

    speed up spec

--- spec/queries/earliest_patient_data_query_spec.rb
+++ spec/queries/earliest_patient_data_query_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe EarliestPatientDataQuery do
     facility_1, facility_2 = FactoryBot.create_list(:facility, 2, facility_group: facility_group_1)
     region = facility_group_1.region
     other_facility = create(:facility)
-    patient_1 = create(:patient, recorded_at: "July 5th 2018", assigned_facility: facility_2)
-    _patient_2 = create(:patient, recorded_at: "June 1st 2018", assigned_facility: other_facility, registration_facility: facility_1)
-    _patient_3 = create(:patient, recorded_at: "January 1 2018", assigned_facility: other_facility, registration_facility: other_facility)
+    patient_1 = create(:patient, recorded_at: "July 5th 2018", assigned_facility: facility_2, registration_user: user)
+    _patient_2 = create(:patient, recorded_at: "June 1st 2018", assigned_facility: other_facility, registration_facility: facility_1, registration_user: user)
+    _patient_3 = create(:patient, recorded_at: "January 1 2018", assigned_facility: other_facility, registration_facility: other_facility, registration_user: user)
 
     expect(EarliestPatientDataQuery.call(region)).to eq(Time.zone.parse("June 1st 2018"))
     patient_1.update! recorded_at: "May 31st 2018"
