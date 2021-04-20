35afddb8d6eeca0be24ad789128a01feda88f92e
kpethtel <kris.pethtel@gmail.com>
2021-04-19 21:21:29 -0400

    add next communication type

--- app/models/appointment_reminder.rb
+++ app/models/appointment_reminder.rb
@@ -3,6 +3,7 @@ class AppointmentReminder < ApplicationRecord
   belongs_to :patient
   belongs_to :experiment, class_name: "Experimentation::Experiment", optional: true
   belongs_to :reminder_template, class_name: "Experimentation::ReminderTemplate", optional: true
+  has_many :communications
 
   validates :status, presence: true
   validates :remind_on, presence: true
@@ -16,4 +17,28 @@ class AppointmentReminder < ApplicationRecord
   }, _prefix: true
 
   scope :due_today, -> { where(remind_on: Date.current, status: [:pending]) }
+
+  def next_communication_type
+    if preferred_communication_method && !previously_communicated_by?(preferred_communication_method)
+      return preferred_communication_method
+    end
+    unless previously_communicated_by?(backup_communication_method)
+      return backup_communication_method
+    end
+    nil
+  end
+
+  private
+
+  def previously_communicated_by?(method)
+    communications.any? { |communication| communication.communication_type == method }
+  end
+
+  def preferred_communication_method
+    CountryConfig.current[:name] == "India" ? "missed_visit_whatsapp_reminder" : nil
+  end
+
+  def backup_communication_method
+    "missed_visit_sms_reminder"
+  end
 end
--- spec/models/appointment_reminder_spec.rb
+++ spec/models/appointment_reminder_spec.rb
@@ -13,4 +13,40 @@ describe AppointmentReminder, type: :model do
     it { should validate_presence_of(:remind_on) }
     it { should validate_presence_of(:message) }
   end
+
+  describe "#next_communication_type" do
+    let(:appointment_reminder) { create(:appointment_reminder) }
+
+    context "in India" do
+      it "returns missed_visit_whatsapp_reminder if it has no whatsapp communications" do
+        expect(appointment_reminder.next_communication_type).to eq("missed_visit_whatsapp_reminder")
+      end
+
+      it "returns missed_visit_sms_reminder if it has a whatsapp communication but no sms communication" do
+        create(:communication, communication_type: "missed_visit_whatsapp_reminder", appointment_reminder: appointment_reminder)
+        expect(appointment_reminder.next_communication_type).to eq("missed_visit_sms_reminder")
+      end
+
+      it "returns nil if it has both a whatsapp and sms communication" do
+        create(:communication, communication_type: "missed_visit_whatsapp_reminder", appointment_reminder: appointment_reminder)
+        create(:communication, communication_type: "missed_visit_sms_reminder", appointment_reminder: appointment_reminder)
+        expect(appointment_reminder.next_communication_type).to eq(nil)
+      end
+    end
+
+    context "outside of India" do
+      before :each do
+        allow(CountryConfig).to receive(:current).and_return(CountryConfig.for(:BD))
+      end
+
+      it "returns missed_visit_sms_reminder if it has no sms communication" do
+        expect(appointment_reminder.next_communication_type).to eq("missed_visit_sms_reminder")
+      end
+
+      it "returns nil if it has an sms communication" do
+        create(:communication, communication_type: "missed_visit_sms_reminder", appointment_reminder: appointment_reminder)
+        expect(appointment_reminder.next_communication_type).to eq(nil)
+      end
+    end
+  end
 end
