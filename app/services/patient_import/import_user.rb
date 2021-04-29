fe12bbc7e779bf0b508ca20f5ea17047406fa028
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-04-29 14:47:31 +0530

    Update transformer specs

--- app/services/patient_import/import_user.rb
+++ app/services/patient_import/import_user.rb
@@ -20,7 +20,7 @@ class PatientImport::ImportUser
     phone_number_authentication = PhoneNumberAuthentication.new(
       phone_number: IMPORT_USER_PHONE_NUMBER,
       password: "#{rand(10)}#{rand(10)}#{rand(10)}#{rand(10)}",
-      registration_facility_id: facility.id
+      registration_facility_id: Facility.take.id
     ).tap do |pna|
       pna.set_otp
       pna.invalidate_otp
--- spec/services/patient_import/spreadsheet_transformer_spec.rb
+++ spec/services/patient_import/spreadsheet_transformer_spec.rb
@@ -6,39 +6,55 @@ RSpec.describe PatientImport::SpreadsheetTransformer do
   let(:facility) { create(:facility) }
 
   it "parses patient data" do
-    params = PatientImport::SpreadsheetTransformer.transform(data, facility: facility)
+    params = PatientImport::SpreadsheetTransformer.call(data, facility: facility)
 
-    patient = params.find { |p| p[:patient][:full_name] == "Basic Patient 1" }
+    patient = params.find { |p| p[:patient][:full_name] == "Basic Patient 1" }.deep_symbolize_keys
+    patient_id = patient[:patient][:id]
+    registration_time = Time.parse("2020-10-16")
+    first_visit_time = Time.parse("2021-01-29")
+    last_visit_time = Time.parse("2021-02-24")
 
-    expect(patient[:patient]).to eq(
-      age: "45",
+    expect(patient[:patient]).to include(
+      age: 45,
+      age_updated_at: registration_time,
       full_name: "Basic Patient 1",
-      gender: "Male",
-      recorded_at: "2020-10-16",
-      registration_facility_id: nil,
+      gender: "male",
+      recorded_at: registration_time,
+      registration_facility_id: facility.id,
       status: :active
     )
 
-    expect(patient[:phone_number]).to eq(
-      number: "936528787",
-      phone_type: :mobile,
-      active: true
+    expect(patient[:patient][:phone_numbers]).to contain_exactly(
+      hash_including(
+        number: "936528787",
+        phone_type: :mobile,
+        active: true,
+        created_at: registration_time,
+        updated_at: registration_time
+      )
     )
 
-    expect(patient[:address]).to eq(
+    expect(patient[:patient][:address]).to include(
       country: "India",
       state: "Addis Ababa",
       street_address: "45 Main Street",
       village_or_colony: "Berrytown",
-      zone: "Fruit County"
+      zone: "Fruit County",
+      created_at: registration_time,
+      updated_at: registration_time
     )
 
-    expect(patient[:business_identifier]).to eq(
-      identifier: "0000001",
-      identifier_type: "ethiopia_medical_record"
+    expect(patient[:patient][:business_identifiers]).to contain_exactly(
+      hash_including(
+        identifier: "0000001",
+        identifier_type: "ethiopia_medical_record",
+        created_at: registration_time,
+        updated_at: registration_time
+      )
     )
 
-    expect(patient[:medical_history]).to eq(
+    expect(patient[:medical_history]).to include(
+      patient_id: patient_id,
       chronic_kidney_disease: "yes",
       diabetes: "no",
       hypertension: "yes",
@@ -47,110 +63,120 @@ RSpec.describe PatientImport::SpreadsheetTransformer do
     )
 
     expect(patient[:blood_pressures]).to contain_exactly(
-      {systolic: "160", diastolic: "90", recorded_at: "2020-01-29"},
-      {systolic: "145", diastolic: "89", recorded_at: "2021-02-24"}
+      hash_including(
+        patient_id: patient_id,
+        systolic: 160,
+        diastolic: 90,
+        recorded_at: first_visit_time,
+        created_at: first_visit_time,
+        updated_at: first_visit_time
+      ),
+      hash_including(
+        patient_id: patient_id,
+        systolic: 145,
+        diastolic: 89,
+        recorded_at: last_visit_time,
+        created_at: last_visit_time,
+        updated_at: last_visit_time
+      )
     )
 
     expect(patient[:prescription_drugs]).to contain_exactly(
       # first visit drugs deleted
-      {
+      hash_including(
         name: "Amlodipine",
         dosage: "5 mg",
         rxnorm_code: "329528",
         is_deleted: true,
         is_protocol_drug: true,
-        created_at: "2020-01-29",
-        deleted_at: "2021-02-24"
-      },
-      {
+        created_at: first_visit_time,
+        updated_at: last_visit_time,
+        deleted_at: last_visit_time
+      ),
+      hash_including(
         name: "Enalapril",
         dosage: "5 mg",
         rxnorm_code: "833236",
         is_deleted: true,
         is_protocol_drug: false,
-        created_at: "2020-01-29",
-        deleted_at: "2021-02-24"
+        created_at: first_visit_time,
+        updated_at: last_visit_time,
+        deleted_at: last_visit_time
 
-      },
-      {
+      ),
+      hash_including(
         name: "Aspirin",
         dosage: "75 mg",
         rxnorm_code: "315429",
         is_deleted: true,
         is_protocol_drug: false,
-        created_at: "2020-01-29",
-        deleted_at: "2021-02-24"
+        created_at: first_visit_time,
+        updated_at: last_visit_time,
+        deleted_at: last_visit_time
 
-      },
-      {
+      ),
+      hash_including(
         name: "Hydrochlorothiazide",
         dosage: "12.5 mg",
         rxnorm_code: "316047",
         is_deleted: true,
         is_protocol_drug: false,
-        created_at: "2020-01-29",
-        deleted_at: "2021-02-24"
+        created_at: first_visit_time,
+        updated_at: last_visit_time,
+        deleted_at: last_visit_time
 
-      },
-      {
+      ),
+      hash_including(
         name: "Lisinopril",
         dosage: "5 mg",
         rxnorm_code: "316156",
         is_deleted: true,
         is_protocol_drug: false,
-        created_at: "2020-01-29",
-        deleted_at: "2021-02-24"
-      },
+        created_at: first_visit_time,
+        updated_at: last_visit_time,
+        deleted_at: last_visit_time
+      ),
       # second visit drugs not deleted
-      {
+      hash_including(
         name: "Amlodipine",
         dosage: "10 mg",
         rxnorm_code: "329526",
+        is_deleted: false,
         is_protocol_drug: false,
-        created_at: "2021-02-24"
-      }
+        created_at: last_visit_time,
+        updated_at: last_visit_time
+      )
     )
   end
 
   it "captures other patient attributes" do
-    params = PatientImport::SpreadsheetTransformer.transform(data, facility: facility)
+    params = PatientImport::SpreadsheetTransformer.call(data, facility: facility)
 
     patient = params.find { |p| p[:patient][:full_name] == "Basic Patient 2" }
 
-    expect(patient[:patient][:gender]).to eq("Female")
+    expect(patient[:patient][:gender]).to eq("female")
     expect(patient[:patient][:status]).to eq(:dead)
   end
 
   context "when last visit is absent" do
     it "retains BP from first visit" do
-      params = PatientImport::SpreadsheetTransformer.transform(data, facility: facility)
+      params = PatientImport::SpreadsheetTransformer.call(data, facility: facility)
       patient = params.find { |p| p[:patient][:full_name] == "No Last Visit" }
+      first_visit_time = Time.parse("2021-01-29")
 
       expect(patient[:blood_pressures]).to contain_exactly(
-        systolic: "160", diastolic: "90", recorded_at: "2020-01-29"
+        hash_including(systolic: 160, diastolic: 90, recorded_at: first_visit_time)
       )
     end
 
     it "does not delete prescription drugs from first visit" do
-      params = PatientImport::SpreadsheetTransformer.transform(data, facility: facility)
+      params = PatientImport::SpreadsheetTransformer.call(data, facility: facility)
       patient = params.find { |p| p[:patient][:full_name] == "No Last Visit" }
 
-      expect(patient[:prescription_drugs]).to eq([
-        {
-          name: "Amlodipine",
-          dosage: "5 mg",
-          rxnorm_code: "329528",
-          is_protocol_drug: true,
-          created_at: "2020-01-29"
-        },
-        {
-          name: "Enalapril",
-          dosage: "5 mg",
-          rxnorm_code: "833236",
-          is_protocol_drug: false,
-          created_at: "2020-01-29"
-        }
-      ])
+      patient[:prescription_drugs].each do |drug|
+        expect(drug[:is_deleted]).to be_falsey
+        expect(drug[:deleted_at]).to be_nil
+      end
     end
   end
 end
