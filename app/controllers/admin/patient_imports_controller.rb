1983674be4901733a598c7c2ea78c957baed1eac
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-04-29 17:59:57 +0530

    Controller (integration) specs

--- app/controllers/admin/patient_imports_controller.rb
+++ app/controllers/admin/patient_imports_controller.rb
@@ -11,14 +11,14 @@ class Admin::PatientImportsController < AdminController
     facility = Facility.find(params[:facility_id])
 
     data = read_xlsx_or_csv_file(params[:patient_import_file])
-    params = PatientImport::SpreadsheetTransformer.call(data, facility: facility, admin: current_admin)
+    params = PatientImport::SpreadsheetTransformer.call(data, facility: facility)
     errors = PatientImport::Validator.new(params).errors
 
     if errors.values.flatten.any?
       @errors = errors
       render :new
     else
-      results = PatientImport::Importer.call(params: params, facility: facility)
+      results = PatientImport::Importer.call(params: params, facility: facility, admin: current_admin)
       redirect_to new_admin_patient_import_url, notice: "Successfully imported #{results.count} patients to #{facility.name}"
     end
   end
---
+++ spec/controllers/admin/patient_imports_controller_spec.rb
@@ -0,0 +1,128 @@
+require "rails_helper"
+
+RSpec.describe Admin::PatientImportsController, type: :controller do
+  describe "GET #new" do
+    it "returns a success response for power users" do
+      admin = create(:admin, :power_user)
+      sign_in(admin.email_authentication)
+
+      get :new
+      expect(response).to be_successful
+    end
+
+    it "denies access to other users" do
+      admin = create(:admin, :manager)
+      sign_in(admin.email_authentication)
+
+      get :new
+      expect(response).to be_redirect
+    end
+  end
+
+  describe "POST #create" do
+    let!(:facility) { create(:facility) }
+
+    context "with valid data in file" do
+      it "imports the patients" do
+        admin = create(:admin, :power_user)
+        sign_in(admin.email_authentication)
+
+        timezone = CountryConfig.current[:time_zone]
+        facility = create(:facility)
+        patient_import_file = fixture_file_upload("files/patient_import_test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        params = {patient_import_file: patient_import_file, facility_id: facility.id}
+
+        post :create, params: params
+        basic_patient_1 = Patient.find_by(full_name: "Basic Patient 1")
+        basic_patient_2 = Patient.find_by(full_name: "Basic Patient 2")
+        no_last_visit_patient = Patient.find_by(full_name: "No Last Visit")
+        no_meds_in_last_visit_patient = Patient.find_by(full_name: "No Meds In Last Visit")
+
+        # Ensure all patients are imported
+        expect(basic_patient_1).to be_present
+        expect(basic_patient_2).to be_present
+        expect(no_last_visit_patient).to be_present
+        expect(no_meds_in_last_visit_patient).to be_present
+
+        # Dig into patient 1's details extensively
+        # Patient 1 attributes
+        expect(basic_patient_1.attributes).to include(
+          "age" => 45,
+          "gender" => "male",
+          "status" => "active"
+        )
+        expect(basic_patient_1.recorded_at.in_time_zone(timezone).to_date).to eq(Date.parse("2020-10-16"))
+
+        # Patient 1 address
+        expect(basic_patient_1.address.attributes).to include(
+          "street_address" => "45 Main Street",
+          "village_or_colony" => "Berrytown",
+          "zone" => "Fruit County",
+          "state" => "Addis Ababa",
+          "country" => CountryConfig.current[:name]
+        )
+
+        # Patient 1 phone number
+        expect(basic_patient_1.latest_phone_number).to eq("936528787")
+
+        # Patient 1 business identifier
+        expect(basic_patient_1.business_identifiers.last.attributes).to include(
+          "identifier" => "0000001",
+          "identifier_type" => "ethiopia_medical_record"
+        )
+
+        # Patient 1 BPs
+        basic_patient_1_bps = basic_patient_1.blood_pressures.map do |bp|
+          "#{bp.systolic}/#{bp.diastolic}"
+        end.join(", ")
+
+        expect(basic_patient_1_bps).to eq("160/90, 145/89")
+
+        # Patient 1 prescription drugs
+        basic_patient_1_prescription_drugs = basic_patient_1.prescription_drugs.map do |pd|
+          "#{pd.name} #{pd.dosage}"
+        end.join(", ")
+
+        expect(basic_patient_1_prescription_drugs).to eq("Amlodipine 10 mg")
+      end
+
+      it "displays a success message" do
+        admin = create(:admin, :power_user)
+        sign_in(admin.email_authentication)
+
+        facility = create(:facility)
+        patient_import_file = fixture_file_upload("files/patient_import_test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        params = {patient_import_file: patient_import_file, facility_id: facility.id}
+
+        post :create, params: params
+        expect(flash[:notice]).to match(/imported 4 patients.*#{facility.name}/)
+      end
+    end
+
+    context "with invalid file" do
+      it "raises validation errors" do
+        admin = create(:admin, :power_user)
+        sign_in(admin.email_authentication)
+
+        facility = create(:facility)
+        patient_import_file = fixture_file_upload("files/patient_import_invalid_test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        params = {patient_import_file: patient_import_file, facility_id: facility.id}
+
+        expect{ post :create, params: params }.not_to change { Patient.count }
+      end
+    end
+
+    context "for non-power users" do
+      it "denies access to import patients" do
+        admin = create(:admin, :manager)
+        sign_in(admin.email_authentication)
+
+        facility = create(:facility)
+        patient_import_file = fixture_file_upload("files/patient_import_test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        params = {patient_import_file: patient_import_file, facility_id: facility.id}
+
+        expect{ post :create, params: params }.not_to change { Patient.count }
+      end
+    end
+  end
+end
