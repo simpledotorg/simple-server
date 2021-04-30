7378806e414086c71406b00095ac174711fce959
kpethtel <kris.pethtel@gmail.com>
2021-04-30 15:31:17 -0400

    do not use repo to fetch assigned patient counts

--- app/services/who_report_service.rb
+++ app/services/who_report_service.rb
@@ -66,22 +66,23 @@ class WhoReportService
 
   def district_row
     region_data = populate_region_data(region)
-    dead = Patient.where(assigned_facility_id: region.facilities.pluck(:id), status: "dead").count
     registered_by_month = months.map { |month|
       dashboard_analytics.sum { |_, data| data.dig(:registered_patients_by_period, month.value) || 0 }
     }
     follow_up_by_month = months.map { |month|
       dashboard_analytics.sum { |_, data| data.dig(:follow_up_patients_by_period, month.value) || 0 }
     }
+    patients = Patient.with_hypertension.where(assigned_facility: region.facilities.pluck(:id))
+
     [
       "All",
       region.name,
       Array.new(5, nil),
       region_data.dig(:cumulative_registrations, period),
-      region_data.dig(:cumulative_patients, period),
+      patients.excluding_dead.count,
       region_data.dig(:ltfu_counts, period),
-      dead,
-      region_data.dig(:adjusted_patient_counts, period),
+      patients.status_dead.count,
+      patients.excluding_dead.not_ltfu_as_of(period.end).count,
       registered_by_month,
       follow_up_by_month,
       region_data.dig(:controlled_patients, period),
@@ -98,7 +99,6 @@ class WhoReportService
     }
     region.facility_regions.map.with_index do |facility, index|
       analytics_data = dashboard_analytics[facility.id]
-      dead = Patient.where(assigned_facility_id: facility.source.id, status: "dead").count
       registration_numbers = []
       follow_up_numbers = []
 
@@ -106,6 +106,7 @@ class WhoReportService
         registration_numbers << (dashboard_analytics.dig(facility.id, :registered_patients_by_period, month.value) || 0)
         follow_up_numbers << (dashboard_analytics.dig(facility.id, :follow_up_patients_by_period, month.value) || 0)
       end
+      patients = Patient.with_hypertension.where(assigned_facility: facility.source)
 
       matching_facility = facilities_data.find { |fac| fac[:region] == facility }
       [
@@ -117,10 +118,10 @@ class WhoReportService
         facility.source.blood_pressures.any? ? "Active" : "Inactive",
         nil,
         matching_facility.dig(:cumulative_registrations, period),
-        matching_facility.dig(:cumulative_patients, period),
+        patients.excluding_dead.count,
         matching_facility.dig(:ltfu_counts, period),
-        dead,
-        matching_facility.dig(:adjusted_patient_counts, period),
+        patients.status_dead.count,
+        patients.excluding_dead.not_ltfu_as_of(period.end).count,
         registration_numbers,
         follow_up_numbers,
         matching_facility.dig(:controlled_patients, period),
@@ -136,11 +137,9 @@ class WhoReportService
     slug = region.slug
     region_data = Hash.new(0)
     region_data[:region] = region
-    region_data[:adjusted_patient_counts] = repo.adjusted_patient_counts[slug]
     region_data[:controlled_patients] = repo.controlled_patients_count[slug]
     region_data[:uncontrolled_patients] = repo.uncontrolled_patients_count[slug]
     region_data[:missed_visits] = repo.missed_visits[slug]
-    region_data[:cumulative_patients] = repo.cumulative_assigned_patients_count[slug]
     region_data[:cumulative_registrations] = repo.cumulative_registrations[slug]
     region_data[:ltfu_counts] = repo.ltfu_counts[slug]
     region_data[:visited_without_bp_taken] = repo.visited_without_bp_taken[slug]
