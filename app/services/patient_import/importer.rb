0bb6c1010157f316b2539e0e4666bc1f6e1104e8
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-05-03 22:46:07 +0530

    Create audit logs instead

--- app/services/patient_import/importer.rb
+++ app/services/patient_import/importer.rb
@@ -23,10 +23,22 @@ class PatientImport::Importer
         blood_pressures_params = patient_record_params[:blood_pressures].map { |bp| Api::V3::BloodPressureTransformer.from_request(bp) }
         prescription_drugs_params = patient_record_params[:prescription_drugs].map { |drug| Api::V3::PrescriptionDrugTransformer.from_request(drug) }
 
-        import_patient(patient_params)
-        import_blood_pressures(blood_pressures_params)
-        import_medical_history(medical_history_params)
-        import_prescription_drugs(prescription_drugs_params)
+        patient = import_patient(patient_params)
+        medical_history = import_medical_history(medical_history_params)
+        blood_pressures = import_blood_pressures(blood_pressures_params)
+        prescription_drugs = import_prescription_drugs(prescription_drugs_params)
+
+        AuditLog.create_logs_async(
+          PatientImport::ImportUser.find_or_create,
+          [
+            patient,
+            medical_history,
+            *blood_pressures,
+            *prescription_drugs
+          ],
+          "import",
+          Time.current
+        )
 
         results.push(
           patient: patient_params,
@@ -45,26 +57,21 @@ class PatientImport::Importer
       request_facility_id: facility.id,
       request_user_id: PatientImport::ImportUser.find_or_create.id
     }).merge
-
-    PatientImportLog.create!(user: admin, record: patient)
   end
 
   def import_blood_pressures(params)
-    params.each do |bp_params|
-      blood_pressure = merge_encounter_observation(:blood_pressures, bp_params)
-      PatientImportLog.create!(user: admin, record: blood_pressure)
+    params.map do |bp_params|
+      merge_encounter_observation(:blood_pressures, bp_params)
     end
   end
 
   def import_medical_history(params)
     medical_history = MedicalHistory.merge(params)
-    PatientImportLog.create!(user: admin, record: medical_history)
   end
 
   def import_prescription_drugs(params)
-    params.each do |pd_params|
-      prescription_drug = PrescriptionDrug.merge(pd_params)
-      PatientImportLog.create!(user: admin, record: prescription_drug)
+    params.map do |pd_params|
+      PrescriptionDrug.merge(pd_params)
     end
   end
 
--- spec/services/patient_import/importer_spec.rb
+++ spec/services/patient_import/importer_spec.rb
@@ -44,40 +44,5 @@ RSpec.describe PatientImport::Importer do
 
       importer.call
     end
-
-    it "creates patient import logs" do
-      facility = create(:facility)
-      admin = create(:admin)
-      params = [{
-        patient: {property: "patient property"},
-        medical_history: {property: "medical history property"},
-        blood_pressures: [{property: "bp1 property"}, {property: "bp2 property"}],
-        prescription_drugs: [{property: "pd1 property"}, {property: "pd2 property"}]
-      }]
-
-      new_patient = create(:patient)
-      new_bp = create(:blood_pressure)
-      new_medical_history = create(:medical_history)
-      new_prescription_drug = create(:prescription_drug)
-
-      importer = PatientImport::Importer.new(params: params, facility: facility, admin: admin)
-
-      allow(Api::V3::PatientTransformer).to receive(:from_nested_request).and_call_original
-      allow(Api::V3::MedicalHistoryTransformer).to receive(:from_request).and_call_original
-      allow(Api::V3::BloodPressureTransformer).to receive(:from_request).and_call_original
-      allow(Api::V3::PrescriptionDrugTransformer).to receive(:from_request).and_call_original
-
-      allow_any_instance_of(MergePatientService).to receive(:merge).and_return(new_patient)
-      allow(importer).to receive(:merge_encounter_observation).and_return(new_bp)
-      allow(MedicalHistory).to receive(:merge).and_return(new_medical_history)
-      allow(PrescriptionDrug).to receive(:merge).and_return(new_prescription_drug)
-
-      importer.call
-
-      expect(PatientImportLog.find_by(user: admin, record: new_patient)).to be_present
-      expect(PatientImportLog.find_by(user: admin, record: new_bp)).to be_present
-      expect(PatientImportLog.find_by(user: admin, record: new_medical_history)).to be_present
-      expect(PatientImportLog.find_by(user: admin, record: new_prescription_drug)).to be_present
-    end
   end
 end
