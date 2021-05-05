6bf401f582d6d8de1f0da113fdc4152cf9512127
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-05-05 19:45:06 +0530

    Dont send state

--- app/transformers/api/v3/facility_transformer.rb
+++ app/transformers/api/v3/facility_transformer.rb
@@ -9,7 +9,8 @@ class Api::V3::FacilityTransformer
           "teleconsultation_isd_code",
           "teleconsultation_phone_numbers",
           "organization_name",
-          "facility_group_name")
+          "facility_group_name",
+          "state")
         .merge(config: {enable_diabetes_management: facility.enable_diabetes_management,
                         enable_teleconsultation: facility.enable_teleconsultation},
                protocol_id: facility.protocol.try(:id))
