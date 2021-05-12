64b1fe68a0f308b4204a16afd2f713dd2637f7a6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-05-12 16:47:38 +0530

    lets not

--- spec/tasks/scripts/split_bathinda_and_mansa_spec.rb
+++ spec/tasks/scripts/split_bathinda_and_mansa_spec.rb
@@ -2,16 +2,15 @@ require "rails_helper"
 require "tasks/scripts/split_bathinda_and_mansa"
 
 RSpec.describe SplitBathindaAndMansa do
-  let!(:organization) { create(:organization) }
-  let!(:bathinda_and_mansa) { create(:facility_group, name: "Bathinda and Mansa", organization: organization, state: "Punjab") }
-  let!(:bathinda) { create(:facility_group, name: "Bathinda", organization: organization, state: "Punjab") }
-  let!(:mansa) { create(:facility_group, name: "Mansa", organization: organization, state: "Punjab") }
-  let!(:facilities) {
-    [create(:facility, district: "Bathinda", block: "Bathinda block", facility_group: bathinda_and_mansa),
-      create(:facility, district: "Mansa", block: "Mansa block", facility_group: bathinda_and_mansa)]
-  }
-
   it "sets the facility group on the facilities" do
+    organization = create(:organization)
+    bathinda_and_mansa = create(:facility_group, name: "Bathinda and Mansa", organization: organization, state: "Punjab")
+    bathinda = create(:facility_group, name: "Bathinda", organization: organization, state: "Punjab")
+    mansa = create(:facility_group, name: "Mansa", organization: organization, state: "Punjab")
+    facilities =
+      [create(:facility, district: "Bathinda", block: "Bathinda block", facility_group: bathinda_and_mansa),
+       create(:facility, district: "Mansa", block: "Mansa block", facility_group: bathinda_and_mansa)]
+
     described_class.call
 
     expect(bathinda_and_mansa.facilities.count).to eq 0
@@ -20,6 +19,14 @@ RSpec.describe SplitBathindaAndMansa do
   end
 
   it "doesn't change the sync_region_id (block id) of the facilities" do
+    organization = create(:organization)
+    bathinda_and_mansa = create(:facility_group, name: "Bathinda and Mansa", organization: organization, state: "Punjab")
+    bathinda = create(:facility_group, name: "Bathinda", organization: organization, state: "Punjab")
+    mansa = create(:facility_group, name: "Mansa", organization: organization, state: "Punjab")
+    facilities =
+      [create(:facility, district: "Bathinda", block: "Bathinda block", facility_group: bathinda_and_mansa),
+       create(:facility, district: "Mansa", block: "Mansa block", facility_group: bathinda_and_mansa)]
+
     original_blocks = facilities.map(&:block)
     described_class.call
 
@@ -28,6 +35,14 @@ RSpec.describe SplitBathindaAndMansa do
   end
 
   it "reparents the block regions correctly" do
+    organization = create(:organization)
+    bathinda_and_mansa = create(:facility_group, name: "Bathinda and Mansa", organization: organization, state: "Punjab")
+    bathinda = create(:facility_group, name: "Bathinda", organization: organization, state: "Punjab")
+    mansa = create(:facility_group, name: "Mansa", organization: organization, state: "Punjab")
+    facilities =
+      [create(:facility, district: "Bathinda", block: "Bathinda block", facility_group: bathinda_and_mansa),
+       create(:facility, district: "Mansa", block: "Mansa block", facility_group: bathinda_and_mansa)]
+
     described_class.call
 
     facilities.map(&:reload)
@@ -36,6 +51,14 @@ RSpec.describe SplitBathindaAndMansa do
   end
 
   it "reparents the facility regions correctly" do
+    organization = create(:organization)
+    bathinda_and_mansa = create(:facility_group, name: "Bathinda and Mansa", organization: organization, state: "Punjab")
+    bathinda = create(:facility_group, name: "Bathinda", organization: organization, state: "Punjab")
+    mansa = create(:facility_group, name: "Mansa", organization: organization, state: "Punjab")
+    facilities =
+      [create(:facility, district: "Bathinda", block: "Bathinda block", facility_group: bathinda_and_mansa),
+       create(:facility, district: "Mansa", block: "Mansa block", facility_group: bathinda_and_mansa)]
+
     described_class.call
 
     facilities.map(&:reload)
