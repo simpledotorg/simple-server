152c13cb10f12288ffcff178979c04dbc1d4053e
Claudio Vallejo <hola@claudiovallejo.mx>
2021-05-13 15:11:19 -0400

    Replace "Protocols" copy with "Medications" (#2481)
    * Updated nav protocol link; Updated protocols/show.html.erb
    
    * Updated protocols/new.html.erb
    
    * Updated protocols/edit.html.erb
    
    * Updated protocols/show.html.erb
    
    * Updated protocol drug form and new.html.erb
    
    * Updated protocol_drugs/edit.html.erb
    
    * Fixed failing protocols/show_spec.rb tests
    
    * Updated test names to use 'medications' copy
    
    * Updated index_spec failing tests
    
    * Replacing 'protocols' and 'drug' copy in protocol tests
    
    * Fixed breaking rspec test
    
    * Copy changes to add descriptions to the Medications screens. Also changed the copy on the buttons to remove "Add"
    
    * Removed trailing white space
    
    * Removed trailing white space
    
    * Added closing tag, using proper 'less than' character
    
    * Renamed 'var_medication_list'
    
    * Renamed 'var_medication_list' and 'var_medication'
    
    * Set custom label for 'Follow-up days' field
    
    * Fixed breaking rspec test
    
    * Retrigger build
    
    Co-authored-by: Daniel Burka <danielburka@gmail.com>

--- spec/pages/admin/protocol_drugs/edit.rb
+++ spec/pages/admin/protocol_drugs/edit.rb
@@ -1,20 +1,20 @@
 module AdminPage
   module ProtocolDrugs
     class Edit < ApplicationPage
-      NAME = {id: "protocol_drug_name"}.freeze
-      PROTOCOL_DRUG_DOSAGE = {id: "protocol_drug_dosage"}.freeze
-      RX_NORM_CODE = {id: "protocol_drug_rxnorm_code"}.freeze
-      UPDATE_PROTOCOL_BUTTON = {css: "input.btn-primary"}.freeze
-      PROTOCOL_NAME_HEADING = {css: "h1"}.freeze
+      NAME = {id: "medication_name"}.freeze
+      MEDICATION_DOSAGE = {id: "medication_dosage"}.freeze
+      RX_NORM_CODE = {id: "medication_rxnorm_code"}.freeze
+      UPDATE_MEDICATION_BUTTON = {css: "input.btn-primary"}.freeze
+      MEDICATION_LIST_NAME_HEADING = {css: "h1"}.freeze
 
-      def edit_protocol_drug_info(dosage, code)
-        present?(PROTOCOL_NAME_HEADING)
-        type(PROTOCOL_DRUG_DOSAGE, dosage)
+      def edit_medication_info(dosage, code)
+        present?(MEDICATION_LIST_NAME_HEADING)
+        type(MEDICATION_DOSAGE, dosage)
         type(RX_NORM_CODE, code)
-        click(UPDATE_PROTOCOL_BUTTON)
+        click(UPDATE_MEDICATION_BUTTON)
 
         # assertion
-        page.has_content?("Protocol drug was successfully updated.")
+        page.has_content?("Medication was successfully updated")
       end
     end
   end
--- spec/pages/admin/protocol_drugs/new.rb
+++ spec/pages/admin/protocol_drugs/new.rb
@@ -1,17 +1,17 @@
 module AdminPage
   module ProtocolDrugs
     class New < ApplicationPage
-      NAME = {id: "protocol_drug_name"}.freeze
-      PROTOCOL_DRUG_DOSAGE = {id: "protocol_drug_dosage"}.freeze
-      RX_NORM_CODE = {id: "protocol_drug_rxnorm_code"}.freeze
-      CREATE_PROTOCOL_BUTTON = {css: "input.btn-primary"}.freeze
-      PROTOCOL_NAME_HEADING = {css: "h1"}.freeze
+      NAME = {id: "medication_name"}.freeze
+      MEDICATION_DOSAGE = {id: "medication_dosage"}.freeze
+      RX_NORM_CODE = {id: "medication_rxnorm_code"}.freeze
+      CREATE_MEDICATION_BUTTON = {css: "input.btn-primary"}.freeze
+      MEDICATION_NAME_HEADING = {css: "h1"}.freeze
 
-      def add_new_protocol_drug(name, dosage, code)
+      def add_new_medication(name, dosage, code)
         type(NAME, name)
-        type(PROTOCOL_DRUG_DOSAGE, dosage)
+        type(MEDICATION_DOSAGE, dosage)
         type(RX_NORM_CODE, code)
-        click(CREATE_PROTOCOL_BUTTON)
+        click(CREATE_MEDICATION_BUTTON)
       end
     end
   end
