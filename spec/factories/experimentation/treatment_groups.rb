1f60f5454f714f97bad80969e803beaef9776975
kpethtel <kris.pethtel@gmail.com>
2021-05-17 17:12:39 -0400

    change treatment group factory

--- spec/factories/experimentation/treatment_groups.rb
+++ spec/factories/experimentation/treatment_groups.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :treatment_group, class: Experimentation::TreatmentGroup do
-    index { 0 }
     description { Faker::Lorem.unique.word }
     association :experiment, factory: :experiment
   end
