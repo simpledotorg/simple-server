aea1a183877191e11927f8328e32c6ed8afe870c
Rob Sanheim <rsanheim@gmail.com>
2021-05-18 13:21:57 -0500

    Add back appointment id to communications
    we need to backfill some data first

--- db/migrate/20210517215935_remove_appointment_from_communication_and_add_patient.rb
+++ db/migrate/20210517215935_remove_appointment_from_communication_and_add_patient.rb
@@ -1,6 +1,5 @@
 class RemoveAppointmentFromCommunicationAndAddPatient < ActiveRecord::Migration[5.2]
   def change
-    remove_column :communications, :appointment_id
     add_reference :communications, :patient, null: true, foreign_key: true, type: :uuid
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_215935) do
     t.string "detailable_type"
     t.bigint "detailable_id"
     t.uuid "notification_id"
+    t.uuid "appointment_id"
     t.uuid "patient_id"
     t.index ["deleted_at"], name: "index_communications_on_deleted_at"
     t.index ["detailable_type", "detailable_id"], name: "index_communications_on_detailable_type_and_detailable_id"
