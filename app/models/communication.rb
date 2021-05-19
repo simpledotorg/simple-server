04e6ddd4262977feb323663e6f3ef8940a8de87e
Rob Sanheim <rsanheim@gmail.com>
2021-05-19 08:02:08 -0500

    log after the create

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -39,19 +39,18 @@ class Communication < ApplicationRecord
   end
 
   def self.create_with_twilio_details!(appointment:, twilio_sid:, twilio_msg_status:, communication_type:, appointment_reminder: nil)
-    logger.info class: self.class.name, msg: "create_with_twilio_details", communication_type: communication_type,
-                appointment_id: appointment.id, result: twilio_msg_status
     transaction do
-      sms_delivery_details =
-        TwilioSmsDeliveryDetail.create!(session_id: twilio_sid,
-                                        result: twilio_msg_status,
-                                        callee_phone_number: appointment.patient.latest_mobile_number)
-      Communication.create!(communication_type: communication_type,
-                            detailable: sms_delivery_details,
-                            appointment: appointment,
-                            appointment_reminder: appointment_reminder,
-                            device_created_at: DateTime.current,
-                            device_updated_at: DateTime.current)
+      sms_delivery_details = TwilioSmsDeliveryDetail.create!(session_id: twilio_sid,
+                                                             result: twilio_msg_status,
+                                                             callee_phone_number: appointment.patient.latest_mobile_number)
+      communication = Communication.create!(communication_type: communication_type,
+                                            detailable: sms_delivery_details,
+                                            appointment: appointment,
+                                            appointment_reminder: appointment_reminder,
+                                            device_created_at: DateTime.current,
+                                            device_updated_at: DateTime.current)
+      logger.info class: self.class.name, msg: "create_with_twilio_details", communication: communication.id,
+                  communication_type: communication_type, appointment_id: appointment.id, result: twilio_msg_status
     end
   end
 
