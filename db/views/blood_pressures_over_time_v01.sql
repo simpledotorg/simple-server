dfcb64e3159561b5fa35dd36d10cdc8a61ae3bb5
Srihari Sriraman <srihari@nilenso.com>
2021-05-20 17:48:57 +0530

    Add function to calculate months difference between timestamps

--- db/views/blood_pressures_over_time_v01.sql
+++ db/views/blood_pressures_over_time_v01.sql
@@ -13,15 +13,8 @@ SELECT DISTINCT ON (bp.patient_id, cal.month_date)
     p.assigned_facility_id     AS patient_assigned_facility_id,
     p.registration_facility_id AS patient_registration_facility_id,
     bp.facility_id             AS blood_pressure_facility_id,
-
-    (DATE_PART('year', cal.month_date) - DATE_PART('year', p.recorded_at)) * 12 +
-    (DATE_PART('month', cal.month_date) - DATE_PART('month', p.recorded_at))
-    AS months_since_registration,
-
-    (DATE_PART('year', cal.month_date) - DATE_PART('year', bp.recorded_at)) * 12 +
-    (DATE_PART('month', cal.month_date) - DATE_PART('month', bp.recorded_at))
-    AS months_since_bp_observation
-
+    months_since(p.recorded_at, cal.month_date) AS months_since_registration,
+    months_since(bp.recorded_at, cal.month_date) AS months_since_bp_observation
 FROM blood_pressures bp
 -- Only fetch BPs that happened on or before the selected calendar month
 -- We use year and month comparisons to avoid timezone errors
