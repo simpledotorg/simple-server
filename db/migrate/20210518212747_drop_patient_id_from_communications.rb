dfee922951a133a0793e295ac99149d8d2d38fdf
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-05-20 20:52:16 +0530

    Set type of column correctly (#2537)

--- db/migrate/20210518212747_drop_patient_id_from_communications.rb
+++ db/migrate/20210518212747_drop_patient_id_from_communications.rb
@@ -1,5 +1,5 @@
 class DropPatientIdFromCommunications < ActiveRecord::Migration[5.2]
   def change
-    remove_column :communications, :patient_id, type: :uuid, foreign_key: true
+    remove_column :communications, :patient_id, :uuid, foreign_key: true
   end
 end
