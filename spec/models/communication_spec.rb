dbfe4bb0bf5ae6e0b5dc8dc8ad61d1494a25fc0f
Kris Pethtel <kris.pethtel@gmail.com>
2021-05-28 10:38:43 -0400

    backfill test for creating communication with twilio detail (#2550)
    * backfill test for creating communication with twilio detail

--- spec/models/communication_spec.rb
+++ spec/models/communication_spec.rb
@@ -70,6 +70,18 @@ describe Communication, type: :model do
         }.to change { Communication.count }.by(1)
           .and change { TwilioSmsDeliveryDetail.count }.by(1)
       end
+
+      it "does not require an appointment" do
+        notification = create(:notification)
+        expect {
+          Communication.create_with_twilio_details!(appointment: nil,
+                                                    twilio_sid: SecureRandom.uuid,
+                                                    twilio_msg_status: "sent",
+                                                    communication_type: :missed_visit_sms_reminder,
+                                                    notification: notification)
+        }.to change { Communication.count }.by(1)
+          .and change { TwilioSmsDeliveryDetail.count }.by(1)
+      end
     end
   end
 
