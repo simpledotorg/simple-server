823db61bb13bdd3a870854f391161ca450ab12c2
Rob Sanheim <rsanheim@gmail.com>
2021-06-02 13:50:45 -0500

    Add to repository; add tests; add to view

--- app/queries/bp_measures_query.rb
+++ app/queries/bp_measures_query.rb
@@ -1,9 +1,22 @@
 class BPMeasuresQuery
   def count(region, period_type, diagnosis: :hypertension, group_by: nil)
-    BloodPressure
+    query = BloodPressure
       .joins(:patient).merge(Patient.with_hypertension)
       .group_by_period(period_type, :recorded_at, {format: Period.formatter(period_type)})
       .where(facility: region.facilities)
-      .count
+    if group_by.present?
+      results = query.group(group_by).count
+      sum_groups_by_period(results)
+    else
+      query.count
+    end
+  end
+
+  def sum_groups_by_period(result)
+    result.each_with_object({}) { |(key, count), hsh|
+      period, field_id = *key
+      hsh[period] ||= {}
+      hsh[period][field_id] = count
+    }
   end
 end
--- spec/queries/bp_measures_query_spec.rb
+++ spec/queries/bp_measures_query_spec.rb
@@ -1,45 +1,38 @@
 require "rails_helper"
 
 RSpec.describe BPMeasuresQuery do
-  let(:user) { create(:user) }
+  let(:user_1) { create(:user) }
+  let(:user_2) { create(:user) }
 
-  it "works" do
+  it "returns counts of BPs per period" do
     facility = create(:facility)
     Timecop.freeze("May 5th 2021") do
-      create(:blood_pressure, facility: facility, recorded_at: 4.months.ago, user: user)
-      create(:blood_pressure, facility: facility, recorded_at: 2.months.ago, user: user)
+      create(:blood_pressure, facility: facility, recorded_at: 4.months.ago, user: user_1)
+      create(:blood_pressure, facility: facility, recorded_at: 2.months.ago, user: user_2)
       expected = {
         Period.month("January 2021") => 1,
         Period.month("February 2021") => 0,
-        Period.month("March 2021") => 1,
+        Period.month("March 2021") => 1
 
       }
       expect(described_class.new.count(facility, :month)).to eq(expected)
     end
   end
 
-  it "can be grouped further" do
-    [ june_1, june_2, june_3, july_1, aug_1, aug_2 ].each do |date|
-      patient = create(:patient, :hypertension, recorded_at: long_ago)
-      create(:blood_pressure, patient: patient, facility: facility_1, recorded_at: date)
-    end
+  it "can return counts of BPs per period per user" do
+    facility = create(:facility)
+    Timecop.freeze("May 5th 2021") do
+      patient = create(:patient)
+      create(:blood_pressure, facility: facility, recorded_at: 4.months.ago, user: user_1)
+      create(:blood_pressure, facility: facility, patient: patient, recorded_at: 2.months.ago, user: user_2)
+      create(:blood_pressure, facility: facility, patient: patient, recorded_at: 2.months.ago, user: user_2)
+      expected = {
+        Period.month("January 2021") => { user_1.id => 1, user_2.id => 0},
+        Period.month("February 2021") => { user_1.id => 0, user_2.id => 0},
+        Period.month("March 2021") => { user_1.id => 0, user_2.id => 2}
 
-    [ june_1, july_1, july_3, aug_1, aug_2 ].each do |date|
-      patient = create(:patient, :hypertension, recorded_at: long_ago)
-      create(:blood_pressure, patient: patient, facility: facility_2, recorded_at: date)
-    end
-
-    activity_service = ActivityService.new(facility_group, group: BloodPressure.arel_table[:facility_id])
-
-    Timecop.freeze(aug_3) do
-      expect(described_class.new.count(facility_group, group: :facility_id).to eq(
-        [june_1, facility_1.id] => 3,
-        [july_1, facility_1.id] => 1,
-        [aug_1, facility_1.id] => 2,
-        [june_1, facility_2.id] => 1,
-        [july_1, facility_2.id] => 2,
-        [aug_1, facility_2.id] => 2
-      )
+      }
+      expect(described_class.new.count(facility, :month, group_by: :user_id)).to eq(expected)
     end
   end
 end
