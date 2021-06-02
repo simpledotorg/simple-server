f7f77444c8d92fe034b8365470a64fae36199bc0
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-06-02 18:06:29 +0530

    Refinement of names and datapoints
    - rename reporting_calendar_months -> reporting_months
    - remove enum-heavy fields for diagnosed disease state and times since last bp/visit
    - introduce controlled, normalized fields to represent dimensions of care

--- db/views/reporting_patient_states_per_month_v01.sql
+++ db/views/reporting_patient_states_per_month_v01.sql
@@ -58,60 +58,44 @@ SELECT
     (cal.month - DATE_PART('month', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE'))))
     AS months_since_registration,
 
-    CASE
-        WHEN mh.hypertension = 'yes' AND (bps.systolic >= 180 OR bps.diastolic >= 110) THEN 'Stage 3'
-        WHEN mh.hypertension = 'yes' AND (bps.systolic >= 160 OR bps.diastolic >= 100) THEN 'Stage 2'
-        WHEN mh.hypertension = 'yes' AND (bps.systolic >= 140 OR bps.diastolic >= 90) THEN 'Stage 1'
-        WHEN mh.hypertension = 'yes' AND (bps.systolic < 140 AND bps.diastolic < 90) THEN 'Controlled'
-        WHEN mh.hypertension = 'yes' AND (bps.systolic IS null) THEN 'Hypertensive Unknown Stage'
-        WHEN mh.hypertension = 'unknown' THEN 'Unknown'
-        WHEN mh.hypertension = 'no' THEN 'Not hypertensive'
-        ELSE 'Undefined'
-        END
-        AS diagnosed_disease_state,
-
-    CASE
-        WHEN visits.months_since_visit < 3 THEN 'Less than 3 months'
-        WHEN visits.months_since_visit < 6 THEN 'Between 3 and 6 months'
-        WHEN visits.months_since_visit < 9 THEN 'Between 6 and 9 months'
-        WHEN visits.months_since_visit < 12 THEN 'Between 9 and 12 months'
-        WHEN visits.months_since_visit >= 12 THEN 'More than 12 months'
-        WHEN visits.months_since_visit IS null THEN 'No visit'
-        ELSE 'Undefined'
-        END
-        AS time_since_last_visit,
-
-    CASE
-        WHEN bps.months_since_bp_observation < 3 THEN 'Less than 3 months'
-        WHEN bps.months_since_bp_observation < 6 THEN 'Between 3 and 6 months'
-        WHEN bps.months_since_bp_observation < 9 THEN 'Between 6 and 9 months'
-        WHEN bps.months_since_bp_observation < 12 THEN 'Between 9 and 12 months'
-        WHEN bps.months_since_bp_observation >= 12 THEN 'More than 12 months'
-        WHEN bps.months_since_bp_observation IS null THEN 'No measurement'
-        ELSE 'Undefined'
-        END
-        AS time_since_last_bp,
+    visits.months_since_visit AS months_since_visit,
+    bps.months_since_bp_observation AS months_since_bp,
 
     ------------------------------------------------------------
-    -- lost to follow up
-    (
-      (cal.year - DATE_PART('year', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
-      (cal.month - DATE_PART('month', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) >= 12
+    -- categorization
+    CASE
+        WHEN (bps.systolic IS NULL OR bps.diastolic IS NULL) THEN 'unknown'
+        WHEN (bps.systolic < 140 AND bps.diastolic < 90) THEN 'controlled'
+        ELSE 'uncontrolled'
+        END
+        AS last_bp_state,
 
-      AND (bps.months_since_bp_observation IS NULL OR bps.months_since_bp_observation >= 12)
-      AND mh.hypertension = 'yes'
-      AND p.status <> 'dead'
-    ) AS lost_to_follow_up
+    CASE
+        WHEN p.status = 'dead' THEN 'died'
+        WHEN (
+          -- months_since_registration
+          (cal.year - DATE_PART('year', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
+          (cal.month - DATE_PART('month', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) < 12
+
+          OR
+
+          bps.months_since_bp_observation < 12
+        ) THEN 'under_care'
+        ELSE 'lost_to_follow_up'
+        END
+        AS care_state
 
 FROM patients p
--- Only fetch BPs that happened on or before the selected calendar month
--- We use year and month comparisons to avoid timezone errors
-LEFT OUTER JOIN reporting_calendar_months cal
+LEFT OUTER JOIN reporting_months cal
     ON to_char(p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= to_char(cal.month_date, 'YYYY-MM')
+
+-- Only fetch BPs and visits that happened on or before the selected calendar month
+-- We use year and month comparisons to avoid timezone errors
 LEFT OUTER JOIN reporting_patient_blood_pressures_per_month bps
     ON p.id = bps.patient_id AND cal.month = bps.month AND cal.year = bps.year
 LEFT OUTER JOIN reporting_patient_visits_per_month visits
     ON p.id = visits.patient_id AND cal.month = visits.month AND cal.year = visits.year
+
 LEFT OUTER JOIN medical_histories mh
     ON p.id = mh.patient_id
     AND mh.deleted_at IS NULL
