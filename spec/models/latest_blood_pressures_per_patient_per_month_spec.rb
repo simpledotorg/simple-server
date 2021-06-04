d8e1cda0ddd41323ea17ab3eb198eb028536c295
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-06-04 15:27:33 +0530

    Remove unused scopes

--- spec/models/latest_blood_pressures_per_patient_per_month_spec.rb
+++ spec/models/latest_blood_pressures_per_patient_per_month_spec.rb
@@ -62,44 +62,6 @@ RSpec.describe LatestBloodPressuresPerPatientPerMonth, type: :model do
     end
   end
 
-  describe "Scopes" do
-    describe ".ltfu_as_of" do
-      it "includes BP for patient who is LTFU" do
-        ltfu_patient = Timecop.freeze(2.years.ago) { create(:patient) }
-        Timecop.freeze(2.years.ago) { create(:blood_pressure, patient: ltfu_patient) }
-        refresh_views
-
-        expect(described_class.ltfu_as_of(Time.current).pluck(:patient_id)).to include(ltfu_patient.id)
-      end
-
-      it "excludes BP for patient who is not LTFU" do
-        not_ltfu_patient = Timecop.freeze(2.years.ago) { create(:patient) }
-        Timecop.freeze(6.months.ago) { create(:blood_pressure, patient: not_ltfu_patient) }
-        refresh_views
-
-        expect(described_class.ltfu_as_of(Time.current)).not_to include(not_ltfu_patient)
-      end
-    end
-
-    describe ".not_ltfu_as_of" do
-      it "excludes patient who is LTFU" do
-        ltfu_patient = Timecop.freeze(2.years.ago) { create(:patient) }
-        Timecop.freeze(2.years.ago) { create(:blood_pressure, patient: ltfu_patient) }
-        refresh_views
-
-        expect(described_class.not_ltfu_as_of(Time.current)).not_to include(ltfu_patient)
-      end
-
-      it "includes patient who is not LTFU" do
-        not_ltfu_patient = Timecop.freeze(2.years.ago) { create(:patient) }
-        Timecop.freeze(6.months.ago) { create(:blood_pressure, patient: not_ltfu_patient) }
-        refresh_views
-
-        expect(described_class.not_ltfu_as_of(Time.current).pluck(:patient_id)).to include(not_ltfu_patient.id)
-      end
-    end
-  end
-
   describe "Materialized view query" do
     let(:months) { [1, 2, 3].map { |n| n.months.ago } }
     let(:facilities) { create_list(:facility, 2) }
