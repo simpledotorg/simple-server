9c41255c206afb0c1a86459facbcbee8cac7257b
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-06-04 23:54:57 +0530

    Fix cache warming timezone (#2569)
    * Ensure region cache warm is always done in reporting timezone
    
    * Use Period constant
    
    * Rename Period constant to REPORTING_TIMEZONE

--- app/controllers/reports/regions_controller.rb
+++ app/controllers/reports/regions_controller.rb
@@ -217,7 +217,7 @@ class Reports::RegionsController < AdminController
   end
 
   def set_time_zone
-    time_zone = Period::ANALYTICS_TIME_ZONE
+    time_zone = Period::REPORTING_TIMEZONE
 
     Groupdate.time_zone = time_zone
 
