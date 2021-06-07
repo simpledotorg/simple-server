7cf7067fde7777225f49bd3e309466a358bdf849
Srihari Sriraman <srihari@nilenso.com>
2021-06-07 14:27:11 +0530

    Use lateral joins to improve performance of visits view

--- db/views/reporting_patient_visits_per_month_v01.sql
+++ db/views/reporting_patient_visits_per_month_v01.sql
@@ -1,46 +1,61 @@
 -- Only most recent Encounter per patient per month. Encounters are ordered appropriately below.
 SELECT
-    DISTINCT ON (p.id, cal.month_date)
+    DISTINCT ON (p.id, p.month_date)
     p.id as patient_id,
-    cal.month_date,
-    cal.month_string,
-    cal.month,
-    cal.quarter,
-    cal.year,
+    p.month_date,
+    p.month_string,
+    p.month,
+    p.quarter,
+    p.year,
     greatest(e.encountered_on, pd.device_created_at, app.device_created_at) AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')) AS visited_at,
     p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')) AS patient_registered_at,
     p.assigned_facility_id AS patient_assigned_facility_id,
     p.registration_facility_id AS patient_registration_facility_id,
     e.facility_id AS encounter_facility_id,
 
-    (cal.year - DATE_PART('year', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
-    (cal.month - DATE_PART('month', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE'))))
+    (p.year - DATE_PART('year', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
+    (p.month - DATE_PART('month', p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE'))))
     AS months_since_registration,
 
-    (cal.year - DATE_PART('year', greatest(e.encountered_on, pd.device_created_at, app.device_created_at) AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
-    (cal.month - DATE_PART('month', greatest(e.encountered_on, pd.device_created_at, app.device_created_at) AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE'))))
+    (p.year - DATE_PART('year', greatest(e.encountered_on, pd.device_created_at, app.device_created_at) AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')))) * 12 +
+    (p.month - DATE_PART('month', greatest(e.encountered_on, pd.device_created_at, app.device_created_at) AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE'))))
     AS months_since_visit
 
-FROM patients p
-LEFT OUTER JOIN reporting_months cal
-    ON to_char(p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= to_char(cal.month_date, 'YYYY-MM')
+FROM
+(
+    SELECT * FROM patients p
+    LEFT OUTER JOIN reporting_months cal
+    ON to_char(p.recorded_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= cal.month_string
+) p
 -- Only fetch Encounters that happened on or before the selected calendar month
 -- We use year and month comparisons to avoid timezone errors
-LEFT OUTER JOIN encounters e
-    ON e.patient_id = p.id
-    AND e.deleted_at IS NULL
-    AND to_char(e.encountered_on AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= to_char(cal.month_date, 'YYYY-MM')
-LEFT OUTER JOIN prescription_drugs pd
-    ON pd.patient_id = p.id
-    AND pd.deleted_at IS NULL
-    AND to_char(pd.device_created_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= to_char(cal.month_date, 'YYYY-MM')
-LEFT OUTER JOIN appointments app
-    ON app.patient_id = p.id
-    AND app.deleted_at IS NULL
-    AND to_char(app.device_created_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= to_char(cal.month_date, 'YYYY-MM')
+LEFT JOIN LATERAL (
+    SELECT encountered_on, facility_id FROM encounters
+    WHERE patient_id = p.id
+      AND to_char(encountered_on AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= p.month_string
+      AND deleted_at is null
+    ORDER BY encountered_on DESC
+    LIMIT 1
+) e ON true
+LEFT JOIN LATERAL (
+    SELECT device_created_at FROM prescription_drugs
+    WHERE patient_id = p.id
+      AND to_char(device_created_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= p.month_string
+      AND deleted_at is null
+    ORDER BY device_created_at DESC
+    LIMIT 1
+) pd ON true
+LEFT JOIN LATERAL (
+    SELECT device_created_at FROM appointments
+    WHERE patient_id = p.id
+      AND to_char(device_created_at AT TIME ZONE 'utc' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM') <= p.month_string
+      AND deleted_at is null
+    ORDER BY device_created_at DESC
+    LIMIT 1
+) app ON true
 -- Ensure most recent visit is fetched
 WHERE p.deleted_at IS NULL
 ORDER BY
     p.id,
-    cal.month_date,
+    p.month_date,
     visited_at DESC
