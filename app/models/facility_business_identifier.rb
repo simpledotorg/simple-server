44815fcfee8339a719a210bc66c4288cb775f044
Tim Cheadle <tcheadle@resolvetosavelives.org>
2021-06-08 10:23:29 -0400

    Fixing format

--- app/models/facility_business_identifier.rb
+++ app/models/facility_business_identifier.rb
@@ -2,7 +2,7 @@ class FacilityBusinessIdentifier < ApplicationRecord
   belongs_to :facility
 
   enum identifier_type: {
-    dhis2_org_unit_id: "dhis2_org_unit_id",
+    dhis2_org_unit_id: "dhis2_org_unit_id"
   }
 
   validates :identifier, presence: true
