55315ba9bed73ecdf9370b347b13f49e810b3405
kpethtel <kris.pethtel@gmail.com>
2021-06-17 13:43:12 -0400

    specs

--- spec/jobs/imo/invite_patient_spec.rb
+++ spec/jobs/imo/invite_patient_spec.rb
@@ -2,47 +2,63 @@ require "rails_helper"
 
 RSpec.describe Imo::InvitePatient, type: :job do
   describe "#perform" do
-    it "raises an error when the patient id is not found" do
-      expect {
-        described_class.perform_async("fake")
-        described_class.drain
-      }.to raise_error(ActiveRecord::RecordNotFound)
+    context "with feature flag turned off" do
+      it "does nothing" do
+        patient = create(:patient)
+        allow_any_instance_of(ImoApiService).to receive(:invite).and_return("invited")
+
+        expect {
+          described_class.perform_async(patient.id)
+          described_class.drain
+        }.not_to change { ImoAuthorization.count }
+      end
     end
 
-    it "creates an ImoAuthorization with 'invited' status when invitation is successful" do
-      patient = create(:patient)
-      allow_any_instance_of(ImoApiService).to receive(:invite).and_return("invited")
+    context "with feature flag turned on" do
+      before { Flipper.enable(:imo_messaging) }
 
-      expect {
-        described_class.perform_async(patient.id)
-        described_class.drain
-      }.to change { ImoAuthorization.count }.from(0).to(1)
-      imo_auth = ImoAuthorization.last
-      expect(imo_auth.status).to eq("invited")
-      expect(imo_auth.patient_id).to eq(patient.id)
-    end
+      it "raises an error when the patient id is not found" do
+        expect {
+          described_class.perform_async("fake")
+          described_class.drain
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
 
-    it "creates an ImoAuthorization with 'no_imo_account' status when invited user has no Imo account" do
-      patient = create(:patient)
-      allow_any_instance_of(ImoApiService).to receive(:invite).and_return("no_imo_account")
+      it "creates an ImoAuthorization with 'invited' status when invitation is successful" do
+        patient = create(:patient)
+        allow_any_instance_of(ImoApiService).to receive(:invite).and_return("invited")
 
-      expect {
-        described_class.perform_async(patient.id)
-        described_class.drain
-      }.to change { ImoAuthorization.count }.from(0).to(1)
-      imo_auth = ImoAuthorization.last
-      expect(imo_auth.status).to eq("no_imo_account")
-      expect(imo_auth.patient_id).to eq(patient.id)
-    end
+        expect {
+          described_class.perform_async(patient.id)
+          described_class.drain
+        }.to change { ImoAuthorization.count }.from(0).to(1)
+        imo_auth = ImoAuthorization.last
+        expect(imo_auth.status).to eq("invited")
+        expect(imo_auth.patient_id).to eq(patient.id)
+      end
 
-    it "does not create an ImoAuthorization when invitation fails" do
-      patient = create(:patient)
-      allow_any_instance_of(ImoApiService).to receive(:invite).and_return("failure")
+      it "creates an ImoAuthorization with 'no_imo_account' status when invited user has no Imo account" do
+        patient = create(:patient)
+        allow_any_instance_of(ImoApiService).to receive(:invite).and_return("no_imo_account")
 
-      expect {
-        described_class.perform_async(patient.id)
-        described_class.drain
-      }.not_to change { ImoAuthorization.count }
+        expect {
+          described_class.perform_async(patient.id)
+          described_class.drain
+        }.to change { ImoAuthorization.count }.from(0).to(1)
+        imo_auth = ImoAuthorization.last
+        expect(imo_auth.status).to eq("no_imo_account")
+        expect(imo_auth.patient_id).to eq(patient.id)
+      end
+
+      it "does not create an ImoAuthorization when invitation fails" do
+        patient = create(:patient)
+        allow_any_instance_of(ImoApiService).to receive(:invite).and_return("failure")
+
+        expect {
+          described_class.perform_async(patient.id)
+          described_class.drain
+        }.not_to change { ImoAuthorization.count }
+      end
     end
   end
 end
--- spec/jobs/imo/invite_unsubscribed_patients_spec.rb
+++ spec/jobs/imo/invite_unsubscribed_patients_spec.rb
@@ -4,50 +4,52 @@ RSpec.describe Imo::InviteUnsubscribedPatients, type: :job do
   let(:date) { "1-1-2020".to_date }
 
   describe "#perform" do
-    it "does nothing when feature flag is off" do
-      create(:patient)
-      expect(Imo::InvitePatient).not_to receive(:perform_at)
-      described_class.perform_async
-      described_class.drain
-    end
-
-    it "queues an Imo::InvitePatient job for patients who have no ImoAuthorization" do
-      patient = create(:patient)
-      Flipper.enable(:imo_messaging)
-      Timecop.freeze(date) do
-        expect(Imo::InvitePatient).to receive(:perform_at).with(date, patient.id)
+    context "with feature flag turned off" do
+      it "does nothing" do
+        create(:patient)
+        expect(Imo::InvitePatient).not_to receive(:perform_at)
         described_class.perform_async
         described_class.drain
       end
     end
 
-    it "queues an Imo::InvitePatient job for non-subscribed patients who were invited over 6 months ago" do
-      patient = create(:patient)
-      create(:imo_authorization, patient: patient, status: "invited", last_invited_at: date - 7.months)
-      Flipper.enable(:imo_messaging)
-      Timecop.freeze(date) do
-        expect(Imo::InvitePatient).to receive(:perform_at).with(date, patient.id)
+    context "with feature flag turned on" do
+      before { Flipper.enable(:imo_messaging) }
+
+      it "queues an Imo::InvitePatient job for patients who have no ImoAuthorization" do
+        patient = create(:patient)
+        Timecop.freeze(date) do
+          expect(Imo::InvitePatient).to receive(:perform_at).with(date, patient.id)
+          described_class.perform_async
+          described_class.drain
+        end
+      end
+
+      it "queues an Imo::InvitePatient job for non-subscribed patients who were invited over 6 months ago" do
+        patient = create(:patient)
+        create(:imo_authorization, patient: patient, status: "invited", last_invited_at: date - 7.months)
+        Timecop.freeze(date) do
+          expect(Imo::InvitePatient).to receive(:perform_at).with(date, patient.id)
+          described_class.perform_async
+          described_class.drain
+        end
+      end
+
+      it "does not queue an Imo::InvitePatient job for patients who were invited less than six months ago" do
+        patient = create(:patient)
+        create(:imo_authorization, patient: patient, status: "invited", last_invited_at: 1.day.ago)
+        expect(Imo::InvitePatient).not_to receive(:perform_at)
         described_class.perform_async
         described_class.drain
       end
-    end
 
-    it "does not queue an Imo::InvitePatient job for patients who were invited less than six months ago" do
-      patient = create(:patient)
-      create(:imo_authorization, patient: patient, status: "invited", last_invited_at: 1.day.ago)
-      Flipper.enable(:imo_messaging)
-      expect(Imo::InvitePatient).not_to receive(:perform_at)
-      described_class.perform_async
-      described_class.drain
-    end
-
-    it "does not queue an Imo::InvitePatient job for patients who have been successfully subscribed" do
-      patient = create(:patient)
-      create(:imo_authorization, patient: patient, status: "subscribed", last_invited_at: 1.year.ago)
-      Flipper.enable(:imo_messaging)
-      expect(Imo::InvitePatient).not_to receive(:perform_at)
-      described_class.perform_async
-      described_class.drain
+      it "does not queue an Imo::InvitePatient job for patients who have been successfully subscribed" do
+        patient = create(:patient)
+        create(:imo_authorization, patient: patient, status: "subscribed", last_invited_at: 1.year.ago)
+        expect(Imo::InvitePatient).not_to receive(:perform_at)
+        described_class.perform_async
+        described_class.drain
+      end
     end
   end
 end
