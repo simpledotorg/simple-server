0aa162839bb9178114782d03b01568ed9d135e2c
kpethtel <kris.pethtel@gmail.com>
2021-06-17 13:50:22 -0400

    fix spec

--- spec/models/imo_authorization_spec.rb
+++ spec/models/imo_authorization_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe ImoAuthorization, type: :model do
 
   describe "validations" do
     it { should validate_presence_of(:status) }
-    it { should validate_presence_of(:last_invitation_date) }
+    it { should validate_presence_of(:last_invited_at) }
   end
 end
