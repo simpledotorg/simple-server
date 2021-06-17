e80546c556edd810f21530380bd4bd633fb1a5c2
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-06-17 18:05:33 +0530

    Squash migrations

--- db/migrate/20210614090147_do_not_materialize_reporting_facilities.rb
+++ db/migrate/20210614090147_do_not_materialize_reporting_facilities.rb
@@ -10,6 +10,7 @@ class DoNotMaterializeReportingFacilities < ActiveRecord::Migration[5.2]
     create_view :reporting_facilities, materialized: false
     create_view :reporting_patient_blood_pressures_per_month, materialized: true
     create_view :reporting_patient_visits_per_month, materialized: true
+    update_view :reporting_patient_visits_per_month, version: 2, revert_to_version: 1, materialized: true
     create_view :reporting_patient_states_per_month, materialized: true
   end
 
