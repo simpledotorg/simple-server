6a9ad0bba09dee722aa52b0b482d27b2cbb86ad9
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-06-17 16:34:11 +0530

    Patient visit specs

--- spec/models/reporting/reporting_patient_visits_per_month_spec.rb
+++ spec/models/reporting/reporting_patient_visits_per_month_spec.rb
@@ -52,13 +52,30 @@ RSpec.describe Reporting::ReportingPatientVisitsPerMonth, {type: :model, reporti
       end
     end
 
-    it "uses the latest of the visit information" do
+    it "uses the latest visit information for a given month" do
+      patient = create(:patient, recorded_at: test_times[:long_ago])
+      bp = create(:blood_pressure, :with_encounter, patient: patient, recorded_at: test_times[:over_three_months_ago])
+      described_class.refresh
+      with_reporting_time_zones do
+        visit = described_class.find_by(patient_id: bp.patient_id, month_date: test_times[:now])
+        expect(visit.encounter_facility_id).to eq bp.facility_id
+        expect(visit.visited_at).to eq bp.recorded_at
+      end
+
+      blood_sugar = create(:blood_sugar, :with_encounter, patient: patient, recorded_at: test_times[:under_three_months_ago])
+      described_class.refresh
+      with_reporting_time_zones do
+        visit = described_class.find_by(patient_id: blood_sugar.patient_id, month_date: test_times[:now])
+        expect(visit.encounter_facility_id).to eq blood_sugar.facility_id
+        expect(visit.visited_at).to eq blood_sugar.recorded_at
+      end
+
+      prescription_drug = create(:prescription_drug, patient: patient, device_created_at: test_times[:now])
+      described_class.refresh
+      with_reporting_time_zones do
+        visit = described_class.find_by(patient_id: prescription_drug.patient_id, month_date: test_times[:now])
+        expect(visit.visited_at).to eq prescription_drug.device_created_at
+      end
     end
-  end
-
-  describe "visited_at" do
-  end
-
-  describe "months_since_visit" do
   end
 end
