b016e639bd51228085f91f0b480242f4a9b734d1
kpethtel <kris.pethtel@gmail.com>
2021-06-23 15:41:04 -0400

    pr feedback

--- db/migrate/20210620144826_add_purpose_to_notifications.rb
+++ db/migrate/20210620144826_add_purpose_to_notifications.rb
@@ -1,8 +1,11 @@
 class AddPurposeToNotifications < ActiveRecord::Migration[5.2]
   def up
     add_column :notifications, :purpose, :string
-    Notification.find_each do |notification|
+    Notification.where(subject: nil).find_each do |notification|
       notification.update_column(:purpose, "covid_medication_reminder")
+    end
+    Notification.where.not(subject: nil).find_each do |notification|
+      notification.update_column(:purpose, "missed_visit_reminder")
     end
     change_column_null :notifications, :purpose, false
   end
