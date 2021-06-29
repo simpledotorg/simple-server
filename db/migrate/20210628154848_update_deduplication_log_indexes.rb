f38f73ed8cc3130e24c60bb740c77bfb8c05bd11
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-06-29 13:43:38 +0530

    Correct order

--- db/migrate/20210628154848_update_deduplication_log_indexes.rb
+++ db/migrate/20210628154848_update_deduplication_log_indexes.rb
@@ -1,6 +1,6 @@
 class UpdateDeduplicationLogIndexes < ActiveRecord::Migration[5.2]
   def change
     remove_index :deduplication_logs, name: :index_deduplication_records_lookup
-    add_index :deduplication_logs, [:deleted_record_id, :deleted_at], name: :idx_deduplication_logs_lookup_deleted_at
+    add_index :deduplication_logs, [:deleted_at, :deleted_record_id], name: :idx_deduplication_logs_lookup_deleted_at
   end
 end
