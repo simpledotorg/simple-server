71fac4a9b9e6ca4f6992f068057b525686c03907
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-06-30 14:18:56 +0530

    Use Time objects everywhere to do tz aware end of month timestamp

--- app/controllers/concerns/set_for_end_of_month.rb
+++ app/controllers/concerns/set_for_end_of_month.rb
@@ -1,11 +1,11 @@
 module SetForEndOfMonth
   def set_for_end_of_month
     @for_end_of_month ||= if params[:for_end_of_month]
-      Date.strptime(params[:for_end_of_month], "%b-%Y").end_of_month
-    elsif (Date.current.end_of_month - Date.current).to_i < 8
-      Date.current.end_of_month
+      Time.zone.strptime(params[:for_end_of_month], "%b-%Y").end_of_month
+    elsif (Time.current.end_of_month - Time.current).to_i < 8
+      Time.current.end_of_month
     else
-      Date.current.prev_month.end_of_month
+      Time.current.prev_month.end_of_month
     end
   end
 end
