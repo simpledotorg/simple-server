78e7cab7914a5db637f644e6e71e300422a048d6
Rob Sanheim <rsanheim@gmail.com>
2021-07-01 16:52:48 -0500

    Try removing the extra tx layer

--- app/models/reporting_pipeline/matview.rb
+++ app/models/reporting_pipeline/matview.rb
@@ -1,10 +1,8 @@
 module ReportingPipeline
   class Matview < ActiveRecord::Base
     def self.refresh
-      ActiveRecord::Base.transaction do
-        ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{Period::REPORTING_TIME_ZONE}'")
-        Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
-      end
+      ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{Period::REPORTING_TIME_ZONE}'")
+      Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
     end
   end
 end
