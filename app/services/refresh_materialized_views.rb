c49c911a413c454744a9386fb1a453c7e01ba8df
Rob Sanheim <rsanheim@gmail.com>
2021-07-02 16:20:19 -0500

    ADd back this code from debugging

--- app/services/refresh_materialized_views.rb
+++ app/services/refresh_materialized_views.rb
@@ -74,14 +74,14 @@ class RefreshMaterializedViews
   end
 
   def refresh_v2
-    # ActiveRecord::Base.transaction do
-    ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{tz}'")
-    V2_MATVIEWS.each do |name|
-      benchmark_and_statsd(name) do
-        klass = name.constantize
-        klass.refresh
+    ActiveRecord::Base.transaction do
+      ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{tz}'")
+      V2_MATVIEWS.each do |name|
+        benchmark_and_statsd(name) do
+          klass = name.constantize
+          klass.refresh
+        end
       end
     end
-    # end
   end
 end
