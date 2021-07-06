c255b559e2cd722c8857fee9372511af81b92bfe
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-07-06 17:58:40 +0530

    Fix user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -369,6 +369,7 @@ RSpec.describe User, type: :model do
     it "is true if any district accessible by the user is enabled" do
       admin = create(:admin, :viewer_all)
       facility_group = create(:facility_group)
+      create(:facility, facility_group: facility_group)
       create(:access, user: admin, resource: facility_group)
       Flipper.enable(:drug_stocks, facility_group.region)
 
