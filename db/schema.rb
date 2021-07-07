300b28f3823ffb1f67ae588bde4f11cebb6988a5
Rob Sanheim <rsanheim@gmail.com>
2021-07-06 20:45:20 -0500

    Add indexes to monthly_patient_states

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_215522) do
+ActiveRecord::Schema.define(version: 2021_07_07_014011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1325,4 +1325,11 @@ ActiveRecord::Schema.define(version: 2021_07_06_215522) do
     WHERE (p.deleted_at IS NULL)
     ORDER BY p.id, cal.month_date;
   SQL
+  add_index "reporting_monthly_patient_states", ["assigned_block_region_id"], name: "patient_states_assigned_block"
+  add_index "reporting_monthly_patient_states", ["assigned_district_region_id"], name: "patient_states_assigned_district"
+  add_index "reporting_monthly_patient_states", ["assigned_facility_region_id"], name: "patient_states_assigned_facility"
+  add_index "reporting_monthly_patient_states", ["assigned_state_region_id"], name: "patient_states_assigned_state"
+  add_index "reporting_monthly_patient_states", ["hypertension", "htn_care_state", "htn_treatment_outcome_in_last_3_months"], name: "patient_states_care_state"
+  add_index "reporting_monthly_patient_states", ["patient_id", "month_date"], name: "patient_states_patient_id_month_date", unique: true
+
 end
