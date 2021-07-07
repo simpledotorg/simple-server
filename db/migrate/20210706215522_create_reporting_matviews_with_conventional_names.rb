74682e2ee44c90d8d38ffe31bdb5f02fb95533d2
Rob Sanheim <rsanheim@gmail.com>
2021-07-06 20:08:44 -0500

    Need materialized

--- db/migrate/20210706215522_create_reporting_matviews_with_conventional_names.rb
+++ db/migrate/20210706215522_create_reporting_matviews_with_conventional_names.rb
@@ -6,8 +6,8 @@ class CreateReportingMatviewsWithConventionalNames < ActiveRecord::Migration[5.2
     create_view :reporting_monthly_patient_visits, materialized: true
     create_view :reporting_monthly_patient_states, materialized: true
 
-    drop_view :reporting_patient_states_per_month, revert_to_version: 2
-    drop_view :reporting_patient_visits_per_month, revert_to_version: 2
-    drop_view :reporting_patient_blood_pressures_per_month, revert_to_version: 1
+    drop_view :reporting_patient_states_per_month, revert_to_version: 2, materialized: true
+    drop_view :reporting_patient_visits_per_month, revert_to_version: 2, materialized: true
+    drop_view :reporting_patient_blood_pressures_per_month, revert_to_version: 1, materialized: true
   end
 end
