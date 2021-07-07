0a8fb9d0caac1dd433cd32fe99c920fa1cf51242
Rob Sanheim <rsanheim@gmail.com>
2021-07-07 18:00:28 -0500

    Drop the views first

--- db/migrate/20210706215522_create_reporting_matviews_with_conventional_names.rb
+++ db/migrate/20210706215522_create_reporting_matviews_with_conventional_names.rb
@@ -2,12 +2,12 @@ class CreateReportingMatviewsWithConventionalNames < ActiveRecord::Migration[5.2
   def change
     execute "SET LOCAL TIME ZONE '#{Rails.application.config.country[:time_zone]}'"
 
-    create_view :reporting_patient_blood_pressures, materialized: true
-    create_view :reporting_patient_visits, materialized: true
-    create_view :reporting_patient_states, materialized: true
-
     drop_view :reporting_patient_states_per_month, revert_to_version: 2, materialized: true
     drop_view :reporting_patient_visits_per_month, revert_to_version: 2, materialized: true
     drop_view :reporting_patient_blood_pressures_per_month, revert_to_version: 1, materialized: true
+
+    create_view :reporting_patient_blood_pressures, materialized: true
+    create_view :reporting_patient_visits, materialized: true
+    create_view :reporting_patient_states, materialized: true
   end
 end
