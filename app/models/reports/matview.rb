340581eee4de7689ccefdc9b3f106e93c3279f5f
Rob Sanheim <rsanheim@gmail.com>
2021-07-08 11:46:33 -0500

    different tz format

--- app/models/reports/matview.rb
+++ app/models/reports/matview.rb
@@ -2,7 +2,9 @@ module Reports
   class Matview < ActiveRecord::Base
     def self.refresh
       ActiveRecord::Base.transaction do
-        ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{Period::REPORTING_TIME_ZONE}'")
+        tz = Rails.application.config.country[:time_zone]
+        ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{tz}'")
+        # ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{Period::REPORTING_TIME_ZONE}'")
         Scenic.database.refresh_materialized_view(table_name, concurrently: false, cascade: false)
       end
     end
