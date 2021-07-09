9e94abc1ab35d6cc89fbadfe504d9739c54ccca2
vkrm <mail@vkrm.is>
2021-07-09 12:31:08 +0530

    make region optional

--- app/controllers/my_facilities/drug_stocks_controller.rb
+++ app/controllers/my_facilities/drug_stocks_controller.rb
@@ -42,7 +42,10 @@ class MyFacilities::DrugStocksController < AdminController
   end
 
   def create
-    DrugStocksCreator.call(user: current_admin, facility: @facility, month: @for_end_of_month, drug_stocks_params: drug_stocks_params[:drug_stocks])
+    DrugStocksCreator.call(user: current_admin,
+                           month: @for_end_of_month,
+                           drug_stocks_params: drug_stocks_params[:drug_stocks],
+                           region: @facility.region)
     redirect_to redirect_url, notice: "Saved drug stocks"
   rescue ActiveRecord::RecordInvalid
     redirect_to redirect_url, alert: "Something went wrong, Drug Stocks were not saved."
