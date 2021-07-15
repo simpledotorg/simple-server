5e3a00a6f386281c074f1a6dff1d606173b237d0
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-07-15 16:14:04 +0530

    Commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_183848) do
+ActiveRecord::Schema.define(version: 2021_07_15_085219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -165,7 +165,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_183848) do
   end
 
   create_table "drug_stocks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "facility_id", null: false
+    t.uuid "facility_id"
     t.uuid "user_id", null: false
     t.uuid "protocol_drug_id", null: false
     t.integer "in_stock"
@@ -174,7 +174,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_183848) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.uuid "region_id"
+    t.uuid "region_id", null: false
     t.integer "redistributed"
     t.index ["facility_id"], name: "index_drug_stocks_on_facility_id"
     t.index ["protocol_drug_id"], name: "index_drug_stocks_on_protocol_drug_id"
