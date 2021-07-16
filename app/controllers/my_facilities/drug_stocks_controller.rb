1f80ca184aece3fb7a6da9444d12f5cf2b7b2b30
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-07-16 17:48:28 +0530

    Revert "Set region type in URL"
    This reverts commit a2bcaa32b0c6c00b10103251adee95c4f730a1d8.

--- app/controllers/my_facilities/drug_stocks_controller.rb
+++ app/controllers/my_facilities/drug_stocks_controller.rb
@@ -95,6 +95,7 @@ class MyFacilities::DrugStocksController < AdminController
   def drug_stocks_params
     params.permit(
       :for_end_of_month,
+      :region_type,
       drug_stocks:
         [:received,
           :in_stock,
