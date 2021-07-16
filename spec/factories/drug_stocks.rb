9ef0f4bdb275b23dbe0fc672908e37c711f9dfad
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-07-16 15:52:01 +0530

    Update drug stock factory

--- spec/factories/drug_stocks.rb
+++ spec/factories/drug_stocks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :drug_stock do
     id { SecureRandom.uuid }
     association :facility, strategy: :create
+    region { facility.region }
     association :user, strategy: :create
     association :protocol_drug, strategy: :create
     for_end_of_month { Date.today.end_of_month }
