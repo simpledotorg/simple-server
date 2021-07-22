2cf52f5a5c8e179462545c4088bc81fe5528cd6f
vkrm <mail@vkrm.is>
2021-07-22 13:51:34 +0530

    Check if index exists before creating it

--- db/migrate/20210721120744_add_facility_id_index_on_drug_stocks.rb
+++ db/migrate/20210721120744_add_facility_id_index_on_drug_stocks.rb
@@ -1,5 +1,5 @@
 class AddFacilityIdIndexOnDrugStocks < ActiveRecord::Migration[5.2]
   def change
-    add_index :drug_stocks, :facility_id
+    add_index :drug_stocks, :facility_id unless index_exists?(:drug_stocks, :facilities)
   end
 end
