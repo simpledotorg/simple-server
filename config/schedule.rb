2db347cd787b50335d504155c2dd958b28a1fbd3
kpethtel <kris.pethtel@gmail.com>
2021-07-26 23:52:37 -0400

    change to keywords

--- config/schedule.rb
+++ config/schedule.rb
@@ -45,7 +45,7 @@ end
 
 every :day, at: local("05:30 am"), roles: [:cron] do
   if CountryConfig.current_country?("India") && SimpleServer.env.production?
-    runner "ExperimentControlService.schedule_daily_stale_patient_notifications('Small Stale Patient July 2021', patients_per_day: 100)"
+    runner "ExperimentControlService.schedule_daily_stale_patient_notifications(name: 'Small Stale Patient July 2021', patients_per_day: 100)"
   end
 end
 
