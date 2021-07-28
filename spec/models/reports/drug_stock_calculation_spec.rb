31c64c73f628131daf7fdcf49e9d9faeb736e05b
vkrm <mail@vkrm.is>
2021-07-28 12:56:49 +0530

    Don't create unnecessary protocols and facilities

--- spec/models/reports/drug_stock_calculation_spec.rb
+++ spec/models/reports/drug_stock_calculation_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe Reports::DrugStockCalculation, type: :model do
   let(:user) { create(:admin) }
   let(:drug_category) { "hypertension_ccb" }
   let(:drug_stocks) {
-    [build(:drug_stock, in_stock: 10000, received: 5000, redistributed: 1000, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329528")),
-      build(:drug_stock, in_stock: 20000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329526")),
-      build(:drug_stock, in_stock: 10000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316764")),
-      build(:drug_stock, in_stock: 20000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316765")),
-      build(:drug_stock, in_stock: 10000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "979467")),
-      build(:drug_stock, in_stock: 10000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316049")),
-      build(:drug_stock, in_stock: 10000, redistributed: 0, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "331132"))]
+    [build(:drug_stock, in_stock: 10000, received: 5000, redistributed: 1000, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329528")),
+      build(:drug_stock, in_stock: 20000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329526")),
+      build(:drug_stock, in_stock: 10000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316764")),
+      build(:drug_stock, in_stock: 20000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316765")),
+      build(:drug_stock, in_stock: 10000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "979467")),
+      build(:drug_stock, in_stock: 10000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "316049")),
+      build(:drug_stock, in_stock: 10000, redistributed: 0, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "331132"))]
   }
   let(:previous_month_drug_stocks) {
-    [build(:drug_stock, in_stock: 10000, facility_id: facility.id, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329528"))]
+    [build(:drug_stock, in_stock: 10000, facility: facility, user: user, protocol_drug: protocol_drugs.find_by(rxnorm_code: "329528"))]
   }
 
   let(:punjab_drug_stock_config) {
@@ -45,11 +45,11 @@ RSpec.describe Reports::DrugStockCalculation, type: :model do
       ).stocks_on_hand
 
       expect(result).to match_array [
-        {protocol_drug: protocol.protocol_drugs.find_by(rxnorm_code: "329528"),
+        {protocol_drug: protocol_drugs.find_by(rxnorm_code: "329528"),
          in_stock: 10000,
          coefficient: 1.2,
          stock_on_hand: 12000},
-        {protocol_drug: protocol.protocol_drugs.find_by(rxnorm_code: "329526"),
+        {protocol_drug: protocol_drugs.find_by(rxnorm_code: "329526"),
          in_stock: 20000,
          coefficient: 2,
          stock_on_hand: 40000}
