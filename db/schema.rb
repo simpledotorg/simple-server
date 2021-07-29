2cdafe038e90f7c584bd30831f0dcabaa06ae48a
Srihari Sriraman <srihari@nilenso.com>
2021-07-29 19:54:08 +0530

    Unit tests for facility states (#2753)
    * Add specs for assigned patient counts by care state
    
    * Add specs for treatment outcomes under care and not under care
    
    * Extract common utility to freeze time in reporting tests
    
    * Add a specific test to illustrate cumulative assigned count including dead patients
    
    * rename helper function to be more explicit
    
    * Add a spec to ensure that deleted data does not appear in visits or patient states
    
    * Assert on not including any non htn patients in the report
    
    * Add variable number of patients to be more thorough in tests
    
    * Include a patient with no visits in the outcomes spec
    
    * Add more variability in numbers to reduce false positives
    
    * Fix tests
    
    * Exclude dead patients from cumulative assignment count

--- db/schema.rb
+++ db/schema.rb
@@ -1464,7 +1464,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_100307) do
               count(DISTINCT reporting_patient_states.patient_id) FILTER (WHERE (reporting_patient_states.htn_care_state = 'under_care'::text)) AS under_care,
               count(DISTINCT reporting_patient_states.patient_id) FILTER (WHERE (reporting_patient_states.htn_care_state = 'lost_to_follow_up'::text)) AS lost_to_follow_up,
               count(DISTINCT reporting_patient_states.patient_id) FILTER (WHERE (reporting_patient_states.htn_care_state = 'dead'::text)) AS dead,
-              count(*) AS cumulative_assigned_patients
+              count(DISTINCT reporting_patient_states.patient_id) FILTER (WHERE (reporting_patient_states.htn_care_state <> 'dead'::text)) AS cumulative_assigned_patients
              FROM reporting_patient_states
             WHERE (reporting_patient_states.hypertension = 'yes'::text)
             GROUP BY reporting_patient_states.assigned_facility_region_id, reporting_patient_states.month_date
