fee1d7b2613f8b08bcba741cba18ba5a44c71441
Rob Sanheim <rsanheim@gmail.com>
2021-07-30 16:53:53 -0500

    psuedocode for how to calc this

--- script/stale_patient_notifications_per_day.rb
+++ script/stale_patient_notifications_per_day.rb
@@ -6,5 +6,7 @@ WHATSAPP_LIMIT_PER_DAY = 10_000
 # to ensure we stay under the WhatsApp rate limit
 STALE_EXPERIMENT_BUFFER = 0.5
 
+
+active_notifications_scheduled_per_day = Notification.group(:schedule_date).count
 active_notifications_sent_via_whats_app_per_day = active_notifications_scheduled_per_day * .50
 (WHATSAPP_LIMIT_PER_DAY - active_notifications_sent_via_whats_app_per_day) * STALE_EXPERIMENT_BUFFER
