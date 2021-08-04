4ad9836780f753ca19ca4ca03acfd7262ddb5e27
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-08-04 12:56:24 +0530

    Guard clause

--- db/data/20210804072005_rename_dhaka_north_to_dhaka.rb
+++ db/data/20210804072005_rename_dhaka_north_to_dhaka.rb
@@ -1,5 +1,7 @@
 class RenameDhakaNorthToDhaka < ActiveRecord::Migration[5.2]
   def up
+    return unless CountryConfig.current_country?("Bangladesh")
+
     Region.state_regions.find_by(name: "Dhaka North").update!(name: "Dhaka")
     Facility.where(state: "Dhaka North").update_all(state: "Dhaka")
   end
