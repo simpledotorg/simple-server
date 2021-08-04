9fef9bd8228d0a6b4907873d88ad3870baeb7ab2
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2021-08-04 21:24:33 +0530

    Fix BD state data migration (#2814)

--- db/data/20210804072005_rename_dhaka_north_to_dhaka.rb
+++ db/data/20210804072005_rename_dhaka_north_to_dhaka.rb
@@ -1,6 +1,6 @@
 class RenameDhakaNorthToDhaka < ActiveRecord::Migration[5.2]
   def up
-    return unless CountryConfig.current_country?("Bangladesh")
+    return unless CountryConfig.current_country?("Bangladesh") && SimpleServer.env.production?
 
     Region.state_regions.find_by(name: "Dhaka North").update!(name: "Dhaka")
     Facility.where(state: "Dhaka North").update_all(state: "Dhaka")
