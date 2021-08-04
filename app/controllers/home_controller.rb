e262e6d7642635023a9115aaa0ed78ff8fb5f93a
Claudio Vallejo <hola@claudiovallejo.mx>
2021-08-04 09:25:09 -0400

    Added webpack feature flag to HomeController index route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
-class HomeController < ApplicationController
+# frozen_string_literal: true
+
+class HomeController < AdminController
   layout "application_v2"
 
+  skip_after_action :verify_authorization_attempted, only: [:index]
+
   def index
+    unless current_admin.feature_enabled?(:webpack)
+      redirect_to my_facilities_overview_path(request.query_parameters)
+    end
   end
 end
