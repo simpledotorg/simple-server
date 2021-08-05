5e3c5dc2842d3e2fd827d79c014d73bfffa1545d
Claudio Vallejo <hola@claudiovallejo.mx>
2021-08-05 15:15:57 -0400

    Updated home/index rspec

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Homes", type: :request do
+  include Devise::Test::IntegrationHelpers
   describe "GET /index" do
     it "returns http success" do
+      user = create(:admin, :power_user)
+      password = user.password
+      Flipper.enable(:dashboard_ui_refresh, user)
+
+      post email_authentication_session_path, params: {email_authentication: {email: user.email, password: password}}
+      follow_redirect!
+
       get "/home/index"
       expect(response).to have_http_status(:success)
     end
