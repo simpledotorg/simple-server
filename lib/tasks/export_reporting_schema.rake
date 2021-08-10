4671c05b628f20741a242c3a51b65be62ae0decd
Srihari Sriraman <srihari@nilenso.com>
2021-08-10 11:15:08 +0530

    Format fixes

--- lib/tasks/export_reporting_schema.rake
+++ lib/tasks/export_reporting_schema.rake
@@ -1,5 +1,5 @@
 desc "Export reporting table schemas in custom formats for documentation"
-task :export_reporting_schema => :environment do |_t, args|
+task export_reporting_schema: :environment do |_t, args|
   require "tasks/scripts/export_reporting_schema"
 
   begin
--- spec/models/reports/matview_spec.rb
+++ spec/models/reports/matview_spec.rb
@@ -1,17 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Reports::Matview, {type: :model, reporting_spec: true} do
-
   def column_descriptions_sql(table_name)
-    <<SQL
-SELECT a.attname, pg_catalog.col_description(a.attrelid, a.attnum)
-FROM pg_catalog.pg_attribute a
-JOIN pg_catalog.pg_class c ON c.oid = a.attrelid
-WHERE c.relname = '#{table_name}'
-  AND pg_catalog.pg_table_is_visible(c.oid)
-  AND a.attnum > 0
-  AND NOT a.attisdropped;
-SQL
+    <<~SQL
+      SELECT a.attname, pg_catalog.col_description(a.attrelid, a.attnum)
+      FROM pg_catalog.pg_attribute a
+      JOIN pg_catalog.pg_class c ON c.oid = a.attrelid
+      WHERE c.relname = '#{table_name}'
+        AND pg_catalog.pg_table_is_visible(c.oid)
+        AND a.attnum > 0
+        AND NOT a.attisdropped;
+    SQL
   end
 
   def column_descriptions(table_name)
