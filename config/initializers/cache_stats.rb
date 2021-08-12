9d82359d24b902b7fedada65bb9a1d505514540b
Rob Sanheim <rsanheim@gmail.com>
2021-08-12 18:13:14 -0500

    Wrap in a ENV var for this logging

--- config/initializers/cache_stats.rb
+++ config/initializers/cache_stats.rb
@@ -1,6 +1,9 @@
-# ActiveSupport::Notifications.subscribe(/cache_.*\.active_support/) do |name, start, finish, arg1, arg2|
-#   Rails.logger.debug name: name, start: start, finish: finish, arg1: arg1, arg2: arg2
-# end
+# This logging can get really verbose, but can be useful when debugging or fine-tuning caching in dev.
+if ENV["CACHE_LOGGING"]
+  ActiveSupport::Notifications.subscribe(/cache_.*\.active_support/) do |name, start, finish, arg1, arg2|
+    Rails.logger.debug name: name, start: start, finish: finish, arg1: arg1, arg2: arg2
+  end
+end
 
 ActiveSupport::Notifications.subscribe(/cache_read\.active_support/) do |name, start, finish, _id, payload|
   RequestStore[:cache_stats] ||= {reads: 0, hits: 0, misses: 0, missed_keys: []}
