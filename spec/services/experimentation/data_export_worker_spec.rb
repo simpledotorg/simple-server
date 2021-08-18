1c809d8d60583eae813b06c7fc861868b4bc823f
kpethtel <kris.pethtel@gmail.com>
2021-08-18 17:23:31 -0400

    reworking specs

--- spec/services/experimentation/data_export_worker_spec.rb
+++ spec/services/experimentation/data_export_worker_spec.rb
@@ -2,17 +2,33 @@ require "rails_helper"
 require_relative "./experiment_data_examples.rb"
 
 RSpec.describe Experimentation::DataExportWorker, type: :model do
-  describe "#as_csv" do
+  describe "#perform" do
     include_context "active experiment data"
 
     it "exports accurate data in the expected format" do
-      described_class.perform_async(@experiment.name, "person@example.com")
+      mail_double = double
+      recipient_email_address = "person@example.com"
+      email_params = {
+        to: recipient_email_address,
+        subject: "Experiment data export: #{@experiment.name}",
+        content_type: "multipart/mixed",
+        body: "Please see attached CSV."
+      }
+      attachment_data = {
+        mime_type: "text/csv",
+        content: "blah"
+      }
+
+      email_double = double("email")
+      allow(email_double).to receive(:attachments).and_return({})
+      mailer = instance_double(ApplicationMailer, mail: email_double)
+      allow(ApplicationMailer).to receive(:new).and_return(mailer)
+
+      expect(email_double).to receive(:attachments)
+      expect(email_double).to receive(:deliver)
+
+      described_class.perform_async(@experiment.name, recipient_email_address)
       described_class.drain
-
-      pp parsed
-
-      # csv_data = subject.mail_csv
-      # csv_data.attachments.to_json
     end
   end
 end
