a05f96cfee1a799e4d5c4d4918e43459b33bb770
Claudio Vallejo <hola@claudiovallejo.mx>
2021-08-18 16:46:07 -0400

    Created mobile top navigation bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,13 @@
 class HomeController < AdminController
   layout "application_v2"
 
+  before_action :set_organization, only: [:edit, :update, :destroy]
   skip_after_action :verify_authorization_attempted, only: [:index]
 
   def index
+    authorize { current_admin.accessible_organizations(:manage).any? }
+    @organization = current_admin.accessible_organizations(:manage).first
+    
     unless current_admin.feature_enabled?(:dashboard_ui_refresh)
       redirect_to my_facilities_overview_path(request.query_parameters)
     end
