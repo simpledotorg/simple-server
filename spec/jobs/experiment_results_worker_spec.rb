aaa0dd8b87a47c9e00d0bc2fe21f0a5a8cf98fd0
kpethtel <kris.pethtel@gmail.com>
2021-08-19 23:14:12 -0400

    fix spec

--- spec/jobs/experiment_results_worker_spec.rb
+++ spec/jobs/experiment_results_worker_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe ExperimentResultsExportWorker, type: :model do
     it "sends the email successfully" do
       experiment = create(:experiment)
       csv_double = double("csv")
+      mail_double = double("mailer")
       export_double = instance_double(Experimentation::Export, as_csv: csv_double)
       expect(Experimentation::Export).to receive(:new).with(experiment).and_return(export_double)
-      expect(ExperimentResultsMailer).to receive(:new).with(csv_double, experiment.name, "person@example.com")
-      expect_any_instance_of(ExperimentResultsMailer).to receive(:deliver_csv)
+      expect(ExperimentResultsMailer).to receive(:new).with(csv_double, experiment.name, "person@example.com").and_return(mail_double)
+      expect(mail_double).to receive(:deliver_csv)
 
       described_class.perform_async(experiment.name, "person@example.com")
       described_class.drain
