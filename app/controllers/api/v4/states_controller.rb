59b701bb87be561fc47a6b72d3c3d9d40bc5975b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-08-23 14:11:51 +0530

    Pick state names from Region

--- app/controllers/api/v4/states_controller.rb
+++ app/controllers/api/v4/states_controller.rb
@@ -2,6 +2,11 @@ class Api::V4::StatesController < APIController
   include Api::V3::PublicApi
 
   def sync_to_user
-    render json: Facility.pluck(:state).uniq
+    state_names = Region.facility_regions
+      .joins("inner join regions states ON states.path @> regions.path and states.region_type = 'state'")
+      .distinct("states.name")
+      .pluck("states.name")
+
+    render json: state_names
   end
 end
