f582759035cc9b857a25bfd450cd3ee865c1afc7
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-08-24 14:43:54 +0530

    lets not

--- spec/api/v4/states_spec.rb
+++ spec/api/v4/states_spec.rb
@@ -6,7 +6,7 @@ describe "states v4 API", swagger_doc: "v4/swagger.json" do
       tags "States"
 
       response "200", "returns state names" do
-        let!(:request_facility) { create(:facility) }
+        let(:request_facility) { create(:facility) }
 
         schema Api::V4::Schema.states_response
         run_test!
