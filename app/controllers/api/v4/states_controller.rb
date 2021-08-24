27cdb118e08f9b5b630ad1205aa54946c09fd3f2
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2021-08-24 15:38:30 +0530

    States list API (#2866)

---
+++ app/controllers/api/v4/states_controller.rb
@@ -0,0 +1,16 @@
+class Api::V4::StatesController < APIController
+  skip_before_action :current_user_present?, only: [:index]
+  skip_before_action :validate_sync_approval_status_allowed, only: [:index]
+  skip_before_action :authenticate, only: [:index]
+  skip_before_action :validate_facility, only: [:index]
+  skip_before_action :validate_current_facility_belongs_to_users_facility_group, only: [:index]
+
+  def index
+    state_names = Region.facility_regions
+      .joins("inner join regions states ON states.path @> regions.path and states.region_type = 'state'")
+      .distinct("states.name")
+      .pluck("states.name")
+
+    render json: state_names
+  end
+end
