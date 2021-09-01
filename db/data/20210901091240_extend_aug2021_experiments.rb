9f29a573cfbe402c9aedec9c9e866feec476081b
vkrm <mail@vkrm.is>
2021-09-01 14:53:07 +0530

    Add data migration to extend experiments

---
+++ db/data/20210901091240_extend_aug2021_experiments.rb
@@ -0,0 +1,15 @@
+class ExtendAug2021Experiments < ActiveRecord::Migration[5.2]
+  def up
+    return unless CountryConfig.current_country?("India") && SimpleServer.env.production?
+
+    experiments = Experimentation::Experiment.where(name: ["Stale Patient August 2021", "Current Patient August 2021"])
+    end_date = Date.new(2021, 09, 19)
+    experiments.each do |experiment|
+      experiment.update!(end_date: end_date)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
