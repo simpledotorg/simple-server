a949c3998dc9b09409fd1da6bedf5b6898f4fed9
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-09-06 22:46:27 +0530

    Specs

--- app/models/reports/patient_follow_up.rb
+++ app/models/reports/patient_follow_up.rb
@@ -2,7 +2,7 @@ module Reports
   class PatientFollowUp < Reports::View
     self.table_name = "reporting_patient_follow_ups"
     belongs_to :patient
-    belongs_to :facility
+    belongs_to :facility, class_name: "::Facility"
     belongs_to :user
 
     def self.materialized?
--- spec/models/reports/patient_follow_up_spec.rb
+++ spec/models/reports/patient_follow_up_spec.rb
@@ -30,4 +30,163 @@ RSpec.describe Reports::PatientFollowUp, {type: :model, reporting_spec: true} do
     follow_up = described_class.find_by(patient: patient, user: user, facility: facility)
     expect(follow_up.month_string).to eq(june_2021[:month_string])
   end
+
+  it "contains separate records for distinct months" do
+    patient = create(:patient, recorded_at: june_2021[:long_ago])
+    user = create(:user)
+    facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:over_3_months_ago]
+    )
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:under_3_months_ago]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(2)
+    follow_ups = described_class.where(patient: patient, user: user, facility: facility)
+    expect(follow_ups.map(&:month_string).sort).to eq(["2021-03", "2021-04"])
+  end
+
+  it "contains separate records for distinct users" do
+    patient = create(:patient, recorded_at: june_2021[:long_ago])
+    user = create(:user)
+    another_user = create(:user)
+    facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:now]
+    )
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: another_user,
+      facility: facility,
+      recorded_at: june_2021[:now]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(2)
+    follow_ups = described_class.where(patient: patient, month_string: june_2021[:month_string], facility: facility)
+    expect(follow_ups.map(&:user)).to contain_exactly(user, another_user)
+  end
+
+  it "contains separate records for distinct facilities" do
+    patient = create(:patient, recorded_at: june_2021[:long_ago])
+    user = create(:user)
+    facility = create(:facility)
+    another_facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:now]
+    )
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: another_facility,
+      recorded_at: june_2021[:now]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(2)
+    follow_ups = described_class.where(patient: patient, month_string: june_2021[:month_string], user: user)
+    expect(follow_ups.map(&:facility)).to contain_exactly(facility, another_facility)
+  end
+
+  it "contains separate records for distinct patients" do
+    patient = create(:patient, recorded_at: june_2021[:long_ago])
+    another_patient = create(:patient, recorded_at: june_2021[:long_ago])
+    user = create(:user)
+    facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:now]
+    )
+
+    create(
+      :blood_pressure,
+      patient: another_patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:now]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(2)
+    follow_ups = described_class.where(month_string: june_2021[:month_string], facility: facility, user: user)
+    expect(follow_ups.map(&:patient)).to contain_exactly(patient, another_patient)
+  end
+
+  it "does not count activity in the registration month" do
+    patient = create(:patient, recorded_at: june_2021[:beginning_of_month])
+    user = create(:user)
+    facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:end_of_month]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(0)
+  end
+
+  it "does not count more than one record per month for the same patient, facility, and user" do
+    patient = create(:patient, recorded_at: june_2021[:long_ago])
+    user = create(:user)
+    facility = create(:facility)
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:beginning_of_month]
+    )
+
+    create(
+      :blood_pressure,
+      patient: patient,
+      user: user,
+      facility: facility,
+      recorded_at: june_2021[:end_of_month]
+    )
+
+    RefreshReportingViews.call
+
+    expect(described_class.count).to eq(1)
+  end
 end
