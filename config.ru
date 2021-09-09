bff644bddd3dff792fe6c10e26d9afae0bb73d6f
Rob Sanheim <rsanheim@gmail.com>
2021-09-09 17:25:49 -0500

    Enable DataDog code profiling in sandbox (#2905)
    * Enable DataDog code profiling in sandbox
    
    This is in public beta, so they recommend starting in a non prod
    enviornment to start
    
    * Turn on profiling for review apps also
    
    * require protobuf everywhere; change order in config.ru
    
    * No reason to turn it on in review
    
    we dont have datadog running in review envs

--- config.ru
+++ config.ru
@@ -2,6 +2,10 @@
 
 require_relative "config/environment"
 
+if ENABLE_DD_PROFILING
+  require "ddtrace/profiling/preload"
+end
+
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -6,8 +6,12 @@ require "datadog/statsd"
 # in dev, test, or on Heroku - so we don't want to send payloads there.
 SEND_DATA_TO_DD_AGENT = !(Rails.env.development? || Rails.env.test? || SimpleServer.env.review?)
 
+# Trying out Ruby code profiling in selected environments
+ENABLE_DD_PROFILING = SimpleServer.env.sandbox?
+
 Datadog.configure do |c|
   c.tracer.enabled = SEND_DATA_TO_DD_AGENT
+  c.profiling.enabled = ENABLE_DD_PROFILING
   c.version = SimpleServer.git_ref(short: true)
   c.use :rails, analytics_enabled: true
   c.use :rack, headers: {request: %w[X-USER-ID X-FACILITY-ID X-SYNC-REGION-ID], response: %w[Content-Type X-Request-ID]}
