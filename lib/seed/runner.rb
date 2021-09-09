0096a4a0cffd021f0abdd3eda0d1fe334af86099
Hari Mohanraj <hari.mohanraj89@gmail.com>
2021-09-09 19:16:18 +0530

    Fixed OTPs also please

--- lib/seed/runner.rb
+++ lib/seed/runner.rb
@@ -57,7 +57,8 @@ module Seed
       [
         :drug_stocks,
         :notifications,
-        (:auto_approve_users if SimpleServer.env.android_review?)
+        (:auto_approve_users if SimpleServer.env.android_review?),
+        (:fixed_otp if SimpleServer.env.android_review?)
       ].compact
     end
 
