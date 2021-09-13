bde5e7705924b2e3eeaf730adca6477fcb60c586
Rob Sanheim <rsanheim@gmail.com>
2021-09-13 16:10:02 -0500

    Simplify this bit of config

--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -4,13 +4,13 @@ require "datadog/statsd"
 
 # Allow running via an ENV var (for development usage, for example) ...otherwise
 # exclude some envs by default
-SEND_DATA_TO_DD_AGENT = ENV["DATADOG_ENABLED"] || !(Rails.env.development? || Rails.env.test? || SimpleServer.env.review?)
+DATADOG_ENABLED = ENV["DATADOG_ENABLED"] || !(Rails.env.development? || Rails.env.test? || SimpleServer.env.review?)
 
 # Trying out Ruby code profiling in selected environments
 ENABLE_DD_PROFILING = SimpleServer.env.sandbox?
 
 Datadog.configure do |c|
-  c.tracer.enabled = SEND_DATA_TO_DD_AGENT
+  c.tracer.enabled = DATADOG_ENABLED
   c.profiling.enabled = ENABLE_DD_PROFILING
   c.version = SimpleServer.git_ref(short: true)
   c.use :rails, analytics_enabled: true
