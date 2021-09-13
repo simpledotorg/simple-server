327c7587725f097075438f3bfa2c9f85a45ac2a3
kpethtel <kris.pethtel@gmail.com>
2021-09-13 18:01:27 -0400

    wip

--- app/models/imo_delivery_detail.rb
+++ app/models/imo_delivery_detail.rb
@@ -2,19 +2,6 @@ class ImoDeliveryDetail < ApplicationRecord
   has_one :communication, as: :detailable
 
   enum result: {
-
+    
   }
-
-  # this might be better as a service object
-  class << self
-    def create(response)
-      parse_notification_response()
-      new()
-    end
-
-    protected
-
-    def parse_notification_response(response)
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -84,6 +84,7 @@ Rails.application.routes.draw do
       resource :twilio_sms_delivery, only: [:create], controller: :twilio_sms_delivery
 
       post "patient/:patient_id/imo_authorization", to: "imo_callbacks#subscribe", as: :imo_authorization_callback
+      post "imo_delivery_detail/:imo_delivery_detail_id/read_receipt", to: "imo_callbacks#read_receipt", as: :imo_notification_callback
 
       scope :users do
         get "find", to: "users#find"
