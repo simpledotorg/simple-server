0c91f9f453e539b52641edc8129c5b182537d145
Rob Sanheim <rsanheim@gmail.com>
2021-09-16 13:35:41 -0500

    Destroy Region when a Facility is destroyed

--- app/models/region_source.rb
+++ app/models/region_source.rb
@@ -1,6 +1,6 @@
 module RegionSource
   def self.extended(klass)
-    klass.has_one :region, inverse_of: :source, foreign_key: "source_id", autosave: true
+    klass.has_one :region, inverse_of: :source, foreign_key: "source_id", autosave: true, dependent: :destroy
     klass.after_discard do
       region&.discard
     end
