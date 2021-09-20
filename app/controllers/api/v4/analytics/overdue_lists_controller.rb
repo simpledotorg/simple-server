4dad33e45144ac22fb7f55391d64ff7dceb73974
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-09-20 20:45:37 +0530

    Standard

--- app/controllers/api/v4/analytics/overdue_lists_controller.rb
+++ app/controllers/api/v4/analytics/overdue_lists_controller.rb
@@ -2,10 +2,10 @@ class Api::V4::Analytics::OverdueListsController < Api::V4::AnalyticsController
   def show
     respond_to do |format|
       format.csv do
-        @patient_summaries = PatientSummaryQuery.call(
-          assigned_facilities: [current_facility],
-          only_overdue: false
-        ).includes(:latest_bp_passport, :current_prescription_drugs).order(risk_level: :desc, next_appointment_scheduled_date: :desc, id: :asc)
+        @patient_summaries =
+          PatientSummaryQuery.call(assigned_facilities: [current_facility], only_overdue: false)
+            .includes(:latest_bp_passport, :current_prescription_drugs)
+            .order(risk_level: :desc, next_appointment_scheduled_date: :desc, id: :asc)
 
         send_data render_to_string("show.csv.erb"), filename: download_filename
       end
