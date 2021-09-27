59c70e4ba6b9931d16907aa6cb96395147edf533
Rob Sanheim <rsanheim@gmail.com>
2021-09-27 13:18:15 -0500

    Destroy related regions only when they have no children

--- app/models/region_source.rb
+++ app/models/region_source.rb
@@ -1,8 +1,12 @@
 module RegionSource
   def self.extended(klass)
-    klass.has_one :region, inverse_of: :source, foreign_key: "source_id", autosave: true, dependent: :destroy
+    klass.has_one :region, inverse_of: :source, foreign_key: "source_id", autosave: true
     klass.after_discard do
       region&.discard
     end
+    klass.define_method :clean_up_empty_regions do
+      region.destroy! if region.children.none?
+    end
+    klass.before_destroy :clean_up_empty_regions
   end
 end
