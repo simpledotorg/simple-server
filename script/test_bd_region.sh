a8a471317880df55fa39b013e25bec895ee6f9d8
Rob Sanheim <rsanheim@gmail.com>
2021-09-27 14:25:22 -0500

    tweak script

--- script/test_bd_region.sh
+++ script/test_bd_region.sh
@@ -2,11 +2,12 @@
 set -e
 
 export DEFAULT_COUNTRY="BD"
+export REFRESH_MATVIEWS_CONCURRENTLY=0
+
 bin/rails db:drop
 bin/rails db:create
 psql simple-server_development < $1
 bin/rails db:environment:set RAILS_ENV=development
-export REFRESH_MATVIEWS_CONCURRENTLY=0
 bin/rails db:refresh_matviews
 bin/rails db:migrate
 script/run_data_script update_bangladesh_regions_script
