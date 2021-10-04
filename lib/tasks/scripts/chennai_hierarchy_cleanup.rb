956607ac0d376cf13ca09197066a271f79eee4c6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-10-04 14:30:15 +0530

    Refactor and add specs

--- lib/tasks/scripts/chennai_hierarchy_cleanup.rb
+++ lib/tasks/scripts/chennai_hierarchy_cleanup.rb
@@ -1,23 +1,47 @@
 class ChennaiHierarchyCleanup
-  def self.run
+  def self.call
     raise "Cannot run this outside India production" unless CountryConfig.current[:name] == "India" && SimpleServer.env.production?
 
-    state = Region.state_regions.find_by_name("Tamil Nadu")
-    new_district = Region.find_by_name("Chennai")
-    raise "Create Chennai district before running" if new_district.blank?
+    new.call
+  end
 
-    old_districts = state.district_regions.where.not(name: new_district.name)
+  def initialize
+    @state = Region.state_regions.find_by_name("Tamil Nadu")
+    @new_district = Region.find_by_name("Chennai")
+    raise "Create Chennai district from dashboard before running" if @new_district.blank?
 
+    @old_districts = @state.district_regions.where.not(name: @new_district.name)
+  end
+
+  def call
     ActiveRecord::Base.transaction do
-      state.block_regions.each do |block|
-        block.reparent_to = new_district
-        block.save!
-      end
-
-      state.facilities.update_all(facility_group_id: new_district.source.id)
-
-      # make sure facility gropus are empty before deleting
-      FacilityGroup.where(id: old_districts.pluck(:source_id)).discard_all
+      reparent_blocks
+      set_fg_id_on_facilities
+      copy_accesses
+      discard_old_fgs
     end
+  end
+
+  def reparent_blocks
+    @state.block_regions.each do |block|
+      block.reparent_to = @new_district
+      block.save!
+    end
+  end
+
+  def set_fg_id_on_facilities
+    @state.facilities.update_all(facility_group_id: @new_district.source.id)
+  end
+
+  def copy_accesses
+    Access.where(resource_id: @old_districts).each do |access|
+      Access.create!(access.slice(:resource_type, :user_id).merge(resource_id: @new_district.id))
+      access.discard
+    end
+  end
+
+  def discard_old_fgs
+    # TODO: make sure facility groups are empty before deleting
+    FacilityGroup.where(id: @old_districts.pluck(:source_id)).discard_all
   end
 end
--- spec/tasks/scripts/chennai_hierarchy_cleanup_spec.rb
+++ spec/tasks/scripts/chennai_hierarchy_cleanup_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 require "tasks/scripts/chennai_hierarchy_cleanup"
 
-describe "ChennaiHierarchyCleanup" do
+RSpec.describe ChennaiHierarchyCleanup do
   it "doesn't run outside india prod" do
-    expect { ChennaiHierarchyCleanup.run }.to raise_error "Cannot run this outside India production"
+    expect { described_class.call }.to raise_error "Cannot run this outside India production"
   end
 
   it "doesn't run if Chennai district hasn't been created on dashboard" do
     stub_const("SIMPLE_SERVER_ENV", "production")
 
-    expect { ChennaiHierarchyCleanup.run }.to raise_error "Create Chennai district before running"
+    expect { described_class.call }.to raise_error "Create Chennai district from dashboard before running"
   end
 
   it "reparents old blocks to the new district" do
@@ -19,7 +19,7 @@ describe "ChennaiHierarchyCleanup" do
     old_district = create(:facility_group, name: "Chennai old", state: "Tamil Nadu")
     block = create(:region, :block, name: "Chennai old - block", reparent_to: old_district.region)
 
-    ChennaiHierarchyCleanup.run
+    described_class.call
 
     expect(block.reload.district_region).to eq(new_district.region)
     expect(old_district.reload.deleted_at).to be_present
@@ -27,8 +27,25 @@ describe "ChennaiHierarchyCleanup" do
   end
 
   it "sync region ID of any facility remains unchanged" do
+    stub_const("SIMPLE_SERVER_ENV", "production")
+
+    new_district = create(:facility_group, name: "Chennai", state: "Tamil Nadu")
+    old_district = create(:facility_group, name: "Chennai old", state: "Tamil Nadu")
+    block = create(:region, :block, name: "Chennai old - block", reparent_to: old_district.region)
+    facility = create(:facility, block: block.name, facility_group: old_district)
+
+    described_class.call
+
+    facility.reload
+
+    expect(facility.region.block_region.id).to eq(block.id)
+    expect(facility.region.district_region).to eq(new_district.region)
   end
 
   it "only one district remains in Tamil Nadu after the cleanup" do
+  end
+
+  it "copies access from old facility groups to new facility group" do
+
   end
 end
