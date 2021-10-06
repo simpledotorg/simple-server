1383097fb90665ad50bf328eb874b3ade1e12045
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-10-06 19:53:36 +0530

    Add spec

--- lib/tasks/scripts/wardha_patient_cleanup.rb
+++ lib/tasks/scripts/wardha_patient_cleanup.rb
@@ -1,17 +1,20 @@
 class WardhaPatientCleanup
   def self.discard
-    patient_ids = CSV.read("lib/data/wardha_discard_patients.csv")
+    patient_ids = CSV.read("lib/data/wardha_discard_patients.csv").map(&:first)
 
-    Patient.where(id: patient_ids).discard_all
+    ActiveRecord::Base.transaction do
+      Patient.where(id: patient_ids).map(&:discard_data)
+    end
   end
 
   def self.deduplicate
     rows = CSV.read("lib/data/wardha_deduplicate_patients.csv")
 
     rows.map do |patient_ids|
+      patient_ids = patient_ids.map(&:strip)
       next if Patient.where(id: patient_ids).count < 2
 
-      PatientDeduplication::Runner.new(patient_ids)
+      PatientDeduplication::Runner.new(patient_ids).perform
     end
   end
 end
