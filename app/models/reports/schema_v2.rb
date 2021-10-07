dd977029ce90904e83182695fedcf42de020b6a5
Rob Sanheim <rsanheim@gmail.com>
2021-10-07 12:30:44 -0500

    Extract RegionCaching
    its used across both schemas

--- app/models/reports/schema_v2.rb
+++ app/models/reports/schema_v2.rb
@@ -2,6 +2,7 @@ module Reports
   class SchemaV2
     include BustCache
     include Memery
+    include RegionCaching
 
     FIELDS = %i[
       adjusted_controlled_under_care
@@ -33,10 +34,9 @@ module Reports
 
     delegate :cache, :logger, to: Rails
     delegate :cache_version, to: self
-    CACHE_MINOR_VERSION = 1
 
     def self.cache_version
-      "2.#{CACHE_MINOR_VERSION}"
+      "2.0"
     end
 
     def initialize(regions, periods:)
@@ -177,35 +177,6 @@ module Reports
       else
         adjusted_patients_without_ltfu[region.slug][period]
       end
-    end
-
-    # Generate all necessary cache keys for a calculation, then yield to the block for every entry.
-    # Once all results are returned via fetch_multi, return the data in a standard format of:
-    #   {
-    #     region_1_slug: { period_1: value, period_2: value }
-    #     region_2_slug: { period_1: value, period_2: value }
-    #   }
-    #
-    def region_period_cached_query(calculation, **options, &block)
-      results = regions.each_with_object({}) { |region, hsh| hsh[region.slug] = Hash.new(0) }
-      items = cache_entries(calculation, **options)
-      cached_results = cache.fetch_multi(*items, force: bust_cache?) { |entry| block.call(entry) }
-      cached_results.each { |(entry, count)| results[entry.region.slug][entry.period] = count }
-      results
-    end
-
-    # Generate all necessary region period cache entries, only going back to the earliest
-    # patient registration date for Periods. This ensures that we don't create many cache entries
-    # with 0 data for newer Regions.
-    def cache_entries(calculation, **options)
-      combinations = regions.each_with_object([]) do |region, results|
-        earliest_period = earliest_patient_recorded_at_period[region.slug]
-        next if earliest_period.nil?
-        periods_with_data = periods.select { |period| period >= earliest_period }
-        results.concat(periods_with_data.to_a.map { |period| [region, period] })
-      end
-      options[:version] = cache_version
-      combinations.map { |region, period| Reports::RegionPeriodEntry.new(region, period, calculation, options) }
     end
 
     def percentage(numerator, denominator)
