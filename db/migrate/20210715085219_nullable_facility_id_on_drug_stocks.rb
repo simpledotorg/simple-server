2c077d1a9986fe864a1c47b79c113ecd08b7659a
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2021-10-11 14:11:57 +0530

    Fix experiment constraint migration (#2974)

--- db/migrate/20210715085219_nullable_facility_id_on_drug_stocks.rb
+++ db/migrate/20210715085219_nullable_facility_id_on_drug_stocks.rb
@@ -1,8 +1,5 @@
 class NullableFacilityIdOnDrugStocks < ActiveRecord::Migration[5.2]
   def change
-    Experimentation::Experiment.where(start_date: nil).update_all(start_date: "2021-05-01")
-    Experimentation::Experiment.where(end_date: nil).update_all(end_date: "2021-06-01")
-
     change_column_null :drug_stocks, :facility_id, true
     change_column_null :drug_stocks, :region_id, false
   end
