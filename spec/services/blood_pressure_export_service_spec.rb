3506c7d6bc8bb60b61e0ff800cc1989974d5d3d3
cpena <carloshpena93@gmail.com>
2021-10-18 22:57:21 -0400

    medium controlled tests

--- spec/services/blood_pressure_export_service_spec.rb
+++ spec/services/blood_pressure_export_service_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe BloodPressureExportService, type: :model do
   end
 
   describe "#call" do #more for a thing
+    puts "hello"
     context "when data_type is bp_controlled" do # more for data setup
       it "processes results for small facilities" do
         small_facility1 = create(:facility, name: "small_1", facility_group: facility_group, facility_size: "small")
@@ -38,6 +39,7 @@ RSpec.describe BloodPressureExportService, type: :model do
           create(:blood_pressure, :under_control, patient: patient, facility: patient.assigned_facility,
                                                   recorded_at: may - 4.months, user: user)
           logger.info "--- end"
+
         end
         create(:blood_pressure, :hypertensive, patient: small_uncontrolled, facility: small_uncontrolled.assigned_facility,
                                                recorded_at: may - 4.months, user: user)
@@ -101,17 +103,86 @@ RSpec.describe BloodPressureExportService, type: :model do
       end
 
       it "processes results for medium sized facilities" do
-        # medium_facility1 = create(:facility, name: "medium_1", facility_size: "medium", facility_group: facility_group)
-        # medium_facility2 = create(:facility, name: "medium_2", facility_size: "medium", facility_group: facility_group)
-        # medium_controlled = create(:patient, full_name: "medium_controlled", registration_facility: medium_facility1,
-        #                                      recorded_at: may - 4.months, registration_user: user)
-        # medium_uncontrolled = create(:patient, full_name: "medium_uncontrolled", registration_facility: medium_facility2,
-        #                                        recorded_at: may - 4.months, registration_user: user)
+        medium_facility1 = create(:facility, name: "medium_1", facility_size: "medium", facility_group: facility_group)
+        medium_facility2 = create(:facility, name: "medium_2", facility_size: "medium", facility_group: facility_group)
+        medium_controlled = create_list(:patient, 2, full_name: "medium_controlled", registration_facility: medium_facility1,
+                                              recorded_at: may - 4.months, registration_user: user)
+        medium_uncontrolled = create(:patient, full_name: "medium_uncontrolled", registration_facility: medium_facility2,
+                                               recorded_at: may - 4.months, registration_user: user)
         # create(:blood_pressure, :under_control, patient: medium_controlled, user: user,
         #                                         facility: medium_controlled.assigned_facility, recorded_at: may - 3.months)
         # create(:blood_pressure, :hypertensive, patient: medium_uncontrolled, user: user,
         #                                        facility: medium_controlled.assigned_facility, recorded_at: may - 3.months)
 
+        medium_controlled.each do |patient|
+          logger.info "--- start"
+          create(:blood_pressure, :under_control, patient: patient, facility: patient.assigned_facility,
+                                                  recorded_at: may - 4.months, user: user)
+          logger.info "--- end"
+        end
+
+        create(:blood_pressure, :hypertensive, patient: medium_uncontrolled, facility: medium_uncontrolled.assigned_facility,
+                                               recorded_at: may - 4.months, user: user)
+
+        facilities = [medium_facility1, medium_facility2]
+
+        service = described_class.new(data_type: "controlled_patients", start_period: start_period, end_period: end_period, facilities: facilities)
+        result = service.call
+
+
+        expected_results ={"medium"=>
+          {"aggregate"=>
+            {"Facilities"=>"All CHCs",
+            "Total assigned"=>"3",
+            "Total registered"=>"3",
+            "Six month change"=>"0%",
+            period_range[0]=>"0%",
+            "Dec-2020-ratio"=>"0 / 0",
+            period_range[1]=>"0%",
+            "Jan-2021-ratio"=>"0 / 0",
+            period_range[2]=>"0%",
+            "Feb-2021-ratio"=>"0 / 0",
+            period_range[3]=>"0%",
+            "Mar-2021-ratio"=>"0 / 0",
+            period_range[4]=>"0%",
+            "Apr-2021-ratio"=>"0 / 3",
+            period_range[5]=>"0%",
+            "May-2021-ratio"=>"0 / 3"},
+          "facilities"=>
+            [{"Facilities"=>"Medium_1",
+              "Total assigned"=>"2",
+              "Total registered"=>"2",
+              "Six month change"=>"0%",
+              period_range[0]=>"0%",
+              "Dec-2020-ratio"=>"0 / 0",
+              period_range[1]=>"0%",
+              "Jan-2021-ratio"=>"0 / 0",
+              period_range[2]=>"0%",
+              "Feb-2021-ratio"=>"0 / 0",
+              period_range[3]=>"0%",
+              "Mar-2021-ratio"=>"0 / 0",
+              period_range[4]=>"0%",
+              "Apr-2021-ratio"=>"0 / 2",
+              period_range[5]=>"0%",
+              "May-2021-ratio"=>"0 / 2"},
+            {"Facilities"=>"Medium_2",
+              "Total assigned"=>"1",
+              "Total registered"=>"1",
+              "Six month change"=>"0%",
+              period_range[0]=>"0%",
+              "Dec-2020-ratio"=>"0 / 0",
+              period_range[1]=>"0%",
+              "Jan-2021-ratio"=>"0 / 0",
+              period_range[2]=>"0%",
+              "Feb-2021-ratio"=>"0 / 0",
+              period_range[3]=>"0%",
+              "Mar-2021-ratio"=>"0 / 0",
+              period_range[4]=>"0%",
+              "Apr-2021-ratio"=>"0 / 1",
+              period_range[5]=>"0%",
+              "May-2021-ratio"=>"0 / 1"}]}}
+
+        expect(result).to eq(expected_results)
 
       end
 
@@ -133,8 +204,7 @@ RSpec.describe BloodPressureExportService, type: :model do
 
       it "processes community sized facilities"
 
-      it "omits sizes that are not represented in the facilities data" do
-      end
+      it "omits sizes that are not represented in the facilities data"
 
       it "processes bp controlled"
 
@@ -147,4 +217,5 @@ RSpec.describe BloodPressureExportService, type: :model do
   describe "#as_csv" do
     it "returns a csv object with expected data"
   end
+
 end
