85553aa62b76b691b3d790c00cfd3e6c7c8797d9
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-10-22 18:48:50 +0530

    Stale Patient Experiment takes a date

--- app/models/experimentation/stale_patient_experiment.rb
+++ app/models/experimentation/stale_patient_experiment.rb
@@ -3,9 +3,7 @@ module Experimentation
     default_scope { where(experiment_type: %w[stale_patients]) }
 
     def self.candidate_patients(date)
-      # TODO: change this to accept a time,
-      # it is used to determine if patients have an appointment in the "future"
-      StalePatientSelection.call(start_time: date)
+      StalePatientSelection.call(date: date)
     end
   end
 end
--- app/services/experimentation/stale_patient_selection.rb
+++ app/services/experimentation/stale_patient_selection.rb
@@ -1,21 +1,17 @@
 module Experimentation
   class StalePatientSelection
-    ELIGIBILITY_START = 365.days.freeze
-    ELIGIBILITY_END = 35.days.freeze
+    PATIENT_VISITED_SINCE = -365.days.freeze
+    PATIENT_VISITED_UNTIL = -35.days.freeze
 
     def self.call(*args)
       new(*args).call
     end
 
-    attr_reader :start_time
-    attr_reader :eligible_range
-
-    def initialize(start_time:)
-      @start_time = start_time
-      range_start = (start_time - ELIGIBILITY_START).beginning_of_day
-      range_end = (start_time - ELIGIBILITY_END).end_of_day
-      @eligible_range = (range_start..range_end)
+    def initialize(date:)
+      @date = date
     end
+
+    attr_reader :date
 
     def call
       candidate_ids = NotificationsExperiment.candidate_patients.pluck(:id)
@@ -24,9 +20,9 @@ module Experimentation
       parameters = {
         candidate_ids: candidate_ids,
         hypertension: "yes",
-        range_start: eligible_range.begin,
-        range_end: eligible_range.end,
-        start_time: start_time
+        last_visit_since: (date + PATIENT_VISITED_SINCE).beginning_of_day,
+        last_visit_until: (date + PATIENT_VISITED_UNTIL).end_of_day,
+        no_appointments_after: date.end_of_day
       }
       sql = GitHub::SQL.new(<<~SQL, parameters)
         SELECT DISTINCT patients.id
@@ -34,13 +30,13 @@ module Experimentation
         INNER JOIN medical_histories mh
          ON patients.id = mh.patient_id
         LEFT JOIN appointments on appointments.patient_id = patients.id
-          AND appointments.device_created_at BETWEEN :range_start AND :range_end
+          AND appointments.device_created_at BETWEEN :last_visit_since AND :last_visit_until
         LEFT JOIN prescription_drugs on prescription_drugs.patient_id = patients.id
-          AND prescription_drugs.device_created_at BETWEEN :range_start AND :range_end
+          AND prescription_drugs.device_created_at BETWEEN :last_visit_since AND :last_visit_until
         LEFT JOIN blood_sugars on blood_sugars.patient_id = patients.id
-          AND blood_sugars.device_created_at BETWEEN :range_start AND :range_end
+          AND blood_sugars.device_created_at BETWEEN :last_visit_since AND :last_visit_until
         LEFT JOIN blood_pressures on blood_pressures.patient_id = patients.id
-          AND blood_pressures.device_created_at BETWEEN :range_start AND :range_end
+          AND blood_pressures.device_created_at BETWEEN :last_visit_since AND :last_visit_until
         WHERE patients.deleted_at IS NULL
           AND mh.hypertension = :hypertension
           AND (appointments.id IS NOT NULL
@@ -50,7 +46,7 @@ module Experimentation
           AND patients.id IN :candidate_ids
           AND NOT EXISTS
             (SELECT 1 FROM appointments WHERE appointments.patient_id = patients.id
-               AND appointments.scheduled_date >= :start_time)
+               AND appointments.scheduled_date >= :no_appointments_after)
       SQL
       sql.values
     end
