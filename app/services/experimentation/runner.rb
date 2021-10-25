394359d375c207a9804c3d185c88cfd33913e4c9
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-10-25 21:05:52 +0530

    Undo runner changes

--- app/services/experimentation/runner.rb
+++ app/services/experimentation/runner.rb
@@ -8,9 +8,20 @@ module Experimentation
     end
 
     def self.start_current_patient_experiment(name:, percentage_of_patients: 100)
+      unless Flipper.enabled?(:experiment)
+        logger.info("Experiment feature flag is off. Experiment #{name} will not be started.")
+        return
+      end
+
       experiment = Experiment.current_patients.upcoming.find_by(name: name)
+      if experiment.nil?
+        logger.info("Experiment #{name} not found and may need to be removed from scheduler - exiting.")
+        return
+      end
 
       eligible_ids = current_patient_candidates(experiment.start_time, experiment.end_time).shuffle!
+      logger.info("Found #{eligible_ids.count} eligible patient ids for #{name} experiment, about to schedule_notifications.")
+
       experiment_patient_count = (0.01 * percentage_of_patients * eligible_ids.length).round
       eligible_ids = eligible_ids.pop(experiment_patient_count)
 
@@ -34,17 +45,27 @@ module Experimentation
     end
 
     def self.schedule_daily_stale_patient_notifications(name:, patients_per_day: PATIENTS_PER_DAY)
-      experiment = Experiment.stale_patients.running.find_by(name: name)
+      unless Flipper.enabled?(:experiment)
+        logger.info("Experiment feature flag is off. No patients will be added to experiment #{name}.")
+        return
+      end
 
-      today = Date.today
-      eligible_ids = StalePatientSelection.call(date: today)
+      experiment = Experiment.stale_patients.running.find_by(name: name)
+      if experiment.nil?
+        logger.info("Experiment #{name} not found and may need to be removed from scheduler - exiting.")
+        return
+      end
+
+      now = Time.current
+      eligible_ids = StalePatientSelection.call(start_time: now)
+      logger.info "Experiment #{name} found #{eligible_ids.count} eligible patient ids for stale patient reminders"
       if eligible_ids.any?
         eligible_ids.shuffle!
         daily_ids = eligible_ids.pop(patients_per_day)
         daily_patients = Patient.where(id: daily_ids).includes(:appointments)
         daily_patients.each do |patient|
           group = experiment.random_treatment_group
-          schedule_notifications(patient, patient.appointments.last, group, today)
+          schedule_notifications(patient, patient.appointments.last, group, now)
           group.enroll(patient)
         end
       end
@@ -52,8 +73,23 @@ module Experimentation
       logger.info("Finished scheduling notifications for #{name} stale experiment - marking as running and exiting.")
     end
 
+    def self.abort_experiment(name)
+      experiment = Experiment.find_by!(name: name)
+      logger.info "Aborting experiment #{name}! About to cancel all pending or scheduled notifications."
+
+      ActiveRecord::Base.transaction do
+        notifications = experiment.notifications.where(status: ["pending", "scheduled"])
+        notifications.find_each do |notification|
+          notification.status_cancelled!
+        end
+
+        experiment.discard
+      end
+      logger.info "Aborting experiment #{name} finished."
+    end
+
     def self.current_patient_candidates(start_time, end_time)
-      NotificationsExperiment.eligible_patients
+      Experiment.candidate_patients
         .joins(:appointments)
         .merge(Appointment.status_scheduled)
         .where("appointments.scheduled_date BETWEEN ? and ?", start_time, end_time)
