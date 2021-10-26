0a3c8a5741b6635ee291a26e52793c7cc9603e20
Rob Sanheim <rsanheim@gmail.com>
2021-10-25 23:46:00 -0500

    Enable reporting schema v2 across dashboard (#3010)
    * Add request spec for some high level spec coverage
    
    * Extract to a repository presenter
    
    * upgrade Guard
    
    * move default_period to Reports module
    
    * Fix spec
    
    * Refactor my facilities to use presenter
    
    also replace a bunch of strings with symbols as I go
    
    * fix spec for symbols
    
    * Keep the facility_size a string
    
    * Fix specs and simplify a bit
    
    * Remove facility performance feature
    
    This was never enabled in any prod environments, and I believe it was
    put on the back-burner because of many other higher priority epics
    getting prioritized. The code has fallen out of date w/ all the recent
    reporting pipeline changes, so we can remove it to avoid unnecessary
    maintenance.
    
    We can always add it back from this commit and
    bring it up to date w/ Repository code if we get it re-prioritized and
    into development.
    
    * Less let! usage
    
    * More cleanup:
    
    Use symbols for accessing keys; fix the spec so it actually calls all
    the actions;
    
    * Remove unused data from this hash
    
    * Remove more unused data
    
    my facilities doesn't have the option to show rates with LTFU
    
    * Move reporting schema toggle to AdminController
    
    We want this feature flag to apply to any reports, including my
    facilities, so we can now move this to apply to the entire dashboard.
    
    * Use params
    
    * Pass thru v2 flag
    
    * Ensure we default to 0 for missing keys
    
    * confirm default behavior with its own spec
    
    * clean up
    
    * Simplify reporting schema v2 setting
    
    * better error message
    
    * Fix spec:
    
    the middleware handles the resetting here, which means we can verify
    RequestStore after the fact in this controller spec
    
    * cleanup

--- spec/services/reports/region_service_spec.rb
+++ spec/services/reports/region_service_spec.rb
@@ -20,14 +20,6 @@ RSpec.describe Reports::RegionService, type: :model do
     RefreshReportingViews.call
   end
 
-  context ".default_period" do
-    it "is the current month in the reporting time zone" do
-      Timecop.freeze("June 1st 2020 1:00 AM IST") do
-        expect(described_class.default_period).to eq(Period.month("June 30 2020"))
-      end
-    end
-  end
-
   it "sets the period" do
     period = Period.month(june_1_2020)
     service = Reports::RegionService.new(region: facility_group_1, period: period)
