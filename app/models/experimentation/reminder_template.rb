f9a84cc9b096d8911345c0c9983f9857073934e2
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-10-28 19:27:04 +0530

    Add validation on reminder template

--- app/models/experimentation/reminder_template.rb
+++ app/models/experimentation/reminder_template.rb
@@ -5,5 +5,13 @@ module Experimentation
 
     validates :message, presence: true
     validates :remind_on_in_days, presence: true, numericality: {only_integer: true}
+
+    validate :unique_message_per_group
+
+    def unique_message_per_group
+      if self.class.find_by(message: message, treatment_group: treatment_group)
+        errors.add(:message, "already exists in this treatment group")
+      end
+    end
   end
 end
--- spec/models/experimentation/reminder_template_spec.rb
+++ spec/models/experimentation/reminder_template_spec.rb
@@ -10,5 +10,18 @@ RSpec.describe Experimentation::ReminderTemplate, type: :model do
     it { should validate_presence_of(:message) }
     it { should validate_presence_of(:remind_on_in_days) }
     it { should validate_numericality_of(:remind_on_in_days) }
+
+    describe "#unique_message_per_group" do
+      it "allows only one template with a message in a group" do
+        treatment_group = create(:treatment_group)
+        message = "Hello please visit the clinic."
+        create(:reminder_template, treatment_group: treatment_group, message: message)
+
+        new_reminder_template = build(:reminder_template, treatment_group: treatment_group, message: message)
+        new_reminder_template.validate
+
+        expect(new_reminder_template.errors[:message]).to be_present
+      end
+    end
   end
 end
