326744ce384f9b2b78f6c7e086454aad0e091a4a
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-11-02 18:37:01 +0530

    Make spec faster

--- spec/models/experimentation/notifications_experiment_spec.rb
+++ spec/models/experimentation/notifications_experiment_spec.rb
@@ -239,17 +239,22 @@ RSpec.describe Experimentation::NotificationsExperiment, type: :model do
 
   describe "#record_notification_statuses" do
     it "records a result for all notifications that were recorded 'pending'" do
+      patient = create(:patient)
       experiment = described_class.find(create(:experiment).id)
       treatment_group = create(:treatment_group, experiment: experiment)
       reminder_template = create(:reminder_template, message: "hello.set01", treatment_group: treatment_group)
-      notification = create(:notification, message: reminder_template.message)
-      membership = create(:treatment_group_membership, treatment_group: treatment_group)
+      notification = create(:notification,
+        purpose: :experimental_appointment_reminder,
+        message: reminder_template.message,
+        patient: patient,
+        subject: nil)
+      membership = create(:treatment_group_membership, treatment_group: treatment_group, patient: patient)
       membership.record_notification(notification)
 
-      successful_communication = create(:communication, notification: notification)
+      successful_communication = create(:communication, notification: notification, user: nil, appointment: nil)
       create(:twilio_sms_delivery_detail, :delivered, communication: successful_communication)
 
-      unsuccessful_communication = create(:communication, notification: notification)
+      unsuccessful_communication = create(:communication, notification: notification, user: nil, appointment: nil)
       create(:twilio_sms_delivery_detail, :failed, communication: unsuccessful_communication)
       notification.update(status: :sent)
 
@@ -267,14 +272,19 @@ RSpec.describe Experimentation::NotificationsExperiment, type: :model do
     end
 
     it "records a failure if only failed deliveries are present" do
+      patient = create(:patient)
       experiment = described_class.find(create(:experiment).id)
       treatment_group = create(:treatment_group, experiment: experiment)
       reminder_template = create(:reminder_template, message: "hello.set01", treatment_group: treatment_group)
-      notification = create(:notification, message: reminder_template.message)
-      membership = create(:treatment_group_membership, treatment_group: treatment_group)
+      notification = create(:notification,
+        purpose: :experimental_appointment_reminder,
+        message: reminder_template.message,
+        patient: patient,
+        subject: nil)
+      membership = create(:treatment_group_membership, treatment_group: treatment_group, patient: patient)
       membership.record_notification(notification)
 
-      unsuccessful_communication = create(:communication, notification: notification)
+      unsuccessful_communication = create(:communication, notification: notification, user: nil, appointment: nil)
       create(:twilio_sms_delivery_detail, :failed, communication: unsuccessful_communication)
       notification.update(status: :sent)
 
@@ -289,14 +299,19 @@ RSpec.describe Experimentation::NotificationsExperiment, type: :model do
     end
 
     it "records notification statuses for all memberships (not just enrolled)" do
+      patient = create(:patient)
       experiment = described_class.find(create(:experiment).id)
       treatment_group = create(:treatment_group, experiment: experiment)
       reminder_template = create(:reminder_template, message: "hello.set01", treatment_group: treatment_group)
-      notification = create(:notification, message: reminder_template.message)
-      membership = create(:treatment_group_membership, treatment_group: treatment_group, status: :evicted)
+      notification = create(:notification,
+        purpose: :experimental_appointment_reminder,
+        message: reminder_template.message,
+        patient: patient,
+        subject: nil)
+      membership = create(:treatment_group_membership, treatment_group: treatment_group, status: :evicted, patient: patient)
       membership.record_notification(notification)
 
-      successful_communication = create(:communication, notification: notification)
+      successful_communication = create(:communication, notification: notification, user: nil, appointment: nil)
       create(:twilio_sms_delivery_detail, :delivered, communication: successful_communication)
 
       experiment.record_notification_results
