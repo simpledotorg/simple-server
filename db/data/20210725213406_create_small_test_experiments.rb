aa9e95ca9398b0483229e768d95584cfe69be6d4
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-11-09 19:07:44 +0530

    Commit schema, fix old script

--- db/data/20210725213406_create_small_test_experiments.rb
+++ db/data/20210725213406_create_small_test_experiments.rb
@@ -5,12 +5,14 @@ class CreateSmallTestExperiments < ActiveRecord::Migration[5.2]
     Seed::ExperimentSeeder.create_current_experiment(
       experiment_name: "Small Current Patient July 2021",
       start_time: Time.parse("Jul 28, 2021"),
-      end_time: Time.parse("Jul 30, 2021")
+      end_time: Time.parse("Jul 30, 2021"),
+      max_patients_per_day: 100
     )
     Seed::ExperimentSeeder.create_stale_experiment(
       experiment_name: "Small Stale Patient July 2021",
       start_time: Time.parse("Jul 28, 2021"),
-      end_time: Time.parse("Jul 30, 2021")
+      end_time: Time.parse("Jul 30, 2021"),
+      max_patients_per_day: 100
     )
   end
 
