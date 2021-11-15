36f1045b60ebbbffb0bde5cd49bc02baeacd0a39
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-11-15 18:49:55 +0530

    Add data migration to setup experiments in IHCI

---
+++ db/data/20211115131602_create_november_ihci_experiments.rb
@@ -0,0 +1,22 @@
+class CreateNovemberIhciExperiments < ActiveRecord::Migration[5.2]
+  def up
+    return unless CountryConfig.current_country?("India") && SimpleServer.env.production?
+
+    Seed::ExperimentSeeder.create_current_experiment(
+      experiment_name: "Current Patient November 2021",
+      start_time: Date.parse("Nov 16, 2021").beginning_of_day,
+      end_time: Date.parse("Nov 15, 2021").end_of_day,
+      max_patients_per_day: 9000
+    )
+    Seed::ExperimentSeeder.create_stale_experiment(
+      experiment_name: "Stale Patient November 2021",
+      start_time: Date.parse("Nov 16, 2021").beginning_of_day,
+      end_time: Date.parse("Nov 15, 2021").end_of_day,
+      max_patients_per_day: 6000
+    )
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
