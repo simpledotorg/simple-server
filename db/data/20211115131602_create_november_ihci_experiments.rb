5dcbeb52cb2b18869df4f70bbf8e4ad46b8a6e4e
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-11-15 22:46:52 +0530

    Fix typo

--- db/data/20211115131602_create_november_ihci_experiments.rb
+++ db/data/20211115131602_create_november_ihci_experiments.rb
@@ -5,13 +5,13 @@ class CreateNovemberIhciExperiments < ActiveRecord::Migration[5.2]
     Seed::ExperimentSeeder.create_current_experiment(
       experiment_name: "Current Patient November 2021",
       start_time: Date.parse("Nov 16, 2021").beginning_of_day,
-      end_time: Date.parse("Nov 15, 2021").end_of_day,
+      end_time: Date.parse("Dec 15, 2021").end_of_day,
       max_patients_per_day: 9000
     )
     Seed::ExperimentSeeder.create_stale_experiment(
       experiment_name: "Stale Patient November 2021",
       start_time: Date.parse("Nov 16, 2021").beginning_of_day,
-      end_time: Date.parse("Nov 15, 2021").end_of_day,
+      end_time: Date.parse("Dec 15, 2021").end_of_day,
       max_patients_per_day: 6000
     )
   end
