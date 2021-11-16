6ea405033265d3e8eed6d493aabfa66c0fb124d0
Rob Sanheim <rsanheim@gmail.com>
2021-11-15 23:34:38 -0600

    docs and logging

--- lib/sentry_handler.rb
+++ lib/sentry_handler.rb
@@ -1,8 +1,20 @@
+# Useful for reporting to Sentry from cron scheduled `runner` tasks - other contexts already have Sentry
+# middlewares setup that do this for us.
+#
+# Example usage:
+#
+# class MyService
+# . prepend SentryHandler
+#
+#   def call
+# .   # do things
+# . end
 module SentryHandler
   def call
     super
-  rescue => e
+  rescue => exception
+    Rails.logger.error("captured exception #{e} in #{self.class.name}, reporting and reraising", exception: exception)
     Sentry.capture_exception(e)
-    raise e
+    raise exception
   end
 end
