c2587cc5ed6e036bb4359aa73c701a930d453d59
Priyanga P Kini <priyangapkini99@gmail.com>
2021-11-26 18:16:42 +0530

    Change varibale name from `default_country_code` to `country_code`

--- app/models/concerns/phone_number_localizable.rb
+++ app/models/concerns/phone_number_localizable.rb
@@ -6,8 +6,8 @@ module PhoneNumberLocalizable
   included do
     def localized_phone_number
       parsed_number = Phonelib.parse(phone_number, Rails.application.config.country[:abbreviation]).raw_national
-      default_country_code = Rails.application.config.country[:sms_country_code]
-      default_country_code + parsed_number
+      country_code = Rails.application.config.country[:sms_country_code]
+      country_code + parsed_number
     end
   end
 end
