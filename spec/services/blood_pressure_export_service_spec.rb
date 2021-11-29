dcfab98e41dfea640a3b5cbb1b5676580cdefbc6
cpena <carloshpena93@gmail.com>
2021-11-29 17:59:30 -0500

    added #call method context, when data is downloaded

--- spec/services/blood_pressure_export_service_spec.rb
+++ spec/services/blood_pressure_export_service_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe BloodPressureExportService, type: :model do
   let(:start_period) { Period.month(december) }
   let(:end_period) { Period.month(may) }
   let(:user) { create(:admin, :manager, :with_access, resource: organization, organization: organization) }
+  let(:supervisor) { create(:admin, :manager, :with_access, resource: facility_group) }
 
   before :each do
     I18n.default_locale = :en_IN
@@ -21,8 +22,23 @@ RSpec.describe BloodPressureExportService, type: :model do
         facility_1 = create(:facility, name: "facility_1", facility_group: facility_group, facility_size: "small")
         facility_2 = create(:facility, name: "facility_2", facility_group: facility_group, facility_size: "medium")
         facility_3 = create(:facility, name: "facility_3", facility_group: facility_group, facility_size: "large")
+        facility_4 = create(:facility, name: "facility_4", facility_group: facility_group, facility_size: "large")
 
-        facilities = [ facility_1, facility_2, facility_3 ]
+        patients = create_list(:patient, 2, full_name: "patient", registration_user: user, registration_facility: facility_3, recorded_at: may - 5.months)
+        patients.each { |p| create(:bp_with_encounter, :under_control, facility: facility_3, patient: p) }
+
+        facility_set_1 = [facility_1, facility_2, facility_3]
+        facility_set_2 = [facility_1, facility_3, facility_4]
+
+        refresh_views
+
+        service_1 = described_class.new(start_period: start_period, end_period: end_period, facilities: facility_set_1)
+        service_2 = described_class.new(start_period: start_period, end_period: end_period, facilities: facility_set_2)
+        debugger
+        expect(service_1.call.keys).to eq(["large", "medium", "small"])
+        expect(service_2.call.keys).to eq(["large", "small"])
+        expect(service_1.call.values.first.keys).to eq(["aggregate", "facilities"])
+        expect(service_1.call["large"]["aggregate"]["Total registered"]).to eq("2")
       end
     end
   end
