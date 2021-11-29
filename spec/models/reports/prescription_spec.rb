ea8901efcc791aea90a061273751826412afb679
Srihari Sriraman <srihari@nilenso.com>
2021-11-29 12:19:57 +0530

    Add test for other medication, fix tests for foreign key change

--- spec/models/reports/prescription_spec.rb
+++ spec/models/reports/prescription_spec.rb
@@ -33,9 +33,9 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
 
   describe "Cleanup and mapping" do
     it "cleans up drug names using lookup tables" do
+      DrugLookup::MedicinePurpose.create(name: "Amlodipine", hypertension: true, diabetes: false)
       DrugLookup::RawToCleanMedicine.create({raw_name: "Amlo", raw_dosage: "5mg", rxcui: 123})
       DrugLookup::CleanMedicineToDosage.create({medicine: "Amlodipine", dosage: 5, rxcui: 123})
-      DrugLookup::MedicinePurpose.create(name: "Amlodipine", hypertension: true, diabetes: false)
 
       patient = FactoryBot.create(:patient, recorded_at: june_2021[:over_12_months_ago])
       add_drug(patient, "Amlo", "5mg", june_2021[:under_3_months_ago])
@@ -45,9 +45,9 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
     end
 
     it "cleans up drug dosages using lookup tables" do
+      DrugLookup::MedicinePurpose.create(name: "Telmisartan", hypertension: true, diabetes: false)
       DrugLookup::RawToCleanMedicine.create({raw_name: "Telmi", raw_dosage: "40 OD", rxcui: 123})
       DrugLookup::CleanMedicineToDosage.create({medicine: "Telmisartan", dosage: 40, rxcui: 123})
-      DrugLookup::MedicinePurpose.create(name: "Telmisartan", hypertension: true, diabetes: false)
 
       patient = FactoryBot.create(:patient, recorded_at: june_2021[:over_12_months_ago])
       add_drug(patient, "Tel mi", "40od", june_2021[:under_3_months_ago])
@@ -59,11 +59,11 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
     it "works correctly for combination medicines" do
       DrugLookup::RawToCleanMedicine.create({raw_name: "Los-H", raw_dosage: "125mg", rxcui: 111})
 
-      DrugLookup::CleanMedicineToDosage.create({medicine: "Losartan Potassium", dosage: 100, rxcui: 111})
       DrugLookup::MedicinePurpose.create(name: "Losartan Potassium", hypertension: true, diabetes: false)
+      DrugLookup::CleanMedicineToDosage.create({medicine: "Losartan Potassium", dosage: 100, rxcui: 111})
 
-      DrugLookup::CleanMedicineToDosage.create({medicine: "Hydrochlorothiazide", dosage: 25, rxcui: 111})
       DrugLookup::MedicinePurpose.create(name: "Hydrochlorothiazide", hypertension: true, diabetes: false)
+      DrugLookup::CleanMedicineToDosage.create({medicine: "Hydrochlorothiazide", dosage: 25, rxcui: 111})
 
       patient = FactoryBot.create(:patient, recorded_at: june_2021[:over_12_months_ago])
       add_drug(patient, "Los-H", "125mg", june_2021[:under_3_months_ago])
@@ -95,7 +95,7 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
     it "creates a correct timeline of medications the patient has been on every month" do
       with_reporting_time_zone do
         DrugLookup::MedicinePurpose.create(name: "Amlodipine", hypertension: true, diabetes: false)
-        DrugLookup::MedicinePurpose.create(name: "Atenolol", hypertension: true, diabetes: false)
+        DrugLookup::MedicinePurpose.create(name: "Captopril", hypertension: true, diabetes: false)
         DrugLookup::MedicinePurpose.create(name: "Telmisartan", hypertension: true, diabetes: false)
 
         DrugLookup::RawToCleanMedicine.create({raw_name: "Amlo", raw_dosage: "5mg", rxcui: 111})
@@ -110,8 +110,8 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
         DrugLookup::RawToCleanMedicine.create({raw_name: "Telmi", raw_dosage: "80mg", rxcui: 444})
         DrugLookup::CleanMedicineToDosage.create({medicine: "Telmisartan", dosage: 80, rxcui: 444})
 
-        DrugLookup::RawToCleanMedicine.create({raw_name: "Aten", raw_dosage: "10mg", rxcui: 555})
-        DrugLookup::CleanMedicineToDosage.create({medicine: "Atenolol", dosage: 10, rxcui: 555})
+        DrugLookup::RawToCleanMedicine.create({raw_name: "Capto", raw_dosage: "10mg", rxcui: 555})
+        DrugLookup::CleanMedicineToDosage.create({medicine: "Captopril", dosage: 10, rxcui: 555})
 
         patient = FactoryBot.create(:patient, recorded_at: june_2021[:two_years_ago])
         add_drug(patient, "Amlo", "5mg", june_2021[:ten_months_ago])
@@ -125,7 +125,7 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
         add_drug(patient, "Telmi", "40mg", june_2021[:three_months_ago])
         # does not visit for 1 month
         add_drug(patient, "Telmi", "80mg", june_2021[:one_month_ago])
-        add_drug(patient, "Aten", "10mg", june_2021[:one_month_ago])
+        add_drug(patient, "Capto", "10mg", june_2021[:one_month_ago])
 
         described_class.refresh
 
@@ -148,7 +148,7 @@ RSpec.describe Reports::Prescription, {type: :model, reporting_spec: true} do
         expect(described_class.find_by(patient: patient, month_date: june_2021[:two_months_ago]).amlodipine).to eq(10)
         expect(described_class.find_by(patient: patient, month_date: june_2021[:two_months_ago]).telmisartan).to eq(40)
 
-        # expect(described_class.find_by(patient: patient, month_date: june_2021[:one_month_ago]).other_bp_medications).to eq(10)
+        expect(described_class.find_by(patient: patient, month_date: june_2021[:one_month_ago]).other_bp_medications).to eq(10)
         expect(described_class.find_by(patient: patient, month_date: june_2021[:one_month_ago]).amlodipine).to eq(10)
         expect(described_class.find_by(patient: patient, month_date: june_2021[:one_month_ago]).telmisartan).to eq(80)
       end
