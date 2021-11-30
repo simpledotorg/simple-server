477af8bfb4b541582a6f7802f582f92af2065be2
Rob Sanheim <rsanheim@gmail.com>
2021-11-30 01:03:21 -0600

    Skip for android_review also

--- config/initializers/rate_limiter.rb
+++ config/initializers/rate_limiter.rb
@@ -1,4 +1,4 @@
-unless SimpleServer.env.sandbox? || SimpleServer.env.qa?
+unless SimpleServer.env.sandbox? || SimpleServer.env.qa? || SimpleServer.env.android_review?
   module RateLimit
     def self.auth_api_options
       limit_proc = proc { |_req| 5 }
