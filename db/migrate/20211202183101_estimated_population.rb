04574f135b4e55d209bef8d4a4be9ffc6c34320e
Claudio Vallejo <hola@claudiovallejo.mx>
2021-12-02 13:38:36 -0500

    Created EstimatedPopulation table

---
+++ db/migrate/20211202183101_estimated_population.rb
@@ -0,0 +1,11 @@
+class EstimatedPopulation < ActiveRecord::Migration[5.2]
+  def change
+    create_table :estimated_populations do |t|
+      t.integer :population, null: false
+      t.string :diagnosis, null: false, default: "HTN"
+      t.uuid :region_id, null: false
+      t.uuid :created_by_user_id
+      t.uuid :updated_by_user_id
+    end
+  end
+end
