0a2285a0525fc2504c12feb7b99885e29fad94b5
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-12-02 14:01:34 +0530

    Add data migration

---
+++ db/data/20211202082234_create_december_bd_experiments.rb
@@ -0,0 +1,22 @@
+class CreateDecemberBdExperiments < ActiveRecord::Migration[5.2]
+  def up
+    return unless CountryConfig.current_country?("Bangladesh") && SimpleServer.env.production?
+
+    Seed::ExperimentSeeder.create_current_experiment(
+      experiment_name: "Current Patient December 2021",
+      start_time: Date.parse("Dec 6, 2021").beginning_of_day,
+      end_time: Date.parse("Jan 5, 2022").end_of_day,
+      max_patients_per_day: 5000
+    )
+    Seed::ExperimentSeeder.create_stale_experiment(
+      experiment_name: "Stale Patient December 2021",
+      start_time: Date.parse("Dec 6, 2021").beginning_of_day,
+      end_time: Date.parse("Jan 5, 2022").end_of_day,
+      max_patients_per_day: 2000
+    )
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
