53ffe03ba67ca14a4e22e8fd6154aa9e0693ef9d
Claudio Vallejo <hola@claudiovallejo.mx>
2021-12-03 13:35:00 -0500

    Created my first validation test :)

--- db/migrate/20211202183101_estimated_population.rb
+++ db/migrate/20211202183101_estimated_population.rb
@@ -6,6 +6,7 @@ class EstimatedPopulation < ActiveRecord::Migration[5.2]
       t.uuid :region_id, null: false
       t.uuid :created_by_user_id
       t.uuid :updated_by_user_id
+      t.datetime :deleted_at
     end
   end
 end
--- spec/models/estimated_population_spec.rb
+++ spec/models/estimated_population_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe EstimatedPopulation, type: :model do
   describe "validations" do
     it "is not valid without a population" do
       region = Region.create(name: "New York", region_type: "state")
-      estimated_population = EstimatedPopulation.new()
+      estimated_population = EstimatedPopulation.new(diagnosis: "HTN", region_id: region.id)
+
+      expect(estimated_population).to_not be_valid
     end
   end
 end
