716b878aed24fcc0d72e230bd6f41e596d48ca6f
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2021-12-06 18:42:00 +0530

    Change date

--- db/data/20211202082234_create_december_bd_experiments.rb
+++ db/data/20211202082234_create_december_bd_experiments.rb
@@ -4,14 +4,14 @@ class CreateDecemberBdExperiments < ActiveRecord::Migration[5.2]
 
     Seed::ExperimentSeeder.create_current_experiment(
       experiment_name: "Current Patient December 2021",
-      start_time: Date.parse("Dec 6, 2021").beginning_of_day,
-      end_time: Date.parse("Jan 5, 2022").end_of_day,
+      start_time: Date.parse("Dec 7, 2021").beginning_of_day,
+      end_time: Date.parse("Jan 6, 2022").end_of_day,
       max_patients_per_day: 5000
     )
     Seed::ExperimentSeeder.create_stale_experiment(
       experiment_name: "Stale Patient December 2021",
-      start_time: Date.parse("Dec 6, 2021").beginning_of_day,
-      end_time: Date.parse("Jan 5, 2022").end_of_day,
+      start_time: Date.parse("Dec 7, 2021").beginning_of_day,
+      end_time: Date.parse("Jan 6, 2022").end_of_day,
       max_patients_per_day: 2000
     )
   end
