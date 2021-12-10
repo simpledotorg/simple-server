3947a8d3c03d63a0708e0a73befa8f7e91401468
vkrm <mail@vkrm.is>
2021-12-10 15:01:01 +0530

    include plpgsql extension

--- db/structure.sql
+++ db/structure.sql
@@ -9,6 +9,12 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
 --
 -- Name: ltree; Type: EXTENSION; Schema: -; Owner: -
 --
