51f01aedc95d085198e144403901b15509136da4
vkrm <mail@vkrm.is>
2021-12-10 13:42:38 +0530

    exclude create schema

--- db/structure.sql
+++ db/structure.sql
@@ -10,13 +10,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA public;
-
-
---
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
 --
 
