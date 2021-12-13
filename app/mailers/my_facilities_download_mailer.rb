eab4b56b466c611c401d6688cfacb363c19b5fc7
cpena <carloshpena93@gmail.com>
2021-12-13 14:51:07 -0500

    change test email

--- app/mailers/my_facilities_download_mailer.rb
+++ app/mailers/my_facilities_download_mailer.rb
@@ -1,5 +1,5 @@
 class MyFacilitiesDownloadMailer < ApplicationMailer
   def my_facilities_mail
-    mail(to: "carloshpena93@gmail.com", subject: "test test test")
+    mail(to: "cpena.intern@resolvetosavelives.org", subject: "test test test")
   end
 end
