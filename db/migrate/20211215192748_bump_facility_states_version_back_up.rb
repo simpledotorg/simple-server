c5643ad027952db1011091c79a36cdb8a6815b9f
Srihari Sriraman <srihari@nilenso.com>
2021-12-16 00:58:38 +0530

    Bump facility states version back up
    This was mistakenly set to v3 in the previous migration

---
+++ db/migrate/20211215192748_bump_facility_states_version_back_up.rb
@@ -0,0 +1,5 @@
+class BumpFacilityStatesVersionBackUp < ActiveRecord::Migration[5.2]
+  def change
+    update_view :reporting_facility_states, version: 4, revert_to_version: 3, materialized: true
+  end
+end
