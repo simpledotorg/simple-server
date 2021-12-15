d1f9abf439068d57f4d88218645e39c8df167d79
Srihari Sriraman <srihari@nilenso.com>
2021-12-15 23:51:30 +0530

    Add comments back in the migration

--- db/migrate/20211210152752_add_prescriptions_to_patient_states.rb
+++ db/migrate/20211210152752_add_prescriptions_to_patient_states.rb
@@ -6,6 +6,7 @@ class AddPrescriptionsToPatientStates < ActiveRecord::Migration[5.2]
     update_view :reporting_patient_states, version: 4, revert_to_version: 3, materialized: true
     add_index :reporting_patient_states, :bp_facility_id, name: "reporting_patient_states_bp_facility_id"
     add_index :reporting_patient_states, :titrated, name: "reporting_patient_states_titrated"
+    Reports::PatientState.add_comments
 
     create_view :reporting_facility_states, version: 3, materialized: true
     add_index :reporting_facility_states, [:month_date, :facility_region_id], name: :facility_states_month_date_region_id, unique: true
@@ -21,6 +22,7 @@ class AddPrescriptionsToPatientStates < ActiveRecord::Migration[5.2]
     remove_index :reporting_patient_states, name: "reporting_patient_states_bp_facility_id"
     remove_index :reporting_patient_states, name: "reporting_patient_states_titrated"
     update_view :reporting_patient_states, version: 3, revert_to_version: 2, materialized: true
+    Reports::PatientState.add_comments
 
     create_view :reporting_facility_states, version: 3, materialized: true
     add_index :reporting_facility_states, [:month_date, :facility_region_id], name: :facility_states_month_date_region_id, unique: true
