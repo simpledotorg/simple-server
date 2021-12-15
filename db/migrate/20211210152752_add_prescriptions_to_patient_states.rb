908db5b54cf7e7792d59e6c37c73f31d56a93037
Srihari Sriraman <srihari@nilenso.com>
2021-12-15 15:45:22 +0530

    Add titration information to patient states

---
+++ db/migrate/20211210152752_add_prescriptions_to_patient_states.rb
@@ -0,0 +1,16 @@
+class AddPrescriptionsToPatientStates < ActiveRecord::Migration[5.2]
+  def change
+    drop_view :reporting_quarterly_facility_states, materialized: true
+    drop_view :reporting_facility_states, materialized: true
+
+    update_view :reporting_patient_states, version: 4, revert_to_version: 3, materialized: true
+    add_index :reporting_patient_states, :bp_facility_id, name: "reporting_patient_states_bp_facility_id"
+    add_index :reporting_patient_states, :titrated, name: "reporting_patient_states_titrated"
+
+    create_view :reporting_facility_states, version: 3, materialized: true
+    add_index :reporting_facility_states, [:month_date, :facility_region_id], name: :facility_states_month_date_region_id, unique: true
+
+    create_view :reporting_quarterly_facility_states, version: 1, materialized: true
+    add_index :reporting_quarterly_facility_states, [:quarter_string, :facility_region_id], name: :quarterly_facility_states_quarter_string_region_id, unique: true
+  end
+end
