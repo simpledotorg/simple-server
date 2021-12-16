4faefd768e9dd715cc799369b78923cd613d30e5
Rob Sanheim <rsanheim@gmail.com>
2021-12-15 22:57:30 -0600

    lint

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -119,7 +119,7 @@ RSpec.describe Period, type: :model do
   it "cannot compare period with other classes" do
     expect {
       q1_2019_period > "January 1 2019"
-    }.to raise_error(ArgumentError, %{you are trying to compare "January 1 2019", a String, with a Period})
+    }.to raise_error(ArgumentError, %(you are trying to compare "January 1 2019", a String, with a Period))
   end
 
   it "month and quarter periods are never equal" do
