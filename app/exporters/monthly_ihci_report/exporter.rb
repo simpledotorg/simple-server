46e2925f4b64a3c9c141d682f7211656c7462ad4
Srihari Sriraman <srihari@nilenso.com>
2021-12-22 13:49:38 +0530

    Clean up notes, and add a TODO

--- app/exporters/monthly_ihci_report/exporter.rb
+++ app/exporters/monthly_ihci_report/exporter.rb
@@ -8,17 +8,9 @@ module MonthlyIHCIReport
       @repo = Reports::Repository.new(district.facility_regions, periods: period_month)
     end
 
-    def dev_stuff
-      # TODO: remove this when you don't need it anymore
-      reload!
-      r = Region.find_by(id: "3de010c5-bb42-4835-9cdd-fbaa6aee08d5")
-      p = Period.month("2021-09-01".to_date)
-      file_content = MonthlyIHCIReport::Exporter.new(r, p).export_file
-      File.write("foo.csv", file_content)
-    end
-
     def export_file
       # https://gist.github.com/aquajach/7fde54aa9bc1ac03740feb154e53eb7d
+      # TODO: collate all 3 sheets into a zip file or excel sheet
       to_csv(FacilityData.new(district, month).rows)
     end
 
