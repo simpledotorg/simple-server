9fd79a61bd6e8c583c22c7dc14cb1cfdbcba63b5
Rob Sanheim <rsanheim@gmail.com>
2022-01-05 22:50:32 -0600

    add a sanity test

--- spec/components/registrations_and_follow_ups_component_spec.rb
+++ spec/components/registrations_and_follow_ups_component_spec.rb
@@ -1,13 +1,18 @@
 require "rails_helper"
 
 RSpec.describe RegistrationsAndFollowUpsComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cvho) { create(:admin, :manager, :with_access, resource: common_org, organization: common_org) }
+  let(:jan_2020) { Time.zone.parse("January 1 2020") }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "can be created" do
+    facility_group = create(:facility_group, organization: common_org)
+    facility = create(:facility, name: "CHC Barnagar", facility_group: facility_group)
+
+    patient = create(:patient, registration_facility: facility, recorded_at: jan_2020.advance(months: -4))
+    create(:bp_with_encounter, :under_control, recorded_at: jan_2020.advance(months: -1), patient: patient, facility: facility)
+    create(:bp_with_encounter, :hypertensive, recorded_at: jan_2020, facility: facility)
+
+    repo = Reports::Repository.new(facility, periods: jan_2020.to_period)
+    component = described_class.new(facility, current_admin: cvho, repository: repo, current_period: jan_2020.to_period)
+  end
 end
