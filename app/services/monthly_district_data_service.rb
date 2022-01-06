dc6166764609cc9479c6589cf12bb2704c1b4ca2
Tim Cheadle <tcheadle@resolvetosavelives.org>
2022-01-06 15:20:13 -0500

    Shaped up district data to only use the repo, match state data

--- app/services/monthly_district_data_service.rb
+++ app/services/monthly_district_data_service.rb
@@ -6,13 +6,12 @@ class MonthlyDistrictDataService
     @months = period.downto(5).reverse
     regions = region.facility_regions.to_a << region
     @repo = Reports::Repository.new(regions, periods: @months)
-    @dashboard_analytics = DistrictAnalyticsQuery.new(region, :month, 6, period.value, include_current_period: true).call
   end
 
   def report
     CSV.generate(headers: true) do |csv|
-      csv << ["Monthly Facility Data: #{region.name} #{period.to_date.strftime("%B %Y")}"]
-      csv << section_labels
+      csv << ["Monthly facility data for #{region.name} #{period.to_date.strftime("%B %Y")}"]
+      csv << section_row
       csv << header_row
       csv << district_row
       facility_rows.each do |row|
@@ -23,9 +22,10 @@ class MonthlyDistrictDataService
 
   private
 
-  def section_labels
+  def section_row
     [
-      Array.new(12, nil),
+      # These just add empty spacer columns
+      Array.new(13, nil),
       "New Registrations",
       Array.new(5, nil),
       "Follow-up patients",
@@ -39,13 +39,15 @@ class MonthlyDistrictDataService
 
   def header_row
     month_labels = months.map { |month| month.value.strftime("%b-%Y") }
+
     [
       "#",
+      "State",
       "District",
+      "Block",
       "Facility",
       "Facility type",
-      "Block",
-      "Active/Inactive (Inactive facilities have 0 BP measures taken)",
+      "Facility size",
       "Estimated hypertensive population",
       "Total registrations",
       "Total assigned patients",
@@ -66,70 +68,70 @@ class MonthlyDistrictDataService
   end
 
   def district_row
-    follow_ups_by_month = months.each_with_object({}) { |month, hsh|
-      monthly_count = dashboard_analytics.sum { |_, data| data.dig(:follow_up_patients_by_period, month.value) || 0 }
-      hsh["follow_ups_#{month.value}".to_sym] = monthly_count
-    }
-    region_data = {
-      serial_number: "All",
-      district_name: region.name,
-      facility_name: nil,
+    row_data = {
+      index: "All",
+      state: region.parent.name,
+      district: region.name,
+      block: nil,
+      facility: nil,
       facility_type: nil,
-      block_name: nil,
-      active: nil
-    }.merge(common_attributes(region, follow_ups_by_month))
-    region_data.values
+      facility_size: nil
+    }.merge(region_data(region))
+
+    row_data.values
   end
 
   def facility_rows
-    region.facility_regions.sort_by(&:name).map.with_index do |facility, index|
-      follow_ups_by_month = months.each_with_object({}) { |month, hsh|
-        monthly_count = dashboard_analytics.dig(facility.source.id, :follow_up_patients_by_period, month.value) || 0
-        hsh["follow_ups_#{month.value}".to_sym] = monthly_count
-      }
-
-      facility_data = {
-        serial_number: index + 1,
-        district_name: region.name,
-        facility_name: facility.name,
+    region.facility_regions.sort_by { |f| [f.block_region.name, f.name] }.map.with_index do |facility, index|
+      row_data = {
+        index: index + 1,
+        state: facility.state_region.name,
+        district: facility.district_region.name,
+        block: facility.block_region.name,
+        facility: facility.name,
         facility_type: facility.source.facility_type,
-        block_name: facility.source.block,
-        active: facility.source.blood_pressures.any? ? "Active" : "Inactive"
-      }.merge(common_attributes(facility, follow_ups_by_month))
-      facility_data.values
+        facility_size: facility.source.facility_size.capitalize,
+      }.merge(region_data(facility))
+
+      row_data.values
     end
   end
 
-  def common_attributes(region, follow_ups_by_month)
-    complete_monthly_registrations = repo.monthly_registrations[region.slug]
-    registered_by_month = months.each_with_object({}) { |month, hsh|
-      hsh["registrations_#{month.value}".to_sym] = (complete_monthly_registrations[month] || 0)
+  def region_data(subregion)
+    total_registrations_count = repo.cumulative_registrations.dig(subregion.slug, period)
+    assigned_patients_count = repo.cumulative_assigned_patients.dig(subregion.slug, period)
+    ltfu_count = repo.ltfu.dig(subregion.slug, period)
+    dead_count = subregion.assigned_patients.with_hypertension.status_dead.count
+    adjusted_patients_under_care_count = repo.adjusted_patients.dig(subregion.slug, period)
+    controlled_count = repo.controlled.dig(subregion.slug, period)
+    uncontrolled_count = repo.uncontrolled.dig(subregion.slug, period)
+    missed_visits_count = repo.missed_visits.dig(subregion.slug, period)
+    no_bp_taken_count = repo.visited_without_bp_taken.dig(subregion.slug, period)
+
+    monthly_registrations = repo.monthly_registrations[subregion.slug]
+    registrations_by_month = months.each_with_object({}) { |month, hsh|
+      hsh["registrations_#{month.value}".to_sym] = monthly_registrations[month]
     }
 
-    dead_count = region.assigned_patients.with_hypertension.status_dead.count
-    assigned_patients_count = repo.cumulative_assigned_patients.dig(region.slug, period) || 0
-    ltfu_count = repo.ltfu.dig(region.slug, period) || 0
-    patients_under_care = assigned_patients_count - ltfu_count
-    controlled_count = repo.controlled.dig(region.slug, period) || 0
-    uncontrolled_count = repo.uncontrolled.dig(region.slug, period) || 0
-    missed_visits = repo.missed_visits.dig(region.slug, period) || 0
-    no_bp_taken = repo.visited_without_bp_taken.dig(region.slug, period) || 0
-    adjusted_patients_under_care = repo.adjusted_patients.dig(region.slug, period) || 0
+    monthly_follow_ups = repo.hypertension_follow_ups[subregion.slug]
+    follow_ups_by_month = months.each_with_object({}) { |month, hsh|
+      hsh["follow_ups_#{month.value}".to_sym] = monthly_follow_ups[month] || 0
+    }
 
     {
       estimated_hypertension_population: nil,
-      total_registrations: repo.cumulative_registrations.dig(region.slug, period) || 0,
+      total_registrations: total_registrations_count,
       total_assigned: assigned_patients_count,
       ltfu: ltfu_count,
       dead: dead_count,
-      patients_under_care: patients_under_care,
-      **registered_by_month,
+      patients_under_care: assigned_patients_count - ltfu_count,
+      **registrations_by_month,
       **follow_ups_by_month,
-      adjusted_patients_under_care: adjusted_patients_under_care,
+      adjusted_patients_under_care: adjusted_patients_under_care_count,
       controlled_count: controlled_count,
       uncontrolled_count: uncontrolled_count,
-      missed_visits: missed_visits,
-      no_bp_taken: no_bp_taken,
+      missed_visits: missed_visits_count,
+      no_bp_taken: no_bp_taken_count,
       amlodipine: nil,
       arbs_and_ace_inhibitors: nil,
       diurectic: nil
--- app/services/monthly_state_data_service.rb
+++ app/services/monthly_state_data_service.rb
@@ -10,11 +10,11 @@ class MonthlyStateDataService
 
   def report
     CSV.generate(headers: true) do |csv|
-      csv << ["Monthly District Data: #{region.name} #{period.to_date.strftime("%B %Y")}"]
+      csv << ["Monthly district data for #{region.name} #{period.to_date.strftime("%B %Y")}"]
       csv << section_row
       csv << header_row
-      csv << parent_row
-      child_rows.each do |row|
+      csv << state_row
+      district_rows.each do |row|
         csv << row
       end
     end
@@ -63,45 +63,45 @@ class MonthlyStateDataService
     ].flatten
   end
 
-  def parent_row
+  def state_row
     row_data = {
       index: "All",
-      parent_name: region.name,
-      child_name: nil
+      state: region.name,
+      district: nil
     }.merge(region_data(region))
 
     row_data.values
   end
 
-  def child_rows
-    region.children.sort_by(&:name).map.with_index do |child, index|
+  def district_rows
+    region.district_regions.sort_by(&:name).map.with_index do |district, index|
       row_data = {
         index: index + 1,
-        parent_name: region.name,
-        child_name: child.name
-      }.merge(region_data(child))
+        state: region.name,
+        district: district.name
+      }.merge(region_data(district))
 
       row_data.values
     end
   end
 
-  def region_data(region)
-    total_registrations_count = repo.cumulative_registrations.dig(region.slug, period)
-    assigned_patients_count = repo.cumulative_assigned_patients.dig(region.slug, period)
-    ltfu_count = repo.ltfu.dig(region.slug, period)
-    dead_count = region.assigned_patients.with_hypertension.status_dead.count
-    adjusted_patients_under_care_count = repo.adjusted_patients.dig(region.slug, period)
-    controlled_count = repo.controlled.dig(region.slug, period)
-    uncontrolled_count = repo.uncontrolled.dig(region.slug, period)
-    missed_visits_count = repo.missed_visits.dig(region.slug, period)
-    no_bp_taken_count = repo.visited_without_bp_taken.dig(region.slug, period)
+  def region_data(subregion)
+    total_registrations_count = repo.cumulative_registrations.dig(subregion.slug, period)
+    assigned_patients_count = repo.cumulative_assigned_patients.dig(subregion.slug, period)
+    ltfu_count = repo.ltfu.dig(subregion.slug, period)
+    dead_count = subregion.assigned_patients.with_hypertension.status_dead.count
+    adjusted_patients_under_care_count = repo.adjusted_patients.dig(subregion.slug, period)
+    controlled_count = repo.controlled.dig(subregion.slug, period)
+    uncontrolled_count = repo.uncontrolled.dig(subregion.slug, period)
+    missed_visits_count = repo.missed_visits.dig(subregion.slug, period)
+    no_bp_taken_count = repo.visited_without_bp_taken.dig(subregion.slug, period)
 
-    monthly_registrations = repo.monthly_registrations[region.slug]
+    monthly_registrations = repo.monthly_registrations[subregion.slug]
     registrations_by_month = months.each_with_object({}) { |month, hsh|
       hsh["registrations_#{month.value}".to_sym] = monthly_registrations[month]
     }
 
-    monthly_follow_ups = repo.hypertension_follow_ups[region.slug]
+    monthly_follow_ups = repo.hypertension_follow_ups[subregion.slug]
     follow_ups_by_month = months.each_with_object({}) { |month, hsh|
       hsh["follow_ups_#{month.value}".to_sym] = monthly_follow_ups[month] || 0
     }
