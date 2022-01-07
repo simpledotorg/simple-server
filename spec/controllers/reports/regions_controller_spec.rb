243fe1aceee88fadb8014e096e295c897b09d46f
Tim Cheadle <tcheadle@resolvetosavelives.org>
2022-01-07 15:39:05 -0500

    Rewrite district facility downloads to match state downloads (#3241)
    * Added sandbox DHIS2 data elements
    
    * Shaped up district data to only use the repo, match state data
    
    * Removed an errant config change from this PR
    
    * Added sandbox DHIS2 data elements
    
    * Fixed specs to match updated copy
    
    * standardrb fix
    
    * Reverting errant commit of unrelated config
    
    * Cleaned up the way headers and spacer columns are produced
    
    * Uncommented and fixed spec assertion

--- spec/controllers/reports/regions_controller_spec.rb
+++ spec/controllers/reports/regions_controller_spec.rb
@@ -529,7 +529,7 @@ RSpec.describe Reports::RegionsController, type: :controller do
         expect_any_instance_of(MonthlyDistrictDataService).to receive(:report).and_call_original
         get :monthly_district_data_report, params: {id: region.slug, report_scope: "district", format: "csv"}
         expect(response.status).to eq(200)
-        expect(response.body).to include("Monthly Facility Data: #{region.name} #{Date.current.strftime("%B %Y")}")
+        expect(response.body).to include("Monthly facility data for #{region.name} #{Date.current.strftime("%B %Y")}")
         report_date = Date.current.strftime("%b-%Y").downcase
         expected_filename = "monthly-facility-data-#{region.slug}-#{report_date}.csv"
         expect(response.headers["Content-Disposition"]).to include(%(filename="#{expected_filename}"))
@@ -583,7 +583,7 @@ RSpec.describe Reports::RegionsController, type: :controller do
         expect_any_instance_of(MonthlyStateDataService).to receive(:report).and_call_original
         get :monthly_state_data_report, params: {id: region.slug, report_scope: "state", format: "csv"}
         expect(response.status).to eq(200)
-        expect(response.body).to include("Monthly District Data: #{region.name} #{Date.current.strftime("%B %Y")}")
+        expect(response.body).to include("Monthly district data for #{region.name} #{Date.current.strftime("%B %Y")}")
         report_date = Date.current.strftime("%b-%Y").downcase
         expected_filename = "monthly-district-data-#{region.slug}-#{report_date}.csv"
         expect(response.headers["Content-Disposition"]).to include(%(filename="#{expected_filename}"))
