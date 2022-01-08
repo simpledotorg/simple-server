ccff0c6ec7702f371682f29f4f9f7692b0277bbd
Rob Sanheim <rsanheim@gmail.com>
2022-01-07 18:37:18 -0600

    Dont turn on profiling anywhere
    we can revisit this if we want to use it in the future

--- config/initializers/00_datadog.rb
+++ config/initializers/00_datadog.rb
@@ -6,12 +6,9 @@ require "datadog/statsd"
 # exclude some envs by default
 DATADOG_ENABLED = ENV["DATADOG_ENABLED"] || !(Rails.env.development? || Rails.env.test? || Rails.env.profiling? || SimpleServer.env.review? || SimpleServer.env.android_review?)
 
-# Trying out Ruby code profiling in selected environments
-ENABLE_DD_PROFILING = SimpleServer.env.sandbox?
-
 Datadog.configure do |c|
   c.tracer.enabled = DATADOG_ENABLED
-  c.profiling.enabled = ENABLE_DD_PROFILING
+  c.profiling.enabled = false
   c.version = SimpleServer.git_ref(short: true)
   c.use :rack, headers: {request: %w[X-USER-ID X-FACILITY-ID X-SYNC-REGION-ID X-APP-VERSION], response: %w[Content-Type X-Request-ID]}
   c.use :rake
