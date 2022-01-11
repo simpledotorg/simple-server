3787d4b501a945410d3be36d8b6bd52508da3abb
Rob Sanheim <rsanheim@gmail.com>
2022-01-11 17:03:00 -0600

    get the initial display working; try to clean up the table

--- app/components/reports/monthly_progress_component.rb
+++ app/components/reports/monthly_progress_component.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Reports::MonthlyProgressComponent < ViewComponent::Base
-  attr_reader :gender_groups
-  attr_reader :range
+  attr_reader :diabetes_enabled
   attr_reader :diagnosis
+  attr_reader :facility
+  attr_reader :gender_groups
   attr_reader :metric
+  attr_reader :range
 
   def initialize(facility:, diagnosis:, metric:, query:, range:, gender_groups: true)
     @facility = facility
@@ -17,12 +19,36 @@ class Reports::MonthlyProgressComponent < ViewComponent::Base
     @gender_groups = gender_groups
   end
 
+  def diagnosis_group_class(gender)
+    classes = []
+    classes << diagnosis unless diagnosis == :all
+    classes << gender
+    classes.compact.join(":")
+  end
+
   def diagnosis_code
     case diagnosis
     when :hypertension then "htn"
     when :diabetes then "dm"
     when :all then nil
     else raise ArgumentError, "invalid diagnosis #{diagnosis}"
+    end
+  end
+
+  def table(grouping, &block)
+    options = { class: ["progress-table", metric, diagnosis_group_class(grouping)] }
+    unless (diagnosis == default_diagnosis && grouping == :all)
+      options[:style] = "display:none"
+    end
+    tag.table(options, &block)
+  end
+
+  # The defaut diagnosis is the one we display at the top level for the first display of progress tab
+  def default_diagnosis
+    if facility.diabetes_enabled?
+      :all
+    else
+      :hypertension
     end
   end
 
--- spec/components/reports/monthly_progress_component_spec.rb
+++ spec/components/reports/monthly_progress_component_spec.rb
@@ -1,13 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Reports::MonthlyProgressComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:facility) { create(:facility) }
+  let(:query) { Reports::FacilityStateGroup.where(facility_region_id: facility.region.id) }
+  let(:current_period) { Period.month("December 2021") }
+  let(:range) { Range.new(current_period.advance(months: -5), current_period) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "returns valid diagnosis gender classes" do
+    component = described_class.new(facility: facility, diagnosis: :all, metric: :registrations, query: query, range: range)
+    expect(component.diagnosis_group_class("all")).to eq("all")
+    expect(component.diagnosis_group_class("male")).to eq("male")
+    expect(component.diagnosis_group_class("female")).to eq("female")
+
+    component = described_class.new(facility: facility, diagnosis: :hypertension, metric: :registrations, query: query, range: range)
+    expect(component.diagnosis_group_class("all")).to eq("hypertension:all")
+    expect(component.diagnosis_group_class("male")).to eq("hypertension:male")
+    expect(component.diagnosis_group_class("female")).to eq("hypertension:female")
+  end
+
 end
