9a7cf5e594343f3d50603cd910716d3139284566
Rick Bradley <rick@rickbradley.com>
2022-01-13 13:29:25 -0600

    Make a pure AR + ltree version of fast regions tree view
    This will not be fast on large data but allows us to have a re-worked recursive renderer
    that's a bit easier to work with, and now we can swap in an in-memory fast_children method.

--- app/controllers/reports/regions_controller.rb
+++ app/controllers/reports/regions_controller.rb
@@ -32,22 +32,9 @@ class Reports::RegionsController < AdminController
   end
 
   def fastindex
-    accessible_facility_regions = authorize { current_admin.accessible_facility_regions(:view_reports) }
+    _accessible_facility_regions = authorize { current_admin.accessible_facility_regions(:view_reports) }
 
-    cache_key = current_admin.regions_access_cache_key
-    cache_version = "#{accessible_facility_regions.cache_key}/#{INDEX_CACHE_KEY}"
-
-    @accessible_regions = cache.fetch(cache_key, force: bust_cache?, version: cache_version, expires_in: 7.days) {
-      accessible_facility_regions.each_with_object({}) { |facility, result|
-        ancestors = facility.cached_ancestors.map { |facility| [facility.region_type, facility] }.to_h
-        org, state, district, block = ancestors.values_at("organization", "state", "district", "block")
-        result[org] ||= {}
-        result[org][state] ||= {}
-        result[org][state][district] ||= {}
-        result[org][state][district][block] ||= []
-        result[org][state][district][block] << facility
-      }
-    }
+    @org = Region.organization_regions.first
   end
 
   def show
