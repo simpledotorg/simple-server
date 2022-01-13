4b0a0323c1aecac6bb740934277164cc82d57f27
Rick Bradley <rick@rickbradley.com>
2022-01-13 15:09:52 -0600

    Add initial RegionTreeService with basic tests

---
+++ app/services/region_tree_service.rb
@@ -0,0 +1,18 @@
+class RegionTreeService
+  attr_reader :org, :tree_map
+
+  def initialize(org)
+    @org = org
+    preload_regions
+  end
+
+  def preload_regions
+    @tree_map = Region.all.each_with_object({}) do |region, memo|
+      memo[region.path] = region
+    end
+  end
+
+  def fast_children(region)
+    tree_map.keys.grep(/^#{region.path}\.[^\.]+$/).map { |path| tree_map[path] }
+  end
+end
