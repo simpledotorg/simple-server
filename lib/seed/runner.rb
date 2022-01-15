ac65f6e750cc72624be192fe58d8d2e1905520f6
Rob Sanheim <rsanheim@gmail.com>
2022-01-15 16:25:42 -0600

    More seed data in profiling (#3297)
    * Add more district names
    
    * wip
    
    * Use plain faker for block names for now
    
    * Use faker + uuid for block names to ensure uniqueness:
    
    we need this because AR import does a direct insert, and so we need
    unique slugs, at least given the current import strategy.
    
    Also adding more logging to see where we are spending most of our time
    on large facility imports.
    
    * More facilities in profiling; more logging
    
    * Add ability to skip creating encounters (ie BPs and BSs)
    
    * logging and skip a slow facility validation
    
    * add attr_reader logger
    
    * fix log output; turn off parent region creationg for the factory
    
    * lazy load more things for seed perf
    
    * Clean up; fix spec
    
    * lint
    
    * Just use env var for this
    
    * Use AR import for drug stocks
    
    * Skip appts when skip_encounters is true too
    
    * Fix the range of facilities
    
    * Set back to info
    
    * lint
    
    * show the current ref / date
    
    * tee append
    
    * Better block names / slugs
    
    * lint

--- lib/seed/runner.rb
+++ lib/seed/runner.rb
@@ -94,8 +94,10 @@ module Seed
           result.merge!(bp_result) { |key, count1, count2| count1 + count2 }
           blood_sugar_result = BloodSugarSeeder.call(config: config, facility: facility, user_ids: registration_user_ids)
           result.merge!(blood_sugar_result) { |key, count1, count2| count1 + count2 }
-          appt_result = create_appts(patient_info, facility: facility, user_ids: registration_user_ids)
-          result[:appointment] = appt_result.ids.size
+          unless config.skip_encounters
+            appt_result = create_appts(patient_info, facility: facility, user_ids: registration_user_ids)
+            result[:appointment] = appt_result.ids.size
+          end
           result
         }
         results.concat batch_result
@@ -104,12 +106,18 @@ module Seed
     end
 
     def seed_drug_stocks
-      Facility.all.each do |facility|
+      ds_attrs = Facility.find_each.with_object([]).each do |facility, attrs|
+        logger.info { "Seeding drug stocks for #{facility.id}" }
         user = facility.users.first
         facility.protocol.protocol_drugs.where(stock_tracked: true).each do |protocol_drug|
-          FactoryBot.create(:drug_stock, facility: facility, user: user, protocol_drug: protocol_drug)
+          attrs << FactoryBot.attributes_for(:drug_stock,
+            facility_id: facility.id,
+            user_id: user.id,
+            protocol_drug_id: protocol_drug.id,
+            region_id: facility.region.id)
         end
       end
+      DrugStock.import(ds_attrs)
     end
 
     def parallel_options(progress)
