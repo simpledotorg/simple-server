8eb55c9c3c93ac91b91b286bfe85c63b9a64038f
vkrm <mail@vkrm.is>
2022-01-21 18:30:56 +0530

    Update sidekiq address

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,2 +1,2 @@
 server "ec2-3-110-215-50.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron whitelist_phone_numbers seed_data]
-server "ec2-13-127-222-227.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
+server "ec2-52-66-125-161.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
