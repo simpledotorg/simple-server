d024ca228988015959e1275b3536c8700efe3a8a
vkrm <mail@vkrm.is>
2022-01-21 18:15:38 +0530

    Update sandbox address

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -1,2 +1,2 @@
-server "ec2-65-0-103-246.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron whitelist_phone_numbers seed_data]
+server "ec2-3-110-215-50.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron whitelist_phone_numbers seed_data]
 server "ec2-13-127-222-227.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
