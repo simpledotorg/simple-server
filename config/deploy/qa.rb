f64d33351f558082f5c81556c0ac698ebac1723a
vkrm <mail@vkrm.is>
2022-01-21 23:07:30 +0530

    Correct qa address

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,1 +1,1 @@
-server "ec2-13-232-139-143.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db sidekiq cron]
+server "ec2-3-108-62-212.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db sidekiq cron]
