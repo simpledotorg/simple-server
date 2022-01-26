cf0ac8e4c0efa86c800af2c29183368abc18451b
Rob Sanheim <rsanheim@gmail.com>
2022-01-25 22:10:55 -0600

    Add some sum methods for registrations and follow ups

--- app/models/reports/facility_state_group.rb
+++ app/models/reports/facility_state_group.rb
@@ -7,5 +7,15 @@ module Reports
     def self.materialized?
       true
     end
+
+    def self.total_registrations(facility, diagnosis, gender)
+      field = "monthly_registrations_#{diagnosis}_#{gender}"
+      where(facility: facility).sum(field).to_i
+    end
+
+    def self.total_follow_ups(facility, diagnosis, gender)
+      field = "monthly_follow_ups_#{diagnosis}_#{gender}"
+      where(facility: facility).sum(field).to_i
+    end
   end
 end
