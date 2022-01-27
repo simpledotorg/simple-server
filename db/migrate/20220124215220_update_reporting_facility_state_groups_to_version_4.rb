0cf4a0423b46656bf187fce208548e595a931c92
Rob Sanheim <rsanheim@gmail.com>
2022-01-27 12:15:04 -0600

    Update Reporting facility state groups to v4 (#3326)
    * Update to reporting_facility_state_groups v4:
    
    * Make sure follow ups are distinct per patient per month / facility
    * Add back the unique index (it looks like it got lost in the matview
      shuffle
    
    * Update structure.sql

---
+++ db/migrate/20220124215220_update_reporting_facility_state_groups_to_version_4.rb
@@ -0,0 +1,9 @@
+class UpdateReportingFacilityStateGroupsToVersion4 < ActiveRecord::Migration[5.2]
+  def change
+    update_view :reporting_facility_state_groups,
+      version: 4,
+      revert_to_version: 3,
+      materialized: true
+    add_index :reporting_facility_state_groups, [:month_date, :facility_region_id], name: :facility_state_groups_month_date_region_id, unique: true
+  end
+end
