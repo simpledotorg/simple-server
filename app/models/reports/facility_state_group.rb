57b4b5835ebf9f79964d893d2094e11b1b6a6436
Rob Sanheim <rsanheim@gmail.com>
2022-02-02 10:42:42 -0600

    Remove dead code

--- app/models/reports/facility_state_group.rb
+++ app/models/reports/facility_state_group.rb
@@ -25,24 +25,6 @@ module Reports
       where(facility: facility).select(calculations).to_a.first
     end
 
-    # Returns the total counts for a facility of either registrations or follow ups
-    #
-    # monthly_registrations_all
-    # monthly_registrations_dm_all
-    # monthly_registrations_htn_all
-    # monthly_follow_ups_all
-    # monthly_follow_ups_dm_all
-    # monthly_follow_ups_htn_all
-    def self.total(facility, metric, diagnosis, gender: "all")
-      diagnosis_code = if diagnosis == :all
-        nil
-      else
-        diagnosis
-      end
-      field = ["monthly", metric, diagnosis_code, gender].compact.join("_")
-      where(facility: facility).sum(field).to_i
-    end
-
     def period
       Period.month(month_date)
     end
