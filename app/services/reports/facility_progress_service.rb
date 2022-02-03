a03504eb456536d8735463847c72af5fd1dfb7c0
Rob Sanheim <rsanheim@gmail.com>
2022-02-03 13:57:59 -0600

    Refactor out the dimensions code

--- app/services/reports/facility_progress_service.rb
+++ app/services/reports/facility_progress_service.rb
@@ -9,6 +9,18 @@ module Reports
       @range = Range.new(@period.advance(months: -5), @period)
     end
 
+    def dimensions(indicator)
+      # handle the special case of any diagnosis and all genders first
+      dimensions = [Reports::FacilityProgressDimension.new(indicator, diagnosis: :all, gender: :all)]
+      combinations = [indicator].product([:diabetes, :hypertension]).product([:all, :male, :female, :transgender])
+      combinations.each_with_object([]) do |c|
+        indicator, diagnosis = *c.first
+        gender = c.last
+        dimensions << Reports::FacilityProgressDimension.new(indicator, diagnosis: diagnosis, gender: gender)
+      end
+      dimensions
+    end
+
     def total_counts
       @total_counts ||= Reports::FacilityStateGroup.totals(facility)
     end
