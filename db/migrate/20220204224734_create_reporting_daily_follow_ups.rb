674753c192a922eacceda8a37a6780453e96ad33
Rob Sanheim <rsanheim@gmail.com>
2022-02-07 08:26:08 -0600

    Reorder the index for patient_id first
    to cut down on cardinality faster

--- db/migrate/20220204224734_create_reporting_daily_follow_ups.rb
+++ db/migrate/20220204224734_create_reporting_daily_follow_ups.rb
@@ -1,6 +1,6 @@
 class CreateReportingDailyFollowUps < ActiveRecord::Migration[5.2]
   def change
     create_view :reporting_daily_follow_ups, materialized: true
-    add_index :reporting_daily_follow_ups, [:day_of_year, :facility_id, :patient_id], name: :daily_follow_ups_day_patient_facility, unique: true
+    add_index :reporting_daily_follow_ups, [:patient_id, :facility_id, :day_of_year], name: :daily_follow_ups_day_patient_facility, unique: true
   end
 end
