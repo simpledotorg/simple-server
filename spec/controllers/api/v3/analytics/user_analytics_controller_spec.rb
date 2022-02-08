511f0e67cb9122e8a0b21b686a37ccf67534e2da
Claudio Vallejo <hola@claudiovallejo.mx>
2022-02-08 11:24:06 -0500

    New "Progress tab" home page (#3352)
    * Started building new progress tab home page
    
    * Continued building home page
    
    * Started working on carousel
    
    * Created follow-ups achievement badges
    
    * Created achievement badges
    
    * Finished first pass
    
    * Updated controller to render proper view
    
    * Reversed change in en.yml
    
    * Fixed StandardRB failing test
    
    * Added new rspec to test conditional rendering of show_v2
    
    * Updated user_analytics_controller_spec
    
    * Updated spacing between cards
    
    * Reverted routes.rb changes
    
    * Retrigger build
    
    * Standardrb fix

--- spec/controllers/api/v3/analytics/user_analytics_controller_spec.rb
+++ spec/controllers/api/v3/analytics/user_analytics_controller_spec.rb
@@ -54,6 +54,12 @@ RSpec.describe Api::V3::Analytics::UserAnalyticsController, type: :controller do
         expect(response.body).to include("Enter drug stock")
       end
 
+      it "renders show_v2 if :new_progress_tab feature flag is enabled" do
+        Flipper.enable(:new_progress_tab)
+        get :show, format: :html
+        expect(response.body).to include("Yearly")
+      end
+
       it "renders successfully for follow_ups_v2 with no data" do
         Flipper.enable(:follow_ups_v2_progress_tab)
         refresh_views
