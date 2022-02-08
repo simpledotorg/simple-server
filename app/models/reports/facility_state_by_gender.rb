719a3cc8cfc44994f4960eb18c2af02145f4f843
Rob Sanheim <rsanheim@gmail.com>
2022-02-08 17:25:29 -0600

    Add the matview model

---
+++ app/models/reports/facility_state_by_gender.rb
@@ -0,0 +1,24 @@
+module Reports
+  class FacilityStateByGender < Reports::View
+    self.table_name = "reporting_facility_states_by_genders"
+
+    belongs_to :facility
+
+    def self.materialized?
+      true
+    end
+
+    def self.for_region(region_or_source)
+      region = region_or_source.region
+      where(region_id_field(region) => region.id)
+    end
+
+    def self.region_id_field(region)
+      "#{region.region_type}_region_id"
+    end
+
+    def period
+      @period ||= Period.month(month_date)
+    end
+  end
+end
