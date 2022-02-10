b3d8cdffab80175affe563f25a631a0f2fa107f3
Rob Sanheim <rsanheim@gmail.com>
2022-02-10 14:27:46 -0600

    Add a component to dislapy new v2 follow ups and registrations

---
+++ app/components/reports/daily_progress_component.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Reports::DailyProgressComponent < ViewComponent::Base
+  # We use 29 here because we also show today, so its 30 days inclusively
+  DAYS_AGO = 29
+  DAY_FORMAT = ApplicationHelper::STANDARD_DATE_DISPLAY_FORMAT
+  include AssetsHelper
+
+  attr_reader :service
+
+  def initialize(service)
+    @service = service
+    @now = Date.current
+    @start = @now - DAYS_AGO
+  end
+
+  delegate :daily_follow_ups, :daily_registrations, to: :service
+
+  def last_30_days
+    (@start..@now).to_a.reverse
+  end
+
+  def display_date(date)
+    date.strftime(DAY_FORMAT)
+  end
+end
