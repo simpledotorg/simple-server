28564e1a86b03aa2eedf987ab4f24755a5d0b451
Rob Sanheim <rsanheim@gmail.com>
2022-02-10 15:22:29 -0600

    Remove Config fallback code for Redis URLs (#3379)
    Redis will already try to connect to REDIS_URL when you call `Redis.new`
    without any arguments...which we do when the specific Redis urls aren't
    found in the enviornment.
    
    We can't depend on a fallback ENV var, because in Heroku the REDIS_URL
    isn't necessarily available at bundle time (Heroku looks to setup the
    Redis env after bundling).

--- app/helpers/config.rb
+++ app/helpers/config.rb
@@ -5,14 +5,6 @@ module Config
     end
   end
 
-  def self.ensure_required_keys_have_fallbacks(required_keys: {})
-    required_keys.each do |key, fallback|
-      ENV.fetch(key)
-    rescue KeyError
-      ENV.fetch(fallback)
-    end
-  end
-
   def self.get_int(config_key_name, default_value)
     (ENV[config_key_name] || default_value).to_i
   end
