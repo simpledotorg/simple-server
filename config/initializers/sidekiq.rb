c7ced63a8e95ec47ac41cbc756ac729292e121cc
Rob Sanheim <rsanheim@gmail.com>
2022-02-10 13:10:46 -0600

    Rename Config to EnvHelper; require explicitly
    Having a top level thing named Config is dangerous; Also, since this
    module is used to verify keys early on in the app boot process, we need
    to explicitly require it early on.
    
    This moves it to lib/env_helper, requires it in application.rb, and then
    renames references

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,7 @@ module SidekiqConfig
   Sidekiq::Extensions.enable_delay!
 
   def self.connection_pool
-    ConnectionPool.new(size: Config.get_int("SIDEKIQ_REDIS_POOL_SIZE", DEFAULT_REDIS_POOL_SIZE)) do
+    ConnectionPool.new(size: EnvHelper.get_int("SIDEKIQ_REDIS_POOL_SIZE", DEFAULT_REDIS_POOL_SIZE)) do
       if ENV["SIDEKIQ_REDIS_HOST"].present?
         Redis.new(host: ENV["SIDEKIQ_REDIS_HOST"])
       else
