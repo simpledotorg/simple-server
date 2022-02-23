b6eb28cfd067ae56db2eec9a0d93be3490138bf2
Hari Mohanraj <hari.mohanraj89@gmail.com>
2022-02-23 17:04:21 +0400

    Clean up MH exporter

--- app/exporters/maharashtra_dhis2_exporter.rb
+++ app/exporters/maharashtra_dhis2_exporter.rb
@@ -25,21 +25,13 @@ class MaharashtraDHIS2Exporter
   end
 
   def facility_bulk_data(facility_identifier)
-    data_elements_map = CountryConfig.current.fetch(:maharashtra_dhis2_data_elements)
-
-    current_month_period = Period.current.previous
-
-    bulk_data = []
+    facility_data = []
     facility = facility_identifier.facility
     slug = facility.region.slug
     org_unit_id = facility_identifier.identifier
+    repository = Reports::Repository.new(facility.region, periods: period_range)
 
-    range = (current_month_period.advance(months: -24)..current_month_period)
-
-    repository = Reports::Repository.new(facility.region, periods: range)
-
-    binding.pry
-    range.each do |month_period|
+    period_range.each do |month_period|
       data = {
         monthly_registrations_male: repository.monthly_registrations_by_gender[slug][month_period]["male"],
         monthly_registrations_female: repository.monthly_registrations_by_gender[slug][month_period]["female"]
@@ -48,25 +40,32 @@ class MaharashtraDHIS2Exporter
       data.each do |data_element, value|
         data_element_id, disaggregation_id = data_elements_map[data_element].split(".")
 
-        bulk_data << {
+        facility_data_element = {
           data_element: data_element_id,
           org_unit: org_unit_id,
           category_option_combo: disaggregation_id,
-          period: reporting_period(month_period),
+          period: month_period.to_s(:dhis2),
           value: value
         }
-        puts "Adding data for #{facility.name}, #{month_period}, #{data_element}: #{bulk_data.last}"
+
+        facility_data << facility_data_element
+
+        puts "Adding data for #{facility.name}, #{month_period}, #{data_element}: #{facility_data_element}"
       end
     end
 
-    bulk_data
+    facility_data
   end
 
-  def reporting_period(month_period)
-    if Flipper.enabled?(:dhis2_use_ethiopian_calendar)
-      EthiopiaCalendarUtilities.gregorian_month_period_to_ethiopian(month_period).to_s(:dhis2)
-    else
-      month_period.to_s(:dhis2)
-    end
+  def data_elements_map
+    @data_elements_map ||= CountryConfig.current.fetch(:maharashtra_dhis2_data_elements)
+  end
+
+  def current_month_period
+    @current_month_period ||= Period.current.previous
+  end
+
+  def period_range
+    @period_range ||= (current_month_period.advance(months: -24)..current_month_period)
   end
 end
