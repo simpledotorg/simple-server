8501167c6111632cfa5ca792b2afdd2b7768b836
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-03-01 18:57:56 +0530

    Undo ScheduleExperimentReminder changes

--- app/jobs/appointment_notification/schedule_experiment_reminders.rb
+++ app/jobs/appointment_notification/schedule_experiment_reminders.rb
@@ -15,22 +15,22 @@ class AppointmentNotification::ScheduleExperimentReminders < ApplicationJob
   def perform
     return unless Flipper.enabled?(:experiment)
 
-    reminders = ExperimentalAppointmentReminder.due_today
+    notifications = Notification.due_today
     next_messaging_time = Communication.next_messaging_time
 
     logger.info "scheduling #{notifications.count} notifications that are due with next_messaging_time=#{next_messaging_time}"
-    reminders.each do |reminder|
-      reminder.status_scheduled!
-      AppointmentNotification::Worker.perform_at(next_messaging_time, reminder.id)
+    notifications.each do |notification|
+      notification.status_scheduled!
+      AppointmentNotification::Worker.perform_at(next_messaging_time, notification.id)
     rescue => e
-      Sentry.capture_message("Scheduling reminder for experiment failed",
+      Sentry.capture_message("Scheduling notification for experiment failed",
         extra: {
-          reminder: reminder.id,
+          notification: notification.id,
           next_messaging_time: next_messaging_time,
           exception: e
         },
-        tags: {type: "reminders"})
+        tags: {type: "notifications"})
     end
-    logger.info "scheduling experiment reminders complete"
+    logger.info "scheduling experiment notifications complete"
   end
 end
