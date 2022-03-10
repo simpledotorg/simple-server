af4293e6ca843bd0f3dfe826452798769ac4eab6
Hari Mohanraj <hari.mohanraj89@gmail.com>
2022-03-10 16:13:48 +0400

    standardrb

--- lib/tasks/refresh_region_names.rb
+++ lib/tasks/refresh_region_names.rb
@@ -4,38 +4,38 @@ class RefreshRegionNames
 
     ActiveRecord::Base.transaction do
       # States
-      states = Region.state_regions;
-      new_state_names = Seed::FakeNames.instance.states.sample(states.count);
+      states = Region.state_regions
+      new_state_names = Seed::FakeNames.instance.states.sample(states.count)
 
       states.zip(new_state_names).each do |state, new_name|
         state.slug = nil
         state.reparent_to = state.organization_region
         state.update!(name: new_name)
-      end;
+      end
 
       # Districts
-      districts = Region.district_regions;
-      new_district_names = Seed::FakeNames.instance.districts.sample(districts.count);
+      districts = Region.district_regions
+      new_district_names = Seed::FakeNames.instance.districts.sample(districts.count)
 
       districts.zip(new_district_names).each do |district, new_name|
         district.slug = nil
         district.reparent_to = district.state_region
         district.update!(name: new_name)
         district.source.update!(name: new_name)
-      end;
+      end
 
       # Blocks
-      blocks = Region.block_regions;
-      new_block_names = Seed::FakeNames.instance.blocks.sample(blocks.count);
+      blocks = Region.block_regions
+      new_block_names = Seed::FakeNames.instance.blocks.sample(blocks.count)
 
       blocks.zip(new_block_names).each do |block, new_name|
         block.slug = nil
         block.reparent_to = block.district_region
         block.update!(name: new_name)
-      end;
+      end
 
       # Facilities
-      facilities = Region.facility_regions;
+      facilities = Region.facility_regions
 
       facilities.each do |facility|
         village = Seed::FakeNames.instance.village
@@ -47,7 +47,7 @@ class RefreshRegionNames
           name: "#{facility.source.facility_type} #{village}",
           zone: facility.block_region.name
         )
-      end;
+      end
     end
 
     Rails.cache.clear
