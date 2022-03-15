552b1c9e5e4e47df2741ac90a09a5073c0c153e7
Claudio Vallejo <hola@claudiovallejo.mx>
2022-03-15 15:04:21 -0400

    Refactored progress_tab_helper.rb

--- app/helpers/progress_tab_helper.rb
+++ app/helpers/progress_tab_helper.rb
@@ -1,50 +1,50 @@
 module ProgressTabHelper
+  GOAL_MULTIPLIERS = [2, 2, 2.5]
+  STARTING_GOAL = 25
+  TOTAL_BADGES = 5
+
   def create_badge_array(current_value)
     # Achievement badges are generated dynamically and follow the following infinite sequence where "S" is the starting badge value 
     # S x 2 x 2 x 2.5 x 2 x 2 x 2.5 x 2 x 2 x 2.5 ...
-    # This will always return an array with 5 badges, where the last badge will always be incomplete
-    # If the current_value is less than the fifth smallest goal (5,000 if starting_goal = 25), the function will return more than one incomplete badge
-    goal_multipliers = [2, 2, 2.5]
-    starting_goal = 25
-    current_goal = starting_goal 
+    current_goal = STARTING_GOAL 
     multiplier_index = 0
     badges = []
 
     while current_value >= current_goal
-      add_new_badge(current_goal, badges, true)
-      update_current_goal(current_goal, multiplier_index, goal_multipliers)
-      set_multiplier_index(multiplier_index, goal_multipliers)
+      add_badges_to_array(current_goal, true, badges)
+      current_goal = set_current_goal(current_goal, multiplier_index)
+      multiplier_index = set_multiplier_index(multiplier_index)
     end
 
-    if badges.length < 5
-      (5 - badges.length).times {
-        add_new_badge(current_goal, badges, false)
-        update_current_goal(current_goal, multiplier_index, goal_multipliers)
-        set_multiplier_index(multiplier_index, goal_multipliers)
+    if badges.length < TOTAL_BADGES 
+      (TOTAL_BADGES - badges.length).times {
+        add_badges_to_array(current_goal, false, badges)
+        current_goal = set_current_goal(current_goal, multiplier_index)
+        multiplier_index = set_multiplier_index(multiplier_index)
       }
     else
-      add_new_badge(current_goal, badges, false)
+      add_badges_to_array(current_goal, false, badges)
     end
 
-    badges.last(5)
+    badges.last(TOTAL_BADGES)
   end
 
-  def update_current_goal(current_goal, multiplier_index, goal_multipliers)
-    current_goal = (current_goal * goal_multipliers[multiplier_index]).to_i
+  def add_badges_to_array(current_goal, is_goal_completed, badge_array)
+    badge_array.push({
+      "goal_value" => current_goal,
+      "is_goal_completed" => is_goal_completed 
+    })
   end
 
-  def set_multiplier_index(multiplier_index, goal_multipliers)
-    if multiplier_index == goal_multipliers.length - 1
+  def set_current_goal(current_goal, multiplier_index)
+    return (current_goal * GOAL_MULTIPLIERS[multiplier_index]).to_i
+  end
+
+  def set_multiplier_index(multiplier_index)
+    if multiplier_index == GOAL_MULTIPLIERS.length - 1
       multiplier_index = 0
     else
       multiplier_index += 1
     end
-  end
-
-  def add_new_badge(current_goal, badges, is_goal_completed)
-    badges.push({
-      "goal_value" => current_goal.to_i,
-      "is_goal_completed" => is_goal_completed
-    })
   end
 end
