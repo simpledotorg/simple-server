8a45ac2e606b2a6290d5588fbe3f6946fff1dc68
Claudio Vallejo <hola@claudiovallejo.mx>
2022-03-15 13:49:46 -0400

    Added new achievements card; Fixed dropdown font size; Fixed create_badge_array function

--- app/controllers/api/v3/analytics/user_analytics_controller.rb
+++ app/controllers/api/v3/analytics/user_analytics_controller.rb
@@ -11,6 +11,10 @@ class Api::V3::Analytics::UserAnalyticsController < Api::V3::AnalyticsController
     @user_analytics = UserAnalyticsPresenter.new(current_facility)
     @achievements = Reports::FacilityProgressAchievementService.new(current_facility)
     @service = Reports::FacilityProgressService.new(current_facility, @period)
+    @total_follow_ups_dimension = Reports::FacilityProgressDimension.new(:follow_ups, diagnosis: :all, gender: :all)
+    @total_registrations_dimension = Reports::FacilityProgressDimension.new(:registrations, diagnosis: :all, gender: :all)
+    @total_follow_ups = Reports::MonthlyProgressComponent.new(@total_follow_ups_dimension, service: @service).total_count
+    @total_registrations = Reports::MonthlyProgressComponent.new(@total_registrations_dimension, service: @service).total_count
     if Flipper.enabled?(:new_progress_tab)
       @period_reports_data = Reports::ReportsFakeFacilityProgressService.new(@current_facility.name).period_reports
       @hypertension_reports_data = Reports::ReportsFakeFacilityProgressService.new(@current_facility.name).hypertension_reports
--- app/helpers/progress_tab_helper.rb
+++ app/helpers/progress_tab_helper.rb
@@ -1,14 +1,15 @@
 module ProgressTabHelper
   def create_badge_array(current_value)
-    # Badge goals: 25, 50, 100, 250, 500, 1000, 2500...
+    # Achievement badges are generated dynamically and follow the following infinite sequence where "S" is the starting badge value 
+    # S x 2 x 2 x 2.5 x 2 x 2 x 2.5 x 2 x 2 x 2.5 ...
     goal_multipliers = [2, 2, 2.5]
     starting_goal = 25
-    current_goal = starting_goal
+    current_goal = starting_goal 
     multiplier_index = 0
 
     badges = []
 
-    while current_value > current_goal
+    while current_value >= current_goal
       badges.push({
         "goal_value" => current_goal,
         "is_goal_completed" => true
@@ -22,10 +23,26 @@ module ProgressTabHelper
       end
     end
 
-    badges.push({
-      "goal_value" => current_goal.to_i,
-      "is_goal_completed" => false
-    })
+    if badges.length < 5
+      (5 - badges.length).times {
+        badges.push({
+          "goal_value" => current_goal.to_i,
+          "is_goal_completed" => false
+        })
+
+        current_goal = (current_goal * goal_multipliers[multiplier_index]).to_i
+        if multiplier_index == goal_multipliers.length - 1
+          multiplier_index = 0
+        else
+          multiplier_index += 1
+        end
+      }
+    else
+      badges.push({
+        "goal_value" => current_goal.to_i,
+        "is_goal_completed" => false
+      })
+    end
 
     badges.last(5)
   end
