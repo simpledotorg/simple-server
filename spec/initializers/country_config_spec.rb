64a1373bb2d5e60351d0837100620418dce8ca24
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-03-23 15:29:32 +0530

    Make appointment reminders channel required in country config

--- spec/initializers/country_config_spec.rb
+++ spec/initializers/country_config_spec.rb
@@ -3,11 +3,12 @@ require "rails_helper"
 RSpec.describe CountryConfig do
   describe "Messaging service" do
     it "has valid messaging channels in country config" do
-      expect(CountryConfig::CONFIGS
-               .values
-               .pluck(:appointment_reminders_channel)
-               .compact
-               .all?(&:constantize)).to eq true
+      expect(
+        CountryConfig::CONFIGS
+          .values
+          .pluck(:appointment_reminders_channel)
+          .all?(&:constantize)
+      ).to eq true
     end
   end
 end
