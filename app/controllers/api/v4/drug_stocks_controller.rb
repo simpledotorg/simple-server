cbb33c86724565feb31a4161a6e04c006bd73d6f
Hari Mohanraj <hari.mohanraj89@gmail.com>
2022-03-24 23:43:35 +0400

    More sad path controller specs

--- app/controllers/api/v4/drug_stocks_controller.rb
+++ app/controllers/api/v4/drug_stocks_controller.rb
@@ -2,17 +2,16 @@ class Api::V4::DrugStocksController < APIController
   def index
     if for_end_of_month.nil?
       render status: 400, json: {errors: ["valid `date` must be provided - eg. 2021-10-29"]}
+      return
     end
 
     @drug_stocks = DrugStock.latest_for_facilities([current_facility], for_end_of_month)
-    return head :not_found if drug_stocks.empty?
+    return head :not_found if @drug_stocks.empty?
   end
 
   private
 
   def for_end_of_month
-    params.require(:date)
-
     @for_end_of_month ||= Date.parse(params[:date]).end_of_month
   rescue Date::Error, TypeError
     nil
--- spec/controllers/api/v4/drug_stocks_controller_spec.rb
+++ spec/controllers/api/v4/drug_stocks_controller_spec.rb
@@ -5,16 +5,11 @@ RSpec.describe Api::V4::DrugStocksController, type: :controller do
   let(:request_facility_group) { request_user.facility.facility_group }
   let(:request_facility) { create(:facility, facility_group: request_facility_group) }
 
-  before :each do
-    request.env["X_USER_ID"] = request_user.id
-    request.env["X_FACILITY_ID"] = request_facility.id
-    request.env["HTTP_AUTHORIZATION"] = "Bearer #{request_user.access_token}"
-  end
-
   describe "user api authentication" do
     it "does not allow sync_from_user requests to the controller with invalid user_id and access_token" do
-      request.env["X_USER_ID"] = "invalid user id"
-      request.env["HTTP_AUTHORIZATION"] = "invalid access token"
+      request.headers["X-User-Id"] = "invalid user id"
+      request.headers["X-Facility-Id"] = request_facility.id
+      request.headers["Authorization"] = "invalid access token"
 
       get :index, params: {date: "2020-10-30"}
 
@@ -23,6 +18,26 @@ RSpec.describe Api::V4::DrugStocksController, type: :controller do
   end
 
   describe "validations" do
-    it "returns"
+    it "returns a bad request if date is missing" do
+      request.headers["X-User-Id"] = request_user.id
+      request.headers["X-Facility-Id"] = request_facility.id
+      request.headers["Authorization"] = "Bearer #{request_user.access_token}"
+
+      get :index
+
+      expect(response.status).to eq(400)
+      expect(response.body).to include("date")
+    end
+
+    it "returns a bad request if date is invalid" do
+      request.headers["X-User-Id"] = request_user.id
+      request.headers["X-Facility-Id"] = request_facility.id
+      request.headers["Authorization"] = "Bearer #{request_user.access_token}"
+
+      get :index, params: {date: "invalid date"}
+
+      expect(response.status).to eq(400)
+      expect(response.body).to include("date")
+    end
   end
 end
