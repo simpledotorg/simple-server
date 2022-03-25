dae20ca6cfffbf530c0078219dd6df83288f1a99
vkrm <mail@vkrm.is>
2022-03-25 16:55:21 +0530

    Add model specs for Configuration

---
+++ spec/models/configuration_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe Configuration, type: :model do
+  context "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:value) }
+    it_behaves_like "a record that validates device timestamps"
+  end
+
+  context "Behavior" do
+    it_behaves_like "a record that is deletable"
+  end
+end
