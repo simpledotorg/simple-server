5883b46b76161b9eaf45a439a9a30ad1300c7469
Govind Krishna Joshi <govind@nilenso.com>
2022-03-30 12:48:07 +0530

    Move patient_states relationship to patient_reportable
    Co-authored-by: Priyanga P Kini <priyangapkini99@gmail.com>

--- app/models/concerns/patient_reportable.rb
+++ app/models/concerns/patient_reportable.rb
@@ -3,6 +3,8 @@ module PatientReportable
   LTFU_TIME = 12.months
 
   included do
+    has_many :patient_states, class_name: "Reports::PatientState"
+
     delegate :sanitize_sql, to: ActiveRecord::Base
 
     scope :with_diabetes, -> { joins(:medical_history).merge(MedicalHistory.diabetes_yes).distinct }
@@ -11,13 +13,14 @@ module PatientReportable
 
     scope :ltfu_as_of, ->(time) do
       left_outer_joins(:patient_states)
-        .merge(Reports::PatientState.where(month_date: time.beginning_of_month, htn_care_state: "lost_to_follow_up"))
+        .merge(Reports::PatientState.by_month_date(time.beginning_of_month))
+        .merge(Reports::PatientState.htn_care_state_lost_to_follow_up)
         .distinct(:patient_id)
     end
 
     scope :not_ltfu_as_of, ->(time) do
       left_outer_joins(:patient_states)
-        .merge(Reports::PatientState.where(month_date: time.beginning_of_month))
+        .merge(Reports::PatientState.by_month_date(time.beginning_of_month))
         .merge(Reports::PatientState.where.not(htn_care_state: "lost_to_follow_up"))
         .distinct(:patient_id)
     end
