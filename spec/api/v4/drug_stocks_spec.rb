cf3191e587c90894049d6aca1f7e304a1b658e30
Hari Mohanraj <hari.mohanraj89@gmail.com>
2022-03-30 16:15:23 +0400

    Passing specs

--- spec/api/v4/drug_stocks_spec.rb
+++ spec/api/v4/drug_stocks_spec.rb
@@ -7,21 +7,22 @@ describe "Drug Stocks v4 API", swagger_doc: "v4/swagger.json" do
       security [access_token: [], user_id: [], facility_id: []]
       parameter name: "HTTP_X_USER_ID", in: :header, type: :uuid
       parameter name: "HTTP_X_FACILITY_ID", in: :header, type: :uuid
-      parameter name: :request_body, in: :body, schema: Api::V4::Schema.drug_stocks_request, description: "Any date in the requested month - eg. 2021-10-29"
+      parameter name: :date, in: :query, type: :date, description: "Any date in the requested month - eg. 2021-10-29"
 
       response "200", "Drug stocks are returned" do
-        let(:request_body) { {date: "2021-10-29"} }
+        let(:date) { "2021-10-29" }
         let(:request_user) { create(:user) }
         let(:request_facility) { create(:facility, facility_group: request_user.facility.facility_group) }
         let(:HTTP_X_USER_ID) { request_user.id }
         let(:HTTP_X_FACILITY_ID) { request_facility.id }
         let(:Authorization) { "Bearer #{request_user.access_token}" }
+        let!(:drug_stock) { create(:drug_stock, facility: request_facility, for_end_of_month: Date.new(2021, 10, 31)) }
 
         run_test!
       end
 
       response "404", "No drug stock report has been submitted for the specified month" do
-        let(:request_body) { {date: "2021-08-30"} }
+        let(:date) { "2021-08-30" }
         let(:request_user) { create(:user) }
         let(:request_facility) { create(:facility, facility_group: request_user.facility.facility_group) }
         let(:HTTP_X_USER_ID) { request_user.id }
