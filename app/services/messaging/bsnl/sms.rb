1a97acaf85304291edef1b86cef82fb20a6dbad2
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-04-01 18:50:12 +0530

    Add Bsnl::send_message and model for DltTemplate

---
+++ app/services/messaging/bsnl/sms.rb
@@ -0,0 +1,27 @@
+class Messaging::Bsnl::Sms < Messaging::Channel
+  # variable_content: A map that takes the values to be interpolated
+  # in the templates. For example: { facility_name: "Facility A", patient_name: "Patient" }
+  # TODO: This should also create communications and return them.
+  def send_message(recipient_number:, dlt_template_name:, variable_content:)
+    template = Messaging::Bsnl::DltTemplate.new(dlt_template_name)
+    template.check_approved
+    variables = template.massaged_variable_content(variable_content)
+
+    Messaging::Bsnl::Api.new.send_sms(
+      recipient_number: recipient_number,
+      dlt_template_id: template.id,
+      key_values: variables.map { |k, v| { "Key" => k.to_s, "Value" => v } }
+    ).then { |response| handle_api_errors(response, template, variables) }
+  end
+
+  private
+
+  def handle_api_errors(response, template, variable_content)
+    error = response["Error"]
+    if error.present?
+      raise Messaging::Bsnl::Error.new("#{error} Error on template #{template.name} with content #{variable_content}")
+    else
+      response
+    end
+  end
+end
---
+++ spec/services/messaging/bsnl/sms_spec.rb
@@ -0,0 +1,48 @@
+require "rspec"
+
+RSpec.describe Messaging::Bsnl::Sms do
+  describe "#send_message" do
+    it "passed variables massaged_variable_content to the API" do
+      mock_api = double("BsnlApiDouble")
+      allow(Messaging::Bsnl::Api).to receive(:new).and_return(mock_api)
+      allow(mock_api).to receive(:send_sms)
+      mock_template = double("BsnlDltTemplate")
+      allow(Messaging::Bsnl::DltTemplate).to receive(:new).and_return(mock_template)
+      allow(mock_template).to receive(:id).and_return("123")
+      allow(mock_template).to receive(:massaged_variable_content).and_return({ "a" => "hash" })
+      allow(mock_api).to receive(:send_sms).and_return({})
+
+      expect(mock_api).to receive(:send_sms).with(
+        recipient_number: "+91123123",
+        dlt_template_id: "123",
+        key_values: [{ "Key" => "a", "Value" => "hash" }]
+      )
+
+      described_class.send_message(
+        recipient_number: "+91123123",
+        dlt_template_name: "notifications.set100.basic",
+        variable_content: {}
+      )
+    end
+
+    it "raises any errors received from BSNL as an exception" do
+      mock_api = double("BsnlApiDouble")
+      allow(Messaging::Bsnl::Api).to receive(:new).and_return(mock_api)
+      allow(mock_api).to receive(:send_sms)
+      mock_template = double("BsnlDltTemplate")
+      allow(Messaging::Bsnl::DltTemplate).to receive(:new).and_return(mock_template)
+      allow(mock_template).to receive(:id).and_return("123")
+      allow(mock_template).to receive(:name).and_return("a.template.name")
+      allow(mock_template).to receive(:massaged_variable_content).and_return({ "a" => "hash" })
+      allow(mock_api).to receive(:send_sms).and_return({"Error" => "An error happened."})
+
+      expect {
+        described_class.send_message(
+          recipient_number: "+91123123",
+          dlt_template_name: "notifications.set100.basic",
+          variable_content: {}
+        )
+      }.to raise_error(Messaging::Bsnl::Error, 'An error happened. Error on template a.template.name with content {"a"=>"hash"}')
+    end
+  end
+end
