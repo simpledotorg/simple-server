379d50d08b36002c7c2096708c6013a0c870181d
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-04-01 20:13:04 +0530

    Add status in BSNL templates

--- lib/tasks/scripts/get_bsnl_templates.rb
+++ lib/tasks/scripts/get_bsnl_templates.rb
@@ -30,7 +30,7 @@ class GetBsnlTemplateDetails
     config = template_details.to_h do |template|
       [
         template["Template_Name"],
-        template.slice("Template_Id", "Template_Keys", "Non_Variable_Text_Length", "Max_Length_Permitted")
+        template.slice("Template_Id", "Template_Keys", "Non_Variable_Text_Length", "Max_Length_Permitted", "Template_Status")
       ]
     end
 
