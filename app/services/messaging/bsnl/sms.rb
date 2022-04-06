bbd73b74260ff4a3ea41a438d75aec92d9d7aaa6
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2022-04-06 16:11:07 +0530

    Add BSNL delivery detail (#3653)
    Co-authored-by: Vikram Chintalapati <mail@vkrm.is>

--- app/services/messaging/bsnl/sms.rb
+++ app/services/messaging/bsnl/sms.rb
@@ -5,27 +5,51 @@ class Messaging::Bsnl::Sms < Messaging::Channel
 
   # variable_content: A map that takes the values to be interpolated
   # in the templates. For example: { facility_name: "Facility A", patient_name: "Patient" }
-  # TODO: This should also create communications and return them.
-  def send_message(recipient_number:, dlt_template_name:, variable_content:)
-    template = Messaging::Bsnl::DltTemplate.new(dlt_template_name)
-    template.check_approved
+  def send_message(recipient_number:, dlt_template_name:, variable_content:, &with_communication_do)
+    template = approved_template(dlt_template_name)
+
+    track_metrics do
+      create_communication(
+        recipient_number,
+        send_bsnl_message(recipient_number, template, variable_content),
+        template.id,
+        &with_communication_do
+      )
+    end
+  end
+
+  private
+
+  def approved_template(template_name)
+    Messaging::Bsnl::DltTemplate.new(template_name).tap(&:check_approved)
+  end
+
+  def send_bsnl_message(recipient_number, template, variable_content)
     variables = template.sanitised_variable_content(variable_content)
 
     Messaging::Bsnl::Api.new.send_sms(
       recipient_number: recipient_number,
       dlt_template: template,
       key_values: variables
-    ).then { |response| handle_api_errors(response, template, variables) }
+    ).tap { |response| handle_api_errors(response, template, variables) }
   end
-
-  private
 
   def handle_api_errors(response, template, variables)
     error = response["Error"]
     if error.present?
       raise Messaging::Bsnl::Error.new("#{error} Error on template #{template.name} with content #{variables}")
-    else
-      response
+    end
+  end
+
+  def create_communication(recipient_number, response, template_id, &with_communication_do)
+    ActiveRecord::Base.transaction do
+      BsnlDeliveryDetail.create_with_communication!(
+        message_id: response["Message_Id"],
+        recipient_number: recipient_number,
+        dlt_template_id: template_id
+      ).tap do |communication|
+        with_communication_do&.call(communication)
+      end
     end
   end
 end
