2fa32926d697c84cde5ac6cd33b3400de06b13c6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-04-07 17:58:30 +0530

    Add error reasons to Messaging:Error

--- app/services/messaging/bsnl/error.rb
+++ app/services/messaging/bsnl/error.rb
@@ -1,2 +1,13 @@
 class Messaging::Bsnl::Error < Messaging::Error
+  ERROR_CODE_REASONS = {"Invalid Mobile Number" => :invalid_phone_number}
+
+  def initialize(message)
+    @message = "Error while calling BSNL API: #{message}"
+    @reason = error_reason(message)
+  end
+
+  def error_reason(message)
+    known_error_message = ERROR_CODE_REASONS.keys.grep(/#{message}/).first
+    ERROR_CODE_REASONS[known_error_message]
+  end
 end
