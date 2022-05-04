44e731d43a2493dab97f28539e63d111dec41bcb
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-05-04 13:57:34 +0530

    Fix lock options

--- app/jobs/patient_list_download_job.rb
+++ app/jobs/patient_list_download_job.rb
@@ -1,12 +1,10 @@
 class PatientListDownloadJob
   include Sidekiq::Worker
 
-  sidekiq_options lock: :while_executing,
-                  lock_timeout: 1,
-                  lock_ttl: 15.minutes.to_i,
-                  on_conflict: {
-                    server: :reject
-                  }
+  sidekiq_options lock: :until_executed,
+                  lock_timeout: 15.minutes,
+                  lock_ttl: 15.minutes,
+                  on_conflict: :reject
 
   def perform(recipient_email, model_type, params)
     case model_type
@@ -25,7 +23,6 @@ class PatientListDownloadJob
     exporter = PatientsWithHistoryExporter
     patients_csv = exporter.csv(patients)
 
-    sleep 10.seconds
     PatientListDownloadMailer.patient_list(recipient_email, model_type, model_name, patients_csv).deliver_now
   end
 end
