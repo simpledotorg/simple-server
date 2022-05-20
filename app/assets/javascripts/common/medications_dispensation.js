cea3f2fd7b0e71ef9805baca88061dd22eaaeb1d
Daniel Burka <danielburka@gmail.com>
2022-05-20 12:40:06 +0100

    Added more months, formatted better on mobile, removed ticks (#3742)
    * Added more months, formatted better on mobile, removed ticks
    
    * Show only 6 months, not 7
    
    Co-authored-by: Hari Mohanraj <hari.mohanraj89@gmail.com>

--- app/assets/javascripts/common/medications_dispensation.js
+++ app/assets/javascripts/common/medications_dispensation.js
@@ -3,117 +3,133 @@ MedicationsDispensationGraph = function () {
 
   this.listen = () => {
     this.initializeGraph();
-  }
+  };
 
   this.getGraphData = () => {
-    return JSON.parse(reports.getChartDataNode().textContent)["medications_dispensation"];
-  }
+    return JSON.parse(reports.getChartDataNode().textContent)[
+      "medications_dispensation"
+    ];
+  };
 
   this.getGraphPeriods = () => {
     let firstBucketData = Object.values(this.getGraphData())[0];
     return Object.keys(firstBucketData["counts"]);
-  }
+  };
 
   this.initializeGraph = () => {
     const graphData = this.getGraphData();
     const graphConfig = reports.createBaseGraphConfig();
-    let datasets = Object.keys(graphData).map(function(bucket, index){
+    let datasets = Object.keys(graphData).map(function (bucket, index) {
       return {
         label: bucket,
         data: Object.values(graphData[bucket]["percentages"]),
         numerators: graphData[bucket]["counts"],
         denominators: graphData[bucket]["totals"],
         borderColor: graphData[bucket]["color"],
-        backgroundColor: graphData[bucket]["color"]
-      }
-    })
+        backgroundColor: graphData[bucket]["color"],
+      };
+    });
 
     graphConfig.plugins = [ChartDataLabels];
-    graphConfig.type = 'bar';
+    graphConfig.type = "bar";
     graphConfig.data = {
       labels: this.getGraphPeriods(),
-      datasets: datasets
+      datasets: datasets,
     };
 
     graphConfig.options.scales = {
-      xAxes: [{
-        stacked: false,
-        display: true,
-        gridLines: {
-          display: false,
-          drawBorder: true,
-        },
-        ticks: {
-          autoSkip: false,
-          fontColor: reports.darkGreyColor,
-          fontSize: 12,
-          fontFamily: "Roboto Condensed",
-          padding: 0,
-          min: 0,
-          beginAtZero: true,
-        },
-      }],
-      yAxes: [{
-        stacked: false,
-        display: true,
-        minBarLength: 4,
-        gridLines: {
+      xAxes: [
+        {
+          stacked: false,
           display: true,
-          drawBorder: false,
+          gridLines: {
+            display: false,
+            drawBorder: true,
+          },
+          ticks: {
+            autoSkip: false,
+            fontColor: reports.darkGreyColor,
+            fontSize: 12,
+            fontFamily: "Roboto Condensed",
+            padding: 0,
+            min: 0,
+            beginAtZero: true,
+          },
         },
-        ticks: {
-          autoSkip: false,
-          fontColor: reports.darkGreyColor,
-          fontSize: 12,
-          fontFamily: "Roboto Condensed",
-          padding: 8,
-          min: 0,
-          beginAtZero: true,
-          stepSize: 25,
-          max: 100,
+      ],
+      yAxes: [
+        {
+          stacked: false,
+          display: true,
+          minBarLength: 4,
+          gridLines: {
+            display: true,
+            drawBorder: false,
+          },
+          ticks: {
+            display: false,
+            autoSkip: false,
+            fontColor: reports.darkGreyColor,
+            fontSize: 12,
+            fontFamily: "Roboto Condensed",
+            padding: 8,
+            min: 0,
+            beginAtZero: true,
+            stepSize: 25,
+            max: 100,
+          },
         },
-      }]
+      ],
     };
 
     graphConfig.options.plugins = {
       datalabels: {
-        align: 'end',
-        color: 'black',
-        anchor: 'end',
+        align: "end",
+        color: "black",
+        anchor: "end",
         offset: 1,
         font: {
-          family:'Roboto Condensed',
+          family: "Roboto Condensed",
           size: 12,
         },
-        formatter: function(value) {
-            return value + '%';
-        }
-      }
-    }
+        formatter: function (value) {
+          return value + "%";
+        },
+      },
+    };
 
     graphConfig.options.tooltips = {
       displayColors: false,
-      xAlign: 'center',
-      yAlign: 'top',
+      xAlign: "center",
+      yAlign: "top",
       xPadding: 6,
       yPadding: 6,
       caretSize: 3,
       caretPadding: 1,
       callbacks: {
-        title: function() {
-          return ""
+        title: function () {
+          return "";
         },
-        label: function(tooltipItem, data) {
-            let numerators = Object.values(data.datasets[tooltipItem.datasetIndex].numerators);
-            let denominators = Object.values(data.datasets[tooltipItem.datasetIndex].denominators);
-          return reports.formatNumberWithCommas(numerators[tooltipItem.index]) + " of " + reports.formatNumberWithCommas(denominators[tooltipItem.index]) + " follow-up patients";
+        label: function (tooltipItem, data) {
+          let numerators = Object.values(
+            data.datasets[tooltipItem.datasetIndex].numerators
+          );
+          let denominators = Object.values(
+            data.datasets[tooltipItem.datasetIndex].denominators
+          );
+          return (
+            reports.formatNumberWithCommas(numerators[tooltipItem.index]) +
+            " of " +
+            reports.formatNumberWithCommas(denominators[tooltipItem.index]) +
+            " follow-up patients"
+          );
         },
-      }
-    }
+      },
+    };
 
     const graphCanvas = document.getElementById("MedicationsDispensation");
     if (graphCanvas) {
       new Chart(graphCanvas.getContext("2d"), graphConfig);
     }
-  }
-}
+  };
+};
