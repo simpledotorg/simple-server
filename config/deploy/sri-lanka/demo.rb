727eb612e78503a24860f474a2a014edba8bf240
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-05-26 17:11:26 +0530

    Update IPs and remove downtime notice

--- config/deploy/sri-lanka/demo.rb
+++ config/deploy/sri-lanka/demo.rb
@@ -1,2 +1,2 @@
-server "ec2-15-206-165-200.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron seed_data]
-server "ec2-15-206-194-112.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
+server "ec2-65-2-161-138.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron seed_data]
+server "ec2-13-233-30-147.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
