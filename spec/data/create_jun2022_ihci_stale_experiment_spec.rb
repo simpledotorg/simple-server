b58d27e57afe89f04f8c8a1acf1307594afba1a1
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-05-31 20:07:11 +0530

    Fix spec

--- spec/data/create_jun2022_ihci_stale_experiment_spec.rb
+++ spec/data/create_jun2022_ihci_stale_experiment_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
-require Rails.root.join("db", "data", "20220510130045_create_jun2022_stale_ihci_experiment")
+require Rails.root.join("db", "data", "20220531134121_create_jun2022_ihci_stale_experiment")
 
-RSpec.describe CreateMay2022IhciExperiment do
+RSpec.describe CreateJun2022IhciStaleExperiment do
   it "creates the stale experiment" do
     allow(CountryConfig).to receive(:current_country?).with("India").and_return true
     stub_const("SIMPLE_SERVER_ENV", "production")
@@ -11,7 +11,7 @@ RSpec.describe CreateMay2022IhciExperiment do
     experiment = Experimentation::StalePatientExperiment.first
 
     expect(experiment).to be_stale_patients
-    expect(experiment.name).to eq("Stale Patient May 2022")
+    expect(experiment.name).to eq("Stale Patient Jun 2022")
     expect(experiment.start_time.to_date).to eq(Date.parse("Jun 1, 2022").to_date)
     expect(experiment.end_time.to_date).to eq(Date.parse("Jun 30, 2022").to_date)
     expect(experiment.max_patients_per_day).to eq(15000)
