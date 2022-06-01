7dedc71d480a92d69ce38442b9a0889de8335f98
Priyanga P Kini <priyangapkini99@gmail.com>
2022-06-01 14:41:01 +0530

    Use latest medical history to calculate the `latest_appointments_per_patient_per_month`

--- db/views/reporting_facility_appointment_scheduled_days_v04.sql
+++ db/views/reporting_facility_appointment_scheduled_days_v04.sql
@@ -1,17 +1,23 @@
-WITH latest_appointments_per_patient_per_month AS (
+WITH latest_medical_histories AS (
+    SELECT DISTINCT ON (patient_id) mh.*
+    FROM medical_histories mh
+    WHERE  mh.deleted_at IS NULL
+    ORDER BY patient_id, mh.device_created_at DESC
+),
+ latest_appointments_per_patient_per_month AS (
     SELECT DISTINCT ON (patient_id, month_date) a.*,
-        mh.hypertension,
-        mh.diabetes,
+        lmh.hypertension,
+        lmh.diabetes,
         to_char(a.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')), 'YYYY-MM-01')::date month_date
     FROM appointments a
     INNER JOIN patients p ON p.id = a.patient_id
-    INNER JOIN medical_histories mh ON mh.patient_id = a.patient_id
+    INNER JOIN latest_medical_histories lmh ON lmh.patient_id = a.patient_id
     WHERE a.scheduled_date >= date_trunc('day', a.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')))
       AND a.device_created_at >= date_trunc('month', (now() AT TIME ZONE 'UTC') - INTERVAL '6 months')
       AND date_trunc('month', a.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')))
           > date_trunc('month', p.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')))
       AND p.deleted_at IS NULL and a.deleted_at IS NULL
-      AND (mh.hypertension = 'yes'  OR mh.diabetes = 'yes')
+      AND (lmh.hypertension = 'yes'  OR lmh.diabetes = 'yes')
     ORDER BY a.patient_id, month_date, a.device_created_at desc
 ),
  scheduled_days_distribution AS (
