6e82b3cc069d8e612060af68073366c3ce0b2280
Priyanga P Kini <priyangapkini99@gmail.com>
2022-06-22 14:31:25 +0530

    Add lost to follow up graph
    - Use view components instead of partials

---
+++ app/components/dashboard/lost_to_follow_up_component.rb
@@ -0,0 +1,21 @@
+class Dashboard::LostToFollowUpComponent < ApplicationComponent
+  attr_reader :region, :data, :period
+
+  def initialize(region:, data:, period:)
+    @region = region
+    @data = data
+    @period = period
+  end
+
+  def cumulative_assigned_patients
+    data.dig(:ltfu_trend, :cumulative_assigned_patients, period)
+  end
+
+  def ltfu_patient_rate
+    data.dig(:ltfu_trend, :ltfu_patients_rate, period)
+  end
+
+  def current_ltfu_patients
+    data.dig(:ltfu_trend, :ltfu_patients, period)
+  end
+end
