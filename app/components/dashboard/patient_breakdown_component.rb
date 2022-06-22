3208f2994f739d58f342256c956c79bfd579861f
Govind Krishna Joshi <govind@nilenso.com>
2022-06-22 18:15:46 +0530

    Make patient breakdown component generic of diagnosis

--- app/components/dashboard/patient_breakdown_component.rb
+++ app/components/dashboard/patient_breakdown_component.rb
@@ -1,30 +1,10 @@
 class Dashboard::PatientBreakdownComponent < ApplicationComponent
-  attr_reader :region, :data, :period, :population_coverage_data, :curent_admin
+  attr_reader :region, :data, :period, :tooltips
 
-  def initialize(region:, data:, period:)
+  def initialize(region:, data:, period:, tooltips:)
     @region = region
     @data = data
     @period = period
-  end
-
-  # TODO: Remove dead patient count from assigned patient count
-  def total_assigned_excluding_dead_patients
-    data.dig(:cumulative_assigned_diabetes_patients, period)
-  end
-
-  def total_registered_patients
-    data.dig(:cumulative_diabetes_registrations, period)
-  end
-
-  def patients_under_care
-    data.dig(:diabetes_under_care, period)
-  end
-
-  def ltfu_patients
-    data.dig(:diabetes_ltfu_patients, period)
-  end
-
-  def dead_patients
-    data.dig(:diabetes_dead, period)
+    @tooltips = tooltips
   end
 end
