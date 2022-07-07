f0a515b8222a3e7a4c2865e7a87a51e6e68f7a08
Govind Krishna Joshi <govind@nilenso.com>
2022-07-07 12:56:05 +0530

    Use slots for tooltip component

--- app/components/dashboard/card/ltfu_toggle_component.rb
+++ app/components/dashboard/card/ltfu_toggle_component.rb
@@ -2,7 +2,6 @@ class Dashboard::Card::LtfuToggleComponent < ApplicationComponent
   attr_reader :id
   attr_reader :enabled
 
-
   def initialize(id:, enabled:)
     @id = id
     @enabled = enabled
--- app/components/dashboard/card/title_component.rb
+++ app/components/dashboard/card/title_component.rb
@@ -4,6 +4,7 @@ class Dashboard::Card::TitleComponent < ApplicationComponent
   attr_reader :tooltip_definitions
 
   renders_one :ltfu_toggle, Dashboard::Card::LtfuToggleComponent
+  renders_one :tooltip, Dashboard::Card::TooltipComponent
 
   def initialize(title:, subtitle: nil, tooltip_definitions: nil)
     @title = title
