6f171f817afefb8b47bee37f2a152bf1bac5afe5
Govind Krishna Joshi <govind@nilenso.com>
2022-07-07 13:43:09 +0530

    Use view component for bs over 200 graph

--- app/components/dashboard/diabetes/bs_over200_graph_component.rb
+++ app/components/dashboard/diabetes/bs_over200_graph_component.rb
@@ -1,10 +1,13 @@
-class Dashboard::Diabetes::RegistrationsAndFollowUpsGraphComponent < ApplicationComponent
+class Dashboard::Diabetes::BsOver200GraphComponent < ApplicationComponent
   attr_reader :region
   attr_reader :period
+  attr_reader :with_ltfu
 
-  def initialize(region:, period:)
+
+  def initialize(region:, period:, with_ltfu: false)
     @region = region
     @period = period
+    @with_ltfu = with_ltfu
   end
 
   def denominator_copy
