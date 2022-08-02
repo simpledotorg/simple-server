27c9b50972359e8ea22662e21b5ddb5ad5ce74e7
Govind Krishna Joshi <govind@nilenso.com>
2022-08-02 16:18:57 +0530

    Use view components for dashboard cards (#4125)
    Co-authored-by: Priyanga P Kini <priyangapkini99@gmail.com>

--- app/components/dashboard/diabetes/healthcare_worker_activity_component.rb
+++ app/components/dashboard/diabetes/healthcare_worker_activity_component.rb
@@ -10,4 +10,20 @@ class Dashboard::Diabetes::HealthcareWorkerActivityComponent < ApplicationCompon
     @period_range = period_range
     @current_admin = current_admin
   end
+
+  def show_user_row?(user)
+    user_registrations_count(user)&.nonzero? || user_blood_pressure_measures_count(user)&.nonzero?
+  end
+
+  def user_registrations_count(user)
+    sum_registration_counts(repository, slug: region.slug, user_id: user.id, diagnosis: :diabetes)
+  end
+
+  def user_blood_pressure_measures_count(user)
+    sum_blood_sugar_measures(repository, slug: region.slug, user_id: user.id)&.nonzero?
+  end
+
+  def blood_sugar_measures_taken_by_user_in_period(user, period)
+    repository.blood_sugar_measures_by_user.dig(region.slug, period, user.id)
+  end
 end
