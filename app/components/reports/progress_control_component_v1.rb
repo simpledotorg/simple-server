beb1757125c561595e27d9a52f2481590b61ccac
Priyanga P Kini <priyangapkini99@gmail.com>
2022-08-25 14:51:58 +0530

    Refactor progress tab code

---
+++ app/components/reports/progress_control_component_v1.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+class Reports::ProgressControlComponentV1 < ViewComponent::Base
+  include AssetsHelper
+  include ActionView::Helpers::NumberHelper
+
+  attr_reader :control_range, :repository, :current_user
+
+  def initialize(service, current_user)
+    @repository = service.control_rates_repository
+    @control_range = repository.range
+    @region = service.region
+    @current_user = current_user
+  end
+
+  def render?
+    Flipper.enabled?(:new_progress_tab_v1, current_user) || Flipper.enabled?(:new_progress_tab_v1)
+  end
+
+  def control_summary
+    controlled_count = format(controlled[control_range.last])
+    registrations = format(adjusted_patients[control_range.last])
+    label = "patient".pluralize(registrations)
+    "#{controlled_count} of #{registrations} #{label}"
+  end
+
+  def format(number)
+    number_with_delimiter(number)
+  end
+
+  def control_rates
+    repository.controlled_rates[@region.slug]
+  end
+
+  def controlled
+    repository.controlled[@region.slug]
+  end
+
+  def adjusted_patients
+    repository.adjusted_patients[@region.slug]
+  end
+
+  def period_info
+    repository.period_info(@region)
+  end
+end
