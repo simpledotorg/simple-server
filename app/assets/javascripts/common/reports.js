5d1bd12fd7421e2a7625f0d8d9ceef35f313de28
Govind krishna Joshi <govind@nilenso.com>
2022-08-31 13:48:17 +0530

    Use view components for treatment card component

--- app/assets/javascripts/common/reports.js
+++ app/assets/javascripts/common/reports.js
@@ -95,6 +95,7 @@ DashboardReports = () => {
   };
 
   const ReportsGraphConfig = {
+    // Hypertension graphs config
     controlledPatientsTrend: function(data) {
       const config = createBaseGraphConfig();
       config.data = {
@@ -394,6 +395,96 @@ DashboardReports = () => {
 
       return config;
     },
+    hypertensionVisitDetails: function(data) {
+      const config = createBaseGraphConfig();
+      config.type = "bar";
+
+      const maxBarsToDisplay = 6;
+      const barsToDisplay = Math.min(
+          Object.keys(data.controlRate).length,
+          maxBarsToDisplay
+      );
+
+      config.data = {
+        labels: Object.keys(data.controlRate).slice(-barsToDisplay),
+        datasets: [
+          {
+            label: "BP controlled",
+            backgroundColor: COLORS.mediumGreen,
+            hoverBackgroundColor: COLORS.darkGreen,
+            data: Object.values(data.controlRate).slice(-barsToDisplay),
+            type: "bar",
+          },
+          {
+            label: "BP uncontrolled",
+            backgroundColor: COLORS.mediumRed,
+            hoverBackgroundColor: COLORS.darkRed,
+            data: Object.values(data.uncontrolledRate).slice(-barsToDisplay),
+            type: "bar",
+          },
+          {
+            label: "Visit but no BP measure",
+            backgroundColor: COLORS.mediumGrey,
+            hoverBackgroundColor: COLORS.darkGrey,
+            data: Object.values(data.visitButNoBPMeasureRate).slice(
+                -barsToDisplay
+            ),
+            type: "bar",
+          },
+          {
+            label: "Missed visits",
+            backgroundColor: COLORS.mediumBlue,
+            hoverBackgroundColor: COLORS.darkBlue,
+            data: Object.values(data.missedVisitsRate).slice(-barsToDisplay),
+            type: "bar",
+          },
+        ],
+      };
+      config.options.scales = {
+        xAxes: [
+          {
+            stacked: true,
+            display: true,
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              autoSkip: false,
+              fontColor: COLORS.darkGrey,
+              fontSize: 12,
+              fontFamily: "Roboto",
+              padding: 8,
+              min: 0,
+              beginAtZero: true,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            stacked: true,
+            display: false,
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              autoSkip: false,
+              fontColor: COLORS.darkGrey,
+              fontSize: 10,
+              fontFamily: "Roboto",
+              padding: 8,
+              min: 0,
+              beginAtZero: true,
+            },
+          },
+        ],
+      };
+
+      return config;
+    },
+
+    // Diabetes graphs config
     bsBelow200PatientsTrend: function(data) {
       const config = createBaseGraphConfig();
       config.data = {
@@ -818,6 +909,8 @@ DashboardReports = () => {
 
       return config;
     },
+
+    // Common graphs config
     MedicationsDispensation: function(data) {
       const config = createBaseGraphConfig();
       const graphPeriods = Object.keys(Object.values(data)[0]["counts"])
@@ -1089,206 +1182,7 @@ Reports = function (withLtfu) {
   };
 
   this.initializeCharts = () => {
-    const data = this.getReportingData();
 
-    this.setupVisitDetailsGraph(data);
-  };
-
-  this.setupVisitDetailsGraph = (data) => {
-    const visitDetailsGraphConfig = this.createBaseGraphConfig();
-    visitDetailsGraphConfig.type = "bar";
-
-    const maxBarsToDisplay = 6;
-    const barsToDisplay = Math.min(
-      Object.keys(data.controlRate).length,
-      maxBarsToDisplay
-    );
-
-    visitDetailsGraphConfig.data = {
-      labels: Object.keys(data.controlRate).slice(-barsToDisplay),
-      datasets: [
-        {
-          label: "BP controlled",
-          backgroundColor: this.mediumGreenColor,
-          hoverBackgroundColor: this.darkGreenColor,
-          data: Object.values(data.controlRate).slice(-barsToDisplay),
-          type: "bar",
-        },
-        {
-          label: "BP uncontrolled",
-          backgroundColor: this.mediumRedColor,
-          hoverBackgroundColor: this.darkRedColor,
-          data: Object.values(data.uncontrolledRate).slice(-barsToDisplay),
-          type: "bar",
-        },
-        {
-          label: "Visit but no BP measure",
-          backgroundColor: this.mediumGreyColor,
-          hoverBackgroundColor: this.darkGreyColor,
-          data: Object.values(data.visitButNoBPMeasureRate).slice(
-            -barsToDisplay
-          ),
-          type: "bar",
-        },
-        {
-          label: "Missed visits",
-          backgroundColor: this.mediumBlueColor,
-          hoverBackgroundColor: this.darkBlueColor,
-          data: Object.values(data.missedVisitsRate).slice(-barsToDisplay),
-          type: "bar",
-        },
-      ],
-    };
-    visitDetailsGraphConfig.options.scales = {
-      xAxes: [
-        {
-          stacked: true,
-          display: true,
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          ticks: {
-            autoSkip: false,
-            fontColor: this.darkGreyColor,
-            fontSize: 12,
-            fontFamily: "Roboto",
-            padding: 8,
-            min: 0,
-            beginAtZero: true,
-          },
-        },
-      ],
-      yAxes: [
-        {
-          stacked: true,
-          display: false,
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          ticks: {
-            autoSkip: false,
-            fontColor: this.darkGreyColor,
-            fontSize: 10,
-            fontFamily: "Roboto",
-            padding: 8,
-            min: 0,
-            beginAtZero: true,
-          },
-        },
-      ],
-    };
-    visitDetailsGraphConfig.options.tooltips = {
-      mode: "x",
-      enabled: false,
-      custom: (tooltip) => {
-        let hoveredDatapoint = tooltip.dataPoints;
-        if (hoveredDatapoint)
-          populateVisitDetailsGraph(hoveredDatapoint[0].label);
-        else populateVisitDetailsGraphDefault();
-      },
-    };
-
-    const populateVisitDetailsGraph = (period) => {
-      const cardNode = document.getElementById("visit-details");
-      const missedVisitsRateNode = cardNode.querySelector(
-        "[data-missed-visits-rate]"
-      );
-      const visitButNoBPMeasureRateNode = cardNode.querySelector(
-        "[data-visit-but-no-bp-measure-rate]"
-      );
-      const uncontrolledRateNode = cardNode.querySelector(
-        "[data-uncontrolled-rate]"
-      );
-      const controlledRateNode = cardNode.querySelector(
-        "[data-controlled-rate]"
-      );
-      const missedVisitsPatientsNode = cardNode.querySelector(
-        "[data-missed-visits-patients]"
-      );
-      const visitButNoBPMeasurePatientsNode = cardNode.querySelector(
-        "[data-visit-but-no-bp-measure-patients]"
-      );
-      const uncontrolledPatientsNode = cardNode.querySelector(
-        "[data-uncontrolled-patients]"
-      );
-      const controlledPatientsNode = cardNode.querySelector(
-        "[data-controlled-patients]"
-      );
-      const periodStartNodes = cardNode.querySelectorAll("[data-period-start]");
-      const periodEndNodes = cardNode.querySelectorAll("[data-period-end]");
-      const registrationPeriodEndNodes = cardNode.querySelectorAll(
-        "[data-registrations-period-end]"
-      );
-      const adjustedPatientCountsNodes = cardNode.querySelectorAll(
-        "[data-adjusted-registrations]"
-      );
-
-      const missedVisitsRate = this.formatPercentage(
-        data.missedVisitsRate[period]
-      );
-      const visitButNoBPMeasureRate = this.formatPercentage(
-        data.visitButNoBPMeasureRate[period]
-      );
-      const uncontrolledRate = this.formatPercentage(
-        data.uncontrolledRate[period]
-      );
-      const controlledRate = this.formatPercentage(data.controlRate[period]);
-
-      const periodInfo = data.periodInfo[period];
-      const adjustedPatientCounts = data.adjustedPatientCounts[period];
-      const totalMissedVisits = data.missedVisits[period];
-      const totalVisitButNoBPMeasure = data.visitButNoBPMeasure[period];
-      const totalUncontrolledPatients = data.uncontrolledPatients[period];
-      const totalControlledPatients = data.controlledPatients[period];
-
-      missedVisitsRateNode.innerHTML = missedVisitsRate;
-      visitButNoBPMeasureRateNode.innerHTML = visitButNoBPMeasureRate;
-      uncontrolledRateNode.innerHTML = uncontrolledRate;
-      controlledRateNode.innerHTML = controlledRate;
-      missedVisitsPatientsNode.innerHTML =
-        this.formatNumberWithCommas(totalMissedVisits);
-      visitButNoBPMeasurePatientsNode.innerHTML = this.formatNumberWithCommas(
-        totalVisitButNoBPMeasure
-      );
-      uncontrolledPatientsNode.innerHTML = this.formatNumberWithCommas(
-        totalUncontrolledPatients
-      );
-      controlledPatientsNode.innerHTML = this.formatNumberWithCommas(
-        totalControlledPatients
-      );
-      periodStartNodes.forEach(
-        (node) => (node.innerHTML = periodInfo.bp_control_start_date)
-      );
-      periodEndNodes.forEach(
-        (node) => (node.innerHTML = periodInfo.bp_control_end_date)
-      );
-      registrationPeriodEndNodes.forEach(
-        (node) => (node.innerHTML = periodInfo.bp_control_registration_date)
-      );
-      adjustedPatientCountsNodes.forEach(
-        (node) =>
-          (node.innerHTML = this.formatNumberWithCommas(adjustedPatientCounts))
-      );
-    };
-
-    const populateVisitDetailsGraphDefault = () => {
-      const cardNode = document.getElementById("visit-details");
-      const mostRecentPeriod = cardNode.getAttribute("data-period");
-
-      populateVisitDetailsGraph(mostRecentPeriod);
-    };
-
-    const visitDetailsGraphCanvas =
-      document.getElementById("missedVisitDetails");
-    if (visitDetailsGraphCanvas) {
-      new Chart(
-        visitDetailsGraphCanvas.getContext("2d"),
-        visitDetailsGraphConfig
-      );
-      populateVisitDetailsGraphDefault();
-    }
   };
 
   this.initializeTables = () => {
@@ -1300,109 +1194,5 @@ Reports = function (withLtfu) {
     if (regionComparisonTable) {
       new Tablesort(regionComparisonTable, tableSortAscending);
     }
-  };
-
-  this.getReportingData = () => {
-    const jsonData = JSON.parse(this.getChartDataNode().textContent);
-
-    return {
-      controlledPatients: jsonData.controlled_patients,
-      controlRate: jsonData.controlled_patients_rate,
-      controlWithLtfuRate: jsonData.controlled_patients_with_ltfu_rate,
-      missedVisits: jsonData.missed_visits,
-      missedVisitsWithLtfu: jsonData.missed_visits_with_ltfu,
-      missedVisitsRate: jsonData.missed_visits_rate,
-      missedVisitsWithLtfuRate: jsonData.missed_visits_with_ltfu_rate,
-      diabetesMissedVisits: jsonData.diabetes_missed_visits,
-      diabetesMissedVisitsWithLtfu: jsonData.diabetes_missed_visits_with_ltfu,
-      diabetesMissedVisitsRate: jsonData.diabetes_missed_visits_rates,
-      diabetesMissedVisitsWithLtfuRate:
-        jsonData.diabetes_missed_visits_with_ltfu_rates,
-      monthlyRegistrations: jsonData.registrations,
-      monthlyDiabetesRegistrations: jsonData.diabetes_registrations,
-      monthlyDiabetesFollowups: jsonData.monthly_diabetes_followups,
-      adjustedPatientCounts: jsonData.adjusted_patient_counts,
-      adjustedPatientCountsWithLtfu: jsonData.adjusted_patient_counts_with_ltfu,
-      cumulativeRegistrations: jsonData.cumulative_registrations,
-      cumulativeDiabetesRegistrations: jsonData.cumulative_diabetes_registrations,
-      cumulativeHypertensionAndDiabetesRegistrations: jsonData.cumulative_hypertension_and_diabetes_registrations,
-      uncontrolledPatients: jsonData.uncontrolled_patients,
-      uncontrolledRate: jsonData.uncontrolled_patients_rate,
-      uncontrolledWithLtfuRate: jsonData.uncontrolled_patients_with_ltfu_rate,
-      visitButNoBPMeasure: jsonData.visited_without_bp_taken,
-      visitButNoBPMeasureRate: jsonData.visited_without_bp_taken_rates,
-      periodInfo: jsonData.period_info,
-      adjustedDiabetesPatientCounts: jsonData.adjusted_diabetes_patient_counts,
-      adjustedDiabetesPatientCountsWithLtfu:
-        jsonData.adjusted_diabetes_patient_counts_with_ltfu,
-      bsBelow200Patients: jsonData.bs_below_200_patients,
-      bsBelow200Rate: jsonData.bs_below_200_rates,
-      bsBelow200WithLtfuRate: jsonData.bs_below_200_with_ltfu_rates,
-      bsBelow200BreakdownRates: jsonData.bs_below_200_breakdown_rates,
-      bsOver200BreakdownRates: jsonData.bs_over_200_breakdown_rates,
-      bs200to300Patients: jsonData.bs_200_to_300_patients,
-      bs200to300Rate: jsonData.bs_200_to_300_rates,
-      bs200to300WithLtfuRate: jsonData.bs_200_to_300_with_ltfu_rates,
-      bsOver300Patients: jsonData.bs_over_300_patients,
-      bsOver300Rate: jsonData.bs_over_300_rates,
-      bsOver300WithLtfuRate: jsonData.bs_over_300_with_ltfu_rates,
-      visitButNoBSMeasure: jsonData.visited_without_bs_taken,
-      visitButNoBSMeasureRate: jsonData.visited_without_bs_taken_rates,
-    };
-  };
-
-  this.createBaseGraphConfig = () => {
-    return {
-      type: "line",
-      options: {
-        animation: false,
-        responsive: true,
-        maintainAspectRatio: false,
-        layout: {
-          padding: {
-            left: 0,
-            right: 0,
-            top: 20,
-            bottom: 0,
-          },
-        },
-        elements: {
-          point: {
-            pointStyle: "circle",
-            hoverRadius: 5,
-          },
-        },
-        legend: {
-          display: false,
-        },
-      },
-    };
-  };
-
-  this.createAxisMaxAndStepSize = (data) => {
-    const maxDataValue = Math.max(...Object.values(data));
-    const maxAxisValue = Math.round(maxDataValue * 1.15);
-    const axisStepSize = Math.round(maxAxisValue / 2);
-
-    return {
-      max: maxAxisValue,
-      stepSize: axisStepSize,
-    };
-  };
-
-  this.formatNumberWithCommas = (value) => {
-    if (value === undefined) {
-      return 0;
-    }
-
-    if (numeral(value) !== undefined) {
-      return numeral(value).format("0,0");
-    }
-
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  };
-
-  this.formatPercentage = (number) => {
-    return (number || 0) + "%";
   };
 };
---
+++ app/components/dashboard/hypertension/treatment_outcomes_card_component.rb
@@ -0,0 +1,70 @@
+class Dashboard::Hypertension::TreatmentOutcomesCardComponent < ApplicationComponent
+  attr_reader :data, :region, :period
+
+  def initialize(data:, region:, period:)
+    @data = data
+    @region = region
+    @period = period
+  end
+
+  def graph_data
+    {adjustedPatientCounts: data[:adjusted_patient_counts],
+     controlRate: data[:controlled_patients_rate],
+     controlledPatients: data[:controlled_patients],
+     uncontrolledPatients: data[:uncontrolled_patients],
+     uncontrolledRate: data[:uncontrolled_patients_rate],
+     missedVisits: data[:missed_visits],
+     missedVisitsRate: data[:missed_visits_rate],
+     visitButNoBPMeasure: data[:visited_without_bp_taken],
+     visitButNoBPMeasureRate: data[:visited_without_bp_taken_rates],
+     **period_data}
+
+  end
+
+  def treatment_outcomes
+    [{key: "missedVisitsRate",
+      count: "missedVisits",
+      class: "c-blue",
+      title: "Missed visits",
+      tooltip: {
+        "Numerator" => t("missed_visits_copy.numerator"),
+        "Denominator" => t("denominator_copy", region_name: @region.name)
+      }},
+      {key: "visitButNoBPMeasureRate",
+       count: "visitButNoBPMeasure",
+       class: "c-grey-dark",
+       title: "Visit but no BP taken",
+       tooltip: {
+         "Numerator" => t("visit_but_no_bp_taken_copy.numerator"),
+         "denominator" => t("denominator_copy", region_name: @region.name)
+       }},
+      {key: "uncontrolledRate",
+       count: "uncontrolledPatients",
+       class: "c-red",
+       title: "BP not controlled".html_safe,
+       tooltip: {
+         "Numerator" => t("bp_not_controlled_copy.numerator"),
+         "Denominator" => t("denominator_copy", region_name: @region.name)
+       }},
+      {key: "controlRate",
+       count: "controlledPatients",
+       title: "BP controlled",
+       class: "c-amber",
+       tooltip: {
+         "Numerator" => t("bp_controlled_copy.numerator"),
+         "Denominator" => t("denominator_copy", region_name: @region.name)}
+      }]
+  end
+
+  def period_data
+    {
+      startDate: period_info(:bp_control_start_date),
+      endDate: period_info(:bp_control_end_date),
+      registrationDate: period_info(:bp_control_registration_date)
+    }
+  end
+
+  def period_info(key)
+    data[:period_info].map { |k, v| [k, v[key]] }.to_h
+  end
+end
