7577de2e7798320afa147d6bbe105d3cf032a31c
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-09-12 18:41:00 +0530

    Add transformer specs

--- app/transformers/api/v4/call_result_transformer.rb
+++ app/transformers/api/v4/call_result_transformer.rb
@@ -2,7 +2,7 @@ class Api::V4::CallResultTransformer < Api::V4::Transformer
   class << self
     def from_request(call_result_payload)
       attributes = super(call_result_payload)
-      return attributes if attributes["patient_id"]
+      return attributes if attributes["patient_id"].present?
 
       appointment = Appointment.find_by(id: call_result_payload["appointment_id"])
       attributes.merge("patient_id" => appointment&.patient_id)
--- spec/transformers/api/v4/call_result_transformer_spec.rb
+++ spec/transformers/api/v4/call_result_transformer_spec.rb
@@ -2,5 +2,18 @@ require "rails_helper"
 
 RSpec.describe Api::V4::CallResultTransformer do
   describe "#from_request" do
+    it "finds the appointment by appointment_id and sets patient_id if missing" do
+      appointment = create(:appointment)
+      expect(Api::V4::CallResultTransformer.from_request(
+        {"appointment_id" => appointment.id, "patient_id" => nil}
+      )["patient_id"]).to eq(appointment.patient_id)
+    end
+
+    it "returns the hash as is if the appointment is missing" do
+      patient_id = SecureRandom.uuid
+      expect(Api::V4::CallResultTransformer.from_request(
+        {"appointment_id" => SecureRandom.uuid, "patient_id" => patient_id}
+      )["patient_id"]).to eq(patient_id)
+    end
   end
 end
