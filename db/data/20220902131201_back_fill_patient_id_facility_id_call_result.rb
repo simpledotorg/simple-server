99b2b1d9eb1939f98fecae08e86567cc24f75019
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-09-12 16:21:31 +0530

    Indentation

--- db/data/20220902131201_back_fill_patient_id_facility_id_call_result.rb
+++ db/data/20220902131201_back_fill_patient_id_facility_id_call_result.rb
@@ -1,18 +1,20 @@
 class BackFillPatientIdFacilityIdCallResult < ActiveRecord::Migration[5.2]
   def up
-    ActiveRecord::Base.connection.execute(<<-SQL
+    ActiveRecord::Base.connection.execute(
+      <<-SQL
       UPDATE call_results SET (facility_id, patient_id) = (
         SELECT facility_id, patient_id FROM appointments
         WHERE call_results.appointment_id = appointments.id
       )
-    SQL
-                                         )
+      SQL
+    )
   end
 
   def down
-    ActiveRecord::Base.connection.execute(<<-SQL
+    ActiveRecord::Base.connection.execute(
+      <<-SQL
       UPDATE call_results SET (facility_id, patient_id) = (NULL, NULL)
-    SQL
-                                         )
+      SQL
+    )
   end
 end
