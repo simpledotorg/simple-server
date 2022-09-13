8252441be1219ee0d612a20e8e01e4505fed82e7
Priyanga P Kini <priyangapkini99@gmail.com>
2022-09-13 18:00:05 +0530

    Reorganise files related to progress tab

--- app/components/progress_tab/hypertension/control_component_v1.rb
+++ app/components/progress_tab/hypertension/control_component_v1.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Reports::ProgressControlComponentV1 < ViewComponent::Base
+class ProgressTab::Hypertension::ControlComponentV1 < ApplicationComponent
   include AssetsHelper
   include ActionView::Helpers::NumberHelper
 
