618e030c8af2ea6c08eb6605b126ff5181643bf6
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-09-13 14:43:04 +0530

    Remove extra X_USER_ID setup from controller specs

--- spec/controllers/api/v3/medical_histories_controller_spec.rb
+++ spec/controllers/api/v3/medical_histories_controller_spec.rb
@@ -11,12 +11,6 @@ RSpec.describe Api::V3::MedicalHistoriesController, type: :controller do
   let(:update_payload) { ->(medical_history) { updated_medical_history_payload medical_history } }
   let(:number_of_schema_errors_in_invalid_payload) { 2 }
 
-  before :each do
-    request.env["X_USER_ID"] = request_user.id
-    request.env["X_FACILITY_ID"] = request_facility.id
-    request.env["HTTP_AUTHORIZATION"] = "Bearer #{request_user.access_token}"
-  end
-
   def create_record(options = {})
     facility = create(:facility, facility_group: request_facility_group)
     patient = build(:patient, registration_facility: facility)
