8c713bc8ffe651bc22e265b1e96f53febaadf71a
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2022-09-13 19:33:12 +0530

    Remove unnecessary headers from API controller specs (#4273)

--- spec/controllers/api/v3/blood_sugars_controller_spec.rb
+++ spec/controllers/api/v3/blood_sugars_controller_spec.rb
@@ -11,12 +11,6 @@ RSpec.describe Api::V3::BloodSugarsController, type: :controller do
   let(:update_payload) { ->(blood_sugar) { updated_blood_sugar_payload(blood_sugar) } }
   let(:number_of_schema_errors_in_invalid_payload) { 2 }
 
-  before :each do
-    request.env["X_USER_ID"] = request_user.id
-    request.env["X_FACILITY_ID"] = request_facility.id
-    request.env["HTTP_AUTHORIZATION"] = "Bearer #{request_user.access_token}"
-  end
-
   def create_record(options = {})
     facility = options[:facility] || create(:facility, facility_group: request_facility_group)
     patient = create(:patient, registration_facility: facility)
