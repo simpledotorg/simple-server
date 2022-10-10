9cd10f8f74fe22b08afef84e32490cee1f8f67ec
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2022-10-10 18:46:48 +0530

    Cache warmer performance improvements (#4312)
    * Start cache warming at 2.30

--- spec/services/reports/region_cache_warmer_spec.rb
+++ spec/services/reports/region_cache_warmer_spec.rb
@@ -111,7 +111,6 @@ RSpec.describe Reports::RegionCacheWarmer, type: :model do
         /monthly_registrations_by_user\/group_by\/registration_user_id\/period_type\/month\/diagnosis\/hypertension/,
         /monthly_registrations_by_user\/group_by\/registration_user_id\/period_type\/month\/diagnosis\/diabetes/,
         /monthly_registrations_by_gender/,
-        /controlled_by_gender/,
         /overdue_calls_by_user/
       ]
 
