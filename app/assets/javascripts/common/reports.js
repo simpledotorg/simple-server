b9f073eae2a5ece1a238315dd80da9a72ebab1be
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2022-10-12 16:49:27 +0100

    Attempt to add several line colors for DM registrations
    Failed attempt - reverted for now

--- app/assets/javascripts/common/reports.js
+++ app/assets/javascripts/common/reports.js
@@ -1693,12 +1693,9 @@ const intersectDataVerticalLine = {
     if (chart.tooltip._active && chart.tooltip._active.length) {
       const ctx = chart.ctx;
       ctx.save();
-      console.log(chart);
       const activePoint = chart.tooltip._active[0];
-      console.log(activePoint);
       const chartArea = chart.chartArea;
-      console.log(chartArea);
-      // vertical hover line - above "point" (line graph value at specific point)
+      // vertical hover line - above FIRST "point" (line graph value at specific point)
       ctx.beginPath();
       ctx.moveTo(activePoint._model.x, chartArea.top);
       ctx.lineTo(activePoint._model.x, activePoint._model.y);
@@ -1706,13 +1703,26 @@ const intersectDataVerticalLine = {
       ctx.strokeStyle = "rgba(0,0,0, 0.1)";
       ctx.stroke();
       ctx.restore();
-      // vertical hover line - below "point" (line graph value at specific point)
+      // vertical hover line - below FIRST "point" (line graph value at specific point)
       ctx.beginPath();
       ctx.moveTo(activePoint._model.x, activePoint._model.y);
       ctx.lineTo(activePoint._model.x, chartArea.bottom);
       ctx.lineWidth = 2;
       ctx.stroke();
       ctx.restore();
+      // any extra points after the highest point - hidden behind datapoints (including bars in bar charts)
+      // if (chart.tooltip._active && chart.tooltip._active.length > 1) {
+      //   for (let index = 1; index < chart.tooltip._active.length; index++) {
+      //     const activePointLoop = chart.tooltip._active[index];
+      //     ctx.beginPath();
+      //     ctx.moveTo(activePointLoop._model.x, activePointLoop._model.y + 6);
+      //     ctx.lineTo(activePointLoop._model.x, chartArea.bottom);
+      //     ctx.lineWidth = 2;
+      //     ctx.strokeStyle = activePointLoop._model.borderColor;
+      //     ctx.stroke();
+      //     ctx.restore();
+      //   }
+      // }
     }
   },
 };
