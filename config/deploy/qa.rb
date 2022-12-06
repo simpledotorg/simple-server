0886dc3468611bf7c2f726954afe281fd316c1ec
roy peter <roypeter@roys-MacBook-Pro.local>
2022-12-06 15:19:39 +0530

    Update qa deployment ip
    IP address got updated afer reboot

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,1 +1,1 @@
-server "ec2-3-111-217-201.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db sidekiq cron]
+server "ec2-13-235-3-72.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db sidekiq cron]
