ca12fcd561554ef396148a10bce57fdc181410d1
Priyanga P Kini <priyangapkini99@gmail.com>
2022-12-06 18:43:50 +0530

    Remove unused functions

--- app/models/reports/facility_progress_dimension.rb
+++ app/models/reports/facility_progress_dimension.rb
@@ -24,12 +24,6 @@ module Reports
       [:monthly, indicator, diagnosis_code, gender].compact.join("_")
     end
 
-    # The function 'field_v1' refers to the columns in 'reporting_facility_monthly_follow_ups_and_registrations'
-    # We have introduced one more diagnosis - `Hypertension and diabetes` in `reporting_facility_monthly_follow_ups_and_registrations`
-    def field_v1
-      [:monthly, indicator, diagnosis_code_v1, gender_code_v1].compact.join("_")
-    end
-
     private
 
     def diagnosis_code
@@ -37,23 +31,6 @@ module Reports
       when :hypertension then :htn
       when :diabetes then :dm
       when :all then nil
-      end
-    end
-
-    def diagnosis_code_v1
-      case diagnosis
-      when :hypertension_and_diabetes then :htn_and_dm
-      when :hypertension then :htn_only
-      when :diabetes then :dm_only
-      when :all then :htn_or_dm
-      end
-    end
-
-    def gender_code_v1
-      if diagnosis == :all || gender == :all
-        nil
-      else
-        gender
       end
     end
   end
