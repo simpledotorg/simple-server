ac79cb1d220354cc45feeb40e5df68c734101cb9
Akshatha <71076330+qptr@users.noreply.github.com>
2022-12-07 14:46:38 +0530

    Add actions to configure and submit protocol coefficients

--- app/controllers/admin/protocols_controller.rb
+++ app/controllers/admin/protocols_controller.rb
@@ -42,6 +42,31 @@ class Admin::ProtocolsController < AdminController
     redirect_to admin_protocols_url, notice: "Medication list was successfully deleted"
   end
 
+  def configure_coefficients
+    @coefficients = ProtocolDrugCalculationCoefficient.find_by(protocol_params[:id]).coefficients
+    if @coefficients.absent?
+      @coefficients = {
+        "load_coefficient": 0,
+      }
+      categories = Set.new
+      @protocol.protocol_drugs.each do |drug|
+        @coefficients[drug[:rxnorm_code]] = 0
+        categories.add(drug[:drug_category])
+      end
+      categories.each do |category|
+        @coefficients[category] = 0
+      end
+    end
+  end
+
+  def submit_coefficients
+    if @coefficients.save
+      redirect_to [:admin, @coefficients], notice: "Medication list was successfully configured"
+    else
+      render :configure_coefficients # renders with errors poopulated
+    end
+  end
+
   private
 
   def set_protocol
