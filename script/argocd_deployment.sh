3d45a3c19b740373c2451e3647a8e496a1af0a25
roy peter <roypeter@roys-MacBook-Pro.local>
2022-12-07 13:13:33 +0530

    Fix semaphoreci docker tags

--- script/argocd_deployment.sh
+++ script/argocd_deployment.sh
@@ -10,6 +10,21 @@ ARGOCD_USERNAME=$2
 ARGOCD_PASSWORD=$3
 IMAGE_TAG=$4
 
+
+# Install argocd if not installed
+if ! command -v argocd &> /dev/null
+then
+  # Set argocd version if env is empty
+  # Select desired TAG from https://github.com/argoproj/argo-cd/releases
+  if [ -z "$ARGOCD_VERSION" ]; then
+    export ARGOCD_VERSION=v2.5.2 # Set default version
+  fi
+
+  curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/$ARGOCD_VERSION/argocd-linux-amd64
+  sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+  rm argocd-linux-amd64
+fi
+
 # Argocd login
 argocd login $ARGOCD_ENDPOINT \
   --username $ARGOCD_USERNAME \
