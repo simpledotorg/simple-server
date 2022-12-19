f69ce470619ecc6aa308edddcfe55123c3a693e0
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-12-19 13:18:57 +0530

    Add questionnaire factory

---
+++ spec/factories/questionnaires.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :questionnaire do
+    id { SecureRandom.uuid }
+    questionnaire_type { "monthly_screening_reports" }
+    dsl_version { 1 }
+    layout { {} }
+  end
+end
