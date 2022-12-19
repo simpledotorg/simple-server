1231948357b0cd7dee7c5464c88eae6c1b69e405
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-12-19 12:43:46 +0530

    Add spec for localized_layout

--- spec/models/questionnaire_spec.rb
+++ spec/models/questionnaire_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Questionnaire, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#localized_layout" do
+    it "localized text attributes in items recursively" do
+      allow(I18n).to receive(:t).with("test_translations.test_string").and_return "Test"
+      layout = {"item" => [
+        {"text" => "test_translations.test_string",
+         "item" => [
+           {"text" => "test_translations.test_string",
+            "item" => [
+              {"text" => "test_translations.test_string"}
+            ]},
+           {"text" => "test_translations.test_string",
+            "item" => [
+              {"text" => "test_translations.test_string"}
+            ]}
+         ]},
+        {"text" => "test_translations.test_string"}
+      ]}
+
+      localized_layout = {"item" => [
+        {"text" => "Test",
+         "item" => [
+           {"text" => "Test",
+            "item" => [
+              {"text" => "Test"}
+            ]},
+           {"text" => "Test",
+            "item" => [
+              {"text" => "Test"}
+            ]}
+         ]},
+        {"text" => "Test"}
+      ]}
+
+      questionnaire = build(:questionnaire, layout: layout)
+      expect(questionnaire.localized_layout).to eq(localized_layout)
+    end
+  end
 end
