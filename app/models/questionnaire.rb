965db134a3f6fe546eddace8badd246c4d6d2034
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-12-19 12:18:45 +0530

    Fix layout localization to work with hashes generally

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,13 +1,25 @@
 class Questionnaire < ApplicationRecord
   scope :for_sync, -> { with_discarded }
 
-  def with_localized_layout(sub_layout = layout)
-    items = sub_layout["item"]
-    return nil if items.nil?
+  def localize_layout(sub_layout = layout)
+    sub_layout
+      .then { |l| localize_text(l) }
+      .then { |l| localize_items_recursively(l) }
+  end
 
-    items.map do |item|
-      item.merge({ "item" => with_localized_layout(item),
-                   "text" => I18n.t(item["text"]) }.compact)
-    end
+  private
+
+  def localize_text(sub_layout)
+    text = sub_layout["text"]
+    return sub_layout unless text
+
+    sub_layout.merge({"text" => I18n.t(text)})
+  end
+
+  def localize_items_recursively(sub_layout)
+    items = sub_layout["item"]
+    return sub_layout unless items
+
+    sub_layout.merge({"item" => items.map { |item| localize_layout(item) }})
   end
 end
