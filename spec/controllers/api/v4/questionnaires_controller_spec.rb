b88d45c366547bda21935029c35f7d747c7b1b8b
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2022-12-21 19:16:06 +0530

    Cleanup controller specs

--- spec/controllers/api/v4/questionnaires_controller_spec.rb
+++ spec/controllers/api/v4/questionnaires_controller_spec.rb
@@ -21,7 +21,7 @@ describe Api::V4::QuestionnairesController, type: :controller do
   end
 
   def create_record_list(n, options = {})
-    (@questionnaire_types.keys-@used_questionnaire_types).take(n).map do |questionnaire_type|
+    (@questionnaire_types.keys - @used_questionnaire_types).take(n).map do |questionnaire_type|
       @used_questionnaire_types << questionnaire_type
       create(:questionnaire, questionnaire_type: questionnaire_type, dsl_version: dsl_version, **options)
     end
@@ -42,99 +42,96 @@ describe Api::V4::QuestionnairesController, type: :controller do
         end
       end
 
-      describe "GET sync: send data from server to device;" do
-        let(:response_key) { model.to_s.underscore.pluralize }
-        it "Returns records from the beginning of time, when process_token is not set" do
-          get :sync_to_user, params: {dsl_version: dsl_version}
+      let(:response_key) { model.to_s.underscore.pluralize }
+      it "Returns records from the beginning of time, when process_token is not set" do
+        get :sync_to_user, params: {dsl_version: dsl_version}
 
+        response_body = JSON(response.body)
+        expect(response_body[response_key].count).to eq model.count
+        expect(response_body[response_key].map { |record| record["id"] }.to_set)
+          .to eq(model.all.map(&:id).to_set)
+      end
+
+      it "Returns new records added since last sync" do
+        expected_records = create_record_list(5, updated_at: 5.minutes.ago)
+        get :sync_to_user, params: {process_token: make_process_token(other_facilities_processed_since: 10.minutes.ago)}.merge(custom_params)
+
+        response_body = JSON(response.body)
+        expect(response_body[response_key].count).to eq 5
+
+        expect(response_body[response_key].map { |record| record["id"] }.to_set)
+          .to eq(expected_records.map(&:id).to_set)
+
+        response_process_token = parse_process_token(response_body)
+        expect(response_process_token[:other_facilities_processed_since].to_time.to_i)
+          .to eq(expected_records.map(&:updated_at).max.to_i)
+      end
+
+      it "Returns an empty list when there is nothing to sync" do
+        sync_time = 10.minutes.ago
+        get :sync_to_user, params: {
+          process_token: make_process_token(other_facilities_processed_since: sync_time),
+          dsl_version: dsl_version
+        }.merge(custom_params)
+        response_body = JSON(response.body)
+        response_process_token = parse_process_token(response_body)
+        expect(response_body[response_key].count).to eq 0
+        expect(response_process_token[:other_facilities_processed_since].to_time.to_i).to eq sync_time.to_i
+      end
+
+      describe "batching" do
+        it "returns the number of records requested with limit" do
+          get :sync_to_user, params: {
+            process_token: make_process_token(other_facilities_processed_since: 20.minutes.ago),
+            limit: 2,
+            dsl_version: dsl_version
+          }
           response_body = JSON(response.body)
-          expect(response_body[response_key].count).to eq model.count
-          expect(response_body[response_key].map { |record| record["id"] }.to_set)
+          expect(response_body[response_key].count).to eq 2
+        end
+
+        it "Returns all the records on server over multiple small batches" do
+          get :sync_to_user, params: {
+            process_token: make_process_token(other_facilities_processed_since: 20.minutes.ago),
+            limit: 7,
+            dsl_version: dsl_version
+          }
+
+          response_1 = JSON(response.body)
+
+          reset_controller
+
+          get :sync_to_user, params: {
+            process_token: response_1["process_token"],
+            limit: 8,
+            dsl_version: dsl_version
+          }
+          response_2 = JSON(response.body)
+
+          received_records = response_1[response_key].concat(response_2[response_key]).to_set
+          expect(received_records.count).to eq model.count
+
+          expect(received_records.map { |record| record["id"] }.to_set)
             .to eq(model.all.map(&:id).to_set)
         end
+      end
 
-        it "Returns new records added since last sync" do
-          expected_records = create_record_list(5, updated_at: 5.minutes.ago)
-          get :sync_to_user, params: {process_token: make_process_token(other_facilities_processed_since: 10.minutes.ago)}.merge(custom_params)
+      it "Returns discarded records" do
+        expected_records = create_record_list(5, updated_at: 5.minutes.ago)
+        expected_records.first.update(deleted_at: Time.now)
 
-          response_body = JSON(response.body)
-          expect(response_body[response_key].count).to eq 5
+        get :sync_to_user, params: custom_params
 
-          expect(response_body[response_key].map { |record| record["id"] }.to_set)
-            .to eq(expected_records.map(&:id).to_set)
+        response_body = JSON(response.body)
+        expect(response_body[response_key].count).to eq 15
 
-          response_process_token = parse_process_token(response_body)
-          expect(response_process_token[:other_facilities_processed_since].to_time.to_i)
-            .to eq(expected_records.map(&:updated_at).max.to_i)
-        end
-
-        it "Returns an empty list when there is nothing to sync" do
-          sync_time = 10.minutes.ago
-          get :sync_to_user, params: {
-            process_token: make_process_token(other_facilities_processed_since: sync_time),
-            dsl_version: dsl_version
-          }.merge(custom_params)
-          response_body = JSON(response.body)
-          response_process_token = parse_process_token(response_body)
-          expect(response_body[response_key].count).to eq 0
-          expect(response_process_token[:other_facilities_processed_since].to_time.to_i).to eq sync_time.to_i
-        end
-
-        describe "batching" do
-          it "returns the number of records requested with limit" do
-            get :sync_to_user, params: {
-              process_token: make_process_token(other_facilities_processed_since: 20.minutes.ago),
-              limit: 2,
-              dsl_version: dsl_version
-            }
-            response_body = JSON(response.body)
-            expect(response_body[response_key].count).to eq 2
-          end
-
-          it "Returns all the records on server over multiple small batches" do
-            get :sync_to_user, params: {
-              process_token: make_process_token(other_facilities_processed_since: 20.minutes.ago),
-              limit: 7,
-              dsl_version: dsl_version
-            }
-
-            response_1 = JSON(response.body)
-
-            reset_controller
-
-            get :sync_to_user, params: {
-              process_token: response_1["process_token"],
-              limit: 8,
-              dsl_version: dsl_version
-            }
-            response_2 = JSON(response.body)
-
-            received_records = response_1[response_key].concat(response_2[response_key]).to_set
-            expect(received_records.count).to eq model.count
-
-            expect(received_records.map { |record| record["id"] }.to_set)
-              .to eq(model.all.map(&:id).to_set)
-          end
-        end
-
-        it "Returns discarded records" do
-          expected_records = create_record_list(5, updated_at: 5.minutes.ago)
-          expected_records.first.update(deleted_at: Time.now)
-
-          get :sync_to_user, params: custom_params
-
-          response_body = JSON(response.body)
-          expect(response_body[response_key].count).to eq 15
-
-          expect(response_body[response_key].map { |record| record["id"] })
-            .to include(discard_record.id)
-        end
+        expect(response_body[response_key].map { |record| record["id"] })
+          .to include(discard_record.id)
       end
     end
 
-
     it "returns questionnaires only for given DSL Version" do
-      version_1_questionnaire = create(:questionnaire, questionnaire_type: "monthly_screening_reports" , dsl_version: 1)
+      version_1_questionnaire = create(:questionnaire, questionnaire_type: "monthly_screening_reports", dsl_version: 1)
       version_2_questionnaire = create(:questionnaire, questionnaire_type: "monthly_screening_reports", dsl_version: 2)
 
       get :sync_to_user, params: {dsl_version: 1}
@@ -148,60 +145,53 @@ describe Api::V4::QuestionnairesController, type: :controller do
       get :sync_to_user
       expect(response.status).to eq 400
     end
-
-    # Specs which are included in Shared specs & excluded here:
-    # 1. Force-resync when resync-token is modified.
-    # 2. When a resource is updated, it is included in next Sync response
-    # 3. Discarded resources are included in Sync response
   end
 
   context "a sync controller that authenticates user requests: sync_to_user" do
-    describe "user api authentication" do
-      let(:request_key) { model.to_s.underscore.pluralize }
-      let(:empty_payload) { {request_key => []} }
+    let(:request_key) { model.to_s.underscore.pluralize }
+    let(:empty_payload) { {request_key => []} }
 
-      before :each do
-        _request_user = FactoryBot.create(:user)
-        set_authentication_headers
-      end
+    before :each do
+      _request_user = FactoryBot.create(:user)
+      set_authentication_headers
+    end
 
-      it "allows sync_to_user requests to the controller with valid user_id and access_token" do
+    it "allows sync_to_user requests to the controller with valid user_id and access_token" do
+      get :sync_to_user, params: empty_payload
+
+      expect(response.status).not_to eq(401)
+    end
+
+    it "returns 403 for user which has been denied access" do
+      request_user.update(sync_approval_status: :denied)
+      get :sync_to_user, params: empty_payload
+
+      expect(response.status).to eq(403)
+    end
+
+    it "returns 403 for users which have sync approval status set to requested" do
+      request_user.update(sync_approval_status: :requested)
+      get :sync_to_user, params: empty_payload
+
+      expect(response.status).to eq(403)
+    end
+
+    it "sets user logged_in_at on successful authentication" do
+      now = Time.current
+      Timecop.freeze(now) do
         get :sync_to_user, params: empty_payload
 
-        expect(response.status).not_to eq(401)
+        request_user.reload
+        expect(request_user.logged_in_at.to_i).to eq(now.to_i)
       end
+    end
 
-      it "returns 403 for user which has been denied access" do
-        request_user.update(sync_approval_status: :denied)
-        get :sync_to_user, params: empty_payload
+    it "does not allow sync_to_user requests to the controller with invalid user_id and access_token" do
+      request.env["HTTP_X_USER_ID"] = "invalid user id"
+      request.env["HTTP_AUTHORIZATION"] = "invalid access token"
+      get :sync_to_user, params: empty_payload
 
-        expect(response.status).to eq(403)
-      end
-
-      it "returns 403 for users which have sync approval status set to requested" do
-        request_user.update(sync_approval_status: :requested)
-        get :sync_to_user, params: empty_payload
-
-        expect(response.status).to eq(403)
-      end
-
-      it "sets user logged_in_at on successful authentication" do
-        now = Time.current
-        Timecop.freeze(now) do
-          get :sync_to_user, params: empty_payload
-
-          request_user.reload
-          expect(request_user.logged_in_at.to_i).to eq(now.to_i)
-        end
-      end
-
-      it "does not allow sync_to_user requests to the controller with invalid user_id and access_token" do
-        request.env["HTTP_X_USER_ID"] = "invalid user id"
-        request.env["HTTP_AUTHORIZATION"] = "invalid access token"
-        get :sync_to_user, params: empty_payload
-
-        expect(response.status).to eq(401)
-      end
+      expect(response.status).to eq(401)
     end
   end
 
@@ -216,25 +206,23 @@ describe Api::V4::QuestionnairesController, type: :controller do
     let(:request_key) { model.to_s.underscore.pluralize }
     let(:model_class_sym) { model.to_s.underscore.to_sym }
 
-    describe "creates an audit log for data synced to user" do
-      let!(:records) { create_record_list(5) }
-      it "creates an audit log for data fetched by the user" do
-        Timecop.freeze do
-          Sidekiq::Testing.inline! do
-            records.each do |record|
-              expect(AuditLogger)
-                .to receive(:info).with({user: request_user.id,
-                                         auditable_type: auditable_type,
-                                         auditable_id: record[:id],
-                                         action: "fetch",
-                                         time: Time.current}.to_json)
-            end
-            get :sync_to_user, params: {
-              processed_since: 20.minutes.ago,
-              limit: 5,
-              dsl_version: dsl_version
-            }, as: :json
+    let!(:records) { create_record_list(5) }
+    it "creates an audit log for data fetched by the user" do
+      Timecop.freeze do
+        Sidekiq::Testing.inline! do
+          records.each do |record|
+            expect(AuditLogger)
+              .to receive(:info).with({user: request_user.id,
+                                       auditable_type: auditable_type,
+                                       auditable_id: record[:id],
+                                       action: "fetch",
+                                       time: Time.current}.to_json)
           end
+          get :sync_to_user, params: {
+            processed_since: 20.minutes.ago,
+            limit: 5,
+            dsl_version: dsl_version
+          }, as: :json
         end
       end
     end
