760c2b4edf24d3e3f59b8f44a099f7646f542724
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-01-13 19:41:22 +0530

    Fill data in seed from reports

--- lib/tasks/scripts/seed_questionnaire_responses.rb
+++ lib/tasks/scripts/seed_questionnaire_responses.rb
@@ -1,14 +1,41 @@
 class SeedQuestionnaireResponses
   def self.call
-    Questionnaire.monthly_screening_reports.active.product(Facility.all).each do |questionnaire, facility|
-      QuestionnaireResponse.create(
+    return unless Flipper.enabled?(:monthly_screening_reports)
+
+    facility_reports =
+      Reports::FacilityMonthlyFollowUpAndRegistration.where(
+        month_date: Date.current.beginning_of_month
+      ).select(
+        "monthly_follow_ups_htn_male",
+        "monthly_follow_ups_htn_female",
+        "monthly_follow_ups_dm_male",
+        "monthly_follow_ups_dm_female",
+        "monthly_follow_ups_htn_and_dm_male",
+        "monthly_follow_ups_htn_and_dm_female"
+      ).load
+
+    Questionnaire.monthly_screening_reports.active.product(facility_reports).each do |questionnaire, facility_report|
+      QuestionnaireResponse.create!(
         questionnaire: questionnaire.id,
-        facility: facility.id,
+        facility_id: facility_report.facility_id,
         user_id: nil,
-        content: {},
+        content: {**prefilled_responses(facility_report)},
         device_created_at: Time.now,
         device_updated_at: Time.now
       )
     end
+  end
+
+  private
+
+  def prefilled_responses(facility_report)
+    {
+      "diagnosed_cases_on_follow_up_htn.male" => facility_report.monthly_follow_ups_htn_male,
+      "diagnosed_cases_on_follow_up_htn.female" => facility_report.monthly_follow_ups_htn_female,
+      "diagnosed_cases_on_follow_up_dm.male" => facility_report.monthly_follow_ups_dm_male,
+      "diagnosed_cases_on_follow_up_dm.female" => facility_report.monthly_follow_ups_dm_female,
+      "diagnosed_cases_on_follow_up_htn_and_dm.male" => facility_report.monthly_follow_ups_htn_and_dm_male,
+      "diagnosed_cases_on_follow_up_htn_and_dm.female" => facility_report.monthly_follow_ups_htn_and_dm_female
+    }
   end
 end
