648e442a0a7ce6fcd58b49d53ec57a08e13678a8
Govind Krishna Joshi <govind@nilenso.com>
2023-02-09 18:21:44 +0530

    Revert "Export patient line list from materialized patient summaries" (#4800)
    This reverts commit f0cf99fdc5e1002c85a48c446261b6c53ab92bb9.

--- db/migrate/20230130161639_update_materialized_patient_summaries_to_version_3.rb
+++
@@ -1,8 +0,0 @@
-class UpdateMaterializedPatientSummariesToVersion3 < ActiveRecord::Migration[6.1]
-  def change
-    update_view :materialized_patient_summaries,
-      version: 3,
-      revert_to_version: 2,
-      materialized: true
-  end
-end
--- spec/jobs/patient_list_download_job_spec.rb
+++ spec/jobs/patient_list_download_job_spec.rb
@@ -4,9 +4,6 @@ require "sidekiq_unique_jobs/testing"
 RSpec.describe PatientListDownloadJob, type: :job do
   let!(:admin) { create(:admin) }
   let!(:facility) { create(:facility, enable_diabetes_management: true) }
-  let!(:other_facility) { create(:facility, enable_diabetes_management: true) }
-  let!(:assigned_patients) { create_list(:patient, 2, assigned_facility: facility) }
-  let!(:registered_patients) { create_list(:patient, 2, registration_facility: facility, assigned_facility: other_facility) }
 
   specify { expect(described_class).to have_valid_sidekiq_options }
 
@@ -14,8 +11,7 @@ RSpec.describe PatientListDownloadJob, type: :job do
     facility_group = create(:facility_group)
     facility = create(:facility, facility_group: facility_group, enable_diabetes_management: true)
     patient = create_list(:patient, 2, assigned_facility: facility)
-    expect(PatientsWithHistoryExporter).to receive(:csv)
-      .with(a_collection_containing_exactly(*patient), {display_blood_sugars: true})
+    expect(PatientsWithHistoryExporter).to receive(:csv).with(a_collection_containing_exactly(*patient))
     described_class.perform_async(admin.email, "facility_group", {id: facility_group.id})
     described_class.drain
   end
@@ -26,18 +22,13 @@ RSpec.describe PatientListDownloadJob, type: :job do
     described_class.drain
   end
 
-  it "should not display blood sugars if diabetes management is disabled for a facility" do
-    facility.update(enable_diabetes_management: false)
-    expect(PatientsWithHistoryExporter).to receive(:csv)
-      .with(Patient.where(id: assigned_patients), {display_blood_sugars: false})
-    described_class.perform_async(admin.email, "facility", {facility_id: facility.id})
-    described_class.drain
-  end
-
   context "facilities" do
+    let!(:other_facility) { create(:facility, enable_diabetes_management: true) }
+    let!(:assigned_patients) { create_list(:patient, 2, assigned_facility: facility) }
+    let!(:registered_patients) { create_list(:patient, 2, registration_facility: facility, assigned_facility: other_facility) }
+
     it "should export only assigned patients" do
-      expect(PatientsWithHistoryExporter).to receive(:csv)
-        .with(Patient.where(id: assigned_patients), {display_blood_sugars: true})
+      expect(PatientsWithHistoryExporter).to receive(:csv).with(Patient.where(id: assigned_patients))
       described_class.perform_async(admin.email, "facility", {facility_id: facility.id})
       described_class.drain
     end
