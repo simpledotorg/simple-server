0cb5f8aa85897cf184572de80693e33fd5003c7e
Akshatha <71076330+qptr@users.noreply.github.com>
2023-04-03 18:47:21 +0530

    Refactoring

---
+++ spec/exporters/dhis2_exporter_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Dhis2Exporter do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
+end
