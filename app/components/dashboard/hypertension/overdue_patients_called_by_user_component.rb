b7fb582e431208bb1b0b1e4099f273f02c483fac
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-04-21 13:14:18 +0100

    merge fixes

--- app/components/dashboard/hypertension/overdue_patients_called_by_user_component.rb
+++ app/components/dashboard/hypertension/overdue_patients_called_by_user_component.rb
@@ -9,16 +9,14 @@ class Dashboard::Hypertension::OverduePatientsCalledByUserComponent < Applicatio
     @with_removed_from_overdue_list = with_removed_from_overdue_list
   end
 
-  def graph_data
-  end
-
   def gen_children_data
     user = {
       user: User.first,
       patients_called: periods.map { |p| {p => rand(0..200)} }.reduce(:merge),
       total_patients: periods.map { |p| {p => rand(0..200)} }.reduce(:merge)
     }
-    [user, user]
+    [user, user, user]
+    pp user
   end
 
   def table_headers
@@ -31,9 +29,16 @@ class Dashboard::Hypertension::OverduePatientsCalledByUserComponent < Applicatio
   end
 
   def is_not_facility
+    return false
     if region.child_region_type != 'facility'
       return true
     end
     false
   end
+  # def row_data(region:)
+  #   [repository.cumulative_diabetes_registrations[region.slug][period],
+  #     repository.cumulative_assigned_diabetic_patients[region.slug][period],
+  #     *range.map { |range_period| repository.monthly_diabetes_registrations[region.slug][range_period] },
+  #     *range.map { |range_period| repository.diabetes_follow_ups[region.slug][range_period] }]
+  # end
 end
