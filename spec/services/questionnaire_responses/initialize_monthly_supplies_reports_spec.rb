c5b895764a332bafb3a175343efce2c39d97a9a2
akshat <olttwa@users.noreply.github.com>
2023-04-25 19:42:32 +0530

    Initialize Supplies reports
    Co-authored-by: Akshatha <71076330+qptr@users.noreply.github.com>

---
+++ spec/services/questionnaire_responses/initialize_monthly_supplies_reports_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+describe 'QuestionnaireResponses::InitializeMonthlySuppliesReports' do
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
+end
