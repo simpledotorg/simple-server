b44180168eda218f715d28a293db5dccdaf68e45
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-04-28 14:50:20 +0100

    tooltip update
    - allow no title entry (just text)
    - divider option
    - note option (italics with no title)

--- app/components/dashboard/card/tooltip_component.rb
+++ app/components/dashboard/card/tooltip_component.rb
@@ -12,13 +12,27 @@ class Dashboard::Card::TooltipComponent < ApplicationComponent
   def tooltip_content
     return definitions.to_s if !definitions.is_a? Hash
     definitions.collect do |name, description|
-      content_tag :p, class: "mb-4px" do
-        if name != "Note"
-          content_tag(:strong, name) << ": #{description}"
-        else
-          content_tag(:i, description)
+      
+      if name == "divider"
+        tag :hr, class: 'bg-white o-65 mt-4px mb-4px'
+      else
+        content_tag :p, class: "mb-4px" do
+          if name == "Note"
+            content_tag(:i, description)
+          elsif name == ""
+            description
+          else
+            content_tag(:strong, name) << ": #{description}"
+          end
         end
-      end
+      end  
+        # elsif name == "divider"
+        #   content_tag :hr, class: 'bg-white mt-4px mb-4px'
+        
+        # else
+          
+        
+      # end
     end.join
   end
 end
