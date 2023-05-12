4357f8405360b20779a1995a5ec1adb1d008efa5
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-05-12 22:03:36 +0530

    Webpack 5 comes with terser, use that instead
    https://stackoverflow.com/questions/65298689/uglifyjs-and-webpack-v5

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,9 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
-webpackConfig["optimization"] = {}
-webpackConfig["optimization"]["minimizer"] = [new UglifyJsPlugin()]
+webpackConfig["optimization"] = {
+  minimize: true,
+  minimizer: [new TerserPlugin()],
+}
 
 module.exports = webpackConfig
