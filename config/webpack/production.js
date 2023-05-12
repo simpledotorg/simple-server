b58edf3d82a1ea5351458881e6e6a93672a8968e
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-05-12 17:02:14 +0530

    Use a webpack compatible uglifier

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,19 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+webpackConfig["plugins"]["UglifyJs"] = new UglifyJsPlugin({
+  uglifyOptions: {
+    compress: {
+      unused: true,
+      dead_code: true,
+      warnings: false,
+    },
+    output: {
+      comments: false,
+    },
+  },
+})
 
 module.exports = webpackConfig
