b875163a60135ffa039528900a4469cac81b9ad9
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-05-15 17:04:21 +0530

    Don't need minimize true and minimizer both

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,11 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
-const TerserPlugin = require("terser-webpack-plugin");
 
 webpackConfig["optimization"] = {
   minimize: true,
-  minimizer: [new TerserPlugin()],
 }
 
 module.exports = webpackConfig
