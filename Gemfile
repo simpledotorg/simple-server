28f27f4a19278057792892717c39eaaef2774b74
prabhanshuguptagit <guptaprabhanshu1@gmail.com>
2023-05-19 15:01:48 +0530

    Use webpack terser

--- Gemfile
+++ Gemfile
@@ -112,6 +112,7 @@ group :development, :test do
   gem "rswag-specs"
   gem "shoulda-matchers", "~> 5.1.0"
   gem "standard", "1.6.0", require: false
+  gem "terser", '~> 1.1'
 end
 
 group :development, :test, :profiling do
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,13 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
-const TerserPlugin = require("terser-webpack-plugin");
-
-console.log("========================")
-console.log(webpackConfig)
-webpackConfig["optimization"]["minimizer"] = [new TerserPlugin()]
-webpackConfig["optimization"]["minimize"] = true
-console.log("========================")
-console.log(webpackConfig)
 
 module.exports = webpackConfig
