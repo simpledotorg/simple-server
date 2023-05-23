8a6c5e525887bb371e028d2a7feecaffeab04e31
danySam <danysamsp@gmail.com>
2023-05-23 15:44:40 +0530

    Minor query refactor

--- db/views/reporting_overdue_patients_v01.sql
+++ db/views/reporting_overdue_patients_v01.sql
@@ -26,7 +26,7 @@ WITH patients_with_appointments AS (
         appointments.device_created_at AS previous_appointment_date,
         appointments.scheduled_date AS previous_appointment_schedule_date
     FROM reporting_patient_states rps
-             LEFT JOIN appointments ON appointments.patient_id = rps.patient_id
+    LEFT JOIN appointments ON appointments.patient_id = rps.patient_id
         AND appointments.device_created_at < rps.month_date
     WHERE rps.status <> 'dead'
     ORDER BY
@@ -35,91 +35,91 @@ WITH patients_with_appointments AS (
         appointments.device_created_at DESC
 ),
 
-     patients_with_appointments_and_visits AS (
-         SELECT
-             DISTINCT ON ( patients_with_appointments.patient_id, patients_with_appointments.month_date)
-             patients_with_appointments.*,
-             visit_id,
-             visited_at_after_appointment
-         FROM patients_with_appointments
-                  LEFT JOIN lateral (
-             SELECT id AS visit_id, patient_id, recorded_at AS visited_at_after_appointment
-             FROM blood_sugars
-             WHERE deleted_at IS NULL
-               AND patient_id = patients_with_appointments.patient_id
-               AND patients_with_appointments.previous_appointment_date < blood_sugars.recorded_at
-               AND blood_sugars.recorded_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
-             UNION
-             (
-                 SELECT id AS visit_id, patient_id, recorded_at AS visited_at_after_appointment
-                 FROM blood_pressures
-                 WHERE deleted_at IS NULL
-                   AND patient_id = patients_with_appointments.patient_id
-                   AND patients_with_appointments.previous_appointment_date < blood_pressures.recorded_at
-                   AND blood_pressures.recorded_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
-             )
-             UNION
-             (
-                 SELECT id AS visit_id, patient_id, device_created_at AS visited_at_after_appointment
-                 FROM appointments AS patients_with_appointments_visit
-                 WHERE deleted_at IS NULL
-                   AND patient_id = patients_with_appointments.patient_id
-                   AND patients_with_appointments.previous_appointment_date < patients_with_appointments_visit.device_created_at
-                   AND patients_with_appointments_visit.device_created_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
-             )
-             UNION
-             (
-                 SELECT id AS visit_id, patient_id, device_created_at AS visited_at_after_appointment
-                 FROM prescription_drugs
-                 WHERE deleted_at IS NULL
-                   AND patient_id = patients_with_appointments.patient_id
-                   AND patients_with_appointments.previous_appointment_date < prescription_drugs.device_created_at
-                   AND prescription_drugs.device_created_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
-             )
-             ) AS visits ON patients_with_appointments.patient_id = visits.patient_id
-         ORDER BY
-             patients_with_appointments.patient_id,
-             patients_with_appointments.month_date,
-             patients_with_appointments.previous_appointment_date DESC,
-             visited_at_after_appointment
-     ),
+patients_with_appointments_and_visits AS (
+    SELECT
+        DISTINCT ON ( patients_with_appointments.patient_id, patients_with_appointments.month_date)
+        patients_with_appointments.*,
+        visit_id,
+        visited_at_after_appointment
+    FROM patients_with_appointments
+    LEFT JOIN lateral (
+        SELECT id AS visit_id, patient_id, recorded_at AS visited_at_after_appointment
+        FROM blood_sugars
+        WHERE deleted_at IS NULL
+          AND patient_id = patients_with_appointments.patient_id
+          AND patients_with_appointments.previous_appointment_date < blood_sugars.recorded_at
+          AND blood_sugars.recorded_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
+        UNION
+        (
+            SELECT id AS visit_id, patient_id, recorded_at AS visited_at_after_appointment
+            FROM blood_pressures
+            WHERE deleted_at IS NULL
+              AND patient_id = patients_with_appointments.patient_id
+              AND patients_with_appointments.previous_appointment_date < blood_pressures.recorded_at
+              AND blood_pressures.recorded_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
+        )
+        UNION
+        (
+            SELECT id AS visit_id, patient_id, device_created_at AS visited_at_after_appointment
+            FROM appointments AS patients_with_appointments_visit
+            WHERE deleted_at IS NULL
+              AND patient_id = patients_with_appointments.patient_id
+              AND patients_with_appointments.previous_appointment_date < patients_with_appointments_visit.device_created_at
+              AND patients_with_appointments_visit.device_created_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
+        )
+        UNION
+        (
+            SELECT id AS visit_id, patient_id, device_created_at AS visited_at_after_appointment
+            FROM prescription_drugs
+            WHERE deleted_at IS NULL
+              AND patient_id = patients_with_appointments.patient_id
+              AND patients_with_appointments.previous_appointment_date < prescription_drugs.device_created_at
+              AND prescription_drugs.device_created_at < patients_with_appointments.month_date + INTERVAL '1 month' + INTERVAL '15 days'
+        )
+    ) AS visits ON patients_with_appointments.patient_id = visits.patient_id
+    ORDER BY
+        patients_with_appointments.patient_id,
+        patients_with_appointments.month_date,
+        patients_with_appointments.previous_appointment_date DESC,
+        visited_at_after_appointment
+),
 
-     patient_with_call_results AS (
-         SELECT
-             DISTINCT ON ( patients_with_appointments_and_visits.patient_id, patients_with_appointments_and_visits.month_date)
-             patients_with_appointments_and_visits.*,
-             previous_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS previous_called_at,
-             previous_call_results.result_type AS previous_call_result_type,
-             previous_call_results.remove_reason AS previous_call_removed_from_overdue_list_reason,
-             next_call_results.device_created_at AT TIME ZONE 'UTC'  AT TIME ZONE 'UTC'  AS next_called_at,
-             next_call_results.result_type AS next_call_result_type,
-             next_call_results.remove_reason as next_call_removed_from_overdue_list_reason,
-             next_call_results.user_id AS called_by_user_id
-         FROM patients_with_appointments_and_visits
-                  LEFT JOIN call_results previous_call_results ON patients_with_appointments_and_visits.patient_id = previous_call_results.patient_id
-             AND previous_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) < patients_with_appointments_and_visits.month_date
-             AND previous_call_results.device_created_at > previous_appointment_schedule_date
-                  LEFT JOIN call_results next_call_results ON patients_with_appointments_and_visits.patient_id = next_call_results.patient_id
-             AND next_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) >= patients_with_appointments_and_visits.month_date
-             AND next_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) < patients_with_appointments_and_visits.month_date + INTERVAL '1 month'
-         ORDER BY
-             patients_with_appointments_and_visits.patient_id,
-             patients_with_appointments_and_visits.month_date,
-             next_call_results.device_created_at,
-             previous_call_results.device_created_at DESC
-     ),
+patient_with_call_results AS (
+    SELECT
+        DISTINCT ON ( patients_with_appointments_and_visits.patient_id, patients_with_appointments_and_visits.month_date)
+        patients_with_appointments_and_visits.*,
+        previous_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS previous_called_at,
+        previous_call_results.result_type AS previous_call_result_type,
+        previous_call_results.remove_reason AS previous_call_removed_from_overdue_list_reason,
+        next_call_results.device_created_at AT TIME ZONE 'UTC'  AT TIME ZONE 'UTC'  AS next_called_at,
+        next_call_results.result_type AS next_call_result_type,
+        next_call_results.remove_reason as next_call_removed_from_overdue_list_reason,
+        next_call_results.user_id AS called_by_user_id
+    FROM patients_with_appointments_and_visits
+    LEFT JOIN call_results previous_call_results ON patients_with_appointments_and_visits.patient_id = previous_call_results.patient_id
+        AND previous_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) < patients_with_appointments_and_visits.month_date
+        AND previous_call_results.device_created_at > previous_appointment_schedule_date
+    LEFT JOIN call_results next_call_results ON patients_with_appointments_and_visits.patient_id = next_call_results.patient_id
+        AND next_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) >= patients_with_appointments_and_visits.month_date
+        AND next_call_results.device_created_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE')) < patients_with_appointments_and_visits.month_date + INTERVAL '1 month'
+    ORDER BY
+        patients_with_appointments_and_visits.patient_id,
+        patients_with_appointments_and_visits.month_date,
+        next_call_results.device_created_at,
+        previous_call_results.device_created_at DESC
+),
 
-     patient_with_call_results_and_phone AS (
-         SELECT
-             DISTINCT ON ( patient_with_call_results.patient_id, patient_with_call_results.month_date)
-             patient_with_call_results.*,
-             patient_phone_numbers.number AS patient_phone_number
-         FROM patient_with_call_results
-                  LEFT JOIN patient_phone_numbers ON patient_phone_numbers.patient_id = patient_with_call_results.patient_id
-         ORDER BY
-             patient_with_call_results.patient_id,
-             patient_with_call_results.month_date
-     )
+patient_with_call_results_and_phone AS (
+    SELECT
+        DISTINCT ON ( patient_with_call_results.patient_id, patient_with_call_results.month_date)
+        patient_with_call_results.*,
+        patient_phone_numbers.number AS patient_phone_number
+    FROM patient_with_call_results
+    LEFT JOIN patient_phone_numbers ON patient_phone_numbers.patient_id = patient_with_call_results.patient_id
+    ORDER BY
+        patient_with_call_results.patient_id,
+        patient_with_call_results.month_date
+)
 
 SELECT
     month_date,
@@ -189,5 +189,5 @@ SELECT
        WHEN next_call_result_type = 'removed_from_overdue_list' THEN 'yes'
        ELSE 'no'
        END AS removed_from_overdue_list_during_the_month
-
 FROM patient_with_call_results_and_phone;
+
