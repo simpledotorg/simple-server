635643d9b25e29687799723a28618d6ee0939132
danySam <danysamsp@gmail.com>
2023-05-26 16:43:50 +0530

    Fix issue with relative dates in test
    Tests would have started failing in a couple of months since
    OverduePatients only hold last 24 months of data.

--- spec/models/reports/overdue_patient_spec.rb
+++ spec/models/reports/overdue_patient_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
+  timezone = Time.find_zone(Period::REPORTING_TIME_ZONE)
+  this_month = timezone.local(Date.today.year, Date.today.month, 1, 0, 0, 0)
+
   around do |example|
-    freeze_time_for_reporting_specs(example)
+    # This is in the style of ReportingHelpers::freeze_time_for_reporting_specs.
+    # Since this view only keeps the last 6 months of data, the date cannot be a
+    # fixed point in time like the spec helper.
+    Timecop.freeze("#{Date.today.end_of_month} 23:00 IST") do
+      example.run
+    end
   end
 
   describe "Associations" do
@@ -27,7 +35,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "next_called_at" do
       it "should only select the first call that took place during the month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         call_result_1 = create(:call_result, patient: patient, device_created_at: month_date + 2.days)
         _call_result_2 = create(:call_result, patient: patient, device_created_at: month_date + 3.days)
 
@@ -41,7 +49,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should not select the call that took place after the month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         _call_result_1 = create(:call_result, patient: patient, device_created_at: month_date + 1.month)
 
         RefreshReportingViews.refresh_v2
@@ -53,7 +61,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should not select the call that took place before the month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         _call_result_1 = create(:call_result, patient: patient, device_created_at: month_date - 1.hour)
 
         RefreshReportingViews.refresh_v2
@@ -67,7 +75,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "previous_called_at" do
       it "should only select the latest call in the previous month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: june_2021[:two_months_ago],
                                            scheduled_date: month_date - 15.days)
         _call_result_1 = create(:call_result, patient: patient, appointment: appointment,
@@ -85,7 +93,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should only select the call that was made after the scheduled date of latest appointment" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: june_2021[:two_months_ago],
                                            scheduled_date: month_date - 15.days)
         _call_result_1 = create(:call_result, patient: patient, appointment: appointment,
@@ -94,7 +102,8 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
         RefreshReportingViews.refresh_v2
 
         with_reporting_time_zone do
-          expect(described_class.find_by(patient_id: patient.id, month_date: month_date).previous_called_at).to be_nil
+          expect(described_class.find_by(patient_id: patient.id,
+            month_date: month_date).previous_called_at).to be_nil
         end
       end
     end
@@ -102,12 +111,12 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "visited_at_after_appointment" do
       it "should only select the first visit that took place after the appointment" do
         patient = create(:patient)
-        month_date = june_2021[:now]
-        _appointment = create(:appointment, patient: patient, device_created_at: june_2021[:now] - 7.days)
+        month_date = this_month
+        _appointment = create(:appointment, patient: patient, device_created_at: this_month - 7.days)
 
-        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: june_2021[:now] + 5.days)
-        _appointment_visit = create(:appointment, patient: patient, device_created_at: june_2021[:now] + 3.days)
-        blood_sugar_visit = create(:blood_sugar, patient: patient, recorded_at: june_2021[:now] - 2.days)
+        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: this_month + 5.days)
+        _appointment_visit = create(:appointment, patient: patient, device_created_at: this_month + 3.days)
+        blood_sugar_visit = create(:blood_sugar, patient: patient, recorded_at: this_month - 2.days)
 
         RefreshReportingViews.refresh_v2
 
@@ -119,12 +128,12 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should not select visits that took place before the appointment or after 15 days of the next month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
-        _appointment = create(:appointment, patient: patient, device_created_at: june_2021[:now] - 7.days)
+        month_date = this_month
+        _appointment = create(:appointment, patient: patient, device_created_at: this_month - 7.days)
 
-        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: june_2021[:now] - 8.days)
+        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: this_month - 8.days)
         _appointment_visit = create(:appointment, patient: patient,
-                                                 device_created_at: june_2021[:now] + 1.month + 16.days)
+                                                  device_created_at: this_month + 1.month + 16.days)
 
         RefreshReportingViews.refresh_v2
 
@@ -136,12 +145,12 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should only select visits that took place in the current month or within the first 15 days of the next month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
-        _appointment = create(:appointment, patient: patient, device_created_at: june_2021[:now] - 7.days)
+        month_date = this_month
+        _appointment = create(:appointment, patient: patient, device_created_at: this_month - 7.days)
 
-        _appointment_visit = create(:appointment, patient: patient, device_created_at: june_2021[:now] + 3.days)
-        blood_sugar_visit = create(:blood_sugar, patient: patient, recorded_at: june_2021[:now] - 2.days)
-        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: june_2021[:now] - 8.days)
+        _appointment_visit = create(:appointment, patient: patient, device_created_at: this_month + 3.days)
+        blood_sugar_visit = create(:blood_sugar, patient: patient, recorded_at: this_month - 2.days)
+        _blood_pressure_visit = create(:blood_pressure, patient: patient, recorded_at: this_month - 8.days)
 
         RefreshReportingViews.refresh_v2
 
@@ -153,10 +162,11 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should select visits that take place in the first 15 days of the next month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
-        _appointment = create(:appointment, patient: patient, device_created_at: june_2021[:now] - 7.days)
+        month_date = this_month
+        _appointment = create(:appointment, patient: patient, device_created_at: this_month - 7.days)
 
-        blood_pressure_visit = create(:blood_pressure, patient: patient, device_created_at: june_2021[:now] + 1.month + 15.days)
+        blood_pressure_visit = create(:blood_pressure, patient: patient,
+                                                       device_created_at: this_month + 1.month + 15.days)
 
         RefreshReportingViews.refresh_v2
 
@@ -170,9 +180,10 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "previous_appointment_id" do
       it "should only select the latest appointment in the previous month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         _appointment_1 = create(:appointment, patient: patient, device_created_at: june_2021[:two_months_ago])
-        appointment_2 = create(:appointment, patient: patient, device_created_at: june_2021[:two_months_ago] + 15.days)
+        appointment_2 = create(:appointment, patient: patient,
+                                             device_created_at: june_2021[:two_months_ago] + 15.days)
 
         RefreshReportingViews.refresh_v2
 
@@ -184,7 +195,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should pick cancelled appointments also" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         _appointment_1 = create(:appointment, patient: patient, device_created_at: month_date - 30.days)
         appointment_2 = create(:appointment, patient: patient, device_created_at: month_date - 15.days,
                                              status: :cancelled)
@@ -201,7 +212,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "is_overdue" do
       it "should be no when the previous appointments scheduled date is during the month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 1.month, scheduled_date: month_date)
 
         RefreshReportingViews.refresh_v2
@@ -213,7 +224,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be yes when the previous appointments scheduled date is in the previous month and the visit date is during the month" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                              scheduled_date: month_date - 15.days)
         create(:blood_pressure, patient: patient, recorded_at: month_date + 1.day)
@@ -227,7 +238,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when the previous appointments scheduled date and visited date is in the previous month and the visit took place after the scheduled date" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                              scheduled_date: month_date - 15.days)
         create(:blood_pressure, patient: patient, recorded_at: month_date - 30.days + 16.days)
@@ -263,7 +274,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "has_visited_following_call" do
       it "should be no when there was no visit" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date - 14.days, appointment: appointment)
@@ -277,7 +288,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when a call was not made" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                              scheduled_date: month_date - 15.days)
         create(:blood_pressure, patient: patient, recorded_at: month_date - 14.days)
@@ -292,7 +303,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be yes when a call was made and the patient visited within 15 days" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date, appointment: appointment)
@@ -308,7 +319,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when a call was made but the patient visited after 15 days of first call" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date, appointment: appointment)
@@ -326,7 +337,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "removed_from_overdue_list" do
       it "should be yes when the call result corresponding to the previous appointment is removed_from_overdue_list" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date - 14.days, appointment: appointment,
@@ -341,7 +352,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when the call result corresponding to the previous appointment is not removed_from_overdue_list" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date - 14.days, appointment: appointment,
@@ -356,7 +367,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when a call was not made after the previous appointment" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                              scheduled_date: month_date - 15.days)
 
@@ -372,7 +383,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
     describe "has_called" do
       it "should be yes when a call was made during the month after the previous appointment" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         appointment = create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                                            scheduled_date: month_date - 15.days)
         create(:call_result, patient: patient, device_created_at: month_date, appointment: appointment)
@@ -385,7 +396,7 @@ RSpec.describe Reports::OverduePatient, {type: :model, reporting_spec: true} do
 
       it "should be no when a call was not made during the month after the previous appointment" do
         patient = create(:patient)
-        month_date = june_2021[:now]
+        month_date = this_month
         create(:appointment, patient: patient, device_created_at: month_date - 30.days,
                              scheduled_date: month_date - 15.days)
 
