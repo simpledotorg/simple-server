a9b5ed73fd1702be1f7ad58ad609677758803bee
roy peter <roypeter@W5V6DYL96K-rpeter.local>
2023-06-01 11:14:12 +0530

    Remove sandbox cap vm configs

--- config/deploy/sandbox.rb
+++
@@ -1,3 +0,0 @@
-server "ec2-3-109-144-155.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron whitelist_phone_numbers seed_data]
-server "ec2-3-108-193-51.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
-server "ec2-65-0-103-254.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
