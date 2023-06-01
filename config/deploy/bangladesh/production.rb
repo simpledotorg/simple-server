36abd765bf3348e9d5cc7c27ac6424b5f3742bbf
roy peter <roypeter@W5V6DYL96K-rpeter.local>
2023-06-01 17:59:30 +0530

    Delete bangladesh cap config

--- config/deploy/bangladesh/production.rb
+++
@@ -1,3 +0,0 @@
-server "ec2-3-110-211-60.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db cron]
-server "ec2-3-110-137-28.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web app db]
-server "ec2-13-234-56-146.ap-south-1.compute.amazonaws.com", user: "deploy", roles: %w[web sidekiq]
