9ce1103705b15ae5d50eef1fa6ed19bf88dd834b
Priyanga P Kini <priyangapkini99@gmail.com>
2023-06-14 22:52:56 +0530

    Update migration file

--- db/migrate/20230614171507_update_overdue_patient_to_version_02.rb
+++ db/migrate/20230614171507_update_overdue_patient_to_version_02.rb
@@ -2,9 +2,9 @@ class UpdateOverduePatientToVersion02 < ActiveRecord::Migration[6.1]
   def change
     drop_view :reporting_facility_states, materialized: true
     update_view :reporting_overdue_patients,
-      version: 2,
-      revert_to_version: 1,
-      materialized: true
+                version: 2,
+                revert_to_version: 1,
+                materialized: true
     create_view :reporting_facility_states, materialized: true, version: 11
     add_index :reporting_facility_states, [:month_date, :facility_region_id], name: :index_fs_month_date_region_id, unique: true
     add_index :reporting_facility_states, [:block_region_id, :month_date], name: :index_fs_block_month_date
