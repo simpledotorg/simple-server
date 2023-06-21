c2db03e70afbe14ec5b6f0921e8cf5bbbe4b4ea6
Atharva Raykar <atharva@nilenso.com>
2023-06-21 13:06:55 +0530

    facility bulk upload: accept business identifiers
    We add a new field to the bulk upload template that if populated, would
    also create a business identifier mapping to the facility.
    
    This is an important first step that will let us accept external
    organisations that want to export their data and see the reports on the
    Simple dashboard.
    
    We also correct a mistake in the template which says that the facility
    size field is optional. This is untrue since 2cdf24c and this was likely
    something that was missed when that change was made.

--- app/jobs/import_facilities_job.rb
+++ app/jobs/import_facilities_job.rb
@@ -2,15 +2,18 @@ class ImportFacilitiesJob < ApplicationJob
   def perform(facilities)
     import_facilities = []
 
-    facilities.each do |facility|
+    facilities.each do |facility:, business_identifiers:|
       organization = Organization.find_by(name: facility[:organization_name])
       facility_group = FacilityGroup.find_by(name: facility[:facility_group_name], organization_id: organization.id)
       import_facility = Facility.new(facility.merge!(facility_group_id: facility_group.id))
+      business_identifiers.each do |identifier|
+        import_facility.business_identifiers.build(identifier)
+      end
       import_facilities << import_facility
     end
 
     ActiveRecord::Base.transaction do
-      Facility.import!(import_facilities, validate: true)
+      Facility.import!(import_facilities, validate: true, recursive: true)
       # import! can't run callbacks, so we manually ensure that we create regions
       import_facilities.each(&:make_region)
     end
