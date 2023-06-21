95b5a3dab47e3a2fe577cdc0016348c33e510062
Akshatha <71076330+qptr@users.noreply.github.com>
2023-06-22 00:10:02 +0530

    Add spec for migration

--- db/data/20230620114230_set_up_six_month_ihci_sms_reminders.rb
+++ db/data/20230620114230_set_up_six_month_ihci_sms_reminders.rb
@@ -3,7 +3,7 @@
 class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
   PATIENTS_PER_DAY = 20_000
   FILTERS = {
-    'states' => { 'include' => ['Tamil Nadu, Andhra Pradesh, West Bengal'] }
+    "states" => {"include" => ["Tamil Nadu", "Andhra Pradesh", "West Bengal"]}
   }.freeze
   MONTHS = (7..12).map do |month_number|
     month = Date::ABBR_MONTHNAMES[month_number]
@@ -16,7 +16,7 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
   end
 
   def up
-    return unless CountryConfig.current_country?('India') && SimpleServer.env.production?
+    return unless CountryConfig.current_country?("India") && SimpleServer.env.production?
 
     MONTHS.map do |month|
       transaction do
@@ -27,9 +27,9 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
           max_patients_per_day: PATIENTS_PER_DAY,
           filters: FILTERS
         ).tap do |experiment|
-          cascade = experiment.treatment_groups.create!(description: 'official_short_cascade')
-          cascade.reminder_templates.create!(message: 'notifications.set03.official_short', remind_on_in_days: 3)
-          cascade.reminder_templates.create!(message: 'notifications.set03.official_short', remind_on_in_days: 7)
+          cascade = experiment.treatment_groups.create!(description: "official_short_cascade")
+          cascade.reminder_templates.create!(message: "notifications.set03.official_short", remind_on_in_days: 3)
+          cascade.reminder_templates.create!(message: "notifications.set03.official_short", remind_on_in_days: 7)
         end
       end
 
@@ -41,9 +41,9 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
           max_patients_per_day: PATIENTS_PER_DAY,
           filters: FILTERS
         ).tap do |experiment|
-          cascade = experiment.treatment_groups.create!(description: 'official_short_cascade')
-          cascade.reminder_templates.create!(message: 'notifications.set02.official_short', remind_on_in_days: 0)
-          cascade.reminder_templates.create!(message: 'notifications.set03.official_short', remind_on_in_days: 7)
+          cascade = experiment.treatment_groups.create!(description: "official_short_cascade")
+          cascade.reminder_templates.create!(message: "notifications.set02.official_short", remind_on_in_days: 0)
+          cascade.reminder_templates.create!(message: "notifications.set03.official_short", remind_on_in_days: 7)
         end
       end
     end
