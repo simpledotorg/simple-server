c0e118023309d3d27e58d1fd8f8497abedd57e40
Akshatha <71076330+qptr@users.noreply.github.com>
2023-06-23 12:51:46 +0530

    Minor refactoring in spec

--- spec/data/set_up_six_month_ihci_sms_reminders_spec.rb
+++ spec/data/set_up_six_month_ihci_sms_reminders_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 require Rails.root.join("db", "data", "20230620114230_set_up_six_month_ihci_sms_reminders")
 
 RSpec.describe SetUpSixMonthIhciSmsReminders do
-  it "sets up cascading current and stale experiments for July through December" do
+  it "sets up cascading current and stale experiments for July through December 2023" do
     allow(CountryConfig).to receive(:current_country?).with("India").and_return true
     stub_const("SIMPLE_SERVER_ENV", "production")
 
@@ -13,14 +13,14 @@ RSpec.describe SetUpSixMonthIhciSmsReminders do
     expect(Experimentation::CurrentPatientExperiment.count).to eq(6)
     expect(Experimentation::StalePatientExperiment.count).to eq(6)
 
-    current_cascade = Experimentation::CurrentPatientExperiment.first.reminder_templates
-    expect(current_cascade.count).to eq(2)
-    expect(current_cascade.find_by(remind_on_in_days: 3).message).to eq("notifications.set03.official_short")
-    expect(current_cascade.find_by(remind_on_in_days: 7).message).to eq("notifications.set03.official_short")
+    current_reminder_templates = Experimentation::CurrentPatientExperiment.first.reminder_templates
+    expect(current_reminder_templates.count).to eq(2)
+    expect(current_reminder_templates.find_by(remind_on_in_days: 3).message).to eq("notifications.set03.official_short")
+    expect(current_reminder_templates.find_by(remind_on_in_days: 7).message).to eq("notifications.set03.official_short")
 
-    stale_cascade = Experimentation::StalePatientExperiment.first.reminder_templates
-    expect(stale_cascade.count).to eq(2)
-    expect(stale_cascade.find_by(remind_on_in_days: 0).message).to eq("notifications.set02.official_short")
-    expect(stale_cascade.find_by(remind_on_in_days: 7).message).to eq("notifications.set03.official_short")
+    stale_reminder_templates = Experimentation::StalePatientExperiment.first.reminder_templates
+    expect(stale_reminder_templates.count).to eq(2)
+    expect(stale_reminder_templates.find_by(remind_on_in_days: 0).message).to eq("notifications.set02.official_short")
+    expect(stale_reminder_templates.find_by(remind_on_in_days: 7).message).to eq("notifications.set03.official_short")
   end
 end
