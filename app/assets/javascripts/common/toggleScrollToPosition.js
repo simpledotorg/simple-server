c3d6f2ff31e71de19b08182816d41f76875a8194
Jamie Carter <9541902+jamiecarter7@users.noreply.github.com>
2023-06-26 13:08:37 +0100

    Concise code
    - remove early extraction
    - rename localStorage variable with explicit naming

--- app/assets/javascripts/common/toggleScrollToPosition.js
+++ app/assets/javascripts/common/toggleScrollToPosition.js
@@ -1,31 +1,18 @@
 // Check for scroll position and scroll
 window.addEventListener("DOMContentLoaded", (event) => {
-  if (doesYPositionExistInLocalStorage()) {
-    scrollToYPosition();
-    removeYPositionLocalStorage();
+  if (localStorage.getItem("yScrollPosition")) {
+    window.scrollTo(
+      0,
+      parseInt(localStorage.getItem("yScrollPosition"))
+    );
+    localStorage.removeItem("yScrollPosition");
   }
 });
 
 // Create toggle change listener and add y position on 'change' event
-function createToggleListener(id) {
+function createScrollTrackingListener(id) {
   const toggleElement = document.getElementById(id);
   toggleElement.addEventListener("change", (event) => {
-    setYPositionLocalStorage();
+    localStorage.setItem("yScrollPosition", window.pageYOffset);
   });
-}
-
-function setYPositionLocalStorage() {
-  localStorage.setItem("toggleScrollToPosition", window.pageYOffset);
-}
-
-function removeYPositionLocalStorage() {
-  localStorage.removeItem("toggleScrollToPosition");
-}
-
-function doesYPositionExistInLocalStorage() {
-    return localStorage.getItem("toggleScrollToPosition")
-}
-
-function scrollToYPosition() {
-  window.scrollTo(0, parseInt(localStorage.getItem("toggleScrollToPosition")));
 }
