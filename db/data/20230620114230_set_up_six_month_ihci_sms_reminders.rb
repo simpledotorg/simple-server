67f761ee58a0aacf2673a83e910dc94c96c18109
Akshatha <71076330+qptr@users.noreply.github.com>
2023-06-28 08:31:05 +0530

    Address review comment

--- db/data/20230620114230_set_up_six_month_ihci_sms_reminders.rb
+++ db/data/20230620114230_set_up_six_month_ihci_sms_reminders.rb
@@ -5,11 +5,11 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
   FILTERS = {
     "states" => {"include" => ["Tamil Nadu", "Andhra Pradesh", "West Bengal"]}
   }.freeze
-  MONTHS = (7..12).map do |month_number|
+  EXPERIMENTS_DATA = (7..12).map do |month_number|
     month = Date::ABBR_MONTHNAMES[month_number]
     {
-      start_time: DateTime.new(2023, month_number, 1).beginning_of_day,
-      end_time: DateTime.new(2023, month_number, 1).end_of_month,
+      start_time: DateTime.new(2023, month_number).beginning_of_month,
+      end_time: DateTime.new(2023, month_number).end_of_month,
       current_patients_experiment_name: "Current Patient #{month} 2023",
       stale_patients_experiment_name: "Stale Patient #{month} 2023"
     }
@@ -18,12 +18,12 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
   def up
     return unless CountryConfig.current_country?("India") && SimpleServer.env.production?
 
-    MONTHS.map do |month|
+    EXPERIMENTS_DATA.map do |experiment_data|
       ActiveRecord::Base.transaction do
         Experimentation::Experiment.current_patients.create!(
-          name: month[:current_patients_experiment_name],
-          start_time: month[:start_time],
-          end_time: month[:end_time],
+          name: experiment_data[:current_patients_experiment_name],
+          start_time: experiment_data[:start_time],
+          end_time: experiment_data[:end_time],
           max_patients_per_day: PATIENTS_PER_DAY,
           filters: FILTERS
         ).tap do |experiment|
@@ -35,9 +35,9 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
 
       ActiveRecord::Base.transaction do
         Experimentation::Experiment.stale_patients.create!(
-          name: month[:stale_patients_experiment_name],
-          start_time: month[:start_time],
-          end_time: month[:end_time],
+          name: experiment_data[:stale_patients_experiment_name],
+          start_time: experiment_data[:start_time],
+          end_time: experiment_data[:end_time],
           max_patients_per_day: PATIENTS_PER_DAY,
           filters: FILTERS
         ).tap do |experiment|
@@ -50,9 +50,9 @@ class SetUpSixMonthIhciSmsReminders < ActiveRecord::Migration[6.1]
   end
 
   def down
-    MONTHS.map do |month|
-      Experimentation::Experiment.current_patients.find_by_name(month[:current_patients_experiment_name])&.cancel
-      Experimentation::Experiment.stale_patients.find_by_name(month[:stale_patients_experiment_name])&.cancel
+    EXPERIMENTS_DATA.map do |experiment_data|
+      Experimentation::Experiment.current_patients.find_by_name(experiment_data[:current_patients_experiment_name])&.cancel
+      Experimentation::Experiment.stale_patients.find_by_name(experiment_data[:stale_patients_experiment_name])&.cancel
     end
   end
 end
