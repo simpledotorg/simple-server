962ebc33fb2e0310f198522ba39aa9e786b306d0
Atharva Raykar <atharva@nilenso.com>
2023-07-13 17:26:00 +0530

    secure the import API with OAuth
    We use the `doorkeeper` library to implement the authorization endpoint.
    Only the client credentials grant is used, and we set the token expiry
    to 8 hours.
    
    We also create a machine user model that will be the resource owner
    associated with the tokens and grants.

--- spec/api/v4/imports/imports_spec.rb
+++ spec/api/v4/imports/imports_spec.rb
@@ -3,25 +3,34 @@ require "swagger_helper"
 describe "Import v4 API", swagger_doc: "v4/import.json" do
   before { Flipper.enable(:imports_api) }
   path "/import" do
+    let(:organization) { FactoryBot.create(:organization) }
+    let(:resource) { build_patient_import_resource }
+    let(:machine_user) { FactoryBot.create(:machine_user, organization: organization) }
+    let(:application) { FactoryBot.create(:oauth_application, owner: machine_user) }
+    let(:token) {
+      FactoryBot.create(:oauth_access_token,
+        application: application,
+        scopes: "write",
+        resource_owner_id: machine_user.id)
+    }
     put "Send bulk resources to Simple" do
       tags "import"
       security [access_token: [], import_auth: ["write"]]
-      parameter name: :organization_header,
+      parameter name: :HTTP_X_ORGANIZATION_ID,
         in: :header,
         type: :uuid,
-        description: "UUID of organization"
+        description: "UUID of organization. The header key should be passed as 'X-Organization-ID'."
       parameter name: :import_request, in: :body, schema: Api::V4::Imports.import_request_payload
 
       response "202", "Accepted" do
-        let(:organization_header) { SecureRandom.uuid }
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64("bogusbogus")}" }
+        let(:HTTP_X_ORGANIZATION_ID) { organization.id }
+        let(:Authorization) { "Bearer #{token.token}" }
         let(:import_request) { {"resources" => [build_patient_import_resource]} }
-        run_test!
       end
 
       response "400", "Bad Request" do
-        let(:organization_header) { SecureRandom.uuid }
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64("bogusbogus")}" }
+        let(:HTTP_X_ORGANIZATION_ID) { organization.id }
+        let(:Authorization) { "Bearer #{token.token}" }
         let(:import_request) { {"resources" => [{"invalid" => "invalid"}]} }
         run_test!
       end
