32b0fd2e1517a9e90fb1dc651e4bb72374de6519
Atharva Raykar <atharva@nilenso.com>
2023-07-13 17:26:00 +0530

    secure the import API with OAuth
    We use the `doorkeeper` library to implement the authorization endpoint.
    Only the client credentials grant is used, and we set the token expiry
    to 8 hours.
    
    We also create a machine user model that will be the resource owner
    associated with the tokens and grants.

--- app/controllers/api/v4/imports_controller.rb
+++ app/controllers/api/v4/imports_controller.rb
@@ -1,5 +1,6 @@
 class Api::V4::ImportsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :doorkeeper_authorize!
 
   def import
     return head :not_found unless Flipper.enabled?(:imports_api)
