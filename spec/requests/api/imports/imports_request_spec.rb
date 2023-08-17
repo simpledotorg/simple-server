f0ddf250c8461e22b6ab82042e5752758620714a
Atharva Raykar <atharva@nilenso.com>
2023-08-17 13:10:08 +0530

    bulk api import: allow importing conditions and medication requests

--- spec/requests/api/imports/imports_request_spec.rb
+++ spec/requests/api/imports/imports_request_spec.rb
@@ -45,6 +45,18 @@ RSpec.describe "Import API", type: :request do
     expect(response.status).to eq(202)
   end
 
+  it "imports medication request resources" do
+    put route, params: {resources: [build_medication_request_import_resource]}.to_json, headers: headers
+
+    expect(response.status).to eq(202)
+  end
+
+  it "imports condition resources" do
+    put route, params: {resources: [build_condition_import_resource]}.to_json, headers: headers
+
+    expect(response.status).to eq(202)
+  end
+
   it "fails to import invalid resources" do
     put route, params: {resources: [invalid_payload]}.to_json, headers: headers
 
