3129130335b7ed070d64ac55266dd38b46d15ed3
Govind krishna Joshi <govind@nilenso.com>
2023-09-07 13:22:59 +0530

    Add a migration to disable users in migrated facilities

---
+++ db/data/20230907045337_disable_users_in_migrated_facilities.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class DisableUsersInMigratedFacilities < ActiveRecord::Migration[6.1]
+  REGIONS_WITH_ONGOING_ACCESS = [
+    {region_type: :state, slug: "west-bengal"},
+    {region_type: :state, slug: "nagaland"},
+    {region_type: :district, slug: "chennai"},
+    {region_type: :district, slug: "pimpri-chinchwad-municipal-corporation"},
+    {region_type: :district, slug: "pune-municipal-corporation"}
+  ]
+  def up
+    users_with_remaining_access = REGIONS_WITH_ONGOING_ACCESS.flat_map do |region_details|
+      region = Region.find_by(region_details)
+      next unless region
+
+      if region.district_region?
+        region.source.users.sync_approval_status_allowed
+      elsif region.state_region?
+        region.children.flat_map { |district| district.source.users.sync_approval_status_allowed }
+      end
+    end
+
+    all_allowed_users = User.all.sync_approval_status_allowed.pluck(:id)
+    disabled_users = all_allowed_users - users_with_remaining_access.map(&:id)
+
+    User.where(id: disabled_users).update_all(
+      sync_approval_status: :denied,
+      sync_approval_status_reason: "Instructions to disable users"
+    )
+  end
+
+  def down
+    print "DisableUsersInMigratedFacilities cannot be reversed."
+  end
+end
