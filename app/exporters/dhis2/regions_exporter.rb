d9cc40eda89de3f9c6430933e4e2ea7b6118c46d
Akshatha <71076330+qptr@users.noreply.github.com>
2023-09-11 17:43:09 +0530

    Get uid from response and store in config

--- app/exporters/dhis2/regions_exporter.rb
+++ app/exporters/dhis2/regions_exporter.rb
@@ -30,22 +30,26 @@ class Dhis2::RegionsExporter
     end
   end
 
+  # Can have a resume feature to keep track of org units created.
+  # can write to file/etc instead of storing in memory
   def export_region
     payload = {
       name: @region.name,
       shortName: @region.slug,
-      openingDate: @region.created_at.iso8601,
+      openingDate: @region.created_at.iso8601
     }
     if has_parent?(@region)
       payload.merge({parent: get_parent_uid(@region)})
     end
 
-    response = Dhis2.client.post(
-      path: "organisationUnit",
-      payload: payload
-    )
+    response = Dhis2.client.post(path: "organisationUnits",
+      payload: payload)
 
-    store_org_unit_uids(response)
+    if response.with_indifferent_access.fetch(:http_status_code).in?([200, 201])
+      store_org_unit_uids(response)
+    else
+      response.with_indifferent_access.dig(:response, :error_reports)
+    end
   end
 
   def has_parent?(region)
@@ -59,12 +63,22 @@ class Dhis2::RegionsExporter
 
   def get_uid(slug)
     DHIS2_CONFIG.fetch(:facilities_to_migrate).map do |facility|
-      if facility.fetch(:display_name) == slug
+      if facility.fetch(:facility_slug) == slug
         return facility.fetch(:org_unit_id)
       end
     end
   end
 
   def store_org_unit_uids(response)
+    response = response.with_indifferent_access
+    uid = response.dig(:response, :uid)
+
+    config = DHIS2_CONFIG
+    config[:facilities_to_migrate] << {
+      facility_slug: @region.slug,
+      org_unit_id: uid
+    }
+    File.write(ENV.fetch("DHIS2_TRACKER_CONFIG_FILE"), config.to_hash.to_yaml)
+    puts "wrote uid to file"
   end
 end
