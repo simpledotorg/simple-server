d81c7d8caf268867bf91aed1f4d1114c2999fb33
Atharva Raykar <atharva@nilenso.com>
2023-10-31 13:30:40 +0530

    import API: allow multiple organizations to share identifiers
    An organization that is using the import API is forced to ensure that
    their identifiers (for facilities and patients) do not clash with any
    identifier chosen by other organizations. This is a bug we did not
    intend to introduce, and was caused by us failing to include the
    organization ID in our UUID hash when deriving our own resource IDs.
    
    Thus our translation from partner org ID to Simple ID previously looked
    like:
        UUIDv5(their_resource_ID) => Simple Resource ID
    
    The issue is that if some Org1 has a Patient with ID of 1, and Org2 has
    a Patient with ID of 2, they will be translated to the same UUID value
    in Simple.
    
    To fix this, we now have:
        UUIDv5(their_org_ID + their_resource_ID) => Simple Resource ID
    
    which was the intended design.

--- app/jobs/bulk_api_import_job.rb
+++ app/jobs/bulk_api_import_job.rb
@@ -1,5 +1,5 @@
 class BulkApiImportJob < ApplicationJob
-  def perform(resources:)
-    BulkApiImport::Importer.new(resource_list: resources).import
+  def perform(resources:, organization_id:)
+    BulkApiImport::Importer.new(resource_list: resources, organization_id: organization_id).import
   end
 end
