f818a317390285d15e2becc2295452ea11ebb867
Akshatha <71076330+qptr@users.noreply.github.com>
2023-11-10 14:39:50 +0530

    Filter patients by facility slugs instead of by id in NotificationExperiment

---
+++ db/data/20231109143922_set_up_bangladesh_december_experiment.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class SetUpBangladeshDecemberExperiment < ActiveRecord::Migration[6.1]
+  def up
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
