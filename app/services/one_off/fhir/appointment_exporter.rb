05c02c729e0813b1f212aae61f6066b62e73b9c2
Akshatha <71076330+qptr@users.noreply.github.com>
2023-11-24 19:46:21 +0530

    Add appointment exporter

---
+++ app/services/one_off/fhir/appointment_exporter.rb
@@ -0,0 +1,46 @@
+require "fhir_models"
+
+module OneOff
+  module Fhir
+    class AppointmentExporter
+      attr_reader :appointment
+
+      def initialize(appointment)
+        @appointment = appointment
+      end
+
+      def export
+        FHIR::Appointment.new(
+          status: appointment.status,
+          start: appointment.scheduled_date,
+          cancellationReason: get_cancellation_code,
+          participant: [FHIR::Appointment::Participant.new(
+            actor: FHIR::Reference.new(
+              reference: FHIR::Patient.new(
+                identifier: FHIR::Identifier.new(
+                  value: appointment.patient_id
+                )
+              )
+            )
+          )],
+          identifier: [
+            FHIR::Identifier.new(
+              value: appointment.id.to_s
+            )
+          ],
+          meta: FHIR::Meta.new(
+            lastUpdated: appointment.device_updated_at,
+            createdAt: appointment.device_created_at
+          )
+        )
+      end
+
+      def get_cancellation_code
+        case appointment.cancel_reason
+        when "dead" then "pat-dec"
+        else "pat"
+        end
+      end
+    end
+  end
+end
