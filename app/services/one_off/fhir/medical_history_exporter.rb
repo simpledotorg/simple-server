0520a194132ae9d2c766d9a8731a10bcc791db2d
Akshatha <71076330+qptr@users.noreply.github.com>
2023-11-28 16:48:29 +0530

    Fix medical history exporter
    - Use correct codes for HTN and DM conditions
    - Format dates and pass reference as expected

--- app/services/one_off/fhir/medical_history_exporter.rb
+++ app/services/one_off/fhir/medical_history_exporter.rb
@@ -4,8 +4,8 @@ module OneOff
   module Fhir
     class MedicalHistoryExporter
       attr_reader :medical_history
-      DM_CONDITION_CODE = 123
-      HTN_CONDITION_CODE = 123
+      DM_CONDITION_CODE = "73211009"
+      HTN_CONDITION_CODE = "38341003"
 
       def initialize(medical_history)
         @medical_history = medical_history
@@ -25,7 +25,7 @@ module OneOff
       def generate_condition(condition_code)
         FHIR::Condition.new(
           subject: FHIR::Reference.new(
-            reference: FHIR::Patient.new(
+            id: FHIR::Patient.new(
               identifier: FHIR::Identifier.new(
                 value: medical_history.patient_id
               )
@@ -35,15 +35,15 @@ module OneOff
             FHIR::CodeableConcept.new(
               coding: [
                 FHIR::Coding.new(
-                  system: "http://loinc.org",
+                  system: "http://snomed.info/sct",
                   code: condition_code
                 )
               ]
             )
           ],
           meta: FHIR::Meta.new(
-            lastUpdated: medical_history.device_updated_at,
-            createdAt: medical_history.device_created_at
+            lastUpdated: medical_history.device_updated_at.iso8601,
+            createdAt: medical_history.device_created_at.iso8601
           )
         )
       end
