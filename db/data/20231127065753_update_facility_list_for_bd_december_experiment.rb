0575d3f38e974ad58dafb1091761c9086759f08b
Akshatha <71076330+qptr@users.noreply.github.com>
2023-11-29 12:47:03 +0530

    Address review comment: Fix weird indentation in region filters map

--- db/data/20231127065753_update_facility_list_for_bd_december_experiment.rb
+++ db/data/20231127065753_update_facility_list_for_bd_december_experiment.rb
@@ -15,31 +15,30 @@ class UpdateFacilityListForBdDecemberExperiment < ActiveRecord::Migration[6.1]
     }.values.reduce(:+)
     updated_region_filters = {"facilities" => {"include" => facilities}}
 
-    Experimentation::Experiment.find_by_name("Current Patient December 2023")
-      &.update!(filters: updated_region_filters)
-    Experimentation::Experiment.find_by_name("Stale Patient December 2023")
-      &.update!(filters: updated_region_filters)
+    Experimentation::Experiment.find_by_name("Current Patient December 2023")&.update!(filters: updated_region_filters)
+    Experimentation::Experiment.find_by_name("Stale Patient December 2023")&.update!(filters: updated_region_filters)
   end
 
   def down
     return unless CountryConfig.current_country?("Bangladesh") && SimpleServer.env.production?
 
-    old_region_filters = {"facilities" =>
-                            {"include" => %w[uhc-agailjhara uhc-babuganj-facility uhc-bakerganj uhc-hijla uhc-mehendiganj
-                              uhc-muladi uhc-wazirpur uhc-kathalia-86845c14-b23d-4578-820a-63eb9628422a uhc-nalchity-97cb9397-8ca7-4edb-81ae-2ac8fb1ea93a
-                              uhc-rajapur-40d0e15a-37c4-4833-8f55-446bb210c457 thc-tejgaon-sadar uhc-dhamrai uhc-dohar uhc-nawabganj uhc-damuddya
-                              uhc-goshairhat uhc-sreenagar uhc-tongibari uhc-sirajdikhan uhc-hathazari uhc-fatikchori uhc-anwara uhc-satkania
-                              uhc-lohagara uhc-patiya uhc-rangunia uhc-sitakunda uhc-chandanaish uhc-raozan uhc-banshkhali uhc-boalkhali uhc-karnaphuli
-                              uhc-mirsharai uhc-sandwip uhc-fulgazi uhc-parsuram uhc-daganbhuiya uhc-sonagazi uhc-chhagalniya uhc-atpara uhc-barhatta
-                              uhc-kalmakanda uhc-kendua uhc-khaliajuri uhc-madan uhc-mohanganj uhc-purbadhala uhc-durgapur-dbd6efea-8950-410e-abc3-ed9a329fb9d3
-                              uhc-belkuchi uhc-chowhali uhc-kamarkhanda uhc-kazipur uhc-shahzadpur uhc-tarash uhc-ullapara uhc-bera uhc-bhangura
-                              uhc-faridpur uhc-iswardi uhc-santhia uhc-chatmohar uhc-sujanagar-4b973cb1-d584-4100-91ca-778f218b502e atgharia-upazila-health-complex
-                              uhc-islampur uhc-melandah uhc-sarishabari uhc-dewanganj uhc-bakshiganj uhc-madarganj uhc-jhenaigati uhc-nalitabari uhc-nakhla
-                              uhc-sribordi]}}
+    old_region_filters = {
+      "facilities" =>
+        {
+          "include" => %w[uhc-agailjhara uhc-babuganj-facility uhc-bakerganj uhc-hijla uhc-mehendiganj uhc-sribordi
+            uhc-muladi uhc-wazirpur uhc-kathalia-86845c14-b23d-4578-820a-63eb9628422a uhc-nalchity-97cb9397-8ca7-4edb-81ae-2ac8fb1ea93a
+            uhc-rajapur-40d0e15a-37c4-4833-8f55-446bb210c457 thc-tejgaon-sadar uhc-dhamrai uhc-dohar uhc-nawabganj uhc-damuddya
+            uhc-goshairhat uhc-sreenagar uhc-tongibari uhc-sirajdikhan uhc-hathazari uhc-fatikchori uhc-anwara uhc-satkania
+            uhc-lohagara uhc-patiya uhc-rangunia uhc-sitakunda uhc-chandanaish uhc-raozan uhc-banshkhali uhc-boalkhali uhc-karnaphuli
+            uhc-mirsharai uhc-sandwip uhc-fulgazi uhc-parsuram uhc-daganbhuiya uhc-sonagazi uhc-chhagalniya uhc-atpara uhc-barhatta
+            uhc-kalmakanda uhc-kendua uhc-khaliajuri uhc-madan uhc-mohanganj uhc-purbadhala uhc-durgapur-dbd6efea-8950-410e-abc3-ed9a329fb9d3
+            uhc-belkuchi uhc-chowhali uhc-kamarkhanda uhc-kazipur uhc-shahzadpur uhc-tarash uhc-ullapara uhc-bera uhc-bhangura
+            uhc-faridpur uhc-iswardi uhc-santhia uhc-chatmohar uhc-sujanagar-4b973cb1-d584-4100-91ca-778f218b502e atgharia-upazila-health-complex
+            uhc-islampur uhc-melandah uhc-sarishabari uhc-dewanganj uhc-bakshiganj uhc-madarganj uhc-jhenaigati uhc-nalitabari uhc-nakhla]
+        }
+    }
 
-    Experimentation::Experiment.find_by_name("Current Patient December 2023")
-      &.update!(filters: old_region_filters)
-    Experimentation::Experiment.find_by_name("Stale Patient December 2023")
-      &.update!(filters: old_region_filters)
+    Experimentation::Experiment.find_by_name("Current Patient December 2023")&.update!(filters: old_region_filters)
+    Experimentation::Experiment.find_by_name("Stale Patient December 2023")&.update!(filters: old_region_filters)
   end
 end
