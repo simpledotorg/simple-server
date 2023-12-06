d8f00af2c5502a2d0cf85b4dd7cf45e42a2003f7
Atharva Raykar <atharva@nilenso.com>
2023-12-06 12:27:33 +0530

    import user: separate users from multiple orgs
    The Import User was previously created by finding the first facility in
    the database and assigning the user to that facility. This is a problem
    in any instance that has more than one organization, since it will
    affiliate the import user with whichever organization that first
    facility belonged to.
    
    We resolve this by requiring an organization ID when finding or creating
    an Import User.

--- spec/jobs/bulk_api_import_job_spec.rb
+++ spec/jobs/bulk_api_import_job_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe BulkApiImportJob do
   include ActiveJob::TestHelper
-  before { FactoryBot.create(:facility) } # needed for our bot import user
+  let(:facility) { create(:facility) }
 
   describe "#perform_later" do
     let(:resource) { build_condition_import_resource }
-    let(:job) { described_class.perform_later(resources: [resource], organization_id: "org_id") }
+    let(:job) { described_class.perform_later(resources: [resource], organization_id: facility.organization_id) }
 
     it "queues the job" do
       expect { job }.to have_enqueued_job(described_class).once.on_queue("default")
