d1ccd17f40242f41da6b88fb760432d9746761b6
Akshatha <71076330+qptr@users.noreply.github.com>
2023-12-08 17:53:00 +0530

    Create the indexes concurrently

--- db/migrate/20231208091419_add_indexes_to_reporting_patient_states.rb
+++ db/migrate/20231208091419_add_indexes_to_reporting_patient_states.rb
@@ -1,6 +1,9 @@
 class AddIndexesToReportingPatientStates < ActiveRecord::Migration[6.1]
+  # Cannot create index concurrently inside a transaction block
+  self.disable_ddl_transaction = true
+
   def change
-    add_index :reporting_patient_states, %i[month_date assigned_facility_id], name: "patient_states_month_date_assigned_facility"
-    add_index :reporting_patient_states, %i[month_date assigned_facility_region_id], name: "patient_states_month_date_assigned_facility_region"
+    add_index :reporting_patient_states, %i[month_date assigned_facility_id], name: "patient_states_month_date_assigned_facility", algorithm: :concurrently
+    add_index :reporting_patient_states, %i[month_date assigned_facility_region_id], name: "patient_states_month_date_assigned_facility_region", algorithm: :concurrently
   end
 end
