0646081159d3361ca92fd1e2da3051a5eed9b9ef
Akshatha <71076330+qptr@users.noreply.github.com>
2023-12-12 18:26:28 +0530

    Skip dead patient query

--- app/jobs/dhis2/bangladesh_exporter_job.rb
+++ app/jobs/dhis2/bangladesh_exporter_job.rb
@@ -9,7 +9,7 @@ module Dhis2
         htn_uncontrolled: PatientStates::Hypertension::UncontrolledPatientsQuery.new(facility_id, period).call.count,
         htn_missed_visits: PatientStates::Hypertension::MissedVisitsPatientsQuery.new(facility_id, period).call.count,
         htn_ltfu: PatientStates::Hypertension::LostToFollowUpPatientsQuery.new(facility_id, period).call.count,
-        htn_dead: PatientStates::Hypertension::DeadPatientsQuery.new(facility_id, period).call.count,
+        # htn_dead: PatientStates::Hypertension::DeadPatientsQuery.new(facility_id, period).call.count,
         htn_cumulative_registrations: PatientStates::Hypertension::CumulativeRegistrationsQuery.new(facility_id, period).call.count,
         htn_monthly_registrations: PatientStates::Hypertension::MonthlyRegistrationsQuery.new(facility_id, period).call.count,
         htn_cumulative_assigned_adjusted: PatientStates::Hypertension::AdjustedAssignedPatientsQuery.new(facility_id, period).call.count
