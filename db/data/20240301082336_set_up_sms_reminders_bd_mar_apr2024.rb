840671300939327e73f39f00f39bbd19b9cc7118
Priyanga P Kini <priyangapkini99@gmail.com>
2024-03-04 14:05:30 +0530

    Refactor migration file

--- db/data/20240301082336_set_up_sms_reminders_bd_mar_apr2024.rb
+++ db/data/20240301082336_set_up_sms_reminders_bd_mar_apr2024.rb
@@ -1,26 +1,23 @@
 # frozen_string_literal: true
 
 class SetUpSmsRemindersBdMarApr2024 < ActiveRecord::Migration[6.1]
-
-  EXPERIMENTS_TO_DISCARD = (3..4).map do |month_number|
-    month = Date::ABBR_MONTHNAMES[month_number]
+  EXPERIMENTS_TO_CANCEL = %w[Mar Apr].map do |month|
     {
       current_experiment_name: "Current patients #{month} 2024",
       stale_experiment_name: "Stale patients #{month} 2024"
     }
   end
-  EXPERIMENTS_DATA = (3..4).map do |month_number|
-    month = Date::ABBR_MONTHNAMES[month_number]
+  EXPERIMENTS_DATA = %w[Mar Apr].map do |month|
     {
       current_experiment_name: "Current patients #{month} 2024 2",
       stale_experiment_name: "Stale patients #{month} 2024 2",
-      start_time: DateTime.new(2024, month_number).beginning_of_month,
-      end_time: DateTime.new(2024, month_number).end_of_month
+      start_time: DateTime.new(2024, month).beginning_of_month,
+      end_time: DateTime.new(2024, month).end_of_month
     }
   end
   MAX_PATIENTS_PER_DAY = 5000
-  DISTRICTS = ["Sylhet", "Moulvibazar", "Habiganj", "Sunamganj", "Barishal", "Jhalokathi", " Feni",
-               "Chattogram", "Bandarban", "Pabna", "Rajshahi", "Sirajganj", "Sherpur", "Jamalpur"].freeze
+  DISTRICTS = %w[Sylhet Moulvibazar Habiganj Sunamganj Barishal Jhalokathi Feni
+    Chattogram Bandarban Pabna Rajshahi Sirajganj Sherpur Jamalpur].freeze
   INCLUDED_FACILITY_SLUG = Facility.where(facility_type: "UHC", district: DISTRICTS).pluck(:slug)
 
   REGION_FILTERS = {
@@ -30,7 +27,7 @@ class SetUpSmsRemindersBdMarApr2024 < ActiveRecord::Migration[6.1]
   def up
     return unless CountryConfig.current_country?("Bangladesh") && SimpleServer.env.production?
 
-    EXPERIMENTS_TO_DISCARD.each do |experiment_data|
+    EXPERIMENTS_TO_CANCEL.each do |experiment_data|
       Experimentation::Experiment.current_patients.find_by_name(experiment_data[:current_experiment_name])&.cancel
       Experimentation::Experiment.stale_patients.find_by_name(experiment_data[:stale_experiment_name])&.cancel
     end
@@ -38,7 +35,7 @@ class SetUpSmsRemindersBdMarApr2024 < ActiveRecord::Migration[6.1]
     EXPERIMENTS_DATA.each do |experiment_data|
       ActiveRecord::Base.transaction do
         Experimentation::Experiment.current_patients.create!(
-          name: "#{experiment_data[:current_experiment_name]}",
+          name: (experiment_data[:current_experiment_name]).to_s,
           start_time: experiment_data[:start_time],
           end_time: experiment_data[:end_time],
           max_patients_per_day: MAX_PATIENTS_PER_DAY,
@@ -52,7 +49,7 @@ class SetUpSmsRemindersBdMarApr2024 < ActiveRecord::Migration[6.1]
 
       ActiveRecord::Base.transaction do
         Experimentation::Experiment.stale_patients.create!(
-          name: "#{experiment_data[:stale_experiment_name]}",
+          name: (experiment_data[:stale_experiment_name]).to_s,
           start_time: experiment_data[:start_time],
           end_time: experiment_data[:end_time],
           max_patients_per_day: MAX_PATIENTS_PER_DAY,
@@ -73,5 +70,8 @@ class SetUpSmsRemindersBdMarApr2024 < ActiveRecord::Migration[6.1]
       Experimentation::Experiment.current_patients.find_by_name(experiment_data[:current_experiment_name])&.cancel
       Experimentation::Experiment.stale_patients.find_by_name(experiment_data[:stale_experiment_name])&.cancel
     end
+    # NOTE: We are not reverting the cancelled experiments.
+    # Instead we are only cancelling the new experiments created during migration.
+    # This is because we cannot revert the cancelled experiments.
   end
 end
