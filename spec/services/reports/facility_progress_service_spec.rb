fa93b6554cd698af5130c441f89d0600fed2cd0e
Govind krishna Joshi <govind@nilenso.com>
2024-03-13 03:36:30 +0530

    Fix flaky timezone tests

--- spec/services/reports/facility_progress_service_spec.rb
+++ spec/services/reports/facility_progress_service_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe Reports::FacilityProgressService, type: :model do
 
   context "daily registrations" do
     it "returns counts for HTN or DM patients if diabetes is enabled" do
-      facility = create(:facility, enable_diabetes_management: true)
-      _htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _htn_patient3 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 1.minute.ago)
-
       with_reporting_time_zone do
+        facility = create(:facility, enable_diabetes_management: true)
+        _htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _htn_patient3 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 1.minute.ago)
+
         refresh_views
         service = described_class.new(facility, Period.current)
         expect(service.daily_registrations(3.days.ago.to_date)).to eq(3)
@@ -26,14 +26,13 @@ RSpec.describe Reports::FacilityProgressService, type: :model do
     end
 
     it "returns counts for HTN only if diabetes is not enabled" do
-      skip "time zone issues in CI"
-      facility = create(:facility, enable_diabetes_management: false)
-      _htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
-      _htn_patient3 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 1.minute.ago)
-
       with_reporting_time_zone do
+        facility = create(:facility, enable_diabetes_management: false)
+        _htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 3.days.ago)
+        _htn_patient3 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 1.minute.ago)
+
         refresh_views
         service = described_class.new(facility, Period.current)
         expect(service.daily_registrations(3.days.ago)).to eq(2)
@@ -45,18 +44,18 @@ RSpec.describe Reports::FacilityProgressService, type: :model do
 
   context "daily follow up counts" do
     it "returns counts for HTN or DM patients if diabetes is enabled" do
-      Timecop.freeze do
-        facility = create(:facility, enable_diabetes_management: true)
-        patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        patient3 = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        patient4 = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        one_day_ago # ensure this is set to UTC time zone
-        create(:appointment, recorded_at: two_days_ago, patient: patient1, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_days_ago, patient: patient2, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_days_ago, patient: patient3, facility: facility, user: user)
-        create(:blood_sugar, recorded_at: two_days_ago, patient: patient4, facility: facility, user: user)
-        with_reporting_time_zone do
+      with_reporting_time_zone do
+        Timecop.freeze do
+          facility = create(:facility, enable_diabetes_management: true)
+          patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          patient3 = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          patient4 = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          one_day_ago # ensure this is set to UTC time zone
+          create(:appointment, recorded_at: two_days_ago, patient: patient1, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_days_ago, patient: patient2, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_days_ago, patient: patient3, facility: facility, user: user)
+          create(:blood_sugar, recorded_at: two_days_ago, patient: patient4, facility: facility, user: user)
           refresh_views
           service = described_class.new(facility, Period.current)
           expect(service.daily_follow_ups(two_days_ago.to_date)).to eq(3)
@@ -66,21 +65,21 @@ RSpec.describe Reports::FacilityProgressService, type: :model do
     end
 
     it "returns counts for HTN only if diabetes is not enabled" do
-      Timecop.freeze do
-        facility = create(:facility, enable_diabetes_management: false)
-        htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        undiagnosed_patient = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        one_day_ago # ensure this is set to UTC time zone
-        create(:appointment, recorded_at: seven_days_ago, patient: htn_patient1, facility: facility, user: user)
-        create(:appointment, recorded_at: two_days_ago, patient: htn_patient1, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_days_ago, patient: htn_patient2, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_days_ago, patient: undiagnosed_patient, facility: facility, user: user)
-        create(:blood_sugar, recorded_at: two_days_ago, patient: dm_patient, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
+      with_reporting_time_zone do
+        Timecop.freeze do
+          facility = create(:facility, enable_diabetes_management: false)
+          htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          undiagnosed_patient = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          one_day_ago # ensure this is set to UTC time zone
+          create(:appointment, recorded_at: seven_days_ago, patient: htn_patient1, facility: facility, user: user)
+          create(:appointment, recorded_at: two_days_ago, patient: htn_patient1, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_days_ago, patient: htn_patient2, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_days_ago, patient: undiagnosed_patient, facility: facility, user: user)
+          create(:blood_sugar, recorded_at: two_days_ago, patient: dm_patient, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
 
-        with_reporting_time_zone do
           refresh_views
           service = described_class.new(facility, Period.current)
           expect(service.daily_follow_ups(seven_days_ago.to_date)).to eq(1)
@@ -92,20 +91,20 @@ RSpec.describe Reports::FacilityProgressService, type: :model do
     end
 
     it "includes data from current day for follow up numbers" do
-      Timecop.freeze(Date.today.at_noon) do
-        facility = create(:facility, enable_diabetes_management: true)
-        htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        undiagnosed_patient = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
-        create(:appointment, recorded_at: seven_days_ago, patient: htn_patient1, facility: facility, user: user)
-        create(:appointment, recorded_at: two_minutes_ago, patient: htn_patient1, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_minutes_ago, patient: htn_patient2, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_minutes_ago, patient: undiagnosed_patient, facility: facility, user: user)
-        create(:blood_sugar, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
-        create(:blood_pressure, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
+      with_reporting_time_zone do
+        Timecop.freeze(Date.today.at_noon) do
+          facility = create(:facility, enable_diabetes_management: true)
+          htn_patient1 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          htn_patient2 = create(:patient, :hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          undiagnosed_patient = create(:patient, :without_hypertension, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          dm_patient = create(:patient, :diabetes, registration_facility: facility, registration_user: user, recorded_at: 2.months.ago)
+          create(:appointment, recorded_at: seven_days_ago, patient: htn_patient1, facility: facility, user: user)
+          create(:appointment, recorded_at: two_minutes_ago, patient: htn_patient1, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_minutes_ago, patient: htn_patient2, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_minutes_ago, patient: undiagnosed_patient, facility: facility, user: user)
+          create(:blood_sugar, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
+          create(:blood_pressure, recorded_at: two_minutes_ago, patient: dm_patient, facility: facility, user: user)
 
-        with_reporting_time_zone do
           refresh_views
           service = described_class.new(facility, Period.current)
           expect(service.daily_follow_ups(Date.current)).to eq(3)
