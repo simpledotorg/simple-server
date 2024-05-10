c61156b9f1161318be398f0eeda3bac0649ef52e
Govind krishna Joshi <govind@nilenso.com>
2024-05-10 12:32:00 +0530

    Add a client to collect prometheus metrics

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,24 @@
-if Rails.env.production?
-  require "prometheus_exporter/middleware"
+require "prometheus_exporter/client"
+require "prometheus_exporter/middleware"
 
+CLIENT = PrometheusExporter::Client.default
+REGISTERED_COLLECTORS = {}
+
+class Prometheus
+  attr_reader :client, :registered_collectors
+
+  def self.register(type, name, description = nil)
+    throw "collector: #{name} is already registered" if REGISTERED_COLLECTORS.has_key?(name)
+    REGISTERED_COLLECTORS[name] = CLIENT.register(type, name, description)
+  end
+
+  def self.observe(name, value, labels = {})
+    throw "collector: #{name} is not registered" unless REGISTERED_COLLECTORS.has_key?(name)
+    REGISTERED_COLLECTORS[name].observe(value, labels)
+  end
+end
+
+if Rails.env.production?
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
