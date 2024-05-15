375c1f691c8c72aa92b47743e2d722f5c9c36b5a
Govind krishna Joshi <govind@nilenso.com>
2024-05-15 12:32:51 +0530

    Send reporting view refresh information to prometheus

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -8,17 +8,21 @@ class Prometheus
   attr_reader :client, :registered_collectors
 
   def self.register(type, name, description = nil)
-    throw "collector: #{name} is already registered" if REGISTERED_COLLECTORS.has_key?(name)
+    throw "collector: #{name} is already registered" if exists?(name)
     REGISTERED_COLLECTORS[name] = CLIENT.register(type, name, description)
   end
 
   def self.observe(name, value, labels = {})
-    throw "collector: #{name} is not registered" unless REGISTERED_COLLECTORS.has_key?(name)
+    throw "collector: #{name} is not registered" unless exists?(name)
     REGISTERED_COLLECTORS[name].observe(value, labels)
+  end
+
+  def self.exists?(name)
+    REGISTERED_COLLECTORS.has_key?(name)
   end
 end
 
-if Rails.env.production?
+if Rails.env.production? && Flipper.enabled?(:prometheus_metrics)
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
