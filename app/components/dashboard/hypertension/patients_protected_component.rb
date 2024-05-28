1ed63cee82b44c778ebdd518e36a183232bf317c
Dany Sam <danisam17@gmail.com>
2024-05-28 16:52:31 +0530

    Move patients-protected and treatment-cascade cards to components

---
+++ app/components/dashboard/hypertension/patients_protected_component.rb
@@ -0,0 +1,28 @@
+class Dashboard::Hypertension::PatientsProtectedComponent < ApplicationComponent
+  attr_reader :data, :contactable, :period
+
+  def initialize(region:, data:, period:)
+    @region = region
+    @data = data
+    @period = period
+  end
+
+  def graph_data
+    {
+      **period_data
+    }
+  end
+
+  private
+
+  def period_data
+    {
+      startDate: period.advance(months: -17),
+      endDate: period_info(:name)
+    }
+  end
+
+  def period_info(key)
+    data[:period_info].map { |k, v| [k, v[key]] }.to_h
+  end
+end
