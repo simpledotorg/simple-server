e378128a77ad1e8a4eb908012bec45fc96e88361
Govind Krishna Joshi <govind@nilenso.com>
2024-05-29 16:17:33 +0530

    Add custom prometheus metrics (#5422)
    Co-authored-by: Atharva Raykar <24277692+tfidfwastaken@users.noreply.github.com>

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,28 @@
-if Rails.env.production?
-  require "prometheus_exporter/middleware"
+require "prometheus_exporter/client"
+require "prometheus_exporter/middleware"
 
+CLIENT = PrometheusExporter::Client.default
+REGISTERED_COLLECTORS = {}
+
+class Prometheus
+  attr_reader :client, :registered_collectors
+
+  def self.register(type, name, description = nil)
+    raise "collector: #{name} is already registered" if exists?(name)
+    REGISTERED_COLLECTORS[name] = CLIENT.register(type, name, description)
+  end
+
+  def self.observe(name, value, labels = {})
+    raise "collector: #{name} is not registered" unless exists?(name)
+    REGISTERED_COLLECTORS[name].observe(value, labels)
+  end
+
+  def self.exists?(name)
+    REGISTERED_COLLECTORS.has_key?(name)
+  end
+end
+
+if Rails.env.production? && Flipper.enabled?(:prometheus_metrics)
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
