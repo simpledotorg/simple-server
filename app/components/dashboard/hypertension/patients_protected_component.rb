1defe0b60f5a14e2c72414fbab798dc349c85173
Dany Sam <danisam17@gmail.com>
2024-06-05 19:14:00 +0530

    Show last 36 months of data even if it's not present

--- app/components/dashboard/hypertension/patients_protected_component.rb
+++ app/components/dashboard/hypertension/patients_protected_component.rb
@@ -1,30 +1,28 @@
 class Dashboard::Hypertension::PatientsProtectedComponent < ApplicationComponent
-  attr_reader :data, :contactable, :period
+  attr_reader :data, :contactable, :period, :period_start
 
   def initialize(region:, data:, period:)
     @region = region
     @data = data
-    @period = period
+    # Show only data upto last month
+    @period = period.advance(months: -1)
+    @period_start = period.advance(months: -36)
   end
 
   def graph_data
+    patients_protected = fill_empty_period_data(:controlled_patients)
     {
-      patientsProtected: data[:controlled_patients],
-      patientsProtectedWithSuffix: data[:controlled_patients].map { |k, v| [k, "#{v} patients"] }.to_h,
-      **period_data
+      patientsProtected: patients_protected,
+      patientsProtectedWithSuffix: patients_protected.map { |k, v| [k, "#{v} patients"] }.to_h
     }
   end
 
   private
 
-  def period_data
-    {
-      startDate: period.advance(months: -36),
-      endDate: period_info(:name)
-    }
-  end
-
-  def period_info(key)
-    data[:period_info].map { |k, v| [k, v[key]] }.to_h
+  def fill_empty_period_data(key)
+    period_range = (period_start..period)
+    period_range.each_with_object(Hash.new(0)) do |period, info|
+      info[period] = data[key].fetch(period, 0)
+    end
   end
 end
