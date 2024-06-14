38359c91828d448708f7cda350b9f4be2e7df557
Dany Sam <danisam17@gmail.com>
2024-06-14 15:51:02 +0530

    Configure data with the HTN cascade card

--- app/components/dashboard/hypertension/hypertension_cascade_component.rb
+++ app/components/dashboard/hypertension/hypertension_cascade_component.rb
@@ -1,28 +1,30 @@
 class Dashboard::Hypertension::HypertensionCascadeComponent < ApplicationComponent
   attr_reader :data, :contactable, :period
 
+  FIXED_RATE_WHEN_NO_ESTIMATE = 5 # We should a fixed rate when there's no esimate for the region
+
   def initialize(region:, data:, period:)
     @region = region
     @data = data
     @period = period
+    @estimated_population = @region.estimated_population
+    @cumulative_registrations = data.dig(:cumulative_registrations, period)
+    @under_care_patients = data.dig(:under_care, period)
+    @controlled_patients = data.dig(:controlled_patients, period)
   end
 
-  def graph_data
-    {
-      **period_data
-    }
+  def cumulative_registrations_rate
+    return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
+    @cumulative_registrations * 100 / @estimated_population.population
   end
 
-  private
-
-  def period_data
-    {
-      startDate: period.advance(months: -17),
-      endDate: period_info(:name)
-    }
+  def under_care_patients_rate
+    return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
+    @under_care_patients * 100 / @estimated_population.population
   end
 
-  def period_info(key)
-    data[:period_info].map { |k, v| [k, v[key]] }.to_h
+  def controlled_patients_rate
+    return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
+    @controlled_patients * 100 / @estimated_population.population
   end
 end
