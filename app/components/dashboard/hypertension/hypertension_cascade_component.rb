dcc08389105b7591222c336f2d8d442b59de86f0
Dany Sam <danisam17@gmail.com>
2024-06-18 10:32:24 +0530

    Add links to add HTN estimates

--- app/components/dashboard/hypertension/hypertension_cascade_component.rb
+++ app/components/dashboard/hypertension/hypertension_cascade_component.rb
@@ -1,7 +1,7 @@
 class Dashboard::Hypertension::HypertensionCascadeComponent < ApplicationComponent
   attr_reader :data, :contactable, :period
 
-  FIXED_RATE_WHEN_NO_ESTIMATE = 5 # We should a fixed rate when there's no esimate for the region
+  FIXED_RATE_WHEN_NO_ESTIMATE = "2%" # We should a fixed rate when there's no esimate for the region
 
   def initialize(region:, data:, period:)
     @region = region
@@ -15,16 +15,20 @@ class Dashboard::Hypertension::HypertensionCascadeComponent < ApplicationCompone
 
   def cumulative_registrations_rate
     return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
-    @cumulative_registrations * 100 / @estimated_population.population
+    number_to_percentage(@cumulative_registrations * 100 / @estimated_population.population, precision: 0)
   end
 
   def under_care_patients_rate
     return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
-    @under_care_patients * 100 / @estimated_population.population
+    number_to_percentage(@under_care_patients * 100 / @estimated_population.population, precision: 0)
   end
 
   def controlled_patients_rate
     return FIXED_RATE_WHEN_NO_ESTIMATE unless @estimated_population
-    @controlled_patients * 100 / @estimated_population.population
+    number_to_percentage(@controlled_patients * 100 / @estimated_population.population, precision: 0)
+  end
+
+  def all_districts_have_hypertension_estimate?
+    @region.district_regions.all?(&:estimated_population)
   end
 end
