8d496835ea74350999020b47f4a8e4c1a078e709
Dany Sam <danisam17@gmail.com>
2024-07-22 14:26:45 +0530

    Remove unnecessary

--- spec/services/messaging/mobitel/sms_spec.rb
+++ spec/services/messaging/mobitel/sms_spec.rb
@@ -2,23 +2,6 @@ require "rails_helper"
 
 RSpec.describe Messaging::Mobitel::Sms do
   describe "#send_message" do
-    it "passes sanitised variable content to the API" do
-      mock_api = double("MobitelApiDouble")
-      allow(Messaging::Mobitel::Api).to receive(:new).and_return(mock_api)
-      allow(mock_api).to receive(:send_sms)
-      allow(mock_api).to receive(:send_sms).and_return(200)
-
-      expect(mock_api).to receive(:send_sms).with(
-        recipient_number: "+11001100",
-        message: "Test message"
-      )
-
-      described_class.send_message(
-        recipient_number: "+11001100",
-        message: "Test message"
-      )
-    end
-
     it "raises any response whose body is non 200 as exception" do
       mock_api = double("MobitelApiDouble")
       allow(Messaging::Mobitel::Api).to receive(:new).and_return(mock_api)
