e9072859022cb7379c8a09e2b4cd1e81c7c0ed22
Gyan Gupta <ggupta@rtsl.org>
2024-11-11 17:44:55 +0530

    sc-13777 spec revised

--- spec/views/reports/regions/_header.html.erb_spec.rb
+++ spec/views/reports/regions/_header.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "reports/regions/_header.html.erb", type: :view do
   let(:organization) { create(:organization) }
   let(:current_admin) { create(:admin, :manager) }
   let(:organization_region) { create(:region, name: "Organization", region_type: "organization", path: "organization") }
-  let(:organization_region_1) { create(:region, name: "facility", region_type: "facility", path: "organization.state.district") }
+  let(:facility_region) { create(:region, name: "facility", region_type: "facility", path: "organization.state.district") }
   let(:current_period) { Period.current }
 
   before do
@@ -17,49 +17,38 @@ RSpec.describe "reports/regions/_header.html.erb", type: :view do
 
     allow(view).to receive(:params).and_return({report_scope: "organization", id: organization_region.slug})
     allow(view).to receive(:action_name).and_return("diabetes")
-    allow(current_admin).to receive(:feature_enabled?).with(:organization_reports).and_return(true)
   end
 
   describe "when the feature flag for organization_reports is enabled" do
     context "and we are on the organization-level report" do
       it "renders the organization name in the breadcrumb without a hyperlink" do
         render partial: "reports/regions/header", locals: {current_admin: current_admin}
-        expect(rendered).to include('<a href="/dashboard/districts">All reports</a>')
-        expect(rendered).to match(/<i class="fas fa-chevron-right"><\/i>\s*Organization/)
-        expect(rendered).not_to include('<a href="/reports/regions/organization/organization">Organization</a>')
+        rendered_content = Capybara.string(rendered)
+        expect(rendered_content).to have_link("All reports", href: "/dashboard/districts")
+        expect(rendered_content).to have_selector("i.fas.fa-chevron-right + span", text: "Organization")
+        expect(rendered_content).to have_no_link("Organization", href: "/reports/regions/organization/organization")
       end
     end
 
     context "when the feature flag for organization_reports is enabled and we are on a sub-level report (e.g., state or facility)" do
       let(:sub_region) { create(:region, name: "State Subregion", region_type: "state", path: "organization.state") }
-
-      before do
-        assign(:region, sub_region)
-        assign(:organization, organization)
-        allow(view).to receive(:params).and_return({report_scope: "state", id: sub_region.slug})
-        allow(current_admin).to receive(:feature_enabled?).with(:monthly_state_data_download).and_return(true)
-      end
-
       it "renders the organization name as a hyperlink in the breadcrumb" do
         render partial: "reports/regions/header", locals: {current_admin: current_admin}
-        expect(rendered).to include('<a href="/dashboard/districts">All reports</a>')
-        expect(rendered).to include('<a href="/reports/regions/organization/organization">Organization</a>')
-        expect(rendered).to match(%r{<i class="fas fa-chevron-right"></i>\s*State Subregion})
+        rendered_content = Capybara.string(rendered)
+        expect(rendered_content).to have_link("All reports", href: "/dashboard/districts")
+        expect(rendered_content).to have_link("Organization", href: "/reports/regions/organization/organization")
+        expect(rendered_content).to have_selector("i.fas.fa-chevron-right + span", text: "State Subregion")
       end
     end
+  end
 
-    context "when the feature flag for organization_reports is disabled" do
-      before do
-        allow(current_admin).to receive(:feature_enabled?).with(:organization_reports).and_return(false)
-        allow(current_admin).to receive(:feature_enabled?).with(:dashboard_progress_reports).and_return(false)
-        allow_any_instance_of(UserAccess).to receive(:can_access?).and_return(true)
-        assign(:region, organization_region_1)
-      end
-
+  describe "when the feature flag for organization_reports is disabled" do
+    context "when we are on the all reports page" do
       it "does not render the organization name in the breadcrumb" do
         render partial: "reports/regions/header", locals: {current_admin: current_admin}
-        expect(rendered).to include('<a href="/dashboard/districts">All reports</a>')
-        expect(rendered).not_to include("Organization")
+        rendered_content = Capybara.string(rendered)
+        expect(rendered_content).to have_link("All reports", href: "/dashboard/districts")
+        expect(rendered_content).to have_no_content("Organization")
       end
     end
   end
