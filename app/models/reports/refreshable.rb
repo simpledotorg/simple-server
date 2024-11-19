463d7444d6c1fc2bac9767c6ad37d89cac5aef0e
Atharva Raykar <atharva@nilenso.com>
2024-11-19 13:26:06 +0530

    Refreshable: set local work_mem for before refresh
    This is an optimization to speed up our matviews. The `work_mem` will be
    set to a value configured in our environment variables.

--- app/models/reports/refreshable.rb
+++ app/models/reports/refreshable.rb
@@ -6,11 +6,19 @@ module Reports
     def refresh(transaction: true)
       return refresh_view unless transaction
       ActiveRecord::Base.transaction do
+        set_work_mem if Flipper.enabled?(:optimize_work_mem)
         refresh_view
       end
     end
 
     private
+
+    def set_work_mem
+      work_mem = ENV.fetch("REFRESH_WORK_MEM", nil)
+      return Rails.logger.warn("REFRESH_WORK_MEM is not set, falling back to default value") if work_mem.nil?
+
+      ActiveRecord::Base.connection.execute("SET LOCAL work_mem TO '#{work_mem}'")
+    end
 
     def refresh_view
       ActiveRecord::Base.connection.execute("SET LOCAL TIME ZONE '#{Period::REPORTING_TIME_ZONE}'")
