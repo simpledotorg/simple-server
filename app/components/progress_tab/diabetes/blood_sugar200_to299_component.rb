3560a5cca67484c998ed374cb6325ff69adb63fa
Gyan Gupta <ggupta@rtsl.org>
2024-12-04 13:19:37 +0530

    sc-14227 new files after using feature flag

--- app/components/progress_tab/diabetes/blood_sugar200_to299_component.rb
+++ app/components/progress_tab/diabetes/blood_sugar200_to299_component.rb
@@ -1,49 +1,47 @@
 class ProgressTab::Diabetes::BloodSugar200To299Component < ApplicationComponent
   include AssetsHelper
-  include ActionView::Helpers::NumberHelper
+  include UseWhoStandard
 
   attr_reader :uncontrolled_rates, :uncontrolled, :adjusted_patients, :period_info, :region
 
-  def initialize(uncontrolled_rates:, uncontrolled:, adjusted_patients:, period_info:, region:)
+  def initialize(uncontrolled_rates:, uncontrolled:, adjusted_patients:, period_info:, region:, use_who_standard: nil)
     @uncontrolled_rates = uncontrolled_rates
     @uncontrolled = uncontrolled
     @adjusted_patients = adjusted_patients
     @period_info = period_info
     @region = region
+    @use_who_standard = resolve_use_who_standard(use_who_standard)
   end
 
   def uncontrolled_threshold_key
-    CountryConfig.current_country?(LK_DIABETES_CONSTANT) ? "lk_diabetes_uncontrolled" : "diabetes_uncontrolled"
+    @use_who_standard ? "_fbs" : ""
   end
 
   def uncontrolled_threshold_long
-    I18n.t("progress_tab.diagnosis_report.diagnosis_thresholds.#{uncontrolled_threshold_key}_long")
+    t("bs_over_200_copy.bs_200_to_299.numerator#{uncontrolled_threshold_key}")
   end
 
   def uncontrolled_threshold_title
-    I18n.t("progress_tab.diagnosis_report.diagnosis_thresholds.#{uncontrolled_threshold_key}_title")
+    t("bs_over_200_copy.reports_card_title#{uncontrolled_threshold_key}")
   end
 
   def uncontrolled_threshold_short
-    I18n.t("progress_tab.diagnosis_report.diagnosis_thresholds.#{uncontrolled_threshold_key}_short")
+    t("bs_over_200_copy.bs_200_to_299.title_dm#{uncontrolled_threshold_key}")
   end
 
   def uncontrolled_bar
-    t("progress_tab.diagnosis_report.diagnosis_thresholds.#{uncontrolled_threshold_key}_bar")
+    t("bs_over_200_copy.bs_200_to_299.report_card_lower_bar#{uncontrolled_threshold_key}")
   end
 
   def subtitle_text
-    I18n.t("progress_tab.diagnosis_report.patient_treatment_outcomes.uncontrolled_card.subtitle",
-      facility_name: region.name, diagnosis: "Diabetes", uncontrolled_threshold: uncontrolled_threshold_long)
+    t("bs_over_200_copy.bs_200_to_299.reports_card_subtitle#{uncontrolled_threshold_key}", region_name: @region.name, diagnosis: "Diabetes", controlled_threshold: uncontrolled_threshold_long)
   end
 
   def numerator_text
-    I18n.t("progress_tab.diagnosis_report.patient_treatment_outcomes.uncontrolled_card.help_tooltip.numerator",
-      uncontrolled_threshold: uncontrolled_threshold_long)
+    t("bs_over_200_copy.bs_200_to_299.numerator_dm#{uncontrolled_threshold_key}")
   end
 
   def denominator_text
-    I18n.t("progress_tab.diagnosis_report.patient_treatment_outcomes.uncontrolled_card.help_tooltip.denominator",
-      facility_name: region.name, diagnosis: "Diabetes")
+    t("bs_below_200_copy.denominator", facility_name: @region.name, diagnosis: "Diabetes")
   end
 end
