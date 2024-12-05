8659b8535a649b8593bd10303aaf5561879df917
Gyan Gupta <ggupta@rtsl.org>
2024-12-05 13:29:14 +0530

    sc-14227 modified logic with locale

--- app/components/progress_tab/diabetes/blood_sugar200_to299_component.rb
+++ app/components/progress_tab/diabetes/blood_sugar200_to299_component.rb
@@ -1,6 +1,6 @@
 class ProgressTab::Diabetes::BloodSugar200To299Component < ApplicationComponent
   include AssetsHelper
-  include UseWhoStandard
+  include FlipperHelper
 
   attr_reader :uncontrolled_rates, :uncontrolled, :adjusted_patients, :period_info, :region
 
@@ -11,37 +11,43 @@ class ProgressTab::Diabetes::BloodSugar200To299Component < ApplicationComponent
     @period_info = period_info
     @region = region
     @use_who_standard = resolve_use_who_standard(use_who_standard)
+
+    set_locale_values
   end
 
-  def uncontrolled_threshold_key
-    @use_who_standard ? "_fbs" : ""
-  end
+  private
 
-  def uncontrolled_threshold_long
-    t("bs_over_200_copy.bs_200_to_299.numerator#{uncontrolled_threshold_key}")
-  end
+  def set_locale_values
+    if @use_who_standard
+      @uncontrolled_threshold_title = t("bs_over_200_copy.reports_card_title_fbs")
+      @uncontrolled_threshold_long = t("bs_over_200_copy.bs_200_to_299.numerator_dm_fbs")
+      @uncontrolled_threshold_short = t("bs_over_200_copy.bs_200_to_299.title_dm_fbs")
+      @uncontrolled_threshold_bar = t("bs_over_200_copy.bs_200_to_299.title_dm_fbs")
+      @subtitle_text = t(
+        "bs_over_200_copy.bs_200_to_299.reports_card_subtitle_fbs",
+        region_name: @region.name,
+        diagnosis: "Diabetes",
+        controlled_threshold: @uncontrolled_threshold_long
+      )
+      @numerator_text = t("bs_over_200_copy.bs_200_to_299.numerator_dm_fbs")
+    else
+      @uncontrolled_threshold_title = t("bs_over_200_copy.reports_card_title_dm")
+      @uncontrolled_threshold_long = t("bs_over_200_copy.bs_200_to_299.numerator_dm")
+      @uncontrolled_threshold_short = t("bs_over_200_copy.bs_200_to_299.title_dm")
+      @uncontrolled_threshold_bar = t("bs_over_200_copy.bs_200_to_299.title_dm")
+      @subtitle_text = t(
+        "bs_over_200_copy.bs_200_to_299.reports_card_subtitle",
+        region_name: @region.name,
+        diagnosis: "Diabetes",
+        controlled_threshold: @uncontrolled_threshold_long
+      )
+      @numerator_text = t("bs_over_200_copy.bs_200_to_299.numerator")
+    end
 
-  def uncontrolled_threshold_title
-    t("bs_over_200_copy.reports_card_title#{uncontrolled_threshold_key}")
-  end
-
-  def uncontrolled_threshold_short
-    t("bs_over_200_copy.bs_200_to_299.title_dm#{uncontrolled_threshold_key}")
-  end
-
-  def uncontrolled_bar
-    t("bs_over_200_copy.bs_200_to_299.report_card_lower_bar#{uncontrolled_threshold_key}")
-  end
-
-  def subtitle_text
-    t("bs_over_200_copy.bs_200_to_299.reports_card_subtitle#{uncontrolled_threshold_key}", region_name: @region.name, diagnosis: "Diabetes", controlled_threshold: uncontrolled_threshold_long)
-  end
-
-  def numerator_text
-    t("bs_over_200_copy.bs_200_to_299.numerator_dm#{uncontrolled_threshold_key}")
-  end
-
-  def denominator_text
-    t("bs_below_200_copy.denominator", facility_name: @region.name, diagnosis: "Diabetes")
+    @denominator_text = t(
+      "progress_tab.diagnosis_report.patient_treatment_outcomes.controlled_card.help_tooltip.denominator",
+      facility_name: @region.name,
+      diagnosis: "Diabetes"
+    )
   end
 end
--- app/components/progress_tab/diabetes/control_component.rb
+++ app/components/progress_tab/diabetes/control_component.rb
@@ -1,6 +1,6 @@
 class ProgressTab::Diabetes::ControlComponent < ApplicationComponent
   include AssetsHelper
-  include UseWhoStandard
+  include FlipperHelper
 
   attr_reader :control_rates, :controlled, :adjusted_patients, :period_info, :region
 
@@ -11,33 +11,41 @@ class ProgressTab::Diabetes::ControlComponent < ApplicationComponent
     @period_info = period_info
     @region = region
     @use_who_standard = resolve_use_who_standard(use_who_standard)
+
+    set_locale_values
   end
 
-  def controlled_threshold_key
-    @use_who_standard ? "_fbs" : ""
-  end
+  private
 
-  def controlled_threshold_long
-    t("bs_below_200_copy.numerator#{controlled_threshold_key}")
-  end
+  def set_locale_values
+    if @use_who_standard
+      @controlled_threshold_long = t("bs_below_200_copy.numerator_fbs")
+      @controlled_threshold_short = t("bs_below_200_copy.reports_card_title_fbs")
+      @controlled_threshold_bar = t("bs_below_200_copy.report_card_lower_bar_fbs")
+      @subtitle_text = t(
+        "bs_below_200_copy.reports_card_subtitle_dm_fbs",
+        region_name: @region.name,
+        diagnosis: "Diabetes",
+        controlled_threshold: @controlled_threshold_long
+      )
+      @numerator_text = t("bs_below_200_copy.numerator_dm_fbs")
+    else
+      @controlled_threshold_long = t("bs_below_200_copy.numerator")
+      @controlled_threshold_short = t("bs_below_200_copy.reports_card_title")
+      @controlled_threshold_bar = t("bs_below_200_copy.report_card_lower_bar")
+      @subtitle_text = t(
+        "bs_below_200_copy.reports_card_subtitle_dm",
+        region_name: @region.name,
+        diagnosis: "Diabetes",
+        controlled_threshold: @controlled_threshold_long
+      )
+      @numerator_text = t("bs_below_200_copy.numerator")
+    end
 
-  def controlled_threshold_short
-    t("bs_below_200_copy.reports_card_title#{controlled_threshold_key}")
-  end
-
-  def controlled_threshold_bar
-    t("bs_below_200_copy.report_card_lower_bar#{controlled_threshold_key}")
-  end
-
-  def subtitle_text
-    t("bs_below_200_copy.reports_card_subtitle_dm#{controlled_threshold_key}", region_name: @region.name, diagnosis: "Diabetes", controlled_threshold: controlled_threshold_long)
-  end
-
-  def numerator_text
-    t("bs_below_200_copy.numerator_dm#{controlled_threshold_key}")
-  end
-
-  def denominator_text
-    t("bs_below_200_copy.denominator", facility_name: @region.name, diagnosis: "Diabetes")
+    @denominator_text = t(
+      "progress_tab.diagnosis_report.patient_treatment_outcomes.controlled_card.help_tooltip.denominator",
+      facility_name: @region.name,
+      diagnosis: "Diabetes"
+    )
   end
 end
