80e5577429a525e9afbbe0dba66210350e6a6b98
Gyan Gupta <ggupta@rtsl.org>
2024-12-05 13:29:14 +0530

    sc-14227 modified logic with locale

--- app/helpers/flipper_helper.rb
+++ app/helpers/flipper_helper.rb
@@ -1,4 +1,4 @@
-module UseWhoStandard
+module FlipperHelper
   def resolve_use_who_standard(use_who_standard)
     use_who_standard.nil? ? Flipper.enabled?(:diabetes_who_standard_indicator) : use_who_standard
   end
