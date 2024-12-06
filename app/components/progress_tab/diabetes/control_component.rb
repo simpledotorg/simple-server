9375ace459f9596a0ba1eac660cd69756e5fb903
Gyan Gupta <ggupta@rtsl.org>
2024-12-06 16:34:22 +0530

    sc-14227 added locales

--- app/components/progress_tab/diabetes/control_component.rb
+++ app/components/progress_tab/diabetes/control_component.rb
@@ -39,7 +39,7 @@ class ProgressTab::Diabetes::ControlComponent < ApplicationComponent
         diagnosis: "Diabetes",
         controlled_threshold: @controlled_threshold_long
       )
-      @numerator_text = t("bs_below_200_copy.numerator")
+      @numerator_text = t("bs_below_200_copy.numerator_dm")
     end
 
     @denominator_text = t(
