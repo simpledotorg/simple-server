2cbfcbe83e9ab7fe1dbc7cc2aa8b63269490eecd
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-01-14 14:19:35 +0100

    standardrb --fix

--- db/migrate/20250108140806_v4_patient_summaries.rb
+++ db/migrate/20250108140806_v4_patient_summaries.rb
@@ -3,8 +3,8 @@ class V4PatientSummaries < ActiveRecord::Migration[6.1]
     revert_version = 3
     revert_version = 2 if CountryConfig.current_country?("Ethiopia")
     update_view :materialized_patient_summaries,
-        version: 4,
-        revert_to_version: revert_version,
-        materialized: true
+      version: 4,
+      revert_to_version: revert_version,
+      materialized: true
   end
 end
