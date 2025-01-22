b8cab3e8f006a4cfc06654d8ab5c4da751c80aaf
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-01-22 16:38:59 +0100

    add stale patients

--- db/data/20250121130710_setup_bdh12025_sms_schedule.rb
+++ db/data/20250121130710_setup_bdh12025_sms_schedule.rb
@@ -41,6 +41,20 @@ class SetupBdh12025SmsSchedule < ActiveRecord::Migration[6.1]
           treatment_group.reminder_templates.create!(message: "notifications.set03.free", remind_on_in_days: 3)
         end
       end
+
+      ActiveRecord::Base.transaction do
+        Experimentation::Experiment.stale_patients.create!(
+          name: (experiment_data[:stale_experiment_name]).to_s,
+          start_time: experiment_data[:start_time],
+          end_time: experiment_data[:end_time],
+          max_patients_per_day: MAX_PATIENTS_PER_DAY,
+          filters: REGION_FILTERS
+        ).tap do |experiment|
+          treatment_group = experiment.treatment_groups.create!(description: "cascade_free")
+          treatment_group.reminder_templates.create!(message: "notifications.set02.free", remind_on_in_days: 0)
+          treatment_group.reminder_templates.create!(message: "notifications.set03.free", remind_on_in_days: 3)
+        end
+      end
     end
   end
 
@@ -48,6 +62,7 @@ class SetupBdh12025SmsSchedule < ActiveRecord::Migration[6.1]
     return unless CountryConfig.current_country?("Bangladesh") && SimpleServer.env.production?
     EXPERIMENTS_DATA.each do |experiment_data|
       Experimentation::Experiment.current_patients.find_by_name(experiment_data[:current_experiment_name])&.cancel
+      Experimentation::Experiment.stale_patients.find_by_name(experiment_data[:stale_experiment_name])&.cancel
     end
   end
 end
