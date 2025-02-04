1826a45e1c3a2f387710d28bfda301b1a25401e1
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-02-04 11:19:35 +0100

    Cancel upcoming BD SMSs

---
+++ db/data/20250204101724_cancel_bangladesh_stale_experiments.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CancelBangladeshStaleExperiments < ActiveRecord::Migration[6.1]
+  def up
+    Experimentation::Experiment.upcoming.where(experiment_type: 'stale_patients').each do |experiment|
+      experiment.destroy  # ...because we need the associated notifications gone too
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
