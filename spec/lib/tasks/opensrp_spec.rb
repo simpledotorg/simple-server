d415911f03428c248557036e36674806ff892b1e
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-03-04 13:04:54 +0100

    ... because more standards must be met

--- spec/lib/tasks/opensrp_spec.rb
+++ spec/lib/tasks/opensrp_spec.rb
@@ -3,10 +3,10 @@ require "rails_helper"
 describe "opensrp:export" do
   include_context "rake"
 
-  TEST_CONFIG = "spec/support/fixtures/test-export-config.yml"
+  let(:test_config_file) { "spec/support/fixtures/test-export-config.yml" }
 
   it "should reqiure a config file" do
-    expect(YAML).to receive(:load_file).with(TEST_CONFIG).and_call_original
-    subject.invoke(TEST_CONFIG, "output.json")
+    expect(YAML).to receive(:load_file).with(test_config_file).and_call_original
+    subject.invoke(test_config_file, "output.json")
   end
 end
