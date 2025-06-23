57903a3e1fd46829bc5669a5a95ad543493e2506
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-06-23 16:01:38 +0100

    fix: Singleton Indicators

--- spec/models/dr_rai/indicator_spec.rb
+++ spec/models/dr_rai/indicator_spec.rb
@@ -1,4 +1,12 @@
 require "rails_helper"
 
 RSpec.describe DrRai::Indicator, type: :model do
+  describe "validations" do
+    it "should be singleton" do
+      existing_indicator = DrRai::ContactOverduePatientsIndicator.create
+      new_same_indicator = DrRai::ContactOverduePatientsIndicator.new
+      expect(new_same_indicator).not_to be_valid
+      expect(new_same_indicator.errors.of_kind?(:type, :taken)).to be_truthy
+    end
+  end
 end
