4a565a15fcb74e611d3b32bb2ba3b98ce3366707
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-06-23 17:48:17 +0100

    add specs

--- spec/models/dr_rai/action_plan_spec.rb
+++ spec/models/dr_rai/action_plan_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe DrRai::ActionPlan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#denominator" do
+    context "for numeric targets" do
+      let(:indicator) { DrRai::ContactOverduePatientsIndicator.create }
+      let(:target) { DrRai::NumericTarget.create(indicator: indicator, numeric_value: 20) }
+      let(:district_with_facilities) { setup_district_with_facilities }
+      let(:region) { district_with_facilities[:region] }
+
+      it "should be the numeric_value" do
+        action_plan = DrRai::ActionPlan.new(dr_rai_target: target,
+          dr_rai_indicator: indicator,
+          region: region,
+          statement: "TODO")
+        expect(action_plan.denominator).to eq 20
+      end
+    end
+  end
 end
