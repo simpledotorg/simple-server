9079cd58c693f510452e5cf524c78ffad7ffb91f
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-08-13 18:39:40 +0100

    added queries to populate the table

---
+++ app/queries/dr_rai/query_factory.rb
@@ -0,0 +1,71 @@
+module DrRai
+  # Query Factory
+  #
+  # When creating SQL-backed indicators, these indicators come with their own
+  # queries. The query factory tells the query necessary for certain
+  # indicators. By default, it vends one query for inserting, and another for
+  # updating. The primary client for this is the DrRai::DataService which uses
+  # this to populate the data in the tables.
+  class QueryFactory
+    attr_reader :from_date, :to_date
+
+    class << self
+      def for klazz, from: nil, to: nil
+        raise unless klazz < ApplicationRecord
+
+        instance = nil
+
+        case
+        when klazz <= Data::Titration
+          instance = DrRai::TitrationQueryFactory.new(from, to)
+        else
+          raise "Unsupported"
+        end
+
+        instance
+      end
+    end
+
+    def initialize from, to
+      @from = from
+      @to = to
+
+      set_date_boundaries!
+    end
+
+    def inserter
+      # Format should be
+      #   insert into <tbl> ([...columns]) [..query]
+      raise "Unimplemented"
+    end
+
+    def updater
+      # Format should be
+      #   merge into <tbl> as t
+      #   using [...query]
+      #   on month_date
+      #   when not matched and [column compare - insert clause] then
+      #     insert values ([select values])
+      #   when matched and [column compare - update clause] then
+      #     update set [col = old_col <op> new_col];
+      # see https://www.postgresql.org/docs/current/sql-merge.html#id-1.9.3.156.9
+      raise "Unimplemented"
+    end
+
+    private
+
+    def set_date_boundaries!
+      if @from.nil?
+        @from_date = 1.year.ago.to_date
+      else
+        @from_date = @from
+      end
+
+      if @to.nil?
+        @to_date = Date.today
+      else
+        @to_date = @to
+      end
+    end
+  end
+end
