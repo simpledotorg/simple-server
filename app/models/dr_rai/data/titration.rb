033a8763248ff863fc47a2d5e03afadb798eced3
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-08-13 22:54:12 +0100

    make the titration data chartable

--- app/models/dr_rai/data/titration.rb
+++ app/models/dr_rai/data/titration.rb
@@ -1,3 +1,9 @@
 class DrRai::Data::Titration < ApplicationRecord
+  include DrRai::Chartable
+
   default_scope { where(month_date: 1.year.ago..Date.today) }
+
+  chartable_internal_keys :follow_up_count, :titrated_count
+  chartable_period_key :month_date
+  chartable_outer_grouping :facility_name
 end
