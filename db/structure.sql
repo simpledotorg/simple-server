2bcbfd88a861c50ed33ccf444a7e5a421247d7f6
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2025-08-15 11:51:57 +0100

    cleaning 04

--- db/structure.sql
+++ db/structure.sql
@@ -8186,34 +8186,6 @@ CREATE INDEX reporting_patient_states_titrated ON ONLY simple_reporting.reportin
 
 
 --
--- Name: reporting_patient_states_202404_assigned_district_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_district ATTACH PARTITION simple_reporting.reporting_patient_states_202404_assigned_district_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202404_assigned_facility_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_facility ATTACH PARTITION simple_reporting.reporting_patient_states_202404_assigned_facility_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202407_assigned_district_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_district ATTACH PARTITION simple_reporting.reporting_patient_states_202407_assigned_district_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202407_assigned_facility_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_facility ATTACH PARTITION simple_reporting.reporting_patient_states_202407_assigned_facility_region_id_idx;
-
-
---
 -- Name: reporting_patient_states_2024_hypertension_htn_care_state__idx1; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
 --
 
@@ -8239,34 +8211,6 @@ ALTER INDEX simple_reporting.patient_states_month_date_registration_facility_reg
 --
 
 ALTER INDEX simple_reporting.patient_states_month_date_registration_facility_region ATTACH PARTITION simple_reporting.reporting_patient_states_2024_registration_facility_region_idx1;
-
-
---
--- Name: reporting_patient_states_202506_assigned_district_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_district ATTACH PARTITION simple_reporting.reporting_patient_states_202506_assigned_district_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202506_assigned_facility_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_facility ATTACH PARTITION simple_reporting.reporting_patient_states_202506_assigned_facility_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202507_assigned_district_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_district ATTACH PARTITION simple_reporting.reporting_patient_states_202507_assigned_district_region_id_idx;
-
-
---
--- Name: reporting_patient_states_202507_assigned_facility_region_id_idx; Type: INDEX ATTACH; Schema: simple_reporting; Owner: -
---
-
-ALTER INDEX simple_reporting.patient_states_assigned_facility ATTACH PARTITION simple_reporting.reporting_patient_states_202507_assigned_facility_region_id_idx;
 
 
 --
