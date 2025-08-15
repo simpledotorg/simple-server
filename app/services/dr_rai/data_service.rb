7974feff21ae64050c6378acca3cab937aa765b2
Igbanam Ogbuluijah <390059+igbanam@users.noreply.github.com>
2025-08-15 14:47:30 +0100

    feat(Dr. Rai Reports): Optimize Titration Indicator Data (#5650)
    **Story card:**
    [sc-16391](https://app.shortcut.com/simpledotorg/story/16391/optimize-data-load-for-titration-indicator)
    
    ## Because
    
    Running the SQL query within the reqyest lifecycle causes a timeout in
    environments which have plenty data.
    
    ## This addresses
    
    - Creating a table / model which would hold the data for the indicator
    - Load in the data from teh SQL query
    - Adding a rake task for populating the data; manually and on schedule
    
    ## Test instructions
    
    suite-tests

---
+++ app/services/dr_rai/data_service.rb
@@ -0,0 +1,36 @@
+module DrRai
+  class DataService
+    class << self
+      def populate klazz, timeline: nil
+        raise "Can only populate models" unless klazz < ApplicationRecord
+        if timeline.present?
+          raise "timeline must be Date range" unless timelines.is_a?(Range)
+        end
+        new(klazz, timeline).populate!
+      end
+    end
+
+    def initialize klazz, timeline
+      @klazz = klazz
+      @timeline = timeline
+      @timeline = 1.year.ago.to_date..Date.today if @timeline.nil?
+      @query_factory = QueryFactory.for(klazz, from: @timeline.begin, to: @timeline.end)
+    end
+
+    def populate!
+      @query = if inserting?
+        @query_factory.inserter
+      else
+        @query_factory.updater
+      end
+
+      ApplicationRecord.connection.exec_query(@query)
+    end
+
+    private
+
+    def inserting?
+      @klazz.where(month_date: @timeline).count == 0
+    end
+  end
+end
