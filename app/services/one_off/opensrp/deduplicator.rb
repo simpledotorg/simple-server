ae36b3d4bb39426f919fb96ff027694e35c66655
Igbanam <390059+igbanam@users.noreply.github.com>
2025-09-10 12:05:09 +0000

    feat(OpenSRP): Deduplicator (#5679)
    …continuing #5677

--- app/services/one_off/opensrp/deduplicator.rb
+++ app/services/one_off/opensrp/deduplicator.rb
@@ -21,32 +21,50 @@ module OneOff
     end
 
     class Deduplicator
-      AFFECTED_ENTITIES = %w[
-        Appointment
-        BloodPressure
-        BloodSugar
+      # The order is important here. Patient must be last.
+      MUTABLE_ENTITIES = %w[
         MedicalHistory
-        PrescriptionDrug
         Patient
       ].freeze
-      # The order is important here. Patient must be last.
 
-      def self.call!
-        new.call!
+      # These are the immutable associations to the patient record, for which
+      # our merge strategy is to point the old record to the new patient.
+      # i.e. take the old record and set the patient_id to the new record
+      IMMUTABLE_PATIENT_ASSOCIATIONS = %i[
+        appointments
+        blood_pressures
+        blood_sugars
+        call_results
+        encounters
+        notifications
+        phone_numbers
+        prescription_drugs
+        teleconsultations
+        treatment_group_memberships
+      ]
+
+      def self.call! duplicates = nil
+        new(duplicates).call!
       end
 
-      def initialize
-        @duplicates = ImportedDuplicatesQuery.call
+      def initialize duplicates
+        @duplicates = if duplicates.nil?
+          ImportedDuplicatesQuery.call
+        else
+          duplicates
+        end
       end
 
       def call!
         @duplicates.each do |old_id, new_id|
-          AFFECTED_ENTITIES.each do |entity|
-            deduplicator = [
-              Module.nesting[1],
-              "Deduplicators",
-              "For#{entity}"
-            ].join("::").constantize
+          IMMUTABLE_PATIENT_ASSOCIATIONS.each do |association|
+            entity = "ImmutableEntity"
+            deduplicator = deduplicator_class(entity)
+            deduplicator.call! old_id, new_id, association
+          end
+
+          MUTABLE_ENTITIES.each do |entity|
+            deduplicator = deduplicator_class(entity)
             deduplicator.call! old_id, new_id
           end
         end
@@ -57,6 +75,16 @@ module OneOff
       def deprecate_old_patients
         @old_patients = @duplicates.map(&:first)
         Patient.where(id: @old_patients).discard!
+      end
+
+      private
+
+      def deduplicator_class entity
+        [
+          Module.nesting[1],
+          "Deduplicators",
+          "For#{entity}"
+        ].join("::").constantize
       end
     end
   end
