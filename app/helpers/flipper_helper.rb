7a69301b855e3c3f3c047e2bbe10d1c6833582ad
Gyan Gupta <ggupta@rtsl.org>
2025-09-17 18:20:52 +0530

    sc-16587 added spec & resolved comments

--- app/helpers/flipper_helper.rb
+++ app/helpers/flipper_helper.rb
@@ -1,9 +1,17 @@
-module FlipperHelper
+module FlipperHelper  
   def resolve_use_who_standard(use_who_standard)
     use_who_standard.nil? ? Flipper.enabled?(:diabetes_who_standard_indicator) : use_who_standard
   end
 
+  def all_district_overview_feature_enabled?
+    Flipper.enabled?(:all_district_overview, current_admin)
+  end
+
   def all_district_overview_enabled?
-    Flipper.enabled?(:all_district_overview) && params[:facility_group] == "all-districts"
+    all_district_overview_feature_enabled? && params[:facility_group] == "all-districts"
+  end
+
+  def access_all_districts_overview?
+    all_district_overview_feature_enabled? && accessible_organization_facilities.present?
   end
 end
