78a4e0ffb552967d6f255f5a77af722e1c366e47
Gyan Gupta <ggupta@rtsl.org>
2025-09-17 20:27:15 +0530

    sc-16587: resolve comments, add specs, optimize code

--- spec/flipper_helper.rb
+++ spec/flipper_helper.rb
@@ -8,8 +8,4 @@ module FlipperHelpers
     allow(Flipper).to receive(:enabled?).and_call_original
     allow(Flipper).to receive(:enabled?).with(*args).and_return(false)
   end
-
-  def all_district_overview_enabled?
-    Flipper.enabled?(:all_district_overview, current_admin) && params[:facility_group] == "all-districts"
-  end
 end
