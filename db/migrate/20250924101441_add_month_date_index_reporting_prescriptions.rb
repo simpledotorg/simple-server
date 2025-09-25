2b9a86f40b887d4b4273f23ba159f8c97e61f7f2
Ayushi Agrawal <ayushiagrawal@RTSL-P172G93770.local>
2025-09-25 17:57:09 +0530

    PR feedback comments

--- db/migrate/20250924101441_add_month_date_index_reporting_prescriptions.rb
+++ db/migrate/20250924101441_add_month_date_index_reporting_prescriptions.rb
@@ -1,7 +1,9 @@
 class AddMonthDateIndexReportingPrescriptions < ActiveRecord::Migration[6.1]
+  self.disable_ddl_transaction = true
+
   def up
     add_index :reporting_prescriptions, [:month_date],
-      name: "reporting_prescriptions_month_date"
+      name: "reporting_prescriptions_month_date", algorithm: :concurrently
   end
 
   def down
