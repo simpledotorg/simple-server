e1f9add54a9de09797b0dd132ffd71ec8926bbf4
Ayushi Agrawal <ayushiagrawal@RTSL-P172G93770.local>
2025-11-24 17:23:10 +0530

    fixing failures on db:schema:load

--- db/migrate/20251120141950_update_latest_blood_pressures_per_patient_per_months_to_version7.rb
+++ db/migrate/20251120141950_update_latest_blood_pressures_per_patient_per_months_to_version7.rb
@@ -7,36 +7,36 @@ class UpdateLatestBloodPressuresPerPatientPerMonthsToVersion7 < ActiveRecord::Mi
     execute <<~SQL
       CREATE MATERIALIZED VIEW public.latest_blood_pressures_per_patient_per_months AS
       WITH registered_patients AS (
-      SELECT DISTINCT id,
-          registration_facility_id,
-          assigned_facility_id,
-          status,
-          recorded_at
-      FROM patients
-      WHERE deleted_at IS NULL
-      AND diagnosed_confirmed_at IS NOT NULL
+          SELECT DISTINCT id,
+              registration_facility_id,
+              assigned_facility_id,
+              status,
+              recorded_at
+          FROM public.patients
+          WHERE deleted_at IS NULL
+          AND diagnosed_confirmed_at IS NOT NULL
       )
-      SELECT DISTINCT ON (patient_id, year, month)
-        blood_pressures.id AS bp_id,
-        blood_pressures.patient_id AS patient_id,
-        registered_patients.registration_facility_id AS registration_facility_id,
-        registered_patients.assigned_facility_id AS assigned_facility_id,
-        registered_patients.status as patient_status,
-        blood_pressures.facility_id AS bp_facility_id,
-        blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS bp_recorded_at,
-        registered_patients.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS patient_recorded_at,
-        blood_pressures.systolic AS systolic,
-        blood_pressures.diastolic AS diastolic,
-        blood_pressures.deleted_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS deleted_at,
-        medical_histories.hypertension as medical_history_hypertension,
-        cast(EXTRACT(MONTH FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS month,
-        cast(EXTRACT(QUARTER FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS quarter,
-        cast(EXTRACT(YEAR FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS year
+      SELECT DISTINCT ON (blood_pressures.patient_id, (date_part('year'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text), (date_part('month'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text)) 
+          blood_pressures.id AS bp_id,
+          blood_pressures.patient_id AS patient_id,
+          registered_patients.registration_facility_id AS registration_facility_id,
+          registered_patients.assigned_facility_id AS assigned_facility_id,
+          registered_patients.status as patient_status,
+          blood_pressures.facility_id AS bp_facility_id,
+          timezone('UTC'::text, timezone('UTC'::text, blood_pressures.recorded_at)) AS bp_recorded_at,
+          timezone('UTC'::text, timezone('UTC'::text, registered_patients.recorded_at)) AS patient_recorded_at,
+          blood_pressures.systolic AS systolic,
+          blood_pressures.diastolic AS diastolic,
+          timezone('UTC'::text, timezone('UTC'::text, blood_pressures.deleted_at)) AS deleted_at,
+          medical_histories.hypertension as medical_history_hypertension,
+          date_part('month'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS month,
+          date_part('quarter'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS quarter,
+          date_part('year'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS year
       FROM blood_pressures JOIN registered_patients ON registered_patients.id = blood_pressures.patient_id
-        LEFT JOIN medical_histories ON medical_histories.patient_id = blood_pressures.patient_id
-        WHERE blood_pressures.deleted_at IS NULL
-        ORDER BY patient_id, year, month, blood_pressures.recorded_at DESC, bp_id
-        WITH NO DATA;
+      LEFT JOIN medical_histories ON medical_histories.patient_id = blood_pressures.patient_id
+      WHERE blood_pressures.deleted_at IS NULL
+      ORDER BY patient_id, year, month, blood_pressures.recorded_at DESC, bp_id
+      WITH NO DATA;
     SQL
 
     add_index "latest_blood_pressures_per_patient_per_months", ["assigned_facility_id"], name: "index_bp_months_assigned_facility_id"
--- db/views/latest_blood_pressures_per_patient_per_months_v07.sql
+++ db/views/latest_blood_pressures_per_patient_per_months_v07.sql
@@ -4,26 +4,26 @@ WITH registered_patients AS (
         assigned_facility_id,
         status,
         recorded_at
-    FROM patients
+    FROM public.patients
     WHERE deleted_at IS NULL
     AND diagnosed_confirmed_at IS NOT NULL
 )
-SELECT DISTINCT ON (patient_id, year, month)
+SELECT DISTINCT ON (blood_pressures.patient_id, (date_part('year'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text), (date_part('month'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text)) 
     blood_pressures.id AS bp_id,
     blood_pressures.patient_id AS patient_id,
     registered_patients.registration_facility_id AS registration_facility_id,
     registered_patients.assigned_facility_id AS assigned_facility_id,
     registered_patients.status as patient_status,
     blood_pressures.facility_id AS bp_facility_id,
-    blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS bp_recorded_at,
-    registered_patients.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS patient_recorded_at,
+    timezone('UTC'::text, timezone('UTC'::text, blood_pressures.recorded_at)) AS bp_recorded_at,
+    timezone('UTC'::text, timezone('UTC'::text, registered_patients.recorded_at)) AS patient_recorded_at,
     blood_pressures.systolic AS systolic,
     blood_pressures.diastolic AS diastolic,
-    blood_pressures.deleted_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC' AS deleted_at,
+    timezone('UTC'::text, timezone('UTC'::text, blood_pressures.deleted_at)) AS deleted_at,
     medical_histories.hypertension as medical_history_hypertension,
-    cast(EXTRACT(MONTH FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS month,
-    cast(EXTRACT(QUARTER FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS quarter,
-    cast(EXTRACT(YEAR FROM blood_pressures.recorded_at AT TIME ZONE 'UTC' AT TIME ZONE (SELECT current_setting('TIMEZONE'))) as text) AS year
+    date_part('month'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS month,
+    date_part('quarter'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS quarter,
+    date_part('year'::text, timezone(( SELECT current_setting('TIMEZONE'::text) AS current_setting), timezone('UTC'::text, blood_pressures.recorded_at)))::text AS year
 FROM blood_pressures JOIN registered_patients ON registered_patients.id = blood_pressures.patient_id
 LEFT JOIN medical_histories ON medical_histories.patient_id = blood_pressures.patient_id
 WHERE blood_pressures.deleted_at IS NULL
