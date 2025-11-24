13ab1f2f0f24e0cea37ad4e872791d9b009da1ec
Gyan Gupta <ggupta@rtsl.org>
2025-11-24 12:05:57 +0530

    sc-17251: handled backward compatibility for screening flow

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -58,6 +58,17 @@ class MedicalHistory < ApplicationRecord
   def update_patient_diagnosed_confirmed_at
     return if patient.blank?
 
+    if htn_diagnosed_at.nil? && dm_diagnosed_at.nil?
+      return if hypertension_suspected? || diabetes_suspected?
+
+      if (hypertension_yes? || hypertension_no?) || (diabetes_yes? || diabetes_no?)
+        if patient.diagnosed_confirmed_at.nil? && patient.recorded_at.present?
+          patient.update_columns(diagnosed_confirmed_at: patient.recorded_at)
+        end
+      end
+      return
+    end
+
     earliest = [htn_diagnosed_at, dm_diagnosed_at].compact.min
     return if earliest.blank?
 
