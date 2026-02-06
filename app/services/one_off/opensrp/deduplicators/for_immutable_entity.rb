53f6e4b41f77031af08ffc663c48d670ff71a6f4
Owajigbanam Ogbuluijah <igbanam@rtsl.org>
2026-02-06 14:46:02 +0100

    fix(Deduplicator): Duplicate Immutable Associations

--- app/services/one_off/opensrp/deduplicators/for_immutable_entity.rb
+++ app/services/one_off/opensrp/deduplicators/for_immutable_entity.rb
@@ -9,13 +9,29 @@ module OneOff
         end
 
         def merge
-          old_patient.send(@association).each do |associated|
-            associated.patient = new_patient
-            associated.save!
+          if is_has_many?(@association)
+            old_patient.send(@association).each do |associated|
+              new_patient.send(@association) << associated.dup
+            end
+          else
+            old_patient.send(@association).each do |associated|
+              duplicate = associated.dup
+              duplicate.patient = new_patient
+              duplicate.save!
+            end
           end
 
           # return the old patient object for the chain
           old_patient
+        end
+
+        private
+
+        def is_has_many? assoc
+          Patient
+            .reflect_on_all_associations(:has_many)
+            .map(&:name)
+            .include?(assoc)
         end
       end
     end
